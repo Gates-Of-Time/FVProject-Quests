0015b45864b77a5a922b68b1535bcb23b2246252
TurmoilToad <TurmoilToad@gmail.com>
2018-02-11 14:33:16 -0500

    Update clockwork_scrubber.pl

--- akanon/clockwork_scrubber.pl
+++ akanon/clockwork_scrubber.pl
@@ -23,7 +23,7 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
 	#:: Check for handin of 12164 - Scrubber Key (Rogue Scrubber Key)
 	if (plugin::check_handin(%itemcount, 12164 => 1)) {
-		if $miner628 eq "true") {
+		if ($miner628 eq "true") {
 			quest::emote(".wizz.click.628.");
 			#:: Choose a random 12162 - Gnome Take (Good Take For Rogues), 12167 - Gnome Take (Bad Take For Rogues)
 			$gnometake = quest::ChooseRandom(12162,12167);
@@ -34,7 +34,7 @@ sub EVENT_ITEM {
 			quest::ding();
 			quest::exp(500);
 		}
-		if $miner628 eq "false") {
+		if ($miner628 eq "false") {
 			plugin::return_items(%itemcount); # return unused items
 		}
 	}
