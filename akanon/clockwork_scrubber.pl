f4a5fdec9d8689ed8f774797b12b1be2936f36f1
TurmoilToad <TurmoilToad@gmail.com>
2018-02-11 14:49:31 -0500

    Update clockwork_scrubber.pl

--- akanon/clockwork_scrubber.pl
+++ akanon/clockwork_scrubber.pl
@@ -7,7 +7,6 @@ sub EVENT_SPAWN {
 	my $random = int(rand(100));
 	if ($random <= 5) {
 		$miner628 = "true";
-		quest::ding();
 	}
 }
 
@@ -23,21 +22,16 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	#:: Check for handin of 12164 - Scrubber Key (Rogue Scrubber Key)
-	if (plugin::check_handin(%itemcount, 12164 => 1)) {
-		if ($miner628 eq "true") {
-			quest::emote(".wizz.click.628.");
-			#:: Choose a random 12162 - Gnome Take (Good Take For Rogues), 12167 - Gnome Take (Bad Take For Rogues)
-			$gnometake = quest::ChooseRandom(12162,12167);
-			#:: Give one of the Gnome Take items
-			quest::summonitem($gnometake);
-			#:: Set faction
-			quest::faction(45,-10); 	#:: -10 Clockwork Gnome
-			quest::ding();
-			quest::exp(500);
-		}
-		if ($miner628 eq "false") {
-			plugin::return_items(%itemcount); # return unused items
-		}
+	if ($miner628 eq "true" && plugin::check_handin(%itemcount, 12164 => 1)) {
+		quest::emote(".wizz.click.628.");
+		#:: Choose a random 12162 - Gnome Take (Good Take For Rogues), 12167 - Gnome Take (Bad Take For Rogues)
+		$gnometake = quest::ChooseRandom(12162,12167);
+		#:: Give one of the Gnome Take items
+		quest::summonitem($gnometake);
+		#:: Set faction
+		quest::faction(45,-10); 	#:: -10 Clockwork Gnome
+		quest::ding();
+		quest::exp(500);
 	}
 	plugin::return_items(%itemcount); # return unused items
 }
