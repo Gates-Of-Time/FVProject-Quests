60cdcf9ceac28ccb06f16c151780ec51378164c8
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-16 13:10:32 -0500

    Update Myre.pl
    Added missing quest instruction dialogue

--- kaladimb/Myre.pl
+++ kaladimb/Myre.pl
@@ -2,5 +2,9 @@ sub EVENT_SAY {
 if($text=~/Hail/i){
 quest::say("I am not here to chitchat dear. If you need some gear, I can help. If you want to talk, go find a tavern."); }
 }
+sub EVENT_SAY { 
+if($text=~/I am from the Eldritch Collective and desire the bozinite pestles./i){
+quest::say("So you came to get the bozinite pestles, eh? Well, dearie, we have no bozinite pestles. We don't have enough bozinite to spare. One of those wondrous clockwork creatures from Ak'Anon has gone and found a home amongst us dwarves. He is sucking up what little bozinite we have mined. You go and find that little bugger and bring me some of the bozinite he slurped up and I will make you one bozinite pestle. Good luck. He is one quick hunk of metal."); }
+}
 #END of FILE Zone:kaladimb  ID:67016 -- Myre 
 
