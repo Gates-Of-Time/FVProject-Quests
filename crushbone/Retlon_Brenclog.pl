f62a107951aba577ceafdb8258fbbe5065a1e9fe
TurmoilToad <TurmoilToad@gmail.com>
2018-02-18 13:12:54 -0500

    Update Retlon_Brenclog.pl

--- crushbone/Retlon_Brenclog.pl
+++ crushbone/Retlon_Brenclog.pl
@@ -1,14 +1,15 @@
 sub EVENT_SAY {
-if ($text=~/Hail/i) {
+	if ($text=~/Hail/i) {
 		quest::say("Oh my word!! A rescuer!! Hurry! [" . quest::saylink("Get me out") . "] of these chains!! Go find the orc who has the keys!");
 	}
-if ($text=~/Get me out/i) {
+	if ($text=~/Get me out/i) {
 		quest::say("I am from Ak'Anon! I must return a [" . quest::saylink("spellbook") . "] to the Eldritch Collective!");
 	}
-if ($text=~/spellbook/i) {
+	if ($text=~/spellbook/i) {
 		quest::say("What?! I am stuck here and you are worried about a spellbook?! Forget that! Just get the keys to my shackles from one of the orcs! HURRY!");
 	}
 }
+
 sub EVENT_ITEM {
 	#:: Turn in for 20015 -  Shackle Key 15
 	if (plugin::check_handin(\%itemcount, 20015 => 1)) {
@@ -22,7 +23,8 @@ sub EVENT_ITEM {
 		quest::faction(176,1); 		#:: + King Ak'Anon
 		quest::faction(115,1); 		#:: + Gem Choppers
 		quest::faction(322,-2); 	#:: - The Dead
-		quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+		#:: Give a small amount of cash copper - plat
+		quest::givecash(0,3,7,0);	
 		#:: Set Depop Timer
 		quest::settimer("depop",240);
 	}
@@ -38,8 +40,19 @@ sub EVENT_ITEM {
 		quest::faction(176,1); 		#:: + King Ak'Anon
 		quest::faction(115,1); 		#:: + Gem Choppers
 		quest::faction(322,-2); 	#:: - The Dead
-		quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+		#:: Give a small amount of cash copper - plat
+		quest::givecash(0,3,7,0);	
 	}
 	plugin::return_items(\%itemcount);
 	quest::say("No, no!! I do not need this!! Get me key number 15!!");
+}
+
+sub EVENT_TIMER {
+	#:: Catch the "depop" timer
+	if ($timer eq "depop") {
+		#:: Stop the timer "depop"
+		quest::stoptimer("depop");
+		#:: Depop with spawn timer active
+		quest::depop_withtimer();
+	}
 }
