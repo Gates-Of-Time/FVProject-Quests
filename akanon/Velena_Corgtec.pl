221b6cf4bdb5de8417ceaee2eadb9dfb440c25ea
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-18 14:00:08 -0500

    Update Velena_Corgtec.pl
    Added Tradeskill basics code

--- akanon/Velena_Corgtec.pl
+++ akanon/Velena_Corgtec.pl
@@ -11,9 +11,19 @@ sub EVENT_ENTER {
         $client->Message(15,"A diminutive, but powerful looking gnome stands before you. 'I am Velena Corgtec. The Dark Reflection has called you. Read the note in your inventory and hand it to me when you wish to begin your training. Your destiny awaits!'");
   }
 }
+sub EVENT_SAY {
+     if ($text=~/trades/i) {
+        quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [" . quest::saylink("second book") . "], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		quest::summonitem(51121); # Tradeskill Basics : Volume I
+     }
+	if($text=~/second book/i){ 
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		quest::summonitem(51122); # Tradeskill Basics : Volume II
+	}
+}
 sub EVENT_ITEM {
     if (plugin::check_handin(\%itemcount, 18704 => 1)) { #Old Folded Letter
-        quest::Say("A new wizard pledging to our cause is always welcome. Take this robe and see Tilkzog Mournumder when you are ready to begin your training.");
+        quest::Say("A new wizard pledging to our cause is always welcome. Take this robe and see Tilkzog Mournumder when you are ready to begin your training. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [" . quest::saylink("trades") . "] you will have available to you.");
 		quest::summonitem(13524); #Dark Gold Felt Robe*
 		quest::faction(76,-10); # Deep Muses
 		quest::faction(91,-10); # eldritch collective
