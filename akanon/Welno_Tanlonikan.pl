beac79ec0983692746cb3907a72230f108d54999
TurmoilToad <TurmoilToad@gmail.com>
2018-02-18 18:15:44 -0500

    Update Welno_Tanlonikan.pl

--- akanon/Welno_Tanlonikan.pl
+++ akanon/Welno_Tanlonikan.pl
@@ -14,27 +14,34 @@ sub EVENT_ENTER {
 		$client->Message(15,"A voice echoes throughout the room as you take in your surroundings. 'Hey you! This is Welno Tanlonikan. Remember me? Read the note in your inventory and then hand it to me. Hurry up! We don't have all day! You owe me a lot of money!'");
 	}
 }
+
 sub EVENT_SAY {
-     if ($text=~/trades/i) {
-        quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [" . quest::saylink("second book") . "], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
-		quest::summonitem(51121); # Tradeskill Basics : Volume I
-     }
-	 if($text=~/second book/i){ 
+	if ($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [" . quest::saylink("second book") . "], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		#:: GIve a 51121 - Tradeskill Basics : Volume I
+		quest::summonitem(51121);
+	}
+	if($text=~/second book/i){ 
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
-		quest::summonitem(51122); # Tradeskill Basics : Volume II
+		#:: GIve a 51122 - Tradeskill Basics : Volume I
+		quest::summonitem(51122);
 	}
 }
+
 sub EVENT_ITEM {
 	#:: match for a 18776 - Recruitment Summons
 	if (plugin::check_handin(\%itemcount, 18776 => 1)) {
 		quest::say("Yes, I just knew you'd see it my way, $name. Anyway, welcome to our little part of Ak'Anon's underworld. We have to pay a high price to keep our small orgainzation hidden, which keeps us all busy around here. Now throw this on, and let's put you to work. Once you are ready to begin your training please make sure that you come back to see me after collecting your initial supplies! I have many things to teach you that you would benefit from, from furthering your knowledge of our arts, to the various [" . quest::saylink("trades") . "] you will have available to you.");
-		quest::summonitem(13519); #Scuffed Tunic*
-		quest::faction(76,100); # Deep Muses
-		quest::faction(210,15); # Merchants of Ak'Anon
-		quest::faction(71,-15); # Dark Reflection
-		quest::faction(115,15); # gem choppers
+		#:: Give a 13519 - Scuffed Tunic*
+		quest::summonitem(13519); 
+		#:: Set faction
+		quest::faction(76,100); 	# Deep Muses
+		quest::faction(210,15); 	# Merchants of Ak'Anon
+		quest::faction(71,-15); 	# Dark Reflection
+		quest::faction(115,15); 	# Gem Choppers
 		quest::ding();
 		quest::exp(100);
 	}
-	plugin::return_items(\%itemcount); # return unused items
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
