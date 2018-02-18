395ae845bda8a9abef5ecc2a7bf6e1b7a649d661
TurmoilToad <TurmoilToad@gmail.com>
2018-02-18 13:01:12 -0500

    Update Zenita_D-Rin.pl

--- akanon/Zenita_D-Rin.pl
+++ akanon/Zenita_D-Rin.pl
@@ -9,10 +9,11 @@ sub EVENT_SAY {
 		quest::say("Great. It is rather simple. I have five cards and only one is King Naythox. Find it. In order to get one card all you need to do is buy me a bottle of Innoruuks Kiss of Death from the barkeep in Chops N Hops. One bottle for one card. Return the King Naythox card to me and you shall get the Spare Lens.");
 	}
 	if ($text=~/fight/i) {
-		quest::say("Darn!! I was hoping not to hear that word, fight. Oh well.");
+		quest::say("Darn!! I was hoping not to hear that word, 'fight'. Oh well.");
 		quest::attack($name);
 	}
 }
+
 sub EVENT_ITEM {
 	#:: Turn in for 22298 -  King Card
 	if (plugin::check_handin(\%itemcount, 22298 => 1)) {
@@ -31,16 +32,12 @@ sub EVENT_ITEM {
 		quest::summonitem(quest::ChooseRandom(22293,22294,22295,22296,22297,22298,22299));
 	}
 	#:: Turn in for Any Card aside from King or Joker 
-	if ((plugin::check_handin(\%itemcount, 22299 => 1)) ||
-		(plugin::check_handin(\%itemcount, 22297 => 1)) ||
-		(plugin::check_handin(\%itemcount, 22296 => 1)) ||
-		(plugin::check_handin(\%itemcount, 22294 => 1)) ||
-		(plugin::check_handin(\%itemcount, 22293 => 1))) {
-		quest::say("Bad luck must be one of your strong suits. You should have been a beggar because you sure aren't a very good %s. You lose! $name");
+	if ((plugin::check_handin(\%itemcount, 22299 => 1)) || (plugin::check_handin(\%itemcount, 22297 => 1)) || (plugin::check_handin(\%itemcount, 22296 => 1)) || (plugin::check_handin(\%itemcount, 22294 => 1)) || (plugin::check_handin(\%itemcount, 22293 => 1))) {
+		quest::say("Bad luck must be one of your strong suits. You should have been a beggar because you sure aren't a very good $class. You lose! $name");
 	}
 	#:: Turn in for 22295 -  Joker Card
 	if (plugin::check_handin(\%itemcount, 22295 => 1)) {
-		quest::say("I see you have drawn the card that best represents a %s such as yourself. You lose! $name");
+		quest::say("I see you have drawn the card that best represents a $class such as yourself. You lose! $name");
 	}
 	plugin::return_items(\%itemcount);
 }
