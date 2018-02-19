1be33485fac99d41300ffa8a0e7900b143c038f3
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-19 12:13:46 -0500

    Update #Captain_Klunga.pl
    Copied from GoT, Added notes

--- cauldron/#Captain_Klunga.pl
+++ cauldron/#Captain_Klunga.pl
@@ -1,14 +1,23 @@
+# Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
+
 sub EVENT_SPAWN {
-   $npc->SetAppearance(1);
+
+	$npc->SetAppearance(1);
 }
 
 sub EVENT_ITEM {
-   if (plugin::check_handin(\%itemcount, 12278 => 1)) {
-      $npc->SetAppearance(0);
-      quest::moveto(-395.87, 807.04, 70.53, 0, 1);
-   }
+
+	if (plugin::check_handin(\%itemcount, 12278 => 1)) { # Abandoned Orc Shovel
+		$npc->SetAppearance(0);
+		quest::moveto(-395.87, 807.04, 70.53, 0, 1);
+	}
+	plugin::return_items(\%itemcount);
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-   quest::creategroundobject(12274, $x, $y, $z, $h);
+
+	quest::creategroundobject(12274, $x, $y, $z, $h); # Chalice of Conquest
 }
+
+# EOF Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
+# copied from GoT by SS
