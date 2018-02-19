dd042a294f9e401aef4afb1c1eb8b772958331d7
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-19 15:01:30 -0500

    Update Nicholas.pl
    Added saylinks and notes

--- rathemtn/Nicholas.pl
+++ rathemtn/Nicholas.pl
@@ -1,7 +1,7 @@
 ##############################################################
 # NPC:  Nicholas
 # Zone:  Rathe Mountains
-# Quest:  Boots of Ro
+# Quest:  Boots of Ro, Chalice of Conquest
 ##############################################################
 sub EVENT_SAY { 
   if ($text=~/boots of ro/i) {
@@ -11,32 +11,32 @@ sub EVENT_SAY {
       quest::say("You and I must be brothers and serve the Lord of Underfoot. Go to Kaladim and serve her cathedral. When you think you are ready. then ask Lord Datur if you are an [honored member] of the temple. If the answer is yes, then I will trust you.");
     }
   } elsif ($text=~/chalice of conquest/i) {
-    if ($faction > 4) {
+    if ($faction > 4) { # amiable or better to get the quest
       quest::say("How dare you approach a member of the Clerics of Underfoot?! Dogs such as you lie in the same bed as aviaks and ogres!!");
     } else {
-      quest::say("You must be the one Sir Dru of Kaladim sent to retrieve the candlestick which was broken in the [orc onslaught]. I have only this piece in my possession as the other portion was taken by an [odd druid] of these mountains. Should you reclaim it, I am sure the maker of the candlestick can patch it together.");
-      quest::summonitem(12276);
+      quest::say("You must be the one Sir Dru of Kaladim sent to retrieve the candlestick which was broken in the [" . quest::saylink("orc onslaught") . "]. I have only this piece in my possession as the other portion was taken by an [" . quest::saylink("odd druid") . "] of these mountains. Should you reclaim it, I am sure the maker of the candlestick can patch it together.");
+      quest::summonitem(12276); # Stem of Candlestick
     }
   } elsif ($text=~/candle of bravery/i) {
-    if ($faction > 4) {
+    if ($faction > 4) { # amiable or better to get the quest
       quest::say("How dare you approach a member of the Clerics of Underfoot?! Dogs such as you lie in the same bed as aviaks and ogres!!");
     } else {
-      quest::say("You must be the one Sir Dru of Kaladim sent to retrieve the candlestick which was broken in the [orc onslaught]. I have only this piece in my possession as the other portion was taken by an [odd druid] of these mountains. Should you reclaim it, I am sure the maker of the candlestick can patch it together.");
-      quest::summonitem(12276);
+      quest::say("You must be the one Sir Dru of Kaladim sent to retrieve the candlestick which was broken in the [" . quest::saylink("orc onslaught") . "]. I have only this piece in my possession as the other portion was taken by an [" . quest::saylink("odd druid") . "] of these mountains. Should you reclaim it, I am sure the maker of the candlestick can patch it together.");
+      quest::summonitem(12276); # Stem of Candlestick
     }
   } elsif ($text=~/orc onslaught/i) {
-    quest::say("The orcs attempted to take the hill, as they do on a frequent basis. We repelled them, of course, but a [blue orc] rushed off with the [Chalice of Conquest]. Where he came from, I do not know. He was no part of the battle!! I believe he just found an opportune moment to loot our camp.");
+    quest::say("The orcs attempted to take the hill, as they do on a frequent basis. We repelled them, of course, but a [" . quest::saylink("blue orc") . "] rushed off with the [" . quest::saylink("Chalice of Conquest") . "]. Where he came from, I do not know. He was no part of the battle!! I believe he just found an opportune moment to loot our camp.");
   } elsif ($text=~/blue orc/i) {
-     quest::say("I had a visiting ranger track him to his camp near Lake Rathetear. I spied the camp and saw his lifeless body near two other blue orcs. Apparently, he must have taken a fatal blow, but had enough stamina to make it to his camp. I returned the following day with my fellow paladins and found the camp long gone. The ranger, who was still with us, found two sets of tracks leading away. We found [orc remnants].");
+     quest::say("I had a visiting ranger track him to his camp near Lake Rathetear. I spied the camp and saw his lifeless body near two other blue orcs. Apparently, he must have taken a fatal blow, but had enough stamina to make it to his camp. I returned the following day with my fellow paladins and found the camp long gone. The ranger, who was still with us, found two sets of tracks leading away. We found [" . quest::saylink("orc remnants") . "].");
   } elsif ($text=~/orc remnants/i) {
     if ($faction > 4) {
       quest::say("How dare you approach a member of the Clerics of Underfoot?! Dogs such as you lie in the same bed as aviaks and ogres!!");
     } else {
       quest::say("Here. We found this worthless orc bracelet with the name Klunga on it. There was also an orc shovel and a bag of Cauldron prawns. We left them, of course. The camp has now been taken over by the green-skinned orcs. I would advise you to stay clear of this camp in Lake Rathe.");
-      quest::summonitem(12280);
+      quest::summonitem(12280); # Klunga's Bracelet
     }
   } elsif ($text=~/odd druid/i) {
-    quest::say("The mountains have been plagued not only by beasts, but by evil druids!! They attempt to force all men from this land. One has stolen the foot of the candlestick which holds the [Candle of Bravery]. I remember that battle cry of hers, 'Long live the green!!'.");
+    quest::say("The mountains have been plagued not only by beasts, but by evil druids!! They attempt to force all men from this land. One has stolen the foot of the candlestick which holds the [" . quest::saylink("Candle of Bravery") . "]. I remember that battle cry of hers, 'Long live the green!!'.");
   }
 }
 sub EVENT_ITEM {
