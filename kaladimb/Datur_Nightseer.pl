92f692f43c8ff64a2b6ee422b9da15e188608a5c
TurmoilToad <TurmoilToad@gmail.com>
2018-02-20 09:30:11 -0500

    Update Datur_Nightseer.pl

--- kaladimb/Datur_Nightseer.pl
+++ kaladimb/Datur_Nightseer.pl
@@ -1,13 +1,13 @@
 sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	$x = $npc->GetX();
+	$y = $npc->GetY();
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 }
 
 sub EVENT_ENTER {
-  if (($ulevel == 1) && ($class eq "Paladin")) { 
+	if (($ulevel == 1) && ($class eq "Paladin")) {
 		$client->Message(15,"Datur Nightseer glances your way. 'Ah a new recruit! Welcome, young one! Read the note in your inventory and then hand it to me when you wish to begin your training!'");
-  }
+	}
 }
 
 #sub EVENT_SAY { # LDoN content
@@ -22,31 +22,37 @@ sub EVENT_ENTER {
 #}
 
 sub EVENT_ITEM {
-   if (plugin::check_handin(\%itemcount, 18768 => 1)) { # Folded Parchment
-	  quest::say("Welcome, we are the Paladins of the Underfoot. I am Datur, and I will help teach you the word and will of the Duke of Below, Brell Serilis. Here is our guild tunic. Let's get started, shall we?");
-      quest::summonitem(13514); # Dusty Tunic*
-      quest::faction(44,100); # Clerics of Underfoot
-      quest::faction(169,100); # Kazon Stormhammer
-      quest::faction(219,75); # Miners Guild 249
-   }
-   if (($faction <= 4) && (plugin::check_handin(\%itemcount, 12279 => 1))) { # Chalice Case
-      quest::say("The chalice is returned!! Praise be to Brell!! You have proven yourself to our church and have earned our respect. Let me welcome you into our brotherhood with the Cape of Underfoot. Wear it with pride as all of our finest paladins do.");
-      quest::faction(44, 25); # Clerics of Underfoot
-      quest::faction(169, 25); # Kazon Stormhammer
-      quest::faction(219, 25); # Miners Guild 249
-      quest::exp(250);
-      quest::givecash(0, 0, 0, 3);
-      quest::summonitem(12281); # Cape of Underfoot
-   }
-   # LDON content
+	#:: Match a 18768 - Folded Parchment
+	if (plugin::check_handin(\%itemcount, 18768 => 1)) {
+		quest::say("Welcome, we are the Paladins of the Underfoot. I am Datur, and I will help teach you the word and will of the Duke of Below, Brell Serilis. Here is our guild tunic. Let's get started, shall we?");
+		#:: Give a 13514 - Dusty Tunic*
+		quest::summonitem(13514);
+		#:: Set Factions
+		quest::faction(44,100); 	# Clerics of Underfoot
+		quest::faction(169,100); 	# Kazon Stormhammer
+		quest::faction(219,75); 	# Miners Guild 249
+	}
+	#:: Match a 12279 - Chalice Case
+	if (($faction <= 4) && (plugin::check_handin(\%itemcount, 12279 => 1))) {
+		quest::say("The chalice is returned!! Praise be to Brell!! You have proven yourself to our church and have earned our respect. Let me welcome you into our brotherhood with the Cape of Underfoot. Wear it with pride as all of our finest paladins do.");
+		#:: Give a 12281 - Cape of Underfoot
+		quest::summonitem(12281); # Cape of Underfoot
+		#:: Set Factions
+		quest::faction(44, 25); 	# Clerics of Underfoot
+		quest::faction(169, 25); 	# Kazon Stormhammer
+		quest::faction(219, 25); 	# Miners Guild 249
+		quest::exp(250);
+		quest::givecash(0, 0, 0, 3);
+	}
+# LDON content
 #   if ($client->GetGlobal("paladin_epic")==9 && plugin::check_handin(\%itemcount, 51157 => 1)) { # Bottle of Double Brewed Double Dry Stout
 #	  quest::summonitem(69963); # Kaladim Prayer Bead
 #	  quest::say("Now that is a tasty beverage!! It sure is strong too for this tiny dwarf! I guess I should give you some kind of thanks. Take this jewel that I found and see if it will help your friend Kemik. I wish I had the second jewel that was with it. You will find that one with a young but brave froglok.");
 #   }
-   #do all other handins first with plugin, then let it do disciplines
+#   do all other handins first with plugin, then let it do disciplines
 #   plugin::try_tome_handins(\%itemcount, $class, 'Paladin');
-
-   plugin::return_items(\%itemcount);
+	#:: Return Unused Items
+	plugin::return_items(\%itemcount);
 }
 # edited by SS Removed LDON Content
 #END of FILE Zone:kaladimb  ID:67029 -- Datur_Nightseer
