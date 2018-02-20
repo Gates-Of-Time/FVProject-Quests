e001f0019c21eebe84c03ed756d46d5c74a7ef7a
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-20 15:52:05 -0500

    Create Margyl_Darklin.pl
    Updated Margyl_Darklin of Butcherblock to perl

---
+++ butcher/Margyl_Darklin.pl
@@ -0,0 +1,11 @@
+#:: Not a Quest NPC - Just a Butcherblock resident
+sub EVENT_WAYPOINT_ARRIVE {
+     #::: When NPC arrives at waypoint 1 or 7
+     if ($wp == 55 || $wp == 117) {
+         quest::SetRunning(1);  #:: Run
+		}
+	 #::: When NPC arrives at waypoint 82
+     if ($wp == 82 || $wp == 10) {	
+		 quest::SetRunning(0);  #:: Walk
+	    }
+}
