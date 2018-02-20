6f791d10f7015c3fc8991f2e7b32f318a7a8e5e7
TurmoilToad <TurmoilToad@gmail.com>
2018-02-20 11:47:32 -0500

    Update #Captain_Klunga.pl

--- cauldron/#Captain_Klunga.pl
+++ cauldron/#Captain_Klunga.pl
@@ -1,9 +1,9 @@
 # Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
 
-sub EVENT_SPAWN {
-
-	$npc->SetAppearance(1);
-}
+#sub EVENT_SPAWN {
+#
+#	$npc->SetAppearance(1);
+#}
 
 sub EVENT_ITEM {
 	#:: Match for 12278 - Abandoned Orc Shovel
@@ -14,10 +14,10 @@ sub EVENT_ITEM {
 	plugin::return_items(\%itemcount);
 }
 
-sub EVENT_WAYPOINT_ARRIVE {
+#sub EVENT_WAYPOINT_ARRIVE {
 	#:: Ground spawn a 122274 - Chalice of Conquest
-	quest::creategroundobject(12274, $x, $y, $z, $h);
-}
+#	quest::creategroundobject(12274, $x, $y, $z, $h);
+#}
 
 # EOF Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
 # copied from GoT by SS
