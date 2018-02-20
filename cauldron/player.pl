a5d8acf91a1f683508b1b9aba3227780e1ffabc8
TurmoilToad <TurmoilToad@gmail.com>
2018-02-20 12:51:31 -0500

    Update player.pl

--- cauldron/player.pl
+++ cauldron/player.pl
@@ -2,9 +2,7 @@
 sub EVENT_PLAYER_PICKUP {
 	#:: Match 12274 - Chalice of Conquest, ground spawn created by #Captain_Klunga.pl
 	if ($picked_up_id == 12274) {
-		#:: Make sure NPC 70072 #Captain_Kluga is spawned
-		if ($entity_list->IsMobSpawnedByNpcTypeID(70072)) {
-			quest::attack($name);
-		}
+		my $klunga = entity_list->GetMobByNpcTypeID(70072);
+		$klunga->AddToHateList($client, 1);
 	}
 }
