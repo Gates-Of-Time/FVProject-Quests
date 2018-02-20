85d5c0af1b6ee837b4c80e01c90afbb6531f1070
TurmoilToad <TurmoilToad@gmail.com>
2018-02-20 11:56:22 -0500

    Update #Captain_Klunga.pl

--- cauldron/#Captain_Klunga.pl
+++ cauldron/#Captain_Klunga.pl
@@ -1,7 +1,22 @@
+# Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
+
+sub EVENT_SPAWN {
+
+	$npc->SetAppearance(1);
+}
+
 sub EVENT_ITEM {
-	#:: Match for 12278 - Abandoned Orc Shovel
+	#:: Match a 12278 - Abandoned Orc Shovel
 	if (plugin::check_handin(\%itemcount, 12278 => 1)) {
-		quest::ding();
+		$npc->SetAppearance(0);
+		quest::moveto(-395.87, 807.04, 70.53, 0, 1);
 	}
 	plugin::return_items(\%itemcount);
 }
+
+sub EVENT_WAYPOINT_ARRIVE {
+
+	quest::creategroundobject(12274, $x, $y, $z, $h);
+}
+
+# EOF Zone: Dagnor's Cauldron (cauldron) >> #Captain Klunga (70072)
