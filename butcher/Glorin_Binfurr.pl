6ce454044110584f61636e48724745a70249da23
TurmoilToad <TurmoilToad@gmail.com>
2018-02-21 11:51:32 -0500

    Update Glorin_Binfurr.pl

--- butcher/Glorin_Binfurr.pl
+++ butcher/Glorin_Binfurr.pl
@@ -1,5 +1,6 @@
-sub EVENT_SAY {	
 #:: The Mudtoes Quest NPC
+
+sub EVENT_SAY {	
 	if ($text=~/hail/i) {
 		quest::say("Pleasure to meet you.  Keep your eye out for the ship.  You don't want to miss it.");
 	}
