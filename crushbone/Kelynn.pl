cc3f3ae776a8e8603a9c6d8ea65658ed3e846c9f
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-02-22 19:03:52 -0500

    Create Kelynn.pl
    Added Kelynn.pl file to crushbone to convert from lua new file no link

---
+++ crushbone/Kelynn.pl
@@ -0,0 +1,20 @@
+sub EVENT_SAY {	
+#:: Scouts Cape Quest
+	if ($text=~/hail/i) {
+		quest::say("What are you doing here?!! Get out before you find yourself mining for these wretched beasts!");
+	}
+}
+#:: Scouts Cape Quest
+sub EVENT_ITEM {
+	#:: Turn in for 12184 -  Useless Token
+	if (plugin::check_handin(\%itemcount, 12184 => 1)) {
+		quest::say("You were sent by Geeda!! Here. Take the information. Maybe next you shall earn your Scout Blade from Laren. Quickly!! Leave at once!!");
+		#:: Give item 12183 - Crushbone Information
+		quest::summonitem(12183);
+		#:: Give a small amount of xp
+		quest::exp(500);
+		#:: Ding!
+		quest::ding();
+	}
+	plugin::return_items(\%itemcount);
+}
