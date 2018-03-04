c013c42d2e94fdcb3c0030d00d5c0b6d9058957f
TurmoilToad <TurmoilToad@gmail.com>
2018-03-03 20:21:55 -0500

    Update Lewis_Reldnok.pl

--- akanon/Lewis_Reldnok.pl
+++ akanon/Lewis_Reldnok.pl
@@ -28,7 +28,7 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
 	#:: Check for 18433 - Gnome Paladin Note
 	if (plugin::check_handin(\%itemcount, 18433 => 1)) {
-		quest::Say("Welcome to the Abbey of Deep Musing, $name! Here is a tunic that you may wear to announce the beginning of your training as a Paladin of Brell Serilis! Be warned that the only dangers do not lie without Ak'Anon. There is an evil society that lurks in the deepest recesses and shadows of our magnificent city. When you are ready to begin your training, let me know. I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [" . quest::saylink("trades") . "] you will have available to you.");
+		quest::say("Welcome to the Abbey of Deep Musing, $name! Here is a tunic that you may wear to announce the beginning of your training as a Paladin of Brell Serilis! Be warned that the only dangers do not lie without Ak'Anon. There is an evil society that lurks in the deepest recesses and shadows of our magnificent city. When you are ready to begin your training, let me know. I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [" . quest::saylink("trades") . "] you will have available to you.");
 		#:: Give a 13517 - Worn Felt Tunic reward
 		quest::summonitem(13517);
 		#:: Set factions
