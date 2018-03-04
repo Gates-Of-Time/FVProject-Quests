db948df8bf36acca380fc85ea2dd95a94fa4e41d
TurmoilToad <TurmoilToad@gmail.com>
2018-03-03 19:33:48 -0500

    Update Zapho_the_Ancient.pl

--- akanon/Zapho_the_Ancient.pl
+++ akanon/Zapho_the_Ancient.pl
@@ -1,10 +1,13 @@
 sub EVENT_SAY {
-     if ($text=~/hail/i) {
-        quest::emote("Oh! Please excuse me. I was busy creating some new illusions for my collection. Hello there, $name! I am Zapho the Ancient. My return from the halls of the Realm of Heroes is the will of Bertoxxulous. I must watch over those who catch the eyes of the gods and recognize their heroic deeds.");
-     }
+	if ($text=~/hail/i) {
+		quest::say("Oh! Please excuse me. I was busy creating some new illusions for my collection.");
+		quest::say("Hello there, $name! I am Zapho the Ancient. My return from the halls of the Realm of Heroes is the will of Bertoxxulous. I must watch over those who catch the eyes of the gods and recognize their heroic deeds.");
+	}
 }
+
 sub EVENT_ITEM {
-    
-    plugin::return_items(\%itemcount); # return unused items
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
+
 #converted to Perl by SS
