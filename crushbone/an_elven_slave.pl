fe307b333acaf44d29f6565cca2ddf2c09d31eb7
TurmoilToad <TurmoilToad@gmail.com>
2018-03-04 14:41:32 -0500

    Update an_elven_slave.pl

--- crushbone/an_elven_slave.pl
+++ crushbone/an_elven_slave.pl
@@ -1,4 +1,4 @@
-sub EVENT_SAY {	
+sub EVENT_SAY {
 	#:: Quest Dialog for Slave Keys, Taskmaster Earring, and Clue to Screaming Mace quest initiation.
 	if ($text=~/hail/i) {
 		quest::say("Slavedrivers are just lackeys for the [" . quest::saylink("taskmaster") . "]. He is the real orc in charge.");
@@ -17,22 +17,27 @@ sub EVENT_ITEM {
 	my $npcgender = $npc->GetGender();
 	#:: Match copper
 	if ($copper == 1) {
-		if ($npcrace == 5 && $npcgender == 0) {  #:: Male High Elf Slave
+		#:: Match Male High Elf
+		if ($npcrace == 5 && $npcgender == 0) {
 			quest::say("Not that!! I need key number 20!!");
 		}
-		elsif ($npcrace == 8 && $npcgender == 1) {  #:: Female High Elf Slave
+		#:: Match Female High Elf
+		elsif ($npcrace == 8 && $npcgender == 1) {
 			quest::say("What is this!!? Get me key number 17!!");
 		}
-		elsif ($npcrace == 4 && $npcgender == 0) {  #:: Male Wood Elf Slave
+		#:: Match Male Wood Elf
+		elsif ($npcrace == 4 && $npcgender == 0) {
 			quest::say("Not that!! I need key number 18!!");
 		}
-		elsif ($npcrace == 4 && $npcgender == 1) {  #:: Female Wood Elf Slave
+		#:: Match Female Wood Elf
+		elsif ($npcrace == 4 && $npcgender == 1) {
 			quest::say("Please!! I need a key with a number 19!!");
 		}
 	}
 	#:: Match item 20020 - Shackle Key 20
 	if (plugin::check_handin(\%itemcount, 20020 => 1)) {
-		if ($npcrace == 5 && $npcgender == 0) {  #:: Male High Elf Slave
+		#:: Match Male High Elf
+		if ($npcrace == 5 && $npcgender == 0) {
 			quest::say("Good work!! I shall be on my way.  Farewell my friend!!");
 			#:: Give a small amount of xp
 			quest::exp(1000);
@@ -41,18 +46,22 @@ sub EVENT_ITEM {
 			#:: Set faction
 			quest::faction(170,1); 		#:: + Keepers of the Art
 			quest::faction(178,1); 		#:: + King Tearis Thex
-			quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+			#:: Give a small amount of cash copper - plat
+			quest::givecash(0,3,7,0);
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
+		#:: Match Female High Elf
 		elsif ($npcrace == 5 && $npcgender == 1) {
 			quest::say("I have no need for this item $name, you can have it back.");
-			quest::summonitem(20016);
+			#:: Give back the 20020 - Shackle Key 20
+			quest::summonitem(20020);
 		}
 	}
 	#:: Match item 10351 - Brass Earring
 	if (plugin::check_handin(\%itemcount, 10351 =>1)) {
-		if ($npcrace == 5 && $npcgender == 0) { #:: Male High Elf Slave 
+		#:: Match Male High Elf
+		if ($npcrace == 5 && $npcgender == 0) {
 			#:: Summon Item 18901 - Ragged Cloth Note (Csb 1.O.U. Hie 1)
 			quest::summonitem(18901);
 			quest::say("You killed the taskmaster?!  Absolutely amazing! The orcs will be fighting among themselves for power now and I can disappear in the commotion. Thanks, friend! Take this for your deeds!");
@@ -63,7 +72,8 @@ sub EVENT_ITEM {
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
-		if ($npcrace == 4 && $npcgender == 0) {  #:: Male Wood Elf Slave
+		#:: Match Male Wood Elf
+		if ($npcrace == 4 && $npcgender == 0) {
 			#:: Summon Item 18903 - Tattered Cloth Note (Csb 1.O.U. Elf 1)
 			quest::summonitem(18903);
 			quest::say("You killed the taskmaster?!  Absolutely amazing! The orcs will be fighting among themselves for power now and I can disappear in the commotion. Thanks, friend! Take this for your deeds!");
@@ -74,7 +84,8 @@ sub EVENT_ITEM {
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
-		if ($npcrace == 4 && $npcgender == 1) {  #:: Female Wood Elf Slave
+		#:: Match Female Wood Elf
+		if ($npcrace == 4 && $npcgender == 1) {
 			#:: Summon Item 18904 - Faded Wedding Cloth (Csb 1.O.U. Elf 2)
 			quest::summonitem(18904);
 			quest::say("You killed the taskmaster?!  Absolutely amazing! The orcs will be fighting among themselves for power now and I can disappear in the commotion. Thanks, friend! Take this for your deeds!");
@@ -88,7 +99,8 @@ sub EVENT_ITEM {
 	}
 	#:: Match item 20021 - Shackle Key 21
 	if (plugin::check_handin(\%itemcount, 20021 => 1)) {
-		if ($npcrace == 5 && $npcgender == 1) {  #:: Female High Elf Slave
+		#:: Match Female High Elf
+		if ($npcrace == 5 && $npcgender == 1) {
 			quest::say("Good work!! I shall be on my way. Farewell my friend!!");
 			#:: Give a small amount of xp
 			quest::exp(1000);
@@ -97,17 +109,22 @@ sub EVENT_ITEM {
 			#:: Set faction
 			quest::faction(170,1); 		#:: + Keepers of the Art
 			quest::faction(178,1); 		#:: + King Tearis Thex
-			quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+			#:: Give a small amount of cash copper - plat
+			quest::givecash(0,3,7,0);
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
+		#:: Match Male High Elf
 		elsif ($npcrace == 5 && $npcgender == 0) {
 			quest::say("I have no need for this item $name, you can have it back.");
-			quest::summonitem(20017);
+			#:: Give back the 20021 - Shackle Key 21
+			quest::summonitem(20021);
 		}
+	}
 	#:: Match item 20018 - Shackle Key 18
 	if (plugin::check_handin(\%itemcount, 20018 => 1)) {
-		if ($npcrace == 4 && $npcgender == 0) {  #:: Male Wood Elf Slave
+		#:: Match Male Wood Elf
+		if ($npcrace == 4 && $npcgender == 0) {
 			quest::say("Good work!! I shall be on my way. Farewell my friend!!");
 			#:: Give a small amount of xp
 			quest::exp(1000);
@@ -117,17 +134,22 @@ sub EVENT_ITEM {
 			quest::faction(99,1); 		#:: + Faydark's Champions
 			quest::faction(174,1); 		#:: + Kelethin Merchants
 			quest::faction(212,1); 		#:: + Merchants of Felwithe
-			quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+			#:: Give a small amount of cash copper - plat
+			quest::givecash(0,3,7,0);
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
+		#:: Match Female Wood Elf
 		elsif ($npcrace == 4 && $npcgender == 1) {
 			quest::say("I have no need for this item $name, you can have it back.");
-			quest::summonitem(20017);
+			#:: Give back the 20018 - Shackle Key 18
+			quest::summonitem(20018);
 		}
+	}
 	#:: Match item 20019 - Shackle Key 19
 	if (plugin::check_handin(\%itemcount, 20019 => 1)) {
-		if ($npcrace == 4 && $npcgender == 1) {  #:: Female Wood Elf Slave
+		#:: Match Female Wood Elf
+		if ($npcrace == 4 && $npcgender == 1) {
 			quest::say("Good work!! I shall be on my way. Farewell my friend!!");
 			#:: Give a small amount of xp
 			quest::exp(1000);
@@ -137,13 +159,16 @@ sub EVENT_ITEM {
 			quest::faction(99,1); 		#:: + Faydark's Champions
 			quest::faction(174,1); 		#:: + Kelethin Merchants
 			quest::faction(212,1); 		#:: + Merchants of Felwithe
-			quest::givecash(0,3,7,0);	#:: Give a small amount of cash copper - plat
+			#:: Give a small amount of cash copper - plat
+			quest::givecash(0,3,7,0);
 			#:: Set Depop Timer
 			quest::settimer("depop",30);
 		}
+		#:: Match Male Wood Elf
 		elsif ($npcrace == 4 && $npcgender == 0) {
 			quest::say("I have no need for this item $name, you can have it back.");
-			quest::summonitem(20017);
+			#:: Return the 20019 - Shackle Key 19
+			quest::summonitem(20019);
 		}
 	}
 	#:: Return unused items
