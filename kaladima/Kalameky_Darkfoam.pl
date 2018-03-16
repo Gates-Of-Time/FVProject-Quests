b6ed55b2892b64f7bfc14025e9d1c979bae3dc1f
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-03-16 15:48:58 -0400

    Update Kalameky_Darkfoam.pl
    Corrected dialog to be updated to live version

--- kaladima/Kalameky_Darkfoam.pl
+++ kaladima/Kalameky_Darkfoam.pl
@@ -1,6 +1,6 @@
 sub EVENT_ITEM{
   if(plugin::check_handin(\%itemcount, 18906 => 1)){
-    quest::say("The slaves...thank you! Here, this will help defend you against those vile orcs.");
+    quest::say("'Why, that..! Bah! Fine. Stupid as a newborn gob. She's not gonna get this, then. Hey, you might as well take it. It was gonna be her reward fer a job well done.'");
     quest::summonitem(23359);
     quest::exp(30000);
     quest::ding();
