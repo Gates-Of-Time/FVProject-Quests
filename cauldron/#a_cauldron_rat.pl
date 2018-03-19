e9f28a75810f3b43475d7c72180789b89e810d80
TurmoilToad <TurmoilToad@gmail.com>
2018-03-19 12:05:09 -0400

    Update #a_cauldron_rat.pl

--- cauldron/#a_cauldron_rat.pl
+++ cauldron/#a_cauldron_rat.pl
@@ -1,22 +1,15 @@
-#Bilge Farfathom spawn
-#npc - #a_caulderon_rat (trigger for #Bilge_Farfathom 70059)
-#zone - Dagnors Cauldron
+#:: Bilge Farfathom spawn
+#:: #a_caulderon_rat - trigger for #Bilge_Farfathom 70059
 
-sub EVENT_DEATH_COMPLETE{
- my $random_result = int(rand(100));
-
- #these are not PEQ IDs:
- my $a=70059; #Bilge
- my $b=70062; #npc - #an_undertow_skeleton
- my $c=70069; #The_Fabled_Bilge_Farfathom
- if($random_result<85){
-  quest::spawn2($b,6,0,-974.5,436.4,-343.8,29);
-  #quest::say("spawn skele");
-  }elsif($random_result<95){
-  quest::spawn2($c,6,0,-974.5,436.4,-343.8,29);
-  #quest::say("spawn Fabled");
-  }else{
-    #quest::say("Spawning Bilge Farfathom");
-    quest::spawn2($a,4,0,-1123.30,-1194.57,-98.32,122);
-  };
- };
+sub EVENT_DEATH_COMPLETE {
+	#:: Set a scalar variable to store random 100
+	my $random_result = int(rand(100));
+	if ($random_result < 85) {
+		#:: Spawn 70062 - #an_undertow_skeleton
+		quest::spawn2(70062,6,0,-974.5,436.4,-343.8,29);
+	}
+	else {
+		#:: Spawn 70059 - #Bilge_Farfathom
+		quest::spawn2(70059,4,0,-1123.30,-1194.57,-98.32,122);
+	}
+}
