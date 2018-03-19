968cf75ce6f81624f6c411d48da77ff9a20c130f
TurmoilToad <TurmoilToad@gmail.com>
2018-03-19 10:52:28 -0400

    Update an_aqua_goblin_mariner.pl

--- cauldron/an_aqua_goblin_mariner.pl
+++ cauldron/an_aqua_goblin_mariner.pl
@@ -1,21 +1,15 @@
-#Bilge Farfathom spawn
-#npc - an_aqua_goblin_mariner (trigger for #Bilge_Farfathom 70059)
-#zone - Dagnors Cauldron
+#:: Bilge Farfathom spawn
+#:: an_aqua_goblin_mariner - trigger for #Bilge_Farfathom 70059
 
 sub EVENT_DEATH_COMPLETE{
- my $random_result = int(rand(100));
- #these are not PEQ IDs
- my $a=70059; #Bilge
- my $b=70061; #npc - #aqua_goblin_shaman
- my $c=70069; #The_Fabled_Bilge_Farfathom
- if($random_result<85){
-  quest::spawn2($b,6,0,-974.5,436.4,-343.8,29);
-  #quest::say("spawn shaman");
-  }elsif($random_result<95){
-  quest::spawn2($c,6,0,-974.5,436.4,-343.8,29);
-  #quest::say("spawn Fabled");
-  }else{
-    #quest::say("Spawning Bilge Farfathom");
-    quest::spawn2($a,4,0,-1123.30,-1194.57,-98.32,122);
-  };
- };
+	#:: Set a scalar variable to store random 100
+	my $random_result = int(rand(100));
+	if ($random_result < 85) {
+		#:: Spawn 70061 - #aqua_goblin_shaman
+		quest::spawn2(70061,6,0,-974.5,436.4,-343.8,29);
+	}
+	else {
+		#:: Spawn 70059 - #Bilge_Farfathom
+		quest::spawn2(70059,4,0,-1123.30,-1194.57,-98.32,122);
+	}
+}
