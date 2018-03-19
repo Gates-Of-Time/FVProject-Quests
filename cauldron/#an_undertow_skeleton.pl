a4a542e1ba4ee4ac3820fc0d8d1fb6795c4b0007
TurmoilToad <TurmoilToad@gmail.com>
2018-03-19 12:33:59 -0400

    Update #an_undertow_skeleton.pl

--- cauldron/#an_undertow_skeleton.pl
+++ cauldron/#an_undertow_skeleton.pl
@@ -1,17 +1,15 @@
-#Bilge Farfathom spawn
-#npc - #an_undertow_skeleton (trigger for Bilge)
-#zone - Dagnors Cauldron
+#:: Bilge Farfathom spawn
+#:: #an_undertow_skeleton trigger for #Bilge_Farfathom 70059
 
-sub EVENT_DEATH_COMPLETE{
- my $random_result = int(rand(100));
- #these are not PEQ ids:
- my $a=70059; #Bilge
- my $b=70060; #npc - an_aqua_goblin_mariner
- if($random_result<90){
-  quest::spawn2($b,6,0,-974.5,436.4,-343.8,29);
-  #quest::say("spawn mariner");
-  }else{
-    #quest::say("Spawning Bilge Farfathom");
-    quest::spawn2($a,4,0,-1123.30,-1194.57,-98.32,122);
-  };
- };
+sub EVENT_DEATH_COMPLETE {
+	#:: Set a scalar variable to store random 100
+	my $random_result = int(rand(100));
+	if ($random_result <90) {
+		#:: Spawn 70060 - an_aqua_goblin_mariner
+		quest::spawn2(70060,6,0,-974.5,436.4,-343.8,29);
+	}
+	else {
+		#:: Spawn 70059 - #Bilge_Farfathom
+		quest::spawn2(70059,4,0,-1123.30,-1194.57,-98.32,122);
+	}
+}
