3299ffbb2e39f34f8932c09598e5acf45c389ab0
TurmoilToad <TurmoilToad@gmail.com>
2018-03-18 21:52:03 -0400

    Update 68228.pl

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -2,16 +2,13 @@
 # Zone: Butcher
 # AngeloX
 
-sub EVENT_SPAWN
-{
+sub EVENT_SPAWN {
 	$x = $npc->GetX();
 	$y = $npc->GetY();
 	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
 }
 
-sub EVENT_ENTER
-{
-#	quest::selfcast("2279");
-	quest::emote ("'s spirit moves you to another land ...");
-	quest::movepc(69,-9193.3,386.7,5.2);
+sub EVENT_ENTER {
+#	quest::emote ("'s spirit moves you to another land ...");
+#	quest::movepc(69,-9193.3,386.7,5.2);
 }
