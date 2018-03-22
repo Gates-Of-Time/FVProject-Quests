6ffdbd2e3107bc2927b5f36af8c4ab08d5e9a53f
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-03-21 20:00:15 -0400

    Update and rename Nuien.pl to kunarkNuien.pl
    OOE, Kunark, Druid Epic added some notes

--- eastkarana/kunarkNuien.pl
+++ eastkarana/kunarkNuien.pl
@@ -4,11 +4,11 @@
 my $move;
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 20451 => 1)) {
+  if (plugin::check_handin(\%itemcount, 20451 => 1)) { # Frayed Braided Grass Amulet
     quest::say("So be it. Do as you have done before and find the next. Teloa is the last.");
-    quest::summonitem(20451);
+    quest::summonitem(20451); # Frayed Braided Grass Amulet
     quest::unique_spawn(15170,0,0,-2854,-3840,126.5,61.8); #spawn teola
-    quest::signalwith(15044,15167,0);
+    quest::signalwith(15044,15167,0); # signal Althele, Nuien
   }
   plugin::return_items(\%itemcount);
 }
@@ -25,7 +25,7 @@ sub EVENT_SIGNAL {
 
 sub EVENT_WAYPOINT_DEPART {
   if ($move == 1) {
-    quest::spawn2(15167,0,0,-1590,-3671,-18,0);
+    quest::spawn2(15167,0,0,-1590,-3671,-18,0); # spawn Nuien
     quest::depop();
   }
 }
