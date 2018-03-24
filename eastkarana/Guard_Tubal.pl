feb9d5b31215ce22225b770f8b7e2368e6f0d009
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-03-24 12:00:38 -0400

    Create Guard_Tubal.pl
    converted

---
+++ eastkarana/Guard_Tubal.pl
@@ -0,0 +1,12 @@
+sub EVENT_COMBAT { # combat state 1 = True
+    if ($combat_state == 1) {
+        quest::say("Time to die $name!");
+    }
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
+}
+
+# Converted to Perl by SS
