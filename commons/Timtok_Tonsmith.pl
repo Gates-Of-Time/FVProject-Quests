d2c39d4b388543287b35706d059f0ea2aeccc29b
TurmoilToad <TurmoilToad@gmail.com>
2018-03-26 14:21:17 -0400

    Update Timtok_Tonsmith.pl

--- commons/Timtok_Tonsmith.pl
+++ commons/Timtok_Tonsmith.pl
@@ -1,48 +1,66 @@
 # resize crafted armor to small size
-#
 
 sub EVENT_SAY {
-  if ($faction <= 5) {
-    if ($text=~/hail/i) {
-      quest::say("Hail! I am Timtok Tonsmith. My brother Ranvigoz and I are some of the finest smiths this side of Kaladim. If you are interested we will happily [resize] your [Crafted Armor] down to a more manageable size.");
-    }
-    if ($text=~/resize/i) {
-      quest::say("I will resize Crafted Gauntlets, a Vambrace, a Helm or Boots. My brother will resize Crafted Bracers, Greaves, a Pauldrun or a Breastplate. Just give me the armor and I will resize it for you.");
-    }
-    if ($text=~/crafted armor/i) {
-      quest::say("Crafted Armor is made by the Meadowgreen smiths. They can be found in the southern part of the Karanas.");
-    }
-  }
-  else {
-    quest::emote("will not speak to you, yet.");
-  }
+	#:: Check that faction is Indifferent or better
+	if ($faction <= 5) {
+		if ($text=~/hail/i) {
+			quest::say("Hail! I am Timtok Tonsmith. My brother Ranvigoz and I are some of the finest smiths this side of Kaladim. If you are interested we will happily [" . quest::saylink("resize") . "] your [" . quest::saylink("Crafted Armor") . "] down to a more manageable size.");
+		}
+		if ($text=~/resize/i) {
+			quest::say("I will resize Crafted Gauntlets, a Vambrace, a Helm or Boots. My brother will resize Crafted Bracers, Greaves, a Pauldrun or a Breastplate. Just give me the armor and I will resize it for you.");
+		}
+		if ($text=~/crafted armor/i) {
+			quest::say("Crafted Armor is made by the Meadowgreen smiths. They can be found in the southern part of the Karanas.");
+		}
+	}
+	else {
+		quest::emote("will not speak to you, yet.");
+	}
 }
 
 sub EVENT_ITEM {
-  if ($faction <= 5) {
-    if (plugin::check_handin(\%itemcount, 4178 => 1)) {
-      quest::summonitem(4186);
-    }
-    elsif (plugin::check_handin(\%itemcount, 4176 => 1)) {
-      quest::summonitem(4184);
-    }
-    elsif (plugin::check_handin(\%itemcount, 4173 => 1)) {
-      quest::summonitem(4181);
-    }
-    elsif (plugin::check_handin(\%itemcount, 4180 => 1)) {
-      quest::summonitem(4188);
-    }
-    else {
-      plugin::return_items(\%itemcount);
-      return 1;
-    }
-    quest::say("Here is your armor. I hope it fits better now.");
-    quest::faction(47,10); #coalition of tradefolk
-    quest::faction(184,10); #knights of truth
-    quest::faction(217,10); #merchants of qeynos
-    quest::faction(369,10); #coalition of tradefolk iii
-  }
+	#:: Check that faction is Indifferent or better
+	if ($faction <= 5) {
+		#:: Match 4178 - Crafted Gauntlets
+		if (plugin::check_handin(\%itemcount, 4178 => 1)) {
+			#:: Summon 4186 - Small Crafted Gauntlets
+			quest::summonitem(4186);
+			quest::say("Here is your armor. I hope it fits better now.");
+			quest::faction(47,10);	#:: Coalition of Tradefolk
+			quest::faction(184,10);	#:: Knights of Truth
+			quest::faction(217,10);	#:: Merchants of Qeynos
+			quest::faction(369,10);	#:: Coalition of Tradefolk III
+		}
+		#:: Match 4176 - Crafted Vambraces
+		if (plugin::check_handin(\%itemcount, 4176 => 1)) {
+			#:: Summon 4184 - Small Crafted Vambraces
+			quest::summonitem(4184);
+			quest::say("Here is your armor. I hope it fits better now.");
+			quest::faction(47,10);	#:: Coalition of Tradefolk
+			quest::faction(184,10);	#:: Knights of Truth
+			quest::faction(217,10);	#:: Merchants of Qeynos
+			quest::faction(369,10);	#:: Coalition of Tradefolk III
+		}
+		#:: Match 4173 - Crafted Helm
+		if (plugin::check_handin(\%itemcount, 4173 => 1)) {
+			#:: Summon 4181 - Small Crafted Helm
+			quest::summonitem(4181);
+			quest::say("Here is your armor. I hope it fits better now.");
+			quest::faction(47,10);	#:: Coalition of Tradefolk
+			quest::faction(184,10);	#:: Knights of Truth
+			quest::faction(217,10);	#:: Merchants of Qeynos
+			quest::faction(369,10);	#:: Coalition of Tradefolk III
+		}
+		#:: Match 4180 - Crafted Plate Boots
+		if (plugin::check_handin(\%itemcount, 4180 => 1)) {
+			#:: Summon 4188 - Small Crafted Plate Boots
+			quest::summonitem(4188);
+			quest::say("Here is your armor. I hope it fits better now.");
+			quest::faction(47,10);	#:: Coalition of Tradefolk
+			quest::faction(184,10);	#:: Knights of Truth
+			quest::faction(217,10);	#:: Merchants of Qeynos
+			quest::faction(369,10);	#:: Coalition of Tradefolk III
+		}
+	}
+	plugin::return_items(\%itemcount);
 }
-
-# EOF zone: commons ID: 21040 NPC: Timtok_Tonsmith
-
