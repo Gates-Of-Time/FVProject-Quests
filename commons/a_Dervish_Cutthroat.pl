ad4d013ec801923372052bc1d2657c8b6c63fb1f
TurmoilToad <TurmoilToad@gmail.com>
2018-03-26 08:32:28 -0400

    Update a_Dervish_Cutthroat.pl

--- commons/a_Dervish_Cutthroat.pl
+++ commons/a_Dervish_Cutthroat.pl
@@ -1,6 +1,9 @@
 sub EVENT_COMBAT {
+	#:: Create a scalar variable to store NPC race
 	my $NPCRace = $npc->GetRace();
+	#:: Combat state 1 is entered combat
 	if ($combat_state == 1) {
+		#:: Match race 54 - Orc
 		if ($NPCRace == 54) {
 			quest::say("Orc stomp, orc kill - orc weapons, your blood will spill.");
 		}
