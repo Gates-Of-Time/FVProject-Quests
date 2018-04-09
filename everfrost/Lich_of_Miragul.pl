5135422add2757ea977d4767c282aa2bf9c97689
TurmoilToad <TurmoilToad@gmail.com>
2018-04-09 09:04:20 -0400

    Update Lich_of_Miragul.pl

--- everfrost/Lich_of_Miragul.pl
+++ everfrost/Lich_of_Miragul.pl
@@ -1,17 +1,19 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Areeeeewwwww");
-		}
 	}
+}
 
 sub EVENT_ITEM {
-    if (plugin::check_handin(\%itemcount, 19072 => 1)) { # handin phlactery
-	    quest::spawn2(30094, 0, 0,$x,$y,$z,$h); # Spawn Miragul MobID 55334, Grid 0, Guildwar 0, X, Y, Z, Heading
-		quest::depop_withtimer(30094) # Miragul will de-spawn and start the spawn timer.
-		}
-
-		#:: Return unused items
+	#:: Match 19072 - Miragul's Phylactery
+	if (plugin::check_handin(\%itemcount, 19072 => 1)) {
+		#:: Spawn Everfrost Peaks >> Miragul (30094), Grid 0, Guildwar 0, Current X, Current Y, Current Z, Current Heading
+		quest::spawn2(30094, 0, 0, $x, $y, $z, $h);
+		#:: Despawn with timer
+		quest::depop_withtimer();
+	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
-# Converted to Perl by SS 
-# END of FILE Zone:everfrost  ID:3233 -- Lich_of_Miragul
+
+# Converted to Perl by SS
