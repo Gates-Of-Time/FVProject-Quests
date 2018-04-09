a03ab75521c5d173c90d22616abfaa409dd9337b
TurmoilToad <TurmoilToad@gmail.com>
2018-04-09 08:17:53 -0400

    Update Merchant_Iludarae.pl

--- gfaydark/Merchant_Iludarae.pl
+++ gfaydark/Merchant_Iludarae.pl
@@ -1,18 +1,11 @@
-sub EVENT_ITEM{
-  if($item1 == 18904){
-    quest::say("I have no words for the gratitude I feel. So many of our young ones have been captured by the filthy orcs. Please, take this for saving my sister.");
-    quest::exp(30000);
-    quest::ding();
-    my $random = int(rand(5));
-    if($random == 0){
-      quest::summonitem(2012); } #boots
-    if($random == 1){
-      quest::summonitem(2004); } #tunic
-    if($random == 2){
-      quest::summonitem(2006); } #cloak
-    if($random == 3){
-      quest::summonitem(2005); } #shoulderpads
-    if($random == 4){
-      quest::summonitem(2011); } #leggings
-  }
+sub EVENT_ITEM {
+	#:: Match 18904 - Faded Wedding Cloth
+	if (plugin::check_handin(\%itemcount, 18904 => 1) {
+		quest::say("I have no words for the gratitude I feel. So many of our young ones have been captured by the filthy orcs. Please, take this for saving my sister.");
+		#:: Give a random leather item: 2012 - Leather Boots, 2004 - Leather Tunic, 2006 - Leather Cloak, 2005 - Leather Shoulderpads, 2011 - Leather Leggings
+		quest::summonitem(quest::ChooseRandom(2004, 2005, 2006, 2011, 2012));
+		quest::exp(30000);
+		quest::ding();
+	}
+	plugin::return_items(\%itemcount);
 }
