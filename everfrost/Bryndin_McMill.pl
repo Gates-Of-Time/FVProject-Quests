508f93726c2692b01140a64e2721458ea10637e8
TurmoilToad <TurmoilToad@gmail.com>
2018-04-09 08:38:38 -0400

    Update Bryndin_McMill.pl

--- everfrost/Bryndin_McMill.pl
+++ everfrost/Bryndin_McMill.pl
@@ -1,20 +1,21 @@
 # Magic Elixir for the Warriors quest
 
 sub EVENT_ITEM {
-    if (plugin::check_handin(\%itemcount, 13242 => 1)) { # Three Quarters of Elixir
-		quest::Say("Ahhh!! I feel warm all over. Thanks. You should go give Arnis McLish a drink of this elixir. Last time I saw him he was headed toward Blackburrow.");
-		quest::SummonItem(13243); # One Half Full Bottle of Elixir
-		quest::Ding();
-		quest::Faction(213,1); # Merchants of Halas
-		quest::Faction(294,1); # Shaman of Justice
-		quest::Faction(311,1); # Steel Warriors
-		quest::Faction(361,1); # Wolves of the North
+	#:: Match 13242 - One Quarter of Elixir
+	if (plugin::check_handin(\%itemcount, 13242 => 1)) {
+		quest::say("Ahhh!! I feel warm all over. Thanks. You should go give Arnis McLish a drink of this elixir. Last time I saw him he was headed toward Blackburrow.");
+		#:: Give a 13243 - One Half of Elixir
+		quest::summonitem(13243);
+		quest::ding();
+		#:: Set factions
+		quest::faction(213,1);	#:: Merchants of Halas
+		quest::faction(294,1);	#:: Shaman of Justice
+		quest::faction(311,1);	#:: Steel Warriors
+		quest::faction(361,1);	#:: Wolves of the North
 		quest::exp(150);
-		}
-
-	
-		#:: Return unused items
+	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
 
-# Converted to Perl by SS 
+# Converted to Perl by SS
