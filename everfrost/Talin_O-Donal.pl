0df71976fa83963ba321e3057760c81b3c1a744f
TurmoilToad <TurmoilToad@gmail.com>
2018-04-15 01:07:20 -0400

    Update Talin_O-Donal.pl

--- everfrost/Talin_O-Donal.pl
+++ everfrost/Talin_O-Donal.pl
@@ -1,16 +1,19 @@
 sub EVENT_ITEM {
-    if (plugin::check_handin(\%itemcount, 13241 => 1)) { # Full Bottle of Elixir
-		quest::Say("Mmmm.. I feel much warmer. Thank you. You should now find Bryndin McMill. He could use a swig also. I saw him hanging around two other guards.");
-		quest::SummonItem(13242); # 3/4 Full Bottle of Elixir
-		quest::Ding();
-		quest::Faction(213,1); # Merchants of Halas
-		quest::Faction(294,1); # Shaman of Justice
-		quest::Faction(311,1); # Steel Warriors
-		quest::Faction(361,1); # Wolves of the North
+	#:: Match 13241 - Full Bottle of Elixir
+	if (plugin::check_handin(\%itemcount, 13241 => 1)) {
+		quest::say("Mmmm.. I feel much warmer. Thank you. You should now find Bryndin McMill. He could use a swig also. I saw him hanging around two other guards.");
+		#:: Give 13242 - One Quarter of Elixir
+		quest::summonitem(13242);
+		quest::ding();
+		#:: Set Factions
+		quest::faction(213,1);	#:: Merchants of Halas
+		quest::faction(294,1);	#:: Shaman of Justice
+		quest::faction(311,1);	#:: Steel Warriors
+		quest::faction(361,1);	#:: Wolves of the North
 		quest::exp(150);
-		}
-
-		#:: Return unused items
+	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
-# Converted to Perl by SS 
+
+# Converted to Perl by SS
