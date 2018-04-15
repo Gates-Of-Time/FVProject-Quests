11fceaf299717a007e8d21572cf1c1fe9b7b30ba
TurmoilToad <TurmoilToad@gmail.com>
2018-04-15 00:59:49 -0400

    Update Beth_Breadmaker.pl

--- erudnext/Beth_Breadmaker.pl
+++ erudnext/Beth_Breadmaker.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::emote("Hello there $name! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around!");
+		quest::say("Hello there $name! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around!");
 	}
 }
