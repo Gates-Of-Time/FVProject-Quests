a2b8c9d6498ba1fb96a7475aac053e1972da7205
TurmoilToad <TurmoilToad@gmail.com>
2018-04-15 21:53:08 -0400

    Update Snowflake.pl

--- everfrost/Snowflake.pl
+++ everfrost/Snowflake.pl
@@ -1,23 +1,29 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Grrrrrr!!");
-		}
+	}
 	if ($text=~/megan/i) {
 		quest::say("Grrroarrr !!");
-		    if ($faction <= 4 && GetX() == -1161 and GetY() == 2022) {
+		#:: Match if Amiable or better at spawn point
+		if ($faction <= 4 && $x == -1161 && $y == 2022) {
 			quest::say("Ruff... ruff... woof!");
-			quest::start(195); # goto Megan O'reilly
-			}
-			if ($faction == 5) {
-			quest::Emote("apparently responds only to one who follows the Wolves of the North.");
-			}
-			if ($faction >= 6) {
-			quest::Emote("growls at you. It can sense you are an enemy of the Wolves of the North.");
-			}
+			#:: Goto Megan O'reilly
+			quest::start(195);
+		}
+		#:: Match if Indifferent at spawn point
+		elsif ($faction == 5 && $x == -1161 && $y == 2022) {
+			quest::emote("apparently responds only to one who follows the Wolves of the North.");
+		}
+		#:: Match if Apprehensive or worse at spawn point
+		elsif ($faction >= 6 && $x == -1161 && $y == 2022) {
+			quest::emote("growls at you. It can sense you are an enemy of the Wolves of the North.");
 		}
 	}
-	sub EVENT_ITEM {
-		#:: Return unused items
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
-# Converted to Perl by SS 
+
+# Converted to Perl by SS
