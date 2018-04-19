2d67d0f20a278f7a37e90c449346b545a0cc2439
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-04-19 07:48:36 -0400

    Create Illithi_Nollith.pl
    Updated Illithi_Nollith to perl  in erudnext - new file no link

---
+++ erudnext/Illithi_Nollith.pl
@@ -0,0 +1,8 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Shhhh... Greetings, $name. Welcome to the Library of Erudin. Please respect the other patrons and speak softly.");
+	}
+	if ($text=~/testament of vanear/i) {
+		quest::say("Oh my!! You have not heard?! The Testament of Vanear was stolen from the library! It was checked out by a man named Moodoro. He is an Erudite, but I believe he was executed for some crime. Now the book is lost forever. You should speak with Tol Nicelot. He used to know Moodoro. Tol is most likely upstairs.");
+	}
+}
