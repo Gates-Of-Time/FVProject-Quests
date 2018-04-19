cc568c757046cedd31a7deceab6a7b97d3dc2048
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-04-19 15:33:17 -0400

    Create Sinnkin_Highbrow.pl
    Converted Sinnkin_Highbrow in erudnext to perl - new file no link

---
+++ erudnext/Sinnkin_Highbrow.pl
@@ -0,0 +1,23 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::emote("Why. hello there fellow scholar!  Don't you just love books?  I am quite a bookworm myself.  I could read forever!  I am currently working my way through the 'T's.");
+	}
+	if ($text=~/erud's tonic/i) {
+		quest::emote("Erud's Tonic! That is none of your concern!");
+	}
+}
+sub EVENT_ITEM {
+	#:: Turn in for 13121 -  Innoruuk's Kiss of Death
+	if (plugin::check_handin(\%itemcount, 13121 => 1)) {
+		quest::say("It's about time you figured it out, genius! Maybe you should spend more time in the library. Here, take this tonic and get out of here before they see me giving it to you and turn us both inside out.");
+		#:: Give item 13118 - Erud's Tonic
+		quest::summonitem(13118);
+		#:: Give a small amount of xp
+		quest::exp(500);
+		#:: Ding!
+		quest::ding();
+		#:: Set faction
+		quest::faction(380,1); 	#:: + Erudite Citizen
+	}
+	plugin::return_items(\%itemcount);
+}
