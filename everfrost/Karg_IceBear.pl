bf77c9f1a2893526b8e0adb265209fd39dc22a26
TurmoilToad <TurmoilToad@gmail.com>
2018-04-20 14:37:20 -0400

    Update Karg_IceBear.pl

--- everfrost/Karg_IceBear.pl
+++ everfrost/Karg_IceBear.pl
@@ -22,7 +22,7 @@ sub EVENT_SAY {
 		quest::say("Have you some polar bear pelts? You know I can furnish warm cloaks from them, and for you I will do it for the measly sum of 5 platinum pieces.");
 	}
 	if ($text=~/werewolf/i) {
-		quest::say("Werewolf?! I have not seen a werewolf in years. Have you slain one and collected its [" . quest::saylink("skin") . "][skin] or [" . quest::saylink("claws") . "]?");
+		quest::say("Werewolf?! I have not seen a werewolf in years. Have you slain one and collected its [" . quest::saylink("skin") . "] or [" . quest::saylink("claws") . "]?");
 	}
 	if ($text=~/skin/i) {
 		quest::say("You have managed to procure a werewolf skin?? Amazing! Well then, I will let you know that for a fee of 100 platinum, I can craft a hearty cloak for you if you leave the skin and the coin with me.");
