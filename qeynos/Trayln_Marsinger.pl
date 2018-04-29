250be408aefba76cae02801bc8e159e53eb2e4db
TurmoilToad <TurmoilToad@gmail.com>
2018-04-29 15:45:21 -0400

    Update Trayln_Marsinger.pl

--- qeynos/Trayln_Marsinger.pl
+++ qeynos/Trayln_Marsinger.pl
@@ -3,6 +3,7 @@ sub EVENT_SAY {
 		quest::say("Hail, $name. If you are interested in helping the League of Antonican Bards by delivering some mail then you should talk to my wife, Eve.");
 	}
 }
+
 sub EVENT_ITEM {
 	#:: Turn in for 18150 or 18151-  Bardic Letter (Qeynos)
 	if (plugin::check_handin(\%itemcount, 18150 =>1 || 18151 => 1)) {
@@ -17,7 +18,8 @@ sub EVENT_ITEM {
 		quest::faction(135,10); 	#:: + Guards of Qeynos
 		quest::faction(273,-30); 	#:: - Ring of Scale
 		quest::faction(207,-30); 	#:: - Mayong Mistmoore
-		quest::givecash(0,0,quest::ChooseRandom(1,2,3,4,5,6),0); #:: Give random amount of cash
+		#:: Give random amount of cash
+		quest::givecash(0,0,quest::ChooseRandom(1,2,3,4,5,6),0);
 	}
 	#:: Turn in for 18153 or 18154-  Bardic Letter (Qeynos)
 	if (plugin::check_handin(\%itemcount, 18153=> 1 || 18154 => 1)) {
@@ -32,7 +34,8 @@ sub EVENT_ITEM {
 		quest::faction(135,10); 	#:: + Guards of Qeynos
 		quest::faction(273,-30); 	#:: - Ring of Scale
 		quest::faction(207,-30); 	#:: - Mayong Mistmoore
-		quest::givecash(0,0,quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10,11,12),0); #:: Give random amount of cash
+		#:: Give random amount of cash
+		quest::givecash(0,0,quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10,11,12),0);
 	}
 	plugin::return_items(\%itemcount);
 }
