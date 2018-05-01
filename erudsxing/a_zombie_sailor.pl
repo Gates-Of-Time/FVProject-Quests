0b76da027a1fea07a63fa57cf66d7ad214262ebf
TurmoilToad <TurmoilToad@gmail.com>
2018-05-01 09:40:18 -0400

    Update a_zombie_sailor.pl

--- erudsxing/a_zombie_sailor.pl
+++ erudsxing/a_zombie_sailor.pl
@@ -1,32 +1,32 @@
 sub EVENT_SPAWN {
-  # use entity variable so each instance has his own copy.
-  # Also because var{$mobid} does not work in EVENT_DEATH
-  plugin::SEV($npc, "hasmap", 0);
-
-  # Check to see if he should have the blurred map.  3.5%
-  # Assigning loot locally since I can't find a way to see if he has
-  # one if I let the loottables do this.
-  $roll = plugin::RandomRange(1,200);
-  if ($roll <= 7) {
-    quest::addloot(13423, 1, 0); #Blurred Map
-    plugin::SEV($npc, "hasmap", 1);
-  }
+	#:: SetEntityVariable variable_name, variable_value
+	plugin::SEV($npc, "hasmap", 0);
+	#:: Set local loot chance for 13423 - Blurred Map at 3.5%
+	$roll = plugin::RandomRange(1,200);
+	if ($roll <= 7) {
+		#:: Add to loot 13423 - Blurred Map
+		quest::addloot(13423, 1, 0);
+		#:: Update Entity Variable
+		plugin::SEV($npc, "hasmap", 1);
+	}
 }
 
 sub EVENT_COMBAT {
-  # Talkative when he enters combat IF he has the map.
-  $myhasmap=plugin::REV($npc, "hasmap");
-  if ($myhasmap && $combat_state == 1) {
-    quest::say("Join.. Glug.. Tombor in his.. watery grave!!");
-    $classplural = "${class}s";
-    quest::say("$classplural like you always bring out the worst in me.");
-  }
+	#:: ReadEntityVariable entity, variable_name
+	$myhasmap=plugin::REV($npc, "hasmap");
+	#:: Match if the entity has 13423 - Blurred Map and enters combat
+	if ($myhasmap && $combat_state == 1) {
+		quest::say("Join.. Glug.. Tombor in his.. watery grave!!");
+		$classplural = "${class}s";
+		quest::say("$classplural like you always bring out the worst in me.");
+	}
 }
 
 sub EVENT_DEATH_COMPLETE {
-  # Talkative when he dies IF he has the map.
-  $myhasmap = plugin::REV($npc, "hasmap");
-  if ($myhasmap) {
-    quest::say("My map!! You have my.. map.");
-  }
+	#:: ReadEntityVariable entity, variable_name
+	$myhasmap = plugin::REV($npc, "hasmap");
+	#:: Match if the entity has 13423 - Blurred Map
+	if ($myhasmap) {
+		quest::say("My map!! You have my.. map.");
+	}
 }
