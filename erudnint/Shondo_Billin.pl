94b23c222b8b9cb5670ac9ae09f40f2969f48f07
TurmoilToad <TurmoilToad@gmail.com>
2018-05-07 08:19:23 -0400

    Update and rename Shondo_Billin.lua to Shondo_Billin.pl

---
+++ erudnint/Shondo_Billin.pl
@@ -0,0 +1,26 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Oh, Brell!! Help me get out of here. I did nothing wrong!! I did not know you could not take that [" . quest::saylink("tonic") . "] out of the city.");
+	}
+	if ($text=~/tonic/i) {
+		quest::say("Oh, why did I have to try and help that Erudite in Qeynos? He was sick and vomiting and needed something called Erud's Tonic!! Now just because I wanted to help, I am going to be vomiting up my insides!! Oh, Brell!!! GET ME OUT OF HERE!!");
+	}
+	if ($text=~/heretic/i) {
+		quest::say("Why are you asking me about the heretic?! I am going to die! DO YOU HEAR ME?!! Oh, I need a drink. Please... honor a dying man's last request, a bottle of Vasty Deep Ale. If you get me that, I will tell you what I know of the heretic.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match 13126 - Vasty Deep Ale
+	if (plugin::check_handin(\%itemcount, 13126 => 1)) {
+		quest::say("Gulp.. Gulp.. Aaahhh!!! That hits the spot. All I can tell you is that the heretic's name was E'lial B'rook. He mumbled something about a rendezvous and he would be floating down a river, smooth sailing back to something called Paineel. That's all I know.");
+		quest::ding();
+		quest::exp(100);
+		#:: Set Factions
+		quest::faction(115,10);		#:: Gem Choppers +10
+		quest::faction(210,10);		#:: Merchants of Ak'Anon +10
+		quest::faction(176,10);		#:: King Ak'Anon +10
+		quest::faction(71,-30);		#:: Dark Reflection -30
+	}
+	plugin::return_items(\%itemcount);
+}
