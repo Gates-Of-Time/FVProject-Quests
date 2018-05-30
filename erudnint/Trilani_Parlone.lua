cddde72c087bdb0fb389a2cc2de50c47f6e9d189
TurmoilToad <TurmoilToad@gmail.com>
2018-05-30 15:13:49 -0400

    Update Trilani_Parlone.lua

--- erudnint/Trilani_Parlone.lua
+++ erudnint/Trilani_Parlone.lua
@@ -1,40 +1,52 @@
--- Tunare Warden
--- Missing The Mystic Cloak
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail. good adventurer!  Do you bring word from my [" . quest::saylink("husband") . "]?");
+	}
+	if ($text=~/husband/i) {
+		quest::say("Tolkar is his name. My beloved husband. He still lives in Felwithe while I study here. It must be hard on him.");
+	}
+}
 
-function event_say(e)
-	if(e.message:findi("hail")) then
-		e.self:Say("Hail. good adventurer!  Do you bring word from my [husband]?");
-	elseif(e.message:findi("husband")) then
-		e.self:Say("Tolkar is his name. My beloved husband. He still lives in Felwithe while I study here. It must be hard on him."); 
-	end
-end
-
-function event_trade(e)
-	local item_lib = require("items");
-
-	if(item_lib.check_turn_in(e.trade, {item1 = 1598})) then -- Black Stone Candlestick
-		e.self:Say("I sense a great evil power in this candlestick. I will need you to concoct a divinatory aid for me before I can discern more. Take this suspension and brew it in a brew barrel with one white hellebore, a pouch of the red dust created by the Fire Peak Goblin Wizards, and the caustic substance used by the werebats in Unrest.");
-		e.other:Ding();
-		e.other:SummonItem(1596); -- Magical Suspension Fluid
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 1597})) then -- Divinatory Concoction 
-		e.self:Emote("performs a subtle divinatory ritual. 'This is a powerful evil indeed. The smoke from special candles crafted by the Teir'Dal and burned in this candlestick allows the creation of undead of unordinary might. The Ghasts are only one of its many possible creations. I will concoct a powder for you to take back to Yeolarn that will assist in defeating the monstrosities the candle has produced. The candlestick itself will remain here within the High Tower of Erudin for the time being.'");
-		e.other:SummonItem(1599); -- Powder of Unanimation
-		e.other:Ding();
-		e.other:Faction(99,10,0);
-		e.other:Faction(170,10,0);
-		e.other:Faction(178,10,0);
-		e.other:Faction(322,-30,0);
-		e.other:AddEXP(1500);
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 1056})) then --Faded Cloak
-		e.self:Say("Oh my lord Tunare! I did not know my daughter was in trouble. I thank you for saving her. Here, I have returned the once lost power to this cloak. Wear it with my humblest gratitude.");
-		e.other:SummonItem(1057); --Mystic Cloak
-		e.other:Ding();
-		e.other:Faction(99,10,0);
-		e.other:Faction(170,10,0);
-		e.other:Faction(178,10,0);
-		e.other:Faction(322,-30,0);
-	end
-	item_lib.return_items(e.self, e.other, e.trade);
-end
-
---END of FILE Zone:erudnint  ID:23020 -- Trilani_Parlone
+sub EVENT_ITEM {
+	#:: Match turn in for 1598 - Black Stone Candlestick
+	if (plugin::check_handin(\%itemcount, 1598 => 1)) {
+		quest::say("I sense a great evil power in this candlestick. I will need you to concoct a divinatory aid for me before I can discern more. Take this suspension and brew it in a brew barrel with one white hellebore, a pouch of the red dust created by the Fire Peak Goblin Wizards, and the caustic substance used by the werebats in Unrest.");
+		#:: Give item 1596 - Magical Suspension Fluid
+		quest::summonitem(1596);
+		#:: Give a small amount of xp
+		quest::exp(1500);
+		#:: Ding!
+		quest::ding();
+	}
+	#:: Match a 1597 - Divinatory Concoction
+	if (plugin::check_handin(\%itemcount, 1597 => 1)) {
+		quest::emote("performs a subtle divinatory ritual. 'This is a powerful evil indeed. The smoke from special candles crafted by the Teir'Dal and burned in this candlestick allows the creation of undead of unordinary might. The Ghasts are only one of its many possible creations. I will concoct a powder for you to take back to Yeolarn that will assist in defeating the monstrosities the candle has produced. The candlestick itself will remain here within the High Tower of Erudin for the time being.'");
+		#:: Give item 1599 - Powder of Unanimation
+		quest::summonitem(1599);
+		#:: Give a small amount of xp
+		quest::exp(1500);
+		#:: Ding!
+		quest::ding();
+		#:: Set faction
+		quest::faction(99,10);		#:: + Faydark's Champions
+		quest::faction(170,10);		#:: + Keepers of the Art
+		quest::faction(178,10);		#:: + King Tearis Thex
+		quest::faction(322,-30);	#:: - The Dead
+	}
+	#:: Match a 1056 - Faded Cloak
+	if (plugin::check_handin(\%itemcount, 1056 => 1)) {
+		quest::say("Oh my lord Tunare! I did not know my daughter was in trouble. I thank you for saving her. Here, I have returned the once lost power to this cloak. Wear it with my humblest gratitude.");
+		#:: Give item 1057 - Mystic Cloak
+		quest::summonitem(1057);
+		#:: Give a small amount of xp
+		quest::exp(1500);
+		#:: Ding!
+		quest::ding();
+		#:: Set faction
+		quest::faction(99,10);		#:: + Faydark's Champions
+		quest::faction(170,10);		#:: + Keepers of the Art
+		quest::faction(178,10);		#:: + King Tearis Thex
+		quest::faction(322,-30);	#:: - The Dead
+	}
+	plugin::return_items(\%itemcount);
+}
