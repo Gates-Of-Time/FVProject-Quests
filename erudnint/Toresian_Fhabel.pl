e0017b4f05f4493fab05ac089d725556081effc6
TurmoilToad <TurmoilToad@gmail.com>
2018-05-30 01:00:06 -0400

    Update Toresian_Fhabel.pl

--- erudnint/Toresian_Fhabel.pl
+++ erudnint/Toresian_Fhabel.pl
@@ -4,15 +4,20 @@ sub EVENT_SAY {
 	}
 	if ($text=~/study/i) {
 		quest::say("Then I welcome you into our circle. It is good to see new blood. I have a favor to ask of you. Do you wish to [" . quest::saylink("assist Toresian") . "] or do you not have the time?");
+		#:: Match The Erudin Palace >> Slansin (23080) or The Erudin Palace >> Slansin (23001)
 		if (!$entity_list->IsMobSpawnedByNpcTypeID(23080) || !$entity_list->IsMobSpawnedByNpcTypeID(23001)) {
+			#:: Randomly spawn Slansin (23080) [Shopkeeper] or Slansin (23001) [Enchanter]
 			quest::unique_spawn(quest::ChooseRandom(23080,23001),0,0,575,683,-12.13,78);
 		}
 	}
 	if ($text=~/assist Toresian/i) {
+		#:: Match if faction is amiable or better
 		if ($faction < 5) {
 			quest::say("Ahhhhh $name.  Slansin used to have need of those potions, however he mysteriously disappeared long ago. The only person I know of who still wants them is a cleric of the Church of Marr in Freeport.");			
+			#:: Give a 13983 - Inert Potion
 			quest::summonitem(13983);
 		}
+		#:: Match if faction is indifferent or worse
 		elsif ($faction => 5) {
 			quest::say("You are lucky to be standing. Leave here immediately or suffer grave consequences! You are not welcome amongst the Craftkeepers.");
 		}
