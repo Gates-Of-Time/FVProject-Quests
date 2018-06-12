798b7e6696b68a60a1efcae36ea67c88eeede477
TurmoilToad <TurmoilToad@gmail.com>
2018-06-12 13:49:49 -0400

    Update abandoned_heretic_pet.pl

--- tox/abandoned_heretic_pet.pl
+++ tox/abandoned_heretic_pet.pl
@@ -1,7 +1,10 @@
-sub EVENT_ITEM { 
-     if (plugin::check_handin(\%itemcount, 13894 => 1)){
-          quest::say("Good work, you should be running this operation instead of that Talrigar fellow. Have a small reward. A little bit of the gems I found while tunneling through this rock.");
-          quest::summonitem(quest::ChooseRandom(10015,10016,10017,10018)); 
-               }
-          plugin::return_items(\%itemcount);
-     }
+sub EVENT_ITEM {
+	#:: Match 13894 - Useless Cloth Cap
+	if (plugin::check_handin(\%itemcount, 13894 => 1)) {
+		quest::say("Good work, you should be running this operation instead of that Talrigar fellow. Have a small reward. A little bit of the gems I found while tunneling through this rock.");
+		#:: Give a random reward: 10015 - Malachite, 10016 - Lapis Lazuli, 10017 - Turquoise, 10018 - Hematite
+		quest::summonitem(quest::ChooseRandom(10015,10016,10017,10018));
+	}
+	#:: Return Unused Items
+	plugin::return_items(\%itemcount);
+}
