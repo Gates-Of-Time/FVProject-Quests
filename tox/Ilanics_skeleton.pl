6405417521ac21af2339b7325623c749aa773eb6
TurmoilToad <TurmoilToad@gmail.com>
2018-06-12 09:55:54 -0400

    Update Ilanics_skeleton.pl

--- tox/Ilanics_skeleton.pl
+++ tox/Ilanics_skeleton.pl
@@ -1,18 +1,15 @@
-# Ilanic's Scroll
-
 sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("My scroll must find my [scroll] so I may finish my research.");
-  }
-  if($text=~/scroll/i) {
-    quest::say("Kobold! Kobold!");
-    quest::depop();
-        quest::spawn2(38228, 174, 0, 758.83, 207.75, 42.35, 232.4);
-        quest::attack($rClient->GetName());
-  }
+	if ($text=~/hail/i) {
+		quest::say("My scroll must find my [" . quest::saylink("scroll") . "] so I may finish my research.");
+	}
+	if ($text=~/scroll/i) {
+		quest::say("Kobold! Kobold!");
+		quest::depop();
+		quest::spawn2(38228, 174, 0, 758.83, 207.75, 42.35, 232.4);
+		quest::attack($rClient->GetName());
+	}
 }
 
 sub EVENT_ITEM {
-  plugin::return_items(\%itemcount);
+	plugin::return_items(\%itemcount);
 }
-#END of FILE Zone:erudnint  ID:38150 --  Ilanics_skeleton
