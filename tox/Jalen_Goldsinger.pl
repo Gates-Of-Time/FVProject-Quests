1b0452a60734b3a9985ebc4656701a5454f7f1f6
TurmoilToad <TurmoilToad@gmail.com>
2018-06-15 13:59:31 -0400

    Update Jalen_Goldsinger.pl

--- tox/Jalen_Goldsinger.pl
+++ tox/Jalen_Goldsinger.pl
@@ -6,3 +6,32 @@ sub EVENT_SAY {
 		quest::say("Hrm? A crystal of capturing?  Aye.  I do know the secret of their making. Not that I would tell you. I will, however, create one for you. Provided you bring me some of their componenents and a large coin reserve, that is.  I require a potion of sorrow and 1000 gold coins.  A tidy sum, in return for which, I will do you the favor of not asking why you need such a... dark item.");
 	}
 }
+
+sub EVENT_ITEM {
+	#:: Set a scalar variable to accept only gold
+	my $cash = $gold);
+ 	#:: Match if cash is greater than 500 gp
+	if ($cash >= 1000) {
+		#:: Match a 7113 - Potion of Sorrow
+		if (plugin::check_handin(\%itemcount, 7113 => 1)) {
+			quest::say("Excuse me while I add some other items to this mix.  There. Finished. Here you go, and thank you for your business.");
+			#:: Grant a small amount of experience
+			quest::exp(1250);
+			#:: Give a 7112 - Empty Crystal Sphere
+			quest::summonitem(7112);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(112,1);	# + Gate Callers
+			quest::faction(145,1);	# + High Council of Erudin
+			quest::faction(147,1);	# + High Guard of Erudin
+			quest::faction(143,-1);	# - Heretics
+		}
+	}
+	else {
+		#:: Return unused money
+		quest::givecash($copper, $silver, $gold, $platinum);
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
+}
