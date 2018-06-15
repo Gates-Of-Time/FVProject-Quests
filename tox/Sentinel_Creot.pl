77a2807747819cc1f1122c924f1a550eebc949a2
TurmoilToad <TurmoilToad@gmail.com>
2018-06-15 09:34:10 -0400

    Update Sentinel_Creot.pl

--- tox/Sentinel_Creot.pl
+++ tox/Sentinel_Creot.pl
@@ -1,11 +1,13 @@
 sub EVENT_WAYPOINT_ARRIVE{
-	if(($wp==1) || ($wp==3)){
+	if (($wp==1) || ($wp==3)) {
 		quest::say("Hail citizens.  Sentinel Creot of the High Guard is on watch.");
 	}
-	if($wp==1){
+	if ($wp==1) {
+		#:: Run!
 		quest::SetRunning(1);
 	}
-	if($wp==2){
+	if ($wp==2) {
+		#:: Walk.
 		quest::SetRunning(0);
 	}
 }
