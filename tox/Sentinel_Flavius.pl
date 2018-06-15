b0362d25a184dccc97f3eb2a038614d69b0731ba
TurmoilToad <TurmoilToad@gmail.com>
2018-06-15 10:45:43 -0400

    Update Sentinel_Flavius.pl

--- tox/Sentinel_Flavius.pl
+++ tox/Sentinel_Flavius.pl
@@ -1,6 +1,5 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Hail. good citizen! You are safe when you are near a sentinel of the High Guard. Feel free to rest and recuperate."); }
+	if ($text=~/Hail/i) {
+		quest::say("Hail. good citizen! You are safe when you are near a sentinel of the High Guard. Feel free to rest and recuperate.");
+	}
 }
-#END of FILE Zone:tox  ID:38123 -- Sentinel_Flavius 
-
