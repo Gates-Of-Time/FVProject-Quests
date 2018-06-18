4c11253a103a177743dea87f92f6784cdc9906a8
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-06-18 18:35:30 -0400

    Update Linara_Parlone.pl
    added say links and notes -

--- najena/Linara_Parlone.pl
+++ najena/Linara_Parlone.pl
@@ -2,10 +2,10 @@
 
 sub EVENT_SAY {
   if($text=~/hail/i) {
-    quest::say("Please [help] me out of this horrid place! My family must be terribly worried!");
+    quest::say("Please [" . quest::saylink("help") . "] me out of this horrid place! My family must be terribly worried!");
   }
   if($text=~/help/i) {
-    quest::say("If you can find the [key] to my restraints. I should be able to escape.");
+    quest::say("If you can find the [" . quest::saylink("key") . "] to my restraints. I should be able to escape.");
   }
   if($text=~/key/i) {
     quest::say("Last I knew, Najena's priest had the key. I fear he has left this place though. I heard talk of traveling to some estate. I know not where. Oh dear, oh dear! I shall never leave this place!");
