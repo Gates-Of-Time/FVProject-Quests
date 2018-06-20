44008049cdbb8909d3cad1f1bed6c4c3f18269a4
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-06-20 18:00:29 -0400

    Update Jail_Clerk_Maryl.pl
    updated dialog to refer to Highkeep instead of Highpass

--- highkeep/Jail_Clerk_Maryl.pl
+++ highkeep/Jail_Clerk_Maryl.pl
@@ -2,8 +2,8 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hail! I am the jail clerk of Highkeep. How may I be of service?");
 	}
-	if ($text=~/highpass lottery/i) {
-		quest::say("The Highpass Lottery is held every season.  The Casino Treasurer, Lynn, handles all ticket sales and information.  She can be found at the vault.");
+	if ($text=~/highkeep lottery/i) {
+		quest::say("The Highkeep Lottery is held every season.  The Casino Treasurer, Lynn, handles all ticket sales and information.  She can be found at the vault.");
 	}
 	if ($text=~/fenn kaedrick/i) {
 		quest::say("Why do you ask of that horrid rogue? We had him locked away with that high elf and then he got bailed out. I know of no reason why anyone would want to bail his bloody carcass out of our dungeon.");
