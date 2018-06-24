f79461dbe4bc027a734539de419a3e4ff8dad21e
TurmoilToad <TurmoilToad@gmail.com>
2018-06-24 19:56:20 -0400

    Update Kirran_Mirrah.pl

--- kerraridge/Kirran_Mirrah.pl
+++ kerraridge/Kirran_Mirrah.pl
@@ -1,8 +1,11 @@
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 9968 => 1)) {
-    quest::say("How dare you bring me this! You shall pay for what you have done!!");
-    quest::attack($name);
-    quest::summonitem(9967); #Karran's Head
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 9968 - Smelly Sealed Bag
+	if (plugin::check_handin(\%itemcount, 9968 => 1)) {
+		quest::say("How dare you bring me this! You shall pay for what you have done!!");
+		quest::attack($name);
+		#:: Give a 9967 - Karran's Head
+		quest::summonitem(9967);
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
