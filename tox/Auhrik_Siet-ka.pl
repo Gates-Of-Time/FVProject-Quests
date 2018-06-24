f9638cd4897e3b4019cc1160a2f2dc40ae350657
TurmoilToad <TurmoilToad@gmail.com>
2018-06-24 11:24:39 -0400

    Create Auhrik_Siet-ka.pl

---
+++ tox/Auhrik_Siet-ka.pl
@@ -0,0 +1,32 @@
+sub EVENT_SAY {
+	if ($text=~/understand common/i) {
+		quest::say("Of course you do. Now, listen carefully. It may be too much for your feeble mind to gather all at once, and I hate to repeat myself. Bring me one bat wing, one rat ear, one snake egg, and one fire beetle eye. Take this bag, and make sure it's sealed before you return it to me. Snake eggs spoil. Now, quit staring at me with your jaw hanging open like a dead codfish. I would have mistaken you for one, but dead codfish smell better. Haha!");
+		#:: Give a 17802 - Small Sealable Bag
+		quest::summonitem(17802);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 14041 - Rolled Up Note
+	if (plugin::check_handin(\%itemcount, 14041 => 1)) {
+		quest::say("Why are you giving this to me? Oh I see. I forgot that rats don't know how to read. Haha! Well, then. I shall read it for you. You do [" . quest::saylink("understand common") . "] don't you? Haha.");
+	}
+	#:: Match a 12993 - Small Sealed Bag
+	if (plugin::check_handin(\%itemcount, 12993 => 1)) {
+		quest::say("My revenge has been satisfied. Thank you, my child. You have proven yourself to be a most worthy asset to our cause. Here, I no longer have any use for this, my ties to the old life are now severed.");
+		#:: Ding!
+		quest::ding();
+		#:: Give a moderate amount of experience
+		quest::exp(2000);
+		#:: Give a small cash reward
+		quest::givecash(5,3,0,0);
+		#:: Set factions
+		quest::faction(143,5);	# + Heretics
+		quest::faction(79,-5);	# - Deepwater Knights
+		quest::faction(112,-5);	# - Gate Callers
+		quest::faction(56,-5);	# - Craftkeepers
+		quest::faction(60,-5);	# - Crimson Hands
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
+}
