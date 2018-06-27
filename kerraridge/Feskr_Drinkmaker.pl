cd0d923d56c76cacf69696322a2ba1be0b2f9d35
TurmoilToad <TurmoilToad@gmail.com>
2018-06-27 15:06:12 -0400

    Update Feskr_Drinkmaker.pl

--- kerraridge/Feskr_Drinkmaker.pl
+++ kerraridge/Feskr_Drinkmaker.pl
@@ -1,18 +1,23 @@
 sub EVENT_SAY {
-  if($text=~/Hail/i) {
-    quest::say("Erh?  If you not be here to help Feskr with his [problem] then Feskr want you to leave.");
-  }
-  if($text=~/problem/i) {
-    quest::say("You wish to help?  Orrderrr of supplies I rrequest not arrive; ship sink.  Now Feskr needs supplies.  Feskr need bottle of Tunare's Finest. thunderhoof mushroom. tea leaf. and handmade backpack.  Rrrr. strange items but Feskr need to sell.  You bring. Feskr give you nice bag.");
-  }
+	if ($text=~/Hail/i) {
+		quest::say("Erh?  If you not be here to help Feskr with his [" . quest::saylink("problem") . "] then Feskr want you to leave.");
+	}
+	if ($text=~/problem/i) {
+		quest::say("You wish to help?  Orrderrr of supplies I rrequest not arrive; ship sink.  Now Feskr needs supplies.  Feskr need bottle of Tunare's Finest, thunderhoof mushroom, tea leaf, and handmade backpack.  Rrrr. Strange items but Feskr need to sell.  You bring. Feskr give you nice bag.");
+	}
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 12360 => 1, 14914 => 1, 14915 => 1, 17969 => 1)) {
-    quest::say("Ahhh. This will help. Many Kerrans will like these. Here. Take this old bag. I have too many. Maybe it help you.");
-    quest::summonitem(17032);
-    quest::exp(5000);
-   }
-   plugin::return_items(\%itemcount);
+	#:: Match a 12360 - Tunare's Finest, 14914 - Thunderhoof Mushroom, 14915 - Tea Leaves, 17969 - Hand Made Backpack
+	if (plugin::check_handin(\%itemcount, 12360 => 1, 14914 => 1, 14915 => 1, 17969 => 1)) {
+		quest::say("Ahhh. This will help. Many Kerrans will like these. Here. Take this old bag. I have too many. Maybe it help you.");
+		#:: Give a 17032 - Rough Leather Sack
+		quest::summonitem(17032);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(5000);
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
-#END of FILE Zone:kerraridge  ID:74076 -- Feskr_Drinkmaker 
