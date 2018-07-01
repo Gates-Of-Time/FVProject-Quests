e506368c6db13f4fb56e159b4a2ead4502e97787
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-07-01 19:34:02 -0400

    Update Brona_Frugrin.pl
    updated quest dialog to classic era info and added saylinks

--- steamfont/Brona_Frugrin.pl
+++ steamfont/Brona_Frugrin.pl
@@ -1,15 +1,12 @@
-# Quest for Staff of the Observers
-# Changed text slightly since Borxx no longer spawns on live.
-
 sub EVENT_SAY { 
   if($text=~/Hail/i){
-    quest::say("Well met, $name!  What brings you out this way?  Are you interested in becoming an observer?  No, you look like the adventuring type.  The wilds of the Steamfont Mountains is as far as my body goes.  But through my [duties] as an observer, my mind travels the cosmos.");
+    quest::say("Well met, $name!  What brings you out this way?  Are you interested in becoming an observer?  No, you look like the adventuring type.  The wilds of the Steamfont Mountains is as far as my body goes.  But through my [" . quest::saylink("duties") . "] as an observer, my mind travels the cosmos.");
   }
   if($text=~/duties/i){
-    quest::say("Well, I am quite an accomplished enchanter but most of my time now is spent crafting the magical lenses that enable us to see beyond the ceiling of Norrath and into other realms and dimensions.  Say, in your travels have you encountered any [evil eyes]?");
+    quest::say("Well, I am quite an accomplished enchanter but most of my time now is spent crafting the magical lenses that enable us to see beyond the ceiling of Norrath and into other realms and dimensions.  Say, in your travels have you encountered any [" . quest::saylink("evil eyes") . "]?");
   }
   if($text=~/evil eyes/i){
-    quest::say("Evil eyes are dangerous creatures of great magical power.  You will know one if you see one.  I believe that with the lens from an evil eye and some expert tinkering, I could create a device that will enable me to observe the gods themselves in their native planes.  If you were to bring me one of these lenses, the Eldritch Collective would reward you greatly.");
+    quest::say("Evil eyes are dangerous creatures of great magical power.  There is a rumor that somewhere on Antonica there lives a powerful evil eye by the name of Borxx. I believe that with the lens from her eye and some expert tinkering, I could create a device that will enable me to observe the gods themselves in their native planes. If you were to bring me this lens, the Eldritch Collective would reward you greatly.");
   }
 }
 
@@ -23,5 +20,3 @@ sub EVENT_ITEM {
   }
   plugin::return_items(\%itemcount);
 }
-
-# Quest by mystic414
