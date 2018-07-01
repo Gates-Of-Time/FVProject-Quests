f8880618e469a57e1325ccbcf0f4e773379f987d
TurmoilToad <TurmoilToad@gmail.com>
2018-07-01 11:24:37 -0400

    Update player.pl

--- paineel/player.pl
+++ paineel/player.pl
@@ -1,4 +1,9 @@
+#:: The only way into Paineel is through hard-coded values on character create.  Zone points have been removed.  
+#:: This is how we will move the player to the evil hut in Tox, and bind them, if they have created an evil Erudite
+
 sub EVENT_ENTERZONE {
-	quest::rebind(38,-751.86,1093.01,-46.54);
+	#:: Reset the player's bind point to a location in Tox (ZoneID 38)
+	$client->SetBindPoint(38,-751.86,1093.01,-46.54);
+	#:: Move the player to Tox
 	quest::movepc(38,-751.86,1093.01,-46.54,221);
 }
