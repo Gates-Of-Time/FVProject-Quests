4ff37290a078806cfc770ec86b53bd8c5b5e09e6
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-07-04 14:39:20 -0400

    Update Laoni_Reista.pl
    Updated quest reward for cleanse the ocean to include cash reward and random minor item based on availabe Alla info.

--- erudnext/Laoni_Reista.pl
+++ erudnext/Laoni_Reista.pl
@@ -9,19 +9,21 @@ sub EVENT_SAY {
 		quest::say("Go to the waters near the harbor. We have heard of Qeynos' rogue guild attempting to smuggle our valuable Vasty Deep water from Odus by way of swimmers. We require proof of their involvement. It is said they often carry special coins. Return one of these coins to me.?");
 	}
 }
-
 sub EVENT_ITEM {
 	#:: Turn in for 13881 -  Nicked Coin
 	if (plugin::check_handin(\%itemcount, 13881 => 1)) {
 		quest::say("Good work. You have shown these rogues who are the better swimmers. Now we have proof of their involvement. You are a fine addition to the temple. Take this small reward. Go, and serve Prexus.");
 		#:: Give a small amount of xp
 		quest::exp(100);
+		#:: Randomly choose Small lantern or copper band
+		quest::summonitem(quest::ChooseRandom(13003,10004));
 		#:: Ding!
 		quest::ding();
 		#:: Set faction
 		quest::faction(79,10);	 	#:: + Deepwater Knights
 		quest::faction(145,10); 	#:: + High Council of Erudin
 		quest::faction(143,-15); 	#:: - Heretics
+		quest::givecash(quest::ChooseRandom(5,6,7,8,9),0,0,0); #:: Give random amount of cash
 	}
 	plugin::return_items(\%itemcount);
 }
