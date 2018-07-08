9d52df0d5ea9834da244dfd96ca80fedec447dec
TurmoilToad <TurmoilToad@gmail.com>
2018-07-08 11:39:48 -0400

    Update diseased_female_rat.pl

--- steamfont/diseased_female_rat.pl
+++ steamfont/diseased_female_rat.pl
@@ -1,9 +1,8 @@
-#Bertoxxulous Initiate quest npc
-
 sub EVENT_DEATH_COMPLETE {
-  quest::spawn2(56086,0,0,$x,$y,$z,$h); #a_young_plague_rat
-  quest::spawn2(56086,0,0,$x+5,$y+5,$z,$h); #a_young_plague_rat
-  quest::spawn2(56086,0,0,$x+5,$y-5,$z,$h); #a_young_plague_rat
-  quest::spawn2(56086,0,0,$x-5,$y+5,$z,$h); #a_young_plague_rat
-  quest::spawn2(56086,0,0,$x-5,$y-5,$z,$h); #a_young_plague_rat
+	#:: Spawn Steamfont Mountains >> a_young_plague_rat (56086)
+	quest::spawn2(56086,0,0,$x,$y,$z,$h);
+	quest::spawn2(56086,0,0,$x+5,$y+5,$z,$h);
+	quest::spawn2(56086,0,0,$x+5,$y-5,$z,$h);
+	quest::spawn2(56086,0,0,$x-5,$y+5,$z,$h);
+	quest::spawn2(56086,0,0,$x-5,$y-5,$z,$h);
 }
