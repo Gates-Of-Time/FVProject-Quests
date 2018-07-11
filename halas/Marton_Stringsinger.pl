c1cd54f5fc35393ff32d003a4b42ad7c3043d552
TurmoilToad <TurmoilToad@gmail.com>
2018-07-11 09:33:40 -0400

    Update Marton_Stringsinger.pl

--- halas/Marton_Stringsinger.pl
+++ halas/Marton_Stringsinger.pl
@@ -1,20 +1,23 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hail. $name - Are you interested in helping the League of Antonican Bards by delivering some  [" . quest::saylink("mail") . "]?");
-		}
+	}
 	if ($text=~/mail/i) {
 		quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers. We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices. Are you [" . quest::saylink("interested") . "]?");
-		}
-	if ($text=~/interested/i) {		
+	}
+	if ($text=~/interested/i) {
 		quest::say("I have messages that need to go to - well, right now I have one that needs to go to Qeynos. Will you [" . quest::saylink("deliver mail to Qeynos") . "] for me?");
-		}
+	}
 	if ($text=~/deliver mail to Qeynos/i) {
 		quest::say("Take this letter to Tralyn Marsinger in Qeynos. You can find him at the bard guild hall. I am sure he will compensate you for your troubles..");
-		 quest::summonitem(18153); #A Bardic Letter (Qeynos)(*A Bardic Letter (Qeynos)) - id : 18153
-		}
+		#:: Give a 18153 - Bardic Letter (Qeynos)
+		quest::summonitem(18153);
+	}
 }
+
 sub EVENT_ITEM {
-		#:: Return unused items
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
-	# Converted to Perl by SS 
+
+# Converted to Perl by SS
