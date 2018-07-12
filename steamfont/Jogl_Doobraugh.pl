029336c6e5e0334f8ecff384d2d75100f993ec63
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 15:57:13 -0400

    Update Jogl_Doobraugh.pl

--- steamfont/Jogl_Doobraugh.pl
+++ steamfont/Jogl_Doobraugh.pl
@@ -1,33 +1,39 @@
 sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("Hellooo!!  My name is Jogl, master engineer with the Eldrithch Collective.  And this here is Charlotte.  Pay no mind to her, She won't bite unless you attack me.");
-	quest::signalwith(56108,1,1);
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hellooo!!  My name is Jogl, master engineer with the Eldritch Collective.  And this here is Charlotte.  Pay no mind to her, She won't bite unless you attack me.");
+		#:: Send a signal "1" to Steamfont Mountains >> Charlotte (56108)
+		quest::signalwith(56108,1,1);
+	}
 }
 
-sub EVENT_WAYPOINT_ARRIVE{
-	if($wp==12){
+sub EVENT_WAYPOINT_ARRIVE {
+	if ($wp==12) {
+		#:: Send a signal "2" to Steamfont Mountains >> Charlotte (56108)
 		quest::signalwith(56108,2,1);
 	}
-	if($wp==18){
+	if ($wp==18) {
+		#:: Send a signal "3" to Steamfont Mountains >> Charlotte (56108)
 		quest::signalwith(56108,3,1);
 	}
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 13209 =>1 )) {
-    quest::say("Hmmm. Here you go. Take this log to Drekon Vebnebber. He is the in-house merchant at Gemchoppers Hall. It is his duty to file away all these logs.");
-    quest::summonitem(quest::ChooseRandom(18837,18838));  #see not below
-    quest::faction( 91,1 );
-    quest::faction( 176,1 );
-    quest::faction(115 ,1 );
-    quest::faction( 209,-1 );
-    quest::faction( 322,-1 );
-    quest::exp(1000);
-    quest::ding();
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 13209 - Blackbox XXVIIC
+	if (plugin::check_handin(\%itemcount, 13209 =>1 )) {
+		quest::say("Hmmm. Here you go. Take this log to Drekon Vebnebber. He is the in-house merchant at Gemchoppers Hall. It is his duty to file away all these logs.");
+		#:: Give a random item:  18837 - Daily Log or 18838 - Daily Log
+		quest::summonitem(quest::ChooseRandom(18837,18838));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(1000);
+		#:: Set factions
+		quest::faction(91,1);		#:: + Eldritch Collective
+		quest::faction(176,1);		#:: + King Ak'Anon
+		quest::faction(115,1);		#:: + Gem Choppers
+		quest::faction(209,-1);		#:: - Meldrath
+		quest::faction(322,-1);		#:: - The Dead
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
-
-#random 18837 and 18838 for two sepearte quests beginnings Red V and duster
-#18837 used for Red V
