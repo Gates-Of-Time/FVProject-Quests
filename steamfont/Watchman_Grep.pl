538b1770327b21cafb51199f7ed354605d153d6c
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 12:15:10 -0400

    Update Watchman_Grep.pl

--- steamfont/Watchman_Grep.pl
+++ steamfont/Watchman_Grep.pl
@@ -1,9 +1,11 @@
 sub EVENT_ITEM {
-  plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
 
 sub EVENT_SIGNAL {
-  quest::say("What a glorious machine the cargo clockwork is!!");
-  quest::signal(56105,1);
+	#:: Signal from steamfont/Cargo_Clockwork.pl
+	quest::say("What a glorious machine the cargo clockwork is!!");
+	#:: Send a signal to Steamfont Mountains >> Cargo_Clockwork (56105)
+	quest::signal(56105,1);
 }
-
