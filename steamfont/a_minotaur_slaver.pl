27bfdf3fd3b133b3618ff1df44367ac6420f5774
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 07:24:54 -0400

    Update a_minotaur_slaver.pl

--- steamfont/a_minotaur_slaver.pl
+++ steamfont/a_minotaur_slaver.pl
@@ -1,13 +1,8 @@
-#NPC: a_minotaur_slaver   Zone: steamfont
-#
-#Emotes & Chance to trigger Minotaur Hero spawn on death
-#
-#by Qadar
-
-sub EVENT_DEATH_COMPLETE{
-  my $random_result = int(rand(100));
-  if($random_result >= 94){
-    quest::unique_spawn(56152,177,0,-1294,1360,-103);
-  }
-  quest::say("I die soon! Meldrath, help me!");
+sub EVENT_DEATH_COMPLETE {
+	my $random_result = int(rand(100));
+	if ($random_result >= 94) {
+		#:: Spawn a Steamfont Mountains >> Minotaur_Hero (56152)
+		quest::unique_spawn(56152,177,0,-1294,1360,-103);
+	}
+	quest::say("I die soon! Meldrath, help me!");
 }
