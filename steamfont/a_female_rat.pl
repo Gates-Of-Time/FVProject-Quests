cae02259cf07bf0fe22d656a9cbe6091c44e1662
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 12:44:37 -0400

    Update a_female_rat.pl

--- steamfont/a_female_rat.pl
+++ steamfont/a_female_rat.pl
@@ -1,16 +1,17 @@
-#Bertoxxulous Initiate quest npc
-
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 10262 => 1)) {
-    quest::emote("ingests the virulent substance and becomes ill.");
-    quest::summonitem(10263); #Empty infectious vial
-    quest::exp(100);
-    my $x = $npc->GetX();
-    my $y = $npc->GetY();
-    my $z = $npc->GetZ();
-    my $h = $npc->GetHeading();
-    quest::spawn2(56148,0,0,$x,$y,$z,$h);
-    quest::depop_withtimer();
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 10262 - Vial of Infectious Disease
+	if(plugin::check_handin(\%itemcount, 10262 => 1)) {
+		quest::emote("ingests the virulent substance and becomes ill.");
+		#:: Give a 10263 - Empty Infectious Vial
+		quest::summonitem(10263);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(100);
+		#:: Spawn a Steamfont Mountains >> diseased_female_rat (56148)
+		quest::spawn2(56148,0,0,$x,$y,$z,$h);
+		quest::depop_withtimer();
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
