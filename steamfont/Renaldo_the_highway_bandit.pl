2c2229f91671ef0cefd7ed83b38266b46e59ea42
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 14:36:36 -0400

    Update Renaldo_the_highway_bandit.pl

--- steamfont/Renaldo_the_highway_bandit.pl
+++ steamfont/Renaldo_the_highway_bandit.pl
@@ -1,4 +1,5 @@
 sub EVENT_SIGNAL {
-  quest::say("Your servos belong to me!");
-  quest::depop();
+	#:: Signal from steamfont/Cargo_Clockwork.pl
+	quest::say("Your servos belong to me!");
+	quest::depop();
 }
