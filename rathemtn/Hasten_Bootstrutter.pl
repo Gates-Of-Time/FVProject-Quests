ad240c3f715dcdd3f5181c5896a398bbdcd5e892
TurmoilToad <TurmoilToad@gmail.com>
2018-07-12 21:05:58 -0400

    Update Hasten_Bootstrutter.pl

--- rathemtn/Hasten_Bootstrutter.pl
+++ rathemtn/Hasten_Bootstrutter.pl
@@ -1,37 +1,56 @@
-# Journeyman Boots
-
 sub EVENT_SAY {
-  if ($text=~ /Hail/i) {
-    quest::say("Stopped I have done. Greet you I will. What business do we have? Hmm? Speak up!!");
-  }
-  if ($text=~ /Journeyman Boots/i) {
-    quest::say("Journeyman Boots are what you seek? Ha!! I have those upon my feet and disinigrate will they if my life is taken. Extra pair have I and [rare trade] can we make.");
-  }
-  if ($text=~ /Rare Trade/i) {
-    quest::say("I call for three things and Journeyman Boots are yours. I call for a [Shadowed Rapier], call for the [Ring of the Ancients] and call for Gold.. 3250 Gold Coins!! This the trade that I call.");
-  }
-  if ($text=~ /Shadowed Rapier/i) {
-    quest::say("Shadowed Men, foes to all, have appeared. Many lands do they walk. Invisible are they, but items they wield can be seen. Seek them out and return to me a Shadowed Rapier. Return it with haste before 'poof' goes the rapier!! No time to camp have you.");
-  }
-  if ($text=~ /Ring of the Ancients/i) {
-    quest::say("Ancient cyclopes do march the land.  Long have they lived and crafted the rings of the ancients.  I must have one and you shall go and fetch one for me.  Seek the lands of the oldest cyclops clans.  Seek the plains. seek the island in tears and search the dunes for there is one who is last.  His clan was blown from the sands.");
-  }
+	if ($text=~ /Hail/i) {
+		quest::say("Stopped I have done. Greet you I will. What business do we have? Hmm? Speak up!!");
+	}
+	if ($text=~ /Journeyman Boots/i) {
+		quest::say("Journeyman Boots are what you seek? Ha!! I have those upon my feet and disinigrate will they if my life is taken. Extra pair have I and [" . quest::saylink("rare trade") . "] can we make.");
+	}
+	if ($text=~ /Rare Trade/i) {
+		quest::say("I call for three things and Journeyman Boots are yours. I call for a [" . quest::saylink("Shadowed Rapier") . "], call for the [" . quest::saylink("Ring of the Ancients") . "] and call for Gold.. 3250 Gold Coins!! This the trade that I call.");
+	}
+	if ($text=~ /Shadowed Rapier/i) {
+		quest::say("Shadowed Men, foes to all, have appeared. Many lands do they walk. Invisible are they, but items they wield can be seen. Seek them out and return to me a Shadowed Rapier. Return it with haste before 'poof' goes the rapier!! No time to camp have you.");
+	}
+	if ($text=~ /Ring of the Ancients/i) {
+		quest::say("Ancient cyclops do march the land.  Long have they lived and crafted the rings of the ancients.  I must have one and you shall go and fetch one for me.  Seek the lands of the oldest cyclops clans.  Seek the plains. seek the island in tears and search the dunes for there is one who is last.  His clan was blown from the sands.");
+	}
 }
 
 sub EVENT_ITEM {
-  my $cash = (($platinum * 10) + $gold); #Only accept platinum and gold 
- 
-  if ($cash >= 3250) { #Gold/platinum equivalent of 325pp
-    if (plugin::check_handin(\%itemcount, 12268 => 1, 7100 => 1)) {
-      quest::say("The time to trade has come!! I am now rich and you are now fast. Take the Journeyman Boots and run like the wind.");
-      quest::exp(1250);
-      quest::summonitem(2300);
-    }
-  }
-  else {
-    quest::givecash(0, 0, $gold, $platinum); #Return gold/plat
-  }
-  plugin::return_items(\%itemcount);
-} 
-
-# EOF zone: rathemtn ID: 50188 NPC: Hasten_Bootstrutter
+	if ($gold >= 3250) {
+		if (plugin::check_handin(\%itemcount, 12268 => 1)) {
+			#:: Store the item in the MQ entity variable
+			plugin::mq_process_items(12268 => 1);
+		}
+		#:: Match trade for 7100 - Shadowed Rapier
+		if (plugin::check_handin(\%itemcount, 7100 => 1)) {
+			#:: Store the item in the MQ entity variable
+			plugin::mq_process_items(7100 => 1);
+		}
+		#:: Check the MQ entity variable for the items 
+		elsif (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
+			quest::say("The time to trade has come!! I am now rich and you are now fast. Take the Journeyman Boots and run like the wind.");
+			#:: Give a 2300 - Journeyman's Boots
+			quest::summonitem(2300);
+			#:: Ding!
+			quest::ding();
+			#:: Grant some experience
+			quest::exp(1250);
+			#:: Clear out the MQ entity variable for the next guy
+			plugin::clear_mq_handin();
+		}
+		else {
+		#:: Return unused coin
+		quest::givecash($copper, $silver, $gold, $platinum);
+		#:: Return unused items
+		plugin::return_items(\%itemcount);
+	}
+	if (plugin::check_handin(\%itemcount, 12268 => 1)) {
+		#:: Store the item in the MQ entity variable
+		plugin::mq_process_items(12268 => 1);
+	}
+	if (plugin::check_handin(\%itemcount, 7100 => 1)) {
+		#:: Store the item in the MQ entity variable
+		plugin::mq_process_items(7100 => 1);
+	}
+}
