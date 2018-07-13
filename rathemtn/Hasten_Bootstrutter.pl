3875a0410f4495d7aeed5e89a57ddb0d18551c8a
TurmoilToad <TurmoilToad@gmail.com>
2018-07-13 12:06:42 -0400

    Update Hasten_Bootstrutter.pl

--- rathemtn/Hasten_Bootstrutter.pl
+++ rathemtn/Hasten_Bootstrutter.pl
@@ -37,80 +37,88 @@ sub EVENT_ITEM {
 	#:: Just Rapier
 	if (plugin::check_handin(\%itemcount, 7100 => 1)) {
 		plugin::mq_process_items(7100 => 1);
-		if (plugin::check_mq_handin(12268 => 1) && $tradestate == 1) {
-			quest::say("Yay, you did it!");
-		}
-		elsif (plugin::check_mq_handin(12268 => 1) && $tradestate == 0) {
-			quest::say("You still owe me the gold!");
+		if ($tradestate == 1) {
+			if (plugin::check_mq_handin(12268 => 1)) {
+				quest::say("Yay, you did it!");
+			} else {
+				quest::say("You still owe me a Ring!");
+			}
 		}
 		elsif ($tradestate == 0) {
-			quest::say("You still owe me the gold and the Ring!");
+			if (plugin::check_mq_handin(12268 => 1)) {
+				quest::say("You still owe me gold!");
+			} else {
+				quest::say("You still owe me a Ring and gold!");
+			}
 		}
 	}
 	#:: Just Ring
 	if (plugin::check_handin(\%itemcount, 12268 => 1)) {
 		plugin::mq_process_items(12268 => 1);
-		if (plugin::check_mq_handin(7100 => 1) && $tradestate == 1) {
-			quest::say("Yay, you did it! $tradestate");
-		}
-		elsif (plugin::check_mq_handin(7100 => 1) && $tradestate == 0) {
-			quest::say("You still owe me the gold! $tradestate");
+		if ($tradestate == 1) {
+			if (plugin::check_mq_handin(7100 => 1)) {
+				quest::say("Yay, you did it!");
+			} else {
+				quest::say("You still owe me a Rapier!");
+			}
 		}
 		elsif ($tradestate == 0) {
-			quest::say("You still owe me the gold and the Rapier! $tradestate");
+			if (plugin::check_mq_handin(7100 => 1)) {
+				quest::say("You still owe me gold!");
+			} else {
+				quest::say("You still owe me a Rapier and gold!");
+			}
 		}
-		
 	}
 	#:: Just Ring and Rapier
 	if (plugin::check_handin(\%itemcount, 7100 => 1, 12268 => 1)) {
 		plugin::mq_process_items(7100 => 1);
 		plugin::mq_process_items(12268 => 1);
 		if ($tradestate == 1) {
-			quest::say("Yay, you did it! $tradestate");
+			quest::say("Yay, you did it!");
 		} else {
-			quest::say("Now just give me the gold! $tradestate");
+			quest::say("Now just give me the gold!");
 		}
 	}
 	#:: Just gold and Rapier
 	if (plugin::takeItemsCoin(0,0,3250,0, 7100 => 1)) {
 		plugin::mq_process_items(7100 => 1);
-		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it! $tradestate");
+		if (plugin::check_mq_handin(12268 => 1)) {
+			quest::say("Yay, you did it!");
 		} else {
 			$tradestate = 1;
-			quest::say("You still owe me a Ring! $tradestate");
+			quest::say("You still owe me a Ring!");
 		}
 	}
 	#:: Just gold and Ring
 	if (plugin::takeItemsCoin(0,0,3250,0, 12268 => 1)) {
 		plugin::mq_process_items(12268 => 1);
-		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it! $tradestate");
+		if (plugin::check_mq_handin(7100 => 1)) {
+			quest::say("Yay, you did it!");
 		} else {
 			$tradestate = 1;
-			quest::say("You still owe me a Rapier! $tradestate");
+			quest::say("You still owe me a Rapier!");
 		}
 	}
 	#:: Gold, Ring and Rapier
 	if (plugin::takeItemsCoin(0,0,3250,0, 7100 => 1, 12268 => 1)) {
-		quest::say("yay, you did it! $tradestate");
+		quest::say("yay, you did it!");
 	}
 	#:: Just Gold
 	if (plugin::takeCoin(0,0,3250,0)) {
 		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it! $tradestate");
+			quest::say("Yay, you did it!");
 		}
 		elsif (plugin::check_mq_handin(12268 => 1)) {
 			$tradestate = 1;
-			quest::say("You still owe me a Rapier! $tradestate");
+			quest::say("You still owe me a Rapier!");
 		}
 		elsif (plugin::check_mq_handin(7100 => 1)) {
 			$tradestate = 1;
-			quest::say("You still owe me a Ring! $tradestate");
-		}
-		else {
+			quest::say("You still owe me a Ring!");
+		} else {
 			$tradestate = 1;
-			quest::say("You still owe me a Ring and a Rapier! $tradestate");
+			quest::say("You still owe me a Ring and a Rapier!");
 		}
 	}
 }
