ef011a012a57835abc6c9b6da9c9b5722678d45d
TurmoilToad <TurmoilToad@gmail.com>
2018-07-13 13:14:26 -0400

    Update Hasten_Bootstrutter.pl

--- rathemtn/Hasten_Bootstrutter.pl
+++ rathemtn/Hasten_Bootstrutter.pl
@@ -55,26 +55,6 @@ sub EVENT_ITEM {
 			quest::say("Yay, you did it!");
 		}
 	}
-	#:: Just gold and Rapier
-	elsif (plugin::takeItemsCoin(0,0,3250,0, 7100 => 1)) {
-		plugin::mq_process_items(7100 => 1);
-		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it!");
-		} else {
-			$tradestate = 1;
-			quest::say("You still owe me a Ring!");
-		}
-	}
-	#:: Just gold and Ring
-	elsif (plugin::takeItemsCoin(0,0,3250,0, 12268 => 1)) {
-		plugin::mq_process_items(12268 => 1);
-		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it!");
-		} else {
-			$tradestate = 1;
-			quest::say("You still owe me a Rapier!");
-		}
-	}
 	#:: Just Rapier
 	elsif (plugin::takeItems(7100 => 1)) {
 		plugin::mq_process_items(7100 => 1);
