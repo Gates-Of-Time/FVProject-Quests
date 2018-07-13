a2f38548f304f8e2825abbda1db57877ed7f7aa3
TurmoilToad <TurmoilToad@gmail.com>
2018-07-13 13:17:26 -0400

    Update Hasten_Bootstrutter.pl

--- rathemtn/Hasten_Bootstrutter.pl
+++ rathemtn/Hasten_Bootstrutter.pl
@@ -48,13 +48,6 @@ sub EVENT_ITEM {
 			quest::say("Now just give me the gold!");
 		}
 	}
-	#:: Just Gold
-	elsif (plugin::takeCoin(325000)) {
-		$tradestate = 1;
-		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
-			quest::say("Yay, you did it!");
-		}
-	}
 	#:: Just Rapier
 	elsif (plugin::takeItems(7100 => 1)) {
 		plugin::mq_process_items(7100 => 1);
@@ -89,6 +82,26 @@ sub EVENT_ITEM {
 			} else {
 				quest::say("You still owe me a Rapier and gold!");
 			}
+		}
+	}
+	#:: Just gold and Rapier
+	elsif (plugin::takeItemsCoin(0,0,3250,0, 7100 => 1)) {
+		plugin::mq_process_items(7100 => 1);
+		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
+			quest::say("Yay, you did it!");
+		} else {
+			$tradestate = 1;
+			quest::say("You still owe me a Ring!");
+		}
+	}
+	#:: Just gold and Ring
+	elsif (plugin::takeItemsCoin(0,0,3250,0, 12268 => 1)) {
+		plugin::mq_process_items(12268 => 1);
+		if (plugin::check_mq_handin(12268 => 1, 7100 => 1)) {
+			quest::say("Yay, you did it!");
+		} else {
+			$tradestate = 1;
+			quest::say("You still owe me a Rapier!");
 		}
 	}
 	plugin::returnUnusedItems();
