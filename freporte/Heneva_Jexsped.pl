37631e5a93b9050587f6f5db223e29f8b1fa83fb
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-07-14 19:12:04 -0400

    Create Heneva_Jexsped.pl
    updated Heneva_Jexsped in freporte to perl new file no link

---
+++ freporte/Heneva_Jexsped.pl
@@ -0,0 +1,23 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::emote("Yes. yes. yes.  Hail to you. too, %s.  Now, quit bugging me! $name.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Turn in for 18855 -  A Tattered note
+	if (plugin::check_handin(\%itemcount, 18855 => 1)) {
+		quest::say("Welcome, friend. I see more than a slight glimmer of hate in your eyes. Good... for we have much work to do.");
+		#:: Give item 13565 - Old Stained Robe*
+		quest::summonitem(13565);
+		#:: Give a small amount of xp
+		quest::exp(100);
+		#:: Ding!
+		quest::ding();
+		#:: Set faction
+		quest::faction(86,100); 	#:: + Dismal Rage
+		quest::faction(184,-15); 	#:: - Knights of Truth
+		quest::faction(235,20); 	#:: + Opal Dark Briar
+	}
+	plugin::return_items(\%itemcount);
+}
