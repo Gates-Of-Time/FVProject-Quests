d1e0321408097ba147c4ae6972e898cad916630d
TurmoilToad <TurmoilToad@gmail.com>
2018-07-15 12:03:37 -0400

    Update Ikthar_Fireheart.pl

--- freporte/Ikthar_Fireheart.pl
+++ freporte/Ikthar_Fireheart.pl
@@ -5,14 +5,15 @@ sub EVENT_SAY {
 	if ($text=~/necessary ores/i) {
 		quest::say("Field plate is stronger than steel or fine steel plate and offers more protection to certain areas of the body. Unfortunately. it can only be crafted with high quality ores from those goblin infested mines. If you are planning on getting some ore I can instruct you in the [" . quest::saylink("techniques of crafting field plate") . "].");
 	}
-if ($text=~/techniques of crafting field plate/i) {
+	if ($text=~/techniques of crafting field plate/i) {
 		quest::say("Field plate may only be crafted in the special Freeport forge located in West Freeport. All pieces will require the appropriate plate mold. sea temper. a smithy hammer. leather backing. and chain jointing. Different amounts of ore will be needed for the various pieces of armor and the ore must be worked into [" . quest::saylink("folded sheets") . "].");
 	}
-if ($text=~/folded sheets/i) {
+	if ($text=~/folded sheets/i) {
 		quest::say("Folded sheets of high quality ore can be crafted only in the special Freeport forge or Qeynos Royal Forge in the same fashion that regular folded sheets are fashioned in a standard forge. Field plate visors. collars. and bracers require only one folded sheet of high quality ore. Helms. pauldrons. girdles. vambraces. gauntlets. and boots require two folded sheets of high quality ore. Breastplates. greaves. and cloaks require three folded sheets of high quality ore.");
 	}
 }
 
 sub EVENT_ITEM {
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
