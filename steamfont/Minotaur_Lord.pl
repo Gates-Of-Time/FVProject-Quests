77df546b85b50313b2ac2ddf67bb8a0c6557706f
TurmoilToad <TurmoilToad@gmail.com>
2018-07-15 11:56:44 -0400

    Update Minotaur_Lord.pl

--- steamfont/Minotaur_Lord.pl
+++ steamfont/Minotaur_Lord.pl
@@ -1,9 +1,7 @@
-#NPC: Minotaur_Lord   Zone: steamfont
-
-sub EVENT_AGGRO{
-    quest::say("I survived the gladiator pens of Qeynos!! What can you do to one such as I?!!");
+sub EVENT_AGGRO {
+	quest::say("I survived the gladiator pens of Qeynos!! What can you do to one such as I?!!");
 }
 
-sub EVENT_DEATH_COMPLETE{
-  quest::say("I die soon! Meldrath, help me!");
+sub EVENT_DEATH_COMPLETE {
+	quest::say("I die soon! Meldrath, help me!");
 }
