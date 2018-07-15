a36f4ab8c8026ab095d8e40314e30acb8e3073d2
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-07-15 10:52:39 -0400

    Create Innkeep_Rastle.pl
    Updated Innkeep Rastle to perl new file no link

---
+++ freporte/Innkeep_Rastle.pl
@@ -0,0 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail $class. Business is going well these days. I have a full stock of baked goods and other items for sale. Have a look around, $name!");
+	}
+}
+
+sub EVENT_ITEM {
+	plugin::return_items(\%itemcount);
+}
