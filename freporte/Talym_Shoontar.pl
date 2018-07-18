65c5348d45ecd83f9edd56aaa6d0e41a52936551
TurmoilToad <TurmoilToad@gmail.com>
2018-07-18 07:07:01 -0400

    Update Talym_Shoontar.pl

--- freporte/Talym_Shoontar.pl
+++ freporte/Talym_Shoontar.pl
@@ -5,14 +5,17 @@ sub EVENT_SAY {
 }
 
 sub EVENT_SIGNAL {
-	if ($signal == 1) {  	#:: Signal From Branis Noolright of East Freeport
+	#:: Signal From Branis Noolright of East Freeport
+	if ($signal == 1) {
 		quest::say("What kind of girly song was tha? It stank as bas as a troll's breath.");
 	}
-	if ($signal == 5) {		#:: Signal From Palana Willin of East Freeport 
+	#:: Signal From Palana Willin of East Freeport 
+	if ($signal == 5) {
 		quest::say("Get off the stage, lass! Can't ye see he left because ye can't carry a tune?!");
 	}
 }
 
 sub EVENT_ITEM {
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
