487887aa954e4d55520bc9db7021216a7e77d39e
TurmoilToad <TurmoilToad@gmail.com>
2018-07-18 07:08:32 -0400

    Update Sir_Edwin_Motte.pl

--- freporte/Sir_Edwin_Motte.pl
+++ freporte/Sir_Edwin_Motte.pl
@@ -5,20 +5,25 @@ sub EVENT_SAY {
 }
 
 sub EVENT_SIGNAL {
-	if ($signal == 1) {  	#:: Signal From Branis Noolright of East Freeport
+	#:: Signal From Branis Noolright of East Freeport
+	if ($signal == 1) {
 		quest::say("What is it with all this political revelry. Let's here more jests!");
 	}
-	if ($signal == 3) {		#:: Signal From Branis Noolright of East Freeport & Signal From Palana Willin of East Freeport 
+	#:: Signal From Branis Noolright of East Freeport & Signal From Palana Willin of East Freeport 
+	if ($signal == 3) {
 		quest::say("I say, that Sir Lucan is no man of nobility. So they say...");
 	}
-	if ($signal == 4) {		#:: Signal From Palana Willin of East Freeport 
+	#:: Signal From Palana Willin of East Freeport 
+	if ($signal == 4) {
 		quest::say("I say!! I have fought side by side with the great Antonius bayle. He is a man of supreme virtue.");
 	}
-	if ($signal == 6) {		#:: Signal From Palana Willin of East Freeport 
+	#:: Signal From Palana Willin of East Freeport 
+	if ($signal == 6) {
 		quest::say("Bloody right, you are! Those fellows can't keep down any grog.");
 	}
 }
 
 sub EVENT_ITEM {
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
