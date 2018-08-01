386779d9796561f73836840bbb8db4288e19f75c
TurmoilToad <TurmoilToad@gmail.com>
2018-08-01 12:15:10 -0400

    Update Doran_Vargnus.pl

--- oot/Doran_Vargnus.pl
+++ oot/Doran_Vargnus.pl
@@ -1,6 +1,6 @@
 sub EVENT_ITEM {
 	#:: Match 13995 - Knight
-	if (plugin::takeItems(13995 =>1 )) {
+	if (plugin::takeItems(13995 => 1)) {
 		quest::say("Ahh! You must have been sent by Beno of the Stormguard. I have been wondering when he would return my card. I have not been playing with a full deck. How about a reward? Hmm. Let's see what I have lying around... Here! Take this. I cannot possibly give you any of my good armor.");
 		#:: Randomly reward 3301 - Dwarven Ringmail Tunic, 3048 - Blackened Iron Bracers, 3042 - Blackened Iron Collar, 3050 - Blackened Iron Legplates, 3044 - Blackened Iron Spaulder, 20809 - Small Shield, 7012 - Bronze Dagger, 3111 - Ringmail Pants, 3110 - Ringmail Gloves, 7015 - Bronze Main Gauche
 		quest::summonitem(quest::ChooseRandom(3301,3048,3042,3050,3044,20809,7012,3103,3111,3110,7015));
