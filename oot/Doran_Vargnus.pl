1b742bc5e9ba7390f4baeb19168003804a910f30
TurmoilToad <TurmoilToad@gmail.com>
2018-08-01 12:11:26 -0400

    Update Doran_Vargnus.pl

--- oot/Doran_Vargnus.pl
+++ oot/Doran_Vargnus.pl
@@ -1,7 +1,20 @@
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 13995 =>1 )) {
-    quest::say("Thank you, here is your reward.");
-    quest::ChooseRandom(3301,3048,3042,3050,3044,20809,7012,3103,3111,3110,7015);
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match 13995 - Knight
+	if (plugin::takeItems(13995 =>1 )) {
+		quest::say("Ahh! You must have been sent by Beno of the Stormguard. I have been wondering when he would return my card. I have not been playing with a full deck. How about a reward? Hmm. Let's see what I have lying around... Here! Take this. I cannot possibly give you any of my good armor.");
+		#:: Randomly reward 3301 - Dwarven Ringmail Tunic, 3048 - Blackened Iron Bracers, 3042 - Blackened Iron Collar, 3050 - Blackened Iron Legplates, 3044 - Blackened Iron Spaulder, 20809 - Small Shield, 7012 - Bronze Dagger, 3111 - Ringmail Pants, 3110 - Ringmail Gloves, 7015 - Bronze Main Gauche
+		quest::summonitem(quest::ChooseRandom(3301,3048,3042,3050,3044,20809,7012,3103,3111,3110,7015));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(500);
+		#:: Set factions
+		quest::faction(314,5);	#:: + Storm Guard
+		quest::faction(169,1);	#:: + Kazon Stormhammer
+		quest::faction(219,1);	#:: + Miners Guild 249
+		quest::faction(215,1);	#:: + Merchants of Kaladim
+		quest::faction(57,-1);	#:: - Craknek Warriors
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
