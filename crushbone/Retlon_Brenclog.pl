2f69ab1e3125ccfe69956af296082f219ad975f2
TurmoilToad <TurmoilToad@gmail.com>
2018-08-04 15:09:30 -0400

    Update Retlon_Brenclog.pl

--- crushbone/Retlon_Brenclog.pl
+++ crushbone/Retlon_Brenclog.pl
@@ -14,35 +14,40 @@ sub EVENT_ITEM {
 	#:: Turn in for 20015 -  Shackle Key 15
 	if (plugin::check_handin(\%itemcount, 20015 => 1)) {
 		quest::say("Good work!! I shall be on my way. Farewell my friend!!");
-		#:: Give a small amount of xp
-		quest::exp(5000);
 		#:: Ding!
 		quest::ding();
+		#:: Create a hash for storing cash - 600 to 800cp
+		my %cash = plugin::RandomCash(600,800);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Grant a moderate amount of experience
+		quest::exp(5000);
 		#:: Set faction
 		quest::faction(91,1); 		#:: + Eldritch Collective
 		quest::faction(176,1); 		#:: + King Ak'Anon
 		quest::faction(115,1); 		#:: + Gem Choppers
-		quest::faction(322,-2); 	#:: - The Dead
-		#:: Give a small amount of cash copper - plat
-		quest::givecash(0,3,7,0);	
+		quest::faction(322,-2); 	#:: - The Dead	
 		#:: Set Depop Timer
 		quest::settimer("depop",240);
 	}
 	#:: Turn in for 18887 -  Sealed Letter (Note to Retlon Brenclog)
 	if (plugin::check_handin(\%itemcount, 18887 => 1)) {
 		quest::say("Ah! A letter from my brother! Mmm... no... no I don't have Tergon's spellbook. The orc's prophet took it and then they locked me in chains! Now why don't you go do something useful and get me key number 15!!");
-		#:: Give a small amount of xp
-		quest::exp(5000);
 		#:: Ding!
 		quest::ding();
+		#:: Create a hash for storing cash - 600 to 800cp
+		my %cash = plugin::RandomCash(600,800);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Grant a moderate amount of experience
+		quest::exp(5000);
 		#:: Set faction
 		quest::faction(91,1); 		#:: + Eldritch Collective
 		quest::faction(176,1); 		#:: + King Ak'Anon
 		quest::faction(115,1); 		#:: + Gem Choppers
-		quest::faction(322,-2); 	#:: - The Dead
-		#:: Give a small amount of cash copper - plat
-		quest::givecash(0,3,7,0);	
+		quest::faction(322,-2); 	#:: - The Dead	
 	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
 
