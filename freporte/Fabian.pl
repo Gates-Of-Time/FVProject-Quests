0724e98ffe762b8ba58c2470b9add06cfe9232c5
Dalethor <30395666+Dalethor@users.noreply.github.com>
2018-08-07 19:19:05 -0400

    Update Fabian.pl
    Updated to pound out later quest additions

--- freporte/Fabian.pl
+++ freporte/Fabian.pl
@@ -19,12 +19,13 @@ sub EVENT_ITEM {
 	}
 	#:: Match 13709 - Lute Strings
 	if (plugin::takeItems(13709 =>1)) {
-		my $random_lute_strings_message = int(rand(5)) +1;
-		if ($random_lute_strings_message == 4) {
-			quest::say("Many thanks, merry gentlefolk! Let me cross your palm in gratitude for your kindness. Hmmmm where did my lucky coin go?");
+		#:: 2007 Release Era Addition to Quest
+		#my $random_lute_strings_message = int(rand(5)) +1;
+		#if ($random_lute_strings_message == 4) {
+			#quest::say("Many thanks, merry gentlefolk! Let me cross your palm in gratitude for your kindness. Hmmmm where did my lucky coin go?");
 			#:: Give a 13710 - Etched Silver Coin
-			quest::summonitem(13710);
-		} else {
+			#quest::summonitem(13710);
+		#} else {
 			quest::say("Many thanks, merry gentlefolk! Let me cross your palm in gratitude for your kindness.");
 		}
 		#:: Ding!
@@ -41,24 +42,24 @@ sub EVENT_ITEM {
 		quest::faction(273,-5);		#:: - Ring of Scale
 		quest::faction(207,-5);		#:: - Mayong Mistmoore		
 	}
-	#:: Match a 13710 - Etched Silver Coin
-	if (plugin::takeItems(13710 =>1)) {
-		quest::say("'My lucky coin! How did it get in there? Well, never mind that. You are an honest person and although honesty is its own reward, I feel obligated to return the favor. Take this to Dionna if you enjoy music. Farewell friend!");
+	#:: 2007 era quest addition Match a 13710 - Etched Silver Coin
+	#if (plugin::takeItems(13710 =>1)) {
+		#quest::say("'My lucky coin! How did it get in there? Well, never mind that. You are an honest person and although honesty is its own reward, I feel obligated to return the favor. Take this to Dionna if you enjoy music. Farewell friend!");
 		#:: Give a 13708 - Note from Fabian
-		quest::summonitem(13708);
+		#quest::summonitem(13708);
 		#:: Ding!
-		quest::ding();
+		#quest::ding();
 		#:: Give a moderate amount of experience
-		quest::exp(5000);
+		#quest::exp(5000);
 		#:: Grant between 1200 and 2200 copper coins
-		%cash = plugin::RandomCash(900,2000);
-		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum}); 
+		#%cash = plugin::RandomCash(900,2000);
+		#quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum}); 
 		#:: Set factions
-		quest::faction(192,5);		#:: + League of Antonican Bards
-		quest::faction(184,5);		#:: + Knights of Truth
-		quest::faction(135,5);		#:: + Guards of Qeynos
-		quest::faction(273,-5);		#:: - Ring of Scale
-		quest::faction(207,-5);		#:: - Mayong Mistmoore
+		#quest::faction(192,5);		#:: + League of Antonican Bards
+		#quest::faction(184,5);		#:: + Knights of Truth
+		#quest::faction(135,5);		#:: + Guards of Qeynos
+		#quest::faction(273,-5);		#:: - Ring of Scale
+		#quest::faction(207,-5);		#:: - Mayong Mistmoore
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
