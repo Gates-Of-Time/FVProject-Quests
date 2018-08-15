e0212e5c9be134c0f184a54f62e7b6917b98e605
TurmoilToad <TurmoilToad@gmail.com>
2018-08-15 08:24:10 -0400

    Update Sarialiyn_Tranquilsong.pl

--- gfaydark/Sarialiyn_Tranquilsong.pl
+++ gfaydark/Sarialiyn_Tranquilsong.pl
@@ -1,4 +1,4 @@
-sub EVENT_SAY{
+sub EVENT_SAY {
 	if ($text=~/Hail/i) {
 		quest::say("Pleased to meet you $name. I am Sarialiyn Tranquilsong. Virtuoso of the Songweavers Kelethin division. I have spent all of my years here in Kelethin training our new recruits to ensure that all are ready to defend themselves once their adventures take them away from the Faydark and beyond.");
 	}
