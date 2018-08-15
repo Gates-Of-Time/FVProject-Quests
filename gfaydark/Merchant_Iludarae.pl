2b7f217c2870dd9358bb636b2ebc220bbd03327c
TurmoilToad <TurmoilToad@gmail.com>
2018-08-15 08:07:19 -0400

    Update Merchant_Iludarae.pl

--- gfaydark/Merchant_Iludarae.pl
+++ gfaydark/Merchant_Iludarae.pl
@@ -4,8 +4,11 @@ sub EVENT_ITEM {
 		quest::say("I have no words for the gratitude I feel. So many of our young ones have been captured by the filthy orcs. Please, take this for saving my sister.");
 		#:: Give a random leather item: 2012 - Leather Boots, 2004 - Leather Tunic, 2006 - Leather Cloak, 2005 - Leather Shoulderpads, 2011 - Leather Leggings
 		quest::summonitem(quest::ChooseRandom(2004, 2005, 2006, 2011, 2012));
-		quest::exp(30000);
+		#:: Ding!!
 		quest::ding();
+		#:: Grant a large amount of experience
+		quest::exp(30000);
 	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
