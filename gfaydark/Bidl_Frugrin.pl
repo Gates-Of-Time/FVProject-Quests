f43878abb490e8063d370c5c21fc3deeb76a771f
TurmoilToad <TurmoilToad@gmail.com>
2018-08-16 10:36:16 -0400

    Update Bidl_Frugrin.pl

--- gfaydark/Bidl_Frugrin.pl
+++ gfaydark/Bidl_Frugrin.pl
@@ -1,19 +1,10 @@
-#########################################################
-# Bidl Frugrin (ID:54302?)
-# Zone:   Greater Faydark (gfaydark)
-# Quest:  Telescope Lenses
-# Author: a_sewer_rat
-#########################################################
-
 sub EVENT_SAY {
-   if($text=~/Hail/i){
-      quest::say("Greeting. Do not you find the wine of Kelethin as sweet as their women?");
-   }
-   if($text=~/lens/i){
-      quest::say("Oh my!! I forgot I had that. Here you are. You better take it back I need to go elsewhere, bye now.");
-      quest::summonitem(13276); #telescope lens [bidl's lens]
-   }
+	if ($text=~/Hail/i) {
+		quest::say("Greetings $name. Do not you find the wine of Kelethin as sweet as their women?");
+	}
+	if ($text=~/lens/i) {
+		quest::say("Oh my!! I forgot I had that. Here you are. You better take it back.  I need to go elsewhere, bye now.");
+		#:: Give a 13276 - Telescope Lens
+		quest::summonitem(13276);
+	}
 }
-
-#END of FILE Zone:gfaydark  ID:54302? -- Bidl_Frugrin
-
