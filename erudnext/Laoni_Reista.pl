255eac9ef9bedf595dd5a9559f43b0292424497e
TurmoilToad <TurmoilToad@gmail.com>
2018-08-15 21:53:41 -0400

    Update Laoni_Reista.pl

--- erudnext/Laoni_Reista.pl
+++ erudnext/Laoni_Reista.pl
@@ -6,7 +6,7 @@ sub EVENT_SAY {
 		quest::say("You do seem a bit young. We shall have to test your mettle. Within this temple. you shall learn to swim as fast as the swordfish and attack with the bravery and skill of the shark. Are you willing to [" . quest::saylink("assist with the cleansing of the ocean") . "]?");
 	}
 	if ($text=~/assist with the cleansing of the ocean/i) {
-		quest::say("Go to the waters near the harbor. We have heard of Qeynos' rogue guild attempting to smuggle our valuable Vasty Deep water from Odus by way of swimmers. We require proof of their involvement. It is said they often carry special coins. Return one of these coins to me.?");
+		quest::say("Go to the waters near the harbor. We have heard of Qeynos' rogue guild attempting to smuggle our valuable Vasty Deep water from Odus by way of swimmers. We require proof of their involvement. It is said they often carry special coins. Return one of these coins to me.");
 	}
 }
 
