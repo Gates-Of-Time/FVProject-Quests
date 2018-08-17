8bb426a889b1bc35122b6e0f5b64935ba823fdf5
TurmoilToad <TurmoilToad@gmail.com>
2018-08-17 10:44:59 -0400

    Update Manik_Compolten.pl

--- akanon/Manik_Compolten.pl
+++ akanon/Manik_Compolten.pl
@@ -24,27 +24,30 @@ sub EVENT_ITEM {
 	#:: Turn in for 13208 -  Rusted Blackbox
 	if (plugin::check_handin(\%itemcount, 13208=> 1)) {
 		quest::say("Ah! Excellent work! I think we may have further use for you if you like.");
-		#:: Give a small amount of xp
-		quest::exp(1000);
 		#:: Ding!
 		quest::ding();
+		#:: Give a moderate amount of xp
+		quest::exp(1000);
+		#:: Create a hash for storing cash - 1300 to 1400cp
+		my %cash = plugin::RandomCash(1300,1400);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Set faction
 		quest::faction(115,3); 		#:: + Gem Choppers
 		quest::faction(210,10); 	#:: + Merchants of Ak'Anon
 		quest::faction(176,3); 		#:: + King Ak'Anon
 		quest::faction(71,-10); 	#:: - Dark Reflection
-		quest::faction(39,-10); 		#:: - Clan Grikbar
-		quest::givecash(2,1,3,1);	#:: Give a small amount of cash copper - plat
+		quest::faction(39,-10); 	#:: - Clan Grikbar
 	}
 	#:: Turn in for 13208 -  Series C Black Boxes Quest: Blackbox XIIC, Blackbox XXVIC, Blackbox XVIIC, Blackbox XXVIIC
 	if (plugin::check_handin(\%itemcount, 13212 => 1, 13211 => 1, 13213 =>1, 13214 =>1)) {
 		quest::say("Ah!! I see you had no problem finding all of the C series clockworks. Good work. Here. Take this blackbox. We received it from Clockwork 27C. Take it to Jogl Doobraugh. He is the operator of the only blackbox definer in Ak'Anon. He is out at the windmills checking on their operation.");
 		#:: Give a 13209 - Blackbox XXVIIC (Blackbox for 27C Returned)
 		quest::summonitem(13209);
-		#:: Give a small amount of xp
-		quest::exp(5000);
 		#:: Ding!
 		quest::ding();
+		#:: Give a moderate amount of xp
+		quest::exp(5000);
 		#:: Set faction
 		quest::faction(115,1); 		#:: + Gem Choppers
 		quest::faction(210,1); 		#:: + Merchants of Ak'Anon
@@ -57,17 +60,21 @@ sub EVENT_ITEM {
 		quest::say("This is fabulous news!! You have done well, young one.");
 		#:: Randomly choose one of the following: Bull Smasher, Iony's Absorber, or various pieces of small cloth armor
 		quest::summonitem(quest::ChooseRandom(13219, 13219, 13220, 13220, 1013, 1018, 1015, 1019, 1022, 1023, 1024, 1017, 1016, 1020, 1014, 1021));
-		#:: Give a small amount of xp
-		quest::exp(5000);
 		#:: Ding!
 		quest::ding();
+		#:: Give a moderate amount of xp
+		quest::exp(5000);
+		#:: Create a hash for storing cash - 1300 to 1400cp
+		my %cash = plugin::RandomCash(1300,1400);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Set faction
 		quest::faction(115,1); 		#:: + Gem Choppers
 		quest::faction(210,1); 		#:: + Merchants of Ak'Anon
 		quest::faction(176,1); 		#:: + King Ak'Anon
 		quest::faction(71,-1); 		#:: - Dark Reflection
 		quest::faction(39,-1); 		#:: - Clan Grikbar
-		quest::givecash(0,0,3,1);	#:: Give a small amount of cash copper - plat
 	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
