6180bf5b142faed867f513f635407e635a7f678d
TurmoilToad <TurmoilToad@gmail.com>
2018-08-17 10:29:53 -0400

    Update Laoni_Reista.pl

--- erudnext/Laoni_Reista.pl
+++ erudnext/Laoni_Reista.pl
@@ -14,18 +14,20 @@ sub EVENT_ITEM {
 	#:: Turn in for 13881 -  Nicked Coin
 	if (plugin::check_handin(\%itemcount, 13881 => 1)) {
 		quest::say("Good work. You have shown these rogues who are the better swimmers. Now we have proof of their involvement. You are a fine addition to the temple. Take this small reward. Go, and serve Prexus.");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Randomly choose 13003 - Small Lantern or 10004 - Copper Band
 		quest::summonitem(quest::ChooseRandom(13003,10004));
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(100);
+		#:: Create a hash for storing cash - 5 to 10cp
+		my %cash = plugin::RandomCash(5,10);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Set faction
 		quest::faction(79,10);	 	#:: + Deepwater Knights
 		quest::faction(145,10); 	#:: + High Council of Erudin
 		quest::faction(143,-15); 	#:: - Heretics
-		#:: Give random amount of cash
-		quest::givecash(quest::ChooseRandom(5,6,7,8,9),0,0,0);
 	}
 	#:: Return unused items
 	plugin::return_items(\%itemcount);
