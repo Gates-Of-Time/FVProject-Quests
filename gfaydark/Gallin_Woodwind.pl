6f8badc24414bd02bdaf009a75d45d268be1caba
TurmoilToad <TurmoilToad@gmail.com>
2018-08-20 16:42:39 -0400

    Update Gallin_Woodwind.pl

--- gfaydark/Gallin_Woodwind.pl
+++ gfaydark/Gallin_Woodwind.pl
@@ -1,26 +1,34 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Greetings. friend! I am Gallin Woodwind of the Emerald Warriors. It is my charge to send forth Fier'Dal to [hunt for meat]. Kelethin's citizens must be nourished and this requires meat. Such is the circle of life.");
-}
-if($text=~/i want to hunt for meat/i){
-quest::say("Good!!  We have enough young warriors hunting in our woods. Your duty shall be to take this pack to the ocean and seek out sharks. Fill the bag with shark meat and combine it. I shall reward you with a fine piercing weapon."); 
-quest::summonitem(17953);
-  }
+	if ($text=~/Hail/i) {
+		quest::say("Greetings. friend! I am Gallin Woodwind of the Emerald Warriors. It is my charge to send forth Fier'Dal to [" . quest::saylink("hunt for meat") . "]. Kelethin's citizens must be nourished and this requires meat. Such is the circle of life.");
+	}
+	if ($text=~/hunt for meat/i) {
+		quest::say("Good!!  We have enough young warriors hunting in our woods. Your duty shall be to take this pack to the ocean and seek out sharks. Fill the bag with shark meat and combine it. I shall reward you with a fine piercing weapon."); 
+		#:: Give a 17953 - Pack for Shark
+		quest::summonitem(17953);
+	}
 }
 
 sub EVENT_ITEM {
-
-  if (plugin::check_handin(\%itemcount,  12121=> 1)) {
-quest::say("Very good. This meat will supply our warriors with much strength. We have found the meat from sharks to be quite nourishing. Here, then, are your wages and your weapon. All of Kelethin appreciates your great deed.");
-   quest::faction( 92, 10);
-   quest::faction( 155, -10);
-   quest::faction( 212, 10);
-   quest::faction( 174, 10);
-   quest::exp(150);
-   quest::givecash(0,7,0,23);
+	#:: Match a 12121 - Pack of Meat
+	if (plugin::takeItems(12121 => 1)) {
+		quest::say("Very good. This meat will supply our warriors with much strength. We have found the meat from sharks to be quite nourishing. Here, then, are your wages and your weapon. All of Kelethin appreciates your great deed.");
+   		#:: Give a random reward: 7001 - Dagger, 7032 - Cast-Iron Rapier
+		quest::summonitem(quest::ChooseRandom(7001, 7032));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(150);
+		#:: Create a hash for storing cash - 700 to 1000cp
+		my %cash = plugin::RandomCash(700,1000);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(92,40);		#:: + Emerald Warriors
+		quest::faction(155,-6);		#:: - Indigo Brotherhood
+		quest::faction(212,4);		#:: + Merchants of Felwithe
+		quest::faction(174,4);		#:: + Kelethin Merchants
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
-  plugin::return_items(\%itemcount);
-}
-#END of FILE Zone:gfaydark  ID:54102 -- Gallin_Woodwind 
