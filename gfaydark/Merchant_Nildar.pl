59c0c13656735fd72155691dcf0a6c7c8876d73c
TurmoilToad <TurmoilToad@gmail.com>
2018-10-11 11:48:30 -0400

    Update Merchant_Nildar.pl

--- gfaydark/Merchant_Nildar.pl
+++ gfaydark/Merchant_Nildar.pl
@@ -1,18 +1,14 @@
-sub EVENT_ITEM{
-  if($item1 == 18903){
-    quest::say("My brother still lives? Thank you for this news. Please, take this with my deepest thanks.");
-    quest::exp(30000);
-    quest::ding();
-    my $random = int(rand(5));
-    if($random == 0){
-      quest::summonitem(2007); } #belt
-    if($random == 1){
-      quest::summonitem(2004); } #tunic
-    if($random == 2){
-      quest::summonitem(2006); } #cloak
-    if($random == 3){
-      quest::summonitem(2010); } #gloves
-    if($random == 4){
-      quest::summonitem(2011); } #leggings
-  }
+sub EVENT_ITEM {
+	#:: Match 18903 - Tattered Cloth Note
+	if (plugin::takeItems(18903 => 1)) {
+		quest::say("My brother still lives? Thank you for this news. Please, take this with my deepest thanks.");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(30000);
+		#:: Give a random reward:  2007 - Leather Belt, 2004 - Leather Tunic, 2006 - Leather Cloak, 2010 - Leather Gloves, 2011 - Leather Leggings
+		quest::summonitem(quest::ChooseRandom(2007, 2004, 2006, 2010, 2011));
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
