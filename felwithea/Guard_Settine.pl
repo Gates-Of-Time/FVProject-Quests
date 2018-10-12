5a65db0c2fce7a484f62f3990ce9addb1c0897b7
TurmoilToad <TurmoilToad@gmail.com>
2018-10-12 11:48:43 -0400

    Update Guard_Settine.pl

--- felwithea/Guard_Settine.pl
+++ felwithea/Guard_Settine.pl
@@ -1,13 +1,14 @@
 sub EVENT_ITEM {
 	#:: Match item 18901 - Ragged Cloth Note
-	if (plugin::check_handin(\%itemcount, 18901 => 1)) {
+	if (plugin::takeItems(18901 => 1)) {
 		quest::say("I.. but.. she.. You can take this. I guess I don't need it any more.");
-		#:: Give item 14640 - Silver Amber Ring
-		quest::summonitem(14640);
-		#:: Give  xp
-		quest::exp(30000);
 		#:: Ding!
 		quest::ding();
+		#:: Give a large amound of experience
+		quest::exp(30000);
+		#:: Give item 14640 - Silver Amber Ring
+		quest::summonitem(14640);
 	}
+	#:: Return unused items
 	plugin::return_items(\%itemcount);
 }
