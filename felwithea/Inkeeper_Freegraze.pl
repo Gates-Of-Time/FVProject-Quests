f406bf97276ea549ecb4829367dcce65ccb82f49
TurmoilToad <TurmoilToad@gmail.com>
2018-10-21 18:45:54 -0400

    Update Inkeeper_Freegraze.pl

--- felwithea/Inkeeper_Freegraze.pl
+++ felwithea/Inkeeper_Freegraze.pl
@@ -1,6 +1,7 @@
 sub EVENT_SAY {
 	if ($text=~/tolon nurbyte/i) {
 		quest::say("So you are inquiring about Mister Tolon Nurbyte, eh? He is on the second floor, last door on the right. You two had best not be up to any mischief. The pair of you look a little shifty for the kingdom of Felwithe.");
+		#:: Spawn a Northern Felwithe >> Tolon_Nurbyte (61095)
 		quest::spawn2(61095, 0, 0, -343, 155, 17, 8);
 	}
 }
