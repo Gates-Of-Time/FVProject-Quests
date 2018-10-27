4d7056bfc37c97706a23244121cb33a429663760
TurmoilToad <TurmoilToad@gmail.com>
2018-10-27 08:59:42 -0400

    Update Hogunk_Ventille.pl

--- kaladima/Hogunk_Ventille.pl
+++ kaladima/Hogunk_Ventille.pl
@@ -1,28 +1,37 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Hail. and welcome to Stormguard Hall. home to all great warriors! Are you also a [warrior of the Stormguard] or do you [follow another guild]?");
-}
-if($text=~/i am a warrior of the stormguard/i){
-quest::say("As I thought. You do not appear to be one who relies on wits rather than brawn. no offense. I have word that a Stormguard on duty has deserted his post at one of the guardposts in the Butcherblocks. I want you to go and seek him out. He is Guard Bundin. Return his Stormguard axe to me. along with his head. There shall be no cowards in the Stormguard!!");
-}
-if($text=~/i follow another guild/i){
-quest::say("Then go to your guild. This is the hall of the dwarven warriors - not a place for common folk to lounge around."); }
+	if ($text=~/Hail/i) {
+		quest::say("Hail, and welcome to Stormguard Hall, home to all great warriors! Are you also a [" . quest::saylink("warrior of the Stormguard") . "] or do you [" . quest::saylink("follow another guild") . "]?");
+	}
+	if ($text=~/warrior of the stormguard/i) {
+		quest::say("As I thought. You do not appear to be one who relies on wits rather than brawn. No offense. I have word that a Stormguard on duty has deserted his post at one of the guardposts in the Butcherblocks. I want you to go and seek him out. He is Guard Bundin. Return his Stormguard axe to me, along with his head. There shall be no cowards in the Stormguard!!");
+	}
+	if ($text=~/follow another guild/i) {
+		quest::say("Then go to your guild. This is the hall of the dwarven warriors - not a place for common folk to lounge around.");
+	}
 }
 
 sub EVENT_ITEM {
-if (plugin::check_handin(\%itemcount, 12156 =>1, 5014 =>1 )) {
-  quest::say("Well Done!");
-  quest::exp(5000);
-  quest::faction(169,5);
-  quest::faction(314,5);
-  quest::faction(219,5);  
-  quest::faction(419,5);
-  quest::faction(57,-5);
-  quest::ChooseRandom(5034,7013,6024,5027,6019);
-  quest::givecash(3,6,10,0);
+	#:: Match a 12156 - Dwarf Head and a 5014 - Rusty Axe
+	if (plugin::takeItems(12156 =>1, 5014 =>1 )) {
+		quest::say("Well Done!");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(5000);
+		#:: Reward a 5034 - Bronze Scimitar, 7013 - Bronze Rapier, 6024 - Bronze Morning Star, 5027 - Bronze Long Sword, 6019 - Bronze Mace
+		quest::summonitem(quest::ChooseRandom(5034,7013,6024,5027,6019));
+		#:: Create a hash for storing cash - 1600 to 1700cp
+		my %cash = plugin::RandomCash(1600,1700);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set Factions
+		quest::faction(169,5);		#:: + Kazon Stormhammer
+		quest::faction(314,5);		#:: + Storm Guard
+		quest::faction(219,5);		#:: + Miners Guild 249
+		quest::faction(419,5);		#:: + Kaladim Merchants
+		quest::faction(57,-5);		#:: - Craknek Warriors
+	}
+	#:: plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
-  #do all other handins first with plugin, then let it do disciplines
-  plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
-  plugin::return_items(\%itemcount);
-}
-#END of FILE Zone:kaladima  ID:60006 -- Hogunk_Ventille 
