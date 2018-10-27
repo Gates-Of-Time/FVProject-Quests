f48ef3c21c5795828184c39c526cba656bf73004
TurmoilToad <TurmoilToad@gmail.com>
2018-10-27 08:25:05 -0400

    Update Canloe_Nusback.pl

--- kaladima/Canloe_Nusback.pl
+++ kaladima/Canloe_Nusback.pl
@@ -1,101 +1,157 @@
 sub EVENT_SAY { 
-  if ($text=~/hail/i) {
-    quest::say("Step forward and speak up, young $name! Kaladim can always use another warrior. Are you [ready to serve Kaladim] or has a yellow streak appeared down your back?");
-  }
-  if ($text=~/ready to serve kaladim/i) {
-    quest::say("Then serve you shall. Let your training begin on the battlefields of Faydwer. Seek out and destroy all [Crushbone orcs]. Return their belts to me. I shall also reward you for every two orc legionnaire shoulder pads returned. A warrior great enough to slay one legionnaire shall surely have no problem with another. Go, and let the cleansing of Faydwer begin.");
-  }
-  if ($text=~/crushbone orcs/i) {
-    quest::say("The army of the Crushbone orcs is deadly indeed. They are great military strategists. It was a legion of them that brought down the great [Trondle Ogrebane]. Speak with Furtog Ogrebane about the Crushbones. He has need of warriors such as you.");
-  }
-  if ($text=~/trondle ogrebane/i) {
-    quest::say("Trondle Ogrebane is the legendary dwarven warrior who single-handedly exterminated the ogre clan called the [Mudtoes]. He was recently killed in battle. It took an entire legion of Crushbone orcs to bring him down. Furtog is still fuming about that.");
-  }
-  if ($text=~/mudtoes/i) {
-    quest::say("The Mudtoes were a small clan of ogres. They lived somewhere in the Butcherblock Mountains. They had an insatiable appetite for dwarves. They were finally destroyed by the hand of Trondle Ogrebane.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Step forward and speak up, young $name! Kaladim can always use another warrior. Are you [" . quest::saylink("ready to serve Kaladim") . "] or has a yellow streak appeared down your back?");
+	}
+	if ($text=~/ready to serve kaladim/i) {
+		quest::say("Then serve you shall. Let your training begin on the battlefields of Faydwer. Seek out and destroy all [" . quest::saylink("Crushbone Orcs") . "]. Return their belts to me. I shall also reward you for every two orc legionnaire shoulder pads returned. A warrior great enough to slay one legionnaire shall surely have no problem with another. Go, and let the cleansing of Faydwer begin.");
+	}
+	if ($text=~/crushbone orcs/i) {
+		quest::say("The army of the Crushbone orcs is deadly indeed. They are great military strategists. It was a legion of them that brought down the great [" . quest::saylink("Trondle Ogrebane") . "]. Speak with Furtog Ogrebane about the Crushbones. He has need of warriors such as you.");
+	}
+	if ($text=~/trondle ogrebane/i) {
+		quest::say("Trondle Ogrebane is the legendary dwarven warrior who single-handedly exterminated the ogre clan called the [" . quest::saylink("Mudtoes") . "]. He was recently killed in battle. It took an entire legion of Crushbone orcs to bring him down. Furtog is still fuming about that.");
+	}
+	if ($text=~/mudtoes/i) {
+		quest::say("The Mudtoes were a small clan of ogres. They lived somewhere in the Butcherblock Mountains. They had an insatiable appetite for dwarves. They were finally destroyed by the hand of Trondle Ogrebane.");
+	}
 }
 
-sub EVENT_ITEM { 
-  if(plugin::check_handin(\%itemcount, 13318 => 4)) { #Crushbone Belt x 4
-    quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::givecash(0,quest::ChooseRandom(20,21,22,23,24),0,0);
-    quest::ding();
-    quest::faction(314,40);
-    quest::faction(169,40);
-    quest::faction(219,40);
-    quest::faction(215,40);
-    quest::faction(57,-120);
-    quest::exp(28000);
-  }
-  elsif (plugin::check_handin(\%itemcount, 13318 => 3)) { #Crushbone Belt x 3
-    quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::givecash(0,quest::ChooseRandom(14,15,16,17,18,19),0,0);
-    quest::ding();
-    quest::faction(314,30);
-    quest::faction(169,30);
-    quest::faction(219,30);
-    quest::faction(215,30);
-    quest::faction(57,-90);
-    quest::exp(21000);
-  }
-  elsif (plugin::check_handin(\%itemcount, 13318 => 2)) { #Crushbone Belt x 2
-    quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::givecash(0,quest::ChooseRandom(10,11,12,13,14),0,0);
-    quest::ding();
-    quest::faction(314,20);
-    quest::faction(169,20);
-    quest::faction(219,20);
-    quest::faction(215,20);
-    quest::faction(57,-60);
-    quest::exp(14000);
-  }
-  elsif (plugin::check_handin(\%itemcount, 13318 => 1)) { #Crushbone Belt x 1
-    quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
-    quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
-    quest::givecash(0,quest::ChooseRandom(5,6,7,8,9),0,0);
-    quest::ding();
-    quest::faction(314,10);
-    quest::faction(169,10);
-    quest::faction(219,10);
-    quest::faction(215,10);
-    quest::faction(57,-30);
-    quest::exp(7000);
-  }
-  elsif (plugin::check_handin(\%itemcount, 13319 => 4)) { #Crushbone Shoulderpads x 4
-    quest::say("Aha!! You have downed a Crushbone legionnaire!! You have shown yourself to be a strong warrior. Take this. This is more becoming of a great warrior such as yourself. Let no creature stand in the way of the Stormguard!");
-    quest::summonitem(10017,2);
-    quest::givecash(0,0,8,0);
-    quest::ding();
-    quest::faction(314,20);
-    quest::faction(169,20);
-    quest::faction(219,20);
-    quest::faction(215,20);
-    quest::faction(57,-60);
-    quest::exp(64000);
-  }
-  elsif (plugin::check_handin(\%itemcount, 13319 => 2)) { #Crushbone Shoulderpads x 2
-    quest::say("Aha!! You have downed a Crushbone legionnaire!! You have shown yourself to be a strong warrior. Take this. This is more becoming of a great warrior such as yourself. Let no creature stand in the way of the Stormguard!");
-    quest::summonitem(10017);
-    quest::givecash(0,0,4,0);
-    quest::ding();
-    quest::faction(314,10);
-    quest::faction(169,10);
-    quest::faction(219,10);
-    quest::faction(215,10);
-    quest::faction(57,-30);
-    quest::exp(32000);
-  }
-  plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
-  plugin::return_items(\%itemcount);
+sub EVENT_ITEM {
+	#:: Match four 13318 - Crushbone Belt
+	if (plugin::takeItems(13318 => 4)) {
+		quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
+		#:: Reward a 9009 - Small Buckler, 2113 - Small Tattered Skullcap, 2114 - Small Tattered Mask, 2115 - Small Tattered Gorget
+		#:: 2116 - Small Patchwork Tunic, 2117 - Small Tattered Shoulderpads, 2118 - Small Patchwork Cloak, 2119 - Small Tattered Belt
+		#:: 2120 - Small Patchwork Sleeves, 2122 - Small Tattered Gloves, 2123 - Small Patchwork Pants, 2124 - Small Patchwork Boots
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(28000);
+		#:: Create a hash for storing cash - 200 to 250cp
+		my %cash = plugin::RandomCash(200,250);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,40);		#:: + Storm Guard
+		quest::faction(169,40);		#:: + Kazon Stormhammer
+		quest::faction(219,40);		#:: + Miners Guild 249
+		quest::faction(215,40);		#:: + Merchants of Kaladim
+		quest::faction(57,-120);	#:: - Craknek Warriors
+	}
+	#:: Match three 13318 - Crushbone Belt
+	elsif (plugin::takeItems(13318 => 3)) {
+		quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
+		#:: Reward a 9009 - Small Buckler, 2113 - Small Tattered Skullcap, 2114 - Small Tattered Mask, 2115 - Small Tattered Gorget
+		#:: 2116 - Small Patchwork Tunic, 2117 - Small Tattered Shoulderpads, 2118 - Small Patchwork Cloak, 2119 - Small Tattered Belt
+		#:: 2120 - Small Patchwork Sleeves, 2122 - Small Tattered Gloves, 2123 - Small Patchwork Pants, 2124 - Small Patchwork Boots
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(21000);
+		#:: Create a hash for storing cash - 140 to 190cp
+		my %cash = plugin::RandomCash(140,190);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,30);		#:: + Storm Guard
+		quest::faction(169,30);		#:: + Kazon Stormhammer
+		quest::faction(219,30);		#:: + Miners Guild 249
+		quest::faction(215,30);		#:: + Merchants of Kaladim
+		quest::faction(57,-90);		#:: - Craknek Warriors
+	}
+	#:: Match two 13318 - Crushbone Belt
+	elsif (plugin::takeItems(13318 => 2)) {
+		quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
+		#:: Reward a 9009 - Small Buckler, 2113 - Small Tattered Skullcap, 2114 - Small Tattered Mask, 2115 - Small Tattered Gorget
+		#:: 2116 - Small Patchwork Tunic, 2117 - Small Tattered Shoulderpads, 2118 - Small Patchwork Cloak, 2119 - Small Tattered Belt
+		#:: 2120 - Small Patchwork Sleeves, 2122 - Small Tattered Gloves, 2123 - Small Patchwork Pants, 2124 - Small Patchwork Boots
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(14000);
+		#:: Create a hash for storing cash - 100 to 140cp
+		my %cash = plugin::RandomCash(100,140);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,20);		#:: + Storm Guard
+		quest::faction(169,20);		#:: + Kazon Stormhammer
+		quest::faction(219,20);		#:: + Miners Guild 249
+		quest::faction(215,20);		#:: + Merchants of Kaladim
+		quest::faction(57,-60);		#:: - Craknek Warriors
+	}
+	#:: Match one 13318 - Crushbone Belt
+	elsif (plugin::takeItems(13318 => 1)) {
+		quest::say("Good work, warrior! Now continue with your training. Only on the battlefield can one become a great warrior.");
+		#:: Reward a 9009 - Small Buckler, 2113 - Small Tattered Skullcap, 2114 - Small Tattered Mask, 2115 - Small Tattered Gorget
+		#:: 2116 - Small Patchwork Tunic, 2117 - Small Tattered Shoulderpads, 2118 - Small Patchwork Cloak, 2119 - Small Tattered Belt
+		#:: 2120 - Small Patchwork Sleeves, 2122 - Small Tattered Gloves, 2123 - Small Patchwork Pants, 2124 - Small Patchwork Boots
+		quest::summonitem(quest::ChooseRandom(9009,2113,2114,2115,2116,2117,2118,2119,2120,2122,2123,2124));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(7000);
+		#:: Create a hash for storing cash - 50 to 90cp
+		my %cash = plugin::RandomCash(50,90);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,10);		#:: + Storm Guard
+		quest::faction(169,10);		#:: + Kazon Stormhammer
+		quest::faction(219,10);		#:: + Miners Guild 249
+		quest::faction(215,10);		#:: + Merchants of Kaladim
+		quest::faction(57,-30);		#:: - Craknek Warriors
+	}
+	#:: Match four 13319 - Crushbone Shoulderpads
+	elsif (plugin::takeItems(13319 => 4)) {
+		quest::say("Aha!! You have downed a Crushbone legionnaire!! You have shown yourself to be a strong warrior. Take this. This is more becoming of a great warrior such as yourself. Let no creature stand in the way of the Stormguard!");
+		#:: Reward two 10017 - Turquoise
+		quest::summonitem(10017,2);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(64000);
+		#:: Create a hash for storing cash - 700 to 900cp
+		my %cash = plugin::RandomCash(700,900);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,20);		#:: + Storm Guard
+		quest::faction(169,20);		#:: + Kazon Stormhammer
+		quest::faction(219,20);		#:: + Miners Guild 249
+		quest::faction(215,20);		#:: + Merchants of Kaladim
+		quest::faction(57,-60);		#:: - Craknek Warriors
+	}
+	#:: Match two 13319 - Crushbone Shoulderpads
+	elsif (plugin::takeItems(13319 => 2)) {
+		quest::say("Aha!! You have downed a Crushbone legionnaire!! You have shown yourself to be a strong warrior. Take this. This is more becoming of a great warrior such as yourself. Let no creature stand in the way of the Stormguard!");
+		#:: Reward a 10017 - Turquoise
+		quest::summonitem(10017);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(32000);
+		#:: Create a hash for storing cash - 350 to 450cp
+		my %cash = plugin::RandomCash(350,450);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(314,10);		#:: + Storm Guard
+		quest::faction(169,10);		#:: + Kazon Stormhammer
+		quest::faction(219,10);		#:: + Miners Guild 249
+		quest::faction(215,10);		#:: + Merchants of Kaladim
+		quest::faction(57,-30);		#:: - Craknek Warriors
+	}
+	#:: plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
-#END of FILE Zone:kaladima  ID:60005 -- Canloe_Nusback
