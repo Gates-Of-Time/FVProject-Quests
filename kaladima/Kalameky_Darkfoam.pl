35f97f4d6a582a77b8ede1757b8d51c26df30515
TurmoilToad <TurmoilToad@gmail.com>
2018-10-27 20:51:40 -0400

    Update Kalameky_Darkfoam.pl

--- kaladima/Kalameky_Darkfoam.pl
+++ kaladima/Kalameky_Darkfoam.pl
@@ -1,9 +1,14 @@
-sub EVENT_ITEM{
-  if(plugin::check_handin(\%itemcount, 18906 => 1)){
-    quest::say("'Why, that..! Bah! Fine. Stupid as a newborn gob. She's not gonna get this, then. Hey, you might as well take it. It was gonna be her reward fer a job well done.'");
-    quest::summonitem(23359);
-    quest::exp(30000);
-    quest::ding();
-  }
-  plugin::return_items(\%itemcount);
+sub EVENT_ITEM {
+	#:: Match a 18906 - Small Wood Carving
+	if (plugin::takeItems(18906 => 1)) {
+		quest::say("Why, that..! Bah! Fine. Stupid as a newborn gob. She's not gonna get this, then. Hey, you might as well take it. It was gonna be her reward fer a job well done.");
+		#:: Give a 23359 - Targ Shield
+		quest::summonitem(23359);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(30000);
+	}
+	#:: Return unused items
+	plugin::return_items(\%itemcount);
 }
