2eb28f76ed5ea59d343926c5d605040a16d81486
TurmoilToad <TurmoilToad@gmail.com>
2018-10-30 13:37:14 -0400

    Update Priestess_Ghalea.pl

--- kaladimb/Priestess_Ghalea.pl
+++ kaladimb/Priestess_Ghalea.pl
@@ -1,10 +1,5 @@
-#####################################################################
-#  NPC:  Priestess Ghalea
-#  Zone: North Kaladim
-#  Quests: Multiple --> Soil of Underfoot
-#####################################################################
-
 sub EVENT_SPAWN {
+	#:: Create a proximity, 100 units across
 	$x = $npc->GetX();
 	$y = $npc->GetY();
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
@@ -22,7 +17,7 @@ sub EVENT_SAY {
 		quest::say("Welcome to the Church of Underfoot. Please open your soul to the greatness of Brell Serilis. May he guide you in all your future endeavors. And may your soules long for the [" . quest::saylink("soil of underfoot") . "].");
 	} 
 	elsif ($text=~/soil of underfoot/i) {
-		#:: Check faction--must be amiable or better to get quest.
+		#:: Match if faction is Indifferent or worse
 		if ($faction >=5 ) {
 			quest::say("The Clerics of Underfoot have yet to see your faith directed towards our wills. Perhaps you should assist Master Gunlok Jure in the crusade against the undead.");
 		} else {
@@ -46,29 +41,27 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM { 
 	#:: Match a 18765 - Dirt Covered Letter
-	if (plugin::check_handin(\%itemcount, 18765 => 1)) {
+	if (plugin::takeItems(18765 => 1)) {
 		quest::say("Welcome to the Underfoot Cathedral. I am High Priestess Ghalea. Here is your guild tunic. Now. let's get you started helping us spread the will of Brell. Once you are ready to begin your training plese make sure that you see Haldorak, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [" . quest::saylink("trades") . "] you will have available to you.");
 		#:: Give a 13514 - Dusty Tunic*
 		quest::summonitem(13514);
-		#:: Set Faction
-		quest::faction(44,100); 	# Clerics of Underfoot
-		quest::faction(169,100); 	# Kazon Stormhammer
-		quest::faction(219,75); 	# Miners Guild 249
+		#:: Grant a small amount of experience
 		quest::exp(100);
+		#:: Set factions
+		quest::faction(44,100); 	#:: + Clerics of Underfoot
+		quest::faction(169,100); 	#:: + Kazon Stormhammer
+		quest::faction(219,75); 	#:: + Miners Guild 249
 	}
-	#:: Match for 12106 - Fairy Dust
-	elsif (plugin::check_handin(\%itemcount, 12106 => 4) && ($faction < 5)) {
+	#:: Match if faction is better than Indifferent and four 12106 - Fairy Dust 
+	elsif ((plugin::takeItems(12106 => 4)) && ($faction < 5)) {
 		quest::say("May the mighty power of Brell saturate this soil with his divinity.  Here you are, my noble friend.  You may have a pouch of the soil of Underfoot.");
 		#:: Give a 12282 - Soil of Underfoot
 		quest::summonitem(12282);
-		#:: Set Faction
-		quest::faction(44,2);		# Clerics of Underfoot
-		quest::faction(169,2); 		# Kazon Stormhammer
-		quest::faction(219,2); 		# Miners Guild 249
+		#:: Set factions
+		quest::faction(44,2);		#:: + Clerics of Underfoot
+		quest::faction(169,2); 		#:: + Kazon Stormhammer
+		quest::faction(219,2); 		#:: + Miners Guild 249
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
-
-#END of FILE Zone:kaladimb  ID:67024 -- Priestess_Ghalea
-#edited by SS
