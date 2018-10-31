65b82650e0328f865629ba44878df7a8c74492d7
TurmoilToad <TurmoilToad@gmail.com>
2018-10-31 07:30:08 -0400

    Update Bndainy_Everhot.pl

--- kaladimb/Bndainy_Everhot.pl
+++ kaladimb/Bndainy_Everhot.pl
@@ -1,6 +1,5 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome to Everhot Forge! I supply weapon molds for skilled blacksmiths. Care to view my wares?"); }
+	if ($text=~/Hail/i) {
+		quest::say("Welcome to Everhot Forge! I supply weapon molds for skilled blacksmiths. Care to view my wares?");
+	}
 }
-#END of FILE Zone:kaladimb  ID:67045 -- Bndainy_Everhot 
-
