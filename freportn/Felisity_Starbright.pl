4acf4d3fbe9e5f81bbc731917bc5cdd57a1eede3
FVFippy <44533609+FVFippy@users.noreply.github.com>
2018-11-01 19:41:51 -0400

    Update Felisity_Starbright.pl
    Added missing turn in based on Allakazam data for Going Postal (Kelethin to Freeport)

--- freportn/Felisity_Starbright.pl
+++ freportn/Felisity_Starbright.pl
@@ -77,6 +77,24 @@ sub EVENT_ITEM {
 		quest::faction(273,-30); 	#:: - Ring of Scale
 		quest::faction(207,-30); 	#:: - Mayong Mistmoore
 	}
+	#:: Turn in for 18167 - Pouch of Mail (Freeport)
+	if (plugin::takeItems(18167 => 1 )) {
+		quest::say("'Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring");
+		#:: Give a small amount of xp
+		quest::exp(100);
+		#:: Ding!
+		quest::ding();
+		#:: Create a hash for storing cash - 1100 to 15000cp
+		my %cash = plugin::RandomCash(1100,1500);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set faction
+		quest::faction(192,100); 	#:: + League of Antonican Bards
+		quest::faction(184,10); 	#:: + Knights of Truth
+		quest::faction(135,10); 	#:: + Guards of Qeynos
+		quest::faction(273,-30); 	#:: - Ring of Scale
+		quest::faction(207,-30); 	#:: - Mayong Mistmoore
+	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
