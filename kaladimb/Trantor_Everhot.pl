4cea50eb24b560731d7e9c021a885e1260bd8dc4
FVFippy <44533609+FVFippy@users.noreply.github.com>
2018-11-06 12:39:28 -0500

    Update Trantor_Everhot.pl
    Updated the dialog with the content from Allakazam and added saylinks

--- kaladimb/Trantor_Everhot.pl
+++ kaladimb/Trantor_Everhot.pl
@@ -9,10 +9,10 @@ sub EVENT_SPAWN {
 
 sub EVENT_SAY {
   if($text=~/hail/i) {
-    quest::say("Yes, yes!! I am a very busy man. I've got blueprints to work on, [dwarven wire] to perfect and formulae to calculate. In between those tasks, I have to find time to head down to Irontoe's and down a few Tumpy Tonics.");
+    quest::say("Yes, yes!! I am a very busy man. I've got blueprints to work on,  [" . quest::saylink("dwarven wire") . "] to perfect and formulae to calculate. In between those tasks, I have to find time to head down to Irontoe's and down a few Tumpy Tonics.");
   }
   if($text=~/dwarven wire/i) {
-    quest::say("Oh!! Interested in my dwarven wire, are you? Many are. I have no problem letting you use a spool, but I have to warn you that I have not perfected the strength of it yet. I will make you a trade. A couple of [Tumpy Tonics] for a spool.");
+    quest::say("Oh!! Interested in my dwarven wire, are you? Many are. I have no problem letting you use a spool, but I have to warn you that I have not perfected the strength of it yet. I will make you a trade. A couple of [" . quest::saylink("Tumpy Tonics") . "] for a spool.");
   }
   if($text=~/tumpy tonics/i) {
     quest::say("Aaah!! I love Tumpy Tonics. They were created by Tumpy Irontoe. He was once a member of the famed Irontoe Brigade. Since they disbanded and left for parts unknown he decided to stay in Kaladim and open Irontoe's, the finest well in Faydwer.");
@@ -21,7 +21,7 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
   if(plugin::check_handin(\%itemcount, 12114 => 2)) { #Tumpy Tonic (Only 2)
-    quest::say("Thank you, that hit the spot!! Here is the spool, as promised."); #Real text still needed
+    quest::say("Great!! I did not have the time to get down to Irontoe's today. Here. Like I said. A spool of dwarven wire. Now go away. I ain't sharing these tonics"); 
     quest::summonitem(12111); #Dwarven Wire
     quest::exp(1000);
     quest::faction(314,5); #Storm Guard
@@ -39,7 +39,7 @@ sub EVENT_ITEM {
       $ItemCount = 1;
     }
     else {
-      quest::say("Thank you, that hit the spot!! Here is the spool, as promised."); #Real text still needed
+      quest::say("Great!! I did not have the time to get down to Irontoe's today. Here. Like I said. A spool of dwarven wire. Now go away. I ain't sharing these tonics"); 
       quest::summonitem(12111); #Dwarven Wire
       quest::exp(1000);
       quest::faction(314,5); #Storm Guard
