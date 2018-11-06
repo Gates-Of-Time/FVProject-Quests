49b3037e7a9b8dbfcb49c23205b5444fe15112a3
TurmoilToad <TurmoilToad@gmail.com>
2018-11-05 21:09:23 -0500

    Update Dunony_Chestire.pl

--- kaladimb/Dunony_Chestire.pl
+++ kaladimb/Dunony_Chestire.pl
@@ -8,13 +8,14 @@ sub EVENT_SAY {
 	elsif ($text=~/skills/i) {
 		#:: Set available (non-trade, non-casting specialization) skills to maximum for race/class at current level
 		foreach my $skill ( 0 .. 42, 48 .. 54, 70 .. 74 ) {
-		next unless $client->CanHaveSkill($skill);
-		#:: Create a scalar variable to store each skill's maximum skill level at the player's current level
-		my $maxSkill = $client->MaxSkill($skill, $client->GetClass(), $ulevel);
-		#:: Check that the player's skill does not already exceed the maximum skill based on level
-		next unless $maxSkill > $client->GetRawSkill($skill);
-		#:: Set the skill to the maximum
-		$client->SetSkill($skill, $maxSkill);
+			next unless $client->CanHaveSkill($skill);
+			#:: Create a scalar variable to store each skill's maximum skill level at the player's current level
+			my $maxSkill = $client->MaxSkill($skill, $client->GetClass(), $ulevel);
+			#:: Check that the player's skill does not already exceed the maximum skill based on level
+			next unless $maxSkill > $client->GetRawSkill($skill);
+			#:: Set the skill to the maximum
+			$client->SetSkill($skill, $maxSkill);
+		}
 		#:: Send a message that only the client can see, in yellow (15) text
 		$client->Message(15, "You look like a more capable $class already! Go out and test your new skills!");
 		#:: Play a Ding! sound
