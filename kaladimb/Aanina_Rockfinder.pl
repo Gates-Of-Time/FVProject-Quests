24acb5e8de23939d608738a46d784cc81e0e9b10
TurmoilToad <TurmoilToad@gmail.com>
2018-11-08 07:33:25 -0500

    Update Aanina_Rockfinder.pl

--- kaladimb/Aanina_Rockfinder.pl
+++ kaladimb/Aanina_Rockfinder.pl
@@ -1,6 +1,10 @@
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 20879 => 1)) {
-    quest::say("Ah, nice of you to come and pick this up for Ganelorn! I know he is a busy fellow. I certainly hope that his girlfriend will like this gift.");
-    quest::summonitem(20880);
-  }
+	#:: Match a 20879 - Note of Credit
+	if (plugin::takeItems(20879 => 1)) {
+		quest::say("Ah, nice of you to come and pick this up for Ganelorn! I know he is a busy fellow. I certainly hope that his girlfriend will like this gift.");
+		#:: Give a 20880 - Gift
+		quest::summonitem(20880);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
