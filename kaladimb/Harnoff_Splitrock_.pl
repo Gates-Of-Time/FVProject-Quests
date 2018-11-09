edcae5bec67a5846e5752cf0b7b68d1382ea5957
TurmoilToad <TurmoilToad@gmail.com>
2018-11-09 13:23:35 -0500

    Create Harnoff_Splitrock_.pl

---
+++ kaladimb/Harnoff_Splitrock_.pl
@@ -0,0 +1,24 @@
+sub EVENT_SPAWN {
+	#:: Create a timer for 6 hours
+	quest::settimer("depop",21600);
+}
+
+sub EVENT_TIMER {
+	if ($timer eq "depop") {
+		#:: Enable spawn point for North Kaladim >> Harnoff_Splitrock (67057)
+		quest::enable_spawn2(67057);
+		#:: Depop
+		quest::depop();
+	}
+}
+
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetins to ya friend Dwarf! I've got some of the finest quality steel ores in all the lands!");
+	}
+}
+
+sub EVENT_ITEM {
+  	#:: Return unused items
+	plugin::returnUnusedItems();
+}
