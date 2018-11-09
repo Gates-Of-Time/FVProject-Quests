535479b289b36a0aabac6a6067a6d64371451ac6
TurmoilToad <TurmoilToad@gmail.com>
2018-11-09 07:49:20 -0500

    Update Guard_Doradek.pl

--- kaladimb/Guard_Doradek.pl
+++ kaladimb/Guard_Doradek.pl
@@ -1,10 +1,5 @@
 sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
-		}
+	if ($combat_state == 1) {
+		quest::say("Time to die $name");
 	}
 }
-	
