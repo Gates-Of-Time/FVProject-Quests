3002a57a684de2e8c514e17d33f2c5dbf46bb23a
TurmoilToad <TurmoilToad@gmail.com>
2018-11-10 16:56:19 -0500

    Update and rename Ambassador_K-Ryn.lua to Ambassador_K-Ryn.pl

---
+++ oggok/Ambassador_K-Ryn.pl
@@ -0,0 +1,20 @@
+sub EVENT_SAY {
+	if ($text =~ /hail/i ) {
+		quest::say("Get your wretched hide away from me! Who knows what vile stench you have been rolling around in?! Do not speak with me unless you have some glimmer of intelligence!");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 18842 - Sealed Letter
+	if (plugin::takeItems(18842)) {
+		quest::say("Another young warrior. I pray you shall not meet the fate of the last twelve. Here then. Take this report to Mistress Seloxia at once. And stay clear of the Froglok lair called Gukk.");
+		#:: Give a 18843 - Sealed Letter
+		quest::summonitem(18843);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(250);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
