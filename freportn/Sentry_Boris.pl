8d6a0359781dc1303e9e6f35d86df084380527ab
TurmoilToad <TurmoilToad@gmail.com>
2018-11-11 14:50:52 -0500

    Update Sentry_Boris.pl

--- freportn/Sentry_Boris.pl
+++ freportn/Sentry_Boris.pl
@@ -10,14 +10,14 @@ sub EVENT_ITEM {
 		quest::say("Ahhh!! That has given me back lost energy. Thank you. Please take this to the next sentry.");
 		#:: Give a small amount of xp
 		quest::exp(100);
-		#:: Give item 12129 - Part of Potion of MArr
+		#:: Give item 12129 - Part of Potion of Marr
 		quest::summonitem(12129);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(258,5); 		#:: + Priests of Marr
-		quest::faction(105,-10); 	#:: - Freeport Militia
-		quest::faction(184,5); 		#:: + Knights of Truth	
+		#:: Set factions
+		quest::faction(258,5);		#:: + Priests of Marr
+		quest::faction(105,-10);	#:: - Freeport Militia
+		quest::faction(184,5);		#:: + Knights of Truth	
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
