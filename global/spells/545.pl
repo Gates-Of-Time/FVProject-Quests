b85d9e941f55f7d39bb1b8c5126d7bc03df84c98
TurmoilToad <TurmoilToad@gmail.com>
2018-11-21 09:05:24 -0500

    Update 545.pl

--- global/spells/545.pl
+++ global/spells/545.pl
@@ -2,7 +2,7 @@ sub EVENT_SPELL_EFFECT_CLIENT
 {
 	if(($client->GetClientVersionBit() & 4294967292)!= 0)
 	{
-		quest::MovePCInstance(25, 1, -715, -57, 42, 0);
+		quest::MovePCInstance(25, 1, -772, 432, 35, 0);
 	}
 	else
 	{
