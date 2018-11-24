470580666e0724ecda9b5f917f7397416cdd15a9
FVFippy <44533609+FVFippy@users.noreply.github.com>
2018-11-24 10:27:20 -0500

    Update Glorin_Binfurr.pl
    Had to add an apostrophe to the say dialog or would not return responce to desired tell.

--- butcher/Glorin_Binfurr.pl
+++ butcher/Glorin_Binfurr.pl
@@ -4,7 +4,7 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Pleasure to meet you.  Keep your eye out for the ship.  You don't want to miss it.");
 	}
-	if ($text=~/mudtoes/i) {
+	if ($text=~/mudtoe's/i) {
 		quest::say("Mudtoes!! I have heard tales of that foul clan of ogres. A few passengers aboard the ships have claimed to have seen one of them, off and on, amongst the pirates in the Ocean of Tears!!");
 	}
 }
