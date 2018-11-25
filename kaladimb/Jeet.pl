b81d5579368c853efe661a3c6092a1ce221bd764
TurmoilToad <TurmoilToad@gmail.com>
2018-11-25 07:46:37 -0500

    Update Jeet.pl

--- kaladimb/Jeet.pl
+++ kaladimb/Jeet.pl
@@ -14,7 +14,7 @@ sub EVENT_SAY  {
 			quest::say("Well, then, get off yer rump and give us a hand! If you don't have a mining pick, then get down there and use your fingernails!! If you're new and you want to earn a pick, you can [" . quest::saylink("volunteer to exterminate the rats") . "] that have been infesting the mines!!");
 		}
 		#:: Match if faction is Indifferent 
-		elsif ($faction = 5) {
+		elsif ($faction == 5) {
 			quest::say("Don't take this personally, but I can't quite trust you with such matters. Maybe a few less Butcherblock bandits would prove your worth.");
 		}
 		else {
@@ -27,7 +27,7 @@ sub EVENT_SAY  {
 			quest::say("Well, someone has to do the dirty work around here. Let it be you new miners. Go patrol the mines and if you see any rats, bash them good!! Return to me with four rat pelts and I will give you some armor as payment. If you want a 628 mining cap, yer gonna have to smash that infernal [" . quest::saylink("cleaner") . "]!!");
 		}
 		#:: Match if faction is Indifferent 
-		elsif ($faction = 5) {
+		elsif ($faction == 5) {
 			quest::say("Don't take this personally, but I can't quite trust you with such matters. Maybe a few less Butcherblock bandits would prove your worth.");
 		}
 		else {
@@ -40,7 +40,7 @@ sub EVENT_SAY  {
 			quest::say("Well, as you most likely know, we are a non-profit organization, at least here in Kaladim. We need to make our profit elsewhere and that means a little bit of thieving. Presently we need loyal members to [" . quest::saylink("collect the gnome profit") . "].");
 		}
 		#:: Match if faction is Indifferent
-		elsif ($faction = 5) {
+		elsif ($faction == 5) {
 			quest::say("Don't take this personally, but I can't quite trust you with such matters. Maybe a few less Butcherblock bandits would prove your worth.");
 		}
 		else {
@@ -55,7 +55,7 @@ sub EVENT_SAY  {
 			quest::summonitem(12164); # Scrubber Key
 		}
 		#:: Match if faction is Indifferent
-		elsif ($faction = 5) {
+		elsif ($faction == 5) {
 			quest::say("Don't take this personally, but I can't quite trust you with such matters. Maybe a few less Butcherblock bandits would prove your worth.");
 		}
 		else {
@@ -68,7 +68,7 @@ sub EVENT_SAY  {
 			quest::say("Darn that blasted clockwork cleaner!! No one in Kaladim even knows what it is! They all think it's some kinda rat in armor!! Rat paladins?!! The name fits. We miners have seen many like it in the gnome city during heists, err.. visits. We have to get rid of this one in Kaladim!! If you smash it and get its scrap metal, I am authorized to give you a mining cap, if you are a member of Miner's Guild 628.");
 		}
 		#:: Match if faction is Indifferent
-		elsif ($faction = 5) {
+		elsif ($faction == 5) {
 			quest::say("Don't take this personally, but I can't quite trust you with such matters. Maybe a few less Butcherblock bandits would prove your worth.");
 		}
 		else {
@@ -148,7 +148,7 @@ sub EVENT_ITEM {
 	}
 	#:: plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
 
 sub EVENT_SIGNAL {
