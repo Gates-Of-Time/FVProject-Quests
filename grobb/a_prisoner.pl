8afe9f673aeb647ea070b6146b7f7a8125e9839d
TurmoilToad <TurmoilToad@gmail.com>
2018-12-02 12:36:46 -0500

    Update and rename a_prisoner.lua to a_prisoner.pl

---
+++ grobb/a_prisoner.pl
@@ -0,0 +1,33 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Rrrribit.. Please, let me go!! I don't want to be eaten!!");
+	}
+	if ($text=~/glib sent me/i) {
+		quest::say("Liessss. ...Groakk.. I do not believe you!! Bring me proof. Bring me the necklace of a basher!! ..Groakk..");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 13311 - Intestine Necklace
+	if (plugin::takeItems(13311 => 1)) {
+		quest::say("Groak.. So you are a friend to the froglok. ..Grooakk.. I am soon to die. My precious legs are a delicacy here. Before I go I must contact my brother Grikk. He is a froglok forager in Innothule. Give him this vial. He will know what it means");
+		#:: Give a 13375 - Empty Vial
+		quest::summonitem(13375);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(500);
+	}
+	#:: Match a 13376 - Ochre Liquid
+	elsif (plugin::takeItems(13376
+		quest::say("Grooak.. You have done much to help me. This will come in handy soon. Thank you. Here is Marda's information. Take it to her. They must know. Farewell.");
+		#:: Give a random reward: 18884 - Tattered Note or 18885 - Tattered Cloth Note
+		quest::summonitem(quest::ChooseRandom(18884,18885));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(500);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
