f54fe499991d0c376bf2ee412ab9ab0d87d102ce
TurmoilToad <TurmoilToad@gmail.com>
2018-12-08 11:30:40 -0500

    Update Crunga.pl

--- oggok/Crunga.pl
+++ oggok/Crunga.pl
@@ -1,6 +1,4 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Hullo. Buy sum of my stuff."); }
+	if ($text=~/Hail/i) {
+		quest::say("Hullo. Buy sum of my stuff."); }
 }
-#END of FILE Zone:oggok  ID:49062 -- Crunga 
-
