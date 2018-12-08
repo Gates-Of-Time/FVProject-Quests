d3b084961adadea7153409e887d4c3a9f926d2ec
TurmoilToad <TurmoilToad@gmail.com>
2018-12-08 11:31:17 -0500

    Update Erung.pl

--- oggok/Erung.pl
+++ oggok/Erung.pl
@@ -1,6 +1,5 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Huh? Oh hi $name. Youse want to buy sumthin?"); }
+	if ($text=~/Hail/i) {
+		quest::say("Huh? Oh hi $name. Youse want to buy sumthin?"); 
+	}
 }
-#END of FILE Zone:oggok  ID:49063 -- Erung 
-
