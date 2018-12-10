580a10eb1486aa45fee3c3ff99b755b9d449696a
TurmoilToad <TurmoilToad@gmail.com>
2018-12-10 09:39:48 -0500

    Update and rename Deputy_Lowmot.lua to Deputy_Lowmot.pl

---
+++ rivervale/Deputy_Lowmot.pl
@@ -0,0 +1,27 @@
+sub EVENT_SAY {
+	if ($text=~/nice hat/i) {
+		quest::say("Thank you, It is worn with pride and is the symbol of the Rivervale Militia.");
+	}
+}
+
+sub EVENT_ITEM {
+	if (plugin::takeItems(13959 => 1) {
+		quest::say("Oh good!  Hey.  You are not Jillin..  Helping out Blinza huh?  She is quite a woman..  Yes indeed.  Quite a woman..  ah..  Oh sorry.  Here you go.  Thanks again.  Mayor Gubbin will be pleased.");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(1000);
+		#:: Create a hash for storing cash - 1 to 300cp
+		my %cash = plugin::RandomCash(1,300);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
+		quest::faction(133, 4);		#:: + Guardians of the Vale
+		quest::faction(208, 4);		#:: + Mayor Gubbin
+		quest::faction(316, 4);		#:: + Storm Reapers
+		quest::faction(218, 4);		#:: + Merchants of Rivervale
+		quest::faction(88, -8);		#:: + Dreadguard Outer
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
