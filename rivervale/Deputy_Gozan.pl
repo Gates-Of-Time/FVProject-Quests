35b6fa901d930d897dcfb955cd5859e552397a90
TurmoilToad <TurmoilToad@gmail.com>
2018-12-10 09:23:08 -0500

    Update and rename Deputy_Gozan.lua to Deputy_Gozan.pl

---
+++ rivervale/Deputy_Gozan.pl
@@ -0,0 +1,10 @@
+sub EVENT_SAY {
+	if ($text=~/nice hat/i) {
+		quest::say("Thank you, It is worn with pride and is the symbol of the Rivervale Militia.");
+	}
+}
+
+sub EVENT_ITEM {	
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
