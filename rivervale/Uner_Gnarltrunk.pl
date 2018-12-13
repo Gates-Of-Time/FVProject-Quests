9851b0dbda549df99c4dfb18d2809ff38dfcfac5
TurmoilToad <TurmoilToad@gmail.com>
2018-12-13 14:37:29 -0500

    Update and rename Uner_Gnarltrunk.lua to Uner_Gnarltrunk.pl

---
+++ rivervale/Uner_Gnarltrunk.pl
@@ -0,0 +1,30 @@
+sub EVENT_SAY { 
+	if ($text=~/hail/i) {
+		quest::say(("Hello there, $name. Please watch where you are stepping when you're out in the field.  Nothing is more frustrating than having some fine jumjum ruined by our own careless feet. That reminds me, [" . quest::saylink("Deputy Tagil") . "] still owes us for that jumjum he stomped on the other day!");
+	}
+	elsif ($text=~/deputy tagil/i) {
+		quest::say("Deputy Tagil is a fine young halfling who serves the vale well. But the other day, chasing that dirty Nillipuss, he trampled some fresh Jumjum.  He promised to make amends but it must have slipped his mind.  Please take this note to him as a friendly reminder.");
+		#:: Give a 18013 - Note
+		quest::summonitem(18013); 
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 13240 - Deputy Tagil's Payment
+	if (plugin::takeItems(13240 => 1)) {
+		quest::say("I knew that Deputy Tagil had simply forgotten. He really is a good young halfling. Here, take this as a small payment for your time.");
+		#:: Give a random reward: 13977 - Carrot, 13100 - Fishing Pole, 14014 - Potion of Wisdom, 13083 - Pine Needles
+		quest::summonitem(quest::ChooseRandom(13977,13100,14014,13083));
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(100);
+		#:: Set factions
+		quest::faction(316, 1);		#:: + Storm Reapers
+		quest::faction(208, 1);		#:: + Mayor Gubbin
+		quest::faction(218, 1);		#:: + Merchants of Rivervale
+		quest::faction(347, -1);	#:: - Unkempt Druids
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
