f584748310a935c69158abcbdd93995b36601dd1
FVFippy <44533609+FVFippy@users.noreply.github.com>
2018-12-28 17:18:59 -0500

    Update The_Gobbler.pl
    Added Saylinks

--- neriaka/The_Gobbler.pl
+++ neriaka/The_Gobbler.pl
@@ -1,21 +1,21 @@
 sub EVENT_SAY {
   if ($text=~/hail/i) {
-    quest::say("Meat!! You need meat. Me gots meat!! High elf meat, dwarf meat, human meat, [HEHE meat], [froggy legs]. Me got a lot.");
+    quest::say("Meat!! You need meat. Me gots meat!! High elf meat, dwarf meat, human meat, [" . quest::saylink("HEHE meat") . "], [" . quest::saylink("froggy legs") . "]. Me got a lot.");
   }
-  if ($text=~/hehe meat/i) {
+  if ($text=~/HEHE meat/i) {
     quest::say("HEHE meat? It human, Erudite 'n high elf, mashed together. Mmmm. Very tasty!");
   }
   if ($text=~/froggy legs/i) {
-    quest::say("Froggy legs come from far away. Me get from friend, [Chef Dooga]. It not tasty. Trolls like it. Eat it up much. They eat too much froggy. Me glad [Nerbilik] leave Neriak!! He eat way too much froggy.");
+    quest::say("Froggy legs come from far away. Me get from friend, [" . quest::saylink("Chef Dooga") . "]. It not tasty. Trolls like it. Eat it up much. They eat too much froggy. Me glad [" . quest::saylink("Nerbilik") . "] leave Neriak!! He eat way too much froggy.");
   }
   if ($text=~/chef dooga/i) {
-    quest::say("Friend in Oggok is Chef Dooga. Chef she is. Used to work in Neriak. She beautiful. She supply the Gobbler with [froglok legs]. They very low now. Fat trolls eat too many!! Me need to [pick up froglok legs].");
+    quest::say("Friend in Oggok is Chef Dooga. Chef she is. Used to work in Neriak. She beautiful. She supply the Gobbler with [" . quest::saylink("froglok legs") . "]. They very low now. Fat trolls eat too many!! Me need to [" . quest::saylink("pick up froglok legs") . "].");
   }
   if ($text=~/pick up froglok legs/i) {
     quest::say("That good news. Oggok far away. Me got no time to go. If you go speak to Chef Dooga, say you pick up froglok legs. You bring back and me give you coin, maybe even something good.");
   }
   if ($text=~/nerbilik/i) {
-    quest::say("Big and fat troll Nerbilik is. He leave to fish in tearsy ocean. Me promised to deliver him food or he come bak. The Gobbler needs some dumb person to [deliver grub locker] to Nerbilik.");
+    quest::say("Big and fat troll Nerbilik is. He leave to fish in tearsy ocean. Me promised to deliver him food or he come bak. The Gobbler needs some dumb person to [" . quest::saylink("deliver grub locker") . "] to Nerbilik.");
   }
   if ($text=~/deliver grub locker/i) {
     quest::say("Good. Me wrap order of dwarf pickles in.. HEY!! Me out of special wrapping paper!! Hmm.. Here. Me use dis. Now, <..click.. Locks grub locker> Here. You take to Nerbilik in tearsy ocean and he pay you. No think of opening locker. Only Nerbilik has key.");
