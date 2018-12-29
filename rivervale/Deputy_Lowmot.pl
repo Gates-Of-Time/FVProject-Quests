eb9544b272c50c4e8c05a75ba3102770b40d2bb0
FVFippy <44533609+FVFippy@users.noreply.github.com>
2018-12-29 16:53:45 -0500

    Update Deputy_Lowmot.pl
    Fixed missing paren

--- rivervale/Deputy_Lowmot.pl
+++ rivervale/Deputy_Lowmot.pl
@@ -5,7 +5,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	if (plugin::takeItems(13959 => 1) {
+	if (plugin::takeItems(13959 => 1)) {
 		quest::say("Oh good!  Hey.  You are not Jillin..  Helping out Blinza huh?  She is quite a woman..  Yes indeed.  Quite a woman..  ah..  Oh sorry.  Here you go.  Thanks again.  Mayor Gubbin will be pleased.");
 		#:: Ding!
 		quest::ding();
