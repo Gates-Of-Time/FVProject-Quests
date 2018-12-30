53a27ab90268c6e3ffa129dcd2876794573318fc
TurmoilToad <TurmoilToad@gmail.com>
2018-12-30 13:31:23 -0500

    Update Marshal_Lanena.pl

--- rivervale/Marshal_Lanena.pl
+++ rivervale/Marshal_Lanena.pl
@@ -1,11 +1,11 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-				quest::say("Hello. $name.  I am Lanena Wickystick. marshal of all Vale concerns.  If there are any troubles brewing in our fine town which concern the Guardians of the Vale. please inform me.  You must be a [" . quest::saylink("new deputy") . "] or are you an [" . quest::saylink("outsider") . "]?");
+		quest::say("Hello. $name.  I am Lanena Wickystick. marshal of all Vale concerns.  If there are any troubles brewing in our fine town which concern the Guardians of the Vale, please inform me.  You must be a [" . quest::saylink("new deputy") . "] or are you an [" . quest::saylink("outsider") . "]?");
 	}
 	if ($text=~/new deputy/i) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
-			quest::say("It is good to see such fine stock in the ranks of the Guardians.  Being new. there is much to learn. in battle and in life itself.  If you are not presently obligated. we have need of you here in the hollow.  There seems to be a [" . quest::saylink("small problem") . "].");
+			quest::say("It is good to see such fine stock in the ranks of the Guardians.  Being new, there is much to learn, in battle and in life itself.  If you are not presently obligated. we have need of you here in the hollow.  There seems to be a [" . quest::saylink("small problem") . "].");
 		}
 		else {
 			quest::say("You are not yet in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
@@ -13,11 +13,11 @@ sub EVENT_SAY {
 	}
 	if ($text=~/outsider/i) {
 			quest::say("Well. then!! You should not be in here.  This place is restricted to all outsiders.  You will leave at once!  Thank you and good day.");
-		}
+	}
 	if ($text=~/small problem/i) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
-			quest::say("For months. Fiddy Bobick has petitioned the marshals to assist him with a problem he has.  With the addition of new deputies such as yourself. we can now give him the assistance he requires.  Just go down to Bobick's shop near the lake.  Tell him I sent you.");
+			quest::say("For months. Fiddy Bobick has petitioned the marshals to assist him with a problem he has.  With the addition of new deputies such as yourself, we can now give him the assistance he requires.  Just go down to Bobick's shop near the lake.  Tell him I sent you.");
 		}
 		else {
 			quest::say("You are not yet in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
@@ -35,52 +35,63 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13870 - Piranha Tooth
-	if (plugin::takeItems(13087 => 1 )) {
+	#:: Match a 13870 - Piranha Tooth
+	if (plugin::takeItems(13087 => 1)) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("What was I thinking?!! Piranha are coming downstream and eating our supply of fish! We have never had a problem like this!!  Where are these little beasts coming from?  For now we must collect more. Take this bag. Collect enough teeth to fill the bag. Don't worry, if it takes a while I shall reward you with the [" . quest::saylink("Rantho Rapier") . "].  We will need to examine the teeth.");
-			#:: Give item 17968 - Piranha Bag
+			#:: Give a 17968 - Piranha Bag
 			quest::summonitem(17968);
-			#:: Give a small amount of xp
-			quest::exp(200);
 			#:: Ding!
 			quest::ding();
-			#:: Set faction
+			#:: Grant a small amount of experience
+			quest::exp(200);
+			#:: Create a hash for storing cash - 9 to 11cp
+			my %cash = plugin::RandomCash(9,10);
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Set factions
 			quest::faction(133,10); 	#:: + Guardians of the Vale
 			quest::faction(208,10); 	#:: + Mayor Gubbin
 			quest::faction(316,10); 	#:: + Storm Reapers
 			quest::faction(218,10); 	#:: + Merchants of Rivervale
 			quest::faction(88,-10); 	#:: - Dreadguard Outer
 			quest::givecash(0,1,0,0);	#:: Give a small amount of cash copper - plat
-			}
+		}
 		else {
 			quest::say("You are not yet in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
-			}
+			#:: Give a 13870 - Piranha Tooth
+			quest::summonitem(13087);
+		}
 	}		
-	#:: Turn in for 12155 - Bag of Piranha Teeth
-	if (plugin::takeItems(12155 => 1 )) {
+	#:: Match a 12155 - Bag of Piranha Teeth
+	if (plugin::takeItems(12155 => 1)) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("Fine work. We shall continue to study these and shall determine if we need to seek the source.");
-			#:: Give item 5423 - Rantho Rapier
+			#:: Give a 5423 - Rantho Rapier
 			quest::summonitem(5423);
-			#:: Give a small amount of xp
-			quest::exp(200);
 			#:: Ding!
 			quest::ding();
-			#:: Set faction
+			#:: Grant a small amount of experience
+			quest::exp(200);
+			#:: Create a hash for storing cash - 9 to 11cp
+			my %cash = plugin::RandomCash(9,11);
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Set factions
 			quest::faction(133,10); 	#:: + Guardians of the Vale
 			quest::faction(208,10); 	#:: + Mayor Gubbin
 			quest::faction(316,10); 	#:: + Storm Reapers
 			quest::faction(218,10); 	#:: + Merchants of Rivervale
 			quest::faction(88,-10); 	#:: - Dreadguard Outer
-			quest::givecash(0,1,0,0);	#:: Give a small amount of cash copper - plat
-			}
+		}
 		else {
 			quest::say("You are not yet in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
-			}
+			#:: Give 12155 - Bag of Piranha Teeth
+			quest::summonitem(12155);
+		}
 	}		
-#:: Return unused items
-plugin::returnUnusedItems();
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
