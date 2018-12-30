26f56f4bf1fa9bc5ad1d175dcd859589c1726444
TurmoilToad <TurmoilToad@gmail.com>
2018-12-29 20:32:27 -0500

    Update Daleen_Leafsway.pl

--- rivervale/Daleen_Leafsway.pl
+++ rivervale/Daleen_Leafsway.pl
@@ -4,7 +4,9 @@ sub EVENT_SPAWN {
 	$y = $npc->GetY();
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
 	#:: Create a timer that loops every 10 minutes
-	quest::set_timer("blurt", 600);
+	quest::settimer("blurt", 600);
+	#:: Enable proximity say
+	quest::enable_proximity_say();
 }
 
 sub EVENT_TIMER {
@@ -14,7 +16,7 @@ sub EVENT_TIMER {
 		quest::stoptimer("blurt");
 		quest::say("Oh Dear.. I can't believe I forgot.. He must be [" . quest::saylink("starving") . "]!");
 		#:: Create a timer that loops every 10 minutes
-		quest::set_timer("blurt", 600);
+		quest::settimer("blurt", 600);
 	}
 }
 
@@ -22,6 +24,9 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Greetings. $name!  Welcome to Tagglefoot's Farm.  We grow nothing but the finest vegetables in our field.  We even manage to harvest the mystical jumjum stalk in our fields.  Karana has blessed us indeed, $name.");
 	}
+}
+
+sub EVENT_PROXIMITY_SAY {
 	if ($text=~/starving/i) {
 		quest::say("Deputy Eigon! I forgot! I was supposed to bring him some turnips to eat while he is on patrol! Oh... He asked so nicely, too. I feel bad that I forgot. If only someone would take these [" . quest::saylink("turnips") . "] to the Deputy.");
 	}
@@ -30,7 +35,7 @@ sub EVENT_SAY {
 		#:: Summon 16165 - Sack of Turnips
 		quest::summonitem(16165);
 	}
-} 
+}
 
 sub EVENT_ITEM {
 	#:: Return unused items
