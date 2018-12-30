53e3a561911d5bec3d8544392a50b40794d6c0be
TurmoilToad <TurmoilToad@gmail.com>
2018-12-29 20:15:03 -0500

    Update Rueppy_Kutpurse.pl

--- rivervale/Rueppy_Kutpurse.pl
+++ rivervale/Rueppy_Kutpurse.pl
@@ -9,7 +9,7 @@ sub EVENT_SAY {
 		quest::say("Oh, well we can't get that here. However, you can always [" . quest::saylink("smuggle") . "] it in...");
 	}
 	if ($text=~/smuggle/i) {
-		quest::say("Well, that depends. Do you want a [job]?");
+		quest::say("Well, that depends. Do you want a [" . quest::saylink("job") . "]?");
 	}	
 	if ($text=~/job/i) {
 		quest::say("Should you choose to accept, you have to go to the ruins on the other side of the great wall, find Gunrich, and tell him that Dark Rivers Flow East.");
@@ -20,31 +20,31 @@ sub EVENT_ITEM {
 	#:: Turn in for 13032 - Short Beer
 	if (plugin::takeItems(13032 => 1 )) {
 		quest::say("Mmm. It's good, but not as good as [" . quest::saylink("Blackburrow Stout") . "].");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(100);
 		#:: Give a small amount of cash copper - plat
 		quest::givecash(5,0,0,0);	
 	}
 	#:: Turn in for 13131 - Case of Blackburrow Stout
-	if (plugin::takeItems(13131 => 1 )) {
+	elsif (plugin::takeItems(13131 => 1 )) {
 		quest::say("Heh heh! You did it! I thought the deputies would get you for sure! I mean... You did it! Heh! Here buy yourself a drink on me.");
-		#:: Give a decent amount of xp
-		quest::exp(5000);
 		#:: Ding!
 		quest::ding();
+		#:: Grant a large amount of xp
+		quest::exp(5000);
 		#:: Create a hash for storing cash - 1 to 20cp
 		my %cash = plugin::RandomCash(1,20);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Faction
-		quest::faction(77,10); 	#:: + Deeppockets
-		quest::faction(33,1); 	#:: + Circle of Unseen Hands
-		quest::faction(218,-1); #:: - Merchants of Rivervale
-		quest::faction(48,1); 	#:: + Coalition of Tradefolk Underground
-		quest::faction(31,1); 	#:: + Carson McCabe	
-	}	
+		#:: Set faction
+		quest::faction(77,10); 		#:: + Deeppockets
+		quest::faction(33,1); 		#:: + Circle of Unseen Hands
+		quest::faction(218,-1); 	#:: - Merchants of Rivervale
+		quest::faction(48,1); 		#:: + Coalition of Tradefolk Underground
+		quest::faction(31,1); 		#:: + Carson McCabe	
+	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
