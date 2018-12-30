de244676748c4a2b927dfa50279234b8a82783b2
TurmoilToad <TurmoilToad@gmail.com>
2018-12-30 11:43:07 -0500

    Update Silna_Songsmith.pl

--- rivervale/Silna_Songsmith.pl
+++ rivervale/Silna_Songsmith.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hail, $Name - Are you [" . quest::saylink("interested") . "] in helping the League of Antonican Bards by delivering some [" . quest::saylink("mail") . "]? ");
+		quest::say("Hail, $name - Are you [" . quest::saylink("interested") . "] in helping the League of Antonican Bards by delivering some [" . quest::saylink("mail") . "]? ");
 	}
 	if ($text=~/mail/i) {
 		quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers. We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices. Are you [" . quest::saylink("interested") . "]?");
