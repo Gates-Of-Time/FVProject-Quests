0786a03e2520b4f9696ff3c10e5a07cfe45253a7
TurmoilToad <TurmoilToad@gmail.com>
2018-12-29 20:17:34 -0500

    Update Felisity_Starbright.pl

--- freportn/Felisity_Starbright.pl
+++ freportn/Felisity_Starbright.pl
@@ -8,10 +8,10 @@ sub EVENT_ITEM {
 	#:: Turn in for 18158 - A Bardic Letter (Freeport)
 	if (plugin::takeItems(18158 => 1 )) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
-		#:: Give a small amount of xp
-		quest::exp(80);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(80);
 		#:: Create a hash for storing cash - 600 to 1000cp
 		my %cash = plugin::RandomCash(600,1000);
 		#:: Grant a random cash reward
@@ -26,15 +26,15 @@ sub EVENT_ITEM {
 	#:: Turn in for 18157 - A Bardic Letter (Freeport)
 	if (plugin::takeItems(18157 => 1 )) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
-		#:: Give a small amount of xp
-		quest::exp(80);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(80);
 		#:: Create a hash for storing cash - 600 to 1000cp
 		my %cash = plugin::RandomCash(600,1000);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
+		#:: Set factions
 		quest::faction(192,100); 	#:: + League of Antonican Bards
 		quest::faction(184,10); 	#:: + Knights of Truth
 		quest::faction(135,10); 	#:: + Guards of Qeynos
@@ -44,10 +44,10 @@ sub EVENT_ITEM {
 	#:: Turn in for 18159 - A Bardic Letter (Freeport)
 	if (plugin::takeItems(18159 => 1 )) {
 		quest::say("'Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring.'");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(100);
 		#:: Create a hash for storing cash - 800 to 1200cp
 		my %cash = plugin::RandomCash(800,1200);
 		#:: Grant a random cash reward
@@ -62,15 +62,15 @@ sub EVENT_ITEM {
 	#:: Turn in for 18155 - A Bardic Letter (Freeport)
 	if (plugin::takeItems(18155 => 1 )) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(100);
 		#:: Create a hash for storing cash - 1100 to 15000cp
 		my %cash = plugin::RandomCash(1100,1500);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
+		#:: Set factions
 		quest::faction(192,100); 	#:: + League of Antonican Bards
 		quest::faction(184,10); 	#:: + Knights of Truth
 		quest::faction(135,10); 	#:: + Guards of Qeynos
@@ -80,15 +80,15 @@ sub EVENT_ITEM {
 	#:: Turn in for 18166 - Pouch of Mail (Freeport)
 	if (plugin::takeItems(18166 => 1 )) {
 		quest::say("'Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
+		#:: Give a small amount of xp
+		quest::exp(100);
 		#:: Create a hash for storing cash - 1100 to 15000cp
 		my %cash = plugin::RandomCash(1100,1500);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
+		#:: Set factions
 		quest::faction(192,100); 	#:: + League of Antonican Bards
 		quest::faction(184,10); 	#:: + Knights of Truth
 		quest::faction(135,10); 	#:: + Guards of Qeynos
