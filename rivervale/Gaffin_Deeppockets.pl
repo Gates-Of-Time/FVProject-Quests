87dd2a1b0aec2e89e954419e5482b78a5668f3fe
TurmoilToad <TurmoilToad@gmail.com>
2018-12-30 20:11:22 -0500

    Update Gaffin_Deeppockets.pl

--- rivervale/Gaffin_Deeppockets.pl
+++ rivervale/Gaffin_Deeppockets.pl
@@ -6,11 +6,7 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
 	#:: Catch the "Lenny" timer
 	if ($timer eq "Lenny") {
-	#:: Stop the timer "Lenny"
-	quest::stoptimer("Lenny");
-	#:: Signal 19062 - Lenny in Rivervale
-	quest::signalwith(19062,1,0);
-	#:: Create a timer that loops every 15 minutes
-	quest::settimer("Lenny", 950);
+		#:: Signal 19062 - Lenny in Rivervale
+		quest::signalwith(19062,1,0);
 	}
 }
