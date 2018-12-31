f3f608d624a756f37077263c46fa4bd76850f1ba
TurmoilToad <TurmoilToad@gmail.com>
2018-12-30 20:15:04 -0500

    Update Charlotte_Diggs.pl

--- rivervale/Charlotte_Diggs.pl
+++ rivervale/Charlotte_Diggs.pl
@@ -7,6 +7,12 @@ sub EVENT_SPAWN {
 
 sub EVENT_TIMER {
 	if ($timer eq "pick") {
+		#:: Stop the timer "pick"
+		quest::stoptimer("pick");
 		quest::emote("picks up something from the ground.");
+		#:: Create a scalar variable to store a random number from 1 - 720
+		my $random = int(rand(720));
+		#:: Set a timer "pick" that loops every 1 - 720 seconds at random
+		quest::settimer("pick",$random);	
 	}
 }
