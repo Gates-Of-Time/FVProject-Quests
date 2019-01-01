48c539de74d4b1f07bb8b7753d87c53149e690a4
TurmoilToad <TurmoilToad@gmail.com>
2019-01-01 17:34:39 -0500

    Update Zapf.pl

--- global/Zapf.pl
+++ global/Zapf.pl
@@ -1545,18 +1545,22 @@ sub EVENT_ITEM {
 	#:: Match 1 copper
 	if (plugin::takeCoin(1, 0, 0, 0)) {
 		$change_amount = -500;
+		quest::say("Now your faction will be adjusted by $change_amount points.");
 	}
 	#:: Match 1 silver
 	if (plugin::takeCoin(0, 1, 0, 0)) {
 		$change_amount = -100;
+		quest::say("Now your faction will be adjusted by $change_amount points.");
 	}
 	#:: Match 1 gold
 	if (plugin::takeCoin(0, 0, 1, 0)) {
 		$change_amount = 100;
+		quest::say("Now your faction will be adjusted by $change_amount points.");
 	}
 	#:: Match 1 platinum
 	if (plugin::takeCoin(0, 0, 0, 1)) {
 		$change_amount = 500;
+		quest::say("Now your faction will be adjusted by $change_amount points.");
 	}
 }
 
