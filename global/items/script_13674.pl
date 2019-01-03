00cb92b196fa177dcbf4e6096898a5f987e2cc25
TurmoilToad <TurmoilToad@gmail.com>
2019-01-03 12:33:08 -0500

    Update script_13674.pl

--- global/items/script_13674.pl
+++ global/items/script_13674.pl
@@ -1,18 +1,10 @@
 sub EVENT_ITEM_CLICK_CAST {
     if ($itemid == 11668) {
-        quest::gmsay("you clicked your guantlets!");
         $client->DeleteItemInInventory($slotid);
-        quest::gmsay("your item is deleted.");
-        quest::castspell(1823, $userid);
-        quest::gmsay("you cast a spell");
         quest::summonitem(11669);
     }
     elsif ($itemid == 11669) {
-        quest::gmsay("you clicked your hammer!");
         $client->DeleteItemInInventory($slotid);
-        quest::gmsay("your item is deleted.");
-        quest::castspell(1824, $userid);
-        quest::gmsay("you cast a spell");
         quest::summonitem(11668);
     }
 }
