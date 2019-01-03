d0ac61e2d2b8ffc466c294d941579b82de9fdb33
TurmoilToad <TurmoilToad@gmail.com>
2019-01-03 12:21:02 -0500

    Update script_13674.pl

--- global/items/script_13674.pl
+++ global/items/script_13674.pl
@@ -1,12 +1,12 @@
 sub EVENT_ITEM_CLICK {
     if ($itemid == 11668) {
         $client->DeleteItemInInventory($slotid);
-        quest::castspell(1824, $userid);
+        quest::castspell(1823, $userid);
         quest::summonitem(11669);
     }
     elsif ($itemid == 11669) {
         $client->DeleteItemInInventory($slotid);
-        quest::castspell(1823, $userid);
+        quest::castspell(1824, $userid);
         quest::summonitem(11668);
     }
 }
