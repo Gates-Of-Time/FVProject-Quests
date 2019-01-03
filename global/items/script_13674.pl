e8f85d65ac55ab7cea4f5cec829ec92670cd7c41
TurmoilToad <TurmoilToad@gmail.com>
2019-01-03 12:41:51 -0500

    Update script_13674.pl

--- global/items/script_13674.pl
+++ global/items/script_13674.pl
@@ -1,10 +1,12 @@
 sub EVENT_ITEM_CLICK_CAST {
 	if ($itemid == 11668) {
 		$client->NukeItem(11668);
+		$client->CastSpell(1823, 0, 10, 0, 0);
 		quest::summonitem(11669);
 	}
 	elsif ($itemid == 11669) {
 		$client->NukeItem(11668);
+		$client->CastSpell(1824, 0, 10, 0, 0);
 		quest::summonitem(11668);
 	}
 }
