649801568a409ab02d31a381f05db18674ac8856
TurmoilToad <TurmoilToad@gmail.com>
2019-01-03 12:18:13 -0500

    Update script_13674.pl

--- global/items/script_13674.pl
+++ global/items/script_13674.pl
@@ -1,21 +1,12 @@
-# Thank you to Skomag over at EQTitan for this.
-
-# handles Vibrating Gauntlets/Hammer of Infuse
-# clicking the items transmutes back and forth
-# script file is used to bypass the lore check error
-# 
-# item 11668 Vibrating Gauntlets of Infuse
-# item 11669 Vibrating Hammer of Infuse
-# spell 1823 Transmute Gauntlets
-# spell 1824 Transmute Hammer
-
-sub EVENT_ITEM_CLICK_CAST {
-        my %transmute = ();
-        $transmute[11668] = 1824;
-        $transmute[11669] = 1823;
-
-        if($itemid && $transmute[$itemid]) {
-                $client->NukeItem($itemid);
-                $client->CastSpell($transmute[$itemid], 0, 10, 0, 0);
-        }
+sub EVENT_ITEM_CLICK {
+    if ($itemid == 11668) {
+        $client->DeleteItemInInventory($slotid);
+        quest::castspell(1823, $userid);
+        quest::summonitem(11669);
+    }
+    elsif ($itemid == 11669) {
+        $client->DeleteItemInInventory($slotid);
+        quest::castspell(1824, $userid);
+        quest::summonitem(11668);
+    }
 }
