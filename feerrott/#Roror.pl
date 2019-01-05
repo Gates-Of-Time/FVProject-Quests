31d8223186099799d3be9ea6c0314f5fe8ca420c
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-01-05 16:21:29 -0500

    Update #Roror.pl
    Added missing quest dialog for Shakey's Stuffing Quest

--- feerrott/#Roror.pl
+++ feerrott/#Roror.pl
@@ -14,17 +14,17 @@ sub EVENT_COMBAT {
 
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Do you [" . quest::saylink("know fear") . "]?");
+		quest::say("Greeetingsssssssssss. What brings sssssuch asssss you to the heart of the Feerrott? Do you know [" . quest::saylink("know fear") . "]?");
 	}
 	if ($text=~/know fear/i) {
-		quest::say("That's great!");
+		quest::say("Yesssss. I can tassssste the fear in your pitiful heart. I am Roror, High Priessst of Cazic-Thule! I sssseek to sssssspread terror acrossssss all of Norrath! I ssssshall let you live thisss day so you can sssshare the fear that liessss within you.");
 	}
 	if ($text=~/know no fear/i) {
 		quest::say("YOU SSSSSSHALL! For I, Roror, High Prissst of Cazic-Thule, I sssshall teach you!");
 		quest::attack($name);
 	}
-	if ($text=~/curse my hay/i) {
-		quest::say("I require 66 gold coin and the hay in order to curse it.");
+	if ($text=~/bless my hay/i) {
+		quest::say("You sssseek the blesssssssing of the Lord of Fear?! Give me what you have and 66 gold piecesss as an offering to Cazic-Thule.");
 	}
 }
 							
@@ -35,6 +35,7 @@ sub EVENT_ITEM {
 	#:: Match 13990 - Bale of Hay and 66gp
 	if ($cash >= 6600) {
 		if (plugin::check_handin(\%itemcount, 13990 => 1)) {
+			quest::say("'Whatsssss thisssss? You sssseek my blessssssssing? Heh heh heh... Very well... CAZIC-THULE! Take this fruit of Karana into horror'sss dark embrace. Fear and death made manifesssssst. A harvesssst of terror! Here, take your gift of blood and sssstraw. Use its dark powersssss in the name of the Fear Lord!' ");
 			#:: Give a 14320 - Sack of Cursed Hay
 			quest::summonitem(14320);
 			quest::exp(300);
