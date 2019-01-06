afc324948b4e0089d7c0ab089105b6990b347177
TurmoilToad <TurmoilToad@gmail.com>
2019-01-06 17:37:32 -0500

    Update Reebo_Leafsway.pl

--- rivervale/Reebo_Leafsway.pl
+++ rivervale/Reebo_Leafsway.pl
@@ -1,10 +1,10 @@
 sub EVENT_SPAWN {
-	#:: Create a proximity, 200 units across
+	#:: Get current location
 	$x = $npc->GetX();
 	$y = $npc->GetY();
-	quest::set_proximity($x - 100, $x + 100, $y - 100, $y + 100);
-	#:: Enable proximity Say
-	quest::enable_proximity_say();
+	$z = $npc->GetZ();
+	#:: Create a proximity, 100 units across, 100 units tall, and enable proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 1);
 }
 
 sub EVENT_SAY {
