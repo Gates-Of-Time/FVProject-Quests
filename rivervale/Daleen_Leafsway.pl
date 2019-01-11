e94c624f7576413b5c2efea1e14f06a8973c9faf
TurmoilToad <TurmoilToad@gmail.com>
2019-01-11 09:11:36 -0500

    Update Daleen_Leafsway.pl

--- rivervale/Daleen_Leafsway.pl
+++ rivervale/Daleen_Leafsway.pl
@@ -5,6 +5,8 @@ sub EVENT_SPAWN {
 	$z = $npc->GetZ();
 	#:: Create a proximity, 100 units across, 100 units tall, and enable proximity say
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 1);
+	#:: Also enable proximity say
+	quest::enable_proximity_say();
 	#:: Create a timer that loops every 5 minutes
 	quest::settimer("blurt", 300);
 }
