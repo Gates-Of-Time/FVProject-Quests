bffaca9d73dc09fc612b42dada65e1e4577f2af7
TurmoilToad <TurmoilToad@gmail.com>
2019-01-13 18:08:19 -0500

    Update Xanuusus.pl

--- northkarana/Xanuusus.pl
+++ northkarana/Xanuusus.pl
@@ -8,17 +8,17 @@ sub EVENT_ITEM {
 	#:: Turn in for 13411 - Case of Jumjum Juice
 	if (plugin::takeItems(13411 => 1 )) {
 		quest::say("Heh heh! Excellent, small one!  My roots have been longing for some fresh jumjum juice!  Thank you!  Take this for your trouble.  Be sure to thank the Tagglefoots for me.");
-		#:: Randomly choose a 13977 - carrot, 84004 - fishing pole, or 13083 - pine needles
+		#:: Give a random reward: 13977 - carrot, 84004 - fishing pole, or 13083 - pine needles
 		quest::summonitem(quest::ChooseRandom(13977,84004,13083));
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
-		quest::ding();	
+		quest::ding();
+		#:: Give a small amount of experience
+		quest::exp(500);
 		#:: Create a hash for storing cash - 4500 to 4600cp
 		my %cash = plugin::RandomCash(900,2000);
-			#:: Grant a random cash reward
-			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Set factions
 		quest::faction(316, 10);	#:: + Storm Reapers
 		quest::faction(208, 1);		#:: + Mayor Gubbin
 		quest::faction(218, 1);		#:: + Merchants of Rivervale
@@ -27,4 +27,3 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-
