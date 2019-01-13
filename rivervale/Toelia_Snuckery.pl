b866165a9f9c827fe0c76733672e2f71ac4f6a3a
TurmoilToad <TurmoilToad@gmail.com>
2019-01-13 18:06:48 -0500

    Update Toelia_Snuckery.pl

--- rivervale/Toelia_Snuckery.pl
+++ rivervale/Toelia_Snuckery.pl
@@ -38,24 +38,26 @@ sub EVENT_ITEM {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {		
 			quest::say("What is this?  The pouch is empty!  Where is the Ruby?!  What do you mean you don't have it?  Oh no.  I bet [" . quest::saylink("Chomper") . "] swallowed it!  Get it back and bring it to me.");
-			#:: Give a small amount of xp
-			quest::exp(1500);
 			#:: Ding!
 			quest::ding();
-			#:: Set faction
-			quest::faction(77, 5);		#:: + Deeppockets
-			quest::faction(33, 5);		#:: + Circle of Unseen Hands
-			quest::faction(218,-5);		#:: - Merchants of Rivervale
-			quest::faction(31, 5);		#:: + Carson McCabe
-			quest::faction(48, 5);		#:: + Coalition of Tradefolk Underground
+			#:: Give a moderate amount of experience
+			quest::exp(1500);
 			#:: Create a hash for storing cash - 900 to 2000cp
 			my %cash = plugin::RandomCash(900,2000);
-				#:: Grant a random cash reward
-				quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Set factions
+			quest::faction(77, 5);		#:: + Deeppockets
+			quest::faction(33, 5);		#:: + Circle of Unseen Hands
+			quest::faction(218, -5);	#:: - Merchants of Rivervale
+			quest::faction(31, 5);		#:: + Carson McCabe
+			quest::faction(48, 5);		#:: + Coalition of Tradefolk Underground
 		}
 		#:: Match if faction is worse than indifferent
 		elsif ($faction > 5) {
 			quest::say("Oh. hi.  Listen. um.. $name. was it?  Listen. pal.  I really don't have time for the friendly-friendly. so why don't you just move on?");
+			#:: Return a 13785 - Torn Old Pouch
+			quest::summonitem(13785);
 		}
 	}
 	#:: Turn in for 13786 - Large Ruby
@@ -63,24 +65,26 @@ sub EVENT_ITEM {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {		
 			quest::say("You found it!  Heh.  Good thing you brought it back bub.  This thing isn't priceless, its worthless but at least you proved you are loyal.  Poor ol' Chomper..");
-			#:: Give a small amount of xp
-			quest::exp(1000);
 			#:: Ding!
 			quest::ding();
-			#:: Set faction
-			quest::faction(77, 10);		#:: + Deeppockets
-			quest::faction(33, 10);		#:: + Circle of Unseen Hands
-			quest::faction(218,-10);		#:: - Merchants of Rivervale
-			quest::faction(31, 10);		#:: + Carson McCabe
-			quest::faction(48, 10);		#:: + Coalition of Tradefolk Underground
+			#:: Give a moderate amount of experience
+			quest::exp(1000);
 			#:: Create a hash for storing cash - 900 to 2000cp
 			my %cash = plugin::RandomCash(1800,4000);
-				#:: Grant a random cash reward
-				quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+			#:: Set factions
+			quest::faction(77, 10);		#:: + Deeppockets
+			quest::faction(33, 10);		#:: + Circle of Unseen Hands
+			quest::faction(218, -10);	#:: - Merchants of Rivervale
+			quest::faction(31, 10);		#:: + Carson McCabe
+			quest::faction(48, 10);		#:: + Coalition of Tradefolk Underground
 		}
 		#:: Match if faction is worse than indifferent
 		elsif ($faction > 5) {
 			quest::say("Oh. hi.  Listen. um.. $name. was it?  Listen. pal.  I really don't have time for the friendly-friendly. so why don't you just move on?");
+			#:: Give back a 13786 - Large Ruby
+			quest::summonitem(13786);
 		}
 	}	
 	#:: Return unused items
