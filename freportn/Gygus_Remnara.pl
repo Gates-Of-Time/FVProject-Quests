0cce7cc80f1cd9bca38fd8204d063d922432b70f
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-01-20 14:38:17 -0500

    Create Gygus_Remnara.pl
    Updated Gygus_Remnara of freportn to perl - new file no link

---
+++ freportn/Gygus_Remnara.pl
@@ -0,0 +1,24 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("The Temple of Marr welcomes you. I am Gygus Remnara. High Sentinel for the Sentries of Passion. We are the order of paladins within the Priests of Marr and whose charge it is to protect the holy Temple of Marr.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Turn in for 18738 - A Tattered Note
+	if (plugin::takeItems(18738 => 1 )) {
+		quest::say("Welcome to the Sentries of Passion. We are the protectors of the Temple of Marr. Wear our tunic with pride, young knight! Find your wisdom within these walls and in the words of our priests. And remember to aid all who follow the twin deities, Mithaniel and Erollisi Marr.");
+		#:: Give item 13556 - White and Blue Tunic*
+		quest::summonitem(13556);
+		#:: Give a small amount of xp
+		quest::exp(100);
+		#:: Ding!
+		quest::ding();
+		#:: Set faction
+		quest::faction(258,100); 	#:: + Priests of Marr
+		quest::faction(105,-10); 	#:: - The Freeport Militia
+		quest::faction(184,15); 	#:: + Knights of Truth
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}	
