767c0e328fa97f3e3293e6b0702ae9f8af566ea6
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-01-21 14:48:52 -0500

    Create Plur_Etinu.pl
    Updated Plur_Etinu of freportn to perl - new file no link

---
+++ freportn/Plur_Etinu.pl
@@ -0,0 +1,46 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Your soul is welcome in our temple. The Queen of Love, Erollisi Marr, desires more to join our ranks. This city of Freeport must not stay under the control of  the Freeport Militia. I am here to [" . quest::saylink("cure poison") . "] and [" . quest::saylink("cure disease") . "], or if any [" . quest::saylink("need healing") . "].");
+	}
+	elsif ($text=~/cure poison/i) {
+		quest::say("Before the Queen of Passion can force the toxin from your system, I shall require 3 bixie stingers to perform the act.");
+	}
+	elsif ($text=~/cure disease/i) {
+		quest::say("The affliction which has been cast upon you shall be purged from your body, but first I need two portions of zombie flesh to perform the act.");
+	}
+	elsif ($text=~/need healing/i) {
+		quest::say("If it is the power of the passion you require to bind your wounds, then lay down your tribute of 10 gold.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Turn in for 13074 - 2x Zombie Skin
+	if (plugin::takeItems(13074 => 2 )) {
+		quest::say("May the Power of Passion purge you of disease!");
+		#:: Cast spell 203 - Cure Poison
+		$npc->CastSpell(203,$userid);
+		#:: Ding!
+		quest::ding();
+	}	
+	#:: Turn in for 14029 - 3x Bixie Stinger
+	elsif (plugin::takeItems(14029 => 3 )) {
+		quest::say("May the Power of Passion forced you free of poison!");
+		#:: Cast spell 203 - Cure Poison
+		$npc->CastSpell(203,$userid);
+		#:: Ding!
+		quest::ding();
+	}		
+	#:: Create a scalar variable for storing money
+	my $cash = 0;
+	$cash = ($platinum * 1000) + ($gold * 100) + ($silver * 10) + $copper;
+	#:: Match 1000 copper - 10gp
+	if ($cash == 1000) {
+		quest::say("May the unbridled passion of Erollisi Marr flow through your body.");
+		#:: Cast spell 17 - Light Healing
+		$npc->CastSpell(17,$userid);
+		#:: Ding!
+		quest::ding();
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}	
