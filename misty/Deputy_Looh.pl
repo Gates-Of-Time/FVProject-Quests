dedd870b42acdd628547c1140f6ad75e9a2b483b
TurmoilToad <TurmoilToad@gmail.com>
2019-02-05 19:45:46 -0500

    Update Deputy_Looh.pl

--- misty/Deputy_Looh.pl
+++ misty/Deputy_Looh.pl
@@ -1,9 +1,3 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
-		}
-	}
+sub EVENT_AGGRO {
+	quest::say("Time to die, $name.");
 }
