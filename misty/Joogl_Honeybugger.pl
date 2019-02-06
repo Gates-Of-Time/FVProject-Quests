7fd25ac64497fe44557ee4b21ed54ed8673e8076
TurmoilToad <TurmoilToad@gmail.com>
2019-02-05 21:09:11 -0500

    Update Joogl_Honeybugger.pl

--- misty/Joogl_Honeybugger.pl
+++ misty/Joogl_Honeybugger.pl
@@ -1,43 +1,75 @@
 sub EVENT_SAY {
-  if($text=~/hail/i){
-   quest::say("Hello. I would shake your hand, but it would [hurt]. Ooooh! Owwy!");
-  }
-  if($text=~/hurt/i){
-    #Bandages for Honeybugger (START)
-    quest::say("I was attacked by the bixies. They swarmed on me!! I think I got too near their queen. I need that honey to make a living! Get me a bandage and I will tell you where all their worker bee's buzz around.");
-  }
-  if ($text=~/I need the honeycomb/i) {
-    quest::say("If you're looking for the honeycombs, I don't have any right now. I have been unable to get any since I was attacked by the little [buggers]. I will tell you where to get some if you will only get me a bandage for my bites.");
-  }
-  if ($text=~/what buggers/i) {
-    quest::say("I was attacked by the bixies. They swarmed on me!! I think I got too near their queen. I need that honey to make a living! Get me a bandage and I will tell you where all their worker bees buzz around.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hello. I would shake your hand, but it would [" . quest::saylink("hurt") . "]. Ooooh! Owwy!");
+	}
+	elsif ($text=~/hurt/i) {
+		quest::say("I was attacked by the bixies. They swarmed on me!! I think I got too near their queen. I need that honey to make a living! Get me a bandage and I will tell you where all their worker bee's buzz around.");
+	}
+	elsif ($text=~/honeycomb/i) {
+		quest::say("If you're looking for the honeycombs, I don't have any right now. I have been unable to get any since I was attacked by the little [" . quest::saylink("buggers") . "]. I will tell you where to get some if you will only get me a bandage for my bites.");
+	}
+	elsif ($text=~/buggers/i) {
+		quest::say("I was attacked by the bixies. They swarmed on me!! I think I got too near their queen. I need that honey to make a living! Get me a bandage and I will tell you where all their worker bees buzz around.");
+	}
 }
 
 sub EVENT_ITEM {
-  my $bandage = 0;
-  #Handin: Bandage
-  if(plugin::check_handin(\%itemcount, 13009 => 4)){ $bandage=4;
-  } elsif(plugin::check_handin(\%itemcount, 13009 => 3)){ $bandage=3;
-  } elsif(plugin::check_handin(\%itemcount, 13009 => 2)){ $bandage=2;
-  } elsif(plugin::check_handin(\%itemcount, 13009 => 1)){ $bandage=1;
-  }
-  if($bandage>=1){
-    for($i=0; $i<$bandage; $i++){ 
-      #Bandages for Honeybugger (END) 
-      quest::say("Oh thank you, $name. If you are ever going to gather bixie honeycomb's pray you do not run into the queen. The only way I know of collecting the honey is by intercepting the drone's and taking the honeycomb's they sometime's carry. Good luck!!");
-      quest::ding();
-      quest::exp(10);
-      quest::faction(77,10);   # +Deeppockets
-      quest::faction(133,10);  # +Guardians of the Vale
-      quest::faction(208,10);  # +Mayor Gubbin
-      quest::faction(218,10);  # +Merchants of Rivervale
-      quest::faction(369,-10); # -Coalition of Trade Folk III
-     }
+	#:: Match four 13009 - Bandages
+	if (plugin::takeItems(13009 => 4)) {
+		quest::say("Oh thank you, $name. If you are ever going to gather bixie honeycomb's pray you do not run into the queen. The only way I know of collecting the honey is by intercepting the drone's and taking the honeycomb's they sometime's carry. Good luck!!");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(40);
+		#:: Set factions
+		quest::faction(218, 4);		#:: + Merchants of Rivervale
+		quest::faction(77, 4);		#:: + Deeppockets
+		quest::faction(133, 4);		#:: + Guardians of the Vale
+		quest::faction(208, 4);		#:: + Mayor Gubbin
+		quest::faction(48, -4);		#:: - Coalition of Tradefolk Underground
+	}
+	#:: Match three 13009 - Bandages
+	elsif (plugin::takeItems(13009 => 3)) {
+		quest::say("Oh thank you, $name. If you are ever going to gather bixie honeycomb's pray you do not run into the queen. The only way I know of collecting the honey is by intercepting the drone's and taking the honeycomb's they sometime's carry. Good luck!!");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(30);
+		#:: Set factions
+		quest::faction(218, 3);		#:: + Merchants of Rivervale
+		quest::faction(77, 3);		#:: + Deeppockets
+		quest::faction(133, 3);		#:: + Guardians of the Vale
+		quest::faction(208, 3);		#:: + Mayor Gubbin
+		quest::faction(48, -3);		#:: - Coalition of Tradefolk Underground
+	}
+	#:: Match two 13009 - Bandages
+	elsif (plugin::takeItems(13009 => 2)) {
+		quest::say("Oh thank you, $name. If you are ever going to gather bixie honeycomb's pray you do not run into the queen. The only way I know of collecting the honey is by intercepting the drone's and taking the honeycomb's they sometime's carry. Good luck!!");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(20);
+		#:: Set factions
+		quest::faction(218, 2);		#:: + Merchants of Rivervale
+		quest::faction(77, 2);		#:: + Deeppockets
+		quest::faction(133, 2);		#:: + Guardians of the Vale
+		quest::faction(208, 2);		#:: + Mayor Gubbin
+		quest::faction(48, -2);		#:: - Coalition of Tradefolk Underground
+	}
+	#:: Match a 13009 - Bandages
+	elsif (plugin::takeItems(13009 => 1)) {
+		quest::say("Oh thank you, $name. If you are ever going to gather bixie honeycomb's pray you do not run into the queen. The only way I know of collecting the honey is by intercepting the drone's and taking the honeycomb's they sometime's carry. Good luck!!");	
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(10);
+		#:: Set factions
+		quest::faction(218, 1);		#:: + Merchants of Rivervale
+		quest::faction(77, 1);		#:: + Deeppockets
+		quest::faction(133, 1);		#:: + Guardians of the Vale
+		quest::faction(208, 1);		#:: + Mayor Gubbin
+		quest::faction(48, -1);		#:: - Coalition of Tradefolk Underground
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-  plugin::return_items(\%itemcount);
-}
-
-
-#EOF
-
