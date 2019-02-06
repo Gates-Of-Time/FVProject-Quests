4963e1b4e6f189fc6d5795739625ca8463154694
TurmoilToad <TurmoilToad@gmail.com>
2019-02-05 20:40:16 -0500

    Update Deputy_Tagil.pl

--- misty/Deputy_Tagil.pl
+++ misty/Deputy_Tagil.pl
@@ -1,19 +1,24 @@
 sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
+	if ($combat_state == 1) {
+		my $cur_target = $npc->GetHateTop();
+		if ($cur_target) {
+			my $target_name = $cur_target->GetCleanName();
+			quest::say("Time to die $target_name!");
 		}
 	}
 }
 
-sub EVENT_ITEM { 
-  if(plugin::check_handin(\%itemcount, 18013 => 1)){
-    quest::say("What's this? Oh dear. I am very sorry. Please take this to Uner with my apologies.");
-    quest::summonitem(13240);
-  }
-  plugin::return_items(\%itemcount);
+sub EVENT_ITEM {
+	#:: Match a 18013 - Note
+	if (plugin::takeItems(18013 => 1)) {
+		quest::say("What's this? Oh dear. I am very sorry. Please take this to Uner with my apologies.");
+		#:: Give a 13240 - Deputy Tagil's Payment
+		quest::summonitem(13240);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(100);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-#End of File, Zone:misty  NPC:33090 -- Deputy_Tagil
