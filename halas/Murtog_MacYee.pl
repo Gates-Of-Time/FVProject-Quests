e3848b3616e0ebf1223f70438a69ace7e99b9280
TurmoilToad <TurmoilToad@gmail.com>
2019-02-22 22:52:47 -0500

    Update Murtog_MacYee.pl

--- halas/Murtog_MacYee.pl
+++ halas/Murtog_MacYee.pl
@@ -1,27 +1,16 @@
-sub EVENT_SPAWN {
-	#:: Get current location
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	$z = $npc->GetZ();
-	#:: Create a proximity, 100 units across, 100 units tall, and enable proximity say
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 1);
-	#:: Also enable proximity say
-	quest::enable_proximity_say();
+sub EVENT_SIGNAL {
+	quest::say("Dinnae be rude to the customers, Holana.");
 }
-	
-sub EVENT_PROXIMITY_SAY {
+
+sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Dinnae be rude to the customers, Holana.");
+		quest::say("Greetin's! I've many fine herbs and potions fer sale. Please look around.");
 	}
 }
-sub EVENT_SAY {
-     if ($text=~/hail/i) {
-        quest::say("Greetin's! I've many fine herbs and potions fer sale. Please look around.");
-     }
-}
+
 sub EVENT_ITEM {
-    
-		#:: Return unused items
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-# Converted to Perl by SS 
+
+#:: Converted to Perl by SS
