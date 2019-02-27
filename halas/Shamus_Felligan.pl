e897346e6c31da13b6f3f0395b6dae298f8cfb0d
TurmoilToad <TurmoilToad@gmail.com>
2019-02-27 12:31:56 -0500

    Update Shamus_Felligan.pl

--- halas/Shamus_Felligan.pl
+++ halas/Shamus_Felligan.pl
@@ -2,54 +2,62 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Greetin's! Justice speaks through us. We're the followers o' the Tribunal. We act as judge. jury and executioner fer all misled Northmen. Sometimes we must also execute our swift justice upon evil races. such as th' [" . quest::saylink("ice goblins") . "].");
 	}
-	if ($text=~/ice goblins/i) {
+	elsif ($text=~/ice goblins/i) {
 		quest::say("The ice goblins are a weak race. They pose no threat to our community. but lately we've heard rumors of ice goblins that can cast spells. They're said to be as weak as the goblin warriors. so I seek to employ the talents of our young shamans to [" . quest::saylink("hunt the goblin casters") . "].");
 	}
-	if ($text=~/hunt the goblin casters/i) {		
+	elsif ($text=~/hunt the goblin casters/i) {		
 		quest::say("Aye. ye'll serve justice.  I must find the source o' their recent spellcasting ability.  I hear reports o' glowing necklaces upon these wicked beasts' necks. Get me one o' these casters' necklaces. Return them in any condition at all. Go! Justice awaits yer return.");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Match a 13968 - Shattered Caster Beads
-	if (plugin::check_handin(\%itemcount, 13968 => 1)) {
+	if (plugin::takeItems(13968 => 1)) {
 		quest::say("Shattered! This has happened frequently! These beads are very delicate. They're useless to me now, however, I'll reward ye fer the execution of yet more goblin casters. Continue yer work. The Tribunal watches ye!");
-		quest::summonitem(quest::ChooseRandom(15270, 15275, 15075, 15271, 15279, 15212, 15079));
+		#:: Give a random reward: 15270 - Spell: Drowsy, 15275 - Spell: Frost Rift, 15075 - Spell: Sicken, 15271 - Spell: Fleeting Fury, 15212 - Spell: Cure Blindness, 15079 - Spell: Spirit Sight
+		quest::summonitem(quest::ChooseRandom(15270, 15275, 15075, 15271, 15212, 15079));
 		#::Ding!
 		quest::ding();
-		#:: Grant a small amount of coin
-		quest::givecash(0,0,6,0);
-		#:: Grand a small amount of experience
+		#:: Grant a small amount of experience
 		quest::exp(800);
+		#:: Create a hash for storing cash - 500 to 700cp
+		my %cash = plugin::RandomCash(500,700);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Set Factions
-		quest::faction(213,1); 		# + Merchants of Halas
-		quest::faction(33,-1);		# - Circle of Unseen Hands
-		quest::faction(294,10);		# + Shamen of Justice
-		quest::faction(48,-1);		# - Coalition of Tradefolk Underground
-		quest::faction(137,-2);		# - Hall of the Ebon Mask
+		quest::faction(294, 10);		#:: + Shamen of Justice
+		quest::faction(213, 1);			#:: + Merchants of Halas
+		quest::faction(33, -1);			#:: - Circle of Unseen Hands
+		quest::faction(48, -1);			#:: - Coalition of Tradefolk Underground
+		quest::faction(90, -2);			#:: - Ebon Mask
 	}
-	if ($class eq "shaman") {
-		#:: Match 13969 - Caster Beads
-		if (plugin::check_handin(\%itemcount, 13969 => 1)) {
+	#:: Match a 13969 - Caster Beads
+	elsif (plugin::takeItems(13969 => 1)) {
+		if ($class eq "shaman") {
 			quest::say("Finally! Intact! This IS good news! I can continue me investigation now. As fer yer loyal deed, I'll offer ye this, the Gavel of Justice. May ye employ it well in the service o' justice.");
 			#:: Give a 6028 - Gavel of Justice
 			quest::summonitem(6028);
 			#:: Ding!
 			quest::ding();
-			#:: Grant a small amount of coin
-			quest::givecash(0,0,0,1);
 			#:: Grant a moderate amount of experience
 			quest::exp(1600);
+			#:: Create a hash for storing cash - 900 to 1100cp
+			my %cash = plugin::RandomCash(900,1100);
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 			#:: Set Factions
-			quest::faction(213,3);		# + Merchants of Halas
-			quest::faction(33,-3);		# - Circle of Unseen Hands
-			quest::faction(294,25);		# + Shamen of Justice
-			quest::faction(48,-3);		# - Coalition of Tradefolk Underground
-			quest::faction(137,-5);		# - Hall of the Ebon Mask
+			quest::faction(294, 25);	#:: + Shamen of Justice
+			quest::faction(213, 3);		#:: + Merchants of Halas
+			quest::faction(33, -3);		#:: - Circle of Unseen Hands
+			quest::faction(48, -3);		#:: - Coalition of Tradefolk Underground
+			quest::faction(90, -5);		#:: - Ebon Mask
+		}
+		else {
+			#:: Return a 13969 - Caster Beads
+			quest::summonitem(13969);
+			quest::say("I have no use for this item, $name.  You can have it back.");
 		}
 	}
     	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
-
-# Converted to Perl by SS
