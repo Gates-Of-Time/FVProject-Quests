3f0c6a4ed07f5f793b4b08496eb79f0f942dc1af
TurmoilToad <TurmoilToad@gmail.com>
2019-03-05 07:25:51 -0500

    Update 46109.pl

--- innothule/46109.pl
+++ innothule/46109.pl
@@ -5,7 +5,7 @@ sub EVENT_SPAWN {
 
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest:say("Yes?");
+		quest::say("Yes?");
 	}
 	elsif ($text=~/come back to the closet/i) {
 		quest::emote("lets out a long sigh.");
@@ -17,7 +17,7 @@ sub EVENT_TIMER {
 	#:: Match the timer "despawn"
 	if ($timer eq "despawn") {
 		#:: Stop the timer from looping over and over
-		quest::stoptimer("despawn")
+		quest::stoptimer("despawn");
 		#:: Depop
 		quest::depop();
 	}
@@ -25,14 +25,14 @@ sub EVENT_TIMER {
 
 sub EVENT_ITEM {
 	#:: Match a 12218 - Ship in a Bottle
-	if (plugin::takeItems(12218 => 1) {
+	if (plugin::takeItems(12218 => 1)) {
 		quest::say("At least I can dream I'm on this ship out on the wide open sea...Thanks friend. You know what would make being in that closet easier? Having my friend The Minstrel playing music. I heard Basher Avisk trying to hum a tune that I know The Minstrel plays a lot. Maybe he knows where to find The Minstrel. Hmm...I doubt he'll know what a minstrel is...better ask him about the [tune] he's always humming.");
 		#:: Give a 12215 - The Captain
 		quest::summonitem(12215);
 		#:: Ding!
 		quest::ding();
 		#:: Stop the timer from looping over and over
-		quest::stoptimer("despawn")
+		quest::stoptimer("despawn");
 		#:: Depop
 		quest::depop();
 	}
