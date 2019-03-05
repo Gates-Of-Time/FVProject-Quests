1ceddf9c706ce88af6e96928dd5ee4384e847206
TurmoilToad <TurmoilToad@gmail.com>
2019-03-04 21:36:13 -0500

    Create Basher_Avisk.pl

---
+++ grobb/Basher_Avisk.pl
@@ -0,0 +1,16 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::emote("stops humming to himself and turns to look at you.");
+		quest::say("Hey dere.");
+	}
+	elsif ($text=~/what tune/i) {
+		quest::say("Yous like da toon me hum? Me hum gud! Me hears it sumtimes when me walks by da Dark Ones place. Dey gots strong majik back dere, me gets funny feeling in me tummy.");
+		#:: Spawn Grobb >> a_skeleton (52119)
+		quest::spawn2(52119,0,0,-459.50,16.00,75.08,0);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
