328879de70f1ab4f87a3002a6bb8ebc7e3f2e96b
TurmoilToad <TurmoilToad@gmail.com>
2019-03-10 11:32:09 -0400

    Update Nerbilik.pl

--- oot/Nerbilik.pl
+++ oot/Nerbilik.pl
@@ -1,11 +1,23 @@
+#:: Create a scalar variable to store wrapper state
+$wrapper = 0;
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("What you want?!! Face so ugly you scare fish away!! ..sniff, sniff. Smell so bad, too!! Me sure is hungry. Wish had [" . quest::saylink("grub locker") . "].");
 	}
-	if ($text=~/grub locker/i) {
+	elsif ($text=~/grub locker/i) {
 		quest::say("You gots grub locker!! Give to me. Me sick of fish and not many dwarves fall off ship. Me pay a plat to get me grub locker from the Gobbler in Neriak. Go to him and say you [deliver grub locker] if you wants one plat.");
 	}
+	elsif ($text=~/wrapper/i) {
+		if ($wrapper == 1) {
+			quest::say("You no get my wrapper!! Me use it later when me needs to make more room in stomach!! You never get it!!");
+			#:: Update the wrapper variable
+			$wrapper = 0;
+		}
+	}
 }
+
+		
 
 sub EVENT_ITEM {
 	#:: Match a 12203 - Grub Locker
@@ -13,8 +25,8 @@ sub EVENT_ITEM {
 		quest::say("Oooh!! Grub locker! There is a loud click. He opens the locker and unwraps dwarf pickles. What this dumb wrapper? R, E, C, I, P, E.. Ahh!! It say pickles!! Me read good. Here your plat. Me give you little more. Me was very hungry for pickles.");
 		#:: Ding!
 		quest::ding();
-		#:: Create a hash for storing cash - 900 to 2000cp
-		my %cash = plugin::RandomCash(900,2000);
+		#:: Create a hash for storing cash - 1500 to 2000cp
+		my %cash = plugin::RandomCash(1500,2000);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Grant a small amount of experience
@@ -22,7 +34,11 @@ sub EVENT_ITEM {
 		#:: Set factions
 		quest::faction(66,10);	#:: + Da Bashers
 		quest::faction(22,10);	#:: + Broken Skull Clan
+		#:: Update wrapper variable
+		$wrapper = 1;
+		#:: Add a 18940 - Tattered Recipe to loot
+		$npc->AddItem(18940);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
