6d069c1b5fe61200cbc5ee5fb991c96d34bae304
TurmoilToad <TurmoilToad@gmail.com>
2019-03-16 11:46:59 -0400

    Update Zeek.pl

--- befallen/Zeek.pl
+++ befallen/Zeek.pl
@@ -1,23 +1,22 @@
 sub EVENT_SAY {
-	#:: NPC For Bregna's Big Mistake Quest
 	if ($text=~/hail/i) {
 		quest::say("Ah, so many dead to destroy, so little time. What is it you want? You seek the [" . quest::saylink("potions") . "] perhaps?");
 	}
-	if ($text=~/potions/i) {
+	elsif ($text=~/potions/i) {
 		quest::say("Give me the four clues.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Match 18651 - Note to Troll, 18652 - First Riddle for the Troll, 18653 - Second Riddle for the Troll, 18654 - Third Riddle for the Troll
-	if (plugin::check_handin(\%itemcount, 18651 => 1, 18652 => 1, 18653 => 1, 18654 => 1 )) {
+	#:: Match a 18651 - Note to Troll, 18652 - First Riddle for the Troll, 18653 - Second Riddle for the Troll, 18654 - Third Riddle for the Troll
+	if (plugin::takeItems(18651 => 1, 18652 => 1, 18653 => 1, 18654 => 1)) {
 		#:: Give item 13984 - Crate of Potions
 		quest::summonitem(13984);
+		#:: Ding!
+		quest::ding();		
 		#:: Give a small amount of xp
 		quest::exp(100);
-		#:: Ding!
-		quest::ding();
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
