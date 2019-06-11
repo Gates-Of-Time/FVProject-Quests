5c4c97f57bbdf3e42bb547e3965f008eafd210a6
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-06-11 10:26:43 -0400

    Create Guard_Alayle.pl
    Updated Guard Alayle of ferportw to perl new file no link

---
+++ freportw/Guard_Alayle.pl
@@ -0,0 +1,42 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings, citizen! You should not be in the Militia House. These are restricted grounds. Please leave at once unless you have business here.");
+	}
+	elsif ($text=~/truth is good/i) {
+		if ($class eq "Paladin") {
+			quest::say("Ssshhh!! Pipe down. The others might hear you. You must have something for me. Kalatrina must have given you something if you serve the Hall of Truth. If you have nothing please leave. You will blow my cover.");
+		}
+	}	
+}
+
+sub EVENT_ITEM {
+	#:: Turn in for 18817 - Sealed Letter
+	if (plugin::takeItems(1797 => 3 )) {
+		if ($class eq "Paladin") {
+		quest::say("This is not good news. I must leave immediately. Here. Take this to Kala.. I mean my father. I found it on the floor of Sir Lucan D'Lere's quarters. Thanks again, messenger. I got this just in time");
+		#:: Ding!
+		quest::ding();
+		#:: Summon 18818 A Tattered Flier
+		quest::summonitem(18818);
+		#:: Set faction
+		quest::faction(311,1); 			#:: + Steel Warriors
+		quest::faction(135,1); 			#:: + Guards of Qeynos
+		quest::faction(53,-1); 			#:: - Corrupt Qeynos Guards
+		quest::faction(105,-1); 		#:: - Freeport Militia
+		quest::faction(184,1); 			#:: + Knights of Truth
+		#:: Spawn Krazen Loosh
+		quest::spawn2(9143,87,0,-154,-55,-10,128);
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}	
+
+sub EVENT_SIGNAL { 
+	#:: Receive Signal 1 from Krazen Loosh
+	if ($signal == 1) {
+		quest::say("Oh no!! It is too late!! Run!!");
+		#Signal Krazen Loosh
+		quest::signal(9143,1,30000); 
+	}
+}
