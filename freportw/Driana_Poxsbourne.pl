00b98850995ad34b95fe18f0a868cf6a6d954b5b
TurmoilToad <TurmoilToad@gmail.com>
2019-06-29 08:59:55 -0400

    Update Driana_Poxsbourne.pl

--- freportw/Driana_Poxsbourne.pl
+++ freportw/Driana_Poxsbourne.pl
@@ -8,8 +8,8 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 1797 x3 - Reagent Pouch
-	if (plugin::takeItems(1797 => 3 )) {
+	#:: Turn in for 1797 - Reagent Pouch x3
+	if (plugin::takeItems(1797 => 3)) {
 		quest::say("Ahh wonderful work $name Here is the substance....don't spill it HAHAHAaahhahehehe...yes......you don't want to spill that heheh..");
 		#:: Give a small amount of xp
 		quest::exp(200);
@@ -18,11 +18,11 @@ sub EVENT_ITEM {
 		#:: Summon 1793 Putrid Substance
 		quest::summonitem(1793);
 		#:: Set faction
-		quest::faction(21,1); 			#:: + Bloodsabers
-		quest::faction(235,1); 			#:: + Opal Dark Briar
-		quest::faction(53,1); 			#:: + Corrupt Qeynos Guards
-		quest::faction(135,-1); 		#:: - Guards of Qeynos
-		quest::faction(257,-1); 		#:: - Priests of Life
+		quest::faction(21, 1); 			#:: + Bloodsabers
+		quest::faction(235, 1); 		#:: + Opal Dark Briar
+		quest::faction(53, 1); 			#:: + Corrupt Qeynos Guards
+		quest::faction(135, -1); 		#:: - Guards of Qeynos
+		quest::faction(257, -1); 		#:: - Priests of Life
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
