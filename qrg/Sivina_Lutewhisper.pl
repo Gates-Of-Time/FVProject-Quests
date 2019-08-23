baf8b334dbdfbc7cd70b73183d32142c1bf03854
TurmoilToad <TurmoilToad@gmail.com>
2019-08-23 11:06:13 -0400

    Update and rename Sivina_Lutewhisper.lua to Sivina_Lutewhisper.pl

---
+++ qrg/Sivina_Lutewhisper.pl
@@ -0,0 +1,21 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail, $name - Are you [" . quest::saylink("interested") . "] in helping the League of Antonican Bards by delivering some [" . quest::saylink("mail") . "]?");
+	}
+	elsif ($text=~/mail/i) {
+		quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers.  We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices.  Are you [" . quest::saylink("interested") . "]?");
+	}
+	elsif ($text=~/interested/i) {
+		quest::say("I have messages that need to go to - well, right now I have one that needs to go to Qeynos.  Will you [" . quest::saylink("deliver mail to Qeynos") . "] for me?");
+	}
+	elsif ($text=~/deliver mail to qeynos/i) {
+		quest::say("Take this letter to Tralyn Marsinger in Qeynos.  You can find him at the bard guild hall.  I am sure he will compensate you for your troubles.");
+		#:: Give a 18150 - Bardic Letter (Qeynos)
+		quest::summonitem(18150);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
