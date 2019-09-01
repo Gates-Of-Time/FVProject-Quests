bbe38a9ee00cfe781931bf92912ef18babd1bc32
TurmoilToad <TurmoilToad@gmail.com>
2019-09-01 09:09:11 -0400

    Update and rename Joffrey_Clay.lua to Joffrey_Clay.pl

---
+++ qeynos/Joffrey_Clay.pl
@@ -0,0 +1,29 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings, friend. My name is Joffrey. I'm trying to learn to play the flute. I'm also working on a new song I learned while I was visiting [" . quest::saylink("Kelethin") . "].");
+	}
+	elsif ($text=~/kelethin/i) {
+		quest::say("I just got back from a trip to Faydwer. I was over there studying the lore of Tunare, in the beautiful elven city of Kelethin. I highly recommend visiting there as soon as you get a chance.");
+	}
+}
+
+sub EVENT_WAYPOINT_ARRIVE {
+	#:: Match when NPC arrives at waypoint 1
+	if ($WP == 1) {
+		quest::say("Belious, I'm working on this new tune I learned on my last trip to Kelethin. It's about Tunare. If you have a free ear, would you care to take a listen?");
+		#:: Send a signal "2" to South Qeynos >> Belious_Naliedin (1125) with 0 second delay
+		quest::signal(1125,2,0);
+	}
+	#:: Match when NPC arrives at waypoint 2
+	elsif ($WP == 2) {
+		quest::say("Ahem. Tunare and the Pact, sung by Joffrey Clay.");
+		#:: Do animation 58 - Dance
+		quest::doanim(58);
+		quest::say("La da di, la di da.. la da di. la di da..");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
