6939766f15ef428cd285a9cea3546b4cb446b62d
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 14:25:35 -0400

    Update Blinza_Toepopal.pl

--- rivervale/Blinza_Toepopal.pl
+++ rivervale/Blinza_Toepopal.pl
@@ -1,11 +1,7 @@
 sub EVENT_SPAWN {
 	#:: Set a timer for 10 minutes
 	quest::settimer("jillin", 600);
-    	#:: Get current location
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	$z = $npc->GetZ();
-	#:: Create a proximity, 50 units across, 100 units tall, and enable proximity say
+	#:: Create a proximity, 50 units across, 50 units tall, enable proximity say
 	quest::set_proximity($x - 25, $x + 25, $y - 25, $y + 25, $z - 25, $z + 25, 1);
 }
 
@@ -38,48 +34,48 @@ sub EVENT_ITEM {
 		quest::say("Well it is about time!  The mayor gets very upset if he does not have the freshest of carrots in his stew.  Here is the money for the carrots.  Be off with you.  Now, where the heck did [" . quest::saylink("Jillin") . "] go?");
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(241, 1);		#:: + Deeppockets
+		quest::faction(223, 1);		#:: + Circle of Unseen Hands
+		quest::faction(292, -1);	#:: - Merchants of Rivervale
+		quest::faction(336, 1);		#:: + Coalition of Tradefolk Underground
+		quest::faction(329, 1);		#:: + Carson McCabe
 		#:: Grant a small amount of experience
 		quest::exp(100);
 		#:: Create a hash for storing cash - 2 to 8cp
 		my %cash = plugin::RandomCash(2,8);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions				
-		quest::faction(241, 1);		#:: + Deeppockets
-		quest::faction(292, 1);		#:: + Merchants of Rivervale
-		quest::faction(329, -2);		#:: - Carson McCabe
-		quest::faction(223, -2);		#:: - Circle of Unseen Hands
-		quest::faction(336, -2);		#:: - Coalition of Tradefolk Underground
 	}
 	#:: Match a 13957 - Crate of Fine Carrots
 	elsif (plugin::takeItems(13957 => 1)) {
 		quest::say("Oh excellent! These carrots are perfect! The finest Reebo has ever sent us. The mayor will be so pleased. Here is the payment for the carrots. Excuse me, but I must finish preparing the stew. Hmm. Where the heck did [" . quest::saylink("Jillin") . "] go?");
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(241, 5);		#:: + Deeppockets
+		quest::faction(223, 1);		#:: + Circle of Unseen Hands
+		quest::faction(292, -1);	#:: - Merchants of Rivervale
+		quest::faction(336, 1);		#:: + Coalition of Tradefolk Underground
+		quest::faction(329, 1);		#:: + Carson McCabe
 		#:: Grant a small amount of experience
 		quest::exp(200);
 		#:: Create a hash for storing cash - 10 to 20cp
 		my %cash = plugin::RandomCash(10,20);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions				
-		quest::faction(241, 5);		#:: + Deeppockets
-		quest::faction(292, 5);		#:: + Merchants of Rivervale
-		quest::faction(329, -10);	#:: - Carson McCabe
-		quest::faction(223, -10);	#:: - Circle of Unseen Hands
-		quest::faction(336, -10);	#:: - Coalition of Tradefolk Underground
 	}
 	#:: Match a 13971 - Crate of Rotten Carrots
 	elsif (plugin::takeItems(13971 => 1)) {
 		quest::say("What are these?!  I am trying to make stew for the mayor and you bring me ROTTEN CARROTS?!  Have you no sense??  Take these back to Reebo.");
 		#:: Give a 13972 - Crate of Rotten Carrots
 		quest::summonitem(13972);
-		#:: Set factions				
-		quest::faction(241, -4);		#:: + Deeppockets
-		quest::faction(292, -4);	#:: + Merchants of Rivervale
-		quest::faction(329, 2);		#:: - Carson McCabe
-		quest::faction(223, 2);		#:: - Circle of Unseen Hands
-		quest::faction(336, 2);		#:: - Coalition of Tradefolk Underground
+		#:: Set factions
+		quest::faction(241, -5);	#:: + Deeppockets
+		quest::faction(223, -1);	#:: + Circle of Unseen Hands
+		quest::faction(292, 1);		#:: - Merchants of Rivervale
+		quest::faction(336, -1);	#:: + Coalition of Tradefolk Underground
+		quest::faction(329, -1);	#:: + Carson McCabe
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
