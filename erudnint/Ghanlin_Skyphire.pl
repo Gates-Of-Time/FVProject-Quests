56169d0d36bf11160af19611a3383a14c4567418
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 14:08:57 -0400

    Update Ghanlin_Skyphire.pl

--- erudnint/Ghanlin_Skyphire.pl
+++ erudnint/Ghanlin_Skyphire.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Set up a 50 unit distance
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
@@ -14,19 +12,20 @@ sub EVENT_ENTER {
 	
 sub EVENT_ITEM {
 	#:: Match turn in for 18727 -  tattered note
-	if (plugin::check_handin(\%itemcount, 18727 => 1)) {
+	if (plugin::takeItems(18727 => 1)) {
 		quest::say("Greetings. I am Ghanlin Skyphire, Master Wizard of the Crimson Hands. All of us here have devoted our lives to the studies of the arcane and mystical. Let's get you started. Here's your training robe.  Now, go find Raskena. She'll help train you and give you your first lesson.");
 		#:: Give item 13550 - Old Used Robe*
 		quest::summonitem(13550);
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(233,100);		#:: + Crimson Hands
-		quest::faction(266,10);		#:: + High Council of Erudin
-		quest::faction(265,-15);	#:: - Heretics
-		quest::faction(267,15);		#:: + High Guard of Erudin
+		#:: Set factions
+		quest::faction(233, 100);	#:: + Crimson Hands
+		quest::faction(266, 10);	#:: + High Council of Erudin
+		quest::faction(265, -15);	#:: - Heretics
+		quest::faction(267, 15);	#:: + High Guard of Erudin
+		#:: Give a small amount of xp
+		quest::exp(100);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
