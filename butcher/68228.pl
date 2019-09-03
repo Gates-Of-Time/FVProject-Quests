af6909d4e47d9605a145056b8558ab8e1a2357bf
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 13:32:34 -0400

    Update 68228.pl

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -1,9 +1,9 @@
 sub EVENT_SPAWN {
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
+	#:: Move client to Ocean of Tears (oot)
 	quest::movepc(69,-9193.3,386.7,5.2);
 }
