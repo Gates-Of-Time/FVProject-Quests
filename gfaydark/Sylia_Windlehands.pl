e8d79f5e90f9e0396428748a13d4edbf8de332ed
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 14:33:08 -0400

    Update Sylia_Windlehands.pl

--- gfaydark/Sylia_Windlehands.pl
+++ gfaydark/Sylia_Windlehands.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Set a proximity, 100 units across
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
@@ -16,15 +14,15 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Salutations! The Song Weavers are always glad to add a new voice to the choir.  In addition to your voice. will you also [" . quest::saylink("fetch spiderling silk") . "]?  We need some to replace our worn lute strings.  Carry out this task in high tempo and we will show our gratitude.");
 	}
-	if ($text=~/fetch spiderling silk/i) {
+	elsif ($text=~/fetch spiderling silk/i) {
 		quest::say("Very spirited of you!!  Gather four spiderling silk and return them to me.  Good hunting, my friend!!");
 	}
-	if ($text=~/trades/i) {
+	elsif ($text=~/trades/i) {
 		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [" . quest::saylink("second book") . "], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
 		#:: Give a 51121 - Tradeskill Basics : Volume I
 		quest::summonitem(51121);
 	}
-	if ($text=~/second book/i) {
+	elsif ($text=~/second book/i) {
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
 		#:: Give a 51122 - Tradeskill Basics : Volume II
 		quest::summonitem(51122);
@@ -39,10 +37,10 @@ sub EVENT_ITEM {
 		quest::summonitem(13534);
 		#:: Ding!
 		quest::ding();
-		#:: Grant a small amount of experience
-		quest::exp(100);
 		#:: Set faction
 		quest::faction(401,100);	#:: + Song Weavers
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
 	#:: Match four 13099 - Spiderling Silk
 	if (plugin::takeItems(13099  => 4)) {
@@ -51,15 +49,15 @@ sub EVENT_ITEM {
 		quest::summonitem(13000);
 		#:: Ding!
 		quest::ding();
+		#:: Set faction
+		quest::faction(401,10);		#:: + Song Weavers
+		#:: Grant a small amount of experience
+		quest::exp(100);
 		#:: Create a hash for storing cash - 90 to 110cp
 		my %cash = plugin::RandomCash(40,60);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Grant a small amount of experience
-		quest::exp(100);
-		#:: Set faction
-		quest::faction(401,10);		#:: + Song Weavers
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
