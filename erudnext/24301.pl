2cb5cb741eab8c1ad093c08c444dd0d3ba333c35
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 13:30:42 -0400

    Update 24301.pl

--- erudnext/24301.pl
+++ erudnext/24301.pl
@@ -1,16 +1,9 @@
-# Zone to Erud Crossing event
-# Zone: Erudnext
-# AngeloX
-
-sub EVENT_SPAWN
-{
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
+sub EVENT_SPAWN {
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
-sub EVENT_ENTER
-{
-#	quest::selfcast("2279");
+sub EVENT_ENTER {
+	#:: Move client to Erud's Crossing (erudsxing)
 	quest::movepc(98,716.9,-1771.8,3.2);
 }
