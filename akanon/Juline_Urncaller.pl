ae5ec6181c1b0010299e9c4841a07dffb07fa039
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 14:53:47 -0400

    Update Juline_Urncaller.pl

--- akanon/Juline_Urncaller.pl
+++ akanon/Juline_Urncaller.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Set up a 50 unit distance
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
@@ -18,7 +16,7 @@ sub EVENT_SAY {
 		#:: Give a 51121 - Tradeskill Basics: Volume 1
 		quest::summonitem(51121); 
 	}
-	if ($text=~/second book/i) { 
+	elsif ($text=~/second book/i) { 
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
 		#:: Give a 51122 - Tradeskill Basics: Volume II
 		quest::summonitem(51122); 
@@ -27,19 +25,21 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	#:: Check for 18773 - Registration Letter
-	if (plugin::check_handin(\%itemcount, 18773 => 1)) {
+	if (plugin::takeItems(18773 => 1)) {
 		quest::say("Welcome! I am Juline Urncaller, I will help to guide you on your path to becoming a powerful enchanter. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of teh various [" . quest::saylink("trades") . "] you will have available to you.");
 		#:: Reward 13524 - Dark Gold Felt Robe*
 		quest::summonitem(13524);
-		#:: Set factions
-		quest::faction(239,-5); 	# The Dead
-		quest::faction(245,100); 	# Eldrich Collective
-		quest::faction(238,-15); 	# Dark Reflection
-		quest::faction(255,15); 	# Gem Choppers
-		quest::faction(333,15); 	# King Ak'Anon		
+		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(239, -5); 	#:: - The Dead
+		quest::faction(245, 100); 	#:: + Eldrich Collective
+		quest::faction(238, -15); 	#:: - Dark Reflection
+		quest::faction(255, 15); 	#:: + Gem Choppers
+		quest::faction(333, 15); 	#:: + King Ak'Anon		
+		#:: Grant a small amount of experience
 		quest::exp(100);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
