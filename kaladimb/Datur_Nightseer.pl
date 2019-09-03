31766b02bd63873f29aa57f54b3644eb17d19706
TurmoilToad <TurmoilToad@gmail.com>
2019-09-03 14:13:53 -0400

    Update Datur_Nightseer.pl

--- kaladimb/Datur_Nightseer.pl
+++ kaladimb/Datur_Nightseer.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Create a proximity, 100 units across
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
@@ -20,36 +18,36 @@ sub EVENT_ITEM {
 		quest::summonitem(13514);
 		#:: Ding!
 		quest::ding();
+		#:: Set Factions
+		quest::faction(227, 100); 	#:: + Clerics of Underfoot
+		quest::faction(274, 100); 	#:: + Kazon Stormhammer
+		quest::faction(293, 75); 	#:: + Miners Guild 249
 		#:: Grant a small amount of experience
 		quest::exp(100);
-		#:: Set Factions
-		quest::faction(227,100); 	#:: + Clerics of Underfoot
-		quest::faction(274,100); 	#:: + Kazon Stormhammer
-		quest::faction(293,75); 	#:: + Miners Guild 249
 	}
 	#:: Match a 12279 - Chalice Case
-	if (plugin::takeItems(12279 => 1)) {
+	elsif (plugin::takeItems(12279 => 1)) {
 		if ($race eq "Dwarf" && $class eq "Paladin") {
 			quest::say("The chalice is returned!! Praise be to Brell!! You have proven yourself to our church and have earned our respect. Let me welcome you into our brotherhood with the Cape of Underfoot. Wear it with pride as all of our finest paladins do.");
 			#:: Give a 12281 - Cape of Underfoot
 			quest::summonitem(12281);
 			#:: Ding!
 			quest::ding();
+			#:: Set Factions
+			quest::faction(227, 25);	#:: + Clerics of Underfoot
+			quest::faction(274, 25);	#:: + Kazon Stormhammer
+			quest::faction(293, 25);	#:: + Miners Guild 249
 			#:: Grant a small amount of experience
 			quest::exp(250);
 			#:: Create a hash for storing cash - 2500 to 3500cp
 			my %cash = plugin::RandomCash(2500,3500);
 			#:: Grant a random cash reward
 			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-			#:: Set Factions
-			quest::faction(227, 25);		#:: + Clerics of Underfoot
-			quest::faction(274, 25);	#:: + Kazon Stormhammer
-			quest::faction(293, 25);	#:: + Miners Guild 249
 		} 
 		else {
 			quest::say("$name, you are no Dwarf! Leave this place at once!");
 		}
 	}
-	#:: Return Unused Items
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
