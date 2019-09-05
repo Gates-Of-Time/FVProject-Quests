b90666e506054c99f87025fd3cffe390161400af
TurmoilToad <TurmoilToad@gmail.com>
2019-09-05 07:56:18 -0400

    Update Jakum_Webdancer.pl

--- gfaydark/Jakum_Webdancer.pl
+++ gfaydark/Jakum_Webdancer.pl
@@ -10,37 +10,37 @@ sub EVENT_ITEM {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(284, 10); 	#:: + League of Antonican Bards
+		quest::faction(281, 10); 	#:: + Knights of Truth
+		quest::faction(262, 10); 	#:: + Guards of Qeynos
+		quest::faction(304, -30); 	#:: - Ring of Scale
+		quest::faction(285, -30); 	#:: - Mayong Mistmoore
 		#:: Grant a small amount of experience
 		quest::exp(50);
 		#:: Create a hash for storing cash - 300 to 600cp
 		my %cash = plugin::RandomCash(300,600);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,10); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
 	#:: Match 18160 - Bardic Letter (Kelethin), 18162 - Bardic Letter (Kelethin), 18163 - Bardic Letter (Kelethin)
-	if (plugin::takeItems(18160 => 1) || plugin::takeItems(18162 => 1) || plugin::takeItems(18163 => 1)) {
+	elsif (plugin::takeItems(18160 => 1) || plugin::takeItems(18162 => 1) || plugin::takeItems(18163 => 1)) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(284, 10); 	#:: + League of Antonican Bards
+		quest::faction(281, 10); 	#:: + Knights of Truth
+		quest::faction(262, 10); 	#:: + Guards of Qeynos
+		quest::faction(304, -30); 	#:: - Ring of Scale
+		quest::faction(285, -30); 	#:: - Mayong Mistmoore
 		#:: Grant a small amount of experience
 		quest::exp(80);
 		#:: Create a hash for storing cash - 600 to 900cp
 		my %cash = plugin::RandomCash(600,900);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,10); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
