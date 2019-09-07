1e5b2c4064bf3d2c021cf2e9452f342ce4f77053
TurmoilToad <TurmoilToad@gmail.com>
2019-09-07 07:23:07 -0400

    Update Raz_The_Rat_Misk.pl

--- qeynos/Raz_The_Rat_Misk.pl
+++ qeynos/Raz_The_Rat_Misk.pl
@@ -10,7 +10,7 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
 	#:: Match five gold
 	if (plugin::takeCoin(0, 5, 0, 0) {
-		quest::ay("Very good, master! Best of luck in locating the proper owner. Maybe try giving it to ol' Crow in North Qeynos. Everyone knows if you need to fence.. er.. find something in Qeynos, Crow is your man. He always seems to know who's missing what. I am off to get some food.");
+		quest::say("Very good, master! Best of luck in locating the proper owner. Maybe try giving it to ol' Crow in North Qeynos. Everyone knows if you need to fence.. er.. find something in Qeynos, Crow is your man. He always seems to know who's missing what. I am off to get some food.");
 		#:: Give a 17600 - Tattered Leather Pouch
 		quest::summonitem(17600);
 		#:: Ding!
@@ -22,11 +22,12 @@ sub EVENT_ITEM {
 		quest::faction(262, -1);	#:: + Guards of Qeynos
 		quest::faction(273, 1);		#:: + Kane Bayle
 		quest::exp(200);
+	}
 	#:: Match 3 gold
 	elsif (plugin::takeCoin(0, 3, 0, 0) {
 		quest::say("Thank you, kind master. I don't wish to trouble you further but I do have another [" . quest::saylink("favor") . "] to ask..");
 		#:: Ding!
-		quest::ding()
+		quest::ding();
 		#:: Set factions
 		quest::faction(223, 1);		#:: + Circle of Unseen Hands
 		quest::faction(291, -1);	#:: - Merchants of Qeynos
