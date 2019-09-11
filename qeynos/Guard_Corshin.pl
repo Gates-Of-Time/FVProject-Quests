75a99e4f30b9fd8d3ff4ae90ab60a078a9fdbe10
TurmoilToad <TurmoilToad@gmail.com>
2019-09-11 13:03:15 -0400

    Update and rename Guard_Corshin.lua to Guard_Corshin.pl

---
+++ qeynos/Guard_Corshin.pl
@@ -0,0 +1,22 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail, $name!  Guard Corshin Kiniss, at your service. You should be careful in Qeynos after dark. We have had several [" . quest::saylink("bodies") . "] wash up under the docks over the past few weeks. We wouldn't want anything to happen to you.");
+		#:: Send a signal "5" to South Qeynos >> Guard_Beren (1090) with no delay
+		quest::signalwith(1090,5,0);
+	}
+	elsif ($text=~/bodies/i) {
+		quest::say("We think the bodies are washing out of the aqueducts beneath Qeynos. It is a dangerous place to go and Commander Bayle refuses to send regular patrols down there.");
+	}
+}
+
+sub EVENT_SIGNAL {
+	#:: Match signal from qeynos/Lieutenant_Arathur.pl
+	if ($signal == 1) {
+		quest::say("Yes, sir!");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
