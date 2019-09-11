67176a35b80059c66b8a9014046ca30ad52fcf6b
TurmoilToad <TurmoilToad@gmail.com>
2019-09-11 15:28:35 -0400

    Update Sunsa_Jocub.pl

--- qeynos/Sunsa_Jocub.pl
+++ qeynos/Sunsa_Jocub.pl
@@ -2,6 +2,8 @@ sub EVENT_SIGNAL {
 	#:: Match signal "1" from qeynos/Trumpy_Irontoe.pl
 	if ($signal == 1) {
 		quest::say("Trumpy, you are one sick little man!");
+		#:: Send a signal "3" to South Qeynos >> Trumpy_Irontoe (1042) with no delay
+		quest::signalwith(1042,3,0);
 	}
 }
 
