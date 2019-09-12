3bc58dbc22cdcd124088ecbf048d0d067c7c5b87
TurmoilToad <TurmoilToad@gmail.com>
2019-09-12 10:35:49 -0400

    Update Sorsha_Hobbitfriend.pl

--- commons/Sorsha_Hobbitfriend.pl
+++ commons/Sorsha_Hobbitfriend.pl
@@ -3,16 +3,16 @@ $y_offset = 0;
 
 sub EVENT_SPAWN {
 	quest::say("Spawned!");
-	quest::settimer("spawnanother",5);
+	quest::settimer("timer",5);
 	quest::say("Timer Set!");
 }
 
 sub EVENT_TIMER {
-	if ($timer eq "spawnanother") {
+	if ($timer eq "timer") {
 		quest::say("Timer!");
-		$x_offset++;
-		$y_offset++;
-		quest::spawn2(21059,0,$x + $x_offset,$y + $y_offset,$z,0);
+		++$x_offset;
+		++$y_offset;
+		quest::spawn2(21059, 0, $x + $x_offset, $y + $y_offset, $z, 0);
 		quest::npcfeature("texture",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
 		quest::npcfeature("helm",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
 		quest::npcfeature("haircolor",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
