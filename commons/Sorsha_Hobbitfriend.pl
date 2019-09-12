b3c9917a12cafe441f231b3898d44b39394dd494
TurmoilToad <TurmoilToad@gmail.com>
2019-09-12 11:07:25 -0400

    Update Sorsha_Hobbitfriend.pl

--- commons/Sorsha_Hobbitfriend.pl
+++ commons/Sorsha_Hobbitfriend.pl
@@ -6,17 +6,17 @@ sub EVENT_SPAWN {
 
 sub EVENT_TIMER {
 	if ($timer eq "timer") {
-		quest::spawn2(21059, 0, $x + 10, $y + 10, $z, 0);
-		my $Number = int(rand(100));
-		quest::say("$Number");
-		quest::npcfeature("texture",$Number);
-		quest::npcfeature("helm",$Number);
-		quest::npcfeature("haircolor",$Number);
-		quest::npcfeature("beardcolor",$Number);
-		quest::npcfeature("eyecolor1",$Number);
-		quest::npcfeature("eyecolor2",$Number);
-		quest::npcfeature("hair",$Number);
-		quest::npcfeature("face",$Number);
-		quest::npcfeature("beard",$Number);
+		my $random = int(rand(100));
+		quest::say("My random number is $random.");
+		quest::spawn2(21059, 0, 0, $x + $random, $y + $random, $z, 0);
+		quest::npcfeature("texture",$random);
+		quest::npcfeature("helm",$random);
+		quest::npcfeature("haircolor",$random);
+		quest::npcfeature("beardcolor",$random);
+		quest::npcfeature("eyecolor1",$random);
+		quest::npcfeature("eyecolor2",$random);
+		quest::npcfeature("hair",$random);
+		quest::npcfeature("face",$random);
+		quest::npcfeature("beard",$random);
 	}
 }
