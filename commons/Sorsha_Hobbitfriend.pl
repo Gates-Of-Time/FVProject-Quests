7ed812b8eb08cba3281bbdf47105e6f515aa5cee
TurmoilToad <TurmoilToad@gmail.com>
2019-09-12 10:39:50 -0400

    Update Sorsha_Hobbitfriend.pl

--- commons/Sorsha_Hobbitfriend.pl
+++ commons/Sorsha_Hobbitfriend.pl
@@ -13,14 +13,14 @@ sub EVENT_TIMER {
 		#++$x_offset;
 		#++$y_offset;
 		#quest::spawn2(21059, 0, $x + $x_offset, $y + $y_offset, $z, 0);
-		quest::npcfeature("texture",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("helm",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("haircolor",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("beardcolor",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("eyecolor1",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("eyecolor2",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("hair",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("face",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
-		quest::npcfeature("beard",quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10));
+		quest::npcfeature("texture",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("helm",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("haircolor",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("beardcolor",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("eyecolor1",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("eyecolor2",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("hair",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("face",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
+		#quest::npcfeature("beard",(quest::ChooseRandom(1,2,3,4,5,6,7,8,9,10)));
 	)
 }
