50dad7b09002bd4d6697c90eb418f50caaf1f9ce
TurmoilToad <TurmoilToad@gmail.com>
2019-09-14 09:40:24 -0400

    Update and rename Mespha_Tevalian.lua to Mespha_Tevalian.pl

---
+++ qeynos/Mespha_Tevalian.pl
@@ -0,0 +1,48 @@
+sub EVENT_SPAWN {
+	#:: Create a proximity, 50 units across, 50 units tall, without proximity say
+	quest::set_proximity($x - 25, $x + 25, $y - 25, $y + 25, $z - 25, $z + 25, 0);
+}
+
+sub EVENT_ENTER {
+	#:: Match a 18720 - Tattered Note
+	if (plugin::check_hasitem($client, 18718)) {
+		$client->Message(15,"As you glance about the room, Mespha Tevalian greets you. 'Why hello there. Are you perhaps a new recruits? If you wish to learn the ways of the Enchanter, read the note in your inventory and hand me your note to start your training.'");
+	}
+}
+
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings.. I am Mespha Tevalian, Master Enchantress of the Hall of Sorcery and the Order of Three. Here, we all study the three circles of magic, and lend our services to citizens of Qeynos. Rumors of an underground group calling themselves the Bloodsabers have recently been spreading throughout the city. It is our duty to help local guards protect Qeynos' citizens from this new evil.");
+	}
+	elsif ($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [" . quest::saylink("second book") . "], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		#:: Give a 51121 - Tradeskill Basics : Volume I
+		quest::summonitem(51121);
+	}
+	elsif ($text=~/second book/i) {
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		#:: Give a 51122 - Tradeskill Basics : Volume II
+		quest::summonitem(51122);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 18720 - Tattered Note
+	if (plugin::takeItems(18720 => 1)) {
+		quest::say("Greetings $name. Welcome to the Hall of Sorcery. You have much to learn. Wear this tunic and represent us well. See my apprentice, Unsar Koldhagon, he will get you started and help you with your studies. Once you are ready to begin your hunting training please see Juegile Sohgohm, he will assist you in some tasks for your early adventures. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [" . quest::saylink("trades") . "] you will have available to you.");
+		#:: Give a 13542 - Faded Blue Robe*
+		quest::summonitem(13542);
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(342, 100);		#:: + Order of Three
+		quest::faction(221, -25);		#:: - Bloodsabers
+		quest::faction(262, 15);		#:: + Guards of Qeynos
+		quest::faction(291, 15);		#:: + Merchants of Qeynos
+		quest::faction(296, -15);		#:: - Opal Darkbriar
+		#:: Grant a small amount of experience
+		quest::exp(100);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
