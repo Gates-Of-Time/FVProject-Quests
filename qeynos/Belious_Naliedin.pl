ed885066b33bb750d6689923ac00f6219aa17ff3
TurmoilToad <TurmoilToad@gmail.com>
2019-09-14 08:56:36 -0400

    Update Belious_Naliedin.pl

--- qeynos/Belious_Naliedin.pl
+++ qeynos/Belious_Naliedin.pl
@@ -1,8 +1,4 @@
 sub EVENT_SPAWN {
-	#:: Get current location
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	$z = $npc->GetZ();
 	#:: Create a proximity, 50 units across, 50 units tall, without proximity say
 	quest::set_proximity($x - 25, $x + 25, $y - 25, $y + 25, $z - 25, $z + 25, 0);
 }
