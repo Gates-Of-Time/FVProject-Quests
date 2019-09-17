02b808e8a33672829dad0814fb86aaabb7a8d6f3
TurmoilToad <TurmoilToad@gmail.com>
2019-09-17 13:12:17 -0400

    Update Guard_Wenbie.pl

--- qeynos/Guard_Wenbie.pl
+++ qeynos/Guard_Wenbie.pl
@@ -9,7 +9,7 @@ sub EVENT_TIMER {
 }
 
 sub EVENT_WAYPOINT_DEPART {
-	if ($following == 1) {
+	if ($following == "1") {
 		quest::stop();
 		if ($wenbie == 47) {
 			quest::moveto(-18.6621, 63.5473, 3.127);
