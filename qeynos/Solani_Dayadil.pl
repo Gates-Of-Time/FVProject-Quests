3007dacba9e32f833df5f41ccf76055bbf0f9cc0
TurmoilToad <TurmoilToad@gmail.com>
2019-09-24 19:00:39 -0400

    Update and rename Solani_Dayadil.lua to Solani_Dayadil.pl

---
+++ qeynos/Solani_Dayadil.pl
@@ -0,0 +1,28 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("If you are a fellow merchant. I must warn you of the inevitable tax increase.  Antonius Bayle is soon to make a [" . quest::saylink("deal") . "] with the people of Surefall Glade.");
+	}
+	elsif ($text=~/deal/i) {
+		quest::say("Bayle is soon to sign a law which will raise the taxes on all bear hides.  This is to keep the purchase of the hides to a minimum.  All this to keep the treehuggers happy!!");
+	}
+}
+
+sub EVENT_SIGNAL {
+	#:: Match signal "1" from qeynos/campnight.pl
+	if ($signal == 1) {
+		#:: Stop pathing grid 59
+		quest::stop();
+		#:: Missing equivalent perl function for lua e.self:MerchantCloseShop();
+	}
+	#:: Match signal "2" from qeynos/campday.pl
+	elsif ($signal == 2) {
+		#:: Start pathing on grid 59
+		quest::start(59);
+		#:: Missing equivalent perl function for lua e.self:MerchantOpenShop();
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
