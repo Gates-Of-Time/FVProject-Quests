b0c6b335c11e97c40e7528e69d60b577f5587aa6
TurmoilToad <TurmoilToad@gmail.com>
2019-09-25 10:54:22 -0400

    Update and rename Lomarc.lua to Lomarc.pl

---
+++ qeynos/Lomarc.pl
@@ -0,0 +1,15 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Do I know you? Don't stick your nose where it don't belong. It might just get cut off.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 18793 - Lomarc's Payment
+	if (plugin::takeItems(18793 => 1) {
+		quest::say("What the heck is this?!? No money at all? You'll pay for this you back-stabbing punk!");
+		quest::attack($name);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
