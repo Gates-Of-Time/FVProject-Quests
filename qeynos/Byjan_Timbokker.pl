f19738e3e39d101b7b1fb16c5da8104bb08c6880
TurmoilToad <TurmoilToad@gmail.com>
2019-09-25 13:30:01 -0400

    Update Byjan_Timbokker.pl

--- qeynos/Byjan_Timbokker.pl
+++ qeynos/Byjan_Timbokker.pl
@@ -3,3 +3,8 @@ sub EVENT_SAY {
 		quest::say("Greetings!  Please do not ask me to donate to the Knights of Truth or the Priests of Life.  I have enough gold going to the city of Qeynos with all these new taxes!");
 	}
 }
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
