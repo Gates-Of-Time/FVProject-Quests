5dcb96bec6004dc86dfb3c3194692d6f3a259ec7
TurmoilToad <TurmoilToad@gmail.com>
2019-09-25 08:40:58 -0400

    Update and rename Lanhern_Firepride.lua to Lanhern_Firepride.pl

---
+++ qeynos/Lanhern_Firepride.pl
@@ -0,0 +1,18 @@
+#:: See also:  qeynos/#Lanhern_Firepride.pl
+
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Good day, traveler! You look as though you are in need of [" . quest::saylink("superior armor") . "]. We here at Firepride's specialized in armor.");
+	}
+	elsif ($text=~/superior armor/i) {
+		quest::say("Our forge skills were acquired from the armor lords of Kaladim, as were our [" . quest::saylink("drinking skills") . "].");
+	}
+	elsif ($text=~/drinking skills/i) {
+		quest::say("I learned the love of ale from the dwarves of Kaladim. Maybe someday I sall see you in the backroom at Fish's. If I do, don't forget to buy me an Ogre Swill.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
