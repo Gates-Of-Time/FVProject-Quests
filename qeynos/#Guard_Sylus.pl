3106415be6ef1485ba0766680c31588bbac95190
TurmoilToad <TurmoilToad@gmail.com>
2019-09-25 15:38:54 -0400

    Delete #Guard_Sylus.pl

--- qeynos/#Guard_Sylus.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_WAYPOINT_ARRIVE {
-	#:: Match waypoint 4
-	if ($wp == 4) {
-		quest::say("Come along, prisoner.");
-		#:: Send a signal "1" to South Qeynos >> #Riley_Shplots (1183) with no delay
-		quest::signalwith(1183,1,0);
-	}
-}
