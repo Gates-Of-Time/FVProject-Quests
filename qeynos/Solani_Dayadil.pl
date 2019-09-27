13eb7bff5c46aa19cdb12066b0ee14703d2e3065
TurmoilToad <TurmoilToad@gmail.com>
2019-09-27 08:00:58 -0400

    Update Solani_Dayadil.pl

--- qeynos/Solani_Dayadil.pl
+++ qeynos/Solani_Dayadil.pl
@@ -12,13 +12,15 @@ sub EVENT_SIGNAL {
 	if ($signal == 1) {
 		#:: Stop pathing grid 59
 		quest::stop();
-		#:: Missing equivalent perl function for lua e.self:MerchantCloseShop();
+		#:: Close up shop for the night
+		$npc->MerchantCloseShop();
 	}
 	#:: Match signal "2" from qeynos/campday.pl
 	elsif ($signal == 2) {
 		#:: Start pathing on grid 59
 		quest::start(59);
-		#:: Missing equivalent perl function for lua e.self:MerchantOpenShop();
+		#:: Open up shop for the day
+		$npc->MerchantOpenShop();
 	}
 }
 
