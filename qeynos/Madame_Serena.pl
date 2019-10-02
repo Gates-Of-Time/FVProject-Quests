0655358bc02c29a59d7fba3171596387e9929773
TurmoilToad <TurmoilToad@gmail.com>
2019-10-02 14:52:01 -0400

    Update Madame_Serena.pl

--- qeynos/Madame_Serena.pl
+++ qeynos/Madame_Serena.pl
@@ -4,7 +4,7 @@ sub EVENT_SPAWN {
     #:: Set gender 1 - Female
     quest::npcgender(1);
     #:: Set Texture 1 - Gypsy Clothes
-    quest::texture(4);
+    quest::texture(5);
 }
 
 sub EVENT_SAY {
