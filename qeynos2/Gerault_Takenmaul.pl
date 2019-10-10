6986bfb1ce3ee3e8f17155c11f39d6cdc22619b0
TurmoilToad <TurmoilToad@gmail.com>
2019-10-10 11:39:51 -0400

    Update Gerault_Takenmaul.pl

--- qeynos2/Gerault_Takenmaul.pl
+++ qeynos2/Gerault_Takenmaul.pl
@@ -2,7 +2,7 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hello.  Please feel free to browse.  We make the finest boots in all of Antonica.");
 	}
-	if ($text=~/treant wood/i) {
+	elsif ($text=~/treant wood/i) {
 		quest::say("Looking for Treant material? I do not work with such material. Ask Rodgar. I believe he is upstairs.");
 	}
 }
