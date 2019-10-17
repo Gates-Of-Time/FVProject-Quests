7a476d3fe8628904665c22a219cee95fedf4b6a8
TurmoilToad <TurmoilToad@gmail.com>
2019-10-17 13:07:55 -0400

    Update Hurrieta_Ironforge.pl

--- qeynos2/Hurrieta_Ironforge.pl
+++ qeynos2/Hurrieta_Ironforge.pl
@@ -1,11 +1,14 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("What are you doing back here?! I cannot stand it when the children allow their friends to run rampant on our estate. Please leave!");
+		quest::say("What are you doing back here?! I cannot stand it when the [" . quest::saylink("children") . "] allow their friends to run rampant on our estate. Please leave!");
+	}
+	elsif ($text=~/children/i) {
+		quest::say("My dear sweet children are working in the storefronts. Well, all of them except for [" . quest::saylink("Tayla") . "].");
 	}
 	elsif ($text=~/tayla/i) {
 		quest::say("Tayla! She is no Ironforge.");
 	}
-	elsif ($text=~/weleana/i) {
+	elsif ($text=~/waleana/i) {
 		quest::say("Waleana is a stain on Brohan's life. A horrid elf. She is gone now and Brohan has moved onto better things and more sanity.");
 	}
 }
