230a751afd6567817bdea0740d0e3b23c6c832a3
TurmoilToad <TurmoilToad@gmail.com>
2019-10-17 14:48:14 -0400

    Update and rename Tyokan_Mekase.lua to Tyokan_Mekase.pl

---
+++ qeynos2/Tyokan_Mekase.pl
@@ -0,0 +1,36 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Please look around. We have many items exclusively for the members of the Temple of Life. We also have [" . quest::saylink("scroll strongboxes") . "]. If you are unsure of what an item is, feel free to ask me.");
+	}
+	elsif ($text=~/scroll strongbox/i) {
+		quest::say("The scroll strongboxes are merely personal safes. Most of the members use them. I am afraid all are taken, but if you are here to remove a scroll, merely hand me your key and I shall get it for you. I handle all 20 numbered keys and [" . quest::saylink("Whysia") . "] handles all 30 and 40 numbered keys.");
+	}
+	elsif ($text=~/whysia/i) {
+		quest::say("Whysia Flock is the night merchant here at the temple shop. She usually arrives around eight at night or so.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 13296 - Prayer Beads and 100 gp
+	if (plugin::takeItemsCoin(0,0,100,0, 13296 => 1)) {
+		quest::say("I see your beads need to be charged. Very Well here you are. Use them and good health to you!!!");
+		#:: Give a 13296 - Prayer Beads
+		quest::summonitem(13296);
+	}
+	#:: Match a 13296 - Prayer Beads
+	elsif (plugin::takeItems(13296 => 1)) {
+		quest::say("As instructed by High Preistess Jahnds I must ask for the beads and a donation of 100 gold coins.");
+		#:: Give a 13296 - Prayer Beads, no charges
+		quest::summonitem(13296,0);
+	}
+	#:: Match a 13306 - T.O.L. 2020
+	elsif (plugin::takeItems(13306 => 1)) {
+		quest::say("Oh, turning in your key, are you? Very well, defender of life. Here you are.");
+		#:: Give a random reward: 15126 - Spell: Inspire Fear or 15248 - Spell: Ward Summoned
+		quest::summonitem(quest::ChooseRandom(15126, 15126, 15248));
+		#:: Ding!
+		quest::ding();
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
