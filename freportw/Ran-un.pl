ec7dbd53996a0c67c8e2f4ab323ce0bc09ac576e
TurmoilToad <TurmoilToad@gmail.com>
2019-10-20 17:09:49 -0400

    Update Ran-un.pl

--- freportw/Ran-un.pl
+++ freportw/Ran-un.pl
@@ -1,11 +1,16 @@
 sub EVENT_SPAWN {
-	#:: Create a timer to trigger every 5 seconds
-	quest::settimer("spar1",5);
+	#:: Create a timer that loops every 5 seconds
+	quest::settimer("spar1", 5);
 }
 
 sub EVENT_TIMER {
-	#:: Match "Kungfu" every five seconds
+	#:: Match the timer "spar1"
 	if ($timer eq "spar1") {
-	quest::doanim(quest::ChooseRandom (1,5,6,7,11,30,45,46,47));
+		quest::doanim(quest::ChooseRandom(1, 5, 6, 7, 11, 30, 45, 46, 47));
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
