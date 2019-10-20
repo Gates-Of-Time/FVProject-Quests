fedf3b99b581a8c3b09c144f987e6a1597a7ae86
TurmoilToad <TurmoilToad@gmail.com>
2019-10-20 13:03:29 -0400

    Update Larn_Brugal.pl

--- freportw/Larn_Brugal.pl
+++ freportw/Larn_Brugal.pl
@@ -1,22 +1,23 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Good day to you $Name I hope you yearn to become a Steel Warrior as we all are here in the Bunker. We are in need of more recruits to continue the [" . quest::saylink("war") . "].");
+		quest::say("Good day to you $name I hope you yearn to become a Steel Warrior as we all are here in the Bunker. We are in need of more recruits to continue the [" . quest::saylink("war") . "].");
 	}
-	elsif($text=~/war/i) {
+	elsif ($text=~/war/i) {
 		quest::say("The war I speak of is the one we Steel Warriors have sworn to wage agianst [" . quest::saylink("Clan Deathfist") . "] in the Commonlands. We are doing a fine job but we require more weapons. We are expecting a shipment of blades from Groflahs Forge. We require a warrior of the bunker to [" . quest::saylink("get our shipment") . "]. Maybe you?");
 	}
-	elsif($text=~/Clan Deathfist/i) {
+	elsif ($text=~/clan deathfist/i) {
 		quest::say("If you do not know of Clan Deathfist go ask Cain who they are. He will not only tell you he will sign you up to help fight our war!");
 	}
-	elsif($text=~/shipment/i) {
+	elsif ($text=~/shipment/i) {
 		#:: Match if Class = Warrior
 		if ($class == 1) {
 			quest::say("We would be most thankful for your service. Please take this voucher over to Groflah at Groflahs Forge in North Freeport. He will give you the shipment of weapons.");
 			#:: Give item 18820 - Sealed Letter
 			quest::summonitem(18820);
-			}
-			else {
+		}
+		else {
 			quest::say("Only a warrior would be capable of this service, but thanks for offering.");
+		}
 	}
 }
 
@@ -28,10 +29,9 @@ sub EVENT_ITEM {
 		quest::ding();
 		#:: Give item 5418 - Groflah's Stoutbite
 		quest::summonitem(5418);
-		#:: Give a small amount of xp
+		#:: Give a small amount of experience
 		quest::exp(500);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-}	
