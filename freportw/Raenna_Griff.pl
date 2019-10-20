25d3b1cd5ad7d3a4f524c8daa9b7727ac756b11d
TurmoilToad <TurmoilToad@gmail.com>
2019-10-20 15:40:15 -0400

    Update Raenna_Griff.pl

--- freportw/Raenna_Griff.pl
+++ freportw/Raenna_Griff.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hail $Name. If it is refuge you seek from the evil that is corrupting this city then I suggest you flee to Qeynos or take up arms along side the Sentries of Passion and the Knights of Truth. You must have faith that the Marr twins will not let this city fall to the hate of the Dismal Rage.");
+		quest::say("Hail $name. If it is refuge you seek from the evil that is corrupting this city then I suggest you flee to Qeynos or take up arms along side the Sentries of Passion and the Knights of Truth. You must have faith that the Marr twins will not let this city fall to the hate of the Dismal Rage.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
