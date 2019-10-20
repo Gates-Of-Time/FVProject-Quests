de043e919343dca630de16c29fc29dc786ef7207
TurmoilToad <TurmoilToad@gmail.com>
2019-10-20 14:32:01 -0400

    Update Opal_Darkbriar.pl

--- freportw/Opal_Darkbriar.pl
+++ freportw/Opal_Darkbriar.pl
@@ -1,18 +1,25 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings! You will have to excuse me. I have no time to talk right now and I prefer to be left alone. Please leave.");
+	}
+}
+
 sub EVENT_ITEM {
-	#:: Turn in for 18739 -  A tattered note
-	if (plugin::check_handin(\%itemcount, 18739 => 1)) {
+	#:: Match a 18739 - Tattered Note
+	if (plugin::takeItems(18739 => 1)) {
 		quest::say("Welcome to the Academy of Arcane Sciences. Here's one of our guild robes for you to wear. Now, let's get to work.");
-		#:: Give item 13558 - Patched Violet Robe
+		#:: Give a 13558 - Patched Violet Robe
 		quest::summonitem(13558);
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(296,-15); 	#:: - Opal Dark Briar
-		quest::faction(220,100); 	#:: + Arcane Scientists
-		quest::faction(281,25); 	#:: + Knights of Truth
-		quest::faction(330,-15); 	#:: - The Freeport Militia
+		#:: Set factions
+		quest::faction(296, -15); 		#:: - Opal Dark Briar
+		quest::faction(220, 100); 		#:: + Arcane Scientists
+		quest::faction(281, 25); 		#:: + Knights of Truth
+		quest::faction(330, -15); 		#:: - The Freeport Militia
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
