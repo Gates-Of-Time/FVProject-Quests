abf3aac807b67e1a681fdc780f88e8a1e343a64b
TurmoilToad <TurmoilToad@gmail.com>
2019-10-20 14:36:25 -0400

    Update Palon_Deskeb.pl

--- freportw/Palon_Deskeb.pl
+++ freportw/Palon_Deskeb.pl
@@ -2,18 +2,18 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Pleased to meet you. $Name. Have you seen how clear the water is underneath the Academy? All sorts of life could flourish there. What a shame there are no [" . quest::saylink("Marr Minnows") . "] there.");
 	}
-	elsif($text=~/Marr Minnows/i) {
+	elsif($text=~/marr minnows/i) {
 		quest::say("The Marr Minnow swims in the pond near the Temple of Marr. I wish I had one. Not a dead one. A live one. I need someone to [" . quest::saylink("get the minnow") . "].");
 	}
 	elsif($text=~/get the minnow/i) {
 		quest::say("Please try. Here you are. Take this jar. Offer the jar to the minnows. Maybe they will swim into it.");
-			#:: Give item 13861 - Jar of Liquid
-			quest::summonitem(13861);
+		#:: Give item 13861 - Jar of Liquid
+		quest::summonitem(13861);
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13862 - Fish in a Jar
+	#:: Match a 13862 - Fish in a Jar
 	if (plugin::takeItems(13862 => 1 )) {
 		#:: Ding!
 		quest::ding();
@@ -23,4 +23,3 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-	
