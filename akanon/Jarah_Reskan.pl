d82cbf0b4aae7ff488a0113f84d44111b0b0b07a
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 15:59:15 -0400

    Update Jarah_Reskan.pl

--- akanon/Jarah_Reskan.pl
+++ akanon/Jarah_Reskan.pl
@@ -2,41 +2,41 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Greetings, child. Welcome to Gemchopper Hall. I trust that you are a [" . quest::saylink("warrior") . "], or perhaps you are [" . quest::saylink("lost") . "]..?");
 	}
-	if ($text=~/lost/i) {
+	elsif ($text=~/lost/i) {
 		quest::say("One can easily get lost in Ak'Anon. You must get your vision checked. If you are truly lost, I would advise speaking with a mechanical guide. There should be one close by on the main walkway.");
 	}
-	if ($text=~/warrior/i) {
+	elsif ($text=~/warrior/i) {
 		quest::say("That is good news! We gnomes are not known for our love of battle so it is always good to bring new blood into our ranks. We shall prove our worth as warriors to all other races. For now. there is much to do in Ak'Anon. There are [" . quest::saylink("rogue clockworks") . "] and the [" . quest::saylink("cargo clockwork") . "].");
 	}
-	if ($text=~/rogue clockworks/i) {
+	elsif ($text=~/rogue clockworks/i) {
 		quest::say("The clockworks are the responsibility of Manik Compolten. Speak with him - I am sure he is here somewhere.");
 	}
-	if ($text=~/cargo clockwork/i) {
+	elsif ($text=~/cargo clockwork/i) {
 		quest::say("The cargo clockwork is located near the entrance to Ak'Anon in the Steamfont Mountains. It runs a delivery to the windmills every five days at eight in the morning. There have been attacks by a group of highwaymen. They are quite a formidable group. I have offered a reward for their heads. Be very careful if you plan on escorting the cargo clockwork. This trio of bandits is very strong.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 85058 -  Highway Protectors Mask
-	if (plugin::check_handin(\%itemcount, 85056 => 1, 85055 => 1, 85057 =>1)) { #:: Hector's Severed Head, Renaldo's Severed Head, Jerald's Severed Head
+	#:: Match a 85056 - Hector's Severed Head, 85055 - Renaldo's Severed Head, and a 85057 - Jerald's Severed Head
+	if (plugin::takeItems(85056 => 1, 85055 => 1, 85057 => 1)) {
 		quest::say("I heard our shipment made it back safely. These heads will send a message to any other thief that plans on robbing our cargo shipments. Thank you $name, take this mask and this coin as your reward.");
-		#:: Give item 85058 -  Highway Protectors Mask
+		#:: Give a 85058 - Highway Protectors Mask
 		quest::summonitem(85058);
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(255, 15); 		#:: + Gem Cutters
+		quest::faction(288, 15); 		#:: + Merchants of Ak'Anon
+		quest::faction(333, 15); 		#:: + King Ak'Anon
+		quest::faction(238, -15); 		#:: - Dark Reflection
+		quest::faction(1604, -15); 		#:: - Clan Grikbar
+		#:: Grant a moderate amount of experience
 		quest::exp(6000);
 		#:: Create a hash for storing cash - 300 to 350cp
 		my %cash = plugin::RandomCash(300,350);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
-		quest::faction(255,15); 	#:: + Gem Cutters
-		quest::faction(288,15); 	#:: + Merchants of Ak'Anon
-		quest::faction(333,15); 	#:: + King Ak'Anon
-		quest::faction(238,-15); 	#:: - Dark Reflection
-		quest::faction(1604,-15); 	#:: - Clan Grikbar
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
