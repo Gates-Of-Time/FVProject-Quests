474d25c873574f3138c6612bfa36184e4a0a92d5
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 14:33:18 -0400

    Update Clockwork_XIIC.pl

--- akanon/Clockwork_XIIC.pl
+++ akanon/Clockwork_XIIC.pl
@@ -1,19 +1,20 @@
 sub EVENT_ITEM {
-	#:: Turn in for 13844 -  Shiny Card for Series C Black Boxes Quest
-	if (plugin::check_handin(\%itemcount, 13844 => 1)) {
+	#:: Match a 13844 - Shiny Card (for Series C Black Boxes Quest)
+	if (plugin::takeItems(13844 => 1)) {
 		quest::say("Tick.. Pop.. Access granted.");
-		#:: Give item 13212 - Blackbox XIIC
+		#:: Give a 13212 - Blackbox XIIC
 		quest::summonitem(13212);
-		#:: Give a small amount of XP
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(255,1); 		#:: + Gem Cutters
-		quest::faction(245,1); 		#:: + Eldritch Collective
-		quest::faction(333,1); 		#:: + King Ak'Anon
-		quest::faction(238,-1); 		#:: - Dark Reflection
-		quest::faction(287,-1); 	#:: - Clan Grikbar
+		#:: Set factions
+		quest::faction(255, 1); 		#:: + Gem Cutters
+		quest::faction(245, 1); 		#:: + Eldritch Collective
+		quest::faction(333, 1); 		#:: + King Ak'Anon
+		quest::faction(238, -1); 		#:: - Dark Reflection
+		quest::faction(287, -1); 		#:: - Clan Grikbar
+		#:: Give a small amount of XP
+		quest::exp(100);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
