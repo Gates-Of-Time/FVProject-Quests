2c75fcbf6f05ef28cfe6750eb4e44ac7c443912b
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 16:12:57 -0400

    Update Lyra_Lyrestringer.pl

--- akanon/Lyra_Lyrestringer.pl
+++ akanon/Lyra_Lyrestringer.pl
@@ -2,15 +2,20 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hail $name. Are you [" . quest::saylink("interested") . "] in helping the League of Antonican Bards by delivering some [" . quest::saylink("mail") . "]?");
 	}
-	if ($text=~/mail/i) {
+	elsif ($text=~/mail/i) {
 		quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers. We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices. Are you [" . quest::saylink("interested") . "]?");
 	}
-	if ($text=~/interested/i) {
+	elsif ($text=~/interested/i) {
 		quest::say("I have messages that need to go to... well, right now I have one that needs to go to Kelethin. Will you deliver mail to [" . quest::saylink("Kelethin") . "] for me?");
 	}
-	if ($text=~/Kelethin/i) {
+	elsif ($text=~/Kelethin/i) {
 		quest::say("Take this letter to Jakum Webdancer in Kelethin. You can find him at the bard guild hall. I am sure he will compensate you for your troubles.");
 		#:: Give a 18163 - Bardic Letter (Kelethin)
 		quest::summonitem(18163);
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
