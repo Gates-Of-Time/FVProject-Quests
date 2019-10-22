8823d95d14dcca628f246608ed8cc548a66cce61
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 14:34:46 -0400

    Update a_clockwork_receptionist.pl

--- akanon/a_clockwork_receptionist.pl
+++ akanon/a_clockwork_receptionist.pl
@@ -1,9 +1,10 @@
 sub EVENT_SAY {
-     if ($text=~/king akanon/i) {
-        quest::emote("Brell strike you down for not knowing!!  Who could not have heard of the name of his royal highness King Horatio Ak'Anon. This great city is named after him. He rules from his throne room in Ak'Anon Palace which he constructed upon the underground lake.");
-     }
+	if ($text=~/king akanon/i) {
+		quest::say("Brell strike you down for not knowing!!  Who could not have heard of the name of his royal highness King Horatio Ak'Anon. This great city is named after him. He rules from his throne room in Ak'Anon Palace which he constructed upon the underground lake.");
+	}
 }
+
 sub EVENT_ITEM {
-    
-    plugin::return_items(\%itemcount); # return unused items
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
