0fbc25d401cb19dde8208d30d8e5c1eccf0b1489
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 18:28:04 -0400

    Update Zenrel_Ottonoggin.pl

--- akanon/Zenrel_Ottonoggin.pl
+++ akanon/Zenrel_Ottonoggin.pl
@@ -1,9 +1,10 @@
 sub EVENT_SAY {
-     if ($text=~/hail/i) {
-        quest::emote("You look like you could use a copy of the Tinker's News and World report $name. Find out everything that is happening both below ground and above. Buy a copy now. It's cheap.");
-     }
+	if ($text=~/hail/i) {
+		quest::say("You look like you could use a copy of the Tinker's News and World report $name. Find out everything that is happening both below ground and above. Buy a copy now. It's cheap.");
+	}
 }
+
 sub EVENT_ITEM {
-    
-    plugin::return_items(\%itemcount); # return unused items
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
