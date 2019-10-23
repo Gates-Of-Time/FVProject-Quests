a65196cec10eaa12716238847d9b46046a4bf3a5
TurmoilToad <TurmoilToad@gmail.com>
2019-10-22 20:29:04 -0400

    Update #a_zombie.pl

--- qcat/#a_zombie.pl
+++ qcat/#a_zombie.pl
@@ -1,10 +1,12 @@
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 18804 => 1)) {
-    quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 18804 - Tattered Note
+	if (plugin::takeItems(18804 => 1)) {
+		quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::say("ARGH!!!!!.. Thank.. you..");
+	quest::say("ARGH!!!!!.. Thank.. you..");
 }
