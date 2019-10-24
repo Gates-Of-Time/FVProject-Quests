671f7b5124edea73aed7e0760448466c160c795f
TurmoilToad <TurmoilToad@gmail.com>
2019-10-23 20:37:25 -0400

    Update Ronn_Castekin.pl

--- qcat/Ronn_Castekin.pl
+++ qcat/Ronn_Castekin.pl
@@ -1,8 +1,3 @@
-################################
-# NPC: Ronn_Castekin.pl
-# Zone: Qcat
-# By Andrew80k
-
 sub EVENT_DEATH_COMPLETE {
-  quest::say("Argh!.. Mer.. o..na.. it's.. not.. your fault.. arhhh...");
+	quest::say("Argh!.. Mer.. o..na.. it's.. not.. your fault.. arhhh...");
 }
