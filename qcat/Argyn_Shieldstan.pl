f2664cb36a6a53c2c16bacecdeea62ba58aa17d6
TurmoilToad <TurmoilToad@gmail.com>
2019-10-24 11:19:54 -0400

    Update Argyn_Shieldstan.pl

--- qcat/Argyn_Shieldstan.pl
+++ qcat/Argyn_Shieldstan.pl
@@ -1,6 +1,14 @@
-sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("You have set foot upon the sacred grounds of the Shrine of Bertoxxulous. You had best have business here or this dirt shall become your grave.");
-  }
+sub EVENT_AGGRO {
+	quest::say("Your rotting corpse will warn others to fear the disease of the Bloodsabers!");
 }
-#END of FILE Zone:qcat  ID:45016 -- Argyn_Shieldstan
+
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("You have set foot upon the sacred grounds of the Shrine of Bertoxxulous. You had best have business here or this dirt shall become your grave.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
