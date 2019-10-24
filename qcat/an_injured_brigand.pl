3c06c983d509bc20ad50bea59f3480a68d0814cf
TurmoilToad <TurmoilToad@gmail.com>
2019-10-24 19:34:32 -0400

    Update an_injured_brigand.pl

--- qcat/an_injured_brigand.pl
+++ qcat/an_injured_brigand.pl
@@ -1,7 +1,7 @@
 sub EVENT_SPAWN {
 	quest::say("I have spawned");
 	#:: Set hitpoints to half of 96
-	$mob->SetHP(48);
+	$npc->SetHP(48);
 	quest::say("I am at half health");
 	#:: Set next HP event at 51 percent
 	quest::setnexthpevent(51);
@@ -12,7 +12,7 @@ sub EVENT_HP {
 	#:: Match if HP is 51 percent
 	if ($hpevent == 51) {
 		#:: Set hitpoints to half of 96
-		$mob->SetHP(48);
+		$npc->SetHP(48);
 		#:: Set next HP event at 51 percent
 		quest::setnexthpevent(51);
 	}
