44314c156b2c0cd4885e846a11111bf72bc3f5ec
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-10-25 16:31:33 -0400

    Create Tarsa_Yovar.pl
    Updated Tarsa Yovar of Freportw to perl - new file no link

---
+++ freportw/Tarsa_Yovar.pl
@@ -0,0 +1,5 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Keep your voice down friend. The Dismal Rage and their fool pawns the Freeport Militia have eyes and ears in every corner of the city. It is not safe for you to be among us unless you too have been placed on the Militias most wanted list.");
+	}
+}
