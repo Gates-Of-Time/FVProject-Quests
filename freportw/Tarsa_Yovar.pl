90e7d0c00aeb127bf16c04571f587efffd7c5a94
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 18:28:09 -0400

    Update Tarsa_Yovar.pl

--- freportw/Tarsa_Yovar.pl
+++ freportw/Tarsa_Yovar.pl
@@ -3,3 +3,8 @@ sub EVENT_SAY {
 		quest::say("Keep your voice down friend. The Dismal Rage and their fool pawns the Freeport Militia have eyes and ears in every corner of the city. It is not safe for you to be among us unless you too have been placed on the Militias most wanted list.");
 	}
 }
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
