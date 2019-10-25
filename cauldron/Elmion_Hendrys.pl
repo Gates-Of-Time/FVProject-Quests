abae9a204dbfdf4a88b45465043e103284d08889
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 12:29:32 -0400

    Update Elmion_Hendrys.pl

--- cauldron/Elmion_Hendrys.pl
+++ cauldron/Elmion_Hendrys.pl
@@ -1,23 +1,26 @@
 sub EVENT_SAY {
-	#:: Match if Faydark's Champions faction is amiable or better.
-	if ($faction <= 4) {
+	#:: Match if Faydark's Champions faction is Indifferent or better.
+	if ($faction <= 5) {
 		if ($text=~/hail/i) {
-			quest::say("How are you? Please rest. Have you [" . quest::saylink("been wounded by the beasts") . "] of this region.");
+			quest::say("How are you? Please rest. Have you [been wounded by the beasts] of this region.");
 		}
-		if ($text=~/been wounded by the beasts/i) {
+		elsif ($text=~/been wounded by the beasts/i) {
 			quest::say("Ah. Perhaps you should speak with Nyrien. Just inform him you 'need to be healed'.");
 		}
-		if ($text=~/faldor hendrys/i) {
+		elsif ($text=~/faldor hendrys/i) {
 			quest::say("Ha!! You seek my foul brother, Faldor Hendrys?!! He is my half brother and has left to live with the vile half of his ancestry in Qeynos. No doubt he has decided to lend his talents to the local thieves' guild. Good luck finding him.");
 		} 
-	} else {
+	} 
+	else {
 		quest::say("Your ways are considered vile to Faydark's Champions. Leave before my rage overcomes my restraint.");
 	}
 }
 
 sub EVENT_SIGNAL {
-	#:: Signal sent from Ghilanbiddle_Nylwadil.pl
-	quest::say("Pipe down, Ghil!! We have no quarrel with this adventurer.");
+	#:: Match a signal "1" from /cauldron/Ghilanbiddle_Nylwadil.pl
+	if ($signal == 1) {
+		quest::say("Pipe down, Ghil!! We have no quarrel with this adventurer.");
+	}
 }
 
 sub EVENT_ITEM {
