44fe4915707fb2ea165fc4a7e81a87d91373cb9b
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 11:54:15 -0400

    Create a_greedy_beggar.pl

---
+++ butcher/a_greedy_beggar.pl
@@ -0,0 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hello there $name. I don't suppose you can spare some coins? I'm just a poor halfling that is far away from home. I can't afford anything to eat or drink. Anything you can offer me will be of help.");
+	}
+}
+
+sub EVENT_ITEM {
+	quest::say("Oh thank you . You are too kind to this poor halfling. Do you have anything else to give me?");
+}
