027fce377d7e26f63114d4b03a7db05387ee954b
TurmoilToad <TurmoilToad@gmail.com>
2019-10-24 20:15:53 -0400

    Update an_injured_brigand.pl

--- qcat/an_injured_brigand.pl
+++ qcat/an_injured_brigand.pl
@@ -1,16 +1,31 @@
 sub EVENT_SPAWN {
 	#:: Set hitpoints to half of 96
 	$npc->SetHP(48);
-	#:: Set next HP event at 51 percent
-	quest::setnexthpevent(51);
+	#:: Create a timer "injure" that loops every 5 seconds
+	quest::settimer("injure", 5);
 }
 
-sub EVENT_HP {
-	quest::say("Event HP triggered.");
-	#:: Set hitpoints to half of 96
-	$npc->SetHP(48);
-	quest::say("Set hitpoints to 48.");
-	#:: Set next HP event at 51 percent
-	quest::setnexthpevent(51);
-	quest::say("set next hp event for 51 percent.");
+sub EVENT_TIMER {
+	#:: Match timer "injure"
+	if ($timer eq "injure") {
+		$npc->SetHP(48);
+	}
+}
+
+sub EVENT_COMBAT {
+	#:: Match if Combat State = 1 (True - Fighting)
+	if ($combat_state == 1) {
+		#:: Stop the timer
+		quest::stoptimer("injure");
+	}
+	#:: Match if Combat State = 0 (False - Not Fighting)
+	elsif ($combat_state == 0) {
+		#:: Create a timer "injure" that loops every 5 seconds
+		quest::settimer("injure", 5);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
