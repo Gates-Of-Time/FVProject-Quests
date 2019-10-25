7f29c944b64505be48306ddeb0e3ab172b3249ee
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 11:40:53 -0400

    Update Guard_Haldin.pl

--- butcher/Guard_Haldin.pl
+++ butcher/Guard_Haldin.pl
@@ -1,16 +1,14 @@
-#:: Quest NPC for Taskmaster Earring - Dwarf Male Quest
-
 sub EVENT_ITEM {
-	#:: Turn in for 18905 -  Worn Rune (Csb I.O.U Dwf 1)
-	if (plugin::check_handin(\%itemcount, 18905 => 1)) {
+	#:: Match a 18905 -  Worn Rune (Csb I.O.U Dwf 1)
+	if (plugin::takeItems(18905 => 1)) {
 		quest::say("'Eh, strapling? Ye got this from me ol' partner? Huh. Aye, I know what it means but ye're not hearin'. Here, take this. It was his but he's not needin' it now.");
-		#:: Randomly choose from 5037 - Bronze Two Handed Battle Axe, 5029 - Bronze Bastard Sword, 13313 - Dwarven Axe
-		quest::summonitem(quest::ChooseRandom(5037,5029,13313));
-		#:: Give a small amount of xp
-		quest::exp(30000);
+		#:: Give a random reward:  5037 - Bronze Two Handed Battle Axe, 5029 - Bronze Bastard Sword, 13313 - Dwarven Axe
+		quest::summonitem(quest::ChooseRandom(5037, 5029, 13313));
 		#:: Ding!
 		quest::ding();
+		#:: Grant a large amount of experience
+		quest::exp(30000);
 	}
-	#:: Return Unused Items
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
