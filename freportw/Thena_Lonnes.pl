00d6a0b41f68bce73add34d6ad9bf034da9a2ef3
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 18:30:42 -0400

    Update Thena_Lonnes.pl

--- freportw/Thena_Lonnes.pl
+++ freportw/Thena_Lonnes.pl
@@ -1,11 +1,16 @@
 sub EVENT_SPAWN {
-	#:: Create a timer to trigger every 3 seconds
-	quest::settimer("spar2",3);
+	#:: Set a timer "spar2" that loops every 3 seconds
+	quest::settimer("spar2", 3);
 }
 
 sub EVENT_TIMER {
-	#:: Match "Kungfu" every five seconds
+	#:: Match timer "spar2"
 	if ($timer eq "spar2") {
-	quest::doanim(quest::ChooseRandom (1,5,6,7,11,30,45,46,47));
+		quest::doanim(quest::ChooseRandom(1, 5, 6, 7, 11, 30, 45, 46, 47));
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
