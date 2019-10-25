727dbf31ba27ba60c665716fa0b3583bce603908
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 13:34:09 -0400

    Update Margyl_Darklin.pl

--- butcher/Margyl_Darklin.pl
+++ butcher/Margyl_Darklin.pl
@@ -1,10 +1,15 @@
 sub EVENT_WAYPOINT_ARRIVE {
 	#::: When NPC arrives at waypoint 1 or 7
-	if ($wp == 55 || $wp == 117) {
+	if ($wp == 1 || $wp == 7) {
 		quest::SetRunning(1);  #:: Run
 	}
 	#::: When NPC arrives at waypoint 82 or 10
 	if ($wp == 82 || $wp == 10) {
 		quest::SetRunning(0);  #:: Walk
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
