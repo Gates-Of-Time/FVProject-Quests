44b69e93411cf45d081775a2a1d1ebd21ea20991
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 18:01:04 -0400

    Update Shintl_Lowbrew.pl

--- freportw/Shintl_Lowbrew.pl
+++ freportw/Shintl_Lowbrew.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hail. mighty one! What are you doing in the city?  You should be out slaying beasts. There is nothing to find here in the city.");
+		quest::say("Hail, mighty one! What are you doing in the city?  You should be out slaying beasts. There is nothing to find here in the city.");
 	}
 	elsif ($text=~/hollish/i) {
 		quest::say("I have never heard of this Hollish character you speak of. You must be confusing me for someone else.");
@@ -11,13 +11,20 @@ sub EVENT_SAY {
 }
 
 sub EVENT_WAYPOINT_ARRIVE{
-#::: When NPC arrives at waypoint 1 spawn Hog Key #2 on the ground
+	#:: Match waypoint 1
 	if ($wp == 1) {
-		quest::creategroundobject(12147,-448,-107,-10.870,0,90000)
+		#:: Spawn a 12147 - Hog Key # 2 on the ground
+		quest::creategroundobject(12147, -448, -107, -10.870, 0, 90000)
 	}
+	#:: Match waypoint 7
 	elsif ($wp == 7) {
 		quest::say("Any mail for room number two?");
-		#:: Signal Swin Blakeye of Freportw
-		quest::signalwith(9103, 1, 1);
+		#:: Send a signal "1" to West Freeport >> Swin_Blackeye (9103) with no delay
+		quest::signalwith(9103, 1, 0);
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
