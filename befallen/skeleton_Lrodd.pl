fcd12312e379a5cd29a95013679c77fc98bb3bdd
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 11:34:26 -0400

    Update skeleton_Lrodd.pl

--- befallen/skeleton_Lrodd.pl
+++ befallen/skeleton_Lrodd.pl
@@ -1,12 +1,12 @@
 sub EVENT_ITEM {
-	#:: Turn in for 18891 -  Tattered Cloth Note (Note to Hubard L'rodd)
-	if (plugin::check_handin(\%itemcount, 18891 => 1)) {
+	#:: Match a 18891 - Tattered Cloth Note (Note to Hubard L'rodd)
+	if (plugin::takeItems(18891 => 1)) {
 		quest::say("Search!! Find Windstream. Find the mallet!! One. Two. Three.");
-		# Spawn Cmdr_Windstream
-		quest::spawn2(36096,0,0,-88,-637,-66,127);
+		# Spawn Befallen >> Cmdr_Windstream (36096)
+		quest::spawn2(36096, 0, 0, -88, -637, -66, 127);
 		#:: Ding!
 		quest::ding();
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
