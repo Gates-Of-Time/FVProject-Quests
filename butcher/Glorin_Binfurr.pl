fc7a2a7d042de3d8e0f9fbbc54e15481f258f884
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 11:41:23 -0400

    Update Glorin_Binfurr.pl

--- butcher/Glorin_Binfurr.pl
+++ butcher/Glorin_Binfurr.pl
@@ -1,5 +1,3 @@
-#:: The Mudtoes Quest NPC
-
 sub EVENT_SAY {	
 	if ($text=~/hail/i) {
 		quest::say("Pleasure to meet you.  Keep your eye out for the ship.  You don't want to miss it.");
@@ -7,4 +5,9 @@ sub EVENT_SAY {
 	if ($text=~/mudtoe's|mudtoes/i) {
 		quest::say("Mudtoes!! I have heard tales of that foul clan of ogres. A few passengers aboard the ships have claimed to have seen one of them, off and on, amongst the pirates in the Ocean of Tears!!");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
