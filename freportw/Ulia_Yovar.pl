eac44fb489f24cabb340560dbbcd944102a1abc9
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-10-25 19:42:20 -0400

    Create Ulia_Yovar.pl
    Updated ulia Yovar of Freportw to perl - new file no link

---
+++ freportw/Ulia_Yovar.pl
@@ -0,0 +1,5 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings $Name. If you have love and kindness in your heart then seek refuge at the Temple of Marr and the Hall of Honor in North Freeport. There is a growing evil spreading through the city. May Erollisi save us and cleanse this city of the hatred and corruption spread by the vile Dismal Rage.");
+	}
+}
