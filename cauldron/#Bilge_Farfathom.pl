3de7f932231c94f21b194280209debebffc7a483
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 12:52:40 -0400

    Update #Bilge_Farfathom.pl

--- cauldron/#Bilge_Farfathom.pl
+++ cauldron/#Bilge_Farfathom.pl
@@ -1,26 +1,19 @@
-#Bilge Farfathom spawn
-#npc - #Bilge_Farfathom - randomly spawns 1 of 4 Bilge triggers around the zone)
-#zone - Dagnors Cauldron
-#Angelox
-
 sub EVENT_DEATH_COMPLETE{
- my $random_result = int(rand(100));
- #these are not PEQ IDs:
- my $a=70060; #npc - an_aqua_goblin_mariner
- my $b=70062; #npc - #an_undertow_skeleton
- my $c=70061; #npc - #aqua_goblin_shaman
- my $d=70063; #npc - #a_cauldron_rat
- if($random_result<25){
-   #quest::say("Spawning mariner trigger1");
-   quest::spawn2($a,5,0,-296.37,-122.31,-349.32,122);
-  }elsif(($random_result>=25) && ($random_result<50)){
-     #quest::say("Spawning skeleton trigger2");
-     quest::spawn2($b,6,0,-974.5,436.4,-343.8,29);
-  }elsif(($random_result>=50) && ($random_result<75)){
-     #quest::say("Spawning shaman trigger3");
-     quest::spawn2($c,7,0,-848.3,-184.8,216.3,37.3);
-  }else{
-    #quest::say("Spawning rat trigger4");
-    quest::spawn2($d,8,0,612.6,625.2,209.0,175.6);
-  };
-};
+	$random_result = quest::ChooseRandom(1, 2, 3, 4);
+	if ($random_result == 1) {
+		#:: Spawn a Dagnor's Cauldron >> an_aqua_goblin_mariner (70060) on pathgrid 5
+		quest::spawn2(70060, 5, 0, -296.37, -122.31, -349.32, 122);
+	}
+	elsif ($random_result == 2) {
+		#:: Spawn a Dagnor's Cauldron >> #an_undertow_skeleton (70062) on pathgrid 6
+		quest::spawn2(70062, 6, 0, -974.5, 436.4, -343.8, 29);
+	}
+	elsif ($random_result == 3) {
+		#:: Spawn a Dagnor's Cauldron >> #aqua_goblin_shaman (70061) on pathgrid 7
+		quest::spawn2(70061, 7, 0, -848.3, -184.8, 216.3, 37.3);
+	}
+	elsif ($random_result == 4) {
+		#:: Spawn a Dagnor's Cauldron >> #a_cauldron_rat (70063) on pathgrid 8
+		quest::spawn2(70063, 8, 0, 612.6, 625.2, 209.0, 175.6);
+	}
+}
