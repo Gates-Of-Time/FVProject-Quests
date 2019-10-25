5eedcc0d8fc13cdc87d73abf72c4fc96fe3d37c2
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 13:14:49 -0400

    Update Ranvigoz_Tonsmith.pl

--- commons/Ranvigoz_Tonsmith.pl
+++ commons/Ranvigoz_Tonsmith.pl
@@ -1,15 +1,13 @@
-# resize crafted armor to small size
-
 sub EVENT_SAY {
-	#:: Check that faction is Indifferent or better
+	#:: Match if faction is Indifferent or better
 	if ($faction <= 5) {
 		if ($text=~/hail/i) {
-			quest::say("Hail! I am Ranvigoz Tonsmith. My brother Timtok and I are some of the finest smiths this side of Kaladim. If you are interested we will happily [" . quest::saylink("resize") . "] your [" . quest::saylink("Crafted Armor") . "] down to a more manageable size.");
+			quest::say("Hail! I am Ranvigoz Tonsmith. My brother Timtok and I are some of the finest smiths this side of Kaladim. If you are interested we will happily [resize] your [Crafted Armor] down to a more manageable size.");
 		}
-		if ($text=~/resize/i) {
+		elsif ($text=~/resize/i) {
 			quest::say("I will resize Crafted Bracers, Greaves, a Pauldron or a Breastplate. My brother will resize Crafted Gauntlets, a Vambrace, a Helm or Boots. Just give me the armor and I will resize it for you.");
 		}
-		if ($text=~/crafted armor/i) {
+		elsif ($text=~/crafted armor/i) {
 			quest::say("Crafted Armor is made by the Meadowgreen smiths. They can be found in the southern part of the Karanas.");
 		}
 	}
@@ -19,48 +17,53 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Check that faction is Indifferent or better
+	#:: Match if faction is Indifferent or better
 	if ($faction <= 5) {
-		#:: Match 4177 - Crafted Bracers
-		if (plugin::check_handin(\%itemcount, 4177 => 1)) {
-			#:: Summon 4185 - Small Crafted Bracers
-			quest::summonitem(4185);
+		#:: Match a 4177 - Crafted Bracers
+		if (plugin::takeItems(4177 => 1)) {
 			quest::say("Here is your armor. I hope it fits better now.");
-			quest::faction(229,10);	#:: Coalition of Tradefolk
-			quest::faction(281,10);	#:: Knights of Truth
-			quest::faction(291,10);	#:: Merchants of Qeynos
-			quest::faction(5015,10);	#:: Coalition of Tradefolk III
+			#:: Give a 4185 - Small Crafted Bracers
+			quest::summonitem(4185);
+			#:: Set factions
+			quest::faction(229, 10);		#:: + Coalition of Tradefolk
+			quest::faction(281, 10);		#:: + Knights of Truth
+			quest::faction(291, 10);		#:: + Merchants of Qeynos
+			quest::faction(5015, 10);		#:: + Coalition of Tradefolk III
 		}
-		#:: Match 4179 - Crafted Greaves
-		if (plugin::check_handin(\%itemcount, 4179 => 1)) {
+		#:: Match a 4179 - Crafted Greaves
+		elsif (plugin::takeItems(4179 => 1)) {
+			quest::say("Here is your armor. I hope it fits better now.");
 			#:: Summon 4187 - Small Crafted Greaves
 			quest::summonitem(4187);
-			quest::say("Here is your armor. I hope it fits better now.");
-			quest::faction(229,10);	#:: Coalition of Tradefolk
-			quest::faction(281,10);	#:: Knights of Truth
-			quest::faction(291,10);	#:: Merchants of Qeynos
-			quest::faction(5015,10);	#:: Coalition of Tradefolk III
+			#:: Set factions
+			quest::faction(229, 10);		#:: + Coalition of Tradefolk
+			quest::faction(281, 10);		#:: + Knights of Truth
+			quest::faction(291, 10);		#:: + Merchants of Qeynos
+			quest::faction(5015, 10);		#:: + Coalition of Tradefolk III
 		}
-		#:: Match 4175 - Crafted Pauldron
-		if (plugin::check_handin(\%itemcount, 4175 => 1)) {
+		#:: Match a 4175 - Crafted Pauldron
+		elsif (plugin::takeItems(4175 => 1)) {
+			quest::say("Here is your armor. I hope it fits better now.");
 			#:: Summon 4183 - Small Crafted Pauldron
 			quest::summonitem(4183);
-			quest::say("Here is your armor. I hope it fits better now.");
-			quest::faction(229,10);	#:: Coalition of Tradefolk
-			quest::faction(281,10);	#:: Knights of Truth
-			quest::faction(291,10);	#:: Merchants of Qeynos
-			quest::faction(5015,10);	#:: Coalition of Tradefolk III
+			#:: Set factions
+			quest::faction(229, 10);		#:: + Coalition of Tradefolk
+			quest::faction(281, 10);		#:: + Knights of Truth
+			quest::faction(291, 10);		#:: + Merchants of Qeynos
+			quest::faction(5015, 10);		#:: + Coalition of Tradefolk III
 		}
-		#:: Match 4174 - Crafted Breastplate
-		if (plugin::check_handin(\%itemcount, 4174 => 1)) {
+		#:: Match a 4174 - Crafted Breastplate
+		elsif (plugin::takeItems(4174 => 1)) {
+			quest::say("Here is your armor. I hope it fits better now.");
 			#:: Summon 4182 - Small Crafted Breastplate
 			quest::summonitem(4182);
-			quest::say("Here is your armor. I hope it fits better now.");
-			quest::faction(229,10);	#:: Coalition of Tradefolk
-			quest::faction(281,10);	#:: Knights of Truth
-			quest::faction(291,10);	#:: Merchants of Qeynos
-			quest::faction(5015,10);	#:: Coalition of Tradefolk III
+			#:: Set factions
+			quest::faction(229, 10);		#:: + Coalition of Tradefolk
+			quest::faction(281, 10);		#:: + Knights of Truth
+			quest::faction(291, 10);		#:: + Merchants of Qeynos
+			quest::faction(5015, 10);		#:: + Coalition of Tradefolk III
 		}
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
