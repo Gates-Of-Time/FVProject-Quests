bedce910d4aa208aab4046c59f2c8c0488f5088c
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 20:33:39 -0400

    Update Ulia_Yovar.pl

--- freportw/Ulia_Yovar.pl
+++ freportw/Ulia_Yovar.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Greetings $Name. If you have love and kindness in your heart then seek refuge at the Temple of Marr and the Hall of Honor in North Freeport. There is a growing evil spreading through the city. May Erollisi save us and cleanse this city of the hatred and corruption spread by the vile Dismal Rage.");
+		quest::say("Greetings $name. If you have love and kindness in your heart then seek refuge at the Temple of Marr and the Hall of Honor in North Freeport. There is a growing evil spreading through the city. May Erollisi save us and cleanse this city of the hatred and corruption spread by the vile Dismal Rage.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
