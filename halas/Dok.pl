364483401ab7833dbe2eb4838b35d416f6d663eb
Dalethor <30395666+Dalethor@users.noreply.github.com>
2019-10-26 13:37:43 -0400

    Update Dok.pl
    Removed Saylinks altered quest language to match online source.

--- halas/Dok.pl
+++ halas/Dok.pl
@@ -1,9 +1,9 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Fine day to ye, $name.  Welcome to me shop.  If ye're searchin' fer a [" . quest::saylink("cigar") . "], ye won't be findin' any, more's th' pity.");
+		quest::say("Fine day to ye, $name.  Welcome to me shop.  If ye're searchin' fer a [cigar], ye won't be findin' any, more's th' pity.");
 	}
 	elsif ($text=~/cigar/i) {
-		quest::say("I was hoping to perfect me creation I was callin' the 'cigar.' but I'm afraid I just couldn't get it right.  To keep from settin' meself on fire, I switched me talents to makin' candles.  I could use help from a person such as yerself.  Ye could [" . quest::saylink("collect the wax") . "] fer me - the candle-makin's.");
+		quest::say("I was hoping to perfect me creation I was callin' the 'cigar.' but I'm afraid I just couldn't get it right.  To keep from settin' meself on fire, I switched me talents to makin' candles.  I could use help from a person such as yerself.  Ye could [collect the wax] fer me - the candle-makin's.");
 	}
 	elsif ($text=~/collect the wax/i) {
 		quest::say("Grreatt!!  Take this wax jarr.  Head to any place ye can find the wee ones they call bixies. I'm afraid ye're going to hafta bash 'em and search to find out if they're carrying any honeycombs.  If they are, then ye can fill the jar with them and combine them, then return the full honeycomb jar to me. I'll be givin' ye a special candle if ye can do that fer me.");
@@ -11,13 +11,13 @@ sub EVENT_SAY {
 		quest::summonitem(17958);
 	}
 	elsif ($text=~/crime/i) { 
-		quest::say("Aye. I happened upon the crime scene, but too late, more's the pity.  I grabbed fer one of the rogues and got only a handful of his shirt.  Before I knew it, I was left holding his sweaty shirt and he was far from the scene.  I called fer the guards and they summoned the shaman.  Methinks one died and one got away in the chase.  I've heard rumors that the leader of [" . quest::saylink("Clan McMannus") . "] has spotted the culprit.  Ye should go to the leader of Clan McMannus and tell him ye're [" . quest::saylink("searching for the fugitive") . "].");
+		quest::say("Aye. I happened upon the crime scene, but too late, more's the pity.  I grabbed fer one of the rogues and got only a handful of his shirt.  Before I knew it, I was left holding his sweaty shirt and he was far from the scene.  I called fer the guards and they summoned the shaman.  Methinks one died and one got away in the chase.  I've heard rumors that the leader of [Clan McMannus] has spotted the culprit.  Ye should go to the leader of Clan McMannus and tell him ye're [searching for the fugitive].");
 	}
 	elsif ($text=~/Clan McMannus/i) {
-		quest::say("Clan McMannus were instructed by the Council to live far away from Halas.  They now live in the Western Plains of Karana and supply Halas with food and materials from the warmer regions.");
+		quest::say("Methinks it is Einhorst McMannus.");
 	}
-	elsif ($text=~/searching for the fugitive/i) {
-		quest::say("Now, why would ye be wantin' that smelly thing?!!  I no longer have it.  [" . quest::saylink("Tundra Jack") . "] came into me shop and bought it from me.  He says he can wash it good and make a new collar for Iceberg out of it.");
+	elsif ($text=~/sweaty/i) {
+		quest::say("Now, why would ye be wantin' that smelly thing?!!  I no longer have it.  [Tundra Jack] came into me shop and bought it from me.  He says he can wash it good and make a new collar for Iceberg out of it.");
 	}
 	elsif ($text=~/Tundra Jack/i) {
 		quest::say("Well, now, let's see...  His name is Tundra Jack.. me first guess would be that ye might find him on the frozen tundra of Everfrost, eh?");
