76f37e08b3ce35bbf2fc8757a3e38bb7ce3d327a
TurmoilToad <TurmoilToad@gmail.com>
2019-10-25 20:58:32 -0400

    Update Yovik_Splegle.pl

--- freportw/Yovik_Splegle.pl
+++ freportw/Yovik_Splegle.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Greetings $Name. Beware the corruption that has infected Freeport. Even the tower of the Arcane Scientists is not safe from the slimy hands of the Dismal Rage. I fear Opal Darkbriar is not what she seems and is darkening the minds of her apprentices.");
+		quest::say("Greetings $name. Beware the corruption that has infected Freeport. Even the tower of the Arcane Scientists is not safe from the slimy hands of the Dismal Rage. I fear Opal Darkbriar is not what she seems and is darkening the minds of her apprentices.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
