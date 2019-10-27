3904859c2436276eab78d9b28fa5ee584e1b2c81
TurmoilToad <TurmoilToad@gmail.com>
2019-10-27 08:54:43 -0400

    Create Riggin_Vix.pl

---
+++ qcat/Riggin_Vix.pl
@@ -0,0 +1,10 @@
+sub EVENT_SAY {
+	if ($text=/hail/i) {
+		quest::say("Be careful down here, $name.  We have seen some strange characters in the Eastern Tunnels.  Some kind of cult members, I think.  Watch out.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
