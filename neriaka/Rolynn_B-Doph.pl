67c295d75e04919f6d347777ee52968c80cddc96
TurmoilToad <TurmoilToad@gmail.com>
2019-10-28 08:12:21 -0400

    Update Rolynn_B-Doph.pl

--- neriaka/Rolynn_B-Doph.pl
+++ neriaka/Rolynn_B-Doph.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/narex/i) {
+		quest::say("Talk to Head Master Narex of the Indigo Brotherhood. He will tell you.");
+	}
+}
+
 sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match waypoints 3, 8, and 18
 	if (($wp eq 3) || ($wp eq 8) || ($wp eq 18)) {
