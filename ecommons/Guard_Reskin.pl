f0c71a9b63a74422bc0804eeb720a945ec8dbaa7
TurmoilToad <TurmoilToad@gmail.com>
2019-10-29 19:39:06 -0400

    Update Guard_Reskin.pl

--- ecommons/Guard_Reskin.pl
+++ ecommons/Guard_Reskin.pl
@@ -1,5 +1,11 @@
 sub EVENT_SIGNAL{
-	if(($signal==1) && ($x==159) && ($y==-143)){
+	#:: Match a signal "1" from /ecommons/Sergeant_Slate.pl if at the given coordinates
+	if (($signal==1) && ($x==159) && ($y==-143)) {
 		quest::say("All quiet, Sergeant.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
