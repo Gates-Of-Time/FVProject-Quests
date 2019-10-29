6c06059e544693c3b04003d47cc82fbe7078fa6b
TurmoilToad <TurmoilToad@gmail.com>
2019-10-29 19:39:35 -0400

    Update Guard_Tolus.pl

--- ecommons/Guard_Tolus.pl
+++ ecommons/Guard_Tolus.pl
@@ -1,5 +1,11 @@
-sub EVENT_SIGNAL{
-	if(($signal==1) && ($x==3016) && ($y==-289)){
+sub EVENT_SIGNAL {
+	#:: Match a signal "1" from /ecommons/Sergeant_Slate.pl if at the given coordinates
+	if (($signal==1) && ($x==3016) && ($y==-289)){
 		quest::say("All quiet, Sergeant.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
