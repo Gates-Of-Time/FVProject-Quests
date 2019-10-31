2f33721935116ec04e3783c481ee76d2a19ee125
TurmoilToad <TurmoilToad@gmail.com>
2019-10-31 16:45:34 -0400

    Update global_player.pl

--- global/global_player.pl
+++ global/global_player.pl
@@ -7,10 +7,10 @@ sub EVENT_ZONE {
 }
 
 sub EVENT_ENTERZONE {
-	if (plugin::deity("Innoruuk")) {
+	if (plugin::deity(Innoruuk)) {
 			quest::ding();
 	}
-	elsif (plugin::deity(206)) {
+	elsif (plugin::deity("206")) {
 		quest::ding();
 		quest::ding();
 	}
