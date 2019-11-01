6a6e2dcb2c013639433e00948a8e7207ba281b6c
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 11:17:42 -0400

    Update Vynon_Estaliun.pl

--- erudnext/Vynon_Estaliun.pl
+++ erudnext/Vynon_Estaliun.pl
@@ -1,26 +1,41 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("I welcome you to the temple of the Peacekeepers. The ways of peace and tranquility are ours to uphold. May you find a place among us. We have much work to do. If you are a paladin of this temple. you must [desire to face fear].");
+		quest::say("I welcome you to the temple of the Peacekeepers. The ways of peace and tranquility are ours to uphold. May you find a place among us. We have much work to do. If you are a paladin of this temple, you must [desire to face fear].");
 	}
-	if ($text=~/desire to face fear/i) {
-		quest::say("Very well. You shall face it. In Toxxulia Forest. you shall seek out Kerra Ridge. Once found. you will bring me the tail of a catfisher. Somehow. you shall find a way. They are weak. but they only work near the opposite side of the bridge. along the water's edge.");
+	elsif ($text=~/desire to face fear/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Very well. You shall face it. In Toxxulia Forest, you shall seek out Kerra Ridge. Once found, you will bring me the tail of a catfisher. Somehow, you shall find a way. They are weak, but they only work near the opposite side of the bridge, along the water's edge.");
+		}
+		else {
+			quest::say("You have not done much to upset the Peacekeepers of this temple, but we must ask you to prove yourself to us before we may discuss things such as this.");
+		}
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13884 -  Fishy Cat Tail
-	if (plugin::check_handin(\%itemcount, 13884 => 1)) {
-		quest::say("Good work. I knew you could do it. Take this as reward.");
-		#:: Give a small amount of xp
-		quest::exp(100);
-		#:: Ding!
-		quest::ding();
-		#:: Give item 13053 - Brass Ring
-		quest::summonitem(13053);
-		#:: Set faction
-		quest::faction(298,10);	 	#:: + Peace Keepers
-		quest::faction(266,5); 		#:: + High Council of Erudin
-		quest::faction(265,-10); 	#:: - Heretics
+	#:: Match a 13884 - Fishy Cat Tail
+	if (plugin::takeItems(13884 => 1)) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Good work. I knew you could do it. Take this as reward.");
+			#:: Give a 13053 - Brass Ring
+			quest::summonitem(13053);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(298, 5);	 		#:: + Peace Keepers
+			quest::faction(266, 1); 		#:: + High Council of Erudin
+			quest::faction(265, -1); 		#:: - Heretics
+			#:: Grant a small amount of experience
+			quest::exp(100);
+		}
+		else {
+			quest::say("You have not done much to upset the Peacekeepers of this temple, but we must ask you to prove yourself to us before we may discuss things such as this.");
+			#:: Return a 13884 - Fishy Cat Tail
+			quest::summonitem(13884);
+		}
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
