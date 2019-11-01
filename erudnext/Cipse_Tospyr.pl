8f3d0419f394de3daf69f35e40e6f53baed70d3b
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 08:32:59 -0400

    Update Cipse_Tospyr.pl

--- erudnext/Cipse_Tospyr.pl
+++ erudnext/Cipse_Tospyr.pl
@@ -2,38 +2,33 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hello, $name. I welcome you to the Temple of Divine Light. I am the resident healer. If you should ever require the power of Quellious to [bind wounds], [cure disease] or [cure poison], speak with me and I shall help you.");
 	}
-	if ($text=~/bind wounds/i) {
+	elsif ($text=~/bind wounds/i) {
 		quest::say("I shall be pleased to help you with your wounds. The Temple of Divine Light requires a tribute of four gold before I may perform the service.");
 	}
-	if ($text=~/cure disease/i) {
+	elsif ($text=~/cure disease/i) {
 		quest::say("Your malady will be nothing more than a memory, but before that can be, we ask that a donation of two gold coins be offered.");
 	}
-	if ($text=~/cure poison/i) {
+	elsif ($text=~/cure poison/i) {
 		quest::say("You must pay the tribute of three gold before I cast the toxin from your body.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Create a scalar variable for storing money
-	my $cash = 0;
-	$cash = ($platinum * 1000) + ($gold * 100) + ($silver * 10) + $copper;
-	#:: Match 400 copper - 4gp
-	if ($cash == 400) {
+	#:: Match four gold pieces
+	if (plugin::takeCoin(0, 0, 4, 0)) {
 		#:: Cast spell 12 - Healing
 		$npc->CastSpell(12,$userid);
 	}
-	#:: Match 300 copper - 3gp
-	elsif ($cash == 300) {
+	#:: Match three gold pieces
+	elsif (plugin::takeCoin(0, 0, 3, 0)) {
 		#:: Cast spell 203 - Cure Poison
 		$npc->CastSpell(203,$userid);
 	}
-	#:: Match 200 copper - 2gp
-	elsif ($cash == 200) {
+	#:: Match two gold pieces
+	elsif (plugin::takeCoin(0, 0, 2, 0)) {
 		#:: Cast spell 213 - Cure Disease
 		$npc->CastSpell(213,$userid);
 	}
-	else {
-    		quest::givecash($copper, $silver, $gold, $platinum);
-	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
