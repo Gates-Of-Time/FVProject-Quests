d2ae0053008d79c7b656df9f6aa545f769f3f6d6
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 08:28:36 -0400

    Update Beth_Breadmaker.pl

--- erudnext/Beth_Breadmaker.pl
+++ erudnext/Beth_Breadmaker.pl
@@ -3,3 +3,8 @@ sub EVENT_SAY {
 		quest::say("Hello there $name! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around!");
 	}
 }
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
