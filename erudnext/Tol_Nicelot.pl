e7c9674f2043bbec8a29224770215db3750f85f2
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 11:12:57 -0400

    Update Tol_Nicelot.pl

--- erudnext/Tol_Nicelot.pl
+++ erudnext/Tol_Nicelot.pl
@@ -2,10 +2,19 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("I have no time to waste with strangers!");
 	}
-	if ($text=~/moodoro/i) {
+	elsif ($text=~/moodoro/i) {
 		quest::say("Yes, I knew Moodoro Finharn. He used to be a good friend until he started visiting Qeynos and indulging in human vices. He was executed for crimes against the state. Not even his sister [Nolusia] could get the charge dropped. There have been some who say he escaped execution and lives in Qeynos. That is insane babbling.");
 	}
-	if ($text=~/Nolusia/i) {
+	elsif ($text=~/nolusia/i) {
 		quest::say("Nolusia is a member of the Craftkeepers. She lives as a guildmaster in Erudin Palace.");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My comrades will avenge my death.");
 }
