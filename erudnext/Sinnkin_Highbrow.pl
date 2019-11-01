bed7b42495b1450e0f959afbeace00b3fe2a7031
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 11:08:44 -0400

    Update Sinnkin_Highbrow.pl

--- erudnext/Sinnkin_Highbrow.pl
+++ erudnext/Sinnkin_Highbrow.pl
@@ -1,24 +1,28 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Why. hello there fellow scholar!  Don't you just love books?  I am quite a bookworm myself.  I could read forever!  I am currently working my way through the 'T's.");
+		quest::say("Why, hello there fellow scholar!  Don't you just love books?  I am quite a bookworm myself.  I could read forever!  I am currently working my way through the 'T's.");
 	}
-	if ($text=~/erud's tonic/i) {
+	elsif ($text=~/erud's tonic/i) {
 		quest::say("Erud's Tonic! That is none of your concern!");
+	}
+	elsif ($text=~/phloatin/i) {
+		quest::say("Phloatin Highbrow is my brother. He spends his time grogging it up.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13121 -  Innoruuk's Kiss of Death
-	if (plugin::check_handin(\%itemcount, 13121 => 1)) {
+	#:: Match a 13121 - Innoruuk's Kiss of Death
+	if (plugin::takeItems(13121 => 1)) {
 		quest::say("It's about time you figured it out, genius! Maybe you should spend more time in the library. Here, take this tonic and get out of here before they see me giving it to you and turn us both inside out.");
-		#:: Give item 13118 - Erud's Tonic
+		#:: Give a 13118 - Erud's Tonic
 		quest::summonitem(13118);
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
 		quest::ding();
 		#:: Set faction
-		quest::faction(694,1); 	#:: + Erudite Citizen
+		quest::faction(694, 5); 		#:: + Erudite Citizen
+		#:: Give a small amount of xp
+		quest::exp(500);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
