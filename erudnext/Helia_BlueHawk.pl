8caecd9c8b58e1bb75ade3de50301451aac0a386
TurmoilToad <TurmoilToad@gmail.com>
2019-11-01 10:20:44 -0400

    Update Helia_BlueHawk.pl

--- erudnext/Helia_BlueHawk.pl
+++ erudnext/Helia_BlueHawk.pl
@@ -2,6 +2,15 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hello there, adventurer! I am sure you are thirsty after that long boat ride from Qeynos!");
 	}
+	elsif ($text=~/vasty deep/i) {
+		quest::say("The Deepwater Knights are clerics and paladins of Prexus. They are to be found at the Deepwater Temple.");
+	}
+	elsif ($text=~/high council/i) {
+		quest::say("The members of the High Council are the leaders of every guild within Erudin. Markus Jaevins, the leader of the Gatecallers, presides over the High Council.");
+	}
+	elsif ($text=~/markus jaevins/i) {
+		quest::say("Markus Jaevins is the highest ranking official in Erudin.");
+	}
 }
 
 sub EVENT_SIGNAL {
