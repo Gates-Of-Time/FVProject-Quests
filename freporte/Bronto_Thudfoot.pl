75154cf218c6381cfc1d5cefd9786123039b2b69
FVFippy <44533609+FVFippy@users.noreply.github.com>
2019-11-03 14:17:52 -0500

    Update Bronto_Thudfoot.pl
    Updated signal catch

--- freporte/Bronto_Thudfoot.pl
+++ freporte/Bronto_Thudfoot.pl
@@ -1,4 +1,5 @@
 #::  Signal From Lenka Stoutheart
 sub EVENT_SIGNAL {
+	if ($signal == 1) {
 	quest::say("You said it, boss!  Stay clear of taking sides and you should be just fine, young one.");
 }
