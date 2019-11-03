21c5d01e96fa931f43bf96a6cdcfff53982b2d84
TurmoilToad <TurmoilToad@gmail.com>
2019-11-03 14:29:23 -0500

    Update Bronto_Thudfoot.pl

--- freporte/Bronto_Thudfoot.pl
+++ freporte/Bronto_Thudfoot.pl
@@ -1,5 +1,11 @@
-#::  Signal From Lenka Stoutheart
+#:: Match a signal "1" from /freporte/Lenka_Stoutheart.pl
 sub EVENT_SIGNAL {
 	if ($signal == 1) {
-	quest::say("You said it, boss!  Stay clear of taking sides and you should be just fine, young one.");
+		quest::say("You said it, boss!  Stay clear of taking sides and you should be just fine, young one.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
