0e214e5ee092f37873c04931dde55286602c7ee5
TurmoilToad <TurmoilToad@gmail.com>
2019-11-04 08:17:37 -0500

    Update skeleton_Lrodd.pl

--- befallen/skeleton_Lrodd.pl
+++ befallen/skeleton_Lrodd.pl
@@ -1,3 +1,10 @@
+sub EVENT_COMBAT {
+	#:: Combat state true (enter combat)
+	if ($combat_state == 1) {
+		quest::say("Run!! Leave this place at once!!");
+	}
+}
+
 sub EVENT_ITEM {
 	#:: Match a 18891 - Tattered Cloth Note (Note to Hubard L'rodd)
 	if (plugin::takeItems(18891 => 1)) {
