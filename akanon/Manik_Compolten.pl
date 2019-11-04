14889424f913cb59e1f9f0c34503afc36f414ac8
TurmoilToad <TurmoilToad@gmail.com>
2019-11-04 08:56:15 -0500

    Update Manik_Compolten.pl

--- akanon/Manik_Compolten.pl
+++ akanon/Manik_Compolten.pl
@@ -3,29 +3,71 @@ sub EVENT_SAY {
 		quest::say("Greetings, young one. I am Manik Compolten, High Watchman. Are you a [new warrior] or an [experienced fighter]?");
 	}
 	elsif ($text=~/new warrior/i) {
-		quest::say("It is always good to see new blood amongst the Gemchoppers. I have a small task for you. Take this keycard. I will give you only one at a time. Use each to obtain blackboxes for the C series clockworks. I am sure you are familiar with the clockworks. When you are done, bring them all to me. Let me know if you need another keycard.");
-		#:: Give a 13844 - Shiny Card
-		quest::summonitem(13844);
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("It is always good to see new blood amongst the Gemchoppers. I have a small task for you. Take this keycard. I will give you only one at a time. Use each to obtain blackboxes for the C series clockworks. I am sure you are familiar with the clockworks. When you are done, bring them all to me. Let me know if you need another keycard.");
+			#:: Give a 13844 - Shiny Card
+			quest::summonitem(13844);
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/keycard/i) {
-		quest::say("Here. Remember, I will give you only one keycard at a time. These keycards only work for the C series clockworks.");
-		#:: Give a 13844 - Shiny Card
-		quest::summonitem(13844);
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Here. Remember, I will give you only one keycard at a time. These keycards only work for the C series clockworks.");
+			#:: Give a 13844 - Shiny Card
+			quest::summonitem(13844);
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/experienced fighter/i) {
-		quest::say("Good. I require your talents to destroy rogue clockworks. After we sent the clockworks to destroy the Asylum of the Mad we found some of the clockworks went haywire and never returned. We must destroy them, not for the safety of the people, but to keep our technology from falling into the hands of any other nation. Go to the Steamfont Mountains and return their rogue blackboxes to me.");
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Good. I require your talents to destroy rogue clockworks. After we sent the clockworks to destroy the Asylum of the Mad we found some of the clockworks went haywire and never returned. We must destroy them, not for the safety of the people, but to keep our technology from falling into the hands of any other nation. Go to the Steamfont Mountains and return their rogue blackboxes to me.");
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/asylum of the mad/i) {
-		quest::say("They are the madmen of Meldrath. While he built the Titan they kept the Watchmen busy with frequent murders and senseless destruction. The Watchmen finally directed the Clockworks to destroy this band of madmen. Some may still be found praising the name of Meldrath as their dagger slices through innocent flesh.");
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("They are the madmen of Meldrath. While he built the Titan they kept the Watchmen busy with frequent murders and senseless destruction. The Watchmen finally directed the Clockworks to destroy this band of madmen. Some may still be found praising the name of Meldrath as their dagger slices through innocent flesh.");
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/meldrath/i) {
-		quest::say("Meldrath was once the leader of the Wizards of this guild. He was absorbed by his work on [other worlds]. It is said that he found something in the sky. Whatever he saw caused a madness, he soon took flight and formed the [Asylum of the Mad]. Most believe he is deceased, we know better. Tobon is intent on finding him.");
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Meldrath was once the leader of the Wizards of this guild. He was absorbed by his work on [other worlds]. It is said that he found something in the sky. Whatever he saw caused a madness, he soon took flight and formed the [Asylum of the Mad]. Most believe he is deceased, we know better. Tobon is intent on finding him.");
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/other worlds/i) {
-		quest::say("We of the Eldritch Collective have been studying the skies for some time. Our Observers can be found scattered among the landscape. We believe there may be other worlds of great technology, but to reach there will require great technology.");
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("We of the Eldritch Collective have been studying the skies for some time. Our Observers can be found scattered among the landscape. We believe there may be other worlds of great technology, but to reach there will require great technology.");
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 	elsif ($text=~/clockwork/i) {
-		quest::say("The clockworks were developed by the Eldritch Collective. They are used as our policing force in Ak'Anon. They come in many series. The letter following their number is the series model.");
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("The clockworks were developed by the Eldritch Collective. They are used as our policing force in Ak'Anon. They come in many series. The letter following their number is the series model.");
+		}
+		else {
+			quest::say("You must show a greater allegiance to the Gemchoppers before we can speak with you. Search the hills for rogue clockworks. Captain Compolten shall accept two of their blackboxes and you shall prove your loyalty to our hall.");
+		}
 	}
 }
 
@@ -88,6 +130,10 @@ sub EVENT_ITEM {
 		my %cash = plugin::RandomCash(130,170);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+	}
+	#:: Match a 13212 - Blackbox XIIC, or a 13211 - Blackbox XXVIC, or a 13213 - Blackbox XVIIC, or a 13214 - Blackbox XXVIIC
+	elsif (plugin::takeItems(13212 => 1) || plugin::takeItems(13211 => 1) || plugin::takeItems(13213 => 1) || plugin::takeItems(13214 => 1)) {
+		quest::say("A generous offer. Thank you for your donations to Gemchopper Hall. I shall have this recyled.");
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
