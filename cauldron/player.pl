48d557d80848cc18fa249c90cb5016061df939ea
TurmoilToad <TurmoilToad@gmail.com>
2019-11-03 19:25:52 -0500

    Update player.pl

--- cauldron/player.pl
+++ cauldron/player.pl
@@ -2,6 +2,7 @@
 sub EVENT_PLAYER_PICKUP {
 	#:: Match 12274 - Chalice of Conquest, ground spawn created by #Captain_Klunga.pl
 	if ($picked_up_id == 12274) {
-		quest::signal(70072,1);
+		#:: Send a signal "1" to Dagnor's Cauldron >> #Captain_Klunga (70072) with no delay
+		quest::signalwith(70072, 1, 0);
 	}
 }
