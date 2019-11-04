65d269829e270bfd842a8244851b37f29937c517
TurmoilToad <TurmoilToad@gmail.com>
2019-11-03 20:07:06 -0500

    Update Lord_Darish.pl

--- crushbone/Lord_Darish.pl
+++ crushbone/Lord_Darish.pl
@@ -1,19 +1,15 @@
 sub EVENT_COMBAT {
-	my $random = int(rand(100));
 	#:: Match combat_state 1 (true) for entered combat
 	if ($combat_state == 1) {
-		#:: For results less than 30
-		if ($random_result<30) {
+		my $RandomResult = quest::ChooseRandom(1, 2, 3);
+		if ($RandomResult == 1) {
 			quest::say("Death!!  Death to all who oppose the Crushbone orcs!!");
 		}
-		#:: For results between 30 and 65
-		elsif (($random_result>=30) && ($random_result<65)) {
+		elsif ($RandomResult == 2) {
 			quest::say("Hail, Emperor Crush!!");
-		} else {
-		#:: Result >65
+		} 
+		else {
 			quest::say("Fall before the might of Clan Crushbone!!");
 		}
 	}
 }
-
-# Converted to Perl by SS
