1d658936139a75568d64918b9323a1740a07138e
TurmoilToad <TurmoilToad@gmail.com>
2019-11-03 20:03:33 -0500

    Update Kelynn.pl

--- crushbone/Kelynn.pl
+++ crushbone/Kelynn.pl
@@ -1,5 +1,3 @@
-#:: Scouts Cape Quest
-
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("What are you doing here?!! Get out before you find yourself mining for these wretched beasts!");
@@ -7,16 +5,16 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 12184 -  Useless Token
-	if (plugin::check_handin(\%itemcount, 12184 => 1)) {
+	#:: Match a 12184 - Useless Token
+	if (plugin::takeItems(12184 => 1)) {
 		quest::say("You were sent by Geeda!! Here. Take the information. Maybe next you shall earn your Scout Blade from Laren. Quickly!! Leave at once!!");
 		#:: Give item 12183 - Crushbone Information
 		quest::summonitem(12183);
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
 		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(500);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
