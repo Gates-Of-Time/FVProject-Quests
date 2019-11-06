f2ece9af3212ddbb9612fc911e0c1f5162a5fb36
TurmoilToad <TurmoilToad@gmail.com>
2019-11-06 08:27:06 -0500

    Delete player.pl

--- cauldron/player.pl
+++
@@ -1,8 +0,0 @@
-#:: Chalice of Conquest quest
-sub EVENT_PLAYER_PICKUP {
-	#:: Match 12274 - Chalice of Conquest, ground spawn created by #Captain_Klunga.pl
-	if ($picked_up_id == 12274) {
-		#:: Send a signal "1" to Dagnor's Cauldron >> #Captain_Klunga (70072) with no delay
-		quest::signalwith(70072, 1, 0);
-	}
-}
