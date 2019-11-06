385cc783a0e6175bc929511361f28413c9a50696
TurmoilToad <TurmoilToad@gmail.com>
2019-11-06 09:06:25 -0500

    Update #Captain_Klunga.pl

--- cauldron/#Captain_Klunga.pl
+++ cauldron/#Captain_Klunga.pl
@@ -1,5 +1,3 @@
-my $PlayerToAttack;
-
 sub EVENT_SPAWN {
 	#:: Appearance 1 - Sit
 	$npc->SetAppearance(1);
@@ -12,15 +10,18 @@ sub EVENT_ITEM {
 		$npc->SetAppearance(0);
 		#:: Move to the specified location and guard 
 		quest::moveto(-395.87, 807.04, 71.78, 0, 1);
-		$PlayerToAttack = $name;
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-	#:: Create a 12274 - Chalice of Conquest at current coordinates
+	#:: Create a 12274 - Chalice of Conquest at specified coordinates (lower z to appear on ground)
 	quest::creategroundobject(12274, -395.87, 807.04, 68, 0);
-	#:: Attack the player that handed in the Abandoned Orc Shovel
-	quest::attack($PlayerToAttack);
+	#:: Create a proximity, 20 units across, 20 units tall, without proximity say
+	quest::set_proximity($x - 10, $x + 10, $y - 10, $y + 10, $z - 10, $z + 10, 0);
+}
+
+sub EVENT_ENTER {
+	quest::attack($name);
 }
