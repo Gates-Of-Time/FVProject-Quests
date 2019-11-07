f87de59713eedc972cc461737022e4e55076eee9
TurmoilToad <TurmoilToad@gmail.com>
2019-11-07 08:13:11 -0500

    Update Wela_Muselender.pl

--- felwithea/Wela_Muselender.pl
+++ felwithea/Wela_Muselender.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::emote("Wela Muselender curtseys and says, 'Welcome to our fine tavern! Our relaxing atmosphere and pleasant wines are excellent for assisting an artist or poet in finding their muse.'");
+		quest::emote("curtseys and says, 'Welcome to our fine tavern! Our relaxing atmosphere and pleasant wines are excellent for assisting an artist or poet in finding their muse.'");
 	}
 }
