825031fc6c878d47c1444956818e9ff1a12ea260
TurmoilToad <TurmoilToad@gmail.com>
2019-11-09 22:19:36 -0500

    Update Phloatin_Highbrow.pl

--- erudnext/Phloatin_Highbrow.pl
+++ erudnext/Phloatin_Highbrow.pl
@@ -10,11 +10,15 @@ sub EVENT_SAY {
 }
 
 sub EVENT_SIGNAL {
-	#:: Match signal "1" from /erudnext/Helia_Bluehawk.pl
+	#:: Match a signal "1" from /erudnext/Geoard_BlueHawk.pl
 	if ($signal == 1) {
 		quest::say("Ha! The closest you will get to tonic is in the library. That's assuming you know how to spell tonic. Bwaha ha! When you see the bookworm give her a KISS for me! HAHA!!");
 		#:: Send a signal "1" to 24067 - Helia_BlueHawk with no delay
 		quest::signalwith(24067, 1, 0);
+	}
+	#:: Match signal "2" from /erudnext/Helia_Bluehawk.pl
+	elsif ($signal == 2) {
+		quest::say("Don't worry, Helia. I don't think you need to push that one to drink.");
 	}
 }
 
