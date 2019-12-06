e9c7379f6c8bce7b0bc361a29d9d059a654cee5b
TurmoilToad <TurmoilToad@gmail.com>
2019-12-06 08:04:46 -0500

    Update and rename a_greedy_beggar.pl to a_greedy_beggar.lua

---
+++ butcher/a_greedy_beggar.lua
@@ -0,0 +1,40 @@
+local followtarget;
+
+function event_spawn(e)
+	followtarget = nil;
+end
+
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Hello there " .. e.other:GetName() .. ". I don't suppose you can spare some coins? I'm just a poor halfling that is far away from home. I can't afford anything to eat or drink. Anything you can offer me will be of help.");  -- response to all messages, even when following
+		if(followtarget == nil) then
+			followtarget = e.other:GetID();
+			e.self:SetAppearance(0); -- Stand
+			eq.set_timer("follow",5000);
+		end
+	end
+end
+
+function event_timer(e)
+	if(e.timer == "follow" and eq.get_entity_list():GetClientByID(followtarget)) then
+		eq.follow(followtarget); -- Follow the player who triggered the event
+	else
+		eq.stop_follow();
+		followtarget = nil;
+		eq.stop_timer("follow");
+		e.self:SetAppearance(3); -- lying down
+	end
+end
+
+function event_trade(e) -- Note that we are intentionally accepting any trade, as long as we are not following
+	local item_lib = require("items");
+	
+	if(followtarget == nil or e.other:GetID() == followtarget) then
+		e.self:Say("Oh thank you. You are too kind to this poor halfling. Do you have anything else to give me?");
+		e.self:SetAppearance(0); -- Stand
+		followtarget = e.other:GetID();
+		eq.set_timer("follow",5000);
+	else
+		item_lib.return_items(e.self, e.other, e.trade)
+	end
+end
