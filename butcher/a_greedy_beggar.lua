e12d42308de1f9d36edfa9b2a35b7dfc18d1b40c
TurmoilToad <TurmoilToad@gmail.com>
2019-12-06 09:26:28 -0500

    Update a_greedy_beggar.lua

--- butcher/a_greedy_beggar.lua
+++ butcher/a_greedy_beggar.lua
@@ -1,4 +1,5 @@
 local followtarget;
+local zonecheck;
 
 function event_spawn(e)
 	e.self:Say("event spawn triggered.");
@@ -15,6 +16,7 @@ function event_say(e)
 		e.self:Say("Hello there " .. e.other:GetName() .. ". I don't suppose you can spare some coins? I'm just a poor halfling that is far away from home. I can't afford anything to eat or drink. Anything you can offer me will be of help.");  -- response to all messages, even when following
 		if(followtarget == nil) then
 			followtarget = e.other:GetID();
+			zonecheck = 1;
 			e.self:Say(string.format("Followtarget is %s", followtarget));
 			e.self:SetAppearance(0); -- Stand
 			eq.set_timer("follow",5000);
@@ -24,12 +26,24 @@ function event_say(e)
 end
 
 function event_timer(e)
-	e.self:Say("event timer triggered.");	
-	if(eq.get_entity_list():GetClientByID(followtarget)) then
-		e.self:Say("Follow target was assigned and is in zone.");
-		e.self:Say(string.format("Followtarget is %s", followtarget));
-		eq.follow(followtarget); -- Follow the player who triggered the event
-		e.self:Say("following target.");
+	e.self:Say("event timer triggered.");
+	if(zonecheck == 1) then
+		if(eq.get_entity_list():GetClientByID(followtarget)) then
+			e.self:Say("Follow target was assigned and is in zone.");
+			e.self:Say(string.format("Followtarget is %s", followtarget));
+			eq.follow(followtarget); -- Follow the player who triggered the event
+			e.self:Say("following target.");
+		else
+			zonecheck = nil;
+			e.self:Say(string.format("Followtarget is %s and he is not in zone", followtarget));
+			eq.stop_follow();
+			e.self:Say("Stopping follow.");
+			followtarget = nil;
+			e.self:Say("Followtarget is nil.");
+			eq.stop_timer("follow");
+			e.self:Say("Timer 'follow' stopped.");
+			e.self:SetAppearance(3); -- lying down
+		end
 	else
 		e.self:Say(string.format("Followtarget is %s and he is not in zone", followtarget));
 		eq.stop_follow();
@@ -50,6 +64,7 @@ function event_trade(e) -- Note that we are intentionally accepting any trade, a
 		e.self:Say("Oh thank you. You are too kind to this poor halfling. Do you have anything else to give me?");
 		e.self:SetAppearance(0); -- Stand
 		followtarget = e.other:GetID();
+		zonecheck = 1;
 		e.self:Say(string.format("Followtarget is %s", followtarget));
 		eq.set_timer("follow",5000);
 		e.self:Say("Timer 'follow' set.");
