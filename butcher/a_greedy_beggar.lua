c367a7fff6a8b86ca9befd6c4f80e0434420124a
TurmoilToad <TurmoilToad@gmail.com>
2019-12-06 08:35:34 -0500

    Update a_greedy_beggar.lua

--- butcher/a_greedy_beggar.lua
+++ butcher/a_greedy_beggar.lua
@@ -1,10 +1,12 @@
 local followtarget;
 
 function event_spawn(e)
+	e.self:Say("event spawn triggered.");
 	e.self:Say(string.format("Followtarget is %s", followtarget));
 end
 
 function event_say(e)
+	e.self:Say("event say triggered.");
 	if(e.message:findi("hail")) then
 		e.self:Say("Hello there " .. e.other:GetName() .. ". I don't suppose you can spare some coins? I'm just a poor halfling that is far away from home. I can't afford anything to eat or drink. Anything you can offer me will be of help.");  -- response to all messages, even when following
 		e.self:Say(string.format("Followtarget is %s", followtarget));
@@ -19,6 +21,7 @@ function event_say(e)
 end
 
 function event_timer(e)
+	e.self:Say("event timer triggered.");
 	if(e.timer == "follow" and eq.get_entity_list():GetClientByID(followtarget)) then
 		e.self:Say("Timer 'follow' triggered.");
 		eq.follow(followtarget); -- Follow the player who triggered the event
@@ -38,7 +41,7 @@ end
 
 function event_trade(e) -- Note that we are intentionally accepting any trade, as long as we are not following
 	local item_lib = require("items");
-	
+	e.self:Say("event item triggered.");
 	if(followtarget == nil or e.other:GetID() == followtarget) then
 		e.self:Say(string.format("Followtarget is %s", followtarget));
 		e.self:Say("Oh thank you. You are too kind to this poor halfling. Do you have anything else to give me?");
