9948792834cbcce0a2d9f19f6523d62371c85a7e
Dalethor <30395666+Dalethor@users.noreply.github.com>
2019-12-08 13:47:21 -0500

    Update Waltor_Felligan.pl
    Was missing a lot of ")"

--- halas/Waltor_Felligan.pl
+++ halas/Waltor_Felligan.pl
@@ -44,7 +44,7 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	#:: Match ten gold
-	if (plugin::takeCoin(0,0,10,0) {
+	if (plugin::takeCoin(0,0,10,0)) {
 		#:: Match if faction is Indifferent or better
 		if ($faction <= 5) {
 			quest::Say("The scales have been balanced and the Tribunal has spoken. Yer body shall be saved.");
@@ -60,7 +60,7 @@ sub EVENT_ITEM {
 		}
 	}
 	#:: Match two 13967 - Wooly Fungus
-	elsif (plugin::takeItems(13967 => 2) {
+	elsif (plugin::takeItems(13967 => 2)) {
 		#:: Match if faction is Indifferent or better
 		if ($faction <= 5) {
 			quest::Say("The scales have been balanced and the Tribunal has spoken. Yer body shall be saved.");
@@ -77,7 +77,7 @@ sub EVENT_ITEM {
 		}
 	}
 	#:: Match a 13445 - Mammoth Steaks
-	elsif (plugin::takeItems(13445 => 1) {
+	elsif (plugin::takeItems(13445 => 1)) {
 		#:: Match if faction is Indifferent or better
 		if ($faction <= 5) {
 			quest::Say("The scales have been balanced and the Tribunal has spoken. Yer body shall be saved.");
@@ -93,7 +93,7 @@ sub EVENT_ITEM {
 		}
 	}
 	#:: Match a 13966 - Jar of Fungus
-	elsif (plugin::takeItems(13966 => 1) {
+	elsif (plugin::takeItems(13966 => 1)) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
 			quest::Say("Aye! Ye've filled the jar. I'll see to it that Holana locks this away. Tis difficult to obtain and we can only spare the talents of our young shamans. Allow me to give ye a reward. Thank ye kindly fer yer service.");
