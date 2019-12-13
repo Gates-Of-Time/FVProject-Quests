01dea3584630ea4593a65450cc38775652b3f218
TurmoilToad <TurmoilToad@gmail.com>
2019-12-13 09:03:51 -0500

    Update and rename #Guard_Elias.lua to #Guard_Elias.pl

---
+++ eastkarana/#Guard_Elias.pl
@@ -0,0 +1,22 @@
+sub EVENT_WAYPOINT_ARRIVE {
+	#:: Match waypoint 2
+	if ($wp == 2) {
+		quest::say("Hello, Milea. It is unfortunate for you that your friends have led me right to you. Soon you will join Nerissa in the afterlife. Do you have any last words?");
+		#:: Send a signal "1" to Eastern Plains of Karana >> Milea_Clothspinner (15055) with a 4 second delay
+		quest::signalwith(15055, 1, 4);
+	}
+}
+
+sub EVENT_SIGNAL {
+	#:: Match a signal "1" from /eastkarana/Milea_Clothspinner.pl
+	if ($signal == 1) {
+		quest::say("I thought you would never ask.");
+		#:: Attack Eastern Plains of Karana >> Milea_Clothspinner (15055)
+		quest::attack(15055);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
