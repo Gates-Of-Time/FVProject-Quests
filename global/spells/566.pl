09215d38516ca2a968243a3ba79481ecc66ca946
TurmoilToad <TurmoilToad@gmail.com>
2019-12-15 10:34:23 -0500

    Update and rename 566.lua to 566.pl

---
+++ global/spells/566.pl
@@ -0,0 +1,6 @@
+#:: Common Portal
+
+sub EVENT_SPELL_EFFECT_CLIENT {
+	#:: Zone 21 - commons, $x (effect_base_value2), $y (effect_base_value1), $z (effect_base_value3), $heading (effect_base_value4)
+	quest::movepc(21, 1840, 2, -14, 0);
+}
