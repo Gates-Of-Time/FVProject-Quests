446aebe9c2dc385018120ca1579533947ed8980b
TurmoilToad <TurmoilToad@gmail.com>
2019-12-15 07:29:27 -0500

    Update 605.pl

--- global/spells/605.pl
+++ global/spells/605.pl
@@ -1,11 +1,8 @@
-sub EVENT_SPELL_EFFECT_CLIENT
-{
-	if(($client->GetClientVersionBit() & 4294967292)!= 0)
-	{
-		quest::MovePCInstance(25, 1, -715, -57, 42, 0);
-	}
-	else
-	{
+sub EVENT_SPELL_EFFECT_CLIENT {
+#::	if(($client->GetClientVersionBit() & 4294967292)!= 0) {
+#::		quest::MovePCInstance(25, 1, -715, -57, 42, 0);
+#::	}
+#::	else {
 		quest::movepc(25, -772, 432, 35);
-	}
+#::	}
 }
