beeab55a01bed341f508c7948ca8e314718d891f
TurmoilToad <TurmoilToad@gmail.com>
2019-12-15 07:36:38 -0500

    Update 605.pl

--- global/spells/605.pl
+++ global/spells/605.pl
@@ -1,8 +1,5 @@
+#:: Evacuate: Nek
+
 sub EVENT_SPELL_EFFECT_CLIENT {
-#::	if(($client->GetClientVersionBit() & 4294967292)!= 0) {
-#::		quest::MovePCInstance(25, 1, -715, -57, 42, 0);
-#::	}
-#::	else {
-		quest::movepc(25, -772, 432, 35);
-#::	}
+	quest::movepc(25, -772, 432, 35, 0);
 }
