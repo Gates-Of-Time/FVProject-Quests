18f64a11931fcc3fa1f62770b570d14ce105b30d
TurmoilToad <TurmoilToad@gmail.com>
2019-12-15 12:30:29 -0500

    Update and rename 555.lua to 555.pl

---
+++ global/spells/555.pl
@@ -0,0 +1,6 @@
+#:: Succor: Ro
+
+sub EVENT_SPELL_EFFECT_CLIENT {
+	#:: Zone 35 - sro, $x (effect_base_value2), $y (effect_base_value1), $z (effect_base_value3), $heading (effect_base_value4)
+	quest::movepc(35, 304, -2000, -25, 0);
+}
