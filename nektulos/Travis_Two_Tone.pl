67a1649e1bd3d7687687bde3ff7d76b25cc7a0fc
TurmoilToad <TurmoilToad@gmail.com>
2019-12-16 20:16:38 -0500

    Update Travis_Two_Tone.pl

--- nektulos/Travis_Two_Tone.pl
+++ nektulos/Travis_Two_Tone.pl
@@ -1,27 +1,21 @@
-#############
-#Quest Name: Bard Mail Quest
-#Author: RealityIncarnate
-#NPCs Involved: Tralyn Marsinger, Eve Marsinger, Lislia Goldtune, Felisity Starbright, Jakum Webdancer, Ton Twostring, Idia, Sivina Lutewhisper, Ticar Lorestring, Marton Stringsinger, Drizda Tunesinger, Travis Two Tone, Silna Songsmith, Siltria Marwind, Tacar Tissleplay, Kilam Oresinger, Lyra Lyrestringer 
-#Items Involved: Bardic letters: 18150-18167
-#################
-
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Hail, $name - Are you [interested] in helping the League of Antonican Bards by delivering some [mail]?");
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail, $name - Are you [interested] in helping the League of Antonican Bards by delivering some [mail]?");
+	}
+	elsif ($text=~/mail/i) {
+		quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers.  We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices.  Are you [interested]?");
+	}
+	elsif ($text=~/interested/i) {
+		quest::say("I have messages that need to go to - well, right now I have one that needs to go to Freeport.  Will you deliver mail to [Freeport] for me?");
+	}
+	elsif ($text=~/freeport/i) {
+		quest::say("Take this letter to Felisity Starbright in Freeport.  You can find her at the bard guild hall.  I am sure she will compensate you for your troubles.");
+		#:: Give a 18158 - Bardic Letter (Freeport)
+		quest::summonitem(18158);
+	}
 }
 
-if($text=~/what mail/i){
-quest::say("The League of Antonican Bards has a courier system made up of travelers and adventurers.  We pay good gold to anyone who will take messages from bards such as myself to one of our more central offices.  Are you [interested]?");
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-if($text=~/i am interested/i){
-quest::say("I have messages that need to go to - well, right now I have one that needs to go to Freeport.  Will you [deliver] mail to [Freeport] for me?");
-}
-
-if($text=~/deliver to Freeport/i){
-quest::say("Take this letter to Felisity Starbright in Freeport.  You can find her at the bard guild hall.  I am sure she will compensate you for your troubles."); 
-quest::summonitem("18158");
-}
-
-}
-#END of FILE Zone:nektulos
