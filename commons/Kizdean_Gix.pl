684fe6c20a59a08c36ddc2d0c15bcbb4dd5db051
TurmoilToad <TurmoilToad@gmail.com>
2019-12-23 08:38:47 -0500

    Update Kizdean_Gix.pl

--- commons/Kizdean_Gix.pl
+++ commons/Kizdean_Gix.pl
@@ -1,8 +1,15 @@
+#:: Create a scalar variable to store a response state
+my $ResponseVersion;
+
+sub EVENT_SPAWN {
+	$ResponseVersion = quest::ChooseRandom(1, 2);
+}
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("If you have business then out with it!! If not, then please be gone. I have little time to waste on chitchat!");
 	}
-	#:: There is some evidence that there is no response without proper faction
+	#:: There is some evidence that there are no responses without proper faction
 	elsif ($text=~/utalk adarev otcin/i) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
@@ -11,10 +18,42 @@ sub EVENT_SAY {
 			quest::summonitem(18891);
 		}
 	}
+	elsif ($text=~/the dead/i) {
+		quest::say("The Dead are the combined might of the [Teir'Dal] shadowknights and necromancers. A mightier force cannot be found in all of Norrath.");
+	}
+	elsif ($text=~/teir'dal/i) {
+		#:: quest::say("Response unknown.");
+	}
 	elsif ($text=~/thex mallet/i) {
-		#:: Match if faction is Amiable or better
-		if ($faction <= 4) {
-			quest::say("The Thex Mallet is a very special weapon of elven creation. It is as ancient as Norrath itself. Queen Cristanos commands that we find it and return it to Loveal S'nez. That is all you need to know.");
+		if ($ResponseVersion == 2) { 
+			#:: Match if faction is Amiable or better
+			if ($faction <= 4) {
+				quest::say("The Thex Mallet is a very special weapon of elven creation. It is as ancient as Norrath itself. Queen Cristanos commands that we find it and return it to Loveal S'nez. That is all you need to know.");
+			}
+		}
+	}
+	elsif ($text=~/messages for neriak/i) {
+		if ($ResponseVersion == 1) { 
+			#:: Match if faction is Amiable or better
+			if ($faction <= 4) {
+				quest::say("It's about time a courier reached these wastelands of humanity. I have urgent news for the Dead. Take this message at once. Let no one stop you. Be on the lookout for the Feir'Dal [Silent Watch]! They have learned of our quest.");
+				#:: Give a 18889 - Sealed Letter
+				quest::summonitem(18889);
+			}
+		}
+		else {
+			#:: Match if faction is Amiable or better
+			if ($faction <= 4) {
+				quest::say("Inform the Dead that I have no further information at this time. I shall continue with my work as directed. Please return regularly, for I am expecting to break new ground.");
+			}
+		}
+	}
+	elsif ($text=~/silent watch/i) {
+		if ($ResponseVersion == 1) { 
+			#:: Match if faction is Amiable or better
+			if ($faction <= 4) {
+				quest::say("The Koada'Dal have created an elite band of paladins, greater even than our own Teir'Dal Dragoons. They exist on the edge of righteousness. They will perform despicable acts in the name of their deity, barely skimming the edge of evil.");
+			}
 		}
 	}
 }
