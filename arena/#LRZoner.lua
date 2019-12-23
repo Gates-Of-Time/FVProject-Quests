17d057c63a3290ffa3e5df1617c5f90d9109fd2f
TurmoilToad <TurmoilToad@gmail.com>
2019-12-23 10:17:58 -0500

    Update #LRZoner.lua

--- arena/#LRZoner.lua
+++ arena/#LRZoner.lua
@@ -1,7 +1,7 @@
 function event_spawn(e)
-	eq.set_proximity(e.self:GetX() - 40, e.self:GetX() + 40, e.self:GetY() - 40, e.self:GetY() + 40);
+	eq.set_proximity(e.self:GetX()-30, e.self:GetX()+30, e.self:GetY()-30, e.self:GetY()+30);
 end
 
 function event_enter(e)
-	e.self:MovePC(51, 2643, 2419, 97, 376.5);
+	e.other:MovePC(51, 2643, 2419, 97, 376.5);
 end
