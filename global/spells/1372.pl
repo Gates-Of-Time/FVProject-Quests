ad801e280688f8e1ad48159dcc619d5d66222de4
TurmoilToad <TurmoilToad@gmail.com>
2019-12-24 16:53:24 -0500

    Update 1372.pl

--- global/spells/1372.pl
+++ global/spells/1372.pl
@@ -1,8 +1,5 @@
 #:: Translocate: Common
 
 sub EVENT_SPELL_EFFECT_TRANSLOCATE_COMPLETE {
-	#:: Create a scalar variable to store the target's client ID
-	my $TargetClient = $entity_list->GetClientByID($targetid);
-	#:: Move the target to commons at the given coordinates
-	$TargetClient->MovePC(21, 1839, 2, -15, 60);
+	quest::movepc(21, 1839, 2, -15, 60);
 }
