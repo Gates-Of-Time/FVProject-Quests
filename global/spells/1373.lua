2d0f9ebaf1911b42325d10c56a36155145baf2c5
TurmoilToad <TurmoilToad@gmail.com>
2019-12-24 16:59:21 -0500

    Delete 1373.lua

--- global/spells/1373.lua
+++
@@ -1,7 +0,0 @@
--- Translocate: Ro 
-function event_spell_effect_translocate_complete(e) 
-  local client = eq.get_entity_list():GetClientByID(e.target:GetID());
-  client:MovePC(34, 820, 1374, 10, 40);
-
-  return 1;
-end
