ff41e50d1c4b3020b7412cd7ab4a708e9e22d5f6
TurmoilToad <TurmoilToad@gmail.com>
2019-12-23 20:33:59 -0500

    Update and rename #LRZoner.lua to #LRZoner.pl

--- arena/#LRZoner.lua
+++
@@ -1,7 +0,0 @@
-function event_spawn(e)
- 	eq.set_proximity(e.self:GetX() - 40, e.self:GetX() + 40, e.self:GetY() - 40, e.self:GetY() + 40);
- end
-
- function event_enter(e)
- 	e.other:MovePCInstance(51, 0, 2643, 2419, 97, 376.5);
- end
