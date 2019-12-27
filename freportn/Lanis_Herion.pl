187a4cf1ff6c2403b3f8f6c732e2293fc7d743f8
TurmoilToad <TurmoilToad@gmail.com>
2019-12-27 14:36:19 -0500

    Update Lanis_Herion.pl

--- freportn/Lanis_Herion.pl
+++ freportn/Lanis_Herion.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings $name. I hope you are following the teachings of the Queen of Passion, Erollisi Marr. The ways of love are many and shall warm the souls of all.");
+	}
+}
+
 sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
