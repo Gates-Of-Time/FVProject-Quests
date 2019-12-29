8e191d33a6cc1c4280773c64cc5b6fa6c04009cb
TurmoilToad <TurmoilToad@gmail.com>
2019-12-29 16:48:34 -0500

    Update and rename Neclo_Rheslar.lua to Neclo_Rheslar.pl

---
+++ qeytoqrg/Neclo_Rheslar.pl
@@ -0,0 +1,33 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Ah.. Hello citizen.. Watch your step, there have been many vicious beasts about today.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 18823 - Note to Neclo
+	if (plugin::takeItems(18823 => 1)) {
+		#:: Match if faction is Indifferent or better
+		if ($faction <= 5) {
+			quest::say("Ah.. Hello friend.. So, I see Daenor sent you.. Uh huh, ok.. Here's something that will be very useful for you. Practice this well, friend.. It will help protect you in this harsh world.");
+			#:: Give a 15288 - Spell: Minor Shielding
+			quest::summonitem(15288);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(342, 5);			#:: + Order of Three
+			quest::faction(221, -1);		#:: - Bloodsabers
+			quest::faction(262, 1);			#:: + Guards of Qeynos
+			quest::faction(296, -1);		#:: - Opal Dark Briar
+			#:: Grant a moderate amount of experience
+			quest::exp(1000);
+		}
+		else {
+			quest::say("The Order of Three has been monitoring your recent activities, and we are appalled by you and your actions! Now, begone!");
+			#:: Return a a 18823 - Note to Neclo
+			quest::summonitem(18823);
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
