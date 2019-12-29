a5b652b2749121de864555f23fe3669659fb35e0
TurmoilToad <TurmoilToad@gmail.com>
2019-12-28 19:53:24 -0500

    Create a_Splitpaw_assassin.pl

---
+++ qey2hh1/a_Splitpaw_assassin.pl
@@ -0,0 +1,17 @@
+sub EVENT_SPAWN {
+	quest::say("You cannot escape the claws of Splitpaw! <BARK!>");
+	#:: Send a signal '1' to The Western Plains of Karana >> Yiz_Pon (12167) with no delay
+	quest::signalwith(12167, 1, 0);
+}
+
+sub EVENT_SIGNAL {
+	#:: Match a signal '1' from /qey2hh1/Yiz_Pon.pl
+	if ($signal == 1) {
+		quest::say("<BARK!> <BARK!> <BARK!> <GRRRRR!>");
+		quest::attacknpctype(12167);
+	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My Splitpaw Clan brothers will tear you to shreds one day! <BARK!>");
+}
