a6bf634d2b4bec8c351789ac12d222c0f926ce21
TurmoilToad <TurmoilToad@gmail.com>
2019-12-30 13:41:17 -0500

    Update and rename a_banished_Kerran.pl to a_banished_kerran.pl

--- kerraridge/a_banished_kerran.pl
+++ kerraridge/a_banished_kerran.pl
@@ -1,21 +1,21 @@
 sub EVENT_SAY {
-	if ($text=~/Hail/i) {
+	if ($text=~/hail/i) {
 		quest::say("Rrrrrr..  Away frrrom this isle of death and.. prrrr.. punishment.");
 	}
-	if ($text=~/remains of Thipt/i) {
+	elsif ($text=~/remains of thipt/i) {
 		quest::say("Clawknight Thipt wished to learn the ways of Ashen.  Prrr.. Together we go into green goblins' lair to train.  Prrr.. We find too many and he is too weak.  Separated and unable to find him, I hear his death cry.  Prr.. I cannot make it back, eyes upon me.  Now I must find one sent by Ashen to aid in the recoverrry of his remains.");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Match a 12369 - Puab's Token
-	if (plugin::check_handin(\%itemcount, 12369 => 1)) {
+	if (plugin::takeItems(12369 => 1)) {
 		quest::say("Ashen Order!!  Prrr.. My order.  I have been expecting one of us.  Prrr.. Help me rejoin my native land.  Take this box.  Combine all the [remains of Thipt] within the box and return it to me.  This shall aid me in my redemption.");
 		#:: Give a 17985 - Box With Pawprints
 		quest::summonitem(17985);
 	}
 	#:: Match a 12371 - Box of Cat Bones
-	if (plugin::check_handin(\%itemcount, 12371 => 1)) {
+	elsif (plugin::takeItems(12371 => 1)) {
 		quest::say("Prrr.. Thank you brother of Ashen. I can now spend my time upon this island in peace, until my penance is serrrved. He dabs his paw into the mud and places it upon a tattered parchment. Take this message to Puab. Farrrwell.");
 		#:: Give a 28055 - Tattered Parchment
 		quest::summonitem(28055);
@@ -25,5 +25,5 @@ sub EVENT_ITEM {
 		quest::exp(100);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
