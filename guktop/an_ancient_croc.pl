f3d851b739b7f894a1a91620ecc63e806afeb994
TurmoilToad <TurmoilToad@gmail.com>
2019-12-29 20:18:44 -0500

    Update an_ancient_croc.pl

--- guktop/an_ancient_croc.pl
+++ guktop/an_ancient_croc.pl
@@ -1,4 +1,4 @@
 sub EVENT_DEATH_COMPLETE {
-	#:: Send a signal '1' to The City of Guk >> croc_trigger (65150) after a 1 second delay
-	quest::signalwith(65150, 1, 1);
+	#:: Send a signal '1' to The City of Guk >> croc_trigger (65150) with no delay
+	quest::signalwith(65150, 1, 0);
 }
