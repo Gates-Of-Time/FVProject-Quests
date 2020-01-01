66b8b27041d2de3279da1ec1c6d6373603a0b6c5
TurmoilToad <TurmoilToad@gmail.com>
2020-01-01 12:09:03 -0500

    Update Krazen_Loosh.pl
    Refactor for consistency

--- freportw/Krazen_Loosh.pl
+++ freportw/Krazen_Loosh.pl
@@ -1,6 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Run	
-	quest::SetRunning(1); 
+	#:: Set running true (run)
+	quest::SetRunning(1);
 }
 
 sub EVENT_SAY {
@@ -9,24 +9,23 @@ sub EVENT_SAY {
 	}
 }
 
-#:: NPC speaks at waypoint 30 then speaks and runs at waypoint 42 
 sub EVENT_WAYPOINT_ARRIVE {
 	if ($wp == 8) {
 		quest::say("Hello, Alayle. We just got a message from Qeynos. I think you should come with me.");
-		#:: Signal Guard Alayle
-		quest::signalwith(9141,1,0);
-		#:: Walk
+		#:: Send a signal '1' to West Freeport >> Guard_Alayle (9141) with no delay
+		quest::signalwith(9141, 1, 0);
+		#:: Set running false (walk)
 		quest::SetRunning(0);
 	}
 } 
 
 sub EVENT_SIGNAL { 
-	#:: Receive Signal 1 from Guard Alayle
+	#:: Match a signal '1' from /freportw/Guard_Alayle.pl
 	if ($signal == 1) {
 		quest::say("As you try to open penetrate the stone wall it is clearly being held in place by a powerful force.");
-		#:: Attack Guard  Alayle
+		#:: Attack West Freeport >> Guard_Alayle (9141)
 		quest::attacknpctype(9141);
-		#:: Signal Guard Lithnon
-		quest::signalwith(9106,1,0);
+		#:: Send a signal '1' to West Freeport >> Guard_Lithnon (9106) with no delay
+		quest::signalwith(9106, 1, 0);
 	}
 }
