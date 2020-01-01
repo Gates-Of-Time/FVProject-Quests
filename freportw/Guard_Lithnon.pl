6de32de79c5a8af5a8013678cf441d83d633a28c
TurmoilToad <TurmoilToad@gmail.com>
2020-01-01 12:10:20 -0500

    Update Guard_Lithnon.pl
    Refactor for consistency

--- freportw/Guard_Lithnon.pl
+++ freportw/Guard_Lithnon.pl
@@ -1,7 +1,12 @@
 sub EVENT_SIGNAL { 
-	#:: Receive Signal 1 from Krazen Loosh
+	#:: Match a signal '1' from /freportw/Krazen_Loosh.pl
 	if ($signal == 1) {
-		#:: Attack Guard Alayle
+		#:: Attack West Freeport >> Guard_Alayle (9141)
 		quest::attacknpctype(9141); 
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
