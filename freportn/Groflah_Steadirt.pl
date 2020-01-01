4b0152886bbb1b3b6eea3eb42005c1e8e0cc5664
FVFippy <44533609+FVFippy@users.noreply.github.com>
2020-01-01 18:09:39 -0500

    Update Groflah_Steadirt.pl
    This Groflah should give back the exact same tattered flier (18818).  The Groflah at the bar is the one who will give you the 18819 version

--- freportn/Groflah_Steadirt.pl
+++ freportn/Groflah_Steadirt.pl
@@ -11,8 +11,8 @@ sub EVENT_ITEM {
 	#:: Turn in for 18818 - a tattered flier
 	if (plugin::takeItems(18818 => 1 )) {
 		quest::say("Where did you find this? This was the main price list of Zimel's Blades, but it should be all burnt up. I was at Zimel's right after the fire and I did not see it hanging where it should have been. The entire inside was gutted and . . . wait . . . the sequence of the dots!! Hmmm. I cannot talk with you here. Meet me at the Seafarer's by the docks at night. Give me the note when next we meet.");
-		#:: Give item 18819 - a tattered flier
-		quest::summonitem(18819);
+		#:: Give item 18818 - a tattered flier
+		quest::summonitem(18818);
 	}
 	#:: Turn in for 13919 Reward Raw Short Sword
 	if (plugin::takeItems(13919 => 1)) {
