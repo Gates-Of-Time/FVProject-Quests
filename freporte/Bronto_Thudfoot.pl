629a21efa85c1362c0e5e54d2ae60ecc903f790a
TurmoilToad <TurmoilToad@gmail.com>
2020-01-03 18:08:34 -0500

    Update Bronto_Thudfoot.pl

--- freporte/Bronto_Thudfoot.pl
+++ freporte/Bronto_Thudfoot.pl
@@ -1,5 +1,11 @@
-#:: Match a signal "1" from /freporte/Lenka_Stoutheart.pl
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("How ya doin' bub. Seeing as you just joined the conversation, I think you need to buy us a round.");
+	}
+}
+
 sub EVENT_SIGNAL {
+	#:: Match a signal "1" from /freporte/Lenka_Stoutheart.pl
 	if ($signal == 1) {
 		quest::say("You said it, boss!  Stay clear of taking sides and you should be just fine, young one.");
 	}
