103de8b5e0104bdf4f5955926e661ed5f003d666
TurmoilToad <TurmoilToad@gmail.com>
2020-01-03 14:18:44 -0500

    Update Trolon_Lightleer.pl
    Refactor for consistency, added signal '1'

--- freporte/Trolon_Lightleer.pl
+++ freporte/Trolon_Lightleer.pl
@@ -1,5 +1,5 @@
 sub EVENT_SPAWN {
-	#:: Set up a timer for five minutes
+	#:: Create a timer 'ready' that loops every 300 seconds (5 minutes)
 	quest::set_timer("ready", 300);
 }
 
@@ -10,31 +10,33 @@ sub EVENT_SAY {
 }
 
 sub EVENT_TIMER {
-	#:: Catch the "ready timer
+	#:: Match timer 'ready'
 	if ($timer eq "ready") {
 		quest::say("What a cheerful crowd.");
 	}
 }
 
 sub EVENT_SIGNAL {
-	#Signal From Henna Treghost
-	#:: Create a scalar to store a random
-	my $singasong = quest::ChooseRandom(1,2);
-	#:: Match singasong = 1
-	if ($singasong == 1) { 
-		quest::say("Here's one for Sir Lucan... He's just a ruler. In a long line of rulers. Looking for a few more orcs to slay. They say that he fights them with one hand tied to his back. And he won't leave Freeport till you remember his name. Thank you.");
+	#:: Match a signal '1' from /freporte/Henna_Treghost.pl
+	if ($signal == 1) {
+		#:: Create a scalar to store a random number
+		my $singasong = quest::ChooseRandom(1, 2);
+		#:: Match singasong = 1
+		if ($singasong == 1) {
+			quest::say("Here's one for Sir Lucan... He's just a ruler. In a long line of rulers. Looking for a few more orcs to slay. They say that he fights them with one hand tied to his back. And he won't leave Freeport till you remember his name. Thank you.");
+		}
+		#:: Match singasong = 2
+		elsif ($singasong == 2) {
+			quest::say("Look around you. Stand up tall!! Feel for the poor and never have a cold heart. Becausee we are all in this. We are not like the rest. We're all here together and know we are the best. So Norrath, can't you see the error in your ways? You're living separated each and every day. Don't be thinking that we don't want you, because, Norrath, we do. Freeport is the only place where your friend can be from afar. I hope you enjoyed that one.");
+			#:: Send a signal '1' to East Freeport >> Sir_Edwin_Motte (10199) with no delay
+			quest::signalwith(10199, 1, 0);
+			#:: Send a signal '1' to East Freeport >> Talym_Shoontar (10182) with a 3 second delay
+			quest::signalwith(10182, 1, 3);
+		}
 	}
-	#:: Match singasong = 2
-	elsif ($singasong == 2) { 
-		quest::say("Look around you. Stand up tall!! Feel for the poor and never have a cold heart. Becausee we are all in this. We are not like the rest. We're all here together and know we are the best. So Norrath, can't you see the error in your ways? You're living separated each and every day. Don't be thinking that we don't want you, because, Norrath, we do. Freeport is the only place where your friend can be from afar. I hope you enjoyed that one.");
-		#:: Send Signal to Sir Edwin Motte of East Freeport
-		quest::signal(10199,1);
-		#:: Send Signal to Talym Shoontar of East Freeport
-		quest::signal(10182,3);
-	}	
 }
 
 sub EVENT_ITEM {
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
