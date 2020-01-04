b63af8f04a8a3f19611fb18a60f629e9b212b9d7
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 18:04:10 -0500

    Update Giz_Dinree.pl
    Added hand in.

--- freporte/Giz_Dinree.pl
+++ freporte/Giz_Dinree.pl
@@ -2,7 +2,16 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Excuse me! I came here in hopes of being alone!");
 	}
-	if ($text=~/sent by tani/i) {
+	elsif ($text=~/sent by tani/i) {
 		quest::say("You're a new rogue, then! Your mission is to kill the young daughter of the owner of the Seafarer's Roost. Wait for the right time. Why? You don't need to know. Do it and return Lyda's head to Tani in Neriak. I hope you can make it back to Neriak alive. Good hunting!");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Match a 18844 - Sealed Letter
+	if (plugin::takeItems(18844 => 1)) {
+		quest::say("I am glad to see you. We have a problem. The last runner and I attempted to carry the chest from a boat. It fell overboard! He went in after it, but the sharks made a meal of him. If you want to try and get it, it is down below in the water in the harbor. Be careful.");
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
