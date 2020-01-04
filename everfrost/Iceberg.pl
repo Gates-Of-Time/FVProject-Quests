1676dc08fe8bafb1284e998205a1271b10a9a9aa
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 10:43:53 -0500

    Update Iceberg.pl
    Update and fix timer, refactor for clarity and consistency, adjust faction reward.

--- everfrost/Iceberg.pl
+++ everfrost/Iceberg.pl
@@ -1,12 +1,15 @@
 sub EVENT_SPAWN {
-	quest::settimer("follow",1000);
+	#:: Create a timer 'follow' that loops every 30 seconds
+	quest::settimer("follow", 30);
 }
 
 sub EVENT_TIMER {
+	#:: Create a scalar variable to store the entity ID of Everfrost Peaks >> Tundra_Jack (30061)
 	my $tundrajack = $entity_list->GetMobByNpcTypeID(30061);
-	if ($timer = "follow") {
+	#:: Match the timer 'follow'
+	if ($timer eq "follow") {
+		#:: Follow Tundra Jack
 		quest::follow($tundrajack);
-		quest::stoptimer("follow");
 	}
 }
 
@@ -14,35 +17,34 @@ sub EVENT_COMBAT {
 	#:: Match combat_state 1 (true) for entered combat
 	if ($combat_state == 1) {
 		quest::say("Grrroarrr !!");
-		#:: Send a signal "1" to Tundra Jack
-		quest::signal(30061,1);
+		#:: Send a signal '1' to Everfrost Peaks >> Tundra_Jack (30061) with no delay
+		quest::signalwith(30061, 1, 0);
 	}
-	if ($combat_state == 0) {
-		#:: Send a signal "3" to Tundra Jack
-		quest::signal(30061,3);
+	#:: Match combat_state 0 (false) for not in combat
+	elsif ($combat_state == 0) {
+		#:: Send a signal '3' to Everfrost Peaks >> Tundra_Jack (30061) with no delay
+		quest::signalwith(30061, 3, 0);
 	}
 }
 
-# Langseax quest
-
 sub EVENT_ITEM {
-	#:: Match 12221 - Lion Delight
-	if (plugin::check_handin(\%itemcount, 12221 => 1)) {
+	#:: Match a 12221 - Lion Delight
+	if (plugin::takeItems(12221 => 1)) {
 		quest::emote("growls with happiness and licks your face.  Just enough time to swipe the sweaty shirt from his collar!!  Iceberg then runs off to enjoy his lion delight!!");
-		#:: Send a signal "2" to Tundra Jack
-		quest::signal(30061,2);
+		#:: Send a signal '2' to Everfrost Peaks >> Tundra_Jack (30061) with no delay
+		quest::signalwith(30061, 2, 0);
 		#:: Give a 12226 - Sweaty Shirt
 		quest::summonitem(12226);
+		#:: Ding!
 		quest::ding();
 		#:: Set Factions
-		quest::faction(328,5);	#:: Merchants of Halas
-		quest::faction(327,5);	#:: Shaman of Justice
-		quest::faction(311,5);	#:: Steel Warriors
-		quest::faction(320,5);	#:: Wolves of the North
+		quest::faction(328, 2);		#:: Merchants of Halas
+		quest::faction(327, 1);		#:: Shaman of Justice
+		quest::faction(311, 1);		#:: Steel Warriors
+		quest::faction(320, 1);		#:: Wolves of the North
+		#:: Grant a moderate amount of experience
 		quest::exp(1000);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
-
-# Converted to Perl by SS
