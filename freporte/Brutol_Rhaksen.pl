8333058d3a501a0a3b8a02d25c580a1f1de6b0d6
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 16:35:44 -0500

    Update Brutol_Rhaksen.pl
    Refactor for consistency, added missing dialogue, removed LoY responses and turn-ins.

--- freporte/Brutol_Rhaksen.pl
+++ freporte/Brutol_Rhaksen.pl
@@ -1,55 +1,34 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Oh. hello down there. puny one. I'm Brutol Rhaksen. Commmander of warriors who serve the Dismal Rage. and that's really all you need to know..for now.");
+		quest::say("Oh, hello down there, puny one. I'm Brutol Rhaksen, Commmander of warriors who serve the Dismal Rage, and that's really all you need to know..for now.");
 	}
-	#Dismal Warrior Armor Quest Dialog (Ykesha)
-	#if ($text=~/serve/i) {
-		#quest::say("You must become strong to survive amongst the ranks of the Dismal Rage. Take this note to Rolfic Gohar and he will help you get a suit of armor to protect your scrawny hide from the weapons of our enemies. Once you have been properly outfitted return to me and will give you your [next orders].");
-		#:: Give item 19843 - Note to Rolfic Gohar
-		#quest::summonitem(19843);
-	#}
-	#Dismal Warrior Armor Quest Dialog (Ykesha)
-	#if ($text=~/next orders/i) {
-		#quest::say("Ready to make yourself useful $name? Beneath West Freeport are sewer tunnels leading to North Freeport being used by the Knights of Truth and the Sentries and Passion that have gained too much notoriety with the Freeport Militia and the Dismal Rage to pass safely through the eastern and western quarters of Freeport. We believe a sympathizer of the Sentries of Passion. Tarsa Yovar. is hiding somewhere in the western tunnel system. The sympathizer is a Steel Warrior faithful to Erollisi and although she is only a minor nuisance must be dealt with. Find her and bring me her head.");
-	#}
+	elsif ($text=~/trades/i) {
+		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		#:: Give a 51121 - Tradeskill Basics : Volume I
+		quest::summonitem(51121);
+	}
+	elsif ($text=~/second book/i) {
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		#:: Give a 51122 - Tradeskill Basics : Volume II
+		quest::summonitem(51122);
+	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18857 -  A Tattered Note
-	if (plugin::check_handin(\%itemcount, 18857 => 1)) {
-		quest::say("Hahaha.. I sure hope you prove more valuable than you look, little one.");
-		#:: Give item 13561 - Faded Crimson Tunic
+	#:: Match a 18857 - A Tattered Note
+	if (plugin::takeItems(18857 => 1)) {
+		quest::say("Hahaha... I sure hope you prove more valuable than you look, little one. Once you are ready to begin your training please let me know and I will get you started. I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		#:: Give a 13561 - Faded Crimson Tunic
 		quest::summonitem(13561);
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
 		#:: Set faction
-		quest::faction(271,100); 	#:: + Dismal Rage
-		quest::faction(281,-15); 	#:: - Knights of Truth
-		quest::faction(296,20); 	#:: + Opal Dark Briar
+		quest::faction(271, 100); 	#:: + Dismal Rage
+		quest::faction(281, -15); 	#:: - Knights of Truth
+		quest::faction(296, 20); 	#:: + Opal Dark Briar
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
-	#Dismal Warrior Armor Quest Dialog (Ykesha)
-	#:: Turn in for 19932 -  Tarsa Yovar's Head
-	#if (plugin::check_handin(\%itemcount, 19932 => 1)) {
-		#quest::say("Take this and get it sharpened. Bring it back to me with a Giant Rattlesnake Skin and you will have proven yourself able to wield a Dismal Rage Battle Axe, to help teach the way of Innoruuk!");
-		#:: Give item 19921 - Dull Dismal Battle Axe
-		#quest::summonitem(19921);
-		#:: Give a small amount of xp
-		#quest::exp(100);
-		#:: Ding!
-		#quest::ding();
-	#}
-	#Dismal Warrior Armor Quest Dialog (Ykesha)
-	#:: Turn in for 19936 and 19852 -  Sharpened dismal battleaze and Giant Rattlesnake Skin
-	#if (plugin::check_handin(\%itemcount, 19936 => 1, 19852 => 1)) { {
-		#quest::say("You have proven your faith $name. Take this to vanquish any and all whom question the absolute power of Innoruuk!");
-		#:: Give item 19938 - Dismal Rage Battle Axe
-		#quest::summonitem(19938);
-		#:: Give a small amount of xp
-		#quest::exp(100);
-		#:: Ding!
-		#quest::ding();
-	#}
-	plugin::return_items(\%itemcount);
-}	
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
