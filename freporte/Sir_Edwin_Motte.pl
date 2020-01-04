d005a2bd9ef91184309e7923993d39f4de688f0a
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 15:23:35 -0500

    Update Sir_Edwin_Motte.pl
    Fix hail dialogue

--- freporte/Sir_Edwin_Motte.pl
+++ freporte/Sir_Edwin_Motte.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Greetings, $class! How lucky you are to encounter the greatness of the legendary Sir Edwin Motte, slayer of cyclopes, battler of beasts, crusher of creatures, masher of monsters, eradicator of evil and champion of the third annual dart championship of Freeport, $name.");
+		quest::say("Greetings, $name! How lucky you are to encounter the greatness of the legendary Sir Edwin Motte, slayer of cyclopes, battler of beasts, crusher of creatures, masher of monsters, eradicator of evil and champion of the third annual dart championship of Freeport.");
 	}
 }
 
