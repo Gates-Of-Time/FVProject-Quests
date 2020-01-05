8d6a41c467c873d12de4280a0a9e78d76091cd81
TurmoilToad <TurmoilToad@gmail.com>
2020-01-05 10:30:03 -0500

    Update Saxarivza_Zexus.pl
    Refactor for consistency

--- freporte/Saxarivza_Zexus.pl
+++ freporte/Saxarivza_Zexus.pl
@@ -2,7 +2,7 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Salutations, $name! I am here as a favor to my brother, High Priest Perrir. We seek loyal followers of Innoruuk to earn their place among the hierarchy of the Spires. Are you [devoted to Innoruuk]?");
 	}
-	if ($text=~/devoted to Innoruuk/i) {
+	elsif ($text=~/devoted to Innoruuk/i) {
 		quest::say("My brother requests your presence in Neriak at the Spires of Innoruuk. Take this note with you, $name. It will inform Perrir of who sent you.");
 		#:: Give item 18148 - Letter of recommendation
 		quest::summonitem(18148);
@@ -11,5 +11,5 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
