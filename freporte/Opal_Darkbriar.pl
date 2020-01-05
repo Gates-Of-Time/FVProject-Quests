69679fd57b863bd4644c80818b3899efa84a814f
TurmoilToad <TurmoilToad@gmail.com>
2020-01-05 08:58:45 -0500

    Update Opal_Darkbriar.pl
    Refactor for consistency, removed cash reward.

--- freporte/Opal_Darkbriar.pl
+++ freporte/Opal_Darkbriar.pl
@@ -5,24 +5,23 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18742 -  A tattered note
-	if (plugin::check_handin(\%itemcount, 18742 => 1)) {
+	#:: Match a 18742 - A tattered note
+	if (plugin::takeItems(18742 => 1)) {
 		quest::say("Welcome to the Guild, here's your guild robe. Now, let's get to work.");
 		#:: Give item 13562 - Dark Stained Robe
 		quest::summonitem(13562);
-		#:: Give a small amount of xp
-		quest::exp(100);
-		#:: Give a small amount of cash copper - plat
-		quest::givecash(1,1,5,4);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(296,100); 	#:: + Opal Dark Briar
-		quest::faction(271,25); 		#:: + Dismal Rage
-		quest::faction(239,25); 	#:: + The Dead
-		quest::faction(238,25); 		#:: + Dark Reflection
-		quest::faction(221,25); 		#:: + Bloodsabers		
+		#:: Set factions
+		quest::faction(296, 100); 		#:: + Opal Darkbriar
+		quest::faction(271, 25); 		#:: + Dismal Rage
+		quest::faction(239, 25); 		#:: + The Dead
+		quest::faction(238, 25); 		#:: + Dark Reflection
+		quest::faction(221, 25); 		#:: + Bloodsabers
+		#:: Grant a small amount of experience
+		quest::exp(100);
+		
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
