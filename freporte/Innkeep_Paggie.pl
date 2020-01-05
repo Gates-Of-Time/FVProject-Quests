fc0bb460b60ef81eb9d7deccf6aa72ddee909a30
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:11:15 -0500

    Update Innkeep_Paggie.pl
    Fix hail response, refactor for consistency

--- freporte/Innkeep_Paggie.pl
+++ freporte/Innkeep_Paggie.pl
@@ -1,10 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hello there $class! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around, $name");
+		quest::say("Hello there $name! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around.");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
