e03d2dc50549eb8a43e3af1c69b5ba6c377076b2
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:36:58 -0500

    Update Jyle_Windshot.pl
    Refactor for consistency, adjust hand in

--- freporte/Jyle_Windshot.pl
+++ freporte/Jyle_Windshot.pl
@@ -1,28 +1,28 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Greetings, friend!! Nice to meet you. I see you enjoy barging into other people's rooms. No problem. I enjoy the company. What are you looking for?");
+		quest::say("Hello. Might I ask what you are looking for?");
 	}
-	if ($text=~/treant wood/i) {
-		quest::say("So you want a treant shards? I have them. If you want them I will trade you for them. Hmm.. I will take 2 gold and how about a nifty Faydark Champion Tunic. That is all I want. No more, no less.");
+	elsif ($text=~/treant wood/i) {
+		quest::say("You are a new courier from the Faydarks. I am sorry I did not stay closer to the docks. I hear that part of Freeport is dangerous and is filled with many rogues. I shall require a new Small Lantern in trade for the Treant Wood.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13536 Dirty Green Tunic and 2 Gold 
-	if (plugin::takeItemsCoin(0,0,2,0, 13536 => 1)) {
+	#:: Match a 13003 - Small Lantern
+	if (plugin::takeItems(13003 => 1)) {
 		quest::say("Thanks, friend. I have run a long way to get here in time. Mostly at night. I lost my lantern in a card game in Highkeep.");
 		#:: Randomly choose from Wooden Shards 90% chance, or A Wooden Heart 10% chance
 		quest::summonitem(quest::ChooseRandom(13824, 13824, 13824, 13824, 13824, 13824, 13824, 13824, 13824, 12334));
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(246,1); 		#:: + Faydark's Champions
-		quest::faction(279,1); 		#:: + King Tearis Thex
-		quest::faction(226,1); 		#:: + Clerics of Tunare
-		quest::faction(310,1); 		#:: + Soldiers of Tunare
-		quest::faction(234,-1); 		#:: - Crushbone Orcs
+		#:: Set factions
+		quest::faction(246, 1); 		#:: + Faydark's Champions
+		quest::faction(279, 1); 		#:: + King Tearis Thex
+		quest::faction(226, 1); 		#:: + Clerics of Tunare
+		quest::faction(310, 1); 		#:: + Soldiers of Tunare
+		quest::faction(234, -1); 		#:: - Crushbone Orcs
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
