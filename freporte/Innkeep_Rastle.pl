bbf023c1c3ae96831145b6ffc43e115e387544a4
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:12:24 -0500

    Update Innkeep_Rastle.pl
    Refactor for consistency, fix hail response.

--- freporte/Innkeep_Rastle.pl
+++ freporte/Innkeep_Rastle.pl
@@ -1,10 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hail $class. Business is going well these days. I have a full stock of baked goods and other items for sale. Have a look around, $name!");
+		quest::say("Hail $name. Business is going well these days. I have a full stock of baked goods and other items for sale. Have a look around!");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
