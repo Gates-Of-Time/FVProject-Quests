e5fbf3d74ff069bd1b2362b5d632a17f86a5be81
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:01:22 -0500

    Update Heneva_Jexsped.pl
    Refactor for consistency, correct dialogue, add missing dialogue

--- freporte/Heneva_Jexsped.pl
+++ freporte/Heneva_Jexsped.pl
@@ -1,23 +1,34 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::emote("Yes. yes. yes.  Hail to you. too, %s.  Now, quit bugging me! $name.");
+		quest::say("Yes, yes, yes,  Hail to you, too, $name.  Now, quit bugging me!");
+	}
+	elsif ($text=~/trades/i) {
+		quest::say("'I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+		#:: Give a 51121 - Tradeskill Basics : Volume I
+		quest::summonitem(51121);
+	}
+	elsif ($text=~/second book/i) {
+		quest::say("Here is the second volume of the book you requested, may it serve you well!");
+		#:: Give a 51122 - Tradeskill Basics : Volume II
+		quest::summonitem(51122);
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18855 -  A Tattered note
-	if (plugin::check_handin(\%itemcount, 18855 => 1)) {
-		quest::say("Welcome, friend. I see more than a slight glimmer of hate in your eyes. Good... for we have much work to do.");
+	#:: Match a 18855 -  A Tattered note
+	if (plugin::takeItems(18855 => 1)) {
+		quest::say("Welcome, friend. I see more than a slight glimmer of hate in your eyes. Good, for we have much work to do. Once you are ready to begin your training please make sure that you see Marv Orilis, he can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
 		#:: Give item 13565 - Old Stained Robe*
 		quest::summonitem(13565);
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(271,100); 	#:: + Dismal Rage
-		quest::faction(281,-15); 	#:: - Knights of Truth
-		quest::faction(296,20); 	#:: + Opal Dark Briar
+		#:: Set factions
+		quest::faction(271, 100); 	#:: + Dismal Rage
+		quest::faction(281, -15); 	#:: - Knights of Truth
+		quest::faction(296, 20); 	#:: + Opal Dark Briar
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
