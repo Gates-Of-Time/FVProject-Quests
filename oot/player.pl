ed86f4dc442e7bf087b0e22ea5560c0736a165d2
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:54:04 -0500

    Update player.pl
    Updated to use Data Buckets rather than qglobals

--- oot/player.pl
+++ oot/player.pl
@@ -1,6 +1,10 @@
 sub EVENT_ENTERZONE {
-	if(defined $qglobals{strongbox}) { 
-		quest::creategroundobject(13860, -9200, -430, -293, 0, 3000000);
-		quest::delglobal("strongbox");
+	#:: Data bucket to verify quest has been started appropriately
+	$key = $client->CharacterID() . "-strongbox";
+	#:: Match if the key exists
+	if (quest::get_data($key)) {
+		quest::creategroundobject(13860, -9200, -430, -293, 0, 3000);
+		#:: Clean up the data bucket
+		quest::delete_data($key);
 	}
 }
