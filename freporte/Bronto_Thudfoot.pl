9dd06856a22b167cc8f603fe06487fa628772503
TurmoilToad <TurmoilToad@gmail.com>
2020-01-04 19:59:21 -0500

    Update Bronto_Thudfoot.pl
    Added missing dialogue

--- freporte/Bronto_Thudfoot.pl
+++ freporte/Bronto_Thudfoot.pl
@@ -2,6 +2,12 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("How ya doin' bub. Seeing as you just joined the conversation, I think you need to buy us a round.");
 	}
+	elsif ($text=~/see the rogue/i) {
+		quest::say("I had quite a bit of grog that night and it was very dark. What I do remember was seeing a tall woman in a dress run from the docks. She sort of smelled, too. Like fish. I know it was the docks, but this woman had a real stench to her. Like dried fish baking in the sun. That is all I remember. It was too dark to see anything but her [silhouette].");
+	}
+	elsif ($text=~/silhouette/i) {
+		quest::say("Yeah!! The silhouette looked like a very brawny woman. It had to be a woman. The silhouette was surely that of one with a short skirt and long hair.");
+	}
 }
 
 sub EVENT_SIGNAL {
