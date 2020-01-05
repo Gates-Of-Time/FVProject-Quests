048a102555ee6ae199d27c21936bb97317dbf1d5
TurmoilToad <TurmoilToad@gmail.com>
2020-01-05 12:09:54 -0500

    Update Harkin_Duskfoot.pl
    Refactor for consistency.

--- freporte/Harkin_Duskfoot.pl
+++ freporte/Harkin_Duskfoot.pl
@@ -1,33 +1,35 @@
 sub EVENT_SPAWN {
 	#:: Create a proximity, 30 units across, 30 units tall, and enable proximity say
 	quest::set_proximity($x - 15, $x + 15, $y - 15, $y + 15, $z - 15, $z + 15, 1);
+	#:: Also enable proximity say
+	quest::enable_proximity_say();
 }
 
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("HA! Greetings there. $name!  How are ya this fine day?  Me?  I'm doing all right.  Guard duty down here always reminds me of home.");
-		#:: Send a signal "1" to East Freeport >> Beur_Tenlah (10106)
-		quest::signalwith(10106,1,0)
+		#:: Send a signal '1' to East Freeport >> Beur_Tenlah (10106) with no delay
+		quest::signalwith(10106, 1, 0)
 	}
 }
 
 sub EVENT_PROXIMITY_SAY {
 	if ($text=~/message/i) {
 		quest::say("Ok, $name, I need you to take this message to Janam in West Freeport. He is usually hanging out in front of the Theater of the Tranquil with that good-for-nothing Rebby. Anyway, give this note to Janam and bring his reply back to me.");
-		#:: Give item 18015 - Note to Janam
+		#:: Give a 18015 - Note to Janam
 		quest::summonitem(18015);
 	}
 }
 
 sub EVENT_SIGNAL {
-	#:: Signal From freporte/Beur_Tenlah.pl
+	#:: Match a signal '1' from freporte/Beur_Tenlah.pl
 	if ($signal == 1) {
 		quest::say("Bah! Don't listen to this fool. Listen, I've something a little more important for you to do than buy ales. I need you to take a [message] to my friend Janam over in West Freeport.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18016 -  Note to Harkin
+	#:: Match a 18016 - Note to Harkin
 	if (plugin::takeItems(18016 => 1)) {
 		quest::say("Ah, good work, $name. And quick too, I'll makes sure that Elisi hears of your loyal work. Here... take this for your efforts.. it's not much, but it's all I have on me right now.");
 		#:: Give item 1054 - Used Merchants Gloves
@@ -35,12 +37,12 @@ sub EVENT_ITEM {
 		#:: Ding!
 		quest::ding();
 		#:: Set factions
-		quest::faction(336,20); 	#:: + Coalition of Tradefolk Underground
-		quest::faction(229,20); 	#:: + Coalition of Trade Folk
-		quest::faction(329,2); 		#:: + Carson McCabe
-		quest::faction(230,1); 		#:: + Corrupt Qeynos Guards
-		quest::faction(330,3); 		#:: + Freeport Militia
-		#:: Give a small amount of xp
+		quest::faction(336, 20); 		#:: + Coalition of Tradefolk Underground
+		quest::faction(229, 20); 		#:: + Coalition of Trade Folk
+		quest::faction(329, 2); 		#:: + Carson McCabe
+		quest::faction(230, 1); 		#:: + Corrupt Qeynos Guards
+		quest::faction(330, 3); 		#:: + Freeport Militia
+		#:: Grant a small amount of experience
 		quest::exp(100);
 		#:: Create a hash for storing cash - 5 to 7cp
 		my %cash = plugin::RandomCash(5,7);
