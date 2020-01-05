ab569fcc63a8ba9058866b2d73ca82fb3206d978
TurmoilToad <TurmoilToad@gmail.com>
2020-01-05 12:06:09 -0500

    Update 10000.pl
    Refactor for consistency.  Finish timer.

--- freporte/10000.pl
+++ freporte/10000.pl
@@ -1,14 +1,21 @@
 sub EVENT_ITEM {
 	#:: Turn in of Large Crate of Mining Supplies 19930 for Rogue Errands Quest - spawned by Rigg_Nostra
-	if (plugin::check_handin(\%itemcount, 19930 => 1)) {
-		#:: Give item 19918 - Rough Blue Gem
-		quest::summonitem(19918);
+	if (plugin::takeItems(19930 => 1)) {
 		quest::say("Arg");
+		#:: Give a 19918 - Rough Blue Gem
+		quest::summonitem(19918);
 		#:: Ding!
 		quest::ding();
-		#:: Set Depop Timer
-		quest::settimer("depop",5);
+		#:: Set a timer 'depop' that loops every 5 seconds
+		quest::settimer("depop", 5);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
+}
+
+sub EVENT_TIMER {
+	#:: Match timer 'depop'
+	if ($timer eq "depop") {
+		quest::depop();
+	}
 }
