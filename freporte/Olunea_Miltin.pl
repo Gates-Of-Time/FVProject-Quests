92a4cb56b16b936b3495e39d1839a76315a7d09f
TurmoilToad <TurmoilToad@gmail.com>
2020-01-05 08:54:26 -0500

    Update Olunea_Miltin.pl
    Added missing dialogue.  Refactor for consistency.  Adjust faction reward.

--- freporte/Olunea_Miltin.pl
+++ freporte/Olunea_Miltin.pl
@@ -2,24 +2,29 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hello. It is good to meet you. Try not to scare the fish away. This is A good spot I supply fish to the Grub N' Grog. The patrons there love me!");
 	}
+	elsif ($text=~/fish/i) {
+		quest::say("I am doing exceptionally well. This harbor is filled with fish. After an hour I have enough to feed me and my family for a week.");
+	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13922 -  Snapped Pole
-	if (plugin::check_handin(\%itemcount, 13922 => 1)) {
+	#:: Match a 13922 - Snapped Pole
+	if (plugin::takeItems(13922 => 1)) {
 		quest::say("Great! Thank you stranger. The rogues must have broken it. At least I could repair it. It would be seasons before I could afford another pole.");
-		#:: Give a small amount of xp
-		quest::exp(100);
-		#:: Give a small amount of cash copper - plat
-		quest::givecash(50,0,0,0);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(330,1); 		#:: + Freeport Militia
-		quest::faction(336,1); 		#:: + Coalition of Tradefolk Underground
-		quest::faction(281,-1); 	#:: - Knights of Truth
-		quest::faction(362,-1); 	#:: - Priests of Marr		
+		#:: Set factions
+		quest::faction(330, 5); 		#:: + The Freeport Militia
+		quest::faction(336, 1); 		#:: + Coalition of Tradefolk Underground
+		quest::faction(281, -1);	 	#:: - Knights of Truth
+		quest::faction(362, -1); 		#:: - Priests of Marr
+		#:: Grant a small amount of experience
+		quest::exp(100);
+		#:: Create a hash for storing cash - 10 to 20cp
+		my %cash = plugin::RandomCash(10,20);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
