4962f97441e85277976917faf022ca75994385df
TurmoilToad <TurmoilToad@gmail.com>
2020-01-16 07:58:00 -0500

    Update Kizzie_Mintopp.pl
    Refactor for consistency, adjust faction reward.

--- rivervale/Kizzie_Mintopp.pl
+++ rivervale/Kizzie_Mintopp.pl
@@ -2,39 +2,39 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Enough talk there. chatterbox.  You buy lots of herbs.  I gotta make a living. not more friends.");
 	}
-	if ($text=~/temple of life/i) {
+	elsif ($text=~/temple of life/i) {
 		quest::say("Oh, yes.. They always come for Kizzie's [honey jum]. They should have sent you next week. There is a problem");
 	}	
-	if ($text=~/problem/i) {
+	elsif ($text=~/problem/i) {
 		quest::say("You see.. Joogl Honeybugger is hurt. He usually collects the [Honeycombs]. Only he knows where to get them. I need the Honeycombs to make the Honey Jum. Go and speak with Joogl. He is at his burrow outside of Rivervale. He says he cannot work. Tell him you need the Honeycombs.");
 	}	
-	if ($text=~/Honeycombs/i) {
+	elsif ($text=~/Honeycombs/i) {
 		quest::say("If Kizzie knew that, then she would not hire Joogl Honeybugger!  Go talk to him in the Misty Thicket.");
 	}		
-	if ($text=~/make/i) {
+	elsif ($text=~/make/i) {
 		quest::say("No, no, no!!  You will not get honey jum until you give Kizzie three honeycombs and thirty gold coins.  Honey jum is valuable.  Kizzie raised the fee!!  That is how it works.");
 	}	
-	if ($text=~/honey jum/i) {
+	elsif ($text=~/honey jum/i) {
 		quest::say("It's a secret!!  I get the honeycombs and combine them with the secret sauce and..presto.. honey jum!!  It is used by only the wisest as a component.  And you can only get it from Kizzie.  It is also good on batwing crunchies.  If you like.. Kizzie can [make] honey jum for you?");
 	}		
 } 
 
 sub EVENT_ITEM {
-	#:: Turn in for 13953 - Honeycomb x3, 30 Gold 
-	if (plugin::takeItemsCoin(0,0,30,0, 13953 => 3)) {
+	#:: Match three 13953 - Honeycomb and 30 gold pieces
+	if (plugin::takeItemsCoin(0, 0, 30, 0, 13953 => 3)) {
 		quest::say("You are one lucky bixie buster. I just made a batch of honey jum. Here. No waiting for you.  One jar for your good work. Bye, now!");
 		#:: Give item 13952 - Honey Jum
 		quest::summonitem(13952);
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(241, 5);		#:: + Deeppockets
-		quest::faction(286, 5);		#:: + Mayor Gubbin
-		quest::faction(292, 5);		#:: + Merchants of Rivervale
-		quest::faction(263, 5);		#:: + Guardians of the Vale
-		quest::faction(336, -5);		#:: - Coalition of Tradefolk Underground
+		#:: Set factions
+		quest::faction(241, 1);			#:: + Deeppockets
+		quest::faction(286, 1);			#:: + Mayor Gubbin
+		quest::faction(292, 1);			#:: + Merchants of Rivervale
+		quest::faction(263, 1);			#:: + Guardians of the Vale
+		quest::faction(336, -1);		#:: - Coalition of Tradefolk Underground
+		#:: Grant a small amount of experience
+		quest::exp(500);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
