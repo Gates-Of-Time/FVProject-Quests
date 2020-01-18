33b972603a76b873efd2997a148d87b3cef592ef
TurmoilToad <TurmoilToad@gmail.com>
2020-01-18 08:22:57 -0500

    Update Dyllin_Starsine.pl
    Added return item.

--- highpass/Dyllin_Starsine.pl
+++ highpass/Dyllin_Starsine.pl
@@ -2,8 +2,13 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hey!! I don't have any spare change, okay?!");
 	}
-	if ($text=~/testament of vanear/i) {
+	elsif ($text=~/testament of vanear/i) {
 		quest::say("So you heard Guard Dyllin has page 30 of the Testament of Vanear, eh?! Well, get lost!! Over my dead body will you get page 30!");
 		quest::attack($name);
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
