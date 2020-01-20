99cbee9ab54614562b5eb466590579fe4bfb20cb
TurmoilToad <TurmoilToad@gmail.com>
2020-01-20 17:37:15 -0500

    Update and rename 531.lua to 531.pl
    Perl convert

---
+++ global/spells/531.pl
@@ -0,0 +1,6 @@
+#:: Ring of Commons
+
+sub EVENT_SPELL_EFFECT_CLIENT {
+	#:: Zone 21 - commons, $x (effect_base_value2), $y (effect_base_value1), $z (effect_base_value3), $heading (effect_base_value4)
+	quest::movepc(21, 1396, 345, -50, 245);
+}
