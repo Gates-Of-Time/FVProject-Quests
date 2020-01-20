b95d08f71d0007b5562d7c21e7469cfd3ef6a3f6
TurmoilToad <TurmoilToad@gmail.com>
2020-01-20 17:37:30 -0500

    Update 551.pl
    Typo fix

--- global/spells/551.pl
+++ global/spells/551.pl
@@ -1,6 +1,6 @@
 #:: Circle of Commons
 
 sub EVENT_SPELL_EFFECT_CLIENT {
-	#:: Zone 33 - misty, $x (effect_base_value2), $y (effect_base_value1), $z (effect_base_value3), $heading (effect_base_value4)
+	#:: Zone 21 - commons, $x (effect_base_value2), $y (effect_base_value1), $z (effect_base_value3), $heading (effect_base_value4)
 	quest::movepc(21, 1396, 345, -50, 245);
 }
