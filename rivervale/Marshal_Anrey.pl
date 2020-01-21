357dd1426cde5cbacb6a5f61d5ad5a8d3188c84a
TurmoilToad <TurmoilToad@gmail.com>
2020-01-21 09:23:46 -0500

    Update and rename Marshall_Anrey.pl to Marshal_Anrey.pl
    Fixed name, added missing dialogue, refactor for consistency

--- rivervale/Marshal_Anrey.pl
+++ rivervale/Marshal_Anrey.pl
@@ -2,25 +2,31 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Stand at attention!! You don't look fit enough to toe-wrestle my grandma!! You cannot be in my squad!! Are you [petitioning] or are you a [visitor]?");
 	}
-	if ($text=~/visitor/i) {
+	elsif ($text=~/hello/i) {
+		quest::say("Do I look like your best friend?!! Well I'm not, bixie buster!! You will address an officer with the word 'Hail' like every other deputy. Stand up straight, hunchback, or you'll be patrolling Rivervale till dawn!");
+	}
+	elsif ($text=~/visitor/i) {
 		quest::say("Well, why didn't you say so?! Forgive me for hollering. Allow me to introduce myself. I am Marshal Anrey Leadladle, commander of the [Leatherfoot Raiders].");
 	}
-	if ($text=~/petitioning/i) {
+	elsif ($text=~/petitioning/i) {
 		quest::say("So you want to be a [Leatherfoot Raider]? What kind of joke is this? Look at you! You're a mess! Where are you [from]?");
 	}
-	if ($text=~/Leatherfoot Raider/i) {
+	elsif ($text=~/leatherfoot raider/i) {
 		quest::say("You must be a visitor. The Leatherfoot Raiders are the elite force of the Guardians of the Vale. I command them. It is good to meet an outsider.");
 	}
-	if ($text=~/from/i) {
+	elsif ($text=~/from rivervale/i) {
 		quest::say("So you're from Rivervale?!! You must be some kind of freak. No halfling from Rivervale would look, smell and act anything like you. Well, freak, if you think you're stout enough to be a Leatherfoot Raider, you prove it!! You travel the lands and return to me a polar bear skin, a grizzly bear skin, a shark skin and an alligator skin. Then maybe, just maybe, I will let you wear the cap of a Leatherfoot Raider.");
+	}
+	elsif ($text=~/from freeport/i) {
+		quest::say("What!! The only thing from there are gnolls and trolls!! Which one are you?!! You kind of look like a troll, but you smell like a gnoll. Get out of my sight or you will be cleaning the latrine with a toothbrush.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 13749 - Alligator Skin, 13942 - Polar Bear Skin, 13075 - Shark Skin, 13765 - Thick Grizzly Bear Skin
+	#:: Match a 13749 - Alligator Skin, a 13942 - Polar Bear Skin, a 13075 - Shark Skin, and a 13765 - Thick Grizzly Bear Skin
 	if (plugin::takeItems(13749 => 1, 13942 => 1, 13075 => 1, 13765 => 1)) {
 		quest::say("Good work, $name. You passed the first test. If you think you are one of us, return this cap to me along with a dagger from a Dark Elf for your true reward.");
-		#:: Give item 13941 - Leatherfoot Skullcap
+		#:: Give a 13941 - Leatherfoot Skullcap
 		quest::summonitem(13941);
 		#:: Ding!
 		quest::ding();
@@ -31,7 +37,7 @@ sub EVENT_ITEM {
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 	}		
-	#:: Turn in for 13942 - Dragoon Dirk and 13941 = Leatherfoot Skullcap
+	#:: Match a 13942 - Dragoon Dirk and 13941 = Leatherfoot Skullcap
 	elsif (plugin::takeItems(13942 => 1, 13941 => 1)) {
 		quest::say("Wonderful, $name. You have proven yourself to the Leatherfoot Squad. Take this and wear it with honor.");
 		#:: Give item 12259 - Leatherfoot Raider Skullcap
