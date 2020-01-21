2dbf2091f4ab9c9780be977f1565b682bb1f6dc4
TurmoilToad <TurmoilToad@gmail.com>
2020-01-21 08:38:51 -0500

    Update Marshal_Ghobber.pl
    Adjust faction checks, faction 'reward', refactor for consistency.

--- rivervale/Marshal_Ghobber.pl
+++ rivervale/Marshal_Ghobber.pl
@@ -2,35 +2,33 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Greetings!  Allow me to introduce myself.  I am Marshal Ghobber Truppack.  Marshal of the Great Wall Patrol.  Are you [new to the squad]?");
 	}
-	if ($text=~/new to the squad/i) {
-		#:: Match if faction is better than indifferent
-		if ($faction < 5) {
+	elsif ($text=~/new to the squad/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
 			quest::say("Great!!! We require more deputies in this squad. We are suffering great losses to the [new threat]. Welcome to the Great Wall Patrol where our motto is, 'United we shall defend.' It will be some time before you can earn your squad ring - first you must prove your strength in battle. Report to the squad leader. He is Deputy Budo. You can find him at the Great Wall of Serilis in the Misty Thicket. Go to him at once and tell him you are reporting for duty.");
 		}
-		#:: Match if faction is indifferent
+		#:: Match if faction is Indifferent
 		elsif ($faction == 5) {
 			quest::say("You are in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
 		}
-		#:: Match if faction is worse than indifferent
-		elsif ($faction > 5) {
+		else {
 			quest::say("How can you expect to just waltz into Guardian Stronghold and expect to gather information?! Consider yourself lucky I don't command the Guardian of the Vale deputies to show you the sharpness of their blades!");
 		}
 	}
-	if ($text=~/new threat/i) {
-		#:: Match if faction is better than indifferent
-		if ($faction < 5) {
+	elsif ($text=~/new threat/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
 			quest::say("The new threat I refer to is the community of goblins which was found beyond the Great Wall of Serilis. They are called Clan Runnyeye. They have begun to sneak over the wall. The deputies have reported seeing them launch themselves up the wall in a matter of seconds using ornate grappling hooks. We have attempted to obtain one of these devices for further inspection, but we have yet to find one on any of the dead goblins. If you should ever find one, be sure to bring it to me immediately.");
 		}
-		#:: Match if faction is indifferent
+		#:: Match if faction is Indifferent
 		elsif ($faction == 5) {
 			quest::say("You are in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
 		}
-		#:: Match if faction is worse than indifferent
-		elsif ($faction > 5) {
+		else {
 			quest::say("How can you expect to just waltz into Guardian Stronghold and expect to gather information?! Consider yourself lucky I don't command the Guardian of the Vale deputies to show you the sharpness of their blades!");
 		}
 	}
-	if ($text=~/one with the wall/i) {
+	elsif ($text=~/one with the wall/i) {
 		#:: Match if faction is Ally
 		if ($faction == 1) {
 			quest::say("Yes!! Welcome, Deputy $name! You are now an elite member of the Great Wall Division. Wear this ring with pride. You may take it to Hendi Mrubble of the Clerics of Mischief for healing at any time. Wear it with pride.");
@@ -38,14 +36,14 @@ sub EVENT_SAY {
 			quest::ding();
 			#:: Give 13936 - Squad Ring
 			quest::summonitem(13936);
-			quest::faction(263,-500);  #:: - Guardians of the Vale
+			#:: Set faction
+			quest::faction(263, -1000);  	#:: - Guardians of the Vale
 		}
-		#:: Match if faction is indifferent
-		elsif ($faction < 5) {
+		#:: Match if faction is Indifferent or better, but not Ally
+		elsif ($faction <= 5 && $faction > 1) {
 			quest::say("You are in good standing with the Guardians of the Vale. Continue with your good work and then we may speak.");
 		}
-		#:: Match if faction is worse than indifferent
-		elsif ($faction > 5) {
+		else {
 			quest::say("How can you expect to just waltz into Guardian Stronghold and expect to gather information?! Consider yourself lucky I don't command the Guardian of the Vale deputies to show you the sharpness of their blades!");
 		}		
 	}
