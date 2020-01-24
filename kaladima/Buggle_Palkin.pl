8a3d0e5fceab523d237e9be2196c7223129f408a
TurmoilToad <TurmoilToad@gmail.com>
2020-01-24 09:33:10 -0500

    Update Buggle_Palkin.pl
    Don't eat items.

--- kaladima/Buggle_Palkin.pl
+++ kaladima/Buggle_Palkin.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY { 
-	if ($text=~/Hail/i) {
-		quest::say("Greetings. $name.  I am Gretta Mottle's assistant, Buggle.  I tend to the bakery for her during the night.  I can only make one thing though..  Meat Pie.  Poor Gretta is in desperate need of help right now so I'm trying to learn how to make muffins for her."); 
+	if ($text=~/hail/i) {
+		quest::say("Greetings, $name.  I am Gretta Mottle's assistant, Buggle.  I tend to the bakery for her during the night.  I can only make one thing though..  Meat Pie.  Poor Gretta is in desperate need of help right now so I'm trying to learn how to make muffins for her."); 
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
