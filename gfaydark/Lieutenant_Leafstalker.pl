5071eb6dc26034fa4c2db018a338e2ed7e4ce10b
TurmoilToad <TurmoilToad@gmail.com>
2020-01-26 12:54:19 -0500

    Update Lieutenant_Leafstalker.pl
    Fixed typo

--- gfaydark/Lieutenant_Leafstalker.pl
+++ gfaydark/Lieutenant_Leafstalker.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest:;say("Hail!! Respect all laws in Kelethin and her territory and you shall live a fruitful life.");
+		quest::say("Hail!! Respect all laws in Kelethin and her territory and you shall live a fruitful life.");
 	}
 	elsif ($text=~/quiver of kithicor/i) {
 		quest::say("Ummm.. I have some bad news. I did find it, but a band of brownie outcasts came in the night, took [all of the arrows] from the quiver and ran back to the forest they came from. As if that were not enough, while I was hot on their little heels, the thief [Wiltin Windwalker] came into Kelethin and took the quiver itself. Whew!! What a day!");
