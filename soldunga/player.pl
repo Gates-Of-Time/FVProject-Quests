5fcd00933d589a76496f46ed66fbb18883f4240c
TurmoilToad <TurmoilToad@gmail.com>
2020-02-01 10:11:44 -0500

    Update player.pl
    Add some checks.

--- soldunga/player.pl
+++ soldunga/player.pl
@@ -2,15 +2,33 @@
 
 sub EVENT_CLICKDOOR {
 	if ($doorid == 57) {
-		quest::toggledoorstate(58);
-		quest::toggledoorstate(58);
+		if (quest::isdooropen(57)) {
+			quest::forcedoorclose(58);
+			quest::forcedoorclose(59);
+		}
+		else {
+			quest::forcedooropen(58);
+			quest::forcedooropen(59);
+		}
 	}
 	elsif ($doorid == 58) {
-		quest::toggledoorstate(58);
-		quest::toggledoorstate(58);
+		if (quest::isdooropen(58)) {
+			quest::forcedoorclose(57);
+			quest::forcedoorclose(59);
+		}
+		else {
+			quest::forcedooropen(57);
+			quest::forcedooropen(59);
+		}
 	}
 	elsif ($doorid == 59) {
-		quest::toggledoorstate(58);
-		quest::toggledoorstate(58);
+		if (quest::isdooropen(59)) {
+			quest::forcedoorclose(58);
+			quest::forcedoorclose(57);
+		}
+		else {
+			quest::forcedooropen(58);
+			quest::forcedooropen(57);
+		}
 	}
 }
