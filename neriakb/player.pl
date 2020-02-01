d757ab62efeec902d1b415f5a3f8caac92198461
TurmoilToad <TurmoilToad@gmail.com>
2020-02-01 10:15:51 -0500

    Update player.pl
    Make this a little more robust.

--- neriakb/player.pl
+++ neriakb/player.pl
@@ -1,16 +1,34 @@
-#:: Forces gates to open at the same time
+#:: Forces gates to open and close at the same time
 
 sub EVENT_CLICKDOOR {
 	if ($doorid == 1) {
-		quest::forcedooropen(2);
-		quest::forcedooropen(49);
+		if (quest::isdooropen(1)) {
+			quest::forcedoorclose(2);
+			quest::forcedoorclose(49);
+		}
+		else {
+			quest::forcedooropen(2);
+			quest::forcedooropen(49);
+		}
 	}
 	elsif ($doorid == 2) {
-		quest::forcedooropen(1);
-		quest::forcedooropen(49);
+		if (quest::isdooropen(2)) {
+			quest::forcedoorclose(1);
+			quest::forcedoorclose(49);
+		}
+		else {
+			quest::forcedooropen(1);
+			quest::forcedooropen(49);
+		}
 	}
 	elsif ($doorid == 49) {
-		quest::forcedooropen(1);
-		quest::forcedooropen(2);
+		if (quest::isdooropen(49)) {
+			quest::forcedoorclose(1);
+			quest::forcedoorclose(2);
+		}
+		else {
+			quest::forcedooropen(1);
+			quest::forcedooropen(2);
+		}
 	}
 }
