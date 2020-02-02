35e3cb170bc76222fcc0e6e1c5149c36b4554662
TurmoilToad <TurmoilToad@gmail.com>
2020-02-01 20:26:39 -0500

    Update Linara_Parlone.pl
    Refactor for consistency. Remove saylinks.

--- najena/Linara_Parlone.pl
+++ najena/Linara_Parlone.pl
@@ -1,21 +1,24 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Please [" . quest::saylink("help") . "] me out of this horrid place! My family must be terribly worried!");
+		quest::say("Please [help] me out of this horrid place! My family must be terribly worried!");
 	}
-	if ($text=~/help/i) {
-		quest::say("If you can find the [" . quest::saylink("key") . "] to my restraints. I should be able to escape.");
+	elsif ($text=~/help/i) {
+		quest::say("If you can find the [key] to my restraints. I should be able to escape.");
 	}
-	if ($text=~/key/i) {
+	elsif ($text=~/key/i) {
 		quest::say("Last I knew, Najena's priest had the key. I fear he has left this place though. I heard talk of traveling to some estate. I know not where. Oh dear, oh dear! I shall never leave this place!");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Match a 12269 - Tarnished Bronze Key
-	if (plugin::check_handin(\%itemcount, 12269 => 1)) {
+	if (plugin::takeItems(12269 => 1)) {
 		quest::say("Oh thank you $name! I must make my escape now. Give this note to my father in Felwithe, he will reward you handsomely for your service.");
 		#:: Give a 5573 - Folded Note
 		quest::summonitem(5573);
+		#:: Ding!
+		quest::ding();
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
