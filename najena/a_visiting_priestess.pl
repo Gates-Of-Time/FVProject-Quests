3173d60d92c73badb12ec994976019509f477bf8
TurmoilToad <TurmoilToad@gmail.com>
2020-02-01 19:59:43 -0500

    Update a_visiting_priestess.pl
    Refactor for consistency, add working faction check.

--- najena/a_visiting_priestess.pl
+++ najena/a_visiting_priestess.pl
@@ -1,48 +1,52 @@
-# Quest for The Visiting Priestess
-# missing how player knows to ask questions
-#
-# Not sure on Faction (Alla posts say dubious does not work I'm not sure on indiff)
-#
-
-# Faction value using function.  The $faction DOES NOT WORK for Priests of Innoruuk (ID 256) in the release on 08/28/2009
-my $facval;
-
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("Hail, $name! Are you the champion Najena sent for?");
-  }
-  if ($text=~/champion of Najena/i) {
-    quest::say("Very well champion.  Have you heard of The Staff of Ankexfen?"); # Not real text
-  }
-  if ($text=~/not heard of the staff/i) {
-    quest::say("No? Why am I not surprised? You look too weak and frail to be of any use. You probably could not even best one of those intoxicated, weak-minded goblins controlled by that pathetic eye called Xxorb. Away with you, useless creature!");
-  }
-  if ($text=~/happened to the Ankexfen/i) {
-    quest::say("The wrath of the unnamed gods was grim. Thousands of ogres were instantly slain - their empire left to crumble under the hand of retribution. The giants were flung across the face of Norrath like seeds to the wind. The Ankexfen's fates were more severe. The entire race was exterminated, but from the ashes were born the four goblin races. What is known is that each goblin clan was stripped of any knowledge and was neither informed of the other clans nor of their own origins. Each clan was then transported to a separate environment to help preserve the gods' will. However, the gods were not without mercy and each clan was granted a section of the staff. Upon each staff, a crystal was placed. These staves served to aid the goblin clans in understanding their environments. These remnants of the staff of Ankexfen are what I desire. Bring them to me and the Priests of Innoruuk shall reward you.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hail, $name! Are you the champion Najena sent for?");
+	}
+	elsif ($text=~/champion of najena/i) {
+		quest::say("Very well then $name. I am Aetherthane D'zitherak, Priestess of Innoruk, seeker of the fabled and ancient Staff of Ankexfen. Have you ever hard of this relic before?");
+	}
+	elsif ($text=~/not heard of the staff/i) {
+		quest::say("Long ago there existed a race known as the Ankexfen, after their progenitor Hazeg Ankexfen, who were created by Rallos Zek during the Elder Age. This race was quite intelligent and cunning. They quickly expanded their knowledge of magic and other planes, yet constantly yearned for more power. So along with their ogre counterparts, they set out to master the Plane of Earth. Alas, their attempts proved futile and the combined might of the other gods forced Rallos Zek back to the Planes of Power, leaving his creations to the whims of other gods.");
+	}
+	elsif ($text=~/ankexfen/i) {
+		quest::say("The wrath of the unnamed gods was grim. Thousands of ogres were instantly slain - their empire left to crumble under the hand of retribution. The giants were flung across the face of Norath like seeds to the wind. The Ankexfen's fate was more severe. The entire race was exterminated, but from the ashes were born the four goblin races. Exactly how this happened is unknown. But what is known is that each goblin was stripped of any knowledge and was neither informed of the other clans nor of their origins. Each clan was then transported to a separate environment to help preserve the gods will. However, the gods were not without mercy and each clan was granted a section of the staff. Upon each staff a crystal was adorned. These staves served to aid the goblin clans in understanding their environment. These remnants of the Staff of Ankexfen are what I desire. Bring them to me and the Priests of Innoruk shall reward you.");
+	}
 }
 
 sub EVENT_ITEM {
-  $facval = quest::factionvalue();
-  if ($facval >= 5) { #indifferent or better priests of innoruuk
-     # 6323 - Ice Crystal Staff
-     # 6324 - Fire Crystal Staff
-     # 6335 - Slime Crystal Staff
-     # 6336 - Water Crystal Staff
-    if (plugin::check_handin(\%itemcount, 6323 => 1, 6324 => 1, 6335 => 1, 6336 => 1)) {
-      quest::emote("cackles with glee as she slowly assembles the runed rods, and with a muttered incantation and a flash of light, disappears, leaving only a note that slowly drifts to the ground.");
-      quest::summonitem(18401); # Scroll of Flayed Goblin Skin
-      quest::faction(340, 20); #Priests of Innoruuk
-      quest::faction(278, 20); #King Naythox Thex
-      quest::faction(362, -60); #Priests of Marr
-      quest::faction(226, -60); #Clerics of Tunare
-      quest::faction(341, -60); #Priests of Life
-      quest::faction(1522, -60); #Primordial Malice
-      quest::exp(132775);   # 2.5% @ level 30
-      quest::depop_withtimer();
-    }
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 6323 - Ice Crystal Staff, a 6324 - Fire Crystal Staff, a 6335 - Slime Crystal Staff, and a 6336 - Water Crystal Staff
+	if (plugin::takeItems(6323 => 1, 6324 => 1, 6335 => 1, 6336 => 1)) {
+		#:: Match if faction is Indifferent or better
+		if ($faction <= 5) {
+			quest::emote("cackles with glee as she slowly assembles the runed rods, and with a muttered incantation and a flash of light, disappears, leaving only a note that slowly drifts to the ground.");
+			#:: Give a 18401 - Scroll of Flayed Goblin Skin
+			quest::summonitem(18401);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(340, 100);		#:: + Priests of Innoruuk
+			quest::faction(278, 15);		#:: + King Naythox Thex
+			quest::faction(362, -34);		#:: - Priests of Marr
+			quest::faction(226, -25);		#:: - Clerics of Tunare
+			quest::faction(341, -15);		#:: - Priests of Life
+			quest::faction(1522, -400);		#:: - Primordial Malice
+			#:: Grant a small amount of experience, based on level
+			$client->AddLevelBasedExp(3, 30);
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
+		}
+		else {
+			#:: Response not found
+			#:: Return a 6323 - Ice Crystal Staff
+			quest::summonitem(6323);
+			#:: Return a 6324 - Fire Crystal Staff
+			quest::summonitem(6324);
+			#:: Return a 6335 - Slime Crystal Staff
+			quest::summonitem(6335);
+			#:: Return a 6336 - Water Crystal Staff
+			quest::summonitem(6366);
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-#END of FILE Zone:najena ID:44107 -- a_visiting_priestess 
