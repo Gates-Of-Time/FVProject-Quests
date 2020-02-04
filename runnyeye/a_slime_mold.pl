37816caaa4c75aacaf0dc6c48d7750acae6691e4
TurmoilToad <TurmoilToad@gmail.com>
2020-02-04 14:45:13 -0500

    Create a_slime_mold.pl

---
+++ runnyeye/a_slime_mold.pl
@@ -0,0 +1,10 @@
+sub EVENT_SPAWN {
+	#:: Create a proximity, 100 units across, 10 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 5, $z + 5, 0);
+}
+
+sub EVENT_ENTER {
+	quest::emote("erupts in a cloud of spores!");
+	#:: Spawn a a_goblin_scavenger
+	quest::spawn2(11000, 0, 0, $x, $y, $z, $h);
+}
