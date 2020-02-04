913bfcbaf946604e2df43d7af1d7d6a0cbd6e9ce
TurmoilToad <TurmoilToad@gmail.com>
2020-02-04 15:34:25 -0500

    Update a_slime_mold.pl
    Fix NPC spawn

--- runnyeye/a_slime_mold.pl
+++ runnyeye/a_slime_mold.pl
@@ -5,6 +5,6 @@ sub EVENT_SPAWN {
 
 sub EVENT_ENTER {
 	quest::emote("erupts in a cloud of spores!");
-	#:: Spawn a a_goblin_scavenger
-	quest::spawn2(11000, 0, 0, $x, $y, $z, $h);
+	#:: Spawn a The Liberated Citadel of Runnyeye >> a_sporeling (11188)
+	quest::spawn2(11188, 0, 0, $x, $y, $z, $h);
 }
