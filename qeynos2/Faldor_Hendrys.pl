7216b173b8f211debd93308f8eb1fb74e8ee2b22
TurmoilToad <TurmoilToad@gmail.com>
2020-02-04 10:13:12 -0500

    Update Faldor_Hendrys.pl
    Attack message.

--- qeynos2/Faldor_Hendrys.pl
+++ qeynos2/Faldor_Hendrys.pl
@@ -1,3 +1,10 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("That's it swine! You're outta here!");
+	}
+}
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("What do you want?! Can't you see I am busy drinking here? You don't even look like you belong here. You can have my seat when I am good and ready to leave, which won't be for a while yet. If I were you, I would get lost before I got myself hurt.");
