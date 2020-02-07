f73791b8d72bd408b7911ebe49a079f9e39b2d28
TurmoilToad <TurmoilToad@gmail.com>
2020-02-07 15:03:09 -0500

    Update 68228.pl
    Consistency with other boats.

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -1,9 +1,21 @@
 sub EVENT_SPAWN {
-	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
+	#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
+}
+
+sub EVENT_WAYPOINT_ARRIVE {
+	#:: Match waypoint 1
+	if ($wp == 1) {
+		#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
+	}
+	#:: Match waypoint 2
+	elsif ($wp == 2) {
+		quest::clear_proximity();
+	}
 }
 
 sub EVENT_ENTER {
 	#:: Move client to Ocean of Tears (oot)
-	quest::movepc(69,-9193.3,386.7,5.2);
+	quest::movepc(69, -9193, 386 , 5.2);
 }
