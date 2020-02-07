327e6dcacc7be6d7ad05e9e010e9fa9eaa3309a4
TurmoilToad <TurmoilToad@gmail.com>
2020-02-07 14:34:24 -0500

    Create 69134.pl
    Add port

---
+++ oot/69134.pl
@@ -0,0 +1,17 @@
+sub EVENT_WAYPOINT_ARRIVE {
+	#:: Match waypoint 1
+	if ($wp == 1) {
+		#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
+	}
+	#:: Match waypoint 2
+	elsif ($wp == 2) {
+		quest::clear_proximity();
+	}
+}
+
+sub EVENT_ENTER {
+	quest::emote("'s spirit moves you to another land ...");
+	#:: Cast 2280 - Portal to Butcher
+	quest::selfcast(2280);
+}
