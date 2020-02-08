a5a9cd8165eddff668ec1ed978e3a2169a7c30ac
TurmoilToad <TurmoilToad@gmail.com>
2020-02-07 23:24:49 -0500

    Update Joffrey_Clay.pl
    Syntax

--- qeynos/Joffrey_Clay.pl
+++ qeynos/Joffrey_Clay.pl
@@ -11,8 +11,8 @@ sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match when NPC arrives at waypoint 1
 	if ($wp == 1) {
 		quest::say("Belious, I'm working on this new tune I learned on my last trip to Kelethin. It's about Tunare. If you have a free ear, would you care to take a listen?");
-		#:: Send a signal "2" to South Qeynos >> Belious_Naliedin (1125) with 0 second delay
-		quest::signal(1125,2,0);
+		#:: Send a signal "2" to South Qeynos >> Belious_Naliedin (1125) with no delay
+		quest::signalwith(1125, 2, 0);
 	}
 	#:: Match when NPC arrives at waypoint 2
 	elsif ($wp == 2) {
