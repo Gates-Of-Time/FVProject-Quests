e49fc5aedb6176d51e707065142ae5093de301fc
TurmoilToad <TurmoilToad@gmail.com>
2020-02-07 23:18:33 -0500

    Update 1173.pl
    Adjust port

--- qeynos/1173.pl
+++ qeynos/1173.pl
@@ -1,7 +1,12 @@
+sub EVENT_SPAWN {
+	#:: Create a proximity, 80 units across, 200 units tall, without proximity say
+	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
+}
+
 sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match waypoint 23
 	if ($wp == 23) {
-		#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+		#:: Create a proximity, 80 units across, 200 units tall, without proximity say
 		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
 	#:: Match waypoint 1
@@ -11,6 +16,6 @@ sub EVENT_WAYPOINT_ARRIVE {
 }
 	
 sub EVENT_ENTER {
-	#:: Cast 2289 - Portal to Erud's Crossing
-	quest::selfcast(2289);
+	#:: Move client to Erud's Crossing (erudsxing)
+	quest::movepc(98, 716.9, -1771.8, 3.2);
 }
