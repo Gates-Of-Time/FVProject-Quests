a69f878ce9d66f124164666506f737084272350b
TurmoilToad <TurmoilToad@gmail.com>
2020-02-08 09:51:23 -0500

    Update 24301.pl
    Adjust move to

--- erudnext/24301.pl
+++ erudnext/24301.pl
@@ -1,13 +1,13 @@
 sub EVENT_SPAWN {
-	#:: Create a proximity, 100 units across, 100 units tall, without proximity say
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
+	#:: Create a proximity, 80 units across, 200 units tall, without proximity say
+	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match waypoint 1
 	if ($wp == 1) {
-		#:: Create a proximity, 100 units across, 100 units tall, without proximity say
-		quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
+		#:: Create a proximity, 80 units across, 200 units tall, without proximity say
+		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
 	#:: Match waypoint 2
 	elsif ($wp == 2) {
@@ -16,6 +16,6 @@ sub EVENT_WAYPOINT_ARRIVE {
 }
 
 sub EVENT_ENTER {
-	#:: Move client to South Qeynos (qeynos)
-	quest::movepc(1, 209, 17, 3.75, 125);
+	#:: Move client to Erud's Crossing (erudsxing)
+	quest::movepc(98, 716.9, -1771.8, 3.2);
 }
