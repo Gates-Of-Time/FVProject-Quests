69325d269eba48cf528720c004295ae3232b4a15
TurmoilToad <TurmoilToad@gmail.com>
2020-02-07 23:23:46 -0500

    Update 24301.pl
    Adjust port

--- erudnext/24301.pl
+++ erudnext/24301.pl
@@ -3,7 +3,19 @@ sub EVENT_SPAWN {
 	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
+sub EVENT_WAYPOINT_ARRIVE {
+	#:: Match waypoint 1
+	if ($wp == 1) {
+		#:: Create a proximity, 100 units across, 100 units tall, without proximity say
+		quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
+	}
+	#:: Match waypoint 2
+	elsif ($wp == 2) {
+		quest::clear_proximity();
+	}
+}
+
 sub EVENT_ENTER {
-	#:: Move client to Erud's Crossing (erudsxing)
-	quest::movepc(98,716.9,-1771.8,3.2);
+	#:: Move client to South Qeynos (qeynos)
+	quest::movepc(1, 209, 17, 3.75, 125);
 }
