8d9efff819c71ef1bdfe80b437440d9473e88ab3
TurmoilToad <TurmoilToad@gmail.com>
2020-02-08 09:49:30 -0500

    Update 98053.pl
    Adjust the Sea King

--- erudsxing/98053.pl
+++ erudsxing/98053.pl
@@ -1,16 +1,18 @@
+#:: Erud's Crossing >> Sea_King (98053)
+
 sub EVENT_SPAWN {
 	#:: Create a proximity, 80 units across, 200 units tall, without proximity say
 	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-	#:: Match waypoint 1
-	if ($wp == 24) {
+	#:: Match waypoint 22
+	if ($wp == 22) {
 		#:: Create a proximity, 80 units across, 200 units tall, without proximity say
 		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
-	#:: Match waypoint 2
-	elsif ($wp == 25) {
+	#:: Match waypoint 23
+	elsif ($wp == 23) {
 		quest::clear_proximity();
 	}
 }
