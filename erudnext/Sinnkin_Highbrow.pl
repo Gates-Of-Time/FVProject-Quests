b65ec5e7b1daa3433846c2cfe5d3f21014f19653
TurmoilToad <TurmoilToad@gmail.com>
2020-02-09 17:40:46 -0500

    Update Sinnkin_Highbrow.pl
    Diminishing Quest Returns

--- erudnext/Sinnkin_Highbrow.pl
+++ erudnext/Sinnkin_Highbrow.pl
@@ -21,7 +21,29 @@ sub EVENT_ITEM {
 		#:: Set faction
 		quest::faction(694, 5); 		#:: + Erudite Citizen
 		#:: Give a small amount of xp
-		quest::exp(500);
+		#:: quest::exp(500);
+		#:: Diminishing Quest Rewards
+		$key = $client->CharacterID() . "-" . $npc->GetNPCTypeID() . "-13121-turnin-count";
+		#:: Match if data bucket exists
+		if (quest::get_data($key) < 20) {
+			#:: Increment the data bucket value
+			quest::set_data($key, quest::get_data($key) + 1);
+			#:: Create a scalar to store the value of the data bucket
+			$turnin_count = quest::get_data($key);
+			#:: Reward experience that decreases as the count increases
+			quest::exp((20/$turnin_count) * 500);
+		}
+		elsif (quest::get_data($key) == 20) {
+			quest::exp(500);
+		}
+		else {
+			#:: Set the data bucket with a value of 1
+			quest::set_data($key, 1);
+			#:: Create a scalar to store the value of the data bucket
+			$turnin_count = quest::get_data($key);
+			#:: Reward experience that decreases as the count increases
+			quest::exp((20/$turnin_count) * 500);
+		}
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
