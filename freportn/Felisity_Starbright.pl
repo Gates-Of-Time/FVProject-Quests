52a2887fa750dd80bf968ce47d05a05195909c64
TurmoilToad <TurmoilToad@gmail.com>
2020-02-11 18:20:14 -0500

    Update Felisity_Starbright.pl
    Refactor for consistency

--- freportn/Felisity_Starbright.pl
+++ freportn/Felisity_Starbright.pl
@@ -5,95 +5,95 @@ sub EVENT_SAY {
 } 
 
 sub EVENT_ITEM {
-	#:: Turn in for 18158 - A Bardic Letter (Freeport)
-	if (plugin::takeItems(18158 => 1 )) {
+	#:: Match a 18158 - A Bardic Letter (Freeport)
+	if (plugin::takeItems(18158 => 1)) {
 		quest::say("Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring");
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(284, 100); 		#:: + League of Antonican Bards
+		quest::faction(281, 10); 		#:: + Knights of Truth
+		quest::faction(262, 10); 		#:: + Guards of Qeynos
+		quest::faction(304, -30); 		#:: - Ring of Scale
+		quest::faction(285, -30); 		#:: - Mayong Mistmoore
+		#:: Grant a small amount of experience
 		quest::exp(80);
 		#:: Create a hash for storing cash - 600 to 1000cp
 		my %cash = plugin::RandomCash(600,1000);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,100); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
-	#:: Turn in for 18157 - A Bardic Letter (Freeport)
-	if (plugin::takeItems(18157 => 1 )) {
+	#:: Match a 18157 - A Bardic Letter (Freeport)
+	elsif (plugin::takeItems(18157 => 1)) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(284, 100); 		#:: + League of Antonican Bards
+		quest::faction(281, 10); 		#:: + Knights of Truth
+		quest::faction(262, 10); 		#:: + Guards of Qeynos
+		quest::faction(304, -30); 		#:: - Ring of Scale
+		quest::faction(285, -30); 		#:: - Mayong Mistmoore
+		#:: Grant a small amount of experience
 		quest::exp(80);
 		#:: Create a hash for storing cash - 600 to 1000cp
 		my %cash = plugin::RandomCash(600,1000);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,100); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
-	#:: Turn in for 18159 - A Bardic Letter (Freeport)
-	if (plugin::takeItems(18159 => 1 )) {
+	#:: Match a 18159 - A Bardic Letter (Freeport)
+	elsif (plugin::takeItems(18159 => 1)) {
 		quest::say("'Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring.'");
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(284, 100); 		#:: + League of Antonican Bards
+		quest::faction(281, 10); 		#:: + Knights of Truth
+		quest::faction(262, 10); 		#:: + Guards of Qeynos
+		quest::faction(304, -30); 		#:: - Ring of Scale
+		quest::faction(285, -30); 		#:: - Mayong Mistmoore
+		#:: Grant a small amount of experience
 		quest::exp(100);
 		#:: Create a hash for storing cash - 800 to 1200cp
 		my %cash = plugin::RandomCash(800,1200);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set faction
-		quest::faction(284,100); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
-	#:: Turn in for 18155 - A Bardic Letter (Freeport)
-	if (plugin::takeItems(18155 => 1 )) {
+	#:: Match a 18155 - A Bardic Letter (Freeport)
+	elsif (plugin::takeItems(18155 => 1)) {
 		quest::say("Incoming mail - very good!  Please take this gold for your troubles.");
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(284, 100); 		#:: + League of Antonican Bards
+		quest::faction(281, 10); 		#:: + Knights of Truth
+		quest::faction(262, 10); 		#:: + Guards of Qeynos
+		quest::faction(304, -30); 		#:: - Ring of Scale
+		quest::faction(285, -30); 		#:: - Mayong Mistmoore
+		#:: Grant a small amount of experience
 		quest::exp(100);
 		#:: Create a hash for storing cash - 1100 to 15000cp
 		my %cash = plugin::RandomCash(1100,1500);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,100); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
-	#:: Turn in for 18166 - Pouch of Mail (Freeport)
-	if (plugin::takeItems(18166 => 1 )) {
+	#:: Match a 18166 - Pouch of Mail (Freeport)
+	elsif (plugin::takeItems(18166 => 1)) {
 		quest::say("'Mail from the front - thank you very much! Please take this gold for your troubles. If you are interested in more work, just ask Ton Twostring");
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(284, 100); 		#:: + League of Antonican Bards
+		quest::faction(281, 10); 		#:: + Knights of Truth
+		quest::faction(262, 10); 		#:: + Guards of Qeynos
+		quest::faction(304, -30); 		#:: - Ring of Scale
+		quest::faction(285, -30); 		#:: - Mayong Mistmoore
+		#:: Grant a small amount of experience
 		quest::exp(100);
 		#:: Create a hash for storing cash - 1100 to 15000cp
 		my %cash = plugin::RandomCash(1100,1500);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set factions
-		quest::faction(284,100); 	#:: + League of Antonican Bards
-		quest::faction(281,10); 	#:: + Knights of Truth
-		quest::faction(262,10); 	#:: + Guards of Qeynos
-		quest::faction(304,-30); 	#:: - Ring of Scale
-		quest::faction(285,-30); 	#:: - Mayong Mistmoore
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
