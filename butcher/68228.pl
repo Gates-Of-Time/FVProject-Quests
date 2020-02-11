6da30c59f8c9fb1e440d865278c58b7dd095d734
TurmoilToad <TurmoilToad@gmail.com>
2020-02-11 13:18:27 -0500

    Update 68228.pl

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -1,12 +1,14 @@
+#:: Butcherblock Mountains >> SirensBane (68228)
+
 sub EVENT_SPAWN {
-	#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+	#:: Create a proximity, 80 units across, 200 units tall, without proximity say
 	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match waypoint 1
 	if ($wp == 1) {
-		#:: Create a proximity, 80 units across, 80 units tall, without proximity say
+		#:: Create a proximity, 80 units across, 100 units tall, without proximity say
 		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
 	#:: Match waypoint 2
