264cb2aa0554780efb6e0ab9972032f54499600a
TurmoilToad <TurmoilToad@gmail.com>
2020-02-11 13:30:42 -0500

    Update 68228.pl

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -7,12 +7,12 @@ sub EVENT_SPAWN {
 
 sub EVENT_WAYPOINT_ARRIVE {
 	#:: Match waypoint 1
-	if ($wp == 1) {
+	if ($wp == 0) {
 		#:: Create a proximity, 80 units across, 100 units tall, without proximity say
 		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
 	#:: Match waypoint 2
-	elsif ($wp == 2) {
+	elsif ($wp == 1) {
 		quest::clear_proximity();
 	}
 }
