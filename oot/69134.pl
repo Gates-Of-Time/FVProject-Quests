d2e253edeb8f75b3a7739807fa0b89c6b3daf1f1
TurmoilToad <TurmoilToad@gmail.com>
2020-02-11 20:38:34 -0500

    Update 69134.pl

--- oot/69134.pl
+++ oot/69134.pl
@@ -4,13 +4,13 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-	#:: Match waypoint 1
-	if ($wp == 1) {
+	#:: Match waypoint 0
+	if ($wp == 0) {
 		#:: Create a proximity, 80 units across, 80 units tall, without proximity say
 		quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 100, $z + 100, 0);
 	}
-	#:: Match waypoint 2
-	elsif ($wp == 2) {
+	#:: Match waypoint 1
+	elsif ($wp == 1) {
 		quest::clear_proximity();
 	}
 }
