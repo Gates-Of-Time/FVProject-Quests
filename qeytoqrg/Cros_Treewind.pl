136fb78bba2935dad16271513b71a6acbaab9330
TurmoilToad <TurmoilToad@gmail.com>
2020-02-19 08:56:40 -0500

    Create Cros_Treewind.pl

---
+++ qeytoqrg/Cros_Treewind.pl
@@ -0,0 +1,36 @@
+sub EVENT_SPAWN {
+	my $min = 1;
+	my $range = 4;
+	my $randomspawn = int(rand($range)) + $min;
+	plugin::RandomRoam(250, 250);
+	quest::settimer("move",$randomspawn);
+}
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Natural selection at work.");
+	}
+}
+
+sub EVENT_TIMER {
+	if ($timer eq "move") {
+		plugin::RandomRoam(250, 250);
+	}
+}
+
+sub EVENT_SAY {
+	#:: Missing Text - Based on Holly Windstalker
+	if ($text=~/hail/i) {
+		quest::say("Well met, $name.  I am Cros Treewind, Protector of the Pine.  What brings you out to the Qeynos Hills?  Not hunting, I hope. These stains on my staff are all that is left of the last adventurer I caught harming my bear and wolf friends.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My comrades will avenge my death.");
+}
