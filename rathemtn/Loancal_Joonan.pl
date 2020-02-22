2779a8fcc4078fc35b6afd41c052749a023f0c9b
TurmoilToad <TurmoilToad@gmail.com>
2020-02-22 14:44:38 -0500

    Update Loancal_Joonan.pl
    Typo

--- rathemtn/Loancal_Joonan.pl
+++ rathemtn/Loancal_Joonan.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("I'm afraid I can't speak right now, Stroh. I'm waiting for for someone very important to arrive.");
+		quest::say("I'm afraid I can't speak right now, $name. I'm waiting for for someone very important to arrive.");
 	}
 }
 
