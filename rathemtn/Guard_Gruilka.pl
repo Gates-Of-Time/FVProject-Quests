1b1b6a2ee9c73b253ce2c2a6e5a81675fc45c0f4
TurmoilToad <TurmoilToad@gmail.com>
2020-02-22 11:12:38 -0500

    Update Guard_Gruilka.pl
    Refactor for consistency, correct grammar.

--- rathemtn/Guard_Gruilka.pl
+++ rathemtn/Guard_Gruilka.pl
@@ -1,23 +1,23 @@
 sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
-		}
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Time to die $name!");
 	}
 }
 
 sub EVENT_AGGRO {
-	my $random = int(rand(3));
-	if($random == 0) {
-		quest::say("it's $class like you that insult all of Norrath!");
+	#:: Create a scalar variable to store a random number
+	$random = int(rand(3));
+	#:: Create a scalar variable to store the plural class name
+	$classplural = "${class}s";
+	if ($random == 0) {
+		quest::say("it's $classplural like you that insult all of Norrath!");
 	}
-	if($random == 1) {
-		quest::say("$class like you are an affront to my senses!");
+	elsif($random == 1) {
+		quest::say("$classplural like you are an affront to my senses!");
 	}
-	if($random == 1) {
-		quest::say("I really hate $class like you!");
+	elsif($random == 1) {
+		quest::say("I really hate $classplural like you!");
 	}	
 }
 
