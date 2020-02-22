56726406ffd1ebed30696a2e27a0f2ceeca6c4c3
TurmoilToad <TurmoilToad@gmail.com>
2020-02-22 09:03:27 -0500

    Update David.pl
    Refactor for consistency, adjust faction reward.

--- rathemtn/David.pl
+++ rathemtn/David.pl
@@ -1,36 +1,54 @@
-#########################################################
-# David
-# Zone:  Rathe Mountains
-# Quest:  Bracers of Ro
-# Author: Unknown
-# Updated:  Andrew80k
-#########################################################
 sub EVENT_SAY { 
-
-   if($text=~/Hail/i){
-      quest::say("I, the keeper of the [Bracers of Ro],  welcome you.  Come and rest within our camp.  You have nothing to fear while such righteous might is gathered.");
-   }
-   if($text=~/bracers of ro/i){
-      if ($faction <= 4) {
-         quest::say("When you can swim the waters of Rathe and return two goblin nets from the elusive goblin net masters. then you will be rewarded the bracer mold.");
-      } else {
-         quest::say("We, the Deepwater Knights, know of your vile ways. You had best leave while you can."); 
-      }
-   }
-}
-sub EVENT_ITEM { 
-   if(plugin::check_handin(\%itemcount,12311 => 2) && $faction <= 4) {
-     quest::say("You have done well. Take the mold for the bracer.  Go forth to speak with Thomas of [Lord Searfire].  Then all components shall be known.");
-     quest::summonitem("12301");
-     quest::faction("695","1");
-     quest::faction("242","1");
-     quest::faction("265","-1"); 
-   } elsif (plugin::check_handin(\%itemcount,12311=>1) && $faction <= 4) {
-     quest::say("Two Deepwater goblin nets are required.");
-     quest::return_items(\%itemcount);
-   }
-   quest::return_items(\%itemcount);
+	if ($text=~/hail/i) {
+		quest::say("I, the keeper of the [Bracers of Ro],  welcome you.  Come and rest within our camp.  You have nothing to fear while such righteous might is gathered.");
+	}
+	elsif ($text=~/bracers of ro/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("When you can swim the waters of Rathe and return two goblin nets from the elusive goblin net masters. then you will be rewarded the bracer mold.");
+		} 
+		else {
+			quest::say("We, the Deepwater Knights, know of your vile ways. You had best leave while you can."); 
+		}
+	}
 }
 
-
-#END of FILE Zone:rathemtn  ID:50108 -- David
+sub EVENT_ITEM {
+	#:: Match two 12311 - Deepwater Goblin Net
+	if (plugin::takeItems(12311 => 2)) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("You have done well. Take the mold for the bracer.  Go forth to speak with Thomas of [Lord Searfire].  Then all components shall be known.");
+			#:: Give a 12301 - Mold of Ro Bracer
+			quest::summonitem(12301);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(242, 20);		#:: + Deepwater Knights
+			quest::faction(266, 3);			#:: + High Council of Erudine
+			quest::faction(265, -3);		#:: - Heretics
+		}
+		else {
+			quest::say("We, the Deepwater Knights, know of your vile ways. You had best leave while you can.");
+			#:: Return two 12311 - Deepwater Goblin Net
+			quest::summonitem(12311);
+			quest::summonitem(12311);
+		}
+	}
+	#:: Match a 12311 - Deepwater Goblin Net
+	elsif (plugin::takeItems(12311 => 1)) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Two Deepwater goblin nets are required.");
+			#:: Return a 12311 - Deepwater Goblin Net
+			quest::summonitem(12311);
+		}
+		else {
+			quest::say("We, the Deepwater Knights, know of your vile ways. You had best leave while you can.");
+			#:: Return a 12311 - Deepwater Goblin Net
+			quest::summonitem(12311);
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
