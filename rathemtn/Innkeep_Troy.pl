f137b78caca287e7aab38583406a3b773e958ff9
TurmoilToad <TurmoilToad@gmail.com>
2020-02-22 11:08:32 -0500

    Update Innkeep_Troy.pl
    Refactor for consistency, return traded items.

--- rathemtn/Innkeep_Troy.pl
+++ rathemtn/Innkeep_Troy.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome to the Rathe Mountains hideaway!"); }
+	if ($text=~/hail/i) {
+		quest::say("Welcome to the Rathe Mountains hideaway!");
+	}
 }
-#END of FILE Zone:rathemtn  ID:50117 -- Innkeep_Troy 
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
