8b052bbf9399192902d24a04683adaf1cefabc08
TurmoilToad <TurmoilToad@gmail.com>
2020-02-24 10:04:05 -0500

    Update Tearon_Bleanix.pl
    Updated with Thex Dagger

--- highkeep/Tearon_Bleanix.pl
+++ highkeep/Tearon_Bleanix.pl
@@ -1,41 +1,128 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Beware!!..  Beware, the Teir'Dal walk the halls of Highkeep!!");
+		quest::say("Hello sir. Are you a citizen of Highpass?");
 	}
-	if ($text=~/princess lenya thex/i) {
+	elsif ($text=~/not a citizen/i) {
+		quest::say("Nor am I. I find this cities love of greed appalling. Do not you?");
+	}
+	elsif ($text=~/appalling/i) {
+		quest::say("Yes. This city of vices is second only to Neriak. And it is trouble with Neriak that has sent me here. In search of my peoples princess.");
+	}
+	elsif ($text=~/princess/i) {
 		quest::say("The Princess Lenya Thex is the daughter of His Royal Majesty, King Tearis Thex of Felwithe. She was on her way to Qeynos when we believe she was kidnapped by Carson McCabe, the governor of this vile city. I await the paladin from Felwithe.");
 	}
-	#:: Match if faction is better than Amiable
-	if ($text=~/all is not bright above the clouds/i) {
-		if ($faction < 4) {
+	elsif ($text=~/all is not bright above the clouds/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
 			quest::say("Taken from this place she has been.  Seek the Highpass hussy.  Ask of her.  Only she knows where.  Find the Princess.  Give her this.  Show your allegiance.  This and her key. Then return the room key to me with the prize from the princess.  Become a hero!!");
 			#:: Give item 13108 - Tearons Bracer
 			quest::summonitem(13108);
 			#:: Ding!
 			quest::ding();	
 		}
-		elsif ($faction > 4 ) {
+		else {
+			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
+		}
+	}
+	elsif ($text=~/want to be a hero/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Well let's get started on making you a hero. You must take Elite Guard Bracer. Look for Princess Lenya. When you find her give her the bracer to prove you are with the Koada'dal. She should trust you then. Then return to me with Princess Lenya and return my bracer. Be safe my friend.");
+			#:: Give item 13108 - Tearons Bracer
+			quest::summonitem(13108);
+			#:: Ding!
+			quest::ding();
+			#:: Key a data bucket
+			$key = $client->CharacterID() . "-bracer-to-lenya";
+			#:: Set the data bucket
+			quest::set_data($key, 1);
+			#:: Spawn one and only one Princess_Lenya_Thex (51176)
+			quest::unique_spawn(51176, 0, 0, 220, 85, 74, 400);
+		}
+		else {
 			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
 		}
 	}
 }
 
-sub EVENT_ITEM {
-	#:: Turn in for 12267 & 13109 -  Highkeep Royal Suite & Royal Amulet of Thex
-	if (plugin::check_handin(\%itemcount, 12267 => 1, 13109 => 1)) {
-		quest::say("Peace..  I can rest now.  You now hold my Silent Watch Shield.  Protect Felwithe..");
-		#:: Give item 9312 - Silent Watch Shield
-		quest::summonitem(9312);
-		#:: Give a small amount of xp
-		quest::exp(2000);
-		#:: Ding!
-		quest::ding();
-		#:: Set faction
-		quest::faction(5001,10); 		#:: + Anti-Mage
-		quest::faction(226,10); 		#:: + Clerics of Tunare
-		quest::faction(279,10); 	#:: + King Tearis Thex
-		#:: Set Depop Timer
-		quest::settimer("depop",240);
+sub EVENT_TIMER {
+	#:: Match a timer 'depop'
+	if ($timer eq "depop") {
+		#:: Stop the timer 'depop' from triggering
+		quest::stoptimer("depop");
+		#:: Depop with spawn timer active
+		quest::depop_withtimer();
 	}
-	plugin::return_items(\%itemcount);
+}
+
+sub EVENT_ITEM {
+	#:: Match a 12267 - Highkeep Royal Suite, and a 13109 - Royal Amulet of Thex
+	if (plugin::takeItems(12267 => 1, 13109 => 1)) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Peace..  I can rest now.  You now hold my Silent Watch Shield.  Protect Felwithe..");
+			#:: Give a 9312 - Silent Watch Shield
+			quest::summonitem(9312);
+			#:: Ding!
+			quest::ding();
+			#:: Set factions
+			quest::faction(5001, 10);			#:: + Anti-Mage
+			quest::faction(226, 25); 			#:: + Clerics of Tunare
+			quest::faction(279, 25); 			#:: + King Tearis Thex
+			#:: Grant a moderate amount of experience
+			quest::exp(2000);
+			#:: Set a timer 'depop' to trigger every 240 seconds (4 min)
+			quest::settimer("depop", 240);
+		}
+		else {
+			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
+			#:: Give a 12267 - Highkeep Royal Suite
+			quest::summonitem(12267);
+			#:: Give a 13109 - Royal Amulet of Thex
+			quest::summonitem(13109);
+		}
+	}
+	#:: Match a 13108 - Tearons Bracer
+	elsif (plugin::takeItems(13108 => 1)) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			#:: Key a data bucket
+			$key = $client->CharacterID() . "-bracer-to-lenya";
+			#:: Match if the key value is 2
+			if (quest::get_data($key) == 2) {
+				quest::say("King Tearis Thex thanks you my friend. Could you please hand the princess this amulet. It is hers. I pryed it from the hands of some beggar.");
+				#:: Give a 13109 - Royal Amulet of Thex
+				quest::summonitem(13109);
+				#:: Ding!
+				quest::ding();
+				#:: Set factions
+				quest::faction(5001, 10);		#:: + Anti-Mage
+				quest::faction(226, 25); 		#:: + Clerics of Tunare
+				quest::faction(279, 25); 		#:: + King Tearis Thex
+				#:: Grant a small amount of experience
+				quest::exp(100);
+				#:: Delete the data bucket
+				quest::delete_data($key);
+				#:: Key a data bucket
+				$key = $client->CharacterID() . "-amulet-to-lenya";
+				#:: Set the data bucket
+				quest::set_data($key, 1);
+				#:: Spawn one and only one Princess_Lenya_Thex (51176)
+				quest::unique_spawn(51176, 0, 0, -325, -28, 54, 88);
+			}
+			else {
+				#:: Text made up
+				quest::say("Give Princess Lenya the bracer to prove you are with the Koada'dal.");
+				#:: Give a 13108 - Tearons Bracer
+				quest::summonitem(13108);
+			}
+		}
+		else {
+			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
+			#:: Give a 13108 - Tearons Bracer
+			quest::summonitem(13108);
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
