1da025bd9cd6dffe358ae91481f8823e45d61e76
TurmoilToad <TurmoilToad@gmail.com>
2020-02-25 20:13:06 -0500

    Update a_pile_of_mold.pl
    Added a data bucket to prevent overpopulation.

--- runnyeye/a_pile_of_mold.pl
+++ runnyeye/a_pile_of_mold.pl
@@ -4,7 +4,14 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_ENTER {
-	quest::emote("erupts in a cloud of spores!");
-	#:: Spawn a The Liberated Citadel of Runnyeye >> a_sporeling (11188)
-	quest::spawn2(11188, 0, 0, $x, $y, $z, $h);
+	#:: Key a data bucket
+	$key = $npc->GetID() . "-trap-sprung";
+	#:: Match if the key does not exist
+	if (!quest::get_data($key)) {
+		quest::emote("erupts in a cloud of spores!");
+		#:: Spawn a The Liberated Citadel of Runnyeye >> a_sporeling (11188)
+		quest::spawn2(11188, 0, 0, $x, $y, $z, $h);
+		#:: Set a data bucket with a value '1' that expires in 300 seconds (5 min)
+		quest::set_data($key, 1, 300);
+	}
 }
