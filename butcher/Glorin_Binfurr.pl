e7cca889d79ae5f0562425bca1ac21283c98c459
TurmoilToad <TurmoilToad@gmail.com>
2020-02-26 18:27:58 -0500

    Update Glorin_Binfurr.pl

--- butcher/Glorin_Binfurr.pl
+++ butcher/Glorin_Binfurr.pl
@@ -2,8 +2,17 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Pleasure to meet you.  Keep your eye out for the ship.  You don't want to miss it.");
 	}
-	if ($text=~/mudtoe's|mudtoes/i) {
+	elsif ($text=~/mudtoe's|mudtoes/i) {
 		quest::say("Mudtoes!! I have heard tales of that foul clan of ogres. A few passengers aboard the ships have claimed to have seen one of them, off and on, amongst the pirates in the Ocean of Tears!!");
+	}
+	elsif ($text=~/i love you/i) {
+		quest::say("Glorin and $name. We would make a fine couple.");
+	}
+	elsif ($text=~/marry me/i) {
+		quest::say("Why yes I'll marry you $name, we'll make a fine couple.");
+	}
+	elsif ($text=~/ugly dorf|ugly dwarf/i) {
+		quest::say("I believe the word '$name' is dwarven for ignorant, smelly skuzz bucket. That's what I hear.");
 	}
 }
 
