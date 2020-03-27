c051249bf6c9d7f916ff66ec6599bbf1c2e9de97
TurmoilToad <TurmoilToad@gmail.com>
2020-03-27 10:57:53 -0400

    Create a_water_elemental.pl
    Add a water elemental

---
+++ erudnint/a_water_elemental.pl
@@ -0,0 +1,21 @@
+sub EVENT_SPAWN {
+	#:: Create a timer 'depop' that loops every 300 seconds (5 min)
+	quest::settimer("depop", 300);
+}
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("The pain cellars of the Tribunal await! The High Guard's death shall be avenged");
+	}
+}
+
+sub EVENT_TIMER {
+	#:: Match timer 'depop'
+	if ($timer eq "depop") {
+		#:: Stop the timer from triggering
+		quest::stoptimer("depop");
+		#:: Depop without spawn timer
+		quest::depop();
+	}
+}
