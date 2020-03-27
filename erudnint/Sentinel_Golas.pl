508235d6cdb14547ebdbc466148e5fb2fab26e45
TurmoilToad <TurmoilToad@gmail.com>
2020-03-27 11:29:25 -0400

    Update Sentinel_Golas.pl
    Combat message

--- erudnint/Sentinel_Golas.pl
+++ erudnint/Sentinel_Golas.pl
@@ -1,3 +1,10 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Prepare to die, infidel!");
+	}
+}
+
 sub EVENT_DEATH_COMPLETE {
 	quest::say("Prepare to meet the vengeance of Erud!");
 	#:: Spawn a The Erudin Palace >> a_water_elemental (23081) at current location
