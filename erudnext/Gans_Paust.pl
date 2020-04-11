d56123e5b2af2a6d1db78a380d1720807d2fbaa1
TurmoilToad <TurmoilToad@gmail.com>
2020-04-11 19:58:05 -0400

    Update Gans_Paust.pl
    Removed diminishing quest reward.

--- erudnext/Gans_Paust.pl
+++ erudnext/Gans_Paust.pl
@@ -61,29 +61,7 @@ sub EVENT_ITEM {
 		quest::faction(266, 3);			#:: + High Council of Erudin
 		quest::faction(265, -3);		#:: - Heretics
 		#:: Grant a small amount of experience
-		#:: quest::exp(100);
-		#:: Diminishing Quest Rewards with Multiple Turn-ins
-		$key = $client->CharacterID() . "-" . $npc->GetNPCTypeID() . "1771-turnin-count";
-		#:: Match if data bucket exists
-		if (quest::get_data($key) < 20) {
-			#:: Increment the data bucket value
-			quest::set_data($key, quest::get_data($key) + 1);
-			#:: Create a scalar to store the value of the data bucket
-			$turnin_count = quest::get_data($key);
-			#:: Reward experience that decreases as the count increases
-			quest::exp((20/$turnin_count) * 100);
-		}
-		elsif (quest::get_data($key) == 20) {
-			quest::exp(100);
-		}
-		else {
-			#:: Set the data bucket with a value of 1
-			quest::set_data($key, 1);
-			#:: Create a scalar to store the value of the data bucket
-			$turnin_count = quest::get_data($key);
-			#:: Reward experience that decreases as the count increases
-			quest::exp((20/$turnin_count) * 100);
-		}
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
