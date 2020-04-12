5c2e358a27831bb26295c15a37f7d7ed92a7a010
TurmoilToad <TurmoilToad@gmail.com>
2020-04-11 20:02:33 -0400

    Update Rarnan_Lapice.pl
    Removed diminishing quest return.

--- erudnext/Rarnan_Lapice.pl
+++ erudnext/Rarnan_Lapice.pl
@@ -26,29 +26,7 @@ sub EVENT_ITEM {
 		quest::faction(266, 12); 		#:: + High Council of Erudin
 		quest::faction(265, -12); 		#:: - Heretics
 		#:: Grant a large amount of experience
-		#:: quest::exp(12500);
-		#:: Diminishing Quest Rewards
-		$key = $client->CharacterID() . "-" . $npc->GetNPCTypeID() . "-13991-turnin-count";
-		#:: Match if data bucket exists
-		if (quest::get_data($key) < 20) {
-			#:: Increment the data bucket value
-			quest::set_data($key, quest::get_data($key) + 1);
-			#:: Create a scalar to store the value of the data bucket
-			$turnin_count = quest::get_data($key);
-			#:: Reward experience that decreases as the count increases
-			quest::exp((20/$turnin_count) * 12500);
-		}
-		elsif (quest::get_data($key) == 20) {
-			quest::exp(12500);
-		}
-		else {
-			#:: Set the data bucket with a value of 1
-			quest::set_data($key, 1);
-			#:: Create a scalar to store the value of the data bucket
-			$turnin_count = quest::get_data($key);
-			#:: Reward experience that decreases as the count increases
-			quest::exp((20/$turnin_count) * 12500);
-		}
+		quest::exp(12500);
 		#:: Create a hash for storing cash - 5000 to 5500cp
 		my %cash = plugin::RandomCash(5000,5500);
 		#:: Grant a random cash reward
