446cae6d59bc143c7a1cc4276a365a766f493c7f
TurmoilToad <TurmoilToad@gmail.com>
2020-04-21 13:34:52 -0400

    Update Forager_Grikk.pl
    Refactor for consistency, adjust faction reward, switch to level-based experience.

--- innothule/Forager_Grikk.pl
+++ innothule/Forager_Grikk.pl
@@ -5,17 +5,17 @@ sub EVENT_SAY {
 } 
 
 sub EVENT_ITEM {
-	#:: Turn in for 13375 - Empty Vial
-	if (plugin::takeItems(13375 => 1 )) {
+	#:: Match a 13375 - Empty Vial
+	if (plugin::takeItems(13375 => 1)) {
 		quest::say("Grrrikk.. This is Groak's vial!! He must be in danger. Here. Return this liquid to him. Be quick!");
-		#:: Give item 13376 - Ochre Liquid
+		#:: Give a 13376 - Ochre Liquid
 		quest::summonitem(13376);
-		#:: Give a small amount of xp
-		quest::exp(500);
 		#:: Ding!
 		quest::ding();
 		#:: Set faction
-		quest::faction(251,20);		#:: + Frogloks of Guk
+		quest::faction(251, 1);		#:: + Frogloks of Guk
+		#:: Grant a small amount of experience based on level
+		$client->AddLevelBasedExp(10,1);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
