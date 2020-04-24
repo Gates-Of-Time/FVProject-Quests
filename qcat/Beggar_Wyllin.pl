b4d317db29420941451a6fc9fa664670c039a963
TurmoilToad <TurmoilToad@gmail.com>
2020-04-24 15:01:41 -0400

    Update Beggar_Wyllin.pl

--- qcat/Beggar_Wyllin.pl
+++ qcat/Beggar_Wyllin.pl
@@ -3,7 +3,7 @@ sub EVENT_SAY {
 		quest::say("Greetings!! This is no place for a person of your standards. I am but a beggar, that is why I live among the sewer rats. It would be best if you were to leave this dangerous place.");
 	}
 	elsif ($text=~/rat king/i) {
-		quest::say("That is but a child's fairy tale.")
+		quest::say("Rat King? What nonsense do you speak? That is nothing more than a child's fairy tale!")
 	}
 }
 
