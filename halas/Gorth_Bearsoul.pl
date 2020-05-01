0aeda6b5d1e4a831d0af0b8e11e37521b13d6466
TurmoilToad <TurmoilToad@gmail.com>
2020-05-01 13:59:00 -0400

    Update Gorth_Bearsoul.pl
    Refactor for consistency.

--- halas/Gorth_Bearsoul.pl
+++ halas/Gorth_Bearsoul.pl
@@ -1,11 +1,11 @@
 sub EVENT_SAY {
-	if ($text=~ /Hail/i) {
+	if ($text=~/hail/i) {
 		quest::say("Hail yerself. I'm sure ye've better things to do than to bother a man makin' his potions, then, eh?");
 	}
-	elsif ($text=~ /potion of sorrow/i) {
+	elsif ($text=~/potion of sorrow/i) {
 		quest::say("Ach! What'd ye be needin' that fer? Only those of dark magicks require such a thing and by me mother's eyes, never could I sell to such a soul.  If yer soul be clean, then put yer [money] in this enchanted pouch and I'll know yer intent.  If yer soul be rotten, I'll knowin', and ye'll be dead.  Tis indifferent I'd better be to yer actions.");
 	}
-	elsif ($text=~ /money/i) {
+	elsif ($text=~/money/i) {
 		quest::say("Sure'n ye dinnae think I'd give ye a potion fer free, then?  I'll be needin' five hundred gold coins to make such an item.  'Tis hard t' make and few are the uses fer such a potion.");
 	}
 }
@@ -18,7 +18,7 @@ sub EVENT_ITEM {
 		quest::summonitem(7113);
 		#:: Ding!
 		quest::ding();
-		#:: Grant a small amount of experience
+		#:: Grant a moderate amount of experience
 		quest::exp(1250);
 	}
 	#:: Return unused items
