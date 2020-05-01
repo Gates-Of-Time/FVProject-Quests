8f98af0fbabbd09f9bda0aa7fdf2c3a133350db3
TurmoilToad <TurmoilToad@gmail.com>
2020-05-01 13:44:41 -0400

    Update Holana_Oleary.pl
    Refactor for consistency, adjust cash reward.

--- halas/Holana_Oleary.pl
+++ halas/Holana_Oleary.pl
@@ -1,8 +1,8 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hmph. Hello, $name, how are ye an' how do ye do? Now, what are ye planning to buy?");
-		#:: Send a signal to Halas >> Murtog_MacYee (29043)
-		quest::signalwith(29043,1,1);
+		#:: Send a signal '1' to Halas >> Murtog_MacYee (29043)
+		quest::signalwith(29043, 1, 1);
 	}
 	elsif ($text=~/assist/i) {
 		#:: Match if faction is Amiable or better
@@ -15,7 +15,7 @@ sub EVENT_SAY {
 			quest::say("Run while ye still can!! The Wolves o' the North will not tolerate yer presence!");
 		}
 	}
-	elsif ($text=~/need directions to Clan McMannus/i) {
+	elsif ($text=~/need directions to clan mcmannus/i) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
 			quest::say("Ye go through Everfrost Peaks and run through Blackburrow. Once in Qeynos Hills, ye'll head to yer left and follow the pathway to the Plains of Karana. In the plains, when the pathway splits, go to yer right. Then just head toward the horizon.");
@@ -38,18 +38,18 @@ sub EVENT_ITEM {
 			quest::summonitem(quest::ChooseRandom(15270, 15275, 15075, 15271, 15279, 15212, 15079, 15274, 15272, 2031, 2036, 2030, 2034, 2027, 2038, 2026, 2029, 2025, 2032, 2028, 2033, 2912, 5043, 6032, 6030, 7022, 7024, 6031, 2912));
 			#:: Ding!
 			quest::ding();
-			#:: Grant a moderate amount of experience
-			quest::exp(1600);
-			#:: Create a hash for storing cash - 600 to 1100 cp
-			my %cash = plugin::RandomCash(600,1100);
-			#:: Grant a random cash reward
-			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-			#:: Set Factions
-			quest::faction(327, 10);	#:: + Shamen of Justice
-			quest::faction(328, 1);		#:: + Merchants of Halas
+			#:: Set factions
+			quest::faction(327, 10);		#:: + Shamen of Justice
+			quest::faction(328, 1);			#:: + Merchants of Halas
 			quest::faction(223, -1);		#:: - Circle of Unseen Hands  
 			quest::faction(336, -1);		#:: - Coalition of Tradefolk Underground 
 			quest::faction(244, -2);		#:: - Ebon Mask
+			#:: Grant a moderate amount of experience
+			quest::exp(1600);
+			#:: Create a hash for storing cash - 300 to 900 cp
+			my %cash = plugin::RandomCash(300,900);
+			#:: Grant a random cash reward
+			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		}
 		else {
 			quest::say("Run while ye still can!! The Wolves o' the North will not tolerate yer presence!");
@@ -60,5 +60,3 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-
-#:: Converted to Perl by SS
