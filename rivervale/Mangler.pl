f1942d7839d6f1dfd8d921cab2d6032cc0733104
TurmoilToad <TurmoilToad@gmail.com>
2020-05-08 21:49:08 -0400

    Update Mangler.pl
    Harden Mangler to not be spammy when the zone wakes.

--- rivervale/Mangler.pl
+++ rivervale/Mangler.pl
@@ -1,13 +1,18 @@
 sub EVENT_SPAWN {
-	#:: Set a timer for 12 minutes
+	#:: Set a timer 'mangler' that loops every 720 seconds (12 min)
 	quest::settimer("mangler", 720);
 }
 
 sub EVENT_TIMER {
-	#:: Match the "mangler" timer as it loops
+	#:: Match the 'mangler'
 	if ($timer eq "mangler") {
-		quest::say("<Bark, bark, bark! Hoooooowwwwwwl!>");
-		#:: Send a signal "1" to Rivervale >> Kevlin_Diggs (19049) after 0 seconds
-		quest::signalwith(19049,1,0);
+		#:: Key up a data bucket
+		$key = mangler_timer_manger;
+		#:: Match if the key does not exist
+		if (!quest::get_data($key) {
+			quest::say("<Bark, bark, bark! Hoooooowwwwwwl!>");
+			#:: Send a signal "1" to Rivervale >> Kevlin_Diggs (19049) after 0 seconds
+			quest::signalwith(19049,1,0);
+		}
 	}
 }
