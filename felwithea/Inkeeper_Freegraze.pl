4c103b61789b76e3d0511eb2e2a5fcc9b43be85e
TurmoilToad <TurmoilToad@gmail.com>
2020-05-18 12:58:15 -0400

    Update Inkeeper_Freegraze.pl
    Adjusted to unique spawn, don't eat items.

--- felwithea/Inkeeper_Freegraze.pl
+++ felwithea/Inkeeper_Freegraze.pl
@@ -1,7 +1,12 @@
 sub EVENT_SAY {
 	if ($text=~/tolon nurbyte/i) {
 		quest::say("So you are inquiring about Mister Tolon Nurbyte, eh? He is on the second floor, last door on the right. You two had best not be up to any mischief. The pair of you look a little shifty for the kingdom of Felwithe.");
-		#:: Spawn a Northern Felwithe >> Tolon_Nurbyte (61095)
-		quest::spawn2(61095, 0, 0, -343, 155, 17, 8);
+		#:: Spawn one and only one Northern Felwithe >> Tolon_Nurbyte (61095), without guild war or pathing grid, at the specified location
+		quest::unique_spawn(61095, 0, 0, -343, 155, 17, 8);
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
