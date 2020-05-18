22ceb936d9fe2513676c3be21cace7904c4f305c
TurmoilToad <TurmoilToad@gmail.com>
2020-05-18 16:05:31 -0400

    Update Tearon_Bleanix.pl
    Adjusted for era.

--- highkeep/Tearon_Bleanix.pl
+++ highkeep/Tearon_Bleanix.pl
@@ -1,3 +1,14 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("The single death of a member of the Paladins of Tunare shall plague your wretched life.");
+	}
+}
+
+sub EVENT_SLAY {
+	quest::say("Let no evil beings stand in the way of the righteousness of the Paladins of Tunare!");
+}
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hello sir. Are you a citizen of Highpass?");
@@ -9,21 +20,21 @@ sub EVENT_SAY {
 		quest::say("Yes. This city of vices is second only to Neriak. And it is trouble with Neriak that has sent me here. In search of my peoples princess.");
 	}
 	elsif ($text=~/princess/i) {
-		quest::say("The Princess Lenya Thex is the daughter of His Royal Majesty, King Tearis Thex of Felwithe. She was on her way to Qeynos when we believe she was kidnapped by Carson McCabe, the governor of this vile city. I await the paladin from Felwithe.");
+		quest::say("The Princess Lenya Thex is the daughter of His Royal Majesty, King Tearis Thex of Felwithe. She was on her way to Qeynos when we believe she was kidnapped by Carson McCabe, the governor of this vile city. I cannot get in the guards are on to me. How do you feel about becoming a hero to the Koada'dal or as you call us, high elves.");
 	}
-	elsif ($text=~/all is not bright above the clouds/i) {
-		#:: Match if faction is Amiable or better
-		if ($faction <= 4) {
-			quest::say("Taken from this place she has been.  Seek the Highpass hussy.  Ask of her.  Only she knows where.  Find the Princess.  Give her this.  Show your allegiance.  This and her key. Then return the room key to me with the prize from the princess.  Become a hero!!");
-			#:: Give item 13108 - Tearons Bracer
-			quest::summonitem(13108);
-			#:: Ding!
-			quest::ding();	
-		}
-		else {
-			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
-		}
-	}
+#::	elsif ($text=~/all is not bright above the clouds/i) {
+#::		#:: Match if faction is Amiable or better
+#::		if ($faction <= 4) {
+#::			quest::say("Taken from this place she has been.  Seek the Highpass hussy.  Ask of her.  Only she knows where.  Find the Princess.  Give her this.  Show your allegiance.  This and her key. Then return the room key to me with the prize from the princess.  Become a hero!!");
+#::			#:: Give item 13108 - Tearons Bracer
+#::			quest::summonitem(13108);
+#::			#:: Ding!
+#::			quest::ding();	
+#::		}
+#::		else {
+#::			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
+#::		}
+#::	}
 	elsif ($text=~/want to be a hero/i) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
@@ -56,34 +67,8 @@ sub EVENT_TIMER {
 }
 
 sub EVENT_ITEM {
-	#:: Match a 12267 - Highkeep Royal Suite, and a 13109 - Royal Amulet of Thex
-	if (plugin::takeItems(12267 => 1, 13109 => 1)) {
-		#:: Match if faction is Amiable or better
-		if ($faction <= 4) {
-			quest::say("Peace..  I can rest now.  You now hold my Silent Watch Shield.  Protect Felwithe..");
-			#:: Give a 9312 - Silent Watch Shield
-			quest::summonitem(9312);
-			#:: Ding!
-			quest::ding();
-			#:: Set factions
-			quest::faction(5001, 10);			#:: + Anti-Mage
-			quest::faction(226, 25); 			#:: + Clerics of Tunare
-			quest::faction(279, 25); 			#:: + King Tearis Thex
-			#:: Grant a moderate amount of experience
-			quest::exp(2000);
-			#:: Set a timer 'depop' to trigger every 240 seconds (4 min)
-			quest::settimer("depop", 240);
-		}
-		else {
-			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
-			#:: Give a 12267 - Highkeep Royal Suite
-			quest::summonitem(12267);
-			#:: Give a 13109 - Royal Amulet of Thex
-			quest::summonitem(13109);
-		}
-	}
 	#:: Match a 13108 - Tearons Bracer
-	elsif (plugin::takeItems(13108 => 1)) {
+	if (plugin::takeItems(13108 => 1)) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
 			#:: Key a data bucket
@@ -96,9 +81,9 @@ sub EVENT_ITEM {
 				#:: Ding!
 				quest::ding();
 				#:: Set factions
-				quest::faction(5001, 10);		#:: + Anti-Mage
 				quest::faction(226, 25); 		#:: + Clerics of Tunare
 				quest::faction(279, 25); 		#:: + King Tearis Thex
+				quest::faction(5001, 10);		#:: + Anti-Mage
 				#:: Grant a small amount of experience
 				quest::exp(100);
 				#:: Delete the data bucket
@@ -123,6 +108,32 @@ sub EVENT_ITEM {
 			quest::summonitem(13108);
 		}
 	}
+#::	#:: Match a 12267 - Highkeep Royal Suite, and a 13109 - Royal Amulet of Thex
+#::	elsif (plugin::takeItems(12267 => 1, 13109 => 1)) {
+#::		#:: Match if faction is Amiable or better
+#::		if ($faction <= 4) {
+#::			quest::say("Peace..  I can rest now.  You now hold my Silent Watch Shield.  Protect Felwithe..");
+#::			#:: Give a 9312 - Silent Watch Shield
+#::			quest::summonitem(9312);
+#::			#:: Ding!
+#::			quest::ding();
+#::			#:: Set factions
+#::			quest::faction(5001, 10);			#:: + Anti-Mage
+#::			quest::faction(226, 25); 			#:: + Clerics of Tunare
+#::			quest::faction(279, 25); 			#:: + King Tearis Thex
+#::			#:: Grant a moderate amount of experience
+#::			quest::exp(2000);
+#::			#:: Set a timer 'depop' to trigger every 240 seconds (4 min)
+#::			quest::settimer("depop", 240);
+#::		}
+#::		else {
+#::			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
+#::			#:: Give a 12267 - Highkeep Royal Suite
+#::			quest::summonitem(12267);
+#::			#:: Give a 13109 - Royal Amulet of Thex
+#::			quest::summonitem(13109);
+#::		}
+#::	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
