fae6f711bb22b043be57e4a61c68685fdbc0ed43
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 10:00:23 -0400

    Update Ilanics_skeleton.pl
    Refactor for consistency.

--- tox/Ilanics_skeleton.pl
+++ tox/Ilanics_skeleton.pl
@@ -2,13 +2,16 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("My scroll must find my [scroll] so I may finish my research.");
 	}
-	if ($text=~/scroll/i) {
+	elsif ($text=~/scroll/i) {
 		quest::say("Kobold! Kobold!");
-		quest::spawn2(38172, 0, 0, $x+5, $y-2, $z, $h);
-		quest::depop();
+		#:: Spawn one and only one Toxxulia Forest >> a_kobold_caster (38172), no guildwar or pathing grid, offset from current position
+		quest::unique_spawn(38172, 0, 0, $x+5, $y-2, $z, $h);
+		#:: Depop with spawn timer active
+		quest::depop_withtimer();
 	}
 }
 
 sub EVENT_ITEM {
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
