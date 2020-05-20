64e8c532c8fc39e7d8da290cd9ce319f0170083b
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 12:39:25 -0400

    Update Shintar_Vinlail.pl
    Missing attack and death messages.

--- tox/Shintar_Vinlail.pl
+++ tox/Shintar_Vinlail.pl
@@ -1,5 +1,21 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("That was not a very intelligent thing to do");
+	}
+}
+
 sub EVENT_SAY { 
-	if ($text=~/Hail/i) {
+	if ($text=~/hail/i) {
 		quest::say("I have no time to answer questions. whelp!  Now. leave this place before the sting of death finds the life in your veins!");
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My comrades will avenge my death.");
 }
