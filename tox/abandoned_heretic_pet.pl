269d4fc405f7e77a0e9b3d4b4e2435ddf8215960
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 10:42:03 -0400

    Update abandoned_heretic_pet.pl
    Refactor for consistency, adjust skeleton signaled, ding!

--- tox/abandoned_heretic_pet.pl
+++ tox/abandoned_heretic_pet.pl
@@ -1,17 +1,21 @@
 sub EVENT_ITEM {
-	#:: Match 13894 - Useless Cloth Cap
-	if (plugin::check_handin(\%itemcount, 13894 => 1)) {
+	#:: Match a 13894 - Useless Cloth Cap
+	if (plugin::takeItems(13894 => 1)) {
 		quest::say("Good work, you should be running this operation instead of that Talrigar fellow. Have a small reward. A little bit of the gems I found while tunneling through this rock.");
 		#:: Give a random reward: 10015 - Malachite, 10016 - Lapis Lazuli, 10017 - Turquoise, 10018 - Hematite
-		quest::summonitem(quest::ChooseRandom(10015,10016,10017,10018));
+		quest::summonitem(quest::ChooseRandom(10015, 10016, 10017, 10018));
+		#:: Ding!
+		quest::ding();
 	}
-	#:: Return Unused Items
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
 
 sub EVENT_SIGNAL {
-	#:: Signal from Tox/a_skeleton.pl
-	quest::say("Blast you, skeletons! Why I ever resurrected you, I don't know!");
-	#:: send a signal to 38017 - a_skeleton
-	quest::signal(38017,51);
+	#:: Match a signal '1' from tox/38016.pl (a_skeleton)
+	if ($signal == 1) {
+		quest::say("Blast you, skeletons! Why I ever resurrected you, I don't know!");
+		#:: send a signal '1' to Toxxulia Forest >> a_skeleton (38016) with no delay
+		quest::signalwith(38016, 1, 0);
+	}
 }
