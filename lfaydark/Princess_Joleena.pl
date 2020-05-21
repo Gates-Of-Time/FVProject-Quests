948e697b0df722e62c0c0aaeac51bf8d16597509
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 21:43:07 -0400

    Update Princess_Joleena.pl
    Remove say links

--- lfaydark/Princess_Joleena.pl
+++ lfaydark/Princess_Joleena.pl
@@ -3,7 +3,7 @@ sub EVENT_SAY {
 		quest::say("I am the Fairie Princess, Joleena. You may stay amongst my people, but do not nibble from our homes.");
 	}
 	if ($text=~/fairie gold dust/i) {
-		quest::say("You wish the magical Fairie Gold Dust? Hmmph!! I refuse to offer it to any of the giants of Faydwer. How can I trust any of you? You have crept up upon my me and snatched one of my wings from my back. There will be no Fairie Gold Dust until my wing has been returned to me. You may not be the stalker who snatched my  [" . quest::saylink("wing") . "], but all you giants are similar. Prove me wrong and return my wing.");
+		quest::say("You wish the magical Fairie Gold Dust? Hmmph!! I refuse to offer it to any of the giants of Faydwer. How can I trust any of you? You have crept up upon my me and snatched one of my wings from my back. There will be no Fairie Gold Dust until my wing has been returned to me. You may not be the stalker who snatched my [wing], but all you giants are similar. Prove me wrong and return my wing.");
 	}
 	if ($text=~/wing/i) {
 		quest::say("I did not see the swift giant, I was blinded by the sun glaring off his balding head. I do remember the smell of rust and refuse. How he got past my Clockwork Guardian, Gearheart, is a mystery to us all.");
@@ -21,7 +21,7 @@ sub EVENT_ITEM {
 		#:: Grant a moderate amount of experience
 		quest::exp(1500);
 		#:: Set faction
-		quest::faction(388,100); 	#:: + Fairie
+		quest::faction(388, 100); 	#:: + Fairie
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
