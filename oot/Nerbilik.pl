cf01a25d8743306173e3920409949c67de44f4cf
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 21:46:01 -0400

    Update Nerbilik.pl
    Remove say links, refactor for consistency

--- oot/Nerbilik.pl
+++ oot/Nerbilik.pl
@@ -3,7 +3,7 @@ $wrapper = 0;
 
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("What you want?!! Face so ugly you scare fish away!! ..sniff, sniff. Smell so bad, too!! Me sure is hungry. Wish had [" . quest::saylink("grub locker") . "].");
+		quest::say("What you want?!! Face so ugly you scare fish away!! ..sniff, sniff. Smell so bad, too!! Me sure is hungry. Wish had [grub locker].");
 	}
 	elsif ($text=~/grub locker/i) {
 		quest::say("You gots grub locker!! Give to me. Me sick of fish and not many dwarves fall off ship. Me pay a plat to get me grub locker from the Gobbler in Neriak. Go to him and say you [deliver grub locker] if you wants one plat.");
@@ -25,15 +25,15 @@ sub EVENT_ITEM {
 		quest::say("Oooh!! Grub locker! There is a loud click. He opens the locker and unwraps dwarf pickles. What this dumb wrapper? R, E, C, I, P, E.. Ahh!! It say pickles!! Me read good. Here your plat. Me give you little more. Me was very hungry for pickles.");
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(235,10);			#:: + Da Bashers
+		quest::faction(222,10);			#:: + Broken Skull Clan
+		#:: Grant a small amount of experience
+		quest::exp(150);
 		#:: Create a hash for storing cash - 1500 to 2000cp
 		my %cash = plugin::RandomCash(1500,2000);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Grant a small amount of experience
-		quest::exp(150);
-		#:: Set factions
-		quest::faction(235,10);	#:: + Da Bashers
-		quest::faction(222,10);	#:: + Broken Skull Clan
 		#:: Update wrapper variable
 		$wrapper = 1;
 		#:: Add a 18940 - Tattered Recipe to loot
