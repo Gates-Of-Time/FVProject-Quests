8f4ff3c5a78563c19f3551b70f8f7e3c63a17db6
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 21:51:58 -0400

    Update Oracle_of_K-Arnon.pl
    Remove say links, refactor for consistency

--- oot/Oracle_of_K-Arnon.pl
+++ oot/Oracle_of_K-Arnon.pl
@@ -1,43 +1,43 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Greetings, $name, I am the Oracle of K'Aron. Do you quest for [" . quest::saylink("knowledge") . "] yourself, or do you offer that which I [" . quest::saylink("seek") . "].");
+		quest::say("Greetings, $name, I am the Oracle of K'Aron. Do you quest for [knowledge] yourself, or do you offer that which I [seek].");
 	}
-	if ($text=~/knowledge/i) {
-		quest::say("I have lived long and have insight into many matters, yet I despise charity. To learn from me, you must first bring me that which I [" . quest::saylink("seek") . "].");
+	elsif ($text=~/knowledge/i) {
+		quest::say("I have lived long and have insight into many matters, yet I despise charity. To learn from me, you must first bring me that which I [seek].");
 	}
-	if ($text=~/seek/i) {
-		quest::say("I seek knowledge, which is superior to all things. And no knowledge is greater than that which is contained within the [" . quest::saylink("Tome of Ages") . "].");
+	elsif ($text=~/seek/i) {
+		quest::say("I seek knowledge, which is superior to all things. And no knowledge is greater than that which is contained within the [Tome of Ages].");
 	}
-	if ($text=~/tome of ages/i) {
-		quest::say("The Tome of Ages has been compiled over the millennia by the Scribes of [" . quest::saylink("Dal") . "]. It was divided into seven distinct [" . quest::saylink("books") . "] by the scribes to keep the knowledge from one such as me. That said, with aid, I shall unite these books and the Tome of Ages shall be mine.");
+	elsif ($text=~/tome of ages/i) {
+		quest::say("The Tome of Ages has been compiled over the millennia by the Scribes of [Dal]. It was divided into seven distinct [books] by the scribes to keep the knowledge from one such as me. That said, with aid, I shall unite these books and the Tome of Ages shall be mine.");
 	}
-	if ($text=~/dal/i) {
+	elsif ($text=~/dal/i) {
 		quest::say("They were elves once, but now are Teir'Dal, and as evil as they are wise. Many tomes have they written, but none greater than the Tome of Ages.");
 	}
-	if ($text=~/books/i) {
-		quest::say("The seven books that make up the Tome of Ages are those of [" . quest::saylink("Enlightenment") . "], [" . quest::saylink("Scale") . "], [" . quest::saylink("Turmoil") . "], [" . quest::saylink("Monuments") . "], [" . quest::saylink("Elders") . "], [" . quest::saylink("Blood") . "], and the [" . quest::saylink("Lost") . "].");
+	elsif ($text=~/books/i) {
+		quest::say("The seven books that make up the Tome of Ages are those of [Enlightenment], [Scale], [Turmoil], [Monuments], [Elders], [Blood], and the [Lost].");
 	}
-	if($text=~/enlightenment/i) {
+	elsif ($text=~/enlightenment/i) {
 		quest::say("I do not seek the Book of Enlightenment at this time, but I might have need of it in the future.");
 	}
-	if ($text=~/scale/i) {
+	elsif ($text=~/scale/i) {
 		quest::say("I do not seek the Book of Scale at this time, but I might have need of it in the future.");
 		#:: Part of The Fiery Avenger, Paladin Epic 1.0
 		#:: quest::say("Ah, the Book of Scale.. It would be wondrous indeed if you could recover the Book of Scale for me. If you do manage to procure the Book of Scale, I will gladly part with an artifact of my own that may be of interest to you.");
 	}
-	if ($text=~/turmoil/i) {
+	elsif ($text=~/turmoil/i) {
 		quest::say("Ah, yes! The Book of Turmoil. I seek this book, and should you find it and return it to me, I will reward you with secret knowledge.");
 	}
-	if ($text=~/monuments/i) {
+	elsif ($text=~/monuments/i) {
 		quest::say("I do not seek the Book of Monuments at this time, but I might have need of it in the future.");
 	}
-	if ($text=~/elders/i) {
+	elsif ($text=~/elders/i) {
 		quest::say("I do not seek the Book of Elders at this time, but I might have need of it in the future.");
 	}
-	if ($text=~/blood/i) {
+	elsif ($text=~/blood/i) {
 		quest::say("I do not seek the Book of Blood at this time, but I might have need of it in the future.");
 	}
-	if ($text=~/lost/i) {
+	elsif ($text=~/lost/i) {
 		quest::say("I do not seek the Lost Book at this time, but I might have need of it in the future.");
 	}
 }
@@ -57,11 +57,12 @@ sub EVENT_ITEM {
 		quest::summonitem(10071);
 		#:: Ding!
 		quest::ding();
-		#:: Grant a large amount of experience
-		quest::exp(72900);
 		#:: Set factions
 		quest::faction(402, 5);		#:: + Oracle of Karnon
 		quest::faction(403, -10);	#:: - Oracle of Marud
+		#:: Grant a large amount of experience
+		quest::exp(72900);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
