de0f071e5841c57de03d6d53c360c1526bb82eb0
TurmoilToad <TurmoilToad@gmail.com>
2020-05-20 21:44:33 -0400

    Update Dorn_B-Dynn.pl
    Remove say links

--- nro/Dorn_B-Dynn.pl
+++ nro/Dorn_B-Dynn.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Just who are you? Do you expect some sort of courteous greeting? I have no time for idle chitchat!! Either you are here to [" . quest::saylink("attend to business") . "] or you [" . quest::saylink("seek shelter") . "], which is it?");
+		quest::say("Just who are you? Do you expect some sort of courteous greeting? I have no time for idle chitchat!! Either you are here to [attend to business] or you [seek shelter], which is it?");
 	}
 	elsif ($text=~/attend to business/i) {
 		quest::say("So, business is your mission! I have never met you. Who sent you?");
@@ -14,8 +14,8 @@ sub EVENT_SAY {
 	elsif ($text=~/mukk/i) {
 		#:: Match if faction is Amiable or better - No response if Indifferent or worse
 		if ($faction <= 4 ) {
-			quest::say("So you were sent by Zimbittle. Take this note to Kraagia of Da Bashers in Grobb. Do not lose the note. If it were to fall into the wrong hands of [" . quest::saylink("Goonda") . "], this whole plan could be disrupted. Now, leave, I can no longer tolerate you foul stench!");
-			#:: 18886 - A Sealed Letter (Letter from The Dead)
+			quest::say("So you were sent by Zimbittle. Take this note to Kraagia of Da Bashers in Grobb. Do not lose the note. If it were to fall into the wrong hands of [Goonda], this whole plan could be disrupted. Now, leave, I can no longer tolerate you foul stench!");
+			#:: Give a 18886 - A Sealed Letter (Letter from The Dead)
 			quest::summonitem(18886);
 		}
 	}
