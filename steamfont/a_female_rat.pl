63c112ac2b8a201ea984c6c03a961f3565f2f2e9
TurmoilToad <TurmoilToad@gmail.com>
2020-06-02 12:48:21 -0400

    Update a_female_rat.pl
    Refactor for consistency.

--- steamfont/a_female_rat.pl
+++ steamfont/a_female_rat.pl
@@ -1,6 +1,6 @@
 sub EVENT_ITEM {
 	#:: Match a 10262 - Vial of Infectious Disease
-	if(plugin::check_handin(\%itemcount, 10262 => 1)) {
+	if (plugin::takeItems(10262 => 1)) {
 		quest::emote("ingests the virulent substance and becomes ill.");
 		#:: Give a 10263 - Empty Infectious Vial
 		quest::summonitem(10263);
@@ -8,10 +8,11 @@ sub EVENT_ITEM {
 		quest::ding();
 		#:: Grant a small amount of experience
 		quest::exp(100);
-		#:: Spawn a Steamfont Mountains >> diseased_female_rat (56148)
-		quest::spawn2(56148,0,0,$x,$y,$z,$h);
+		#:: Spawn a Steamfont Mountains >> diseased_female_rat (56148) at the current location
+		quest::spawn2(56148, 0, 0, $x, $y, $z, $h);
+		#:: Depop with spawn timer active
 		quest::depop_withtimer();
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
