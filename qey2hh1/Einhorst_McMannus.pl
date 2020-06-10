08ea2072a9329fa8f6768ea130070f97ba2ae9d6
TurmoilToad <TurmoilToad@gmail.com>
2020-06-09 21:31:50 -0400

    Update Einhorst_McMannus.pl
    Refactor for consistency, added missing death message, adjusted dialogue.

--- qey2hh1/Einhorst_McMannus.pl
+++ qey2hh1/Einhorst_McMannus.pl
@@ -9,7 +9,7 @@ sub EVENT_SAY {
 			quest::say("Welcome to the village of McMannus. We sell many goods. Feel free to look around - just keep your hands to yourself. Good day.");
 		}
 	}
-	if ($text=~/fugitive/i) {
+	elsif ($text=~/fugitive/i) {
 		#:: Match if faction is Amiable or better
 		if ($faction <= 4) {
 			quest::say("Searching for someone, eh? You will most likely never find them on the plains. This is a vast area. I could have Frostbite help you search. He is quite good at [tracking prey].");
@@ -19,13 +19,12 @@ sub EVENT_SAY {
 			quest::say("I see they have sent someone to hunt the scoundrel down. I am afraid I haven't seen the barbarian thief in quite some time. You can search and search for him here in the western plains, but I doubt you shall find him. Good luck.");
 		}
 	}
-	if ($text=~/tracking prey/i) {
+	elsif ($text=~/tracking prey/i) {
 	   	quest::say("Frostbite can track down anyone, be they man or beast. All one needs to do is obtain a SWEATY piece of clothing recently worn by the one you seek.");
      	}
-	if ($text=~/lion meat shipment/i) {
-		quest::say("Oh, yes, the lion meat shipment. Aye, it's here somewhere. Just a moment. Ah! Here you go. Apologize for the delay in this.");
-		#:: Give a 13961 - Lion Meat Shipment
-		quest::summonitem(13961);
+	elsif ($text=~/lion meat shipment/i) {
+		quest::say("That would be splinded Go to Misla and tell her you will deliver lion meat to Halas. She will instruct you.");
+
 	}
 }
 	
@@ -37,20 +36,22 @@ sub EVENT_ITEM {
 		quest::summonitem(13962);
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(320, 1);		#:: + Wolves of the North
+		quest::faction(327, 1);		#:: + Shamen of Justice
+		quest::faction(328, 1);		#:: + Merchants of Halas 
+		quest::faction(311, 1);		#:: + Steel Warriors
 		#:: Grant a moderate amount of experience
 		quest::exp(1600);
 		#:: Create a hash for storing cash - 600 to 1100 cp
 		my %cash = plugin::RandomCash(600,1100);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-		#:: Set Factions
-		quest::faction(320, 1);		#:: + Wolves of the North
-		quest::faction(327, 1);		#:: + Shamen of Justice
-		quest::faction(328, 1);		#:: + Merchants of Halas 
-		quest::faction(311, 1);		#:: + Steel Warriors	
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
 
-#:: Converted to Perl by SS
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You fool! My brothers and sisters of the Wolves of the North shall hunt you down! You... are... through...");
+}
