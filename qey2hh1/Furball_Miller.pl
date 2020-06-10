ef0aa4fc258dfc7658468686af7f2756ed768bb7
TurmoilToad <TurmoilToad@gmail.com>
2020-06-10 08:34:03 -0400

    Update and rename Furball_Miller.lua to Furball_Miller.pl
    Convert to perl, correct dialogue

---
+++ qey2hh1/Furball_Miller.pl
@@ -0,0 +1,15 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("<BARK!>  Hiya!  <Bark!>  <Bark!>  My name is Furball Miller.  I work here on my father's farm.  Of course, he is not my real [father] but he is the one who raisd me from a pup.  I hope to go back to [Blackburrow] some day and try to get them to stop all the senseless fighting with the humans of Qeynos.");
+	}
+	elsif ($text=~/blackburrow/i) {
+		quest::say("Blackburrow is north west of here. Bark! But I wouldn't go there $name. They don't like folks like you very much. I wish they were not so BARK! stubborn.");
+	}
+	elsif ($text=~/cleat/i) {
+		quest::say("Cleet and Henina Miller are the only parents I have ever known. They found me hurt in their field and raised me as if I was one of their own children. I love them very much. I don't remember my true parents at all but I know they came from Blackburrow.");
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
