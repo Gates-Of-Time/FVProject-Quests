d01888314d70a94f5b7bf47f9da30eb02ea0c9a8
TurmoilToad <TurmoilToad@gmail.com>
2020-06-09 21:38:02 -0400

    Update Mistrana_Two_Notes.pl
    Fix typo.

--- qey2hh1/Mistrana_Two_Notes.pl
+++ qey2hh1/Mistrana_Two_Notes.pl
@@ -3,7 +3,7 @@ sub EVENT_SAY {
 		quest::say("Hail, $name - Are you [interested] in helping the League of Antonican Bards by delivering some [mail]?");
 	}
 	elsif ($text=~/mail/i) {
-		quest::say(""I have messages that need to go to - well, right now I have one that needs to go to Qeynos.  Will you [deliver] mail to [Qeynos] for me?");
+		quest::say("I have messages that need to go to - well, right now I have one that needs to go to Qeynos.  Will you [deliver] mail to [Qeynos] for me?");
 	}
 	elsif ($text=~/qeynos/i) {
 		quest::say("Take this letter to Tralyn Marsinger in Qeynos.  You can find him at the bard guild hall.  I am sure he will compensate you for your troubles.");
