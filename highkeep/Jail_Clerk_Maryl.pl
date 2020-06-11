f3eeb47d318e9919880d615a7ae8b345474b9e46
TurmoilToad <TurmoilToad@gmail.com>
2020-06-11 09:22:08 -0400

    Update Jail_Clerk_Maryl.pl
    Refactor for consistency

--- highkeep/Jail_Clerk_Maryl.pl
+++ highkeep/Jail_Clerk_Maryl.pl
@@ -2,24 +2,25 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hail! I am the jail clerk of Highkeep. How may I be of service?");
 	}
-	if ($text=~/highkeep lottery/i) {
+	elsif ($text=~/highkeep lottery/i) {
 		quest::say("The Highkeep Lottery is held every season.  The Casino Treasurer, Lynn, handles all ticket sales and information.  She can be found at the vault.");
 	}
-	if ($text=~/fenn kaedrick/i) {
+	elsif ($text=~/fenn kaedrick/i) {
 		quest::say("Why do you ask of that horrid rogue? We had him locked away with that high elf and then he got bailed out. I know of no reason why anyone would want to bail his bloody carcass out of our dungeon.");
 	}
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18935 -  Sealed Note
-	if (plugin::check_handin(\%itemcount, 18935 => 1)) {
+	#:: Match a 18935 - Sealed Note
+	if (plugin::takeItems(18935 => 1)) {
 		quest::say("That little halfing has escaped our prison. No doubt he will travel back to the safety of Rivervale. He left this cracked emerald. It appears he must have broken off a piece of it. I believe it belongs in Kaladim. You may take it, but I am sure your master will not be satisfied until you combine the final piece with this one. Of course, first you will have to find Bronin and ask him of the missing emerald shard.");
-		#:: Give item 17927 - Cracked Emerald
+		#:: Give a 17927 - Cracked Emerald
 		quest::summonitem(17927);
-		#:: Give a small amount of xp
-		quest::exp(1000);
 		#:: Ding!
 		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(1000);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
