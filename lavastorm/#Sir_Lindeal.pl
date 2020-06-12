47f5eee292691944b1b5de51c48f32f8ac5da2f9
TurmoilToad <TurmoilToad@gmail.com>
2020-06-12 13:33:05 -0400

    Update #Sir_Lindeal.pl
    Found additional death message...

--- lavastorm/#Sir_Lindeal.pl
+++ lavastorm/#Sir_Lindeal.pl
@@ -10,7 +10,13 @@ sub EVENT_COMBAT {
 }
 
 sub EVENT_DEATH_COMPLETE {
-	quest::say("You will meet your own doom soon enough, my friend.. my influence with the Knights of Thunder is great, and they will not rest until I am avenged!");
+	$Random = quest::ChooseRandom(1,2);
+	if ($Random == 1) {
+		quest::say("You will meet your own doom soon enough, my friend.. my influence with the Knights of Thunder is great, and they will not rest until I am avenged!");
+	}
+	else {
+		quest::say("Karana shall guide the Knights of Thunder to avenge the death of one of his flock.");
+	}
 }
 
 sub EVENT_ITEM {
