856b0998532699cab7a9b32947d7f9fb36e5cea7
FVFippy <44533609+FVFippy@users.noreply.github.com>
2020-07-05 15:49:54 -0400

    Update Zanotix_Ixtaz.pl
    Fixed a typo in the Spawn code (missing #2) and added Hail dialog.

--- neriakb/Zanotix_Ixtaz.pl
+++ neriakb/Zanotix_Ixtaz.pl
@@ -1,7 +1,11 @@
 sub EVENT_SAY {
-if($text=~/what indigo sabres/i){
+if($text=~/hail/i){
+  quest::say("Zanotix Ixtaz casts an icy glare at you and says 'I, Zanotix Ixtaz, am the greatest weaponsmith in all of Neriak. Have you disturbed me to purchase one of my superior products or have you come to learn the art of crafting [indigo sabres]? '");
+  }
+elsif($text=~/what indigo sabres/i){
   quest::say("Indigo sabres are the weapons of choice for true swordsmen. They are constructed of the finest adamantite and can only be crafted in our unique Teir'Dal forge. In order to successfully craft an indigo sabre. you will require a curved blade mold. Neriak nectar. a folded sheet of adamantite. and a mold for the hilt and pommel. If you are a faithful follower of our Lord of Hate then you may be interested in crafting an imbued indigo sabre. You will need to temper the sword with shadow temper instead of Neriak nectar. and will have to forge a sapphire that has been imbued by a cleric into the sword at the time of its creation.");
   }
+}
 
 # Quest for The Visiting Priestess
 # Nothing prevents them from doing the plat hand in more than once not sure if it is a problem
@@ -12,7 +16,6 @@ if($text=~/blue flame/i){
 if($text=~/ritual/i){
   quest::say("The Ritual? Well I was young when it took place. I don't really remember. I've had so much on my mind lately, what with all these new taxes. For instance, I need to come up with an extra 75 pieces of platinum just to cover the forge tax. I don't know what I'm going to do.....*cough*.");
   }
-}
 
 sub EVENT_ITEM {
 # 75pp
@@ -23,7 +26,7 @@ if ($platinum == 75) {
 # 1890 - Vial of Noble's Blood
 elsif (plugin::check_handin(\%itemcount, 1890 => 1)) {
   quest::emote("accepts the vial and looks you over, eyebrow raised. He says, 'Well, a sacrifice to feed my forge. You have done Neriak a great service, Citizen $name. I shall burn it now.' He then holds the vial up the the flames, the blood seems to glow brightly as he stares into it. Then suddenly, with the shout of an arcane phrase, he throws the vial violently into the flame. The force shatters the vial, spilling the blood about the flames. Almost immediately the flames flare up to the ceiling. When they die down a presence is felt within the flames.");
-  quest::spawn(41106, 0, 0, -867, 70, -35);   # 41106 -- vengeful_spirit
+  quest::spawn2(41106, 0, 0, -867, 70, -35);   # 41106 -- vengeful_spirit
   }
   plugin::return_items(\%itemcount);
 }
