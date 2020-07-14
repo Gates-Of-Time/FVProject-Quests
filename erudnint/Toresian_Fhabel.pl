cfb7fc9527bd18614dd42e87f6f6a223deba360c
TurmoilToad <TurmoilToad@gmail.com>
2020-07-14 09:29:35 -0400

    Update Toresian_Fhabel.pl
    Refactor for consistency.

--- erudnint/Toresian_Fhabel.pl
+++ erudnint/Toresian_Fhabel.pl
@@ -2,7 +2,7 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Welcome. my friend! We are the Craftkeepers. We study the circle of magic called enchantment. What circle do you [study]?");
 	}
-	if ($text=~/study/i) {
+	elsif ($text=~/study/i) {
 		quest::say("Then I welcome you into our circle. It is good to see new blood. I have a favor to ask of you. Do you wish to [assist Toresian] or do you not have the time?");
 		#:: Match The Erudin Palace >> Slansin (23080) or The Erudin Palace >> Slansin (23001)
 		if (!$entity_list->IsMobSpawnedByNpcTypeID(23080) || !$entity_list->IsMobSpawnedByNpcTypeID(23001)) {
@@ -10,20 +10,20 @@ sub EVENT_SAY {
 			quest::unique_spawn(quest::ChooseRandom(23080,23001),0,0,575,683,-12.13,78);
 		}
 	}
-	if ($text=~/assist Toresian/i) {
+	elsif ($text=~/assist Toresian/i) {
 		#:: Match if faction is amiable or better
-		if ($faction < 5) {
+		if ($faction <= 4) {
 			quest::say("Ahhhhh $name.  Slansin used to have need of those potions, however he mysteriously disappeared long ago. The only person I know of who still wants them is a cleric of the Church of Marr in Freeport.");			
 			#:: Give a 13983 - Inert Potion
 			quest::summonitem(13983);
 		}
-		#:: Match if faction is indifferent or worse
-		elsif ($faction => 5) {
+		else {
 			quest::say("You are lucky to be standing. Leave here immediately or suffer grave consequences! You are not welcome amongst the Craftkeepers.");
 		}
 	}
 }
 
 sub EVENT_ITEM {
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
