b6276bbad7c506fb303e4bdaa97ed38226255727
TurmoilToad <TurmoilToad@gmail.com>
2020-07-15 09:39:37 -0400

    Update player.pl
    ...typo

--- soldungb/player.pl
+++ soldungb/player.pl
@@ -9,6 +9,7 @@ sub EVENT_TARGET_CHANGE {
 				quest::gmsay("$Target.");
 				#quest::ze(0,"I will not fight you, but I will banish you!");
 				#$client->MovePC(27,-64,262,-93.96,0); # Zone: lavastorm
+			}
 		}
 	}
 }
