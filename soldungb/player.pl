e49270404b1ca74c1e6421b68202a2c699c54f37
TurmoilToad <TurmoilToad@gmail.com>
2020-07-15 09:38:12 -0400

    Update player.pl
    Export validation

--- soldungb/player.pl
+++ soldungb/player.pl
@@ -1,6 +1,14 @@
 sub EVENT_TARGET_CHANGE {
-  if (($client->GetTarget()->GetCleanName() eq "Lord Nagafen") && ($ulevel > 52) && ($status < 80)) {
-    quest::ze(0,"I will not fight you, but I will banish you!");
-    $client->MovePC(27,-64,262,-93.96,0);
-  }
+	if ($ulevel > 52) {
+		quest::gmsay("User Level Match.");
+		if ($status > 80) {
+			quest::gmsay("User Status Match.");
+			if ($client) {
+				quest::gmsay("Client $client Match.");
+				$Target = $client->GetTarget()->GetCleanName();
+				quest::gmsay("$Target.");
+				#quest::ze(0,"I will not fight you, but I will banish you!");
+				#$client->MovePC(27,-64,262,-93.96,0); # Zone: lavastorm
+		}
+	}
 }
