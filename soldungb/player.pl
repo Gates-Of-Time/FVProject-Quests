1193b214beb261cbfa26a854a3abab02e51fc43f
TurmoilToad <TurmoilToad@gmail.com>
2020-07-15 09:50:48 -0400

    Update player.pl

--- soldungb/player.pl
+++ soldungb/player.pl
@@ -1,14 +1,10 @@
 sub EVENT_TARGET_CHANGE {
 	if ($ulevel > 1) {
-		quest::gmsay("User Level Match.");
 		if ($status > 80) {
-			quest::gmsay("User Status Match.");
-			if ($client) {
-				quest::gmsay("Client $client Match.");
-				$Target = $client->GetTarget()->GetCleanName();
-				quest::gmsay("$Target.");
-				#quest::ze(0,"I will not fight you, but I will banish you!");
-				#$client->MovePC(27,-64,262,-93.96,0); # Zone: lavastorm
+			if ($client->GetTarget()->GetCleanName() eq "Lord Nagafen") {
+				quest::ze(0, "I will not fight you, but I will banish you!");
+				#:: Move the client to Lavastorm at the specified coordinates
+				$client->MovePC(27, -64, 262, -93.96, 0);
 			}
 		}
 	}
