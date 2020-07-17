64290e3108eac9cde8f569722998b546a22c25f7
TurmoilToad <TurmoilToad@gmail.com>
2020-07-17 08:56:58 -0400

    Update Gynok_Moltor.pl
    Added missing attack / death messages.

--- befallen/Gynok_Moltor.pl
+++ befallen/Gynok_Moltor.pl
@@ -1,3 +1,10 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Oooohhh... Arghhh... Yooouuu sssshhhalll neeeveer ffffiiinnnd the paaawwww..");
+	}
+}
+
 sub EVENT_WAYPOINT_ARRIVE {
 	#::: When NPC arrives at waypoint 55 or 117
 	if ($wp == 55 || $wp == 117) {
@@ -7,4 +14,8 @@ sub EVENT_WAYPOINT_ARRIVE {
 	if ($wp == 82) {	
 		quest::SetRunning(0);  #:: Walk
 	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("Aaaatt laaasssssstttt! Opolla's cuuurrsssee is lifted!");
 }
