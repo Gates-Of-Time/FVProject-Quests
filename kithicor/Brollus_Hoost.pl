7f542cbe401473d5ae3ed2fe77301d6b5f7be7cf
TurmoilToad <TurmoilToad@gmail.com>
2020-08-11 11:45:41 -0400

    Update Brollus_Hoost.pl
    Added missing dialogue.

--- kithicor/Brollus_Hoost.pl
+++ kithicor/Brollus_Hoost.pl
@@ -5,6 +5,9 @@ sub EVENT_SAY {
 	elsif ($text=~/battle staff/i) {
 		quest::say("I was hired to steal a High Guard battle staff from the High Guard of Erudin. It is a gift for Carson McCabe. I guess he collects weapons.");
 	}
+	elsif ($text=~/tallus holton/i) {
+		quest::say("Tallus Holton was my partner on my last mission. He is actually a wizard, but instead of yearning for knowledge, he yearns for money. He likes to hang around the Karana Bandits. They like him, he can ignite a campfire in a snap.");
+	}
 }
 
 sub EVENT_ITEM {
