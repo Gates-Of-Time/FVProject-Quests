4957f75437e18f3cc86d1d2e2cad9be79cb9c621
FVFippy <44533609+FVFippy@users.noreply.github.com>
2020-09-13 15:09:41 -0400

    Update an_avenging_caitiff.pl
    It is a little unclear if this "who must be slain" test is original or not, but how would you know.  It is a duplicate of the turn in dialog, but that seems to make sense to me at least regarding his statement for either scenario.

--- mistmoore/an_avenging_caitiff.pl
+++ mistmoore/an_avenging_caitiff.pl
@@ -15,6 +15,9 @@ sub EVENT_SAY {
   if ($text=~/electrum-bladed wakizashi/i) {
     quest::say("HA! As if a mortal such as yourself is worthy of such a weapon. Leave me to my mission - my purpose in life - the slaying of those who brought me into this existence, then exiled me from this place.");
   }
+    if ($text=~/Who must be slain?/i) {
+    quest::emote("Mother? Father? May you find peace at last. I shall avenge your suffering! I will grow stronger and set free the souls of the others.");
+  }
   if ($text=~/mother/i) {
     quest::say("My mother was named Issis. She is, to my knowledge, employed as Lord Mistmoores maid.");
   }
