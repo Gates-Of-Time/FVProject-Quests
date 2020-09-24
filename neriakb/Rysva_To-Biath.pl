7ae05213963e5ab3b75956bfef6289bf4415ebbb
TurmoilToad <TurmoilToad@gmail.com>
2020-09-24 13:11:47 -0400

    Update Rysva_To-Biath.pl
    Refactored for consistency, corrected experience reward, corrected faction reward, missing dialogue for Book of Ages.

--- neriakb/Rysva_To-Biath.pl
+++ neriakb/Rysva_To-Biath.pl
@@ -1,31 +1,47 @@
-#Rysva_To-Biath.pl
-#Book of Turmoil
-
 sub EVENT_SAY { 
-	if($text=~/hail/i){
+	if ($text=~/hail/i) {
 		quest::say("Hello.  Let me know if I can get you a drink.");
 	}
 }
+
 sub EVENT_ITEM {
-	if(plugin::check_handin(\%itemcount, 18200 => 1)){#
+	#:: Match a 18200 - Note (TomeOfAges1)
+	if (plugin::takeItems(18200 => 1)) {
 		quest::say("Take this note and be gone with you.");
-		quest::summonitem(18201);#a note (book of Turmoil, Text: TomeOfAges2)
-		quest::faction(370, 10);#Dreadguard Inner
-		quest::faction(334, 10);#Dreadguard Outer
-		quest::faction(236, 10);#Dark Bargainers
+		#:: Give a 18201 - Note (TomeOfAges2)
+		quest::summonitem(18201);
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(236, 10);	#:: + Dark Bargainers
+		quest::faction(334, 1);		#:: + Dreadguard Outer
+		quest::faction(370, 1);		#:: + Dreadguard Inner
+		#:: Grant a small amount of experience, based on level
+		$client->AddLevelBasedExp(10, 10);
 	}
-	elsif(plugin::check_handin(\%itemcount, 13846 => 1, 18201 =>1)){#Dark Elf Decapitated Head & Note
+	#:: Match a 13846 - Dark Elf Decapitated Head and a 18201 - Note (TomeOfAges2)
+	elsif (plugin::takeItems(13846 => 1, 18201 =>1)) {
 		quest::say("Take this book and be gone with you.");
-		quest::summonitem(18202);#Book of Turmoil
-		quest::faction(370, 10);#Dreadguard Inner
-		quest::faction(334, 10);#Dreadguard Outer
-		quest::faction(236, 10);#Dark Bargainers
-		quest::exp(72900);#10% of level 10 xp
-		#The mob you kill for this quest is trivial at even level 10, or should be.  Giving a percentage of level 40
-		#xp seems too much.
+		#:: Give a 18202 - Book of Turmoil
+		quest::summonitem(18202);
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(236, 10);	#:: + Dark Bargainers
+		quest::faction(334, 1);		#:: + Dreadguard Outer
+		quest::faction(370, 1);		#:: + Dreadguard Inner
+		#:: Grant a small amount of experience, based on level
+		$client->AddLevelBasedExp(10, 10);
 	}
-    elsif(plugin::check_handin(\%itemcount, 19071 => 1, 19070 => 1) && $platinum >= 1000) {
-        quest::summonitem(18302);
-    }
-  	plugin::return_items(\%itemcount);
-}#END of FILE Zone:neriakb  ID:41042 -- Rysva_To`Biath 
+	#:: Match 1000pp, a 19071 - Torn, Burnt Book, and a 19070 - Torn, Frost-covered Book
+	elsif (plugin::takeItemsCoin(0, 0, 0, 1000, 19071 => 1, 19070 => 1)) {
+		#:: Missing dialogue
+		#:: Give a 18302 - Book of Scale
+		quest::summonitem(18302);
+		#:: Ding
+		quest::ding();
+		#:: Missing faction reward
+	}
+  	#:: Return unused items
+	plugin::returnUnusedItems();
+}
