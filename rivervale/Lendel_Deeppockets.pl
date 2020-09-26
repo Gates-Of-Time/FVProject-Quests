76fed5371f3a59cb7c7d0393c0f47302a53f04e9
TurmoilToad <TurmoilToad@gmail.com>
2020-09-25 20:22:31 -0400

    Update Lendel_Deeppockets.pl
    Refactor for consistency, add data bucket to prevent spam.

--- rivervale/Lendel_Deeppockets.pl
+++ rivervale/Lendel_Deeppockets.pl
@@ -1,13 +1,11 @@
 sub EVENT_SPAWN {
-	#:: Set a proximity 100 unit distance
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Set a proximity 100 units across, 100 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
 	#:: Check for 18732 - A Tattered Note
-	if(plugin::check_hasitem($client, 18732)) {
+	if (plugin::check_hasitem($client, 18732)) {
 		$client->Message(15,"As you get your bearings, you see that you are inside the Fool's Gold, the best place in all of Rivervale to have a drink and relax. Before you can reflect on relaxing however, a serious looking Halfling looks your way. 'Hey you! No loitering. What's that you say! You wish to become a Rogue? Well, you are in the right place. I am Lendel. Read the note in your inventory and when you are ready to start your training, hand me your note.'");
 	}
 }
@@ -29,8 +27,12 @@ sub EVENT_SAY {
 }
 
 sub EVENT_SIGNAL { 
-	#::Receive Signal 1 from Gaffin Deeppockets of Rivervale
+	#:: Match a signal '1' from rivervale/Gaffin_Deeppockets.pl
 	if ($signal == 1) {
+		#:: Key up a data bucket
+		$key = gaffin_timer_lenny;
+		#:: Set the key to a value of '1' for 600 seconds (10 min)
+		quest::set_data($key, 1, 600);
 		quest::say("Find somebody to go see what he wants. I don't like the idea of making Hanns mad at us, but I also don't like the idea of looking over my shoulder for Stanos. He is not somebody I would care to dance with, and I DO owe him.");
 	}
 }
@@ -38,20 +40,20 @@ sub EVENT_SIGNAL {
 
 sub EVENT_ITEM {
 	#:: Turn in for 18732 - Tattered Note
-	if (plugin::takeItems(18732 => 1 )) {
+	if (plugin::takeItems(18732 => 1)) {
 		quest::say("HA! I asked that fool Denry to send me a professional, and this is what I get?!? Oh diddlepicks! That crotchety old coot never liked me anyway. And after all I've done for him! Hrrmf! Ah well, let's get you started and see what ya got, huh, kid? Here, wear this. Maybe I'll have Toelia break you in, huh? Yeah, that'll work! Go find her, and she'll put you to work. Just remember, we all earn our keep around here, or else it's back to hay farm for you! Oh yeah, tell her you're the new dishwasher so she knows you are on the level. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
 		#:: Give item 13539 - Old Brown Vest*
 		quest::summonitem(13539);
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of experience
-		quest::exp(100);
 		#:: Set factions
 		quest::faction(241, 5);		#:: + Deeppockets
 		quest::faction(223, 5);		#:: + Circle of Unseen Hands
 		quest::faction(292, -5);	#:: - Merchants of Rivervale
 		quest::faction(329, 5);		#:: + Carson McCabe
 		quest::faction(336, 5);		#:: + Coalition of Tradefolk Underground
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
