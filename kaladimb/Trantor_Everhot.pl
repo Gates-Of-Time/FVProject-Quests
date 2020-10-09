51e37d5be2245c3e4360b69397f9fc3fe834431c
TurmoilToad <TurmoilToad@gmail.com>
2020-10-09 09:07:54 -0400

    Update Trantor_Everhot.pl
    Refactor for consistency, adjust faction reward, added missing dialogue

--- kaladimb/Trantor_Everhot.pl
+++ kaladimb/Trantor_Everhot.pl
@@ -10,11 +10,14 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Yes, yes!! I am a very busy man. I've got blueprints to work on,  [dwarven wire] to perfect and formulae to calculate. In between those tasks, I have to find time to head down to Irontoe's and down a few Tumpy Tonics.");
 	}
-	if ($text=~/dwarven wire/i) {
+	elsif ($text=~/dwarven wire/i) {
 		quest::say("Oh!! Interested in my dwarven wire, are you? Many are. I have no problem letting you use a spool, but I have to warn you that I have not perfected the strength of it yet. I will make you a trade. A couple of [Tumpy Tonics] for a spool.");
 	}
-	if ($text=~/tumpy tonics/i) {
+	elsif ($text=~/tumpy tonics/i) {
 		quest::say("Aaah!! I love Tumpy Tonics. They were created by Tumpy Irontoe. He was once a member of the famed Irontoe Brigade. Since they disbanded and left for parts unknown he decided to stay in Kaladim and open Irontoe's, the finest well in Faydwer.");
+	}
+	elsif ($text=~/irontoe/i) {
+		quest::say("If you're asking about the Brigade, who knows where they are?! If you're looking for Irontoe Tavern, it's here in Kaladim, near the arena.");
 	}
 }
 
@@ -26,14 +29,14 @@ sub EVENT_ITEM {
 		quest::summonitem(12111);
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(312, 1);			#:: + Storm Guard
+		quest::faction(274, 1);			#:: + Kazon Stormhammer
+		quest::faction(293, 1);			#:: + Miners Guild 249
+		quest::faction(290, 1);			#:: + Merchants of Kaladim
+		quest::faction(232, -1);		#:: - Craknek Warriors
 		#:: Grant a moderate amount of experience
 		quest::exp(1000);
-		#:: Set factions
-		quest::faction(312,5);		#:: + Storm Guard
-		quest::faction(274,5);		#:: + Kazon Stormhammer
-		quest::faction(293,5);		#:: + Miners Guild 249
-		quest::faction(290,5);		#:: + Merchants of Kaladim
-		quest::faction(232,-5);		#:: - Craknek Warriors
 		#:: Reset item count
 		$ItemCount = 0;
 	}
@@ -49,14 +52,14 @@ sub EVENT_ITEM {
 			quest::summonitem(12111);
 			#:: Ding!
 			quest::ding();
+			#:: Set factions
+			quest::faction(312, 1);		#:: + Storm Guard
+			quest::faction(274, 1);		#:: + Kazon Stormhammer
+			quest::faction(293, 1);		#:: + Miners Guild 249
+			quest::faction(290, 1);		#:: + Merchants of Kaladim
+			quest::faction(232, -1);	#:: - Craknek Warriors
 			#:: Grant a moderate amount of experience
 			quest::exp(1000);
-			#:: Set factions
-			quest::faction(312,5);		#:: + Storm Guard
-			quest::faction(274,5);		#:: + Kazon Stormhammer
-			quest::faction(293,5);		#:: + Miners Guild 249
-			quest::faction(290,5);		#:: + Merchants of Kaladim
-			quest::faction(232,-5);		#:: - Craknek Warriors
 			#:: Reset item count
 			$ItemCount = 0;
 		}
