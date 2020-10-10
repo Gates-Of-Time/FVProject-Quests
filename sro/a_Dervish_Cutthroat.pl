12dd3e9d17c57c6482a706f670412215c2fc510c
TurmoilToad <TurmoilToad@gmail.com>
2020-10-10 08:42:21 -0400

    Update a_Dervish_Cutthroat.pl

--- sro/a_Dervish_Cutthroat.pl
+++ sro/a_Dervish_Cutthroat.pl
@@ -1,22 +1,23 @@
-#:: Create a scalar variable to store NPC race
-my $NPCRace = $npc->GetRace();
-
 sub EVENT_COMBAT {
 	#:: Combat state 1 is entered combat
 	if ($combat_state == 1) {
+		#:: Create a scalar variable to store NPC race
+		my $npcrace = $npc->GetRace();
 		#:: Match race 54 - Orc
-		if ($NPCRace == 54) {
+		if ($npcrace == 54) {
 			quest::say("Orc stomp, orc kill - orc weapons, your blood will spill.");
 		}
 		else {
-			quest::say("Time to die $name!");
+			quest::say("Time to die, $name!");
 		}
 	}
 }
 
-sub EVENT_DEATH_COMPLETE {
+sub EVENT_DEATH {
+	#:: Create a scalar variable to store NPC race
+	my $npcrace = $npc->GetRace();
 	#:: Match race 54 - Orc
-	if ($NPCRace == 54) {
+	if ($npcrace == 54) {
 		quest::say("My comrades will avenge my death.");
 	}
 }
