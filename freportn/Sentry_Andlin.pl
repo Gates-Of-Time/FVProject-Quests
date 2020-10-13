6df4e2e1a0f866d61eb9c876190d4e6cd0e213f7
TurmoilToad <TurmoilToad@gmail.com>
2020-10-13 10:04:45 -0400

    Update Sentry_Andlin.pl
    Refactor for consistency

--- freportn/Sentry_Andlin.pl
+++ freportn/Sentry_Andlin.pl
@@ -8,8 +8,6 @@ sub EVENT_ITEM {
 	#:: Turn in for 12127- Full Potion of Marr
 	if (plugin::takeItems(12127 => 1 )) {
 		quest::say("My thanks to you. I feel much strength. You may take the bottle to the next sentry.");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Give item 12128 - Part of Potion of Marr
 		quest::summonitem(12128);
 		#:: Ding!
@@ -17,7 +15,9 @@ sub EVENT_ITEM {
 		#:: Set factions
 		quest::faction(362, 2); 		#:: + Priests of Marr
 		quest::faction(330, -1); 		#:: - Freeport Militia
-		quest::faction(281, 1); 		#:: + Knights of Truth	
+		quest::faction(281, 1); 		#:: + Knights of Truth
+		#:: Give a small amount of xp
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
