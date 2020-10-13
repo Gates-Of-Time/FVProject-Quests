772879fcd096cf4008eaf82faedebebe0e72ea7b
TurmoilToad <TurmoilToad@gmail.com>
2020-10-13 10:06:01 -0400

    Update Sentry_Gallius.pl
    Refactor for consistency

--- freportn/Sentry_Gallius.pl
+++ freportn/Sentry_Gallius.pl
@@ -8,8 +8,6 @@ sub EVENT_ITEM {
 	#:: Turn in for 12129 - Part of Potion of Marr
 	if (plugin::takeItems(12129 => 1 )) {
 		quest::say("Excellent!! I feel quite refreshed with but a sip. You may take this to the next sentry.");
-		#:: Give a small amount of xp
-		quest::exp(100);
 		#:: Give item 12130 - Part of Potion of Marr
 		quest::summonitem(12130);
 		#:: Ding!
@@ -17,7 +15,9 @@ sub EVENT_ITEM {
 		#:: Set factions
 		quest::faction(362, 2); 		#:: + Priests of Marr
 		quest::faction(330, -1);	 	#:: - Freeport Militia
-		quest::faction(281, 1); 		#:: + Knights of Truth	
+		quest::faction(281, 1); 		#:: + Knights of Truth
+		#:: Give a small amount of xp
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
