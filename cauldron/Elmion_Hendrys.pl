5bf85662054a74d0a1a6f246753e6324a54b5a47
TurmoilToad <TurmoilToad@gmail.com>
2020-10-14 15:58:23 -0400

    Update Elmion_Hendrys.pl
    Added combat and death messages.

--- cauldron/Elmion_Hendrys.pl
+++ cauldron/Elmion_Hendrys.pl
@@ -27,3 +27,14 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("You have met your match!!");
+	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You have earned the vengeance of Faydark's Champions.");
+}
