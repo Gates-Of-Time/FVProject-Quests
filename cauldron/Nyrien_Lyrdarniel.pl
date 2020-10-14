68567ef85d165c97958d214d06b85897e14ef47e
TurmoilToad <TurmoilToad@gmail.com>
2020-10-14 16:07:53 -0400

    Update Nyrien_Lyrdarniel.pl
    Added attack and death messages.

--- cauldron/Nyrien_Lyrdarniel.pl
+++ cauldron/Nyrien_Lyrdarniel.pl
@@ -29,3 +29,14 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Fabulous Four, attack!!");
+	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My comrades will avenge my death.");
+}
