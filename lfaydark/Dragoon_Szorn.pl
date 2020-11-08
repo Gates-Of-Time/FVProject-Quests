7ae6ae0ad231de6bad29272120a4b789d218d057
TurmoilToad <TurmoilToad@gmail.com>
2020-11-07 20:19:55 -0500

    Update Dragoon_Szorn.pl
    Refactor for consistency, adjust dialogue to match Alla / Vaslin

--- lfaydark/Dragoon_Szorn.pl
+++ lfaydark/Dragoon_Szorn.pl
@@ -1,33 +1,38 @@
-#Dragoon_Szorn.pl
-#Part of Innoruuk Regent
-#Some of this was done already (all the important bits, really).  
-#Cleaned up what was there and added a response to quest text -Kilelen
-
-
 sub EVENT_SAY{
-	if($text=~/missing shipment/i) {
-		quest::say("Seek out Ennixy and tell her [hate be my guide]. She typically works from her homeland in the steamfont mountains.");
+	if ($text=~/missing shipment/i) {
+		quest::say("Our sources in these lands tell us that the previous courier was waylaid by a member of the Fayguard and the crate of valuables taken back to Felwithe. Our source has been working on a plan to recover the crate but we have not received word on her progress in quite some time. Seek Ennixy and tell her 'hate be my guide.' She typically works from her homeland in the Steamfont Mountains.");
 	}
 }
-sub EVENT_ITEM { 
-	if(plugin::check_handin(\%itemcount, 19030 => 1)){#Crate of Teir`Dal Provisions
-		quest::say("Finally!! We have been in dire need of these supplies for quite some time, $name!  Here is the receipt for the shipment. The last shipment never made it here and there is an item of great importance stored in the lost crate. Before you return to Neriak, your services are needed to retrieve our [missing shipment]. I command your obedience as is my right as a dragoon of Naythox Thex's personal regime.");
-		quest::summonitem(19029);#Receipt for Provisions Crate
+
+sub EVENT_ITEM {
+	#:: Match a 19030 - Crate of Teir`Dal Provisions
+	if (plugin::takeItems(19030 => 1)){
+		quest::say("Finally!! We have been in dire need of these supplies for quite some time, Kalaelil! Here is the receipt for the shipment. The last shipment never made it here and there is an item of great importance stored in the lost crate. Before you return to Neriak, your services are needed to retrieve our [missing shipment]. I command your obedience as is my right as a dragoon of Naythox Thex's personal regime.");
+		#:: Give a 19029 - Receipt for Provisions Crate
+		quest::summonitem(19029);
 		#:: Ding!
 		quest::ding();
-		quest::givecash(0,0,0,1);
+		#:: Create a hash for storing cash - 900 to 1100cp
+		my %cash = plugin::RandomCash(900, 1100);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Grant a small amount of experience
 		quest::exp(500);
 	}
-	if(plugin::check_handin(\%itemcount, 19028 => 1)){
+	#:: Match a 19028 - Missing Teir`Dal Crate
+	elsif (plugin::takeItems(19028 => 1)){
 		quest::say("What is this!! The items of importance have been removed from the crate!! This is most unfortunate. However, you have done well in your service to the Teir'Dal Empire. Take this voucher of services rendered under the command of Naythox Thex back to Ithvol along with the head of that slain Fier'Dal, the receipt for the provisions, and your disciple symbol of Innoruuk.");
-		quest::summonitem(12499);#Voucher of Service to Naythox
+		#:: Give a 12499 - Voucher of Service to Naythox
+		quest::summonitem(12499);
 		#:: Ding!
 		quest::ding();
-		quest::givecash(0,0,0,4);
-		#:: Grant a small amount of experience
+		#:: Create a hash for storing cash - 3900 to 4100cp
+		my %cash = plugin::RandomCash(3900, 4100);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		#:: Grant a moderate amount of experience
 		quest::exp(1500);
 	}
-    plugin::return_items(\%itemcount);
-}#END of FILE Zone:lfaydark  ID:57063 -- Dragoon_Szorn 
-
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
