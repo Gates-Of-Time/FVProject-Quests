a41f97f9b32fc448fd227dec47a187fe32d91819
TurmoilToad <TurmoilToad@gmail.com>
2020-11-11 21:48:45 -0500

    Update Guard_Mezzt.pl
    Trigger signal for Guard_Cyrillian.pl

--- qeynos/Guard_Mezzt.pl
+++ qeynos/Guard_Mezzt.pl
@@ -1,6 +1,8 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Haawwwl. Whasnsuud grurffnunnnsuuuss.");
+		#:: Send a signal '2' to South Qeynos >> Guard_Cyrillian (1006) with no delay
+		quest::signalwith(1006, 2, 0);
 	}
 }
 
