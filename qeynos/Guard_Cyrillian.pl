f0f0ffd9ee5db20b1e9dfeb42db77910c42252bf
TurmoilToad <TurmoilToad@gmail.com>
2020-11-11 21:47:10 -0500

    Update Guard_Cyrillian.pl
    Missing dialogue.

--- qeynos/Guard_Cyrillian.pl
+++ qeynos/Guard_Cyrillian.pl
@@ -3,6 +3,10 @@ sub EVENT_SIGNAL {
 		#:: Signal sent from qeynos/Lieutenant_Arathur.pl
 		quest::say("Yes, sir!");
 	}
+	elsif ($signal == 2) {
+		#:: Signal sent from qeynos/Guard_Mezzt.pl
+		quest::say("Hey! Quit pestering Mezzt! Can't you see he has no tongue. And before you ask, it is none of your business what happened to him. Bug off.");
+	}
 }
 
 sub EVENT_ITEM {
