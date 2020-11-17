e819a84114efc2d70125cbb4774f8d51da9a5094
TurmoilToad <TurmoilToad@gmail.com>
2020-11-17 00:17:46 -0500

    Update Lord_Darish.pl

--- crushbone/Lord_Darish.pl
+++ crushbone/Lord_Darish.pl
@@ -1,15 +1,10 @@
 sub EVENT_COMBAT {
 	#:: Match combat state 1 - entered combat
 	if ($combat_state == 1) {
-		$random = quest::ChooseRandom(1, 2, 3);
-		if ($random == 1) {
-			quest::say("Death!! Death to all who oppose the Crushbone orcs!!");
-		}
-		elsif ($random == 2) {
-			quest::say("Hail, Emperor Crush!!");
-		}
-		else {
-			quest::say("Fall before the might of Clan Crushbone!!");
-		}
+		quest::say("Centurions!! Legionnaires!! Come join the fight!");
 	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You shall have all the Crushbone orc legions on your tail for my death!");
 }
