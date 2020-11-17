4806c73e15b7ccff78c67ed5fabbc765de336af1
TurmoilToad <TurmoilToad@gmail.com>
2020-11-17 00:46:14 -0500

    Update an_elven_priest.pl
    Add missing dialogue, refactor for consistency.

--- crushbone/an_elven_priest.pl
+++ crushbone/an_elven_priest.pl
@@ -1,5 +1,3 @@
-#:: Screaming Mace Quest
-
 sub EVENT_SAY {
 	if ($text=~/king is dead/i) {
 		quest::say("Dead you say? My goodness! Did you manage to slay the warlord too? If you provide me with proof of his death, I can enchant the mace the dwarf gave you as payment. But, I will require the proof first!");
@@ -7,26 +5,38 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 2301 -  Bracers of Battle
-	if (plugin::check_handin(\%itemcount, 2301 => 1)) {
+	#:: Match a 2301 - Bracers of Battle
+	if (plugin::takeItems(2301 => 1)) {
 		quest::say("Erollisi be praised! You slew the orc lord! Well, here is a token of my appreciation! Should you wish to have that mace enchanted, just hand me the mace and my token and I shall uphold my end of the bargain!");
-		#:: Give item 1900 - Prayer Cloth of Tunare
+		#:: Give a 1900 - Prayer Cloth of Tunare
 		quest::summonitem(1900);
-		#:: Give a small amount of xp
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
 		quest::exp(5000);
-		#:: Ding!
-		quest::ding();
 	}
-	#:: Give item 1900 - Prayer Cloth of Tunare and Dwarven Mace
-	if (plugin::check_handin(\%itemcount, 1900 => 1, 6315 => 1)) {
+	#:: Match a 1900 - Prayer Cloth of Tunare, and a 6315 - Dwarven Mace
+	elsif (plugin::takeItems(1900 => 1, 6315 => 1)) {
 		quest::say("Erollisi be praised! You slew the orc lord! Well, here is a token of my appreciation! Should you wish to have that mace enchanted, just hand me the mace and my token and I shall uphold my end of the bargain!");
-		#:: Give item 6305 - Screaming Mace
+		#:: Give a 6305 - Screaming Mace
 		quest::summonitem(6305);
-		#:: Give a small amount of xp
-		quest::exp(10000);
 		#:: Ding!
 		quest::ding();
+		#:: Grant a large amount of experience
+		quest::exp(10000);
+	}
+	#:: Match a 1900 - Prayer Cloth of Tunare
+	elsif (plugin::takeItems(1900 => 1)) {
+		quest::say("Have you the other item I require?");
+		#:: Return a 1900 - Prayer Cloth of Tunare
+		quest::summonitem(1900);
+	}
+	#:: Match a 6315 - Dwarven Mace
+	elsif (plugin::takeItems(6315 => 1)) {
+		quest::say("Have you the other item I require?");
+		#:: Return a 6315 - Dwarven Mace
+		quest::summonitem(6315);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
