1839cfa0d4b524a888611ef61e97f6dfdb6fc8e8
TurmoilToad <TurmoilToad@gmail.com>
2020-11-17 23:06:44 -0500

    Update Vorshar_the_Despised.pl

--- neriakb/Vorshar_the_Despised.pl
+++ neriakb/Vorshar_the_Despised.pl
@@ -8,28 +8,28 @@ sub EVENT_SAY {
 	elsif ($text=~/why did you return/i) {
 		quest::say("It is the Dark Father's will that I have returned to this realm. I do not question his judgement. nor shall I ever. His will is mine. and I do not question myself. The Dark Father wishes to expand his followers within the Realm of Heroes. and thus. I have been sent as more of a judge and jury for those who would consider themselves [worthy of Innoruuk's recognition] and divine favor.");
 	}
-	elsif ($text=~/who king/i) {
+	elsif ($text=~/who.*king/i) {
 		quest::say("The mighty empire of the Teir'Dal is ruled by King Naythox Thex.  All hail Thex!");
 	}
-	elsif ($text=~/who the dead/i) {
+	elsif ($text=~/who.*the dead/i) {
 		quest::say("The Dead are the shadowknights and necromancers of Neriak.  They were formed by Queen Cristanos herself.  Even among the Teir'Dal they are feared and they keep to themselves within the Lodge of the Dead in the Third Gate.  I have heard they take orders only from the queen.");
 	}
-	elsif ($text=~/where lodge of the dead/i) {
+	elsif ($text=~/where.*lodge of the dead/i) {
 		quest::say("The Dead are the shadowknights and necromancers of Neriak.  They were formed by Queen Cristanos herself.  Even among the Teir'Dal they are feared and they keep to themselves within the Lodge of the Dead in the Third Gate.  I have heard they take orders only from the queen.");
 	}
-	elsif ($text=~/who queen cristanos/i) {
+	elsif ($text=~/who.*queen cristanos/i) {
 		quest::say("Queen Cristanos is the queen of Neriak, of course!  She was the one who formed the necromancers and shadowknights into the guild called the Dead.");
 	}
-	elsif ($text=~/what indigo brotherhood/i) {
+	elsif ($text=~/what.*indigo brotherhood/i) {
 		quest::say("In the Commons Quarter can be found the Indigo Brotherhood, Teir'Dal warriors.  They are the armies of King Naythox Thex and serve as the trainers of the Dreadguard and dragoons.  Why the masters chose to build the Cauldron of Hate in that area and not the Third Gate is an amazement to me.");
 	}
-	elsif ($text=~/what cauldron of hate/i) {
+	elsif ($text=~/what.*cauldron of hate/i) {
 		quest::say("In the Commons Quarter can be found the Indigo Brotherhood, Teir'Dal warriors.  They are the armies of King Naythox Thex and serve as the trainers of the Dreadguard and dragoons.  Why the masters chose to build the Cauldron of Hate in that area and not the Third Gate is an amazement to me.");
 	}
-	elsif ($text=~/what commons quarter/i) {
+	elsif ($text=~/what.*commons quarter/i) {
 		quest::say("You are in the Commons Quarter.  This quarter is designated for all common Teir'Dal.  You can find an abundance of merchants as well as the arena and warriors guild here.");
 	}
-	elsif ($text=~/what warriors guild/i) {
+	elsif ($text=~/what.*warriors guild/i) {
 		quest::say("Within this area you will find the Cauldron of Hate which is home to the Teir'Dal warriors.");
 	}
 }
