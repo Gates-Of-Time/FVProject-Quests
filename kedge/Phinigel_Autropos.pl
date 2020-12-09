9017f12cf99de1b92c37bf925ae024486dc62341
TurmoilToad <TurmoilToad@gmail.com>
2020-12-08 21:29:30 -0500

    Update Phinigel_Autropos.pl

--- kedge/Phinigel_Autropos.pl
+++ kedge/Phinigel_Autropos.pl
@@ -2,7 +2,7 @@ sub EVENT_DEATH_COMPLETE {
 	$key = "serverprogress";
 	if (!quest::get_data($key)) {
 		quest::set_data($key, 1);
-		$npc->CameraEffect(10000, 10, 0, 1);
+		$npc->CameraEffect(5000, 5, 0, 1);
 		quest::we(13, "Cazic Thule shouts, 'Beware all infidels who dare to taint my plane!'");
 		$connect = plugin::LoadMysql();
 		my $query = "UPDATE `zone` SET `min_status` = '0' WHERE `zoneidnumber` = '72';";
@@ -11,7 +11,7 @@ sub EVENT_DEATH_COMPLETE {
 	}
 	elsif (quest::get_data($key) eq "1") {
 		quest::set_data($key, quest::get_data($key) + 1);
-		$npc->CameraEffect(10000, 10, 0, 1);
+		$npc->CameraEffect(5000, 5, 0, 1);
 		quest::we(13, "Innoruuk shouts, 'I LIVE! Prepare yourselves, mortals. You will bow before the terrible might of the Prince of Hate!'");
 		$connect = plugin::LoadMysql();
 		my $query = "UPDATE `zone` SET `min_status` = '0' WHERE `zoneidnumber` = '76';";
@@ -20,7 +20,7 @@ sub EVENT_DEATH_COMPLETE {
 	}
 	elsif (quest::get_data($key) eq "2") {
 		quest::set_data($key, quest::get_data($key) + 1);
-		$npc->CameraEffect(10000, 10, 0, 1);
+		$npc->CameraEffect(5000, 5, 0, 1);
 		quest::we(13, "Sirran the Lunatic shouts, 'Give me your trinkets, or give me death!'");
 		$connect = plugin::LoadMysql();
 		my $query = "UPDATE `zone` SET `min_status` = '0' WHERE `zoneidnumber` = '71';";
