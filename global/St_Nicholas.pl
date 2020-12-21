6e0f184d46f21990b17a24f49866e39b797a7d92
TurmoilToad <TurmoilToad@gmail.com>
2020-12-21 18:22:48 -0500

    Update St_Nicholas.pl

--- global/St_Nicholas.pl
+++ global/St_Nicholas.pl
@@ -13,25 +13,35 @@ sub EVENT_SAY {
 
 sub EVENT_TIMER {
 	if ($timer eq "client_check") {
+		quest::gmsay("client check timer.");
 		my @ClientList = $entity_list->GetClientList();
 		if (scalar @ClientList == 0) {
+			quest::gmsay("client check false.");
 			quest::stoptimer("client_check");
 			quest::stoptimer("deliver_gifts");
 			quest::stoptimer("roam");
 			quest::depop_withtimer();
 		}
 		else {
+			quest::gmsay("client check true.");
 			quest::settimer("deliver_gifts", 90);
+			quest::gmsay("deliver gifts timer set.");
 		}
 	}
 	elsif ($timer eq "deliver_gifts") {
-		if ($Deliveries < 6) {
+		quest::gmsay("deliver gifts timer triggered.");
+		quest::gmsay("$Deliveries");
+		if ($Deliveries <= 6) {
 			quest::stoptimer("deliver_gifts");
+			quest::gmsay("stop timer deliver gifts");
 			quest::settimer("roam", 120);
+			quest::gmsay("set timer roam.");
 			$Deliveries++;
+			quest::gmsay("Deliveries incremented.  Value is now $Deliveries.");
 			Give_Gifts();
 		}
 		else {
+			quest::gmsay("Deliveries is now $Deliveries, despawning.");
 			quest::ze("St. Nicholas says, 'Okay kids, I have deliveries to make all over Norrath.  Up the chimney I go!'");
 			quest::stoptimer("client_check");
 			quest::stoptimer("deliver_gifts");
@@ -42,13 +52,18 @@ sub EVENT_TIMER {
 		}
 	}
 	elsif ($timer eq "roam") {
+		quest::gmsay("Timer roam triggered.");
 		quest::stoptimer("roam");
+		quest::gmsay("Timer roam stopped.");
 		quest::settimer("deliver_gifts", 90);
+		quest::gmsay("Timer deliver gifts started.");
 		plugin::RandomRoam(250, 250);
+		quest::gmsay("Roaming begin.");
 	}
 }
 
 sub Give_Gifts {
+	quest::gmsay("Give Gifts subroutine triggered.");
 	quest::ze(0, "St. Nicholas says, 'Ho ho ho!  Merry Christmas!'");
 	quest::doanim(43);
 	$npc->CastSpell(15911, $caster_id);
