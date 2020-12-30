a99aa94933f34d453aaf1de744aabaf84b8cf4e3
TurmoilToad <TurmoilToad@gmail.com>
2020-12-30 18:21:55 -0500

    Create The_stench.pl

---
+++ hateplane/The_stench.pl
@@ -0,0 +1,9 @@
+sub EVENT_SPAWN {
+	#:: Create a proximity, 50 units across, 50 units tall, without proximity say
+	quest::set_proximity($x - 25, $x + 25, $y - 25, $y + 25, $z - 25, $z + 25, 0);
+}
+
+sub EVENT_ENTER {
+	quest::emote("of decay is overpowering");
+	quest::depop_withtimer();
+}
