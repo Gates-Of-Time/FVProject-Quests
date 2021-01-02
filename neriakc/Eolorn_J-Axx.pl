9475a54a36294a5ba727972ac3205267fbeec250
TurmoilToad <TurmoilToad@gmail.com>
2021-01-02 17:28:48 -0500

    Update Eolorn_J-Axx.pl
    Some semicolons

--- neriakc/Eolorn_J-Axx.pl
+++ neriakc/Eolorn_J-Axx.pl
@@ -67,11 +67,11 @@ sub EVENT_ITEM {
 		quest::faction(263, -1);		#:: - Guardians of the Vale
 		quest::faction(329, -2);		#:: - Carson McCabe
 		#:: Grant a small amount of experience
-		quest::exp(100)
+		quest::exp(100);
 		#:: Create a hash for storing cash - 5 to 15cp
 		my %cash = plugin::RandomCash(5, 15);
 		#:: Grant a random cash reward
-		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});;
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 	}
 	#:: plugin::try_tome_handins(\%itemcount, $class, 'Rogue');
 	#:: Return unused items
