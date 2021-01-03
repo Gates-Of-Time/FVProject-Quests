691a8ca6077548d78737379c2d3b2a9b58992049
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 16:41:52 -0500

    Create 622.pl

---
+++ airplane/622.pl
@@ -0,0 +1,11 @@
+#:: NAME: skel_pet_29_
+#:: ID: 622
+#:: TYPE: Pet (Skeleton)
+
+sub EVENT_SPAWN {
+	#:: Race 69 - Will-O-Wisp
+	quest::npcrace(69);
+	#:: Pink Will-O-Wisp
+	quest::npctexture(1);
+	quest::npcsize(4);
+}
