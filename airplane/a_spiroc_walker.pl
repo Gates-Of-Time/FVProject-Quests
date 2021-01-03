a7057ccfa09d5b7c9ad1c2936e63a04518dce2ff
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 10:22:10 -0500

    Create a_spiroc_walker.pl

---
+++ airplane/a_spiroc_walker.pl
@@ -0,0 +1,7 @@
+sub EVENT_DEATH_COMPLETE_COMPLETE {
+	#:: Match a The Plane of Sky >> a_spiroc_vanquisher (71009) on the entity list
+	if ($entity_list->GetMobByNpcTypeID(71009)) {
+		#:: Update spawn point to respawn in 1000ms (1 sec)
+		quest::updatespawntimer(157334, 1000);
+	}
+}
