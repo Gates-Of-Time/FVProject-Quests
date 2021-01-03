f5ab620417f4db74f3431141e89a69a8ac9739bf
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 16:27:19 -0500

    Update Brother_Trintle.pl

--- qey2hh1/Brother_Trintle.pl
+++ qey2hh1/Brother_Trintle.pl
@@ -9,7 +9,7 @@ sub EVENT_WAYPOINT_ARRIVE {
 	#:: Retrieve the entity id for The Western Plains of Karana >> Brother_Estle (12043)
 	my $npc_ent = $entity_list->GetMobByNpcTypeID(12043);
 	#:: Match if distance is less than 100 units
-	if ($npc_ent->CalculateDistance($x, $y, $z) < 100) {
+	if ($npc_ent->CalculateDistance($npc->GetX(), $npc->GetY(), $npc->GetZ()) < 100) {
 		#:: Get him.
 		$npc->Attack($npc_ent);
 	}
