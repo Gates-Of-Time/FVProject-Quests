245be1c8e8d260a1dd37e79f4ce01939ceb82ff2
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 16:44:21 -0500

    Create 627.pl

---
+++ airplane/627.pl
@@ -0,0 +1,11 @@
+#:: NAME: skel_pet_44_
+#:: ID: 627
+#:: TYPE: Pet (Skeleton)
+
+sub EVENT_SPAWN {
+	#:: Race 69 - Will-O-Wisp
+	quest::npcrace(69);
+	#:: Pink Will-O-Wisp
+	quest::npctexture(1);
+	quest::npcsize(4);
+}
