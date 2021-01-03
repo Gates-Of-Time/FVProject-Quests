22b03f0b3a1a8722091e211258cc233b1d67a93e
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 16:51:48 -0500

    Create 634.pl

---
+++ airplane/634.pl
@@ -0,0 +1,11 @@
+#:: NAME: sk_pet_68_
+#:: ID: 634
+#:: TYPE: Pet (Skeleton)
+
+sub EVENT_SPAWN {
+	#:: Race 69 - Will-O-Wisp
+	quest::npcrace(69);
+	#:: Pink Will-O-Wisp
+	quest::npctexture(1);
+	quest::npcsize(4);
+}
