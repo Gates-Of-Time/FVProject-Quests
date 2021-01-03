918d6368b6e9dc819a2c8b5ee8e63ce3ca57c179
TurmoilToad <TurmoilToad@gmail.com>
2021-01-03 16:50:28 -0500

    Create 633.pl

---
+++ airplane/633.pl
@@ -0,0 +1,11 @@
+#:: NAME: skel_pet_70_
+#:: ID: 633
+#:: TYPE: Pet (Spectre)
+
+sub EVENT_SPAWN {
+	#:: Race 69 - Will-O-Wisp
+	quest::npcrace(69);
+	#:: Pink Will-O-Wisp
+	quest::npctexture(1);
+	quest::npcsize(4);
+}
