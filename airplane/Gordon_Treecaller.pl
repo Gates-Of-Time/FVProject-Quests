e28069acdd561b126bee8debafff6c5ef05c9646
TurmoilToad <TurmoilToad@gmail.com>
2021-01-07 16:02:47 -0500

    Update Gordon_Treecaller.pl
    Fix timer

--- airplane/Gordon_Treecaller.pl
+++ airplane/Gordon_Treecaller.pl
@@ -1,6 +1,6 @@
 sub EVENT_SPAWN {
 	#:: Create a timer 'gordon_depop' that triggers every 300 seconds (5 min)
-	quest::settimer("depop",300);
+	quest::settimer("gordon_depop",300);
 }
 
 sub EVENT_TIMER {
