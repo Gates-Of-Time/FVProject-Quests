801ece22edeea8942648d60b52535b9781f7d5f0
TurmoilToad <TurmoilToad@gmail.com>
2021-01-09 20:29:30 -0500

    Update Lempeck_Hargrin.pl
    Added opportunity to get attacked, added missing aggro and combat messages

--- qey2hh1/Lempeck_Hargrin.pl
+++ qey2hh1/Lempeck_Hargrin.pl
@@ -1,3 +1,14 @@
+sub EVENT_AGGRO {
+	quest::say("Guards! Guards!");
+}
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("It's time for you to take your blasphemy into the next realm.");
+	}
+}
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Gree.. .. tings. How are you? Don't get too close to me. I have come down with something bad.");
@@ -10,6 +21,20 @@ sub EVENT_ITEM {
 		quest::say("Thank you!! I felt the madness nearing my brain, but now I feel much better. Please take this as thanks - it is all I have to donate to Astaed Wemor. Be sure he gets it. I shall thank Rodcet Nife every day for sending help.");
 		#:: Give a 13970 - Rusty Scythe
 		quest::summonitem(13970);
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(341, 1);		#:: + Priests of Life
+		quest::faction(280, 1);		#:: + Knights of Thunder
+		quest::faction(262, 1);		#:: + Guards of Qeynos
+		quest::faction(221, -1);	#:: - Bloodsabers
+		quest::faction(219, 1);		#:: + Antonius Bayle
+		#:: Grant a moderate amount of experience
+		quest::exp(1000);
+	}
+	#:: Match a 13955 - Prime Healer Potion
+	elsif (plugin::takeItems(13955 => 1)) {
+		quest::say("Please let this help.. nnnnghhh!!!.. I fear it is too late.. Aaaarrrgh!!!!.. I.. I will.. KILL YOU!!!!!");
 		#:: Ding!
 		quest::ding();
 		#:: Set factions
