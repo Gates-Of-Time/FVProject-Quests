f313cf34ab5fe4c801384399bfba9a3ea460fe43
Dalethor <30395666+Dalethor@users.noreply.github.com>
2021-01-17 16:29:59 -0500

    Update #a_zombie.pl
    getting the flavor text when looting the item made the sequence backwards and was not working anyway.  not sure if we need to do something in the database to tell the character he has a text file.?.

--- qcat/#a_zombie.pl
+++ qcat/#a_zombie.pl
@@ -1,11 +1,9 @@
-sub EVENT_ITEM {
-	#:: Match a 18804 - Tattered Note
-	if (plugin::takeItems(18804 => 1)) {
-		quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
-	}
-	#:: Return unused items
-	plugin::returnUnusedItems();
+
+#:: Flavor Text
+sub EVENT_AGGRO {
+    quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
 }
+
 
 sub EVENT_DEATH_COMPLETE {
 	quest::say("ARGH!!!!!.. Thank.. you..");
