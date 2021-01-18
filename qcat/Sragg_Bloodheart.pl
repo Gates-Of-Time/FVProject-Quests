1e4218ee4cb5b86cf62d7fc0dcf6405b5cbd74a2
TurmoilToad <TurmoilToad@gmail.com>
2021-01-18 12:52:50 -0500

    Update Sragg_Bloodheart.pl
    Specify loc, adjust signal

--- qcat/Sragg_Bloodheart.pl
+++ qcat/Sragg_Bloodheart.pl
@@ -10,15 +10,6 @@ sub EVENT_ENTER {
 	}
 }
 
-sub EVENT_TIMER {
-	#:: Match timer 'sragg_tovax'
-	if ($timer eq "sragg_tovax") {
-		quest::stoptimer("sragg_tovax");
-		#:: Send a signal '1' to /qcat/Tovax_Vmar.pl (4020) with no delay
-		quest::signalwith(4020, 1, 0);
-	}
-}
-
 sub EVENT_SIGNAL {
 	#:: Match a signal '1' from /qcat/Tovax_Vmar.pl
 	if ($signal == 1) {
@@ -62,8 +53,6 @@ sub EVENT_ITEM {
 		quest::exp(100);
 		#:: Spawn one and only one The Qeynos Hills >> Tovax_Vmar (4020), without grid or guild war, in front of the current location
 		quest::unique_spawn(4020, 0, 0, -313.54, 411.46, -39.47, 77.8);
-		#:: Set a timer 'sragg_tovax' that triggers every 30 seconds
-		quest::settimer("sragg_tovax", 30);
 	}
 	#:: plugin::try_tome_handins(\%itemcount, $class, 'Shadowknight');
 	#:: Return unused items
