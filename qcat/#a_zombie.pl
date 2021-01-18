3b2ae9c7846a4c5f164ea8f20b1ff0159cd20f59
TurmoilToad <TurmoilToad@gmail.com>
2021-01-18 13:26:59 -0500

    Update #a_zombie.pl
    Added missing dialogue for note turn in

--- qcat/#a_zombie.pl
+++ qcat/#a_zombie.pl
@@ -1,9 +1,19 @@
-
-#:: Flavor Text
 sub EVENT_AGGRO {
-    quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
+	quest::say("Rele.. ase.. me.. from.. this.. tor.. ment.. ARGH! You will die for entering the domain of the Bloodsabers!! Karana.. help.. me?");
 }
 
+sub EVENT_ITEM {
+	#:: Match a 18804 - Tattered Note
+	if (plugin::takeItems(18804 => 1)) {
+		quest::say("kill me... kill...me");
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(1000);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
 
 sub EVENT_DEATH_COMPLETE {
 	quest::say("ARGH!!!!!.. Thank.. you..");
