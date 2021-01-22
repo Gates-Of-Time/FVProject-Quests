907f7c7cc78fd5041bdc77415bffafdd32a02456
Dalethor <30395666+Dalethor@users.noreply.github.com>
2021-01-22 16:14:46 -0500

    Update Parcil_Vinder.pl
    Moved the emotes from database to github

--- qey2hh1/Parcil_Vinder.pl
+++ qey2hh1/Parcil_Vinder.pl
@@ -3,6 +3,16 @@ sub EVENT_SPAWN {
 	quest::SetRunning(1);
 }
 
+sub EVENT_AGGRO {
+
+quest::say("In the name of the Rainkeeper, I strike at thee!!");
+}
+
+sub EVENT_DEATH_COMPLETE {
+
+quest::say("You will meet your own doom soon enough, my friend.. my influence with the Knights of Thunder is great, and they will not rest until I am avenged!");
+}
+
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("No time to talk!! I must be off. The grains of sand are falling and I must prove that we paladins from the Temple of Thunder are the quickest.");
