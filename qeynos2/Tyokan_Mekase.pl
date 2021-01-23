97ee121d90cba31b3487ccc3f6223356ae1c3eb1
TurmoilToad <TurmoilToad@gmail.com>
2021-01-23 15:40:33 -0500

    Update Tyokan_Mekase.pl
    Refactor a bit

--- qeynos2/Tyokan_Mekase.pl
+++ qeynos2/Tyokan_Mekase.pl
@@ -21,7 +21,7 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	#:: Match a 13296 - Prayer Beads and 100 gp
-	if (plugin::takeItemsCoin(0,0,100,0, 13296 => 1)) {
+	if (plugin::takeItemsCoin(0, 0, 100, 0, 13296 => 1)) {
 		quest::say("I see your beads need to be charged. Very Well here you are. Use them and good health to you!!!");
 		#:: Give a 13296 - Prayer Beads
 		quest::summonitem(13296);
@@ -30,18 +30,18 @@ sub EVENT_ITEM {
 	elsif (plugin::takeItems(13296 => 1)) {
 		quest::say("As instructed by High Priestess Jahnda I must ask for the beads and a donation of 100 gold coins.");
 		#:: Give a 13296 - Prayer Beads, no charges
-		quest::summonitem(13296,0);
+		quest::summonitem(13296, 0);
 	}
 	#:: Match a 13306 - T.O.L. 2020
 	elsif (plugin::takeItems(13306 => 1)) {
 		#:: Match if faction is Indifferent or better
-		if ($faction <= 4) {
+		if ($faction <= 5) {
 			quest::say("Oh, turning in your key, are you? Very well, defender of life. Here you are.");
+			#:: Give a random reward: 15126 - Spell: Inspire Fear or 15248 - Spell: Ward Summoned
+			quest::summonitem(quest::ChooseRandom(15126, 15126, 15248));
+			#:: Ding!
+			quest::ding();
 		}
-		#:: Give a random reward: 15126 - Spell: Inspire Fear or 15248 - Spell: Ward Summoned
-		quest::summonitem(quest::ChooseRandom(15126, 15126, 15248));
-		#:: Ding!
-		quest::ding();
 		else {
 			quest::say("Your mere presence disgusts me. Please remove yourself from my sight. Until you change yourself and your ways, you are unwelcome in the Temple of Life.");
 		}
