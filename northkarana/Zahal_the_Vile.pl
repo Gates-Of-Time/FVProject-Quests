fe3e9b99f1232f346d445d59c662556774461a95
TurmoilToad <TurmoilToad@gmail.com>
2021-01-26 14:06:34 -0500

    Update Zahal_the_Vile.pl

--- northkarana/Zahal_the_Vile.pl
+++ northkarana/Zahal_the_Vile.pl
@@ -11,3 +11,8 @@ sub EVENT_TIMER {
 		plugin::RandomRoam(250, 250);
 	}
 }
+
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn one and only one The Northern Plains of Karana >> #Regis_the_Reverent (13042), no grid or guildwar, at the specified location
+	quest::unique_spawn(13042, 0, 0, -1560, 1450, 50, 125.5);
+}
