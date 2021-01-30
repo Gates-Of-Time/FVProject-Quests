c71a4d44267a7cf1790bd56804cc6fc19fff31cd
TurmoilToad <TurmoilToad@gmail.com>
2021-01-30 18:25:43 -0500

    Update Gindlin_Toxfodder.pl
    Fix comment

--- qey2hh1/Gindlin_Toxfodder.pl
+++ qey2hh1/Gindlin_Toxfodder.pl
@@ -8,7 +8,7 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Match two 14018 - Spider Venom Sac, a 14018 - Crow's Special Brew, and 20 gold
+	#:: Match 20 gold pieces, two 14018 - Spider Venom Sac, and a 13901 - Crow's Special Brew
 	if (plugin::takeItemsCoin(0, 0, 20, 0, 14018 => 2, 13901 => 1)) {
 		quest::say("Here.  I could care less what you do with this.  Hopefully you'll lay some on the Circle of Unseen Hands.");
 		#:: Give a 14015 - Spider Venom
