ae4d9539ade2fbca92f60cb99002d59c2d860e87
TurmoilToad <TurmoilToad@gmail.com>
2021-02-04 11:12:50 -0500

    Update a_soul_carrier.pl

--- airplane/a_soul_carrier.pl
+++ airplane/a_soul_carrier.pl
@@ -6,8 +6,8 @@ my $mobnpc1;
 my $mobnpc2;
 
 sub EVENT_DEATH {
-	#:: Send a signal '2' to The Plane of Sky >> Key_Master (71056) after a 200 second delay (3.33 min)
-  	quest::signalwith(71056, 2, 200000);
+	#:: Spawn one and only one The Plane of Sky >> Nottap_Ekim (71111), with no grid or guildwar, at the specified location
+  	quest::unique_spawn(71111, 0, 0, 0, 0, 0, 0);
 	#:: Spawn a The Plane of Sky >> an_essence_carrier (71070)
 	$entid1 = quest::spawn2(71070, 0, 0, $x + 10, $y, $z, $h);
 	$mob1 = $entity_list->GetMobID($entid1);
