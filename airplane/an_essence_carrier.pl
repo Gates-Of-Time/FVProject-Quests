e69dc6e2f77ef9c48ce22ffa9d1830685c8524ca
TurmoilToad <TurmoilToad@gmail.com>
2021-02-04 08:49:25 -0500

    Update an_essence_carrier.pl

--- airplane/an_essence_carrier.pl
+++ airplane/an_essence_carrier.pl
@@ -10,10 +10,10 @@ sub EVENT_DEATH_COMPLETE {
 	$entid1 = quest::spawn2(71071, 0, 0, $x + 10, $y, $z, $h);
 	$mob1 = $entity_list->GetMobID($entid1);
 	$mobnpc1 = $mob1->CastToNPC();
-	$mobnpc1->AddToHateList($npc->GetHateTop());
+	$mobnpc1->AddToHateList($killer_id);
 	#:: Spawn a The Plane of Sky >> an_essence_tamer (71071)
 	$entid2 = quest::spawn2(71071, 0, 0, $x - 10, $y, $z, $h);
 	$mob2 = $entity_list->GetMobID($entid2);
 	$mobnpc2 = $mob2->CastToNPC();
-	$mobnpc2->AddToHateList($npc->GetHateTop());
+	$mobnpc2->AddToHateList($killer_id);
 }
