7514e9f34e810ab7367e5e9311f57a2a242776ec
TurmoilToad <TurmoilToad@gmail.com>
2021-02-13 11:11:52 -0500

    Update Innkeep_Gub.pl
    Added missing dialogue.

--- feerrott/Innkeep_Gub.pl
+++ feerrott/Innkeep_Gub.pl
@@ -1,6 +1,17 @@
-sub EVENT_SAY {
+sub EVENT_SAY { 
 	if ($text=~/hail/i) {
-		quest::say("Huh? Oh hi $name. Youse want to buy sumthin?");
+		quest::say("Huh? Oh hi $name. Youse want to buy sumthin?  Or maybe youz a [baker]?"); 
+	}
+	elsif ($text=~/baker/i) {
+		quest::say("Gud, you help me restock. You helps me out by restocking my [muffinz]. Or you go findz da guy wid da gud bread loaves and bring mez a [shipment of bread]."); 
+	}
+	elsif ($text=~/muffin/i) {
+		quest::say("You take dis crate and fill it with muffinz, den seal it up and bring da full crate bak to me. Don't you tink you can go buy dat store stuff either, I wants fresh baked muffinz.  Da store ones too old and will gets moldy too fast, den I have to toss out.  Da make me very unhappy wid you!"); 
+		#:: Give a 17881 - Muffin Crate
+		quest::summonitem(17881);
+	}
+	elsif ($text=~/shipment of bread/i) {
+		quest::say("You go to da south plains and find da Meadowgreen guy.  He is very gud baker and we wantz his bag of bread loaves.  You go tell him dat, get movin!"); 
 	}
 }
 
