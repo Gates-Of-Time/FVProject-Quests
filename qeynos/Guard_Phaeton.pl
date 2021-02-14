ff1adcc5eab607357bc0785deecd4196132ab2b4
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 15:24:49 -0500

    Update Guard_Phaeton.pl
    Added missing dialogue.

--- qeynos/Guard_Phaeton.pl
+++ qeynos/Guard_Phaeton.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hail, $name! What brings you to the docks? Come to visit ol' Captain Rohand and do some fishing? I plan on doing that when I can get a day off.");
+	}
+}
+
 sub EVENT_SIGNAL {
 	#:: Match signal "1" from qeynos/Lieutenant_Arathur.pl
 	if ($signal == 1) {
