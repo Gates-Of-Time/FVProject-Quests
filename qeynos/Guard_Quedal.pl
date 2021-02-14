d10c866b76fd603118d48bbc6d2da964118c995d
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 15:50:45 -0500

    Update Guard_Quedal.pl
    Add missing dialogue.

--- qeynos/Guard_Quedal.pl
+++ qeynos/Guard_Quedal.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("<Burp!> Heh? $name? Never hoid o' ya. Beat it. I got woik ta do.");
+	}
+}
+
 sub EVENT_COMBAT {
 	if ($combat_state == 1) {
 		quest::say("Time to die $name!");
