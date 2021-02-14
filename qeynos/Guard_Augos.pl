369c109475d39c432544a244387adbdd1bb3282c
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 15:54:40 -0500

    Update Guard_Augos.pl
    Add missing dialogue.

--- qeynos/Guard_Augos.pl
+++ qeynos/Guard_Augos.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Yeah, my name is Augos... Listen up! Kane says I don't have to talk to none of you peons if I don't feel like it. I DON'T feel like it. So unless you want trouble, get a move on!");
+	}
+}
+
 sub EVENT_COMBAT {
 	if ($combat_state == 1) {
 		quest::say("Time to die $name");
