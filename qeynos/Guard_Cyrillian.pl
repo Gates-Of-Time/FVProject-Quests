988774d98bee0491499c368f59344b8632214328
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 15:21:59 -0500

    Update Guard_Cyrillian.pl
    Added missing dialogue.

--- qeynos/Guard_Cyrillian.pl
+++ qeynos/Guard_Cyrillian.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hey, $name. Heh, heh.  Keep up the good work.  Commander Bayle needs all the patrols he can get.  He says the time is near, now that the Shrine of Bertoxxulous in the aqueducts is complete.");
+	}
+}
+
 sub EVENT_SIGNAL {
 	if ($signal == 1) {
 		#:: Signal sent from qeynos/Lieutenant_Arathur.pl
