eaf79ab743a19cfc4a6eff0efd09263bad7de6d9
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 15:49:01 -0500

    Update Guard_Oleph.pl
    Add missing dialogue.

--- qeynos/Guard_Oleph.pl
+++ qeynos/Guard_Oleph.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Oh good. Another fine example of why I am growing to hate this city. If Commander Bayle were running things here, we would not have to put up with weaklings like you walking our streets.");
+	}
+}
+
 sub EVENT_COMBAT {
 	if ($combat_state == 1) {
 		quest::say("Time to die $name!");
