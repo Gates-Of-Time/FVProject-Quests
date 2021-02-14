4297372691b9e3d85b9817deb4e2dadde3e27503
TurmoilToad <TurmoilToad@gmail.com>
2021-02-14 16:01:28 -0500

    Update Guard_Dunix.pl
    Added missing dialogue.

--- qeynos/Guard_Dunix.pl
+++ qeynos/Guard_Dunix.pl
@@ -1,3 +1,9 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings, $name. Have you seen the fine clock in Merchant's Square? It was a gift from the gnomes of Ak'Anon to Antonius Bayle and the people of Qeynos.");
+	}
+}
+
 sub EVENT_SIGNAL {
 	if ($signal == 1) {
 		#:: Signal sent from qeynos/Lieutenant_Arathur.pl
