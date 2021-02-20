783db5530928aedea94d88c4c83807c7be524d30
FVFippy <44533609+FVFippy@users.noreply.github.com>
2021-02-20 16:39:34 -0500

    Update Durkis_Battlemore.pl
    Item ID was incorrect in the plugin.  Fixed, so Durkis should now appreciate the note more than before.

--- butcher/Durkis_Battlemore.pl
+++ butcher/Durkis_Battlemore.pl
@@ -1,6 +1,6 @@
 sub EVENT_ITEM {
 	#:: Match a 8910 - A Note to Battlemore
-	if (plugin::takeItems(18770 => 1)) {
+	if (plugin::takeItems(8910 => 1)) {
 		quest::say("What have ye got there, lad? Ack. I thought I would never hear from that crazy old man again. He wants me to give you a pair o' me gauntlets. I suppose I kin do that. Here you are, Southern. Wear them in good health");
 		#:: Give item 8909 - Gem Inlaid Gauntlets
 		quest::summonitem(8909);
