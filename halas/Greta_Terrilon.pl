080ad483df55d7da7d70670ef8d61e3774934a4b
TurmoilToad <TurmoilToad@gmail.com>
2021-03-04 15:07:32 -0500

    Update Greta_Terrilon.pl
    Correct dialogue.  No response to "hail"

--- halas/Greta_Terrilon.pl
+++ halas/Greta_Terrilon.pl
@@ -1,11 +1,11 @@
 sub EVENT_SAY {
-	if ($text=~/hail/i) {
-		quest::say("You will have to excuse me. I am quite busy.");
-	}
-	elsif ($text=~/some ink/i) {
-		quest::say("Oh, sure. Here you go.");
+	if ($text=~/some ink/i) {
+		quest::say("Jinkus must've sent ye fer some of me special ink made from the pigement of th' dalura flower.  Ye may take some free o' charge as a donation to the church and to the will of the Tribunal as well.");
 		#:: Give a 12619 - Vial of Datura Ink
 		quest::summonitem(12619);
+	}
+	elsif ($text=~/shaman of justice/i) {
+		quest::say("The Shamans of Justice serve the will of the Tribunal. They search out those who defy the laws set by the Tribunal.");
 	}
 }
 
