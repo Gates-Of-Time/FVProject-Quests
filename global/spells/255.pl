db457f760d73ad245f51eefc356433fe2427bd20
TurmoilToad <TurmoilToad@gmail.com>
2021-03-25 21:10:28 -0400

    Create 255.pl

---
+++ global/spells/255.pl
@@ -0,0 +1,8 @@
+#:: 255 - Invisibility versus Animals
+
+sub EVENT_SPELL_EFFECT_CLIENT {
+	if ($client->GetPetID()) {
+		$PetID = $entity_list->GetMobByID($client->GetPetID());
+		$PetID->Kill();
+	}
+}
