2424e1446c3810716c7969becf3e8e0d18cc02c2
TurmoilToad <TurmoilToad@gmail.com>
2021-03-25 21:11:30 -0400

    Create 719.pl

---
+++ global/spells/719.pl
@@ -0,0 +1,8 @@
+#:: 719 - Shauri's Sonorous Clouding
+
+sub EVENT_SPELL_EFFECT_CLIENT {
+	if ($client->GetPetID()) {
+		$PetID = $entity_list->GetMobByID($client->GetPetID());
+		$PetID->Kill();
+	}
+}
