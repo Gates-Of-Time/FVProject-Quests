82662dcb334175ee1fa298c9546d4c4ca18a31af
TurmoilToad <TurmoilToad@gmail.com>
2021-04-11 12:35:34 -0400

    Update Gern_Tassel.pl
    Syntax and refactor for consistency

--- freportn/Gern_Tassel.pl
+++ freportn/Gern_Tassel.pl
@@ -1,21 +1,21 @@
 sub EVENT_SAY { 
 	if ($text=~/hail/i) {
 		quest::say("Welcome! Why don't you browse through my selection of fine goods and pick out some things you like $name?");
-		}
+	}
 	elsif ($text=~/baker/i) {
-		quest::say("You are a baker?  I could use a new supply of [muffins]. Or maybe you would travel to South Karana for a [shipment of bread]?"); 
+		quest::say("You are a baker?  I could use a new supply of [muffins]. Or maybe you would travel to South Karana for a [shipment of bread]?");
 	}
 	elsif ($text=~/muffins/i) {
-		quest::say("Take this crate and fill it with fresh baked muffins."); 
+		quest::say("Take this crate and fill it with fresh baked muffins.");
 		#:: Give a 17881 - Muffin Crate
 		quest::summonitem(17881);
 	}
 	elsif ($text=~/shipment of bread/i) {
-		quest::say("Ok, find the centaur named Meadowgreen.  His bread is the best I have tasted."); 
+		quest::say("Ok, find the centaur named Meadowgreen.  His bread is the best I have tasted.");
 	}
 }
 
-sub EVENT_ITEM
+sub EVENT_ITEM {
 	#:: Match 1839 - Full Muffin Crate
 	if (plugin::takeItems(1839 => 1)) {
 		quest::say("These are quality muffins! You are obviously quite a skilled baker. Here is your payment as promised. Now I can get back to business again.");
@@ -29,9 +29,9 @@ sub EVENT_ITEM
 		#:: Grant a small amount of level-based experience
 		$client->AddLevelBasedExp(4, 14);
 		#:: Create a hash for storing cash - 500 to 750cp
-		my %cash = plugin::RandomCash(500,750);
+		my %cash = plugin::RandomCash(500, 750);
 		#:: Grant a random cash reward
-		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+		quest::givecash($cash{copper}, $cash{silver}, $cash{gold}, $cash{platinum});
 		#:: North Freeport >> Gern Tessel (8014), restock 13014 - Muffin, quantity 20
 		quest::MerchantSetItem(8014, 13014, 20);
 	}
