2a997ebf8b5c593a29150b3ded4a9e739c3b1a5d
TurmoilToad <TurmoilToad@gmail.com>
2021-04-11 10:43:51 -0400

    Update Trug.pl
    Refactor for consistency.

--- lakerathe/Trug.pl
+++ lakerathe/Trug.pl
@@ -1,15 +1,19 @@
 sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("Gimme duffynitey rocks! Me powdrem GUUD!!!");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Gimme duffynitey rocks! Me powdrem GUUD!!!");
+	}
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 10073 => 1)) { #Dufrenite
-    quest::emote("takes the dufrenite and smashes it with his bare hands.");
-    quest::say("Here ya go!");
-    quest::summonitem(19052); #Powdered Dufrenite
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 10073 - Dufrenite
+	if (plugin::takeItems(10073 => 1)) {
+		quest::emote("takes the dufrenite and smashes it with his bare hands.");
+		quest::say("Here ya go!");
+		#:: Give a 19052 - Powdered Dufrenite
+		quest::summonitem(19052);
+		#:: Ding!
+		quest::ding();
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-#END of FILE Zone:lakerathe  ID:51071 -- Trug
