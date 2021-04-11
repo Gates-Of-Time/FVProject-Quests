09a466e31bfa72e35397d35d4abec6df1ca60353
TurmoilToad <TurmoilToad@gmail.com>
2021-04-11 19:00:38 -0400

    Update orc_runner.pl
    Missing attack message.

--- butcher/orc_runner.pl
+++ butcher/orc_runner.pl
@@ -1,4 +1,8 @@
- sub EVENT_SPAWN {
+sub EVENT_SPAWN {
 	#:: Start NPC running
 	quest::SetRunning(1);
+}
+
+sub EVENT_AGGRO {
+	quest::say("Fool!  The dark elves will slay you for interfering with my duties!");
 }
