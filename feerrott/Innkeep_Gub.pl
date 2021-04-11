d99a76f4c58d7b9dab4b368950e1d83759664ffb
TurmoilToad <TurmoilToad@gmail.com>
2021-04-11 11:31:52 -0400

    Update Innkeep_Gub.pl
    Add bread.

--- feerrott/Innkeep_Gub.pl
+++ feerrott/Innkeep_Gub.pl
@@ -25,14 +25,30 @@ sub EVENT_ITEM {
 		quest::faction(338, 2);		#:: + Merchants of Oggok
 		quest::faction(228, 1);		#:: + Clurg
 		quest::faction(337, 1);		#:: + Oggok Guards
-		#:: Grant a moderate amount of experience
-		quest::exp(1000);
+		#:: Grant a small amount of level-based experience
+		$client->AddLevelBasedExp(4, 14);
 		#:: Create a hash for storing cash - 500 to 750cp
 		my %cash = plugin::RandomCash(500,750);
 		#:: Grant a random cash reward
 		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 		#:: Feerrott >> Innkeep Gub (47114), restock 13014 - Muffin, quantity 20
 		quest::MerchantSetItem(47114, 13014, 20);
+	}
+	#:: Match a 1838 - Bag of Bread Loaves
+	elsif (plugin::takeItems(1838 => 1)) {
+		quest::say("You take some money. Weeze happy now dat weeze got more bread for sale. You maybe check back later if weeze run out of bread again.");
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(338, 4);		#:: + Merchants of Oggok
+		quest::faction(228, 1);		#:: + Clurg
+		quest::faction(337, 1);		#:: + Oggok Guards
+		#:: Grant a small amount of level-based experience
+		$client->AddLevelBasedExp(4, 14);
+		#:: Create a hash for storing cash - 1200 to 1400cp
+		my %cash = plugin::RandomCash(1200, 1400);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
