e8f21ac9c786b8d01b5f8fea557dd5ef090ecb4d
TurmoilToad <TurmoilToad@gmail.com>
2021-04-16 21:53:51 -0400

    Update Noble_Dojorn.pl

--- airplane/Noble_Dojorn.pl
+++ airplane/Noble_Dojorn.pl
@@ -5,7 +5,7 @@ sub EVENT_COMBAT {
 	}
 }
 
-sub EVENT_DEATH {
+sub EVENT_DEATH_COMPLETE {
 	#:: Spawn a The Plane of Sky >> Overseer_of_Air (71034), with no grid or guild war, at the specified location
 	quest::spawn2(71034, 0, 0, -540.6, 767.1, 174.1, 65);
 }
