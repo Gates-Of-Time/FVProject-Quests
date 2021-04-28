61821e67f723a1919625db8b752d37f8d7eb0dad
TurmoilToad <TurmoilToad@gmail.com>
2021-04-28 16:07:04 -0400

    Update Watchman_Grep.pl
    Adjust to avoid spam

--- steamfont/Watchman_Grep.pl
+++ steamfont/Watchman_Grep.pl
@@ -1,11 +1,18 @@
-sub EVENT_ITEM {
-	#:: Return unused items
-	plugin::return_items(\%itemcount);
+sub EVENT_SIGNAL {
+	#:: Match a signal '1' from steamfont/Cargo_Clockwork.pl
+	if ($signal == 1) {
+		#:: Pull a list of clients from the entity list
+		my @ClientList = $entity_list->GetClientList();
+		#:: Match if the ClientList is not empty
+		if (scalar @ClientList > 0) {
+			quest::say("What a glorious machine the cargo clockwork is!!");
+			#:: Send a signal '1' to Steamfont Mountains >> Cargo_Clockwork (56105) with no delay
+			quest::signal(56105, 1, 0);
+		}
+	}
 }
 
-sub EVENT_SIGNAL {
-	#:: Signal from steamfont/Cargo_Clockwork.pl
-	quest::say("What a glorious machine the cargo clockwork is!!");
-	#:: Send a signal to Steamfont Mountains >> Cargo_Clockwork (56105)
-	quest::signal(56105,1);
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
