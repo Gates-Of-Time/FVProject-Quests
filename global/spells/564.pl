0970c33e984c5f83eea7b6d6a05f80cae00b2d77
TurmoilToad <TurmoilToad@gmail.com>
2021-04-29 12:32:18 -0400

    Update 564.pl
    No point in checking client.

--- global/spells/564.pl
+++ global/spells/564.pl
@@ -1,11 +1,4 @@
-sub EVENT_SPELL_EFFECT_CLIENT
-{
-	if(($client->GetClientVersionBit() & 4294967292)!= 0)
-	{
-		quest::MovePCInstance(25, 1, -715, -57, 42, 0);
-	}
-	else
-	{
-		quest::movepc(25, -772, 432, 35);
-	}
+sub EVENT_SPELL_EFFECT_CLIENT {
+	#:: Move player to Nektulos (25) at the specified coordinates
+	quest::movepc(25, -772, 432, 35);
 }
