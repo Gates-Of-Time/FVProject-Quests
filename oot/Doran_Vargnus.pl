11f93e5bf1be7d96cf3efb3af3ad7636c651b6bd
TurmoilToad <TurmoilToad@gmail.com>
2021-04-29 13:17:03 -0400

    Update Doran_Vargnus.pl
    Add missing dialogue, adjust item reward.

--- oot/Doran_Vargnus.pl
+++ oot/Doran_Vargnus.pl
@@ -1,9 +1,15 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Greetings $name. We do not get too many visitors to the island. I have a few pieces of armor for sale, fashioned for the small of course.");
+	}
+}
+
 sub EVENT_ITEM {
 	#:: Match a 13995 - Knight
 	if (plugin::takeItems(13995 => 1)) {
 		quest::say("Ahh! You must have been sent by Beno of the Stormguard. I have been wondering when he would return my card. I have not been playing with a full deck. How about a reward? Hmm. Let's see what I have lying around... Here! Take this. I cannot possibly give you any of my good armor.");
-		#:: Randomly reward 3301 - Dwarven Ringmail Tunic, 3048 - Blackened Iron Bracers, 3042 - Blackened Iron Collar, 3050 - Blackened Iron Legplates, 3044 - Blackened Iron Spaulder, 20809 - Small Shield, 7012 - Bronze Dagger, 3111 - Ringmail Pants, 3110 - Ringmail Gloves, 7015 - Bronze Main Gauche
-		quest::summonitem(quest::ChooseRandom(3301,3048,3042,3050,3044,20809,7012,3103,3111,3110,7015));
+		#:: Randomly reward 3301 - Dwarven Ringmail Tunic, 3048 - Blackened Iron Bracers, 3042 - Blackened Iron Collar, 3050 - Blackened Iron Legplates, 3044 - Blackened Iron Spaulder, 9009 - Small Buckler, 7012 - Bronze Dagger, 3111 - Ringmail Pants, 3110 - Ringmail Gloves, 7015 - Bronze Main Gauche
+		quest::summonitem(quest::ChooseRandom(3301, 3048, 3042, 3050, 3044, 9009, 7012, 3103, 3111, 3110, 7015));
 		#:: Ding!
 		quest::ding();
 		#:: Set factions
