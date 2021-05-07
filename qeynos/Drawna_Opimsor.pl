cdc16fe546e25f056102aa0ee8f7f99e00fb057d
TurmoilToad <TurmoilToad@gmail.com>
2021-05-07 09:35:49 -0400

    Update Drawna_Opimsor.pl
    Less spam

--- qeynos/Drawna_Opimsor.pl
+++ qeynos/Drawna_Opimsor.pl
@@ -1,9 +1,14 @@
 sub EVENT_SIGNAL {
 	#:: Match a signal "1" from 
 	if ($signal == 1) {
-		quest::say("Yeah, it seems hard to come by lately. We're all out of rat whiskers.");
-		#:: Send a signal "1" to South Qeynos >> Caleah_Herblender (1118) with no delay
-		quest::signalwith(1118,1,0);
+		#:: Pull a list of clients from the entity list
+		my @ClientList = $entity_list->GetClientList();
+		#:: Match if the ClientList is not empty
+		if (scalar @ClientList > 0) {
+			quest::say("Yeah, it seems hard to come by lately. We're all out of rat whiskers.");
+			#:: Send a signal "1" to South Qeynos >> Caleah_Herblender (1118) with no delay
+			quest::signalwith(1118,1,0);
+		}
 	}
 }
 
