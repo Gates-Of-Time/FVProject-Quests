0dba00878aa187c6e26b235d3b6d73cea862988c
TurmoilToad <TurmoilToad@gmail.com>
2021-05-27 19:05:07 -0400

    Update Guard_Leopold.pl
    Syntax error.

--- qeytoqrg/Guard_Leopold.pl
+++ qeytoqrg/Guard_Leopold.pl
@@ -12,7 +12,7 @@ sub EVENT_SAY {
 sub EVENT_COMBAT {
 	#:: Match combat state 1 - entered combat
 	if ($combat_state == 1) {
-		my $cur_target = $npc->GetHateTop()
+		my $cur_target = $npc->GetHateTop();
 		if ($cur_target) {
 			my $target_name = $cur_target->GetCleanName();
 			quest::say("Time to die " . $target_name . "!");
