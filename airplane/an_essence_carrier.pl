5aa9e4194d1233f26ebbb7edd6608b0ea9b2cc90
TurmoilToad <TurmoilToad@gmail.com>
2021-05-28 12:36:31 -0400

    Update an_essence_carrier.pl

--- airplane/an_essence_carrier.pl
+++ airplane/an_essence_carrier.pl
@@ -1,3 +1,4 @@
+my $killed;
 my $entid1;
 my $entid2;
 my $mob1;
@@ -6,13 +7,16 @@ my $mobnpc1;
 my $mobnpc2;
 
 sub EVENT_DEATH {
-	#:: Spawn a The Plane of Sky >> an_essence_tamer (71071)
-	$entid1 = quest::spawn2(71071, 0, 0, $x+10, $y, $z, $h);
-	$entid2 = quest::spawn2(71071, 0, 0, $x-10, $y, $z, $h);
-	$mob1 = $entity_list->GetMobID($entid1);
-	$mob2 = $entity_list->GetMobID($entid2);
-	$mobnpc1 = $mob1->CastToNPC();
-	$mobnpc2 = $mob2->CastToNPC();
-	$mobnpc1->AddToHateList($npc->GetHateTop());
-	$mobnpc2->AddToHateList($npc->GetHateTop());
+	if (!$killed) {
+		#:: Spawn a The Plane of Sky >> an_essence_tamer (71071)
+		$entid1 = quest::spawn2(71071, 0, 0, $x+10, $y, $z, $h);
+		$entid2 = quest::spawn2(71071, 0, 0, $x-10, $y, $z, $h);
+		$mob1 = $entity_list->GetMobID($entid1);
+		$mob2 = $entity_list->GetMobID($entid2);
+		$mobnpc1 = $mob1->CastToNPC();
+		$mobnpc2 = $mob2->CastToNPC();
+		$mobnpc1->AddToHateList($npc->GetHateTop());
+		$mobnpc2->AddToHateList($npc->GetHateTop());
+		$killed = 1;
+	}
 }
