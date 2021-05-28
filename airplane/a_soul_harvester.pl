a8f38553d575773b2e920624e17a8701e0f683d2
TurmoilToad <TurmoilToad@gmail.com>
2021-05-28 17:24:04 -0400

    Update a_soul_harvester.pl
    Switch to data bucket, add timer

--- airplane/a_soul_harvester.pl
+++ airplane/a_soul_harvester.pl
@@ -3,13 +3,29 @@ my $entid1;
 my $mob1;
 my $mobnpc1;
 
+sub EVENT_SPAWN {
+	#:: Create a timer 'depop' that triggers in 4800 seconds (80 min)
+	quest::settimer("depop", 4800);
+}
+
+sub EVENT_TIMER {
+	#:: Match if timer named "depop"
+	if ($timer eq "depop") {
+		#:: Stop the timer "depop"
+		quest::stoptimer("depop");
+		#:: Depop with spawn timer active
+		quest::depop();
+	}
+}
+
 sub EVENT_DEATH {
-	if (!$killed) {
+        $key = $npc->GetID() . "-killed";
+        if (!quest::get_data($key)) {
 		#:: Spawn a The Plane of Sky >> a_soul_tamer (71100)
 		$entid1 = quest::spawn2(71100, 0, 0, $x, $y, $z, $h);
 		$mob1 = $entity_list->GetMobID($entid1);
 		$mobnpc1 = $mob1->CastToNPC();
 		$mobnpc1->AddToHateList($npc->GetHateTop());
-		$killed = 1;
+		quest::set_data($key, 1, 30);
 	}
 }
