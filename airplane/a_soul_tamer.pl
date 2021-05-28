b080f9ae75be7b9133a3f871e00b113933f7bbb8
TurmoilToad <TurmoilToad@gmail.com>
2021-05-28 17:26:00 -0400

    Create a_soul_tamer.pl
    Add depop timer

---
+++ airplane/a_soul_tamer.pl
@@ -0,0 +1,14 @@
+sub EVENT_SPAWN {
+	#:: Create a timer 'depop' that triggers in 4800 seconds (80 min)
+	quest::settimer("depop", 4800);
+}
+
+sub EVENT_TIMER {
+	#:: Match if timer named "depop"
+	if ($timer eq "depop") {
+		#:: Stop the timer "depop"
+		quest::stoptimer("depop");
+		#:: Depop with spawn timer active
+		quest::depop();
+	}
+}
