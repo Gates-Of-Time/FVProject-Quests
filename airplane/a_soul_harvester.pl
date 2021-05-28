7ccde107cd51100945eedd5c93a1be900f0e657a
TurmoilToad <TurmoilToad@gmail.com>
2021-05-28 12:38:12 -0400

    Update a_soul_harvester.pl

--- airplane/a_soul_harvester.pl
+++ airplane/a_soul_harvester.pl
@@ -1,11 +1,15 @@
+my $killed;
 my $entid1;
 my $mob1;
 my $mobnpc1;
 
 sub EVENT_DEATH {
-	#:: Spawn a The Plane of Sky >> a_soul_tamer (71100)
-	$entid1 = quest::spawn2(71100, 0, 0, $x, $y, $z, $h);
-	$mob1 = $entity_list->GetMobID($entid1);
-	$mobnpc1 = $mob1->CastToNPC();
-	$mobnpc1->AddToHateList($npc->GetHateTop());
+	if (!$killed) {
+		#:: Spawn a The Plane of Sky >> a_soul_tamer (71100)
+		$entid1 = quest::spawn2(71100, 0, 0, $x, $y, $z, $h);
+		$mob1 = $entity_list->GetMobID($entid1);
+		$mobnpc1 = $mob1->CastToNPC();
+		$mobnpc1->AddToHateList($npc->GetHateTop());
+		$killed = 1;
+	}
 }
