482a99f68df2ab8e5d301bde8144bd42b0284f30
TurmoilToad <TurmoilToad@gmail.com>
2021-07-23 21:35:19 -0400

    Update Wizard_Schrock.pl
    Repop someday...

--- airplane/Wizard_Schrock.pl
+++ airplane/Wizard_Schrock.pl
@@ -10,13 +10,15 @@ sub EVENT_SAY {
 			quest::say("I will summon him for you then");
 			#:: Spawn a The Plane of Sky >> Neasin_Leornic (71092), without grid or guild war, at the specified location
 			quest::spawn2(71092, 0, 0, 585, 1304.3, -766.9, 0.1);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
   		elsif ($text=~/abec/i) {
 			quest::say("I will summon him for you then");
 			#:: Spawn a The Plane of Sky >> Abec_Ianor (71079), without grid or guild war, at the specified location
 			quest::spawn2(71079, 0, 0, 585, 1304.3, -766.9, 0.1);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 	}
 }
