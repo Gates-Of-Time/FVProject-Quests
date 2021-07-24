3102a113a60662fa803aa4ff14e95368fa9be7a8
TurmoilToad <TurmoilToad@gmail.com>
2021-07-23 21:37:01 -0400

    Update Drakis_Bloodcaster.pl
    Repop someday...

--- airplane/Drakis_Bloodcaster.pl
+++ airplane/Drakis_Bloodcaster.pl
@@ -10,13 +10,15 @@ sub EVENT_SAY {
 			quest::say("Very well.");
 			#:: Spawn a The Plane of Sky >> Jzil_GSix (71074), without grid or guild war, at the specified location
 			quest::spawn2(71074, 0, 0, 654.9, 1305, -762.2, 22);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 		elsif ($text=~/dugaas/i) { 
 			quest::say("I will summon him for you then");
 			#:: Spawn a The Plane of Sky >> Dugaas_Helpyre (71084), without grid or guild war, at the specified location
 			quest::spawn2(71084, 0, 0, 661.6, 1302.8, -766.9, 254.9);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 	}
 }
