01e43ea03efb3786b46c53006ad52b352b81042e
TurmoilToad <TurmoilToad@gmail.com>
2021-07-23 21:38:10 -0400

    Update Josin_Faithbringer.pl
    Repop someday...

--- airplane/Josin_Faithbringer.pl
+++ airplane/Josin_Faithbringer.pl
@@ -10,13 +10,15 @@ sub EVENT_SAY {
 			quest::say("I shall summon them for you");
 			#:: Spawn The Plane of Sky >> Alan_Harten (71080), without grid or guild war, at the specified location
 			quest::spawn2(71080, 0, 0, 660.7, 1361.6, -766.9, 193.2);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 		elsif ($text=~/deric/i) {
 			quest::say("I shall summon them for you");
 			#:: Spawn The Plane of Sky >> Deric_Lennox (71083), without grid or guild war, at the specified location
 			quest::spawn2(71083, 0, 0, 660.7, 1338.0, -766.9, 193.2);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 	}
 }
