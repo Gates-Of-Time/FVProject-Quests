ae23544d360ffaf29bbbc8beebb56d11d00d124a
TurmoilToad <TurmoilToad@gmail.com>
2021-07-23 21:35:55 -0400

    Update Magus_Frinon.pl
    Repop someday...

--- airplane/Magus_Frinon.pl
+++ airplane/Magus_Frinon.pl
@@ -10,13 +10,15 @@ sub EVENT_SAY {
 			quest::say("I will summon him for you then");
 			#:: Spawn a The Plane of Sky >> Frederic_Calermin (71088), without grid or guild war, at the specified location
 			quest::spawn2(71088, 0, 0, 614.5, 1304.1, -766.9, 255);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 		elsif ($text=~/roanis/i) {
 			quest::say("I will summon him for you then");
 			#:: Spawn a The Plane of Sky >> Roanis_Elindar (71094), without grid or guild war, at the specified location
 			quest::spawn2(71094, 0, 0, 614.5, 1304.1, -766.9, 255);
-			quest::depop();
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 	}
 }
