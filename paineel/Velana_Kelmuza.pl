480349b52b6c93592cdab6fdf7d673a84a25e4e0
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:33:51 -0400

    Update Velana_Kelmuza.pl
    Commas, tabs, refactor for consistency, don't eat items.

--- paineel/Velana_Kelmuza.pl
+++ paineel/Velana_Kelmuza.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("Hello there. $name. I hope you are faring well this day.");
-    }
+	if ($text=~/hail/i) {
+		quest::say("Hello there, $name. I hope you are faring well this day.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
