16db8d1c25106b483745a859fec4f2ff344dbd0b
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:11:21 -0400

    Update Guard_Heridion.pl
    Refactor, comment

--- paineel/Guard_Heridion.pl
+++ paineel/Guard_Heridion.pl
@@ -1,12 +1,13 @@
 sub EVENT_SAY {
-    if ($faction <= 5) {
-        if ($text=~/hail/i) {
-            quest::say("Uggg. You needz [keyz]? Rrrrrrr.");
-        }
-        elsif ($text=~/key/i) {
-            quest::say("Uggggg. Take dis keyz.");
-            #:: Give a 6378 - Bone Crafted Key
-		    quest::summonitem(6378);
-        }
-    }
+	#:: Match if faction is Indifferent or better
+	if ($faction <= 5) {
+		if ($text=~/hail/i) {
+			quest::say("Uggg. You needz [keyz]? Rrrrrrr.");
+		}
+		elsif ($text=~/key/i) {
+			quest::say("Uggggg. Take dis keyz.");
+			#:: Give a 6378 - Bone Crafted Key
+			quest::summonitem(6378);
+		}
+	}
 }
