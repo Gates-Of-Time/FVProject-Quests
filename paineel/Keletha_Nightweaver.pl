a4062c664319f7e844679d4d6d79f8ea116c2245
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:20:21 -0400

    Update Keletha_Nightweaver.pl
    Commas

--- paineel/Keletha_Nightweaver.pl
+++ paineel/Keletha_Nightweaver.pl
@@ -8,8 +8,8 @@ sub EVENT_ITEM {
 	#:: Match 1774 - Envelope with dust sample
 	if (plugin::takeItems(1774 => 1)) {
 		quest::emote("takes the note, dumps the dust into her hand and examines it for a moment. She looks up and chuckles");
-        quest::say("It's amuzing how one of our Royal Guards cannot identify this substance. Do you feel how it's slightly cooler than other metals? It's called Permafrost Iron, also known as Cold Iron to laymen. Miragul wrote of it's properties in his memoirs. There are much better metals at our disposal now, so it's use is rare except in more primitive cultures. Here is my report.");
-        #:: Give a 1775 - Heretic's Report
+		quest::say("It's amuzing how one of our Royal Guards cannot identify this substance. Do you feel how it's slightly cooler than other metals? It's called Permafrost Iron, also known as Cold Iron to laymen. Miragul wrote of it's properties in his memoirs. There are much better metals at our disposal now, so it's use is rare except in more primitive cultures. Here is my report.");
+		#:: Give a 1775 - Heretic's Report
 		quest::summonitem(1775); 
 		#:: Ding!
 		quest::ding();
