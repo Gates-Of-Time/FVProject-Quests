026c915be9b23a3c5e10865b08f875d73d2d99ac
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:31:14 -0400

    Update Linnleu_Brackmar.pl
    Commas, tabs, don't eat items.

--- paineel/Linnleu_Brackmar.pl
+++ paineel/Linnleu_Brackmar.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Greetings. $name.  My wares are few but valuable. including books of research into the less widely known arts of necromancy.  Perhaps you should have a look.");
-    }
+		quest::say("Greetings, $name.  My wares are few but valuable, including books of research into the less widely known arts of necromancy.  Perhaps you should have a look.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
