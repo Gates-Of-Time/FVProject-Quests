b5ab4c454d5f72be2f7e341e08aa242812878413
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:31:44 -0400

    Update Taria_Clayspinner.pl
    Commas, tabs, refactor for consistency, don't eat items.

--- paineel/Taria_Clayspinner.pl
+++ paineel/Taria_Clayspinner.pl
@@ -1,8 +1,13 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("Hello. $name.  I make the finest statuettes in all the land.  Each one depicts a power on Norrath.  I have some of the most realistic and lifelike statues.  I only take [special orders] as they are rather time-consuming to make.");
-    }
-    elsif($text=~/what special orders/i){
-        quest::say("Well. if you must know. many priests and devout worshipers of Cazic-Thule purchase my statues and pray over them.  I have made many other statuettes of the gods just to practice. on the off chance that a worshiper of another faith might care to order one from me.");
-    }
+	if ($text=~/hail/i) {
+		quest::say("Hello, $name.  I make the finest statuettes in all the land.  Each one depicts a power on Norrath.  I have some of the most realistic and lifelike statues.  I only take [special orders] as they are rather time-consuming to make.");
+	}
+	elsif ($text=~/special orders/i) {
+		quest::say("Well, if you must know. many priests and devout worshipers of Cazic-Thule purchase my statues and pray over them.  I have made many other statuettes of the gods just to practice, on the off chance that a worshiper of another faith might care to order one from me.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
