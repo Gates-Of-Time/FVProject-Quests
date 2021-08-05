bc70a15e88069684b1dc306cfb371253d99e55f7
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:17:27 -0400

    Update Geomar_Hapera.pl
    Don't eat stuff.

--- paineel/Geomar_Hapera.pl
+++ paineel/Geomar_Hapera.pl
@@ -3,3 +3,8 @@ sub EVENT_SAY {
         	quest::say("Oh, hello there, and welcome to my humble store.  We have everything you might need here, for just about anywhere you'd want to go.  Matter of fact, a party supplied themselves for a trip into Old Paineel just moments ago!");
 	}
 }
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
