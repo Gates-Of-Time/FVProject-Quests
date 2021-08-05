8132edb1e844a955cb09b48663f90a2cf83e7bb6
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:21:41 -0400

    Update Leovre_Kieqmarn.pl
    Commas, tabbing, don't eat stuff.

--- paineel/Leovre_Kieqmarn.pl
+++ paineel/Leovre_Kieqmarn.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Good day. $name.  Feel free to look through my inventory of spells. but if you touch anything without first buying it I. will have your skin pulled from your flesh. Slowly.");
-    }
+		quest::say("Good day, $name.  Feel free to look through my inventory of spells, but if you touch anything without first buying it, I will have your skin pulled from your flesh. Slowly.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
