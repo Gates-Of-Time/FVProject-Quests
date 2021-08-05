6b34fc75a4e214faf1dac96f990f528cf3b4737c
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:30:01 -0400

    Update Taralani_Rahnta.pl
    Tabs, commas, don't eat items, adjust dialogue.

--- paineel/Taralani_Rahnta.pl
+++ paineel/Taralani_Rahnta.pl
@@ -1,8 +1,14 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("Greetings. $name.  You have found the only place to purchase liquor in this fine city.  It seems that many of my countrymen do not appreciate liquor as I do.  Fear not if you spill. I have plenty of [skeletal servants] to clean up the mess.");
-    }
-    elsif($text=~/what skeletal servants/i){
-        quest::say("Can't you see. child?  These dead servants of mine are bussing tables and dancing!  They work incredibly cheaply.  Heh.  Don't try to harm them. though. they don't like that very much. Say. would you like to see something interesting?");
-    }
+	if ($text=~/hail/i) {
+		quest::say("Greetings, $name.  You have found the only place to purchase liquor in this fine city.  It seems that many of my countrymen do not appreciate liquor as I do.  Fear not if you spill, I have plenty of [skeletal servants] to clean up the mess.");
+	}
+	elsif ($text=~/skeletal servants/i) {
+		#:: Removed 'Don't try to harm them, though, they don't like that very much. Say, would you like to see something interesting?'
+		quest::say("Can't you see, child?  These dead servants of mine are bussing tables and dancing!  They work incredibly cheaply.  Heh.");  
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
