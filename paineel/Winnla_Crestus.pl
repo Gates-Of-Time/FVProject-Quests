d98f18c00746dcbc430902c8ca5b5c925d65d98a
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:35:09 -0400

    Update Winnla_Crestus.pl
    Commas, tabs, refactor for consistency, don't eat items.

--- paineel/Winnla_Crestus.pl
+++ paineel/Winnla_Crestus.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("By Cazic-Thule. I am going to kill him!  If he keeps up that endless chatter.. Oh. hello there. $name.  Please feel free to peruse my wares.");
-    }
+	if ($text=~/hail/i) {
+		quest::say("By Cazic-Thule. I am going to kill him!  If he keeps up that endless chatter.. Oh, hello there, $name.  Please feel free to peruse my wares.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
