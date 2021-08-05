6d806d922380001f55c31da4b1f731d75dfe0330
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:15:36 -0400

    Update Gorng_Alusnein.pl
    Don't eat stuff.

--- paineel/Gorng_Alusnein.pl
+++ paineel/Gorng_Alusnein.pl
@@ -3,3 +3,8 @@ sub EVENT_SAY {
 	    quest::say("Hello.  Be careful what you say to Iva.  She has been rather touchy lately.  She seems as if she is waiting for something to happen.  What, I know not.");
     }
 }
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
