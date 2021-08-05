5411ef228df30148b1b73e29c4d13ed387f3ac52
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:41:29 -0400

    Update Rallia_Hapera.pl
    Commas, tabs, don't eat items.

--- paineel/Rallia_Hapera.pl
+++ paineel/Rallia_Hapera.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Hello. $name.  Thankyou for coming in and looking at our wares.  We should have just about everything you need if you wish to take a trip.");
-    }
+		quest::say("Hello, $name.  Thank you for coming in and looking at our wares.  We should have just about everything you need if you wish to take a trip.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
