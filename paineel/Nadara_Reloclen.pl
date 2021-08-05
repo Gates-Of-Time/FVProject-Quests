e93b701d98bdc6f3be592fc6e9ee758ebe4e8a9d
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:34:33 -0400

    Update Nadara_Reloclen.pl
    Commas, tabs, don't eat stuff.

--- paineel/Nadara_Reloclen.pl
+++ paineel/Nadara_Reloclen.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Hello there. $name.  Fear not for your valuables.  I have known Marsa for many years and she has not once taken anyone's property.  She is. perhaps. the last honest person in this city.");
-    }
+		quest::say("Hello there, $name.  Fear not for your valuables.  I have known Marsa for many years and she has not once taken anyone's property.  She is, perhaps, the last honest person in this city.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
