0f91cab516c9e49b433115aa074852cec46c2dd6
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:33:16 -0400

    Update Marsa_Folor.pl
    Commas, tabs, don't eat items.

--- paineel/Marsa_Folor.pl
+++ paineel/Marsa_Folor.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Hello there. $name!  Can't take your valuables with you?  Weighing you down. are they. $name?  Leave them with me.  In my capable hands and in the hands of my capable staff. they will be safe from prying eyes and quick hands!");
-    }
+		quest::say("Hello there, $name!  Can't take your valuables with you?  Weighing you down, are they, $name?  Leave them with me.  In my capable hands and in the hands of my capable staff, they will be safe from prying eyes and quick hands!");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
