4932906c6edd9124725a36a99aa50c76f6bf27af
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:18:52 -0400

    Update Keeshla_Levlora.pl
    Commas, don't eat stuff.

--- paineel/Keeshla_Levlora.pl
+++ paineel/Keeshla_Levlora.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Hail and well met. $name.  I am Keeshla Levlora. a resident necromancer of this beautiful city.  If you are a practitioner of the dark arts under our lord Cazic-Thule. know that you are welcome here.");
+	    quest::say("Hail and well met, $name.  I am Keeshla Levlora, a resident necromancer of this beautiful city.  If you are a practitioner of the dark arts under our lord Cazic-Thule, know that you are welcome here.");
     }
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
