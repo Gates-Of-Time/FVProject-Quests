e69e77ef21cdeb1e5a45223de88995aa8fb7e1d8
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:16:00 -0400

    Update Sheras_Milaku.pl
    Commas, tabs, don't eat stuff

--- paineel/Sheras_Milaku.pl
+++ paineel/Sheras_Milaku.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("Hello. $name.  Feel free to peruse my wares but waste not my time with idle browsing if you do not intend to purchase something.");
-    }
+	if ($text=~/hail/i) {
+		quest::say("Hello, $name.  Feel free to peruse my wares but waste not my time with idle browsing if you do not intend to purchase something.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
