f0aeb12e5b3a3d3dfbd8aa891b59a4b2599b492a
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 18:35:14 -0400

    Update Nedorl_Ipabrun.pl
    Commas, tabs, don't eat stuff.

--- paineel/Nedorl_Ipabrun.pl
+++ paineel/Nedorl_Ipabrun.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-	    quest::say("Buy. or leave.  Make up your mind.  I have no time for idle chitchat.");
-    }
+		quest::say("Buy, or leave.  Make up your mind.  I have no time for idle chitchat.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
