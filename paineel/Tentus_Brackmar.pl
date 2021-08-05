7cffc61e37835487ae4b254551b1d4a02b5b992b
TurmoilToad <TurmoilToad@gmail.com>
2021-08-05 19:32:38 -0400

    Update Tentus_Brackmar.pl
    Commas, tabs, don't eat items.

--- paineel/Tentus_Brackmar.pl
+++ paineel/Tentus_Brackmar.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {   
-    if($text=~/hail/i){
-        quest::say("Hey you!  You watch yourself.  These items are priceless.  You break it. you buy it!");
-    }
+	if ($text=~/hail/i) {
+		quest::say("Hey you!  You watch yourself.  These items are priceless.  You break it, you buy it!");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
