5aecd3bce758bcbac062bc5d55349c0907362582
TurmoilToad <TurmoilToad@gmail.com>
2021-08-16 13:31:47 -0400

    Update #LRZoner.pl
    Clean up debug message

--- arena/#LRZoner.pl
+++ arena/#LRZoner.pl
@@ -2,12 +2,10 @@
 
 sub EVENT_SPAWN {
 	#:: Set a proximity 80 units wide by 120 units tall, without proximity say
-	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 60, $z + 60);
+	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40, $z - 60, $z + 60, 0);
 }
 
 sub EVENT_ENTER {
 	#:: Move the player who triggerd the event to 51 - lakerathe at the specified coordinates
 	quest::movepc(51, 2643, 2419, 97);
-	#:: if($status > 100) {
-		#:: $client->Message(15, "DEBUG: You have entered a proximity!");
 }
