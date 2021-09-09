6fcd093ae1ff384027ebc1b2239c46f32737b73a
TurmoilToad <TurmoilToad@gmail.com>
2021-09-09 08:24:43 -0400

    Update The_Spiroc_Lord.pl
    Add timer

--- airplane/The_Spiroc_Lord.pl
+++ airplane/The_Spiroc_Lord.pl
@@ -1,3 +1,15 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		#:: Create a timer 'dt' that triggers every 45 seconds
+		quest::settimer("dt", 45);
+	}
+	else {
+		#:: Stop the timer 'dt' from triggering
+		quest::stoptimer("dt");
+	}
+}
+
 sub EVENT_TIMER {
 	#:: Match timer 'dt'
 	if ($timer eq "dt") {
