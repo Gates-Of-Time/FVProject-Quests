7acc65fb8edeae9dac80f58b0d97f758ff978b97
TurmoilToad <TurmoilToad@gmail.com>
2021-09-09 08:21:24 -0400

    Update #Sister_of_the_Spire.pl
    Add timer, refactor for consistency

--- airplane/#Sister_of_the_Spire.pl
+++ airplane/#Sister_of_the_Spire.pl
@@ -1,10 +1,13 @@
-sub EVENT_DEATH_COMPLETE {
-	#:: Key a data bucket
-	$key = sirran_status;
-	#:: Set a value of '7' for 900 seconds (15 min)
-	quest::set_data($key, "7", 900);
-	#:: Spawn one and only one The Plane of Sky >> Sirran_the_Lunatic (71058), without grid or guild war, at the current location
-	quest::unique_spawn(71058, 0, 0, $x, $y, $z);
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		#:: Create a timer 'dt' that triggers every 45 seconds
+		quest::settimer("dt", 45);
+	}
+	else {
+		#:: Stop the timer 'dt' from triggering
+		quest::stoptimer("dt");
+	}
 }
 
 sub EVENT_TIMER {
@@ -19,4 +22,14 @@ sub EVENT_TIMER {
 			$npc->CastSpell(982, $target);
 		}
 	}
+}
+
+
+sub EVENT_DEATH_COMPLETE {
+	#:: Key a data bucket
+	$key = sirran_status;
+	#:: Set a value of '7' for 900 seconds (15 min)
+	quest::set_data($key, "7", 900);
+	#:: Spawn one and only one The Plane of Sky >> Sirran_the_Lunatic (71058), without grid or guild war, at the current location
+	quest::unique_spawn(71058, 0, 0, $x, $y, $z);
 }
