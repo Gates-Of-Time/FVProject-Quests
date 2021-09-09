627eedda464b77e8524a4c6393301384df5c062c
TurmoilToad <TurmoilToad@gmail.com>
2021-09-08 20:52:29 -0400

    Update Fright.pl
    Remove Iksar Broodling spawn, add DT logic

--- fearplane/Fright.pl
+++ fearplane/Fright.pl
@@ -1,3 +1,29 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		#:: Create a timer 'dt' that triggers every 45 seconds
+		quest::settimer("dt", 45);
+	}
+	else {
+		#:: Stop the timer 'dt' from triggering
+		quest::stoptimer("dt");
+	}
+}
+
+sub EVENT_TIMER {
+	#:: Match timer 'dt'
+	if ($timer eq "dt") {
+		$target = $npc->GetHateTop();
+		if ($target->IsPet()) {
+			$owner = $target->GetOwnerID();
+			$npc->CastSpell(982, $owner);
+		}
+		else {
+			$npc->CastSpell(982, $target);
+		}
+	}
+}
+
 sub EVENT_SIGNAL {
 	#:: Match a signal '1' sent from /fearplane/Cazic_Thule.pl
 	if ($signal == 1) {
@@ -16,8 +42,8 @@ sub EVENT_SIGNAL {
 	}
 }
 
-sub EVENT_DEATH_COMPLETE {
-	#:: Spawn a The Plane of Fear >> Iksar_broodling (72105) at the current location 75 percent of the time
-	my $Random = quest::ChooseRandom(72105, 72105, 72105, 0);
-	quest::spawn2($Random, 0, 0, $x, $y, $z, $h);
-}
+#:: sub EVENT_DEATH_COMPLETE {
+#:: 	#:: Spawn a The Plane of Fear >> Iksar_broodling (72105) at the current location 75 percent of the time
+#:: 	my $Random = quest::ChooseRandom(72105, 72105, 72105, 0);
+#:: 	quest::spawn2($Random, 0, 0, $x, $y, $z, $h);
+#:: }
