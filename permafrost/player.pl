9bac6d368a8dbab4d6795d76e6c56732b89669ab
TurmoilToad <TurmoilToad@gmail.com>
2021-09-10 21:25:15 -0400

    Update player.pl
    Long ways from Soldungb!

--- permafrost/player.pl
+++ permafrost/player.pl
@@ -1,7 +1,7 @@
 sub EVENT_TARGET_CHANGE {
 	if ($ulevel > 52) {
 		if ($status < 80) {
-			if ($client->GetTarget()->GetCleanName() eq "Lord Nagafen") {
+			if ($client->GetTarget()->GetCleanName() eq "Lady Vox") {
 				quest::ze(0, "I will not fight you, but I will banish you!");
 				#:: Move the client to Everfrost Peaks at the specified coordinates
 				$client->MovePC(30, -7024, 2020, -60.7, 0);
