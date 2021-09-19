6f69df278b8f3b8eaf97d216f55fbcdebf8f6a2a
TurmoilToad <TurmoilToad@gmail.com>
2021-09-19 11:00:19 -0400

    Update Keeper_of_Souls.pl
    Adjust DT

--- airplane/Keeper_of_Souls.pl
+++ airplane/Keeper_of_Souls.pl
@@ -6,6 +6,24 @@ sub EVENT_SPAWN {
 sub EVENT_COMBAT {
 	#:: Match combat state 1 - entered combat
 	if ($combat_state == 1) {
+		$key = $npc->GetCleanName() . "-dt";
+		#:: Match if the key does not exist
+		if (!quest::get_data($key)) {
+			$target = $npc->GetHateTop();
+			if ($target->IsPet()) {
+				$owner = $target->GetOwnerID();
+				$Client = $entity_list->GetClientByID($owner);
+				$Client->BuffFadeAll();
+				$npc->CastSpell(982, $owner);
+				quest::set_data($key, 1, 44);
+			}
+			else {
+				$Client = $entity_list->GetClientByID($target);
+				$Client->BuffFadeAll();
+				$npc->CastSpell(982, $target);
+				quest::set_data($key, 1, 44);
+			}
+		}
 		#:: Create a timer 'dt' that triggers every 45 seconds
 		quest::settimer("dt", 45);
 	}
@@ -23,13 +41,23 @@ sub EVENT_TIMER {
 	}
 	#:: Match timer 'dt'
 	elsif ($timer eq "dt") {
-		$target = $npc->GetHateTop();
-		if ($target->IsPet()) {
-			$owner = $target->GetOwnerID();
-			$npc->CastSpell(982, $owner);
-		}
-		else {
-			$npc->CastSpell(982, $target);
+		$key = $npc->GetCleanName() . "-dt";
+		#:: Match if the key does not exist
+		if (!quest::get_data($key)) {
+			$target = $npc->GetHateTop();
+			if ($target->IsPet()) {
+				$owner = $target->GetOwnerID();
+				$Client = $entity_list->GetClientByID($owner);
+				$Client->BuffFadeAll();
+				$npc->CastSpell(982, $owner);
+				quest::set_data($key, 1, 44);
+			}
+			else {
+				$Client = $entity_list->GetClientByID($target);
+				$Client->BuffFadeAll();
+				$npc->CastSpell(982, $target);
+				quest::set_data($key, 1, 44);
+			}
 		}
 	}
 }
