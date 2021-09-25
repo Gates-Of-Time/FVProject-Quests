79cd712b9ca3c874cb47f483a299192b79186282
TurmoilToad <TurmoilToad@gmail.com>
2021-09-25 13:13:21 -0400

    Update Keeper_of_Souls.pl

--- airplane/Keeper_of_Souls.pl
+++ airplane/Keeper_of_Souls.pl
@@ -18,9 +18,8 @@ sub EVENT_COMBAT {
 				quest::set_data($key, 1, 44);
 			}
 			else {
-				$Client = $entity_list->GetClientByID($target);
-				$Client->BuffFadeAll();
-				$npc->CastSpell(982, $target);
+				$target->BuffFadeAll();
+				$npc->CastSpell(982, $target->GetID());
 				quest::set_data($key, 1, 44);
 			}
 		}
@@ -53,9 +52,8 @@ sub EVENT_TIMER {
 				quest::set_data($key, 1, 44);
 			}
 			else {
-				$Client = $entity_list->GetClientByID($target);
-				$Client->BuffFadeAll();
-				$npc->CastSpell(982, $target);
+				$target->BuffFadeAll();
+				$npc->CastSpell(982, $target->GetID());
 				quest::set_data($key, 1, 44);
 			}
 		}
