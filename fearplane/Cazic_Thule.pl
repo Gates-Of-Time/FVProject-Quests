c77938f0b581a588d852d1b551561c19cf78d2da
TurmoilToad <TurmoilToad@gmail.com>
2021-09-25 13:11:01 -0400

    Update Cazic_Thule.pl

--- fearplane/Cazic_Thule.pl
+++ fearplane/Cazic_Thule.pl
@@ -81,9 +81,8 @@ sub EVENT_TIMER {
 				quest::set_data($key, 1, 44);
 			}
 			else {
-				$Client = $entity_list->GetClientByID($target);
-				$Client->BuffFadeAll();
-				$npc->CastSpell(982, $target);
+				$target->BuffFadeAll();
+				$npc->CastSpell(982, $target->GetID());
 				quest::set_data($key, 1, 44);
 			}
 		}
@@ -132,9 +131,8 @@ sub EVENT_COMBAT {
 				quest::set_data($key, 1, 44);
 			}
 			else {
-				$Client = $entity_list->GetClientByID($target);
-				$Client->BuffFadeAll();
-				$npc->CastSpell(982, $target);
+				$target->BuffFadeAll();
+				$npc->CastSpell(982, $target->GetID());
 				quest::set_data($key, 1, 44);
 			}
 		}
