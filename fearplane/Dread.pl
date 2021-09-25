0e23f855f809268e4149e83ba311f2047fb6d5f1
TurmoilToad <TurmoilToad@gmail.com>
2021-09-25 12:26:01 -0400

    Update Dread.pl

--- fearplane/Dread.pl
+++ fearplane/Dread.pl
@@ -2,18 +2,23 @@ sub EVENT_COMBAT {
 	#:: Match combat state 1 - entered combat
 	if ($combat_state == 1) {
 		$key = $npc->GetCleanName() . "-dt";
+		quest::gmsay("key");
 		#:: Match if the key does not exist
 		if (!quest::get_data($key)) {
 			$target = $npc->GetHateTop();
+			quest::gmsay("$target");
 			if ($target->IsPet()) {
 				$owner = $target->GetOwnerID();
+				quest::gmsay("$owner");
 				$Client = $entity_list->GetClientByID($owner);
+				quest::gmsay("$Client");
 				$Client->BuffFadeAll();
 				$npc->CastSpell(982, $owner);
 				quest::set_data($key, 1, 44);
 			}
 			else {
 				$Client = $entity_list->GetClientByID($target);
+				quest::gmsay("$Client");
 				$Client->BuffFadeAll();
 				$npc->CastSpell(982, $target);
 				quest::set_data($key, 1, 44);
