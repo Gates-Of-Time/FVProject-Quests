9fc519408f2aa14806e2b8ac706f05b899f7f5ac
TurmoilToad <TurmoilToad@gmail.com>
2021-10-19 09:34:31 -0400

    Update default.pl

--- rivervale/default.pl
+++ rivervale/default.pl
@@ -1,12 +1,3 @@
 sub EVENT_SPAWN {
-	quest::settimer("die", 5);
-}
-
-sub EVENT_TIMER {
-	if ($timer eq "die") {
-		quest::say("Oh no, Inny killed me")
-		plugin::SetAnim("dead");
-		quest::sethp(1);
-		quest::stoptimer("die");
-	}
+	plugin::SetAnim("dead");
 }
