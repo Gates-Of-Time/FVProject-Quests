d2c9490f2a7829f8f36df9f542859a1848910536
TurmoilToad <TurmoilToad@gmail.com>
2021-10-19 09:19:48 -0400

    Update default.pl

--- rivervale/default.pl
+++ rivervale/default.pl
@@ -1,4 +1,10 @@
 sub EVENT_SPAWN {
-	quest::setanim($mobid, 3);
-	quest::sethp(1);
+	quest::settimer("die", 5);
+}
+
+sub EVENT_TIMER {
+	if ($timer eq "die") {
+		quest::setanim($mob->GetNPCTypeID(), 3);
+		quest::sethp(1);
+	}
 }
