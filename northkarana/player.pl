8b08fc72fd35bb7f5dd84a1e682eb20c6f477d07
TurmoilToad <TurmoilToad@gmail.com>
2021-11-29 09:07:19 -0500

    Update player.pl
    Fix item ID

--- northkarana/player.pl
+++ northkarana/player.pl
@@ -1,5 +1,6 @@
 sub EVENT_PLAYER_PICKUP {
-	if ($picked_up_id == 147636) { # Ear of Corn (147636)
+	#:: Match a 148000 - Ear of Corn
+	if ($picked_up_id == 148000) {
 		if (quest::istaskactivityactive(273, 1)) {
 			$client->Message(15, "yay, some corn!  But now what?");
 			quest::updatetaskactivity(273, 1, 1);
