f319988054a7c63c4caa207aa53a8fe1188ca9e0
TurmoilToad <TurmoilToad@gmail.com>
2021-11-30 16:19:07 -0500

    Update player.pl

--- northkarana/player.pl
+++ northkarana/player.pl
@@ -2,11 +2,11 @@ sub EVENT_PLAYER_PICKUP {
 	#:: Match a 148000 - Ear of Corn
 	if ($picked_up_id == 148000) {
 		if (quest::istaskactivityactive(273, 1)) {
-			$client->Message(15, "yay, some corn!  But now what?");
+			$client->Message(0, "Yay, some corn!  But now what?  Better find someone who knows what they are doing...");
 			quest::updatetaskactivity(273, 1, 1);
 		}
 		else {
-			$client->Message(15, "yay, some corn!");
+			$client->Message(15, "Yay, some corn!");
 		}
 	}
 }
