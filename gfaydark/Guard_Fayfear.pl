92f1f8488d5fd4b0843b0fb8992764413ea452e4
TurmoilToad <TurmoilToad@gmail.com>
2021-12-23 19:38:32 -0500

    Update Guard_Fayfear.pl
    name001

--- gfaydark/Guard_Fayfear.pl
+++ gfaydark/Guard_Fayfear.pl
@@ -1,6 +1,9 @@
 sub EVENT_COMBAT {
-	#:: Match if combat is true (fighting)
-	if ($combat_state == 1) {
-		quest::say("Time to die $name.");
+	if($combat_state == 1){
+	my $cur_target = $npc->GetHateTop();
+		if($cur_target) {
+		my $target_name = $cur_target->GetCleanName();
+		quest::say("Time to die $target_name!");
+		}
 	}
 }
