89790030c55853d90a16e7c77c538b7409a9e831
TurmoilToad <TurmoilToad@gmail.com>
2021-12-23 19:50:14 -0500

    Create Guard_Plage.pl
    Noudess

---
+++ felwitheb/Guard_Plage.pl
@@ -0,0 +1,22 @@
+sub EVENT_COMBAT {
+	if ($combat_state == 1) {
+	my $cur_target = $npc->GetHateTop();
+		if ($cur_target) {
+			my $target_name = $cur_target->GetCleanName();
+			quest::say("Time to die $target_name!");
+			$random = quest::ChooseRandom(1..4);
+			if ($random == 1) {
+				quest::say("It's $race" . "s like you who have ruined your own lands.  You'll not ruin mine!");
+			}
+			elsif ($random == 2) {
+				quest::say("Filthy $race" . "s like you must die!");
+			}
+			elsif ($random == 3) {
+				quest::say("$race" . "s like you are better left dead than alive!");
+			}
+			elsif ($random == 4) {
+				quest::say("$race" . "s have no place in our realm!");
+			}
+		}
+	}
+}
