412d9cf04d8e7f08f1d832a45b0232bd3324b902
TurmoilToad <TurmoilToad@gmail.com>
2021-12-23 20:10:36 -0500

    Update Tynkale.pl
    Refactor for consistency

--- felwithea/Tynkale.pl
+++ felwithea/Tynkale.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Set up a proximity, 50 units across
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Set up a proximity, 100 units across and 50 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 25, $z +25, 0);
 }
 
 sub EVENT_ENTER {
@@ -16,17 +14,17 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Hail noble, $name!  Can you be of [service to the Clerics of Tunare] or are you not from our order?");
 	}
-	if ($text=~/trades/i) {
+	elsif ($text=~/trades/i) {
 		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
 		#:: Give item 51121 - Tradeskill Basics: Volume I
 		quest::summonitem(51121);
 	}
-	if ($text=~/second book/i) {
+	elsif ($text=~/second book/i) {
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
 		#:: Give item 51122 - Tradeskill Basics: Volume II
 		quest::summonitem(51122);
 	}
-	if ($text=~/service to the clerics of tunare/i) {
+	elsif ($text=~/service to the clerics of tunare/i) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("That is good. Prove yourself, then.  Are you [new to Felwithe] or have I been conversing with a [veteran of the good fight]?");
@@ -36,7 +34,7 @@ sub EVENT_SAY {
 			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
 		}
 	}
-	if ($text=~/new to felwithe/i) {
+	elsif ($text=~/new to felwithe/i) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("Then stand at attention when I speak.  I want you to venture to Kelethin and seek out Tandan Nybright.  He is an old member of ours.  He had some hardships and he fell from Tunare's grace, yet he is still a member of our family.  He will be the one who reeks of elven wine.  Greet him by name.  We are worried for him.");
@@ -46,7 +44,7 @@ sub EVENT_SAY {
 			quest::say("When you have furthered your service to the Paladins of Tunare, we shall make conversation.");
 		}
 	}
-	if ($text=~/veteran of the good fight/i) {
+	elsif ($text=~/veteran of the good fight/i) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("I command you to venture to Innothule and seek a beast called Jojongua, a name penned by the trolls.  It is said to be ten feet tall and nothing but fury.  Some say it resembles a kobold, but how can that be?  You bring this beast's hide to me and then I will believe you to be a veteran.  Beware!  Innothule is a very dangerous place even for a noble veteran.");
@@ -59,35 +57,41 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18781 -  A Tattered Note
+	#:: Match a 18781 -  A Tattered Note
 	if (plugin::takeItems(18781 => 1)) {
 		quest::say("Greetings. young paladin!  I am Master Tynkale of the Clerics of Tunare.  Here, we shall teach and train you in the skills needed to defeat our evil and diseased enemies.  Take this, our guild tunic - it will help protect you.  Once you are ready to begin your training please make sure that you see Seria Woodwind, she can assist you in experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
 		#:: Give item 13591 - Used Gold Training Tunic*
 		quest::summonitem(13591);
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of xp
+		#:: Set factions
+		quest::faction(226, 100); 		#:: + Clerics of Tunare
+		quest::faction(279, 100); 		#:: + King Tearis Thex
+		quest::faction(5001, 75); 		#:: + Anti-mage
+		#:: Grant a small amount of experience
 		quest::exp(100);
-		#:: Set faction
-		quest::faction(226,100); 	#:: + Clerics of Tunare
-		quest::faction(279,100); 	#:: + King Tearis Thex
-		quest::faction(5001,75); 		#:: + Anti-mage
 	}
-	#:: Turn in for 13351 -  A Very Large Pelt
-	if (plugin::takeItems(13351 => 1)) {
+	#:: Match a 13351 -  A Very Large Pelt
+	elsif (plugin::takeItems(13351 => 1)) {
 		#:: Match if faction is better than indifferent
 		if ($faction < 5) {
 			quest::say("So you have proven yourself to be a great slayer of beasts. Now it is time to prove yourself to be an asset to the Crown. You are to meet a man named Tolon Nurbyte, he will be at the local inn. Go to him and repeat the phrase, 'The glory of the Mother shines bright.' I can say no more.");
 			#:: Ding!
 			quest::ding();
-			#:: Give a small amount of xp
-			quest::exp(150);
 			#:: Set faction
-			quest::faction(226,10); 		#:: + Clerics of Tunare
-			quest::faction(279,10); 	#:: + King Tearis Thex
-			quest::faction(5001,10); 		#:: + Anti-mage
+			quest::faction(226, 10); 		#:: + Clerics of Tunare
+			quest::faction(279, 10);	 	#:: + King Tearis Thex
+			quest::faction(5001, 10); 		#:: + Anti-mage
+			#:: Grant a small amount of experience
+			quest::exp(100);
+		}
+		else {
+			#:: made up
+			quest::say("I have no use for this item, $name.  You can have it back.");
+			#:: Give a 13351 - A Very Large Pelt
+			quest::summonitem(13351);
 		}
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
