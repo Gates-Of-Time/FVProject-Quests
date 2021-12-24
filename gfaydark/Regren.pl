4b3439c4ac1a02d9c31d62b743dada584ef2d339
TurmoilToad <TurmoilToad@gmail.com>
2021-12-23 20:55:20 -0500

    Update Regren.pl
    Refactor for consistency, factions by Noudess

--- gfaydark/Regren.pl
+++ gfaydark/Regren.pl
@@ -1,8 +1,6 @@
 sub EVENT_SPAWN {
-	#:: Set up a proximity, 100 units across
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
+	#:: Create a proximity, 100 units across by 50 units tall, without proximity say
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 25, $z + 25, 0);
 }
 
 sub EVENT_ENTER {
@@ -16,12 +14,12 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Welcome, warrior! Show the Emerald Warriors your mettle and bring me a ruined wolf pelt, some bat fur, some bone chips, and a spiderling eye from the depths of Greater Faydark. If you succeed, my admiration and a reward will be yours. To battle!");
 	}
-	if ($text=~/trades/i) {
+	elsif ($text=~/trades/i) {
 		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
 		#:: Give a 51121 - Tradeskill Basics : Volume I
 		quest::summonitem(51121);
 	}
-	if ($text=~/second book/i) {
+	elsif ($text=~/second book/i) {
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
 		#:: Give a 51122 - Tradeskill Basics : Volume II
 		quest::summonitem(51122);
@@ -36,15 +34,15 @@ sub EVENT_ITEM {
 		quest::summonitem(13533);
 		#:: Ding!
 		quest::ding();
+		#:: Set factions
+		quest::faction(326, 100);		#:: + Emerald Warriors
+		quest::faction(270, -15);		#:: - Indigo Brotherhood
+		quest::faction(325, 10);		#:: + Merchants of Felwithe
+		quest::faction(276, 10);		#:: + Kelethin Merchants
 		#:: Grant a small amount of experience
 		quest::exp(100);
-		#:: Set factions
-		quest::faction(326,100);		#:: + Emerald Warriors
-		quest::faction(270,-15);	#:: - Indigo Brotherhood
-		quest::faction(325,10);		#:: + Merchants of Felwithe
-		quest::faction(276,10);		#:: + Kelethin Merchants
 	}
-	#:: Match 13073 - Bone Chips, 13782 - Ruined Wolf Pelt, 13253 - Spiderling Eye, 13069 - Bat Fur
+	#:: Match a 13073 - Bone Chips, a 13782 - Ruined Wolf Pelt, a 13253 - Spiderling Eye, and a 13069 - Bat Fur
 	if (plugin::takeItems(13073 => 1, 13782 => 1, 13253 => 1, 13069 => 1)) {
 		#:: Match if faction is amiably or better
 		if ($faction <= 4) {
@@ -53,22 +51,22 @@ sub EVENT_ITEM {
 			quest::summonitem(quest::ChooseRandom(10018, 10016, 10015, 6014, 10005, 2124, 2119, 2122, 2115, 2113, 5043, 5045, 5042, 6012));
 			#:: Ding!
 			quest::ding();
+			#:: Set factions
+			quest::faction(326, 1);		#:: + Emerald Warriors
+			quest::faction(270, -1);	#:: - Indigo Brotherhood
+			quest::faction(276, 1);		#:: + Kelethin Merchants
+			quest::faction(325, 1);		#:: + Merchants of Felwithe
 			#:: Grant a small amount of experience
 			quest::exp(500);
 			#:: Create a hash for storing cash - 20 to 150cp
 			my %cash = plugin::RandomCash(20,150);
 			#:: Grant a random cash reward
 			quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
-			#:: Set factions
-			quest::faction(326,10);		#:: + Emerald Warriors
-			quest::faction(270,-10);	#:: - Indigo Brotherhood
-			quest::faction(276,10);		#:: + Kelethin Merchants
-			quest::faction(325,10);		#:: + Merchants of Felwithe
 		}
 		else {
 			quest::say("I will not aid beings like you.");
 		}
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
