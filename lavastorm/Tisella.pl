ea48beba3143ae29f43ca9cf6eed693a9be6aec7
TurmoilToad <TurmoilToad@gmail.com>
2022-01-08 18:59:40 -0500

    Create Tisella.pl

---
+++ lavastorm/Tisella.pl
@@ -0,0 +1,15 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Natural selection at work.");
+	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My comrades will avenge my death.");
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
