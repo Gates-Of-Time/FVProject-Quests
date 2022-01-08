53c82aa4171e369f4954fb8d2495e164865dc994
TurmoilToad <TurmoilToad@gmail.com>
2022-01-08 12:34:33 -0500

    Update Guard_Valon.pl
    Emotes

--- commons/Guard_Valon.pl
+++ commons/Guard_Valon.pl
@@ -82,3 +82,14 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Stand where you are, scum!!");
+	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("Your name shall be added to the most wanted list of the Freeport Militia!!");
+}
