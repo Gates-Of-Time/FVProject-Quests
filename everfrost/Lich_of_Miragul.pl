7f21e8dfb194c1d05bf4bec3a9416a50c0739f23
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 20:43:28 -0500

    Update Lich_of_Miragul.pl
    Refactor

--- everfrost/Lich_of_Miragul.pl
+++ everfrost/Lich_of_Miragul.pl
@@ -5,15 +5,13 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Match 19072 - Miragul's Phylactery
-	if (plugin::check_handin(\%itemcount, 19072 => 1)) {
+	#:: Match a 19072 - Miragul's Phylactery
+	if (plugin::takeItems(19072 => 1)) {
 		#:: Spawn Everfrost Peaks >> Miragul (30094), Grid 0, Guildwar 0, Current X, Current Y, Current Z, Current Heading
 		quest::spawn2(30094, 0, 0, $x, $y, $z, $h);
 		#:: Despawn with timer
 		quest::depop_withtimer();
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
-
-# Converted to Perl by SS
