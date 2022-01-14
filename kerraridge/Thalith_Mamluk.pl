9e3329cea5afb941f87ea9493b25d1ae8a250ddf
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 21:27:27 -0500

    Update Thalith_Mamluk.pl
    Refactor

--- kerraridge/Thalith_Mamluk.pl
+++ kerraridge/Thalith_Mamluk.pl
@@ -1,23 +1,23 @@
 sub EVENT_SAY {
-	if ($text=~/Hail/i) {
+	if ($text=~/hail/i) {
 		quest::say("Wha?  Rarrrr!  Thalith try to fish but with [rrrats] and you botherring him, he cannot concentrate on water!  Go away, let Thalith fish!");
 	}
-	if ($text=~/rats/i) {
+	elsif ($text=~/rats/i) {
 		quest::say("Rats. Yes. Small, furry, brown, with teeth. They come, bite my paws, steal my bait, chew on lines and rods. They nasty little beasts and easy to kill but me not able to leave this position or fish get away. Errr. Maybe you keep the rats away? You kill baddest rat, Sharptooth. You bring me his tooth. I be very happy.");
 	}
 }
 
 sub EVENT_ITEM {
 	#:: Match a 6347 - Sharp Tooth
-	if (plugin::check_handin(\%itemcount, 6347 => 1)) {
+	if (plugin::takeItems(6347 => 1)) {
 		quest::say("You.. You kill the rats? Errr. Thalith thanks you. Here. Take this. It's good luck charm I've had for years.");
 		#:: Give a 1061 - Fishbone Necklace
 		quest::summonitem(1061);
 		#:: Ding!
 		quest::ding();
 		#:: Set factions
-		quest::faction(382,10);	# + Kerra Isle
+		quest::faction(382, 10);	# + Kerra Isle
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
