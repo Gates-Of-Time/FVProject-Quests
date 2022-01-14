c048913fcf0625459189de6ae6ec27fe431d5e48
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 20:38:34 -0500

    Update Kirran_Mirrah.pl
    Refactor

--- kerraridge/Kirran_Mirrah.pl
+++ kerraridge/Kirran_Mirrah.pl
@@ -1,8 +1,9 @@
 sub EVENT_ITEM {
-	#:: Match a Smelly Sealed Bag
-	if (plugin::check_handin(\%itemcount, 9968 => 1)) { 
+	#:: Match a 9968 - Smelly Sealed Bag
+	if (plugin::takeItems(9968 => 1)) { 
 		quest::say("Ahhhhhhhh! I can't believe that you ---- ahhhhhhhh! Die you, you, you evil evil heretic!");
 		quest::attack($name);
 	}
-	plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
