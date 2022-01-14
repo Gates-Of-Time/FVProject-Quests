af1db6204a32a618a2ddaa0d8b914046a5c41a92
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 21:11:20 -0500

    Update Turgan.pl
    Refactor

--- lakerathe/Turgan.pl
+++ lakerathe/Turgan.pl
@@ -1,22 +1,19 @@
-#BeginFile: Turgan.pl
-# Part of the quest for Boots of the Reliant
-
 sub EVENT_SAY {
-  if($text=~/Hail/i) {
-    quest::say("Hail!  What brings you here?");
-  }
-  if($text=~/icon of the reliant/i) {
-    quest::say("Where did you hear... I don't know what you are talking about.  Begone!  Back to Kithicor with you!");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hail!  What brings you here?");
+	}
+	elsif ($text=~/icon of the reliant/i) {
+		quest::say("Where did you hear... I don't know what you are talking about.  Begone!  Back to Kithicor with you!");
+	}
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 18145 => 1)) { #Turgan's Note
-    quest::say("I am that Turgan, but I am no longer worthy of this title.  Take this in hopes that you become a better steward than I was.");
-    quest::summonitem(19002); #Icon of the Reliant
-  }
-  else {
-    quest::say("I have no need for this, $name.");
-  }
+	#:: Match a 18145 - Turgan's Note
+	if (plugin::takeItems(18145 => 1)) {
+		quest::say("I am that Turgan, but I am no longer worthy of this title.  Take this in hopes that you become a better steward than I was.");
+		#:: Give a 19002 - Icon of the Reliant
+		quest::summonitem(19002);
+	}	
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-#EndFile: lakerathe - Turgan.pl (51092)
