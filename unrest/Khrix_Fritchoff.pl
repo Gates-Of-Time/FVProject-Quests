0fd042aa6eacbcdd29a164a98ff240c06a20e507
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 21:15:13 -0500

    Update Khrix_Fritchoff.pl
    Refactor

--- unrest/Khrix_Fritchoff.pl
+++ unrest/Khrix_Fritchoff.pl
@@ -6,21 +6,21 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 10400 -  Greater Lighstone
-	if (plugin::check_handin(\%itemcount, 10400 => 1)) {
+	#:: Match a 10400 -  Greater Lighstone
+	if (plugin::takeItems(10400 => 1)) {
 		quest::say("At last I may leave this place!! Good luck destroying my abomination, you will surely need it against my most powerful yet uncontrolable creation!");
-		#:: Spawns Khrixs Abomination
-		quest::spawn2(63111,0,0,60,622,20,0);
-		#:: Give a small amount of xp
-		quest::exp(1000);
 		#:: Ding!
 		quest::ding();
-		#:: Set faction
-		quest::faction(238,1); 		#:: + Dark Reflection
-		quest::faction(245,-1); 		#:: - Eldritch Collective
-		quest::faction(255,-1); 	#:: - Gem Choppers
-		quest::faction(240,-1); 		#:: - Deep Muses
+		#:: Set factions
+		quest::faction(238, 1); 		#:: + Dark Reflection
+		quest::faction(245, -1); 		#:: - Eldritch Collective
+		quest::faction(255, -1); 		#:: - Gem Choppers
+		quest::faction(240, -1); 		#:: - Deep Muses
+		#:: Grant a small amount of experience
+		quest::exp(1000);
+		#:: Spawns a The Estate of Unrest >> Khrix_Abomination (63111) at the specified coordinates
+		quest::spawn2(63111, 0, 0, 60, 622, 20, 0);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
