4b1833aa438cbea6e4198dde280e9d7bedc1c3f4
TurmoilToad <TurmoilToad@gmail.com>
2022-01-13 21:09:13 -0500

    Update Bryndin_McMill.pl
    Refactor

--- everfrost/Bryndin_McMill.pl
+++ everfrost/Bryndin_McMill.pl
@@ -1,21 +1,19 @@
-# Magic Elixir for the Warriors quest
-
 sub EVENT_ITEM {
-	#:: Match 13242 - One Quarter of Elixir
-	if (plugin::check_handin(\%itemcount, 13242 => 1)) {
+	#:: Match a 13242 - One Quarter of Elixir
+	if (plugin::takeItems(13242 => 1)) {
 		quest::say("Ahhh!! I feel warm all over. Thanks. You should go give Arnis McLish a drink of this elixir. Last time I saw him he was headed toward Blackburrow.");
 		#:: Give a 13243 - One Half of Elixir
 		quest::summonitem(13243);
+		#:: Ding!
 		quest::ding();
 		#:: Set factions
-		quest::faction(328,1);	#:: Merchants of Halas
-		quest::faction(327,1);	#:: Shaman of Justice
-		quest::faction(311,1);	#:: Steel Warriors
-		quest::faction(320,1);	#:: Wolves of the North
+		quest::faction(328, 1); 	#:: Merchants of Halas
+		quest::faction(327, 1); 	#:: Shaman of Justice
+		quest::faction(311, 1); 	#:: Steel Warriors
+		quest::faction(320, 1); 	#:: Wolves of the North
+		#:: Grant a small amount of experience
 		quest::exp(150);
 	}
 	#:: Return unused items
-	plugin::return_items(\%itemcount);
+	plugin::returnUnusedItems();
 }
-
-# Converted to Perl by SS
