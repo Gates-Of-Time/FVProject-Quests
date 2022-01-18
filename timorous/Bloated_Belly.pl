ec59ce67ab0a45e928fcb0a5230664c01d944db3
TurmoilToad <TurmoilToad@gmail.com>
2022-01-17 22:12:02 -0500

    Update Bloated_Belly.pl

--- timorous/Bloated_Belly.pl
+++ timorous/Bloated_Belly.pl
@@ -1,6 +1,6 @@
 sub EVENT_SPAWN {
 	#:: Create a proximity, 20 units across, 20 units tall, without proximity say
-	quest::set_proximity($x - 10, $x + 10, $y - 30, $y + 20, $z - 10, $z + 10, 0);
+	quest::set_proximity($x - 50, $x + 50, $y - 30, $y + 30, $z - 10, $z + 10, 0);
 }
 
 sub EVENT_ENTER {
