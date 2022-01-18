fdffed30ce83c0aeab34b378d0128293504cf480
TurmoilToad <TurmoilToad@gmail.com>
2022-01-17 20:01:09 -0500

    Create Barrel_Barge.pl

---
+++ timorous/Barrel_Barge.pl
@@ -0,0 +1,9 @@
+sub EVENT_SPAWN {
+	#:: Create a proximity, 20 units across, 20 units tall, without proximity say
+	quest::set_proximity($x - 10, $x + 10, $y - 10, $y + 10, $z - 10, $z + 10, 0);
+}
+
+sub EVENT_ENTER {
+	#:: Move client to Oasis of Marr (oasis)
+	quest::movepc(37, -778.40, 879.15, -0.65, 389.5);
+}
