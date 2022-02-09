01504afbfa8dd5132cdf9a39e59ae495217a4923
TurmoilToad <TurmoilToad@gmail.com>
2022-02-09 09:10:40 -0500

    Update Trooper_Mozo.pl
    Refactor for consistency, ding

--- fieldofbone/Trooper_Mozo.pl
+++ fieldofbone/Trooper_Mozo.pl
@@ -1,32 +1,31 @@
-# items: 13073
 sub EVENT_SAY {
-  if($text=~/hail/i){
-   quest::emote("look's upon you smugly.");
-   quest::say("And what are you, some sort of adventurer? Bah!! You will be shaken by the first undead you come across. Wetting your pant's you shall be. Ha Ha!! I doubt you could even [kill the decaying skeletons]!");
-  }
-  #Bone Chips (fieldofbone)(START)
-  if($text=~/kill the decaying skeletons/i){
-   quest::say("You kill skeletons?!! I think not!! They will bounce you with no trouble at all. Ten silvers says you cannot collect four bone chips!! When Velious melts!! Hah!");
- }
+	if ($text=~/hail/i) {
+		quest::emote("look's upon you smugly.  'And what are you, some sort of adventurer? Bah!! You will be shaken by the first undead you come across. Wetting your pant's you shall be. Ha Ha!! I doubt you could even [kill the decaying skeletons]!'");
+	}
+	elsif ($text=~/kill the decaying skeletons/i) {
+		quest::say("You kill skeletons?!! I think not!! They will bounce you with no trouble at all. Ten silvers says you cannot collect four bone chips!! When Velious melts!! Hah!");
+	}
 }
 
 sub EVENT_ITEM {
-  #Handin: 4x Bone chips
-  if(plugin::check_handin(\%itemcount, 13073 => 4)){
-   #Bone Chips (fieldofbone)(END)
-   quest::emote("gasp's in astonishment.");
-   quest::say("Wha...? This!! But... I... Why you... I oughta.. All right!! A bet is a bet. Here you go. A few silver piece's for your achievement. What?!! You thought I would give you ten? Fat chance! Now get lost or I will haul you in for impersonating a froglok.");
-   quest::faction(441,1); # +Legion of Cabilis
-   quest::faction(440,1);  # +Cabilis Residents
-   quest::faction(445,1); # +Scaled Mystics
-   quest::faction(442,1);  # +Crusaders of Greenmist
-   quest::faction(444,1); # +Swifttails
-   my $a = quest::ChooseRandom(1,2,3,4,5,6,7);
-   quest::givecash(0,$a,0,0);
-   quest::exp(250);
-  } 
-  plugin::return_items(\%itemcount);
+	#:: Match four 13073 - Bone Chips
+	if (plugin::takeItems(13073 => 4)) {
+		quest::emote("gasp's in astonishment.  'Wha...? This!! But... I... Why you... I oughta.. All right!! A bet is a bet. Here you go. A few silver piece's for your achievement. What?!! You thought I would give you ten? Fat chance! Now get lost or I will haul you in for impersonating a froglok.'");
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(441, 1);		#:: + Legion of Cabilis
+		quest::faction(440, 1);		#:: + Cabilis Residents
+		quest::faction(445, 1);		#:: + Scaled Mystics
+		quest::faction(442, 1);		#:: + Crusaders of Greenmist
+		quest::faction(444, 1);		#:: + Swifttails
+		#:: Grant a small amount of experience
+		quest::exp(250);
+		#:: Create a hash for storing cash - 40 to 100cp
+		my %cash = plugin::RandomCash(40, 100);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+	} 
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-
-#EOF
