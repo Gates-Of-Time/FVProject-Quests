429f0e723ec92b5ae8f357e90df114789f20b13a
TurmoilToad <TurmoilToad@gmail.com>
2022-02-09 08:30:27 -0500

    Update #an_iksar_manslayer.pl
    Refactor for consistency

--- fieldofbone/#an_iksar_manslayer.pl
+++ fieldofbone/#an_iksar_manslayer.pl
@@ -1,12 +1,6 @@
-# This is to spawn a lesser spirit upon an_iksar_marauder's death for Shaman epic 1.0
+#:: Shaman epic 1.0
 
 sub EVENT_DEATH_COMPLETE {
-  my $x = $npc->GetX();
-  my $y = $npc->GetY();
-  my $z = $npc->GetZ();
-  my $h = $npc->GetHeading();
-  quest::spawn2(78211,0,0,$x,$y,$z,$h); # NPC: a_lesser_spirit
+	#:: Spawn a The Field of Bone >> a_lesser_spirit (78211) at the current location
+	quest::spawn2(78211, 0, 0, $x, $y, $z, $h);
 }
-
-
-#Submitted by: Jim Mills
