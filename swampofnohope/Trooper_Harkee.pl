1cf50fdffd30d632b17d9b51b82ad4f7b462ca89
TurmoilToad <TurmoilToad@gmail.com>
2022-02-09 20:59:04 -0500

    Update Trooper_Harkee.pl
    Refactor for consistency, don't eat items

--- swampofnohope/Trooper_Harkee.pl
+++ swampofnohope/Trooper_Harkee.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("If you find yourself deep in the swamp and starving. try foraging.  I am sure you'll find something to eat.  If not. there are always froglok tongues."); }
+	if ($text=~/hail/i) {
+		quest::say("If you find yourself deep in the swamp and starving. try foraging.  I am sure you'll find something to eat.  If not. there are always froglok tongues.");
+	}
 }
-#END of FILE Zone:swampofnohope  ID:83065 -- Trooper_Harkee 
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
