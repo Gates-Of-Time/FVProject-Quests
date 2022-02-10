50c96d3b3200744289512a899acc6777167d80d0
TurmoilToad <TurmoilToad@gmail.com>
2022-02-09 21:06:32 -0500

    Update Trooper_Lorgen.pl
    Refactor for consistency, don't eat items

--- swampofnohope/Trooper_Lorgen.pl
+++ swampofnohope/Trooper_Lorgen.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("It is assumed that the froglok stronghold is well defended. None of the troopers sent from this garrison to find it have ever returned.  At least. not in one piece."); }
+	if ($text=~/hail/i) {
+		quest::say("It is assumed that the froglok stronghold is well defended. None of the troopers sent from this garrison to find it have ever returned.  At least. not in one piece.");
+	}
 }
-#END of FILE Zone:swampofnohope  ID:83066 -- Trooper_Lorgen 
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
