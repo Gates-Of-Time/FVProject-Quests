5ebb6e160dd48f69f5cd52afec4cf8374767075e
TurmoilToad <TurmoilToad@gmail.com>
2022-02-12 09:56:51 -0500

    Update an_Iksar_knight.pl
    Refactor for consistency, fix hand ins, FEC

--- warslikswood/an_Iksar_knight.pl
+++ warslikswood/an_Iksar_knight.pl
@@ -1,34 +1,58 @@
-#This iksar knight is actually Grizzle, for the fourth quest in the Greenmist line, The Test of the Zealot.
-# items: 12492, 12493, 12494, 6026, 12496
-
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("wipes the sweat from his forehead and spits some flarefire seeds on the ground near your feet. 'Whew!! Good to be back and safe at the Cabilis Gate. The troopers will protect us.'");
-  }
- elsif($text=~/chalp diagram/i){ #Test of the Zealot (Greenmist Quest 4/8)
-  quest::emote("spits out some flarefire seeds. 'Great!! Sent to retrieve the Chalp diagram. I will give them to you, but I want you to do me a little favor first. I want you to [retrieve some spells] I lost. They are incorrectly scribed and useless, but I yearn for their return nonetheless.'");
-  }
- elsif($text=~/retrieve some spells/i){ #Test of the Zealot (Greenmist Quest 4/8)
-  quest::emote("spits flarefire seeds on you. 'Oops!! Sorry. Yeah!! You go to Chalp and get two incorrectly scribed spells which they grabbed from me. The third one, I threw in a river crossing by Chalp. It was in a bottle and probably drifted way down the river. Bring me all three and also a shovel and I will make it worth your while.'");
-  }
- elsif($text=~/flarefire seeds/i){ #Test of the Zealot (Greenmist Quest 4/8)
-  quest::emote("spits some flarefire seeds on your face. 'What? Interested in flarefire seeds? There you go. Ha!!'");
-  }
+	if ($text=~/hail/i) {
+		quest::emote("wipes the sweat from his forehead and spits some flarefire seeds on the ground near your feet. 'Whew!! Good to be back and safe at the Cabilis Gate. The troopers will protect us.'");
+	}
+	elsif ($text=~/chalp diagram/i) {
+		quest::emote("spits out some flarefire seeds. 'Great!! Sent to retrieve the Chalp diagram. I will give them to you, but I want you to do me a little favor first. I want you to [retrieve some spells] I lost. They are incorrectly scribed and useless, but I yearn for their return nonetheless.'");
+	}
+	elsif ($text=~/retrieve some spells/i) {
+		quest::emote("spits flarefire seeds on you. 'Oops!! Sorry. Yeah!! You go to Chalp and get two incorrectly scribed spells which they grabbed from me. The third one, I threw in a river crossing by Chalp. It was in a bottle and probably drifted way down the river. Bring me all three and also a shovel and I will make it worth your while.'");
+	}
+	elsif ($text=~/flarefire seeds/i) {
+		quest::emote("spits some flarefire seeds on your face. 'What? Interested in flarefire seeds? There you go. Ha!!'");
+	}
 }
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 12492 => 1, 12493 => 1, 12494 => 1, 6026 => 1)){ #Test of the Zealot (Greenmist Quest 4/8)
-  quest::emote("accidently swallows a mouthful of flarefire seeds. 'ACCCKK!!  I can't believe you did it!! Here is the Chalp diagram.'");
-  quest::summonitem(12496); # Item: Chalp Diagram
-  quest::exp(1000);
-  quest::ding();
-  quest::faction(442,20); # Faction: Crusaders of Greenmist
-  quest::faction(441,10); # Faction: Legion of Cabilis
-  quest::depop_withtimer();
- }
- else{ 
- quest::emote("spits flarefire seeds on your chest. 'Listen up!! I asked for the misscribed spells; Fear, Lifetap and Gate, and also, a shovel.'");
- }
-  plugin::return_items(\%itemcount);
+	#:: Match a 12492 - Misscribed Lifetap, a 12493 - Misscribed Gate, a 12494 - Scroll in a Bottle, and a 6026 - Shovel
+	if (plugin::takeItems(12492 => 1, 12493 => 1, 12494 => 1, 6026 => 1)) {
+		quest::emote("accidently swallows a mouthful of flarefire seeds. 'ACCCKK!!  I can't believe you did it!! Here is the Chalp diagram.'");
+		#:: Give a 12496 - Chalp Diagram
+		quest::summonitem(12496);
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(442, 1);		#:: + Crusaders of Greenmist
+		quest::faction(441, 1);		#:: + Legion of Cabilis
+		#:: Grant a moderate amount of experience
+		quest::exp(1000);
+		#:: Depop with spawn timer active
+		quest::depop_withtimer();
+ 	}
+	#:: Match a 12492 - Misscribed Lifetap
+	elsif (plugin::takeItems(12492 => 1)) {
+		quest::emote("spits flarefire seeds on your chest. 'Listen up!! I asked for the misscribed spells; Fear, Lifetap and Gate, and also, a shovel.'");
+		#:: Give a 12492 - Misscribed Lifetap
+		quest::summonitem(12492);
+	}
+	#:: Match a 12493 - Misscribed Gate
+	elsif (plugin::takeItems(12493 => 1)) {
+		quest::emote("spits flarefire seeds on your chest. 'Listen up!! I asked for the misscribed spells; Fear, Lifetap and Gate, and also, a shovel.'");
+		#:: Give a 12493 - Misscribed Gate
+		quest::summonitem(12493);
+	}
+	#:: Match a 12494 - Scroll in a Bottle
+	elsif (plugin::takeItems(12494 => 1)) {
+		quest::emote("spits flarefire seeds on your chest. 'Listen up!! I asked for the misscribed spells; Fear, Lifetap and Gate, and also, a shovel.'");
+		#:: Give a 12494 - Scroll in a Bottle
+		quest::summonitem(12494);
+	}
+	#:: Match a 6026 - Shovel
+	elsif (plugin::takeItems(6026 => 1)) {
+		quest::emote("spits flarefire seeds on your chest. 'Listen up!! I asked for the misscribed spells; Fear, Lifetap and Gate, and also, a shovel.'");
+		#:: Give a 6026 - Shovel
+		quest::summonitem(6026);
+	}	
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-#Submitted by: Jim Mills
