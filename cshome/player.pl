590e5769ff0b9314c9e35277d67de4bb66bfdbda
TurmoilToad <TurmoilToad@gmail.com>
2022-02-11 20:47:21 -0500

    Update player.pl

--- cshome/player.pl
+++ cshome/player.pl
@@ -1,17 +1,4 @@
 sub EVENT_COMMAND {
   if ($text=~/1/i) {
-    PerlPacket::SendTo(LiveOP_MOTD, $client);
-    my $packet = "LiveOP_MOTD";
-    PerlPacket::SendTo($packet, $client);
-
-    
-  }
-  elsif ($text=~/2/i) {
-      my $packet = 0x01b2;
-    PerlPacket::SendTo($packet, $client);
-  }
-  elsif ($text=~/3/i) {
-     my $packet = "0x01b2";
-    PerlPacket::SendTo($packet, $client);
-   }
+    PerlPacket::SendTo(OP_MOTD, $client);
 }
