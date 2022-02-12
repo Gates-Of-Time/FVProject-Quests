efabde2c1a40a21de6a3e0c5c0e1a5e7e8c73000
TurmoilToad <TurmoilToad@gmail.com>
2022-02-11 20:53:52 -0500

    Update player.pl

--- cshome/player.pl
+++ cshome/player.pl
@@ -1,9 +1,8 @@
 sub EVENT_COMMAND {
-  if ($text=~/1/i) {
-    PerlPacket::SendTo(OP_MOTD, $client);
-   }
-   if ($text=~/2/i) {
-    PerlPacket::SetnTo(OP_Save, $client);
-   }
-   
+	if ($text=~/1/i) {
+		PerlPacket::SendTo(MOTD, $client);
+	}
+	elsif ($text=~/2/i) {
+		PerlPacket::SetnTo(Save, $client);
+	}
 }
