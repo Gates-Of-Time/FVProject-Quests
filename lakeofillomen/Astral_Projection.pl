90dbba853f403f10e9e128c24576c63bd0d55755
TurmoilToad <TurmoilToad@gmail.com>
2022-02-12 10:10:56 -0500

    Update Astral_Projection.pl
    Refactor for consistency

--- lakeofillomen/Astral_Projection.pl
+++ lakeofillomen/Astral_Projection.pl
@@ -1,26 +1,27 @@
-# items: 1687
+#:: Monk Epic 1.0
+
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("ignores you.");
- }
+	if ($text=~/hail/i) {
+		quest::emote("ignores you.");
+	}
 }
 
 sub EVENT_ITEM {
- if((plugin::check_handin(\%itemcount, 1687 => 1)) && ($ulevel>=46) && ($class=="Monk")){
-   my $x = $npc->GetX();
-   my $y = $npc->GetY();
-   my $z = $npc->GetZ();
-   my $h = $npc->GetHeading();
-  # Monk Epic 1.0
-  quest::depop();
-  # Spawn: Vorash
-  quest::spawn2(85211,0,0,$x,$y,$z,$h); # NPC: Vorash
-  # Spawn: Deep
-  quest::spawn2(85223,0,0,$x,$y,$z,$h); # NPC: Deep
- }
-  else {
-    quest::say("I have no use for this, $name.");
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match if user's class is monk
+	if ($class eq "Monk") {
+		#:: Match if user's level is over 45
+		if ($ulevel > 45) {
+			#:: Match a 1687 - Eye of Kaiaren
+			if (plugin::takeItems(1687 => 1)) {
+				#:: Spawn NPC:  Lake of Ill Omen >> Vorash (85211) at the current location
+				quest::spawn2(85211, 0, 0, $x, $y, $z, $h);
+				#:: Spawn NPC:  Lake of Ill Omen >> Deep (85223) at the current location
+				quest::spawn2(85223, 0, 0, $x, $y, $z, $h);
+				#:: Depop with spawn timer active
+				quest::depop_withtimer();
+			}
+		}
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-# End of File
