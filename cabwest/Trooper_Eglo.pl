98f7c28597eab3300effcecc1d5219c4790a7c4f
TurmoilToad <TurmoilToad@gmail.com>
2022-02-13 13:42:31 -0500

    Update Trooper_Eglo.pl

--- cabwest/Trooper_Eglo.pl
+++ cabwest/Trooper_Eglo.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("The waters of Cabilis are teeming with life.  A good fisherman could make heavy coin catching fish."); }
+	if ($text=~/hail/i) {
+		quest::say("The waters of Cabilis are teeming with life.  A good fisherman could make heavy coin catching fish.");
+	}
 }
-#END of FILE Zone:cabwest  ID:5099 -- Trooper_Eglo 
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
