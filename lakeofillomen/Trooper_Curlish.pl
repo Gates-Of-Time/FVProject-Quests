50ab5156486fb67f553acbc3bd3b3c6171c729e8
TurmoilToad <TurmoilToad@gmail.com>
2022-02-13 18:40:00 -0500

    Update Trooper_Curlish.pl
    Add missing dialogues, refactor for consistency.

--- lakeofillomen/Trooper_Curlish.pl
+++ lakeofillomen/Trooper_Curlish.pl
@@ -1,8 +1,29 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Have you tried to drink from the lake?  It is poisoned with salt!  If you plan on venturing to the far side of the lake, you had best visit one of the Kloks to purchase a Water Extractor."); }
+	if ($text=~/hail/i) {
+		quest::say("Have you tried to drink from the lake?  It is poisoned with salt!  If you plan on venturing to the far side of the lake, you had best visit one of the Kloks to purchase a Water Extractor.");
+	}
 }
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
 
-#END of FILE Zone:lakeofillomen  ID:5096 -- Trooper_Curlish 
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("Fear the blades of the Iksar!!");
+	}
+}
 
+sub EVENT_NPC_SLAY {
+	quest::say("Another victory for the Legion of Cabilis.");
+}
+
+sub EVENT_SLAY {
+	quest::say("Another victory for the Legion of Cabilis.");
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You have not won. Consider yourself wanted by the Legion of Cabilis.");
+}
