141ea130563519612f7ed41644ea46bf0f8d3f60
TurmoilToad <TurmoilToad@gmail.com>
2022-02-13 18:36:16 -0500

    Update Deep.pl
    Refactor for consistency, fix proximity, don't eat items

--- lakeofillomen/Deep.pl
+++ lakeofillomen/Deep.pl
@@ -1,22 +1,22 @@
+#:: Monk Epic 1.0 
+
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("ignores you.");
- }
+	if ($text=~/hail/i) {
+		quest::emote("ignores you.");
+	}
 }
 
 sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  # Set proximity
-  quest::set_proximity($x-50,$x+50,$y-50,$y+50);
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
- if($class eq "Monk"){
-  # Monk Epic 1.0
-  quest::attack($name);
- }
+	if ($class eq "Monk") {
+		quest::attack($name);
+	}
 }
 
-
-# End of File
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
