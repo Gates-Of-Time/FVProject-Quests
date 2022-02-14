75eff1392e9a7e4933efc167e8fca2b6c5c12673
TurmoilToad <TurmoilToad@gmail.com>
2022-02-13 20:23:46 -0500

    Update Xenevorash.pl
    Refactor for consistency, set proximity correctly, don't eat items

--- lakeofillomen/Xenevorash.pl
+++ lakeofillomen/Xenevorash.pl
@@ -1,18 +1,22 @@
+#:: Monk Epic 1.0
+
 sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  # Set proximity
-  quest::set_proximity($x-50,$x+50,$y-50,$y+50);
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
- if(($class eq "Monk") && ($ulevel>=46)){
-  # Monk Epic 1.0
-  quest::attack($name);
- }
+	if ($ulevel > 45) {
+		if ($class eq "Monk") {
+			quest::attack($name);
+		}
+	}
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::shout("Grraaaagghhhh!! Not..Possible!");
+	quest::shout("Grraaaagghhhh!! Not..Possible!");
 }
-# End of File
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
