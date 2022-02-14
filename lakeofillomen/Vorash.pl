069097b6a890497b8219c740ec1cb1e47511eaf3
TurmoilToad <TurmoilToad@gmail.com>
2022-02-13 20:11:01 -0500

    Update Vorash.pl
    Refactor for consistency, correctly set proximity, don't eat items.

--- lakeofillomen/Vorash.pl
+++ lakeofillomen/Vorash.pl
@@ -1,27 +1,30 @@
+#:: Monk Epic 1.0
+
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("ignores you.");
- }
+	if ($text=~/hail/i) {
+		quest::emote("ignores you.");
+	}
 }
 
 sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  # Set proximity
-  quest::set_proximity($x-50,$x+50,$y-50,$y+50);
+	quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50, $z - 50, $z + 50, 0);
 }
 
 sub EVENT_ENTER {
- if(($class eq "Monk") && ($ulevel>=46)) {
-  # Monk Epic 1.0
-  quest::attack($name);
- }
+	if ($ulevel > 45) {
+		if ($class eq "Monk") {
+			quest::attack($name);
+		}
+	}
 }
 
 sub EVENT_DEATH_COMPLETE {
-  # MonkEpic 1.0
-  quest::say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
-  # Spawn: Xenevorash
-  quest::spawn(85208,0,0,-542,-807,79.12);
+	quest::say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
+	#:: Spawn one and only one Lake of Ill Omen >> Xenevorash (85208) at the specified coordinates
+	quest::unique_spawn(85208, 0, 0, -542, -807, 79.12, 0);
 }
-# End of File
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
