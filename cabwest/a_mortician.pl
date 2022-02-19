aa3a5d7f1d3607d3d0c37b850d795cc477ae518d
TurmoilToad <TurmoilToad@gmail.com>
2022-02-19 12:48:49 -0500

    Update a_mortician.pl
    Refactor for consistency

--- cabwest/a_mortician.pl
+++ cabwest/a_mortician.pl
@@ -1,13 +1,15 @@
 sub EVENT_SAY {
- if($text=~/hail/i){
-  quest::emote("stares at you with hollow eye sockets. As he opens his jaw to speak, you are overcome by the foul smell of a thousand rotted corpses. 'What is your desire, master?'");
-  }
- elsif($text=~/mortar and pestle/i){ #The Penance quest
-  quest::emote("moves with a mixture of creaks and clicks and produces a grotesque looking mortar and pestle set. He places the items in your hands and motions indicating you dismissal. You can feel the vile power of your god moving throughout the material that makes up the two items.");
-  quest::summonitem(17092); # Item: Grotesque Mortar and Pestle
-  }
+	if ($text=~/hail/i) {
+		quest::emote("stares at you with hollow eye sockets. As he opens his jaw to speak, you are overcome by the foul smell of a thousand rotted corpses. 'What is your desire, master?'");
+	}
+	elsif ($text=~/mortar and pestle/i) {
+		quest::emote("moves with a mixture of creaks and clicks and produces a grotesque looking mortar and pestle set. He places the items in your hands and motions indicating you dismissal. You can feel the vile power of your god moving throughout the material that makes up the two items.");
+		#:: Give a 17092 - Grotesque Mortar and Pestle
+		quest::summonitem(17092);
+	}
 }
 
 sub EVENT_ITEM {
-  plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
