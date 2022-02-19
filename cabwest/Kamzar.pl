332f54360f86f3a0666085430937d1e97f400f60
TurmoilToad <TurmoilToad@gmail.com>
2022-02-19 12:35:03 -0500

    Update Kamzar.pl
    Refactor for consistency, don't eat items.

--- cabwest/Kamzar.pl
+++ cabwest/Kamzar.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Much beauty can be found upon the skins of our enemies.  And... even upon that of our own allies?"); }
+	if ($text=~/hail/i) {
+		quest::say("Much beauty can be found upon the skins of our enemies.  And... even upon that of our own allies?");
+	}
 }
-#END of FILE Zone:cabwest  ID:2891 -- Kamzar 
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
