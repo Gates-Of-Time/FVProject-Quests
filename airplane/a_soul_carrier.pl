b47461cc39526bd7c25fd70246d66e7f5f5cba03
TurmoilToad <TurmoilToad@gmail.com>
2022-02-25 13:32:51 -0500

    Update a_soul_carrier.pl
    Simplify

--- airplane/a_soul_carrier.pl
+++ airplane/a_soul_carrier.pl
@@ -1,25 +1,7 @@
-my $entid1;
-my $entid2;
-my $mob1;
-my $mob2;
-my $mobnpc1;
-my $mobnpc2;
-
-sub EVENT_DEATH {
-        $key = $npc->GetID() . "-killed";
-        if (!quest::get_data($key)) {
-		#:: Spawn one and only one The Plane of Sky >> Nottap_Ekim (71111), with no grid or guildwar, at the specified location
-  		quest::unique_spawn(71111, 0, 0, 0, 0, 0, 0);
-                #:: Spawn a The Plane of Sky >> an_essence_carrier (71070)
-                $entid1 = quest::spawn2(71070, 0, 0, $x + 10, $y, $z, $h);
-                $mob1 = $entity_list->GetMobID($entid1);
-                $mobnpc1 = $mob1->CastToNPC();
-                $mobnpc1->AddToHateList($npc->GetHateTop());
-                #:: Spawn a The Plane of Sky >> an_essence_carrier (71070)
-                $entid2 = quest::spawn2(71070, 0, 0, $x - 10, $y, $z, $h);
-                $mob2 = $entity_list->GetMobID($entid2);
-                $mobnpc2 = $mob2->CastToNPC();
-                $mobnpc2->AddToHateList($npc->GetHateTop());
-                quest::set_data($key, 1, 30);
-        }
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn one and only one The Plane of Sky >> Nottap_Ekim (71111), with no grid or guildwar, at the specified location
+	quest::unique_spawn(71111, 0, 0, 0, 0, 0, 0);
+	#:: Spawn two The Plane of Sky >> an_essence_carrier (71070), with no grid or guildwar, offset from the current location
+	quest::spawn2(71070, 0, 0, $x + 10, $y, $z, $h);
+	quest::spawn2(71070, 0, 0, $x - 10, $y, $z, $h);
 }
