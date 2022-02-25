9643b961238268a407e5127c539f3f9947291204
TurmoilToad <TurmoilToad@gmail.com>
2022-02-25 13:44:24 -0500

    Update an_essence_harvester.pl
    Simplify

--- airplane/an_essence_harvester.pl
+++ airplane/an_essence_harvester.pl
@@ -1,23 +1,5 @@
-my $entid1;
-my $entid2;
-my $mob1;
-my $mob2;
-my $mobnpc1;
-my $mobnpc2;
-
-sub EVENT_DEATH {
-        $key = $npc->GetID() . "-killed";
-        if (!quest::get_data($key)) {
-                #:: Spawn a The Plane of Sky >> a_soul_harvester (71110)
-                $entid1 = quest::spawn2(71110, 0, 0, $x + 10, $y, $z, $h);
-                $mob1 = $entity_list->GetMobID($entid1);
-                $mobnpc1 = $mob1->CastToNPC();
-                $mobnpc1->AddToHateList($npc->GetHateTop());
-                #:: Spawn a The Plane of Sky >> a_soul_harvester (71110)
-                $entid2 = quest::spawn2(71110, 0, 0, $x - 10, $y, $z, $h);
-                $mob2 = $entity_list->GetMobID($entid2);
-                $mobnpc2 = $mob2->CastToNPC();
-                $mobnpc2->AddToHateList($npc->GetHateTop());
-                quest::set_data($key, 1, 30);
-        }
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn a The Plane of Sky >> a_soul_harvester (71110), with no grid or guildwar set, offset from the current location
+	quest::spawn2(71110, 0, 0, $x + 10, $y, $z, $h);
+	quest::spawn2(71110, 0, 0, $x - 10, $y, $z, $h);
 }
