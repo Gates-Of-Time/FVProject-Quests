2f9509a1bf35307b854268c387ad0c59859575ed
TurmoilToad <TurmoilToad@gmail.com>
2022-02-25 13:45:46 -0500

    Update a_soul_harvester.pl
    Simplify

--- airplane/a_soul_harvester.pl
+++ airplane/a_soul_harvester.pl
@@ -1,8 +1,3 @@
-my $killed;
-my $entid1;
-my $mob1;
-my $mobnpc1;
-
 sub EVENT_SPAWN {
 	#:: Create a timer 'depop' that triggers in 4800 seconds (80 min)
 	quest::settimer("depop", 4800);
@@ -18,14 +13,7 @@ sub EVENT_TIMER {
 	}
 }
 
-sub EVENT_DEATH {
-        $key = $npc->GetID() . "-killed";
-        if (!quest::get_data($key)) {
-		#:: Spawn a The Plane of Sky >> a_soul_tamer (71100)
-		$entid1 = quest::spawn2(71100, 0, 0, $x, $y, $z, $h);
-		$mob1 = $entity_list->GetMobID($entid1);
-		$mobnpc1 = $mob1->CastToNPC();
-		$mobnpc1->AddToHateList($npc->GetHateTop());
-		quest::set_data($key, 1, 30);
-	}
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn a The Plane of Sky >> a_soul_tamer (71100), with no grid or guildwar, at the current location
+	quest::spawn2(71100, 0, 0, $x, $y, $z, $h);
 }
