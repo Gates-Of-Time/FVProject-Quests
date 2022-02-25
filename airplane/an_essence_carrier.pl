3388e5cade9bdc3849daf3d4e4bec53b8ffe1b12
TurmoilToad <TurmoilToad@gmail.com>
2022-02-25 13:42:20 -0500

    Update an_essence_carrier.pl
    Simplify

--- airplane/an_essence_carrier.pl
+++ airplane/an_essence_carrier.pl
@@ -1,10 +1,3 @@
-my $entid1;
-my $entid2;
-my $mob1;
-my $mob2;
-my $mobnpc1;
-my $mobnpc2;
-
 sub EVENT_SPAWN {
 	#:: Create a timer 'depop' that triggers in 4800 seconds (80 min)
 	quest::settimer("depop", 4800);
@@ -20,19 +13,8 @@ sub EVENT_TIMER {
 	}
 }
 
-sub EVENT_DEATH {
-        $key = $npc->GetID() . "-killed";
-        if (!quest::get_data($key)) {
-                #:: Spawn a The Plane of Sky >> an_essence_tamer (71071)
-                $entid1 = quest::spawn2(71071, 0, 0, $x + 10, $y, $z, $h);
-                $mob1 = $entity_list->GetMobID($entid1);
-                $mobnpc1 = $mob1->CastToNPC();
-                $mobnpc1->AddToHateList($npc->GetHateTop());
-                #:: Spawn a The Plane of Sky >> an_essence_tamer (71071)
-                $entid2 = quest::spawn2(71071, 0, 0, $x - 10, $y, $z, $h);
-                $mob2 = $entity_list->GetMobID($entid2);
-                $mobnpc2 = $mob2->CastToNPC();
-                $mobnpc2->AddToHateList($npc->GetHateTop());
-                quest::set_data($key, 1, 30);
-        }
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn two The Plane of Sky >> an_essence_tamer (71071), with no grid or guildwar, offset from the current location
+	quest::spawn2(71071, 0, 0, $x + 10, $y, $z, $h);
+	quest::spawn2(71071, 0, 0, $x - 10, $y, $z, $h);
 }
