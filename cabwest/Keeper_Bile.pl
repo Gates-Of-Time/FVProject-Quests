d931fa101f4f047c368380f37b3153a994d9b632
TurmoilToad <TurmoilToad@gmail.com>
2022-02-26 20:29:59 -0500

    Update Keeper_Bile.pl
    Added correct dialogues, added correct factions rewards, added correct cash rewards, corrected item rewards, refactored for consistency

--- cabwest/Keeper_Bile.pl
+++ cabwest/Keeper_Bile.pl
@@ -1,23 +1,30 @@
-# items: 12739, 12659, 12426
 sub EVENT_SAY {
-   if($text=~/hail/i)   {
-	quest::say("Greetings $name. Lately the creatures of Lake of Ill Omen have be causing problems for us. Are you [interested] in helping me reduce the threat from this creatures?");
-   }
-   if($text=~/interested/i)   {
-	quest::say("Good. Bring me a Brittle Iksar Skull, a Sabertooth Cub Canine, and a Large Scorpion Pincer. And i shall reward you with a spell.");
-   }
+	if ($text=~/hail/i) {
+		quest::emote("bows before you as a gesture of kindness and respect. 'Greetings. I am Bile, Keeper of the Apprentice Ranks. My knowledge is that of the first circle of necromancy. I was once a great necromancer, but now i have a greater calling to protect the words of Kotiz. I also Conduct many tests to ensure the spells are deciphered correctly. I am in need of an apprentice to [collect components].'");
+	}
+	elsif ($text=~/collect components/i) {
+		quest::emote("takes out a tattered scroll and looks upon its words. 'Hmm. Yes. I will need the following; One large scorpion pincer, one sabertooth cub canine and two brittle skulls. Bring these to me and I shall pay you and if I have an overscribed spell, I shall give you that also.'");
+	}
 }
 
 sub EVENT_ITEM {
-  $spellnum = quest::ChooseRandom(15338,15344,15339,15340,15225,15229,15346,15491,15502,15341,15342,15347,15348,15331,15221,15343); #I am not 100 percent sure on this spells list. It is simply everything he sells I gathered from the information available that this is correct. 
-  if (plugin::check_handin(\%itemcount, 12739 => 1, 12659 => 1, 12426 => 1)) {
-    quest::say("Good Work $name. This will surely hold back those vile creatures for some time. Here is your spell.");
-    quest::summonitem($spellnum);
-    quest::exp(10);
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match two 12739 - Brittle Iksar Skull, a 12659 - Large Scorpion Pincer, and a 12426 - Sabertooth Cub Canine
+	if (plugin::takeItems(12739 => 2, 12659 => 1, 12426 => 1)) {
+		quest::emote("tosses the items into a nearby box and reaches into a sack at his feet. He hands you your reward. 'This spell is going to need some major testing. I can always use more of the same components.'");
+		#:: Give a random reward: 15342 - Spell: Locate Corpse, 15348 - Spell: Poison Bolt, 15331 - Spell: Reclaim Energy, 15221 - Spell: Sense the Dead
+		quest::summonitem(quest::ChooseRandom(15342, 15348, 15331, 15221));
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(443, 2);			#:: + Brood of Kotiz
+		quest::faction(441, 1);			#:: + Legion of Cabilis
+		#:: Grant a small amount of experience
+		quest::exp(100);
+		#:: Create a hash for storing cash - 50 to 100cp
+		my %cash = plugin::RandomCash(50, 100);
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper},$cash{silver},$cash{gold},$cash{platinum});
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-#Dialogue is made up since I was not able to find the correct dialogue on Alla's
-#End of File zone:cabwest ID: 82038 -- Keeper Bile
-#By Kovou
