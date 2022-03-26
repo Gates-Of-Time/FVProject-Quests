4369bc93dcf7f6168b4ba23534735ab1d05387b8
TurmoilToad <TurmoilToad@gmail.com>
2022-03-26 10:47:40 -0400

    Update Half_Elf_Maiden.pl
    Add missing dialogue, refactor for consistency

--- cabeast/Half_Elf_Maiden.pl
+++ cabeast/Half_Elf_Maiden.pl
@@ -1,15 +1,20 @@
-#BeginFile cabeast\Half_Elf_Maiden.pl
-#
-#Quest: Tayla Ironforge
-#
-# items: 54026, 18863
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("What do you want? You do not look like the type of person the Baron usually sends to me.");
+	}
+}
 
 sub EVENT_ITEM {
-  if(plugin::check_handin(\%itemcount, 54026 => 1)) { #Klok's Seal
-    quest::say("Oh! thank you! I did not expect to hear of my family ever again. This seal should grant me protection out of the city if I am careful. Please take this note to my father and let him know I am safe!");
-    quest::summonitem(18863); #Sealed Letter
-    quest::exp(2000);
-  }
-  plugin::return_items(\%itemcount);
+	#:: Match a 54026 - Klok's Seal
+	if (plugin::takeItems(54026 => 1)) {
+		quest::say("Oh! thank you! I did not expect to hear of my family ever again. This seal should grant me protection out of the city if I am careful. Please take this note to my father and let him know I am safe!");
+		#:: Give a 18863 - Sealed Letter
+		quest::summonitem(18863);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a moderate amount of experience
+		quest::exp(2000);
+	}
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-#END of FILE Zone:cabeast  ID:Not_Found -- Half Elf Maiden
