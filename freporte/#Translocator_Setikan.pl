e0f4a2461311ef2e6695cc14faf772e689c40db5
TurmoilToad <TurmoilToad@gmail.com>
2022-04-03 11:07:14 -0400

    Create #Translocator_Setikan.pl

---
+++ freporte/#Translocator_Setikan.pl
@@ -0,0 +1,13 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Hello there, $name.  There seem to be some strange problems with the boats in this area.  The Academy of Arcane Sciences has sent a small team of us to investigate them.  If you need to [travel to Ocean of Tears] in the meantime, I can transport you to my companion there.");
+	}
+	elsif ($text=~/travel to ocean of tears/i) {
+		quest::selfcast(2279);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
