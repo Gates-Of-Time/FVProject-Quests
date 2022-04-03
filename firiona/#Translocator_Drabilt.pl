3ca61b7455a709ae271dc1ff53c3436d7569b463
TurmoilToad <TurmoilToad@gmail.com>
2022-04-03 11:40:22 -0400

    Create #Translocator_Drabilt.pl

---
+++ firiona/#Translocator_Drabilt.pl
@@ -0,0 +1,14 @@
+sub EVENT_SAY { 
+	if ($text=~/hail/i) {
+		quest::say("Hello there, $name. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Timorous Deep] in the meantime, I can transport you to my companion there.");
+	}
+	elsif ($text=~/timorous deep/i) {
+		quest::say("Off you go!");
+		quest::selfcast(2292);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
