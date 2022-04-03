eda3d761e36b7fad3d0cd506e72e62fd62a9ae20
TurmoilToad <TurmoilToad@gmail.com>
2022-04-03 17:25:32 -0400

    Update Rinmark.pl
    Refactor for consistency, don't depop in the middle of a fight, don't eat items

--- timorous/Rinmark.pl
+++ timorous/Rinmark.pl
@@ -1,22 +1,34 @@
-#Rinmark is spawned from an iksar master in this zone and is part of the Test of Patience. Please see the iksar master's quest file for details.
-
 sub EVENT_SPAWN {
-  quest::settimer("Depop3",90);
+	#:: Create a timer 'Depop' that triggers every 90 seconds
+	quest::settimer("Depop", 90);
 }
 
-sub EVENT_AGGRO {
-  quest::say("Good, your sense of timing is correct. Now begin! Show me that your training has not been wasted.");
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::stoptimer("Depop");
+		quest::say("Good, your sense of timing is correct. Now begin! Show me that your training has not been wasted.");
+	}
+	elsif ($combat_state == 0) {
+		#:: Create a timer 'Depop' that triggers every 90 seconds
+		quest::settimer("Depop", 90);
+	}
 }
 
 sub EVENT_TIMER {
- if ($timer eq "Depop3") {
-  quest::stoptimer("Depop3");
-  quest::depop();
- }
+	#:: Match timer 'Depop'
+	if ($timer eq "Depop") {
+		quest::stoptimer("Depop");
+		#:: Depop without spawn timer active
+		quest::depop();
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::say("Your ability is strong, may you serve Cazic Thule with all your might.");
+	quest::say("Your ability is strong, may you serve Cazic Thule with all your might.");
 }
-
-#Submitted by: Jim Mills
