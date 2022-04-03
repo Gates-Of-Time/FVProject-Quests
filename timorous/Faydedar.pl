40420e43140b9cdd68dda92de3fd8418dd597bad
TurmoilToad <TurmoilToad@gmail.com>
2022-04-03 16:44:43 -0400

    Update Faydedar.pl
    Refactor for consistency

--- timorous/Faydedar.pl
+++ timorous/Faydedar.pl
@@ -1,5 +1,6 @@
-sub EVENT_SPAWN { 
-if ($entity_list->IsMobSpawnedByNpcTypeID(96073)){
-quest::depop();
-  }
+sub EVENT_SPAWN {
+	#:: Match a Timorous Deep >> #Faydedar (96073) on the entity list
+	if ($entity_list->IsMobSpawnedByNpcTypeID(96073)) {
+		quest::depop();
+	}
 }
