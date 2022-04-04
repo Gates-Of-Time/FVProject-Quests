59a59d181a5c6162aceb505a3ce91e75cbf860b1
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 19:16:19 -0400

    Update Valeron_Dushire.pl
    Add missing quest

--- freportn/Valeron_Dushire.pl
+++ freportn/Valeron_Dushire.pl
@@ -14,40 +14,89 @@ sub EVENT_SAY {
 	elsif ($text=~/stop/i) {
 		quest::say("That would be good, but I doubt you can. We have been trying for years and are unable to get him apart from the militia. If you do the impossible and destroy Sir Lucan, bring me his testimony. Every knight of the Hall of Truth carries a signed testimony. Sir Lucan still holds his.");
 	}
+	elsif ($text=~/him/i) {
+		quest::say("According to this crest on the tunic, you have recovered one of the bones of a lost Paladin [Hero].");
+	}
+	elsif ($text=~/diggs duggan/i) {
+		quest::say("According to this crest on the tunic, you have recovered one of the bones of a lost Paladin [Hero].");
+	}
+	elsif ($text=~/hero/i) {
+		quest::say("His name has been long lost but his legend lives on in every young knight who aspires to continue his fight. He attempted to quell an evil that resided within Kithicor, perverting nature itself. Much to his relief, it was an earthly foe he fought against and not a denizen summoned from the planes of evil. Then, suddenly, he was surrounded by dozens of dark elves. He was captured and taken to the Dead to be experimented upon and had his soul twisted by vile necromantic magics. Then the Tier'Dal cast his remains to the winds. Where they landed, no one knew, until now. You have found one of his bones. I hereby charge you with a Holy Quest to bring this Hero's bones back for a burial with honors. Do you [accept], knight?");
+	}
+	elsif ($text=~/accept/i) {
+		quest::say("Good! I knew you would. It is one of the highest honors one can receive. Your quest will not be easy for I know not where his other bones lay. A spy of ours within Neriak was able to surmise that six of his bones along with his sword and shield were scattered throughout Norrath. You must search far and wide to find the remaining pieces, as you already have one.. Do not give up. It is now your sacred duty. Once you have retrieved all eight pieces, come back to me and inform me that you [have all the bones].");
+	}
+	elsif ($text=~/have all the bones/i) {
+		#:: Match a 6700 - Dusty Old Skull
+		if (plugin::check_hasitem($client, 6700) {
+			#:: Match a 6701 - Dusty Old Leg Bone
+			if (plugin::check_hasitem($client, 6701) {
+				#:: Match a 6702 - Dusty Old Jawbone
+				if (plugin::check_hasitem($client, 6702) {
+					#:: Match a 6703 - Dusty Old Finger Bones
+					if (plugin::check_hasitem($client, 6703) {
+						#:: Match a 6704 - Dusty Old Backbone
+						if (plugin::check_hasitem($client, 6704) {
+							#:: Match a 6705 - Dusty Ribcage
+							if (plugin::check_hasitem($client, 6705) {
+								#:: Match a 6706 - Dusty Broken Shield
+								if (plugin::check_hasitem($client, 6706) {
+									#:: Match a 6707 - Dusty Broken Sword
+									if (plugin::check_hasitem($client, 6707) {
+										quest::say("Excellent! You are indeed worthy to bear the title of Holy Knight! Now, about the coffin. I have spoken with Ping Fuzzlecutter, outside the western gates. Ask him about the coffin and once it is made, bring it to Simon Aldicott of Marr. I shall have Simon meet you by the lake in the Commonlands after Ping gives you the coffin.");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}		
 }
 
 sub EVENT_ITEM {
-	#:: Turn in for 18737 - A tattered note
+	#:: Match a 18737 - A tattered note
 	if (plugin::takeItems(18737 => 1 )) {
 		quest::say("Welcome to the Guild, here's your guild tunic. Now, let's get to work.");
 		#:: Give item 13554 - Faded Purple Tunic*
 		quest::summonitem(13554);
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of experience
-		quest::exp(100);
 		#:: Set factions
 		quest::faction(271, -15);		#:: - Dismal Rage
 		quest::faction(281, 100);		#:: + Knights of Truth
 		quest::faction(330, -15);		#:: - The Freeport Militia
 		quest::faction(362, 20);		#:: + Priests of Marr
 		quest::faction(311, 10);		#:: + Steel Warriors
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
-	#:: Turn in for 18827 - A Testimony of Truth
-	if (plugin::takeItems(18827 => 1 )) {
+	#:: Match a 18827 - A Testimony of Truth
+	elsif (plugin::takeItems(18827 => 1 )) {
 		quest::say("Praise be to Marr!! You have done the impossible!! Sir Lucan is finally sent to the higher courts of the Tribunal. The city now has a chance to prosper. The Hall of Truth has been redeemed and gives you thanks. Take this, it is the Sword of Faith. May you wield it with righteousness. Beware of the remainder of the militia. They will be hunting for your head.");
 		#:: Give item 13947 - Brilliant Sword of Faith
 		quest::summonitem(13947);
 		#:: Ding!
 		quest::ding();
-		#:: Give a small amount of experience
-		quest::exp(100);
 		#:: Set factions
 		quest::faction(271, -15);		#:: - Dismal Rage
 		quest::faction(281, 100);		#:: + Knights of Truth
 		quest::faction(330, -15);		#:: - The Freeport Militia
 		quest::faction(362, 20);		#:: + Priests of Marr
 		quest::faction(311, 10);		#:: + Steel Warriors
+		#:: Grant a small amount of experience
+		quest::exp(100);
+	}
+	#:: Match a 6701 - Dusty Old Leg Bone
+	elsif (plugin::takeItems(6701 => 1)) {
+		quest::say("Hail, adventurer! I... what's this? Where did you get this? Never mind.. I.. I never thought we would find [him]!");
+		#:: Give a 6701 - Dusty Old Leg Bone
+		quest::summonitem(6701);
+		#:: Ding!
+		quest::ding();
+		#:: Grant a small amount of experience
+		quest::exp(100);
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
