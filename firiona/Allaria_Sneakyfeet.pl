5f3fd011d23a10486996d16fdd6a0e81606987e9
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 18:29:52 -0400

    Update Allaria_Sneakyfeet.pl
    Refactor for consistency, correct dialogues, don't eat items.

--- firiona/Allaria_Sneakyfeet.pl
+++ firiona/Allaria_Sneakyfeet.pl
@@ -1,15 +1,18 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("Well hello, $name. What brings you to me? Are you one of the sneaky folk in search of the plans for the Whistler?");
-  }
-  if ($text=~/plans for the whistler/i) {
-    quest::say("I must tell you first off, there was much bloodshed in getting these plans. Three of Rivervale's bravest warrior were killed by Rozzut's brother, Neezee in order to keep the plans out of the hands of the Goblins. Allaria sighs deeply. There was much sorrow over this. The only positive that seemed to come out of the whole ordeal, was the finding of the plans to make the weapon that Rozzut, Chief Zambooz's main assassin uses so effectively. I have now made it my calling to give a copy of these plans to those that are worthy of such knowledge. Do you believe yourself to be worthy of such an honor?");
-  }
-  if ($text=~/i am worthy/i) {
-    quest::say("Very well then. Here are the plans for the weapon that is said to cut holes into the enemy without much effort. Legend has it that if the wind is blowing, your enemy will be whistling after the fight. But they won't be whistling voluntarily.");
-    quest::emote("smiles");
-    quest::say("Good luck to you friend.");
-    #:: Give a 18347 - Rozzuts plans
+	if ($text=~/hail/i) {
+		quest::say("Well hello, $name. What brings you to me? Are you one of the sneaky folk in search of the plans for the Whistler?");
+	}
+	elsif ($text=~/plans for the whistler/i) {
+		quest::say("I must tell you first off, there was much bloodshed in getting these plans. Three of Rivervale's bravest warrior were killed by Rozzut's brother, Neezee in order to keep the plans out of the hands of the Goblins. Allaria sighs deeply. There was much sorrow over this. The only positive that seemed to come out of the whole ordeal, was the finding of the plans to make the weapon that Rozzut, Chief Zambooz's main assassin uses so effectively. I have now made it my calling to give a copy of these plans to those that are worthy of such knowledge. Do you believe yourself to be worthy of such an honor?");
+	}
+	elsif ($text=~/i am worthy/i) {
+		quest::say("Very well then. Here are the plans for the weapon that is said to cut holes into the enemy without much effort. Legend has it that if the wind is blowing, your enemy will be whistling after the fight. But they won't be whistling voluntarily.  Allaria smiles.  Good luck to you friend.");
+		#:: Give a 18347 - Rozzuts plans
 		quest::summonitem(18347);
-  }
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
