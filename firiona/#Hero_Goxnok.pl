d2a6aa5e616edca5b2aefd322a79dc39932afe69
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 18:14:32 -0400

    Update #Hero_Goxnok.pl
    Fix dialogue, tab out

--- firiona/#Hero_Goxnok.pl
+++ firiona/#Hero_Goxnok.pl
@@ -1,14 +1,13 @@
 sub EVENT_SAY {
-  if($text=~/hail/i){
-	  quest::emote("sighs slowly, seeing the confusion on your face.");
-    quest::say("Yes, $name, I am the traitor. What I can gain by giving up this tome outweighs any patriotic obligations I may have once had. That city is dead, it is now time for us to grasp at our own destinies. I now give you two choices, $race. You can walk away and never speak of this to anyone......or you can die. What will it be?");
-  }
+	if ($text=~/hail/i) {
+		quest::emote("sighs slowly, seeing the confusion on your face.  'Yes, $name, I am the traitor. What I can gain by giving up this tome outweighs any patriotic obligations I may have once had. That city is dead, it is now time for us to grasp at our own destinies. I now give you two choices, $race. You can walk away and never speak of this to anyone......or you can die. What will it be?'");
+	}
 }
 
 sub EVENT_AGGRO {  
-  quest::say("Die by the power of Greenmist!!");
+	quest::say("Die by the power of Greenmist!!");
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::say("You shall now be a wanted man.. The Union of Vie shall punish you.");
+	quest::say("You shall now be a wanted man.. The Union of Vie shall punish you.");
 }
