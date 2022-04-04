4a71230b9ce829e2cd2753f53971d95df1f3f55e
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 18:23:51 -0400

    Update #halfling_swashbuckler.pl
    Refactor for consistency, don't eat items

--- firiona/#halfling_swashbuckler.pl
+++ firiona/#halfling_swashbuckler.pl
@@ -1,4 +1,8 @@
 sub EVENT_AGGRO {
-	quest::emote(" performs an acrobatic tumble and unsheathes his foil without pause.");
-	quest::say("Short, stout and deadly am I. Stand and deliver!");
+	quest::emote("performs an acrobatic tumble and unsheathes his foil without pause. Short, stout and deadly am I. Stand and deliver!");
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
