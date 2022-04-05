d6c5422a4943dc5e7a7afbf17d2c85cfd9a10d92
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 22:00:50 -0400

    Update Drake_Mountainstorm.pl
    Refactor for consistency, add missing dialogue, correct syntax

--- firiona/Drake_Mountainstorm.pl
+++ firiona/Drake_Mountainstorm.pl
@@ -1,5 +1,11 @@
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Welcome, welcome. Buy some gems or sell some gems. I have flawless merchandise. I have the finest miners scouring Kunark for it's precious gems. Kunark has many rare minerals. The gnomes even say some of the rocks are from high up in the sky. From an orb floating so far away we cannot see it. Bah!! Those gnomes are insane.");
+	}
+}
+
 sub EVENT_ITEM {
-	#:: Match four 10033 - Fire Emerald
+	#:: Match a 10033 - Fire Emerald
 	if (plugin::takeItems(10033 => 1)) {
 		quest::emote("examines the fire emerald, reaches into his pouch and hands you another. 'There you go, it doesn't look special but it is. If you don't believe me I'll take another look at it. Just remember this special fire emerald only has one purpose!'");
 		#:: Give a 10013 - Special Fire Emerald
@@ -7,12 +13,6 @@ sub EVENT_ITEM {
 		#:: Ding!
 		quest::ding();
 	}
-	else {
-		quest::say("I have no need for this, $name.");
-	}
-
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-
-
