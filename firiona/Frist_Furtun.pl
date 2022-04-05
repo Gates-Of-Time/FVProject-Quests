0666c6f99b7f75e6d3229d29ea39211532a2d27e
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 22:13:06 -0400

    Update Frist_Furtun.pl

--- firiona/Frist_Furtun.pl
+++ firiona/Frist_Furtun.pl
@@ -1,13 +1,13 @@
 sub EVENT_SAY {
-  if ($text=~/Hail/i) {
-    quest::say("Hail to thee, adventurer! I have been sent to this new land by the Clerics of Tunare in search of the new arcane magiks said to exist beyond this outpost. I myself once searched the nearby Outlands, but to go any further would lead to my certain death. Lately, the dangers have proven to be too much for adventurers and ones like myself. Have you also ventured to this land in search of these [new magiks?]");
-  }
-  if ($text=~/new magiks/i) {
-    quest::say("Ahh, wonderful! The more souls who search for these scrolls, the sooner I'll be able to return home and share this knowledge. The magiks are in the form of scrolls held by the strongest creatures of the Outlands. You will probably have the best luck searching in the darkest depths of the dungeons beyond this outpost. You might want to talk to the residents of this outpost for locations of these dreadful places. If you should happen to [have] any duplicate scrolls], please come speak to me. I might be interested in working out a trade for a scroll you do not yet have.");
-  }
-  if ($text=~/I have duplicate scrolls/i){
-    quest::say("Great! The scrolls I am interested in are those of Death Pact, Upheaval, Yaulp IV, and Reckoning. If you bring me any one of these scrolls, I'll let you reach into my bag and pull out one of the four very rare scrolls that have come into my possession.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hail to thee, adventurer! I have been sent to this new land by the Clerics of Tunare in search of the new arcane magiks said to exist beyond this outpost. I myself once searched the nearby Outlands, but to go any further would lead to my certain death. Lately, the dangers have proven to be too much for adventurers and ones like myself. Have you also ventured to this land in search of these [new magiks?]");
+	}
+	elsif ($text=~/new magiks/i) {
+		quest::say("Ahh, wonderful! The more souls who search for these scrolls, the sooner I'll be able to return home and share this knowledge. The magiks are in the form of scrolls held by the strongest creatures of the Outlands. You will probably have the best luck searching in the darkest depths of the dungeons beyond this outpost. You might want to talk to the residents of this outpost for locations of these dreadful places. If you should happen to [have] any duplicate scrolls], please come speak to me. I might be interested in working out a trade for a scroll you do not yet have.");
+	}
+	elsif ($text=~/duplicate scrolls/i) {
+		quest::say("Great! The scrolls I am interested in are those of Death Pact, Upheaval, Yaulp IV, and Reckoning. If you bring me any one of these scrolls, I'll let you reach into my bag and pull out one of the four very rare scrolls that have come into my possession.");
+	}
 }
 
 sub EVENT_ITEM(){
@@ -35,7 +35,7 @@ sub EVENT_ITEM(){
 		#:: Ding!
 		quest::ding();
 	}
-	#:: Match a 19212 - Spell: Reckoning or 19233 - Spell: Upheaval
+	#:: Match a 19212 - Spell: Reckoning
 	elsif (plugin::takeItems(19212 => 1)) {
 		quest::say("Here is the scroll that I promised. We have both gained much knowledge today. I hope to do business with you again soon. Farewell!");   
 		#:: Choose a random 19210 - Spell: Unswerving Hammer, 19224 - Spell: Heroic Bond, 19420 - Spell: Sunskin or 19206 - Spell: Word of Vigor
@@ -51,8 +51,6 @@ sub EVENT_ITEM(){
 		#:: Ding!
 		quest::ding();
 	}
-
-
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
