f95fb47c930293c4e9fc3d32cf74aa4f16de3c53
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 20:48:04 -0400

    Update Brisha_Fyrestone.pl
    Refactor for consistency, don't eat items

--- firiona/Brisha_Fyrestone.pl
+++ firiona/Brisha_Fyrestone.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
-  if ($text=~/special fire emerald/i) {
-    quest::say("Why would you want one of those? Not many people request them, except in special cases. Even you with all your powers can't tell the difference between them. Simply give Drake any fire emerald that you may have and he will make sure you get the special one.");
-  }
+	if ($text=~/special fire emerald/i) {
+		quest::say("Why would you want one of those? Not many people request them, except in special cases. Even you with all your powers can't tell the difference between them. Simply give Drake any fire emerald that you may have and he will make sure you get the special one.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
