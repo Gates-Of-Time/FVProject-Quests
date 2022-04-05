5bcd8d19a75d3f2236ccb6be226a2818a6ec4676
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 21:56:03 -0400

    Update Dom_Pathfinder.pl
    Refactor for consistency, add animation, don't eat items

--- firiona/Dom_Pathfinder.pl
+++ firiona/Dom_Pathfinder.pl
@@ -1,5 +1,12 @@
 sub EVENT_SAY { 
-    if($text=~/Hail/i){
-        quest::say("Whoa!!  Hold up there. adventurer!  Gaze upon the fine packs and containers I have available.  Such fine items could only be crafted by a Fier'Dal tailor.  And lucky you!!  You made it here just as I lowered my prices.  How can you resist these bargains?  I am taking a loss here!!  Last day!!  Buy now!!"); 
-    }
+	if ($text=~/hail/i) {
+		quest::say("Whoa!!  Hold up there, adventurer!  Gaze upon the fine packs and containers I have available.  Such fine items could only be crafted by a Fier'Dal tailor.  And lucky you!!  You made it here just as I lowered my prices.  How can you resist these bargains?  I am taking a loss here!!  Last day!!  Buy now!!");
+		#:: Do animation 49 - Lean Back
+		quest::doanim(49);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
