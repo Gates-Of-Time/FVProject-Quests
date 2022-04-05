6e917e143b2702b69cbbaec12a635f4394f957de
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 22:15:52 -0400

    Update Frothy.pl
    Refactor for consistency, don't eat items

--- firiona/Frothy.pl
+++ firiona/Frothy.pl
@@ -1,6 +1,10 @@
 sub EVENT_SAY { 
-    if($text=~/what jungle city/i){
-        quest::say("I've heard adventurers talk of a city deep in the jungles.  They say the dead walk its streets.  Many have claimed to discover great treasures there as well as to lose many friends.  I also heard it may have a working brew barrel."); 
-    }
+	if ($text=~/what jungle city/i) {
+		quest::say("I've heard adventurers talk of a city deep in the jungles.  They say the dead walk its streets.  Many have claimed to discover great treasures there as well as to lose many friends.  I also heard it may have a working brew barrel.");
+	}
 }
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
