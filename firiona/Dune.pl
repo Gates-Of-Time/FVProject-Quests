54314f600cea9d4bff53adc7d4834a9b843d4ac2
TurmoilToad <TurmoilToad@gmail.com>
2022-04-04 22:04:33 -0400

    Update Dune.pl
    Refactor for consistency, don't eat items

--- firiona/Dune.pl
+++ firiona/Dune.pl
@@ -1,13 +1,20 @@
 sub EVENT_SPAWN {
-  quest::settimer("follow", 1);
+	#:: Set a timer 'follow' that triggers every 10 seconds
+	quest::settimer("follow", 10);
 }
 
 sub EVENT_TIMER{
-  if ($timer eq "follow"){
-    my $OpponentID = 84131;
-    my $getmobbynpctype = $entity_list->GetMobByNpcTypeID($OpponentID);
-    my $follow_target = $getmobbynpctype->GetID();
-    quest::follow($follow_target);
-    quest::stoptimer("follow");
-  }
+	if ($timer eq "follow") {
+		#:: Create a scaler variable to store the npc_type ID of Firiona Vie >> Scout_Bowfist (84131)
+		my $OpponentID = 84131;
+		my $getmobbynpctype = $entity_list->GetMobByNpcTypeID($OpponentID);
+		my $follow_target = $getmobbynpctype->GetID();
+		quest::follow($follow_target);
+		quest::stoptimer("follow");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
