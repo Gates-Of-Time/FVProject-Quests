f5b7ba4e6750d054e2f1eb90d1abeaf98f77b55a
TurmoilToad <TurmoilToad@gmail.com>
2022-04-05 19:30:20 -0400

    Update Gracie_F-Liler.pl
    Refactor for consistency

--- firiona/Gracie_F-Liler.pl
+++ firiona/Gracie_F-Liler.pl
@@ -1,10 +1,10 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("It's so good to see new faces, not to mention more adventurers in this area. On some nights, the sounds that come from the nearby hills will scare even the hardiest of travelers. I volunteered in the name of the Keepers of the Art to help locate any [new spell scrolls] that might surface. Maybe you will have a successful journey and find some of these scrolls yourself.");
-  }
-  if ($text=~/new spell scrolls/i) {
-    quest::say("I have recently come into possession of some of these scrolls. They seem to be very promising in adding to the strengths of our occupation. Some more good news is that I have a few extra of these scrolls. Perhaps you might have or find an extra of your own and be willing to trade? In case you are interested, I am looking for the scrolls Theft of Thought, Color Slant, Cripple, and lastly Dementia. Bring me one of these and I'll make an even trade.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("It's so good to see new faces, not to mention more adventurers in this area. On some nights, the sounds that come from the nearby hills will scare even the hardiest of travelers. I volunteered in the name of the Keepers of the Art to help locate any [new spell scrolls] that might surface. Maybe you will have a successful journey and find some of these scrolls yourself.");
+	}
+	elsif ($text=~/new spell scrolls/i) {
+		quest::say("I have recently come into possession of some of these scrolls. They seem to be very promising in adding to the strengths of our occupation. Some more good news is that I have a few extra of these scrolls. Perhaps you might have or find an extra of your own and be willing to trade? In case you are interested, I am looking for the scrolls Theft of Thought, Color Slant, Cripple, and lastly Dementia. Bring me one of these and I'll make an even trade.");
+	}
 }
 
 sub EVENT_ITEM() {
@@ -47,8 +47,7 @@ sub EVENT_ITEM() {
 		quest::ding();
 		#:: Grant a small amount of experience
 		quest::exp(1000);
-	}
-	
+	}	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
