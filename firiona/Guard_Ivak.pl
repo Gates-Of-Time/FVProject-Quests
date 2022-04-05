916ce971faad90e55dcc235b4bd59ed05ca59126
TurmoilToad <TurmoilToad@gmail.com>
2022-04-05 19:33:25 -0400

    Update Guard_Ivak.pl
    Refactor for consistency, don't eat items

--- firiona/Guard_Ivak.pl
+++ firiona/Guard_Ivak.pl
@@ -1,8 +1,13 @@
 sub EVENT_SAY { 
-    if($text=~/Hail/i){
-        quest::say("Blast!!  Hundreds of goblins to slay and [great treasure] to be had and I am stuck at my post!");
-    }
-    if($text=~/what great treasure?/i){
-        quest::say("A fellow paladin who was a member of an expedition into the jungles of Kunark returned with a jade green broadsword.  He said he came across it while they were fighting their way out of some ancient city filled with golems.  They lost many strong troops on that expedition."); 
-    }
+	if ($text=~/hail/i) {
+		quest::say("Blast!!  Hundreds of goblins to slay and [great treasure] to be had and I am stuck at my post!");
+	}
+	elsif ($text=~/great treasure/i) {
+		quest::say("A fellow paladin who was a member of an expedition into the jungles of Kunark returned with a jade green broadsword.  He said he came across it while they were fighting their way out of some ancient city filled with golems.  They lost many strong troops on that expedition."); 
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
