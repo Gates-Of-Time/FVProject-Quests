5e32307640583581ba5ac3e35f37e6df70dcf645
TurmoilToad <TurmoilToad@gmail.com>
2022-04-05 20:33:55 -0400

    Update Hiptal_Frizzleboth.pl

--- firiona/Hiptal_Frizzleboth.pl
+++ firiona/Hiptal_Frizzleboth.pl
@@ -1,13 +1,13 @@
 sub EVENT_SAY { 
-  if($text=~/Hail/i){
-    quest::say("Bah!! It's not been a good day! Reports from the Outlands are even worse than I was led to believe. Many would-be adventurers have turned up missing of late. One of them was one of my messenger. He was out collecting some of the various new and [interesting scrolls] that have been surfacing in the farthest reaches of this land. I fear for his safety and that of the others searching for the lost travelers. I wish there was more I could do.");
-  }
-  if ($text=~/interesting scrolls/i) {
-    quest::emote("reaches into his pouch. 'Well, let's see what I have in here. Ah, yes. The scrolls are definitely worth finding. Possibly even dangerous if they should fall into the wrong hands. But that is why the Collective sent me here. To procure all of the scrolls so that they may be studied at the Library of Mechanimagica. Bah!! But my messenger is missing! I can't fail them. Would you be willing to [help locate] some of the scrolls I am missing?");
-  }
-  if($text=~/help locate/i){
-    quest::say("You would do that for me? Of course you would, you realize how important my work is here. Your task is quite simple, really. I'll part with one of my rare scrolls if you bring me one of the common ones from the surrounding areas. To be more precise, I am looking for the scrolls Gift of Xev and Bristlebane's Bundle. Oh, my nephew is going to love these! I can't wait to get my hands on them! I'm also looking for the scrolls Quiver of Marr and Scars of Sigil. Bring them back to me as soon as you find one.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Bah!! It's not been a good day! Reports from the Outlands are even worse than I was led to believe. Many would-be adventurers have turned up missing of late. One of them was one of my messenger. He was out collecting some of the various new and [interesting scrolls] that have been surfacing in the farthest reaches of this land. I fear for his safety and that of the others searching for the lost travelers. I wish there was more I could do.");
+	}
+	elsif ($text=~/interesting scrolls/i) {
+		quest::emote("reaches into his pouch. 'Well, let's see what I have in here. Ah, yes. The scrolls are definitely worth finding. Possibly even dangerous if they should fall into the wrong hands. But that is why the Collective sent me here. To procure all of the scrolls so that they may be studied at the Library of Mechanimagica. Bah!! But my messenger is missing! I can't fail them. Would you be willing to [help locate] some of the scrolls I am missing?");
+	}
+	elsif ($text=~/help locate/i) {
+		quest::say("You would do that for me? Of course you would, you realize how important my work is here. Your task is quite simple, really. I'll part with one of my rare scrolls if you bring me one of the common ones from the surrounding areas. To be more precise, I am looking for the scrolls Gift of Xev and Bristlebane's Bundle. Oh, my nephew is going to love these! I can't wait to get my hands on them! I'm also looking for the scrolls Quiver of Marr and Scars of Sigil. Bring them back to me as soon as you find one.");
+	}
 }
 
 sub EVENT_ITEM(){  
@@ -51,7 +51,6 @@ sub EVENT_ITEM(){
 		#:: Grant a small amount of experience
 		quest::exp(1000);
 	}
-
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
