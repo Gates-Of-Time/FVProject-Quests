268fcbb120e1acdbed6993825c68669fcd08dbe8
TurmoilToad <TurmoilToad@gmail.com>
2022-04-07 20:24:30 -0400

    Delete Quegmor_O-Cranic.pl
    OOE

--- firiona/Quegmor_O-Cranic.pl
+++
@@ -1,5 +0,0 @@
-sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("Greetings traveler! There are many strange beasts that inhabit these lands with behavioral patterns different than those of the antonican beasts I have trained with. I hope to one day have mastered these foreign beasts so that my wisdom and spirit may be expanded.");
-  }
-}
