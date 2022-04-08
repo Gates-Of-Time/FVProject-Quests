e12365443e0550aedb004952383a1e50a722b4fa
TurmoilToad <TurmoilToad@gmail.com>
2022-04-07 20:30:33 -0400

    Update Samitha_Lightheart.pl
    Refactor for consistency

--- firiona/Samitha_Lightheart.pl
+++ firiona/Samitha_Lightheart.pl
@@ -1,13 +1,14 @@
 sub EVENT_SAY {
-  if($text=~/Hail/i){
-    quest::say("And a good day to you, too! I have traveled here in the name of the Jaggedpine Treefolk to search out any new and [wonderful powers] that would aid us in preserving the wildlife back home. I have not been able to venture far from this outpost as the inhabitants of the nearby woods pose a great danger. I have decided to wait for more of my Treefolk to arrive before adventuring further.  There's safety in numbers, they always say.");
-  }
-  if($text=~/wonderful powers/i){
-    quest::say("From what I have gathered from the residents of this outpost and from others like myself, there are a fair number of scrolls to be found in the outlying areas.  These scrolls are said to contain new and powerful magic. I myself have found a few of these scrolls. But the problem is, I don't believe I have a complete collection. If you would care to [help] me, I'd be willing to trade some of the extra ones I have for some of the extras you may run across.");
-  }
-  if($text=~/help/i){
-    quest::say("I am still looking for four scrolls that I have not been able to locate. They are the scrolls of Circle of Winter, Circle of Summer, Spirit of Scale, and Form of the Howler. If you bring any of these back, I'll give you one of four very rare scrolls in my possession."); }
-  }
+	if ($text=~/hail/i) {
+		quest::say("And a good day to you, too! I have traveled here in the name of the Jaggedpine Treefolk to search out any new and [wonderful powers] that would aid us in preserving the wildlife back home. I have not been able to venture far from this outpost as the inhabitants of the nearby woods pose a great danger. I have decided to wait for more of my Treefolk to arrive before adventuring further.  There's safety in numbers, they always say.");
+	}
+	elsif ($text=~/wonderful powers/i) {
+		quest::say("From what I have gathered from the residents of this outpost and from others like myself, there are a fair number of scrolls to be found in the outlying areas.  These scrolls are said to contain new and powerful magic. I myself have found a few of these scrolls. But the problem is, I don't believe I have a complete collection. If you would care to [help] me, I'd be willing to trade some of the extra ones I have for some of the extras you may run across.");
+	}
+	elsif ($text=~/help/i) {
+		quest::say("I am still looking for four scrolls that I have not been able to locate. They are the scrolls of Circle of Winter, Circle of Summer, Spirit of Scale, and Form of the Howler. If you bring any of these back, I'll give you one of four very rare scrolls in my possession.");
+	}
+}
 
 sub EVENT_ITEM {  
 	#:: Match a 19238 - Spell: Spirit of Scale
@@ -50,7 +51,6 @@ sub EVENT_ITEM {
 		#:: Grant a small amount of experience
 		quest::exp(1000);
 	}
-	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
