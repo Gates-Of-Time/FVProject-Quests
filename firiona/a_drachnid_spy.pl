8ab54a1e4fc3562921341ee99ecd309c51210eb8
TurmoilToad <TurmoilToad@gmail.com>
2022-04-07 21:50:55 -0400

    Update a_drachnid_spy.pl
    Refactor for consistency, don't eat items

--- firiona/a_drachnid_spy.pl
+++ firiona/a_drachnid_spy.pl
@@ -1,11 +1,17 @@
 sub EVENT_SAY {
- if($text=~/hail/i){
-    quest::emote("looks upon you with evil eyes and you hear a horrid chatter. Hairs rise from the spider's thorax. 'Greetings, creature. What a fine meal you would make!'");
-    quest::attack($name);
-    quest::say("You shall not take the tome from me!! I shall snack upon your blood-drained corpse.");
-  }
+	if ($text=~/hail/i) {
+		quest::emote(" looks upon you with evil eyes and you hear a horrid chatter. Hairs rise from the spider's thorax. 'Greetings, creature. What a fine meal you would make!'");
+		quest::attack($name);
+		quest::say("You shall not take the tome from me!! I shall snack upon your blood-drained corpse.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::signalwith(84319, 0, 0);
+	#:: Send a signal '1' to Firiona Vie >> Hero_Goxnok (84319) with no delay
+	quest::signalwith(84319, 1, 0);
 }
