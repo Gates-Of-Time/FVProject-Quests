8e9d4137e47c2a9be0332ef0b063cd6aa77e8ecb
TurmoilToad <TurmoilToad@gmail.com>
2022-04-07 20:35:03 -0400

    Update Scout_Bladethorn.pl
    Refactor for consistency, don't eat items

--- firiona/Scout_Bladethorn.pl
+++ firiona/Scout_Bladethorn.pl
@@ -1,5 +1,10 @@
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::say("Greetings! Keep an eye out for invaders. We may patrol the outpost, but every now and then a goblin or two manages to sneak in.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Greetings! Keep an eye out for invaders. We may patrol the outpost, but every now and then a goblin or two manages to sneak in.");
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
