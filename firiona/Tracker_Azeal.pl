ef7f22ac1f84bbfd05df10a6e970cf6a2c185c1e
TurmoilToad <TurmoilToad@gmail.com>
2022-04-07 21:22:27 -0400

    Update Tracker_Azeal.pl
    Refactor for consistency

--- firiona/Tracker_Azeal.pl
+++ firiona/Tracker_Azeal.pl
@@ -1,17 +1,28 @@
 sub EVENT_SPAWN {
-  quest::settimer("ZoneTime",30);
+	#:: Create a timer 'ZoneTime' that triggers every 30 seconds
+	quest::settimer("ZoneTime", 30);
 }
 
 sub EVENT_SAY {
-  if ($text=~/hail/i) {
-    quest::emote("looks over at you and gives you an awkward smile, then quickly looks away. You notice he isn't like most of the elves you've seen. You can't quite put your finger on it, but for one thing he smells like he hasn't bathed in weeks.");
-  }
+	if ($text=~/hail/i) {
+		quest::emote("looks over at you and gives you an awkward smile, then quickly looks away. You notice he isn't like most of the elves you've seen. You can't quite put your finger on it, but for one thing he smells like he hasn't bathed in weeks.");
+	}
 }
 
 sub EVENT_TIMER {
-  if (($timer eq "ZoneTime") && ($zonehour == 21)) {
-    quest::spawn2(84028, 0, 0, $x, $y, $z, $h);
-    quest::stoptimer("ZoneTime");
-    quest::depop_withtimer();
-  }
+	#:: Match timer 'ZoneTime'
+	if ($timer eq "ZoneTime") {
+		#:: Match if zone time is 21:00
+		if ($zonehour == 21) {
+			#:: Spawn one and only one Firiona Vie >> Tracker_Azeal_ (84028) with no grid or guild war set, at the current location
+			quest::unique_spawn(84028, 0, 0, $x, $y, $z);
+			quest::stoptimer("ZoneTime");
+			quest::depop_withtimer();
+		}
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
