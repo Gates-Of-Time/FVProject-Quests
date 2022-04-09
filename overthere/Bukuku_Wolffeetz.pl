f82b020f58d4f6d2174f7cc51ed2db8eb7e0f8d1
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 09:51:32 -0400

    Update Bukuku_Wolffeetz.pl
    Refactor for consistency

--- overthere/Bukuku_Wolffeetz.pl
+++ overthere/Bukuku_Wolffeetz.pl
@@ -1,10 +1,10 @@
 sub EVENT_SAY {
-  if($text=~/Hail/i) {
-    quest::say("Har har har! Yu funy lukking. Oooo.. do u hav doze [smarty writin's]?");
-  }
-  elsif($text=~/smarty writin's/i) {
-    quest::say("Me not know what dem ar for shure. Dem only hav dees names. Umm.. let me see if I can member dem. Taaalisman de umm.. Jasinth. Dat's one of dem. Spirited of Scaley?? OH!! Dis my favorite. Kripple. Den the last is the painful one. Canaabaalize canaabaalize canaabaalize. Yep, dat super duper one. Bring me bak one of dem, me trade.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Har har har! Yu funy lukking. Oooo.. do u hav doze [smarty writin's]?");
+	}
+	elsif ($text=~/smarty writin/i) {
+		quest::say("Me not know what dem ar for shure. Dem only hav dees names. Umm.. let me see if I can member dem. Taaalisman de umm.. Jasinth. Dat's one of dem. Spirited of Scaley?? OH!! Dis my favorite. Kripple. Den the last is the painful one. Canaabaalize canaabaalize canaabaalize. Yep, dat super duper one. Bring me bak one of dem, me trade.");
+	}
 }
 
 sub EVENT_ITEM {
@@ -48,7 +48,6 @@ sub EVENT_ITEM {
 		#:: Grant a small amount of experience
 		quest::exp(1000);
 	}
-
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
