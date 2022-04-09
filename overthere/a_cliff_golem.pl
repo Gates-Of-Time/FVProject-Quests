da620a65e6ae13b24df9d13c46d7421b08e6d2b2
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 09:36:46 -0400

    Update a_cliff_golem.pl
    Refactor for consistency, don't eat items

--- overthere/a_cliff_golem.pl
+++ overthere/a_cliff_golem.pl
@@ -1,5 +1,12 @@
 sub EVENT_SIGNAL {
-  if($signal == 0) {
-    quest::depop_withtimer();
-  }
+	#:: Match a signal '1' from /overthere/Alchemist_Granika.pl
+	if ($signal == 1) {
+		#:: Depop with spawn timer active
+		quest::depop_withtimer();
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
