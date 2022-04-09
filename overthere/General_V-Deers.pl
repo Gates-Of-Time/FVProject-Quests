105770846c25456c45cc30d850fee0853b73fd06
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 09:15:01 -0400

    Update General_V-Deers.pl
    Refactor for consistency

--- overthere/General_V-Deers.pl
+++ overthere/General_V-Deers.pl
@@ -1,28 +1,43 @@
-sub EVENT_SIGNAL {
-	if($signal==1) {
-		quest::depop_withtimer();
-	}
-	if($signal==2) {
-		quest::moveto(2717,2500,-49,130,1);
-		quest::settimer("getset",525);
-	}
+sub EVENT_SPAWN {
+	#:: Create a timer 'ZoneTime' that triggers every 60 seconds
+	quest::settimer("ZoneTime", 60);
 }
 
 sub EVENT_TIMER {
-	quest::stoptimer("getset");
-	quest::say("Dragoons!!  Attention!!  Right face!!");
-	quest::start(277);
-}
-
-sub EVENT_WAYPOINT_ARRIVE {
-	if($wp==1) {
-		quest::say("Forward.. march!!  Your left.. Your.. left, right, march!  Eighty.. second.. dragoon soldier!!  Pick up your weapon and follow me!!");
-		quest::signalwith(93186,3); # NPC: #High_Priest_I`Xar
-		quest::signalwith(93124,3); # NPC: Dragoon_Barber_W`Selo
-		quest::signalwith(93088,3); # NPC: Dragoon_T`Vem
-		quest::signalwith(93084,3); # NPC: Dragoon_T`Vex
-		quest::signalwith(93125,3); # NPC: Dragoon_V`Lask
-		quest::signalwith(93086,3); # NPC: Dragoon_V`Resh
+	#:: Match timer 'ZoneTime'
+	if ($timer eq "ZoneTime") {
+		if ($zonehour == 21) {
+			quest::moveto(2717, 2500, -49, 130, 1);
+			#:: Create a timer 'Dialogue' that triggers every 525 seconds
+			quest::settimer("Dialogue", 525);
+		}
+		elsif ($zonehour == 7) {
+			quest::depop_withtimer();
+		}
+	}
+	#:: Match timer 'Dialogue'
+	elsif ($timer eq "Dialogue") {
+		quest::stoptimer("Dialogue");
+		quest::say("Dragoons!!  Attention!!  Right face!!");
+		#:: Start pathing grid 277
+		quest::start(277);
 	}
 }
 
+sub EVENT_WAYPOINT_ARRIVE {
+	if ($wp==1) {
+		quest::say("Forward.. march!!  Your left.. Your.. left, right, march!  Eighty.. second.. dragoon soldier!!  Pick up your weapon and follow me!!");
+		#:: Send a signal '1' to the indicated NPC with no delay
+		quest::signalwith(93186, 1, 0);		#:: #High_Priest_I`Xar
+		quest::signalwith(93124, 1, 0);		#:: Dragoon_Barber_W`Selo
+		quest::signalwith(93088, 1, 0);		#:: Dragoon_T`Vem
+		quest::signalwith(93084, 1, 0);		#:: Dragoon_T`Vex
+		quest::signalwith(93125, 1, 0);		#:: Dragoon_V`Lask
+		quest::signalwith(93086, 1, 0);		#:: Dragoon_V`Resh
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
