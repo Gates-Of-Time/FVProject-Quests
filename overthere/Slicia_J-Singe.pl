019485ecfa25cfa796047c4cb44190f3219c6507
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 14:42:48 -0400

    Update Slicia_J-Singe.pl
    Refactor for consistency

--- overthere/Slicia_J-Singe.pl
+++ overthere/Slicia_J-Singe.pl
@@ -1,13 +1,13 @@
 sub EVENT_SAY { 
-  if($text=~/Hail/i){
-    quest::say("Why is it that you have come to this place? If you are coming here to search for [magical scrolls], just get back on that so-called ship you came here on and forget everything you thought you heard.");
-  }
-  elsif($text=~/magical scrolls/i){
-    quest::say("Is your hearing failing you!? Unless you [have something I need]. be gone!");
-  }
-  elsif($text=~/i have something you need/i){
-    quest::say("I must have the scrolls of Gift of Xev. Bristlebane's Bundle. Quiver of Marr. and the Scars of Sigil. If you don't have one of these. leave my sight!  If you do. I think we can work up a fair trade.");
-  }
+	if ($text=~/hail/i) {
+		quest::say("Why is it that you have come to this place? If you are coming here to search for [magical scrolls], just get back on that so-called ship you came here on and forget everything you thought you heard.");
+	}
+	elsif ($text=~/magical scrolls/i) {
+		quest::say("Is your hearing failing you!? Unless you [have something I need]. be gone!");
+	}
+	elsif ($text=~/i have something you need/i) {
+		quest::say("I must have the scrolls of Gift of Xev. Bristlebane's Bundle. Quiver of Marr. and the Scars of Sigil. If you don't have one of these. leave my sight!  If you do. I think we can work up a fair trade.");
+	}
 }
 
 sub EVENT_ITEM {
@@ -51,7 +51,6 @@ sub EVENT_ITEM {
 		#:: Grant a small amount of experience
 		quest::exp(1000);
 	}
-
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
