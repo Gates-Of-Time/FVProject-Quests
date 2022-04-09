901a4fe2bc8039e3e3578ef60025c89462a4a744
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 14:41:28 -0400

    Update Siladdarae_N-Riese.pl
    Refactor for consistency

--- overthere/Siladdarae_N-Riese.pl
+++ overthere/Siladdarae_N-Riese.pl
@@ -1,10 +1,10 @@
 sub EVENT_SAY { 
-  if($text=~/Hail/i) {
-    quest::say("Hello, $name. This place is quite a formidable outpost, but it lacks the comforts of home. Once I have finished collecting some of my [missing scrolls], I'll be able to leave this place and all its hidden dangers.");
-  }
-  elsif($text=~/missing scrolls/i) {
-    quest::say("Travelers have been bringing back numerous scrolls from the depths of darkness in the Outlands. They contain arcane knowledge specific to our classes. Only four are left that I seek. Keep a wary out for Theft of Thoughts, Color Slant, Cripple, and Dementia. Return any one of these to me and your reward shall be a scroll that can be found nowhere else."); 
-  }
+	if ($text=~/hail/i) {
+		quest::say("Hello, $name. This place is quite a formidable outpost, but it lacks the comforts of home. Once I have finished collecting some of my [missing scrolls], I'll be able to leave this place and all its hidden dangers.");
+	}
+	elsif ($text=~/missing scrolls/i) {
+		quest::say("Travelers have been bringing back numerous scrolls from the depths of darkness in the Outlands. They contain arcane knowledge specific to our classes. Only four are left that I seek. Keep a wary out for Theft of Thoughts, Color Slant, Cripple, and Dementia. Return any one of these to me and your reward shall be a scroll that can be found nowhere else."); 
+	}
 }
 
 sub EVENT_ITEM {
@@ -48,7 +48,6 @@ sub EVENT_ITEM {
 		#:: Grant a small amount of experience
 		quest::exp(1000);
 	}
-	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
