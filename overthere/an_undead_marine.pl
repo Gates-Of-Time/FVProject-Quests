55a3454621a1870649c05a276fcde8eeafa74b21
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 10:02:41 -0400

    Update an_undead_marine.pl
    Refactor for consistency, don't eat items

--- overthere/an_undead_marine.pl
+++ overthere/an_undead_marine.pl
@@ -1,16 +1,28 @@
 sub EVENT_SIGNAL {
-	if($x==2146 && $y==2445) {
-		quest::say("Sir, yes sir!!");
-	} elsif ($x==2151 && $y==2440) {
-		quest::say("Sir, yes sir!!");
-	} elsif ($x==2136 && $y==2453) {
-		quest::say("Sir, yes sir!!");
-	} elsif ($x==2124 && $y==2465) {
-		quest::say("Sir, yes sir!!");
-	} elsif ($x==2130 && $y==2459) {
-		quest::say("Sir, yes sir!!");
-	} elsif ($x==2142 && $y==2448) {
-		quest::say("Sir, yes sir!!");
+	#:: Match a signal '1' from /overthere/Captain_Rottgrime.pl
+	if ($signal == 1) {
+		if ($x == 2146 && $y == 2445) {
+			quest::say("Sir, yes sir!!");
+		}
+		elsif ($x == 2151 && $y == 2440) {
+			quest::say("Sir, yes sir!!");
+		}
+		elsif ($x == 2136 && $y == 2453) {
+			quest::say("Sir, yes sir!!");
+		}
+		elsif ($x == 2124 && $y == 2465) {
+			quest::say("Sir, yes sir!!");
+		}
+		elsif ($x == 2130 && $y == 2459) {
+			quest::say("Sir, yes sir!!");
+		}
+		elsif ($x == 2142 && $y == 2448) {
+			quest::say("Sir, yes sir!!");
+		}
 	}
 }
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
