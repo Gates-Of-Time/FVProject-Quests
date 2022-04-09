33ea076076c4a32cf336a70bfc40210c35958db3
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 09:21:40 -0400

    Update Dragoon_T-Vem.pl
    Refactor for consistency, don't eat items

--- overthere/Dragoon_T-Vem.pl
+++ overthere/Dragoon_T-Vem.pl
@@ -1,19 +1,39 @@
+sub EVENT_SPAWN {
+	#:: Create a timer 'ZoneTime' that triggers every 60 seconds
+	quest::settimer("ZoneTime", 60);
+}
+
+sub EVENT_TIMER {
+	#:: Match timer 'ZoneTime'
+	if ($timer eq "ZoneTime") {
+		if ($zonehour == 21) {
+			quest::moveto(2733, 2470, -49, 390, 1);
+		}
+		elsif ($zonehour == 7) {
+			quest::depop_withtimer();
+		}
+	}
+}
+
 sub EVENT_SIGNAL {
-	if($signal==1) {
-		quest::depop_withtimer();
-	}
-	if($signal==2) {
-		quest::moveto(2733,2470,-49,390,1);
-	}
-	if($signal==3) {
+	#:: Match a signal '1' from /overthere/General_V-Deers.pl
+	if ($signal == 1) {
+		#:: Set running to true (Run)
 		quest::SetRunning(1);
+		#:: Start path grid 278
 		quest::start(278);
 	}
 }
 
 sub EVENT_WAYPOINT_ARRIVE {
-	if($wp==28){
+	#:: Match waypoint 28
+	if ($wp == 28) {
+		#:: Set running to false (Walk)
 		quest::SetRunning(0);
 	}
 }
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
