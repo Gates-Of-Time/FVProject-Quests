5aacef554574cde3763bdd6335d86b8637a0f901
TurmoilToad <TurmoilToad@gmail.com>
2022-04-09 15:25:30 -0400

    Update a_glowing_cliff_golem.pl
    Refactor for consistency, don't depop mid-fight, don't eat items

--- overthere/a_glowing_cliff_golem.pl
+++ overthere/a_glowing_cliff_golem.pl
@@ -1,8 +1,29 @@
 sub EVENT_SPAWN {
-    quest::settimer(1,640);
-    quest::start(274);
+	#:: Create a timer 'Depop' that triggers every 640 seconds
+	quest::settimer("Depop", 640);
+	#:: Start pathing grid 274
+	quest::start(274);
 }
 
 sub EVENT_TIMER {
-    quest::depop();
+	#:: Match timer 'Depop'
+	if ($timer eq "Depop") {
+		quest::depop();
+	}
+}
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::stoptimer("Depop");
+	}
+	elsif ($combat_state == 0) {
+		#:: Create a timer 'Depop' that triggers every 640 seconds
+		quest::settimer("Depop", 640);
+	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
