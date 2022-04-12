c6633b0350710fd01ff9273d1be8fc66cd0bcbb6
Aleksander Spro <abspro@gmail.com>
2022-04-12 13:08:17 +0200

    Content flag VP key quests

--- timorous/Xiblin_Fizzlebik.pl
+++ timorous/Xiblin_Fizzlebik.pl
@@ -13,16 +13,24 @@ sub EVENT_TIMER {
 
 sub EVENT_SAY {
 	if ($text=~/hail/i) {
-		quest::say("Hello, $name! I am Xiblin Fizzlebik, renowned archeologist and historian of Ak'Anon. I'm currently in search of artifacts and relics on the Iksar [Jarsath tribe]. If you stumble upon anything, please bring it to me.");
+		if ( quest::is_content_flag_enabled("Kunark_HoleEra")) {		
+			quest::say("Hello, $name! I am Xiblin Fizzlebik, renowned archeologist and historian of Ak'Anon. I'm currently in search of artifacts and relics on the Iksar [Jarsath tribe]. If you stumble upon anything, please bring it to me.");
+		}
 	}
-	elsif ($text=~/jarsath tribe/i) {
-		quest::say("About two thousand years ago, there were five different tribes of Iksar who dwelled on the continent of Kunark. One of these tribes was the Jarsath. They were a very primitive sort, proud and tribal, but also cannibalistic. Often, they went to war with the other tribes that co-existed with them back then. The ruins found on this island were of structures created by the Jarsath. I've been excavating for quite some time now, but haven't been able to find anything of significance.");
+	elsif ($text=~/jarsath tribe/i) {		
+		if ( quest::is_content_flag_enabled("Kunark_HoleEra")) {
+			quest::say("About two thousand years ago, there were five different tribes of Iksar who dwelled on the continent of Kunark. One of these tribes was the Jarsath. They were a very primitive sort, proud and tribal, but also cannibalistic. Often, they went to war with the other tribes that co-existed with them back then. The ruins found on this island were of structures created by the Jarsath. I've been excavating for quite some time now, but haven't been able to find anything of significance.");
+		}
 	}
-	elsif ($text=~/looking for/i) {
-		quest::say("In my studies, I have learned that the Jarsath believed they could draw power from a holy symbol... specifically a medallion that represented their tribe. When Salthir became king of the Iksar empire Sebilis, he had the medallion broken into three pieces and hid them on the Kunark continent so that they would never be found. I'm searching for the pieces so that I can put it back together and learn more about this tribe. Perhaps you wish to help?");
+	elsif ($text=~/looking for/i) {	
+		if ( quest::is_content_flag_enabled("Kunark_HoleEra")) {
+			quest::say("In my studies, I have learned that the Jarsath believed they could draw power from a holy symbol... specifically a medallion that represented their tribe. When Salthir became king of the Iksar empire Sebilis, he had the medallion broken into three pieces and hid them on the Kunark continent so that they would never be found. I'm searching for the pieces so that I can put it back together and learn more about this tribe. Perhaps you wish to help?");
+		}
 	}
 	elsif ($text=~/help/i) {
-		quest::say("All I know is that the pieces were most likely hidden in water regions. If you manage to find them, bring them to me and I will be able to put the medallion of the Jarsath back together. I am sure there is much to be learned from it.");
+		if ( quest::is_content_flag_enabled("Kunark_HoleEra")) {
+			quest::say("All I know is that the pieces were most likely hidden in water regions. If you manage to find them, bring them to me and I will be able to put the medallion of the Jarsath back together. I am sure there is much to be learned from it.");
+		}
 	}
 }
 
@@ -44,34 +52,37 @@ sub EVENT_SIGNAL {
 }
 
 sub EVENT_ITEM {
-	#:: Match a 19959 - Piece of a medallion, a 19960 - Piece of a medallion, and a 19961 - Piece of a medallion
-	if (plugin::takeItems(19959 => 1, 19960 => 1, 19961 => 1)) {
-		quest::emote("tinkers with the three pieces and snaps the pieces together. 'Wow.. You've done it.. but I suddenly feel very.. strange.. This medallion is cursed! Here, you take it!'");
-		#:: Give a 19954 - Medallion of the Jarsath
-		quest::summonitem(19954);
-		#:: Ding!
-		quest::ding();
-		#:: Grant a small amount of experience
-		quest::exp(100);
+	if ( quest::is_content_flag_enabled("Kunark_HoleEra")) {
+		#:: Match a 19959 - Piece of a medallion, a 19960 - Piece of a medallion, and a 19961 - Piece of a medallion
+		if (plugin::takeItems(19959 => 1, 19960 => 1, 19961 => 1)) {
+			quest::emote("tinkers with the three pieces and snaps the pieces together. 'Wow.. You've done it.. but I suddenly feel very.. strange.. This medallion is cursed! Here, you take it!'");
+			#:: Give a 19954 - Medallion of the Jarsath
+			quest::summonitem(19954);
+			#:: Ding!
+			quest::ding();
+			#:: Grant a small amount of experience
+			quest::exp(100);
+		}
+		#:: Match a 19959 - Piece of a medallion
+		elsif (plugin::takeItems(19959 => 1)) {
+			quest::say("Yes yes, now where is the rest of the medallion?");
+			#:: Give a 19959 - Piece of a medallion
+			quest::summonitem(19959);
+		}
+		#:: Match a 19960 - Piece of a medallion
+		elsif (plugin::takeItems(19960 => 1)) {
+			quest::say("Yes yes, now where is the rest of the medallion?");
+			#:: Give a 19960 - Piece of a medallion
+			quest::summonitem(19960);
+		}
+		#:: Match a 19961 - Piece of a medallion
+		elsif (plugin::takeItems(19961 => 1)) {
+			quest::say("Yes yes, now where is the rest of the medallion?");
+			#:: Give a 19961 - Piece of a medallion
+			quest::summonitem(19961);
+		}
 	}
-	#:: Match a 19959 - Piece of a medallion
-	elsif (plugin::takeItems(19959 => 1)) {
-		quest::say("Yes yes, now where is the rest of the medallion?");
-		#:: Give a 19959 - Piece of a medallion
-		quest::summonitem(19959);
-	}
-	#:: Match a 19960 - Piece of a medallion
-	elsif (plugin::takeItems(19960 => 1)) {
-		quest::say("Yes yes, now where is the rest of the medallion?");
-		#:: Give a 19960 - Piece of a medallion
-		quest::summonitem(19960);
-	}
-	#:: Match a 19961 - Piece of a medallion
-	elsif (plugin::takeItems(19961 => 1)) {
-		quest::say("Yes yes, now where is the rest of the medallion?");
-		#:: Give a 19961 - Piece of a medallion
-		quest::summonitem(19961);
-	}
+	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
