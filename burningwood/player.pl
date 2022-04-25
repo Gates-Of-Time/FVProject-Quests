455f3a815aa865e6a3efdb826862a2e7a34de68c
Aleksander Spro <abspro@gmail.com>
2022-04-25 11:48:39 +0200

    Convert lua to perl Convert qglobal to databucket

---
+++ burningwood/player.pl
@@ -0,0 +1,12 @@
+sub EVENT_ENTER {
+	$key = $name . "-druid_epic";
+	#:: Match if the key does not exist
+	if (quest::get_data($key) == 12) {
+		#:: Match if #a_crippled_wurm is not spawned
+		if($entity_list->IsMobSpawnedByNpcTypeID(87155) == false) {
+			#:: Spawn one and only one Burning Wood >> #a_crippled_wurm (87155), without grid or guild war, at the given location
+			quest::unique_spawn(87155, 0, 0, 0, 3000, -259, 0);
+
+		}
+	}
+}
