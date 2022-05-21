83d194ebd4f2812b575471f0bbd4cb5b44a2b2a9
TurmoilToad <TurmoilToad@gmail.com>
2022-05-20 20:43:20 -0400

    Update Hager_Sureshot.pl
    Add missing dialogue

--- qrg/Hager_Sureshot.pl
+++ qrg/Hager_Sureshot.pl
@@ -28,14 +28,50 @@ sub EVENT_SAY {
 	elsif ($text=~/mammoth/i) {
 		quest::say("Mammoth is the great bear of the caves.  [Tunare] has blessed our home with Mammoth's presence.  It is said that when the great bear dies, so too shall the Jaggedpine.  He would be quite a trophy to some hunter.  And quite profitable to a poacher. Even the infamous [Talym Shoontar] has tried his hand at the hunt.");
 	}
-	elsif ($text=~/tunare/i) {
-		quest::say("Tunare is the Mother of All.  It is through Her will that we protect this land and its many creatures.");
-	}
 	elsif ($text=~/talym shoontar/i) {
 		quest::say("Talym Shoontar is a poacher.  He has eluded us for quite some time.  He has organized parties of poachers to plague our forest as well as others.  He hails from Halas, but I have heard he now calls the Plains of Karana home.  I have placed a bounty upon his head.  Do you [wish to collect the bounty]?");
 	}
 	elsif ($text=~/wish to collect the bounty/i) {
 		quest::say("That's the spirit!!  Spoken like a true Protector of the Pine!  Last I heard, Talym Shoontar was hunting in the Plains of Karana.  He has also been spotted running to and from Qeynos.  No doubt he is selling hides to some merchant.  End his life of hunting.  Bring me his head to collect the bounty.");
+	}
+	elsif ($text=~/who.*leader/i) {
+		quest::say("The land of Surefall Glade is ruled by no single hand other than Tunare, but if guidance is what you seek, I would suggest you speak with Te`Anara.  She is the head of the Jaggedpine Treefolk.  Otherwise, you could speak with Hager Sureshot of the Protectors of the Pine.");
+	}
+	elsif ($text=~/what poachers/i) {
+		quest::say("Poachers have been plaguing our land.  We do our best to stop them.  If you wish to join the fight, seek the masters of the Protectors of the Pine.");
+	}
+	elsif ($text=~/where.*mammoth/i) {
+		quest::say("That information is best kept secret.");
+	}
+	elsif ($text=~/where.*cave/i) {
+		quest::say("That information is best kept secret.");
+	}
+	elsif ($text=~/where.*druid guild/i) {
+		quest::say("The Jaggedpine Treefolk are the local druids.  The masters can be found here within the great tree.");
+	}
+	elsif ($text=~/where.*forge/i) {
+		quest::say("We have nothing like that here in Surefall Glade.  You must venture to Qeynos.");
+	}
+	elsif ($text=~/where.*oven/i) {
+		quest::say("We have nothing like that here in Surefall Glade.  You must venture to Qeynos.");
+	}
+	elsif ($text=~/where.*armor/i) {
+		quest::say("Oftentimes you can find a traveling merchant in one of the nearby houses.  Other than that you would have to travel to Qeynos.");
+	}
+	elsif ($text=~/where.*qeynos/i) {
+		quest::say("The great city of Qeynos can be found by walking along the path outside of Surefall Glade.  Many of our rangers and druids serve alongside the Qeynos Guard when the need arises.");
+	}
+	elsif ($text=~/where.*bank/i) {
+		quest::say("There is no need for a vault among our people.  You could try the Qeynos Hold in Qeynos.");
+	}
+	elsif ($text=~/who.*talym shoontar/i) {
+		quest::say("Talym Shoontar is a wanted man.  He is a very infamous poacher.  Hager Sureshot has placed a bounty upon his head.");
+	}
+	elsif ($text=~/who.*chanda/i) {	
+		quest::say("The entire Miller family are nothing more than scum.  It is they who entice poachers to continue with their slaughter so they can profit from the skins of the wildlife.");
+	}
+	elsif ($text=~/tunare/i) {
+		quest::say("Tunare is the Mother of All.  It is through Her will that we protect this land and its many creatures.");
 	}
 }
 
