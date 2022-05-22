845c37e10e5c801f4699f267f810745981ee856f
TurmoilToad <TurmoilToad@gmail.com>
2022-05-22 17:08:21 -0400

    Update Guard_Mezzt.pl
    Removed signal from Lt. A since Mezz can't speak.

--- qeynos/Guard_Mezzt.pl
+++ qeynos/Guard_Mezzt.pl
@@ -6,13 +6,6 @@ sub EVENT_SAY {
 	}
 }
 
-sub EVENT_SIGNAL {
-	if ($signal == 1) {
-		#:: Signal sent from qeynos/Lieutenant_Arathur.pl
-		quest::say("Yes, sir!");
-	}
-}
-
 sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
