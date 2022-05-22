bc06e24bf750aa84396f31efb61eeb75a9efa44d
TurmoilToad <TurmoilToad@gmail.com>
2022-05-22 17:30:44 -0400

    Update Guard_Relam.pl
    Add missing dialogue.

--- qeynos/Guard_Relam.pl
+++ qeynos/Guard_Relam.pl
@@ -4,6 +4,59 @@ sub EVENT_COMBAT {
 	}
 }
 
+sub EVENT_SAY {
+	if ($text=~/hail/i) {
+		quest::say("Well met, $name.  My name is Relam Yenn and I have sworn to uphold the laws of Qeynos.  As long as you stay out of trouble, we will get along fine.");
+	}
+	elsif ($text=~/order of the three/i) {
+		quest::say("The Order of Three consists of the three positive circles of the arcane known as magic, enchantment and wizardry.  This guild keeps very busy in its hall near the arena.  All in Qeynos respect their powers.");
+	}
+	elsif ($text=~/where.*monk guild/i) {
+		quest::say("In North Qeynos sits the house of the Silent Fist Clan.  These monks are welcome in Qeynos and often assist in training the Qeynos Guard in hand to hand combat.");
+	}
+	elsif ($text=~/where.*bank/i) {
+		quest::say("Qeynos Hold is South Qeynos is just across from the arena.  You know, over the bridge behind Firepride's.");
+	}
+	elsif ($text=~/where.*port/i) {
+		quest::say("The port of Qeynos is located in South Qeynos.  From here one may catch the ship to Erudin.");
+	}
+	elsif ($text=~/where.*dock/i) {
+		quest::say("The port of Qeynos is located in South Qeynos.  From here one may catch the ship to Erudin.");
+	}
+	elsif ($text=~/who.*kane/i) {
+		quest::say("Commander Kane Bayle is the commander of all the Qeynos Guard.  He is second only to his brother, Antonius Bayle.  His post is in the guard house at the city gates.  Mind you, do not bother him, he has a bit of a temper.");
+	}
+	elsif ($text=~/who.*circle of unseen hands/i) {
+		quest::say("The Circle of the Unseen Hand?  I have heard nothing of them.  Are they some sort of performing magic troupe?");
+	}
+	elsif ($text=~/(cleric|paladin) guild/i) {
+		quest::say("Within Qeynos are the Knights of Thunder and the Priests of Life.  Both of these orders consist of clerics and paladins and are respected by all.  The Hall of Thunder lies in South Qeynos and the Temple of Life is in North Qeynos.");
+	}
+	elsif ($text=~/(ranger|druid) guild/i) {
+		quest::say("I believe you shall find the Protectors of the Pine and Jaggedpine Treefolk in the hollow of Surefall Glade.");
+	}
+	elsif ($text=~/(necromancer|shadowknight) guild/i) {
+		quest::say("What gibberish are you spouting?  There is not, nor will there ever be, such an organization as that within a hundred miles of Qeynos, not with the Qeynos Guard on patrol.  Next you will be inquiring of a rogue guild!  Really!");
+	}
+	elsif ($text=~/who.*jaggedpine treefolk/i) {
+		quest::say("In Surefall Glade there are druids called the Jaggedpine Treefolk.  These men and women often assist our army during times of war.");
+	}
+	elsif ($text=~/captain linarius/i) {
+		quest::say("The Qeynos Tower Guards of the hills and plains are commanded by Captain Linarius Graffe.  He often has much to report.  He is posted in a keep which spans a river.  I believe it is in the northern Plains of Karana.");
+	}
+	elsif ($text=~/where.*north qeynos/i) {
+		quest::say("The passages to North Qeynos can be found near the bard's guild or next to the Clock of Ak'Anon.");
+	}
+	elsif ($text=~/where.*south qeynos/i) {
+		quest::say("The passages to North Qeynos can be found near the bard's guild or next to the Clock of Ak'Anon.");
+	}
+	elsif ($text=~/where.*inn/i) {
+		quest::say("If you need a place to rest, I highly recommend the Lion's Mane Inn and Tavern.  Poor adventurers all huddle together in one of the backrooms of Fish's.");
+	}
+	elsif ($text=~/where.*weapons/i) {
+		quest::say("This city has been blessed with the craftsmanship of the Ironforges.  Their weapons are supreme.  Their shop can be found near the Temple of Life in North Qeynos.  You may also try the local warrior guild.");
+	}
+}
 sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
