7b69601ce05a44c577d0dcc9cb24e35faae3d5e9
Aleksander Spro <abspro@gmail.com>
2022-05-25 16:34:27 +0200

    Wrogn group

--- frontiermtns/a_goblin_scavenger.pl
+++ frontiermtns/a_goblin_scavenger.pl
@@ -1,6 +1,6 @@
-#:: Frontier Mountains Named Cycle - Goblin-Giant-Burynai Cycle 
-#:: Southeast Camp (Goblins)
-#:: Spawns Southwest Camp (Giants)
+#:: Frontier Mountains Named Cycle - Brute-Sarnak Cycle 
+#:: Southwest Camp (Various mobs)
+#:: Spawns Northwest Camp (Sarnak)
 sub EVENT_DEATH_COMPLETE {
   $spawnPointId = $npc->GetSpawnPointID();
 	#:: Send a signal <$spawnPointId> to The Plane of Sky >> zone_controller (50) with no delay
