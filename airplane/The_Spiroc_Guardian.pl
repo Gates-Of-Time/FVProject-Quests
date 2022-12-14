3f063593eb7efe4392f9f59105d4c1044a4abc0f
TurmoilToad <TurmoilToad@gmail.com>
2022-12-14 09:22:23 -0500

    Update The_Spiroc_Guardian.pl
    Toothless idiot.

--- airplane/The_Spiroc_Guardian.pl
+++ airplane/The_Spiroc_Guardian.pl
@@ -2,6 +2,6 @@ sub EVENT_DEATH_COMPLETE {
 	#:: Match a The Plane of Sky >> a_spiroc_walker (71014) or a_spiroc_vanquisher (71009) or a_spiroc_revolter (71010) or a_spiroc_expulser (71011) or a_spiroc_caller (71015) or a_spiroc_banisher (71007) or a_spiroc_arbiter (71008)
 	if ($entity_list->IsMobSpawnedByNpcTypeID(71014) || $entity_list->IsMobSpawnedByNpcTypeID(71009) || $entity_list->IsMobSpawnedByNpcTypeID(71010) || $entity_list->IsMobSpawnedByNpcTypeID(71011) || $entity_list->IsMobSpawnedByNpcTypeID(71015) || $entity_list->IsMobSpawnedByNpcTypeID(71007) || $entity_list->IsMobSpawnedByNpcTypeID(71008)) {
 		#:: Reset spawn timer for 1000ms
-		quest::UpdateSpawnTimer(2631, 1000);
+		quest::updatespawntimer(2631, 1000);
 	}
 }
