c372c29064c33f250f137979360613c9dc438f30
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 10:57:30 -0500

    Update a_drunkard.pl
    add faction checks

--- gfaydark/a_drunkard.pl
+++ gfaydark/a_drunkard.pl
@@ -2,14 +2,17 @@ sub EVENT_SAY {
 	if ($text=~/hail/i) {
 		quest::say("Can you -hic- . . . spare some copper or -hic- . . . silver? I just . . . -Hic- . . . need some change to buy something to drin . . Er . . Eat . -Hic-");
 	}
-	if ($text=~/tandan nybright/i) {
-		quest::say("How did you know my name? You must be from Felwithe. Hic.. Excuse my condition. I am not the man I was. I have had a [shocking experience].");
-	}
-	if ($text=~/shocking experience/i) {
-		quest::say("My daughters have become blood thirsty bandits. They went off to visit Antonica and when they returned they became bandits. They now attack every poor traveler who happens upon them. They have disgraced the name of Nybright. Will you [hunt the Nybright Sisters]or is this not to your liking?");
-	}
-	if ($text=~/hunt/i) {
-		quest::say("I am most thankful. Many adventurers would not help me with this matter. Go and seek out the Nybright sisters. I gave all four of them a Personalized Necklace. Return them all to me when the deed is done. May the Tribunal watch over you.");
+	#:: Match if faction is Indifferent or better
+	if ($faction < 5) {
+		if ($text=~/tandan nybright/i) {
+			quest::say("How did you know my name? You must be from Felwithe. Hic.. Excuse my condition. I am not the man I was. I have had a [shocking experience].");
+		}
+		elsif ($text=~/shocking experience/i) {
+			quest::say("My daughters have become blood thirsty bandits. They went off to visit Antonica and when they returned they became bandits. They now attack every poor traveler who happens upon them. They have disgraced the name of Nybright. Will you [hunt the Nybright Sisters]or is this not to your liking?");
+		}
+		elsif ($text=~/hunt/i) {
+			quest::say("I am most thankful. Many adventurers would not help me with this matter. Go and seek out the Nybright sisters. I gave all four of them a Personalized Necklace. Return them all to me when the deed is done. May the Tribunal watch over you.");
+		}
 	}
 }
 
@@ -22,19 +25,30 @@ sub EVENT_ITEM {
 		#:: Grants a small amount of experience
 		quest::exp(150);
 	}
-	#:: Match a 13347 - Dull Pearl Necklace, 13349 - Dull Pearl Necklace, 13348 - Dull Pearl Necklace, 13350 - Dull Pearl Necklace
-	elsif (plugin::takeItems(13347 => 1, 13349 => 1, 13348 => 1, 13350 => 1)) {
-		quest::say("This is a sad day, but it was the just thing to do. The Nybright name will no longer be tarnished. Here you are my friend. This is a bit uncared for, but it is yours. My warrior days are over. Now go. I must be alone.");
-		#:: Give a random item: 2314 - Bearskin Gloves, 2007 - Leather Belt, 2005 - Leather Shoulderpads  (later also has the chance to give 23359 - Targ Shield)
-		quest::summonitem(quest::ChooseRandom(2314, 2007, 2005));
-		#:: Ding!
-		quest::ding();
-		#:: Grant a small amount of experience
-		quest::exp(300);
-		#:: Set faction
-		quest::faction(5001, 20);		#:: + Anti-Mage
-		quest::faction(226, 20);		#:: + Clerics of Tunare
-		quest::faction(279, 20);	#:: + King Tearis Thex
+	#:: Match a 13347 - Dull Pearl Necklace, 13348 - Dull Pearl Necklace, 13349 - Dull Pearl Necklace, 13350 - Dull Pearl Necklace
+	elsif (plugin::takeItems(13347 => 1, 13348 => 1, 13349 => 1, 13350 => 1)) {
+		#:: Match if faction is Indifferent or better
+		if ($faction < 5) {
+			quest::say("This is a sad day, but it was the just thing to do. The Nybright name will no longer be tarnished. Here you are my friend. This is a bit uncared for, but it is yours. My warrior days are over. Now go. I must be alone.");
+			#:: Give a random item: 2314 - Bearskin Gloves, 2007 - Leather Belt, 2005 - Leather Shoulderpads  (later also has the chance to give 23359 - Targ Shield)
+			quest::summonitem(quest::ChooseRandom(2314, 2007, 2005));
+			#:: Ding!
+			quest::ding();
+			#:: Grant a small amount of experience
+			quest::exp(300);
+			#:: Set faction
+			quest::faction(5001, 20);		#:: + Anti-Mage
+			quest::faction(226, 20);		#:: + Clerics of Tunare
+			quest::faction(279, 20);	#:: + King Tearis Thex
+		}
+		else {
+			quest::say("No thanks.");
+			#:: Return a 13347 - Dull Pearl Necklace, 13348 - Dull Pearl Necklace, 13349 - Dull Pearl Necklace, 13350 - Dull Pearl Necklace
+			quest::summonitem(13347);
+			quest::summonitem(13348);
+			quest::summonitem(13349);
+			quest::summonitem(13350);
+		}
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
