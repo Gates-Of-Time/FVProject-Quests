409a3eebddd58b3801da11e99e2fd6e4de946d65
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 10:24:39 -0500

    Update Phiz_Frugrin.pl
    Don't eat items, added faction check

--- steamfont/Phiz_Frugrin.pl
+++ steamfont/Phiz_Frugrin.pl
@@ -2,9 +2,24 @@ sub EVENT_SAY {
 	if ($text=~/Hail/i) {
 		quest::say("Hello my friend. Good to meet you.");
 	}
-	if ($text=~/lens/i) {
-		quest::say("Oh my. I have bad news. It was stolen by a dark elf rogue. She fled very quickly. My little legs could not keep up. Here, she dropped this. You must find her. Get it back. We have very few Lenses.");
-		#:: Give a 18867 - Filthy Towel
-		quest::summonitem(18867);
+	elsif ($text=~/lens/i) {
+		#:: Match if faction is Amiable or better
+		if ($faction <= 4) {
+			quest::say("Oh my. I have bad news. It was stolen by a dark elf rogue. She fled very quickly. My little legs could not keep up. Here, she dropped this. You must find her. Get it back. We have very few Lenses.");
+			#:: Give a 18867 - Filthy Towel
+			quest::summonitem(18867);
+		}
+		#:: Match if faction is Indifferent
+		elsif ($faction == 5) {
+			quest::say("I cannot yet fully trust you");
+		}
+		else {
+			quest::say("You dare to speak to a member of the Eldritch Collective! You had best leave before you find your soul displaced from your body.");
+		}
 	}
+}
+
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
