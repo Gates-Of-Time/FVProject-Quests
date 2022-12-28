a0cb1772e2280386830cd29be5b36b01566723af
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 21:48:28 -0500

    Update #Neh-Ashiir.pl
    Refactor for consistency

--- citymist/#Neh-Ashiir.pl
+++ citymist/#Neh-Ashiir.pl
@@ -4,14 +4,13 @@ sub EVENT_ITEM {
 		quest::say("Nothing is left to hide now. You shall have the truth. But truth is not won easily and if you cannot defeat me, you have not the ability to see that vengeance is served. Brace yourself!");
 		#:: Ding!
 		quest::ding();
-		#:: Depop without spawn timer
-		quest::depop();
 		#:: Set factions
 		quest::faction(404, 50);		#:: + Truespirit
 		#:: Spawn a City of Mist >> #Neh`Ashiir_  (90181), without grid or guild war, at the current location
 		quest::spawn2(90181, 0, 0, $x, $y,- $z, 0);
+		#:: Depop without spawn timer
+		quest::depop();
 	}
-	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
