eda1cbc72f87318b6853cb21df43785573362d55
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 21:37:45 -0500

    Update #Lhranc.pl
    Refactor for consistency, don't eat items

--- citymist/#Lhranc.pl
+++ citymist/#Lhranc.pl
@@ -1,11 +1,15 @@
-# Part of SK Epic 1.0
+#:: Part of SK Epic 1.0
 
 sub EVENT_SPAWN {
-  quest::say("Young one, you cannot fathom how long it has been since I breathed air and felt flesh on my bones. I told you I would show you a weapon that would make you the most powerful of your kind. I said nothing of giving it to you. Feast your eyes on Innoruuk's Curse, mortal! Thank you for your service.");
+	quest::say("Young one, you cannot fathom how long it has been since I breathed air and felt flesh on my bones. I told you I would show you a weapon that would make you the most powerful of your kind. I said nothing of giving it to you. Feast your eyes on Innoruuk's Curse, mortal! Thank you for your service.");
 }
 
 sub EVENT_DEATH_COMPLETE {
-  quest::spawn2(90189, 0, 0, 28.1, 1.4, 7, 385); ##Marl Kastane
+	#:: Spawn a The City of Mist >> #Marl_Kastane (90189) without grid or guildwar, at the specified coordinates
+	quest::spawn2(90189, 0, 0, 28.1, 1.4, 7, 385);
 }
 
-# Quest by mystic414
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}
