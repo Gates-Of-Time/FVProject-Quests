35c8d081837569c31cb2ef4cb3065459ad17b59e
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 21:40:49 -0500

    Update #Lord_Rak-Ashiir.pl
    Refactor for consistency

--- citymist/#Lord_Rak-Ashiir.pl
+++ citymist/#Lord_Rak-Ashiir.pl
@@ -1,22 +1,24 @@
 sub EVENT_ITEM {
-	if ($faction==1) { # Ally faction
-		#:: Match a 1673 - Child's Tear
-		if (plugin::takeItems(1673 => 1)) {
-      quest::emote("looks down at the tear in his hand and says 'A minion of my god came to me one night. I knew it was of Cazic-Thule as I was frozen in terror. My mind screamed for me to flee but my body would not respond. The being took my daughter and vanished to only he knew where. When I regained control of my body and thoughts, I felt nothing but betrayal. I don't care anymore about anything. If you want repentance then slay me, $name.");
+	#:: Match a 1673 - Child's Tear
+	if (plugin::takeItems(1673 => 1)) {
+		#:: Match if faction is Ally
+		if ($faction == 1) {
+			quest::emote("looks down at the tear in his hand and says 'A minion of my god came to me one night. I knew it was of Cazic-Thule as I was frozen in terror. My mind screamed for me to flee but my body would not respond. The being took my daughter and vanished to only he knew where. When I regained control of my body and thoughts, I felt nothing but betrayal. I don't care anymore about anything. If you want repentance then slay me, $name.");
 			#:: Ding!
 			quest::ding();
-			#:: Depop without spawn timer
-      quest::depop();
 			#:: Set factions
 			quest::faction(404, 100);		#:: + Truespirit
 			#:: Spawn a City of Mist >> #Lord_Rak`Ashiir_  (90183), without grid or guild war, at the current location
 			quest::spawn2(90183, 0, 0, $x, $y, $z, 0);
+			#:: Depop without spawn timer
+			quest::depop();
 		}
-  }
+	}
 	else {
 		quest::say("Go away! We don't have time for the likes of you.");
+		#:: Return a 1673 - Child's Tear
+		quest::summonitem(1673);
 	}
-	
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
