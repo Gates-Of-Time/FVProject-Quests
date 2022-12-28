b363f896871ba3d11cef61056282257856210014
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 22:16:24 -0500

    Update Wraith_of_Jaxion.pl

--- citymist/Wraith_of_Jaxion.pl
+++ citymist/Wraith_of_Jaxion.pl
@@ -1,13 +1,11 @@
-# Test of Charm - enchanter epic - Enchanted Ruby
+# Test of Charm - Enchanter epic - Enchanted Ruby
 
 sub EVENT_ITEM {
 	#:: Match a 10633 - Dull Ruby
-  if (plugin::takeItems(10633 => 1)) {
+	if (plugin::takeItems(10633 => 1)) {
 		#:: Give a 10620 - Enchanted Ruby
-    quest::summonitem(10620);
-  }
-	
+		quest::summonitem(10620);
+	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
-
