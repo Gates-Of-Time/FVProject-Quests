28f4782c4fd6fde9b842cad8c33afbb1c9ceced0
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 22:11:30 -0500

    Update Azdalin.pl
    Syntax

--- burningwood/Azdalin.pl
+++ burningwood/Azdalin.pl
@@ -3,10 +3,10 @@ sub EVENT_COMBAT {
 	if ($combat_state == 1) {
 		#:: Create a scalar variable to store a random number from 1 - 60
 		my $Number = int(rand(60)) + 1;
-		if(random_result <=20) {
+		if($Number <=20) {
 			quest::say("$class like you always bring out the worst in me!");
 		}
-		elsif(random_result <=40) {
+		elsif($Number <=40) {
 			quest::say("I really hate $class like you!");
 		}
 		else {
