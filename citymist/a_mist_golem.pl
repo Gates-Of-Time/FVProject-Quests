758df1179288048849f339fcd76349ab6a8b519f
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 22:19:32 -0500

    Delete a_mist_golem.pl

--- citymist/a_mist_golem.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_DEATH_COMPLETE {
-	my $random = int(rand(100));
-	if ($random <= 10) {
-			#:: Spawn a City of Mist >> #a_raging_golem  (90190), without grid or guild war, at the current location
-   	quest::spawn2(90190, 0, 0, $x, $y, $z, $h);
-	}
-}
