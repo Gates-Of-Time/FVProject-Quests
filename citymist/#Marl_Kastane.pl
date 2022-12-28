84fe463a37b96b2e33a50cc877534b31efbc5239
TurmoilToad <TurmoilToad@gmail.com>
2022-12-27 21:47:29 -0500

    Update #Marl_Kastane.pl
    Refactor for consistency

--- citymist/#Marl_Kastane.pl
+++ citymist/#Marl_Kastane.pl
@@ -1,31 +1,31 @@
-# Part of SK Epic 1.0
-# items: 14384, 14383
+#:: Part of SK Epic 1.0
 
 sub EVENT_SAY {
-  $faction = $client->GetCharacterFactionLevel(404); #True Spirit
-  if ($faction >= 82) {
-    quest::say("Alas, I cannot be the one to carry the sword back to my people as proof in fear they will kill me to possess it for their own. I think a simple trade is in order. Perhaps you have a symbol or token of Lhanrc's that I could take back to the others to ease their worries?");
-  }
-  else {
-    quest::emote("seems to ignore you.");
-  }
+	#:: Create a scalar variable to store a faction value
+	$faction = $client->GetCharacterFactionLevel(404);	#:: Truespirit (404)
+	if ($faction >= 82) {
+		quest::say("Alas, I cannot be the one to carry the sword back to my people as proof in fear they will kill me to possess it for their own. I think a simple trade is in order. Perhaps you have a symbol or token of Lhanrc's that I could take back to the others to ease their worries?");
+	}
+	else {
+		quest::emote("seems to ignore you.");
+	}
 }
 
 sub EVENT_ITEM {
-  $faction = $client->GetCharacterFactionLevel(404); #True Spirit
-  if ($faction >= 82) {
+	#:: Create a scalar variable to store a faction value
+	$faction = $client->GetCharacterFactionLevel(404);	#:: Truespirit (404)
+	if ($faction >= 82) {
 		#:: Match a 14384 - Lhranc's Token
 		if (plugin::takeItems(14384 => 1)) {
-      quest::say("Very good, I will go deliver this right away.");
+			quest::say("Very good, I will go deliver this right away.");
 			#:: Give a 14383 - Innoruuk's Curse
-      quest::summonitem(14383);
+			quest::summonitem(14383);
 			#:: Ding!
 			quest::ding();
 			#:: Depop without spawn timer
-      quest::depop();
-    }
-  }
-	
+			quest::depop();
+		}
+	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
