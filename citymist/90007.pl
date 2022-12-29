714a1e6a5ec03e440111c2f88390113fc781b5a9
TurmoilToad <TurmoilToad@gmail.com>
2022-12-29 11:46:37 -0500

    Create 90007.pl

---
+++ citymist/90007.pl
@@ -0,0 +1,12 @@
+sub EVENT_DEATH_COMPLETE {
+	#:: Create a scalar variable to store a random number 1 to 100
+	my $random = int(rand(100)) + 1;
+	if ($random < 15) {
+		#:: Spawn a City of Mist >> #Neh`Ashiir (90184), without grid or guild war, at the current location
+		quest::spawn2(90184, 0, 0, $x, $y, $z, $h);
+	}
+	else {
+		#:: Spawn a City of Mist >> a_black_reaver  (90004), without grid or guild war, at the current location
+		quest::spawn2(90007, 0, 0, $x, $y, $z, $h);
+	}
+}
