28a10331663cda235e025905c1001bbea6756c2c
TurmoilToad <TurmoilToad@gmail.com>
2022-12-29 11:43:48 -0500

    Update and rename a_black_reaver.pl to 90004.pl

---
+++ citymist/90004.pl
@@ -0,0 +1,12 @@
+sub EVENT_DEATH_COMPLETE {
+	#:: Create a scalar variable to store a random number 1 to 100
+	my $random = int(rand(100)) + 1;
+	if ($random < 15) {
+		#:: Spawn a City of Mist >> Lord_Ghiosk (90180), without grid or guild war, at the current location
+		quest::spawn2(90180, 0, 0, $x, $y, $z, $h);
+	}
+	else {
+		#:: Spawn a City of Mist >> a_black_reaver  (90004), without grid or guild war, at the current location
+		quest::spawn2(90004, 0, 0, $x, $y, $z, $h);
+	}
+}
