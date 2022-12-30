be6c127f8de49de74345f8db4db7ccdacd818105
TurmoilToad <TurmoilToad@gmail.com>
2022-12-29 22:29:58 -0500

    Update Guide_I.pl
    Try without stop

--- akanon/Guide_I.pl
+++ akanon/Guide_I.pl
@@ -44,7 +44,6 @@ sub EVENT_TIMER {
 }
 
 sub EVENT_SAY {
-	$npc->StopNavigation();
 	if ($text=~/hail/i) {
 		quest::say("Greetings, lost soul. I am a guide, automaton, series G. I can assist you by leading you to all the important destinations in Ak'Anon. All you need to do is ask 'where' and state a valid destination. ..Bzzz.. Click!");
 	}
