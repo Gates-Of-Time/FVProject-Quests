0545edc393e0289bf9808ceaf84bf589bf22eaa1
TurmoilToad <TurmoilToad@gmail.com>
2023-01-14 14:13:05 -0500

    Update a_clockwork_receptionist.pl
    Attack message

--- akanon/a_clockwork_receptionist.pl
+++ akanon/a_clockwork_receptionist.pl
@@ -23,3 +23,14 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
+
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+	my $cur_target = $npc->GetHateTop();
+		if ($cur_target) {
+			my $target_name = $cur_target->GetCleanName();
+			quest::say("Time to die, $target_name!");
+		}
+	}
+}
