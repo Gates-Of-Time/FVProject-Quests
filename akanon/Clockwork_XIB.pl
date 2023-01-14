fd1031d4c1c79cf12464f80d8d9249bc87ea04a4
TurmoilToad <TurmoilToad@gmail.com>
2023-01-14 11:07:33 -0500

    Update Clockwork_XIB.pl
    MM

--- akanon/Clockwork_XIB.pl
+++ akanon/Clockwork_XIB.pl
@@ -8,3 +8,8 @@ sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
 }
+
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn a Ak'Anon >> Clockwork_MM (55164) at the current coordinates
+	quest::spawn2(55164, 0, 0, $x, $y, $z, $h+256);
+}
