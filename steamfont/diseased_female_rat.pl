9b24d8d9e73d8b183c5a8b5db63d67de3f045aee
TurmoilToad <TurmoilToad@gmail.com>
2023-01-14 14:11:55 -0500

    Update diseased_female_rat.pl
    Formatting

--- steamfont/diseased_female_rat.pl
+++ steamfont/diseased_female_rat.pl
@@ -1,8 +1,8 @@
 sub EVENT_DEATH_COMPLETE {
 	#:: Spawn Steamfont Mountains >> a_young_plague_rat (56086)
-	quest::spawn2(56086,0,0,$x,$y,$z,$h);
-	quest::spawn2(56086,0,0,$x+5,$y+5,$z,$h);
-	quest::spawn2(56086,0,0,$x+5,$y-5,$z,$h);
-	quest::spawn2(56086,0,0,$x-5,$y+5,$z,$h);
-	quest::spawn2(56086,0,0,$x-5,$y-5,$z,$h);
+	quest::spawn2(56086, 0, 0, $x , $y, $z, $h);
+	quest::spawn2(56086, 0, 0, $x + 5, $y + 5, $z, $h);
+	quest::spawn2(56086, 0, 0, $x + 5, $y - 5, $z, $h);
+	quest::spawn2(56086, 0, 0, $x - 5, $y + 5, $z, $h);
+	quest::spawn2(56086, 0, 0, $x - 5, $y - 5, $z, $h);
 }
