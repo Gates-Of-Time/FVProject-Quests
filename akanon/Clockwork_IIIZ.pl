cf0ab25e2d9973622f75f01d7f82635daeeda705
TurmoilToad <TurmoilToad@gmail.com>
2023-01-14 11:13:24 -0500

    Update Clockwork_IIIZ.pl
    Attack / death

--- akanon/Clockwork_IIIZ.pl
+++ akanon/Clockwork_IIIZ.pl
@@ -12,7 +12,12 @@ sub EVENT_ITEM {
 	plugin::returnUnusedItems();
 }
 
+sub EVENT_AGGRO {
+	quest::say("Click... Tick... City violation. Code 5150. This unit is authorized to execute punishment. Please stand still. Thank you.");
+}
+
 sub EVENT_DEATH_COMPLETE {
+	quest::say("Bzzzz... Click... This unit is terminally damaged. Destruction of city property is a violation of city code 22468. All units shall be notified of your offense...");
 	#:: Spawn a Ak'Anon >> Clockwork_MM (55164) at the current coordinates
 	quest::spawn2(55164, 0, 0, $x, $y, $z, $h+256);
 }
