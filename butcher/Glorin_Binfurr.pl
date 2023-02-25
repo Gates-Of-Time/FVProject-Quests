a88d8ede045b5b6647973d5d80ab48bf4f6531f8
TurmoilToad <TurmoilToad@gmail.com>
2023-02-25 11:33:24 -0500

    Update Glorin_Binfurr.pl
    attack, slay, death messages

--- butcher/Glorin_Binfurr.pl
+++ butcher/Glorin_Binfurr.pl
@@ -1,3 +1,18 @@
+sub EVENT_COMBAT {
+	#:: Match combat state 1 - entered combat
+	if ($combat_state == 1) {
+		quest::say("For the glory of Kaladim, have at thee!!");
+	}
+}
+
+sub EVENT_NPC_SLAY {
+	quest::say("For the glory of Kaladim!! You are no more.");
+}
+
+sub EVENT_SLAY {
+	quest::say("For the glory of Kaladim!! You are no more.");
+}
+
 sub EVENT_SAY {	
 	if ($text=~/hail/i) {
 		quest::say("Pleasure to meet you.  Keep your eye out for the ship.  You don't want to miss it.");
@@ -19,4 +34,8 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
 	#:: Return unused items
 	plugin::returnUnusedItems();
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("My brothers of the Stormguard shall avenge me!! Your days are numbered!");
 }
