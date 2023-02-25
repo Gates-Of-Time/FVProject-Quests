a8182a70221476ffcc73729d061695928611ee3c
TurmoilToad <TurmoilToad@gmail.com>
2023-02-25 11:18:33 -0500

    Create a_goblin.pl
    attack and death messages

---
+++ butcher/a_goblin.pl
@@ -0,0 +1,7 @@
+sub EVENT_AGGRO {
+	quest::say("Ugly creature near my feet, me thinks that you'll be good to eat!");
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("I wiggled my fingers and waved my hands. But there is no glory in failed last stands!");
+}
