7f87ab04556b5b4fe94b2114c2c3ddc9f44ca15f
TurmoilToad <TurmoilToad@gmail.com>
2023-02-25 11:37:22 -0500

    Create an_orc_scoutsman.pl
    attack and death messages

---
+++ butcher/an_orc_scoutsman.pl
@@ -0,0 +1,7 @@
+sub EVENT_AGGRO {
+	quest::say("Death!! Death to all who oppose the Crushbone orcs!!");
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You shall have all the Crushbone orc legions on your tail for my death!");
+}
