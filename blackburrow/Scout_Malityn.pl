cc899194a6d3d7abec1ecb6628272f010e1370c0
TurmoilToad <TurmoilToad@gmail.com>
2023-02-25 11:10:41 -0500

    Create Scout_Malityn.pl
    attack and death message

---
+++ blackburrow/Scout_Malityn.pl
@@ -0,0 +1,7 @@
+sub EVENT_AGGRO {
+	quest::say("In the name of the Rainkeeper, I strike at thee!!");
+}
+
+sub EVENT_DEATH_COMPLETE {
+	quest::say("You will meet your own doom soon enough, my friend.. my influence with the Knights of Thunder is great, and they will not rest until I am avenged!");
+}
