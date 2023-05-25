7b8aed42eb050748ed1bdebc44a59f302df1123e
Nazwadi <nazwadi@pm.me>
2023-05-25 22:19:23 +0000

    Fix typo on Translocator Narrik

--- oot/#Translocator_Narrik.pl
+++ oot/#Translocator_Narrik.pl
@@ -1,6 +1,6 @@
 sub EVENT_SAY { 
 	if ($text=~/hail/i) {
-		quest::say("Hello there, $name. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Freeport] or [Buthcherblock] in the meantime, I can transport you to my companion there.");
+		quest::say("Hello there, $name. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Freeport] or [Butcherblock] in the meantime, I can transport you to my companion there.");
 	}
 	elsif ($text=~/freeport/i) {
 		quest::say("Off you go!");
