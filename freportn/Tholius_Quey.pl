da4840b11bef6392f0e1477f8ddc42cd6acf8191
Aleksander Spro <abspro@gmail.com>
2023-09-27 08:44:15 +0200

    Fix quest handing for clrs following Erollisi Marr Wrong item accepted

--- freportn/Tholius_Quey.pl
+++ freportn/Tholius_Quey.pl
@@ -11,8 +11,8 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-	#:: Match a 18735 - A Tattered Note
-	if (plugin::takeItems(18735 => 1 )) {
+	#:: Match a 18736 - Tattered Note
+	if (plugin::takeItems(18736 => 1 )) {
 		quest::say("Welcome to the Priests of Marr. Here, you will be taught how powerful passion truly is. The passion of Erollisi Marr, the Queen of Love, shall flow through you and into all those you meet. Wear this tunic in the name of Love.");
 		#:: Give a 13556 - White and Blue Tunic*
 		quest::summonitem(13556);
