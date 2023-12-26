4229642eec856ddb618b4812a2e23ec31b524586
TurmoilToad <TurmoilToad@gmail.com>
2023-12-26 16:25:10 -0500

    Update Swirlspine_Guardian.pl
    Based on Noudess file

--- kedge/Swirlspine_Guardian.pl
+++ kedge/Swirlspine_Guardian.pl
@@ -1,6 +1,13 @@
 sub EVENT_SPAWN {
 	#:: Create a timer 'depop' that triggers every 1800 seconds (30 minutes)
 	quest::settimer("depop", 1800);
+ 	#:: Create a scalar variable to store a random number
+  	$roll = int(rand(4));
+	#:: Match if random number is 0 (25 percent)
+	if ($roll == 0) {
+ 		#:: Add see special ability see invis
+		$npc->ModifyNPCStat("see_invis",  "1");
+	}
 }
 
 sub EVENT_TIMER {
