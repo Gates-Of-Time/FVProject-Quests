08e6374da6f0d12f3afb31b7b86241762f46b2ae
Aleksander Spro <abspro@gmail.com>
2024-01-07 17:05:08 +0100

    Refactor to fit FVP syntax

--- citymist/Lhranc.pl
+++ citymist/Lhranc.pl
@@ -1,6 +1,3 @@
-# Part of SK Epic 1.0
-# items: 14367, 14368, 14369, 14370, 14384
-
 sub EVENT_SAY {
   $faction = $client->GetCharacterFactionLevel(404); #True Spirit
   if ($faction >=42) {
@@ -22,15 +19,21 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
   $faction = $client->GetCharacterFactionLevel(404); #True Spirit
   if ($faction >= 72) {
-    if (plugin::check_handin(\%itemcount, 14367 => 1, 14368 => 1, 14369 => 1, 14370 => 1)) { #Corrupted Ghoulbane, Heart of the Innocent, Head of the Valiant, Will of Innoruuk
+	#:: Match a 14367 - Corrupted Ghoulbane, a 14368 - Heart of the Innocent, a 14369 - Head of the Valiant, and a 14370 - Will of Innoruuk
+    if (plugin::check_handin(\%itemcount, 14367 => 1, 14368 => 1, 14369 => 1, 14370 => 1)) {
       quest::say("Ahh, at last I can free myself of this prison! Centuries have passed since I enjoyed the fruits of mortality.' He hurls the components above his head where they remain suspended in midair. They hover in place while he chants the words of an ancient spell. As he speaks, the items begin to rotate around his head, slowly at first, then faster as the chanting grows louder, until it spins in a blur. Soon, the room is filled with a deafening shriek that pierces you to the core.");
-      quest::faction(404, 10); #True Spirit
-      quest::summonitem(14384); #Lhranc's Token
-      quest::spawn2(90187, 0, 0, 85.1, 0.9, 7.8, 384); ##Lhranc
+      #:: Give a 14384 - Lhranc's Token
+      quest::summonitem(14384);
+      #:: Set factions
+	  quest::faction(404, 10);		#:: + True Spirit
+      #:: Ding!
+      quest::ding();
+      #:: Spawn a City of Mist >> #Lhranc (90187), without grid or guild war, at the current location
+      quest::spawn2(90187, 0, 0, 85.1, 0.9, 7.8, 384);
+      #:: Depop without spawn timer
       quest::depop_withtimer();
     }
   }
-  plugin::return_items(\%itemcount);
+	#:: Return unused items
+	plugin::returnUnusedItems();
 }
-
-# Quest by mystic414
