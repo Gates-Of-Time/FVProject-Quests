1e1637464b505cdb558b69b7fea2fe35c1aaec2b
Aleksander Spro <abspro@gmail.com>
2025-02-21 12:38:58 +0100

    Different approach to get dark elves to druids

--- eastkarana/Dark_Elf_Corruptor.pl
+++ eastkarana/Dark_Elf_Corruptor.pl
@@ -2,8 +2,6 @@ sub EVENT_SPAWN {
 	#:: Create a timer 'despawn' that triggers in 300 seconds (5 min)
   	quest::settimer("despawn", 300);
 	quest::SetRunning(1);
-	#:: Move to the specified location and guard 
-	quest::moveto(-1558,-3558,3,0,1);
 }
 
 sub EVENT_TIMER {
@@ -27,4 +25,19 @@ sub EVENT_COMBAT {
 sub EVENT_DEATH_COMPLETE {
 	#:: Stop the timer "despawn"
   	quest::stoptimer("despawn");
+}
+
+sub EVENT_WAYPOINT_ARRIVE {
+	if ($wp == 1) {
+		#:: Get mob East Karana >>  Althele (15044)
+		my $mob = $entity_list->GetMobID(15044);
+		if($mob) {
+			#:: Add the Althele to the hate list of the Dark_Elf_Corruptor
+			my $mobnpc = $mob->CastToNPC();
+			if($mobnpc) {
+				quest::shout("Innoruuk claims your souls and this land!");
+				$npc->AddToHateList($mobnpc, 1);
+			}
+		}
+	}
 }
--- eastkarana/Dark_Elf_Reaver.pl
+++ eastkarana/Dark_Elf_Reaver.pl
@@ -28,3 +28,18 @@ sub EVENT_DEATH_COMPLETE {
 	#:: Stop the timer "despawn"
   	quest::stoptimer("despawn");
 }
+
+sub EVENT_WAYPOINT_ARRIVE {
+	if ($wp == 1) {
+		#:: Get mob East Karana >>  Althele (15044)
+		my $mob = $entity_list->GetMobID(15044);
+		if($mob) {
+			#:: Add the Althele to the hate list of the Dark_Elf_Corruptor
+			my $mobnpc = $mob->CastToNPC();
+			if($mobnpc) {
+				quest::shout("Innoruuk claims your souls and this land!");
+				$npc->AddToHateList($mobnpc, 1);
+			}
+		}
+	}
+}
