f9386d9bffe2e79150fc7e552cdbc7ac9184e1c5
Aleksander Spro <abspro@gmail.com>
2025-03-24 20:34:10 +0100

    Fixes to scripts

--- halas/Dargon.pl
+++ halas/Dargon.pl
@@ -15,39 +15,25 @@ sub EVENT_ITEM {
 			quest::depop_withtimer();
 		}
 		#:: Match a 14335 - Arantir's Ring
-		if(plugin::takeItems(14335 => 1)){
+		elsif(plugin::takeItems(14335 => 1)){
 			#:: Spawn one and only one Halas >> Arantir_Karondor (29089), without grid or guild war, at the current location
 			quest::unique_spawn(29089,0,0,$x,$y,$z,$h);
-			#:: Create a timer 'sendsignala' that triggers every 3 seconds
-			quest::settimer("sendsignala",3);
+
+    		quest::set_data("wizepicA",1);
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 		#:: Match a 18168 - Note to Arantir
-		if(plugin::takeItemsn(18168 => 1)) {
+		elsif(plugin::takeItemsn(18168 => 1)) {
 			#:: Spawn one and only one Halas >> Arantir_Karondor (29089), without grid or guild war, at the current location
 			quest::unique_spawn(29089,0,0,$x,$y,$z,$h);
-			#:: Create a timer 'sendsignalb' that triggers every 3 seconds
-			quest::settimer("sendsignalb",3);
+			
+			quest::delete_data("wizepicA");
+			quest::set_data("wizepicB",1);
+			#:: Depop with spawn timer active
+			quest::depop_withtimer();
 		}
 	}
 	#:: Return unused items
 	plugin::returnUnusedItems();
-}
-
-sub EVENT_TIMER {
-	if ($timer eq "sendsignala") {
-		#:: Send a signal '101' to Halas >> Arantir_Karondor (29089) with no delay
-		quest::signalwith(29089,101,0); #signal arantir to set wizepicA = 1
-		#:: Stop the timer 'sendsignala' from triggering
-		quest::stoptimer("sendsignala");
-		#:: Depop with spawn timer active
-		quest::depop_withtimer();
-	}
-	if ($timer eq "sendsignalb") {
-		#:: Send a signal '102' to Halas >> Arantir_Karondor (29089) with no delay
-		quest::signalwith(29089,102,0); #signal arantir to set wizepicB = 1
-		#:: Stop the timer 'sendsignalb' from triggering
-		quest::stoptimer("sendsignalb");
-		#:: Depop with spawn timer active
-		quest::depop_withtimer();
-	}
 }
