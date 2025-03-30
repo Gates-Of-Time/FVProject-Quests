20d687e2ad07f3d5f02951c35123b5c33dcff48f
Aleksander Spro <abspro@gmail.com>
2025-03-30 15:06:08 +0200

    Only 1 spawn

--- timorous/Dolgin_Codslayer.pl
+++ timorous/Dolgin_Codslayer.pl
@@ -19,8 +19,8 @@ sub EVENT_ITEM {
 		quest::say("What's this? Egads! It's perfect.  Now all we gotta do is hope he bites.' Dolgin takes a little of the essence and smears it on a piece of bread. He places the bait into the water and watches intently.");
 		#:: Give a 20696 - Froglok Essence
 		quest::summonitem(20696);
-		#:: Spawn a Timorous Deep >> #Faydedar (96073), without grid or guildwar, at the specified coordinates
-		quest::spawn2(96073, 0, 0, -2178, -11745, -0.63, 126);
+		#:: Spawn one and only a Timorous Deep >> #Faydedar (96073), without grid or guildwar, at the specified coordinates
+		quest::unique_spawn(96073, 0, 0, -2178, -11745, -0.63, 126);
 		#:: Depop with spawn timer active
 		quest::depop_withtimer();
 	}
