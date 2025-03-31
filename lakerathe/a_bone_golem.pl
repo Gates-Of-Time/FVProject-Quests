41770e9facb5989cd8a0ce958fd01831423adcc4
Aleksander Spro <abspro@gmail.com>
2025-03-31 16:45:37 +0200

    Use a more safe event

--- lakerathe/a_bone_golem.pl
+++ lakerathe/a_bone_golem.pl
@@ -1,4 +1,4 @@
-sub EVENT_DEATH {
+sub EVENT_DEATH_COMPLETE {
 	#:: Spawn one and only a lake Rathetear >> a_failed_apprentice (51152), without grid or guild war, at the given location
     quest::unique_spawn(51152,0,0,-1518.9,347.6,53,98.9);
 }
--- lakerathe/a_failed_apprentice.pl
+++ lakerathe/a_failed_apprentice.pl
@@ -1,4 +1,4 @@
-sub EVENT_DEATH {
+sub EVENT_DEATH_COMPLETE {
 	#:: Spawn one and only a lake Rathetear >> a_tortured_soul (51144), without grid or guild war, at the given location
     quest::unique_spawn(51144,0,0,-921,210,72.3,31);
 }
