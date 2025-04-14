2f62de04a81ba6fd747296c17ef60b6b5d6311ab
Aleksander Spro <abspro@gmail.com>
2025-04-14 18:17:35 +0200

    script syntax fix

--- kaesora/spectral_librarian.pl
+++ kaesora/spectral_librarian.pl
@@ -1,10 +1,10 @@
 sub EVENT_ITEM {
 	if ( quest::is_content_flag_enabled("Kunark_EpicsEra")) {
-		#:: Match a 4196 - Dull Diamond
+		#:: Match a 10631 - Dull Diamond
 		if (plugin::takeItems(10631 => 1)) {
 			quest::say("You have no idea what it is like to be trapped in this place. Take your precious little stone and leave this place.");
 			#:: Give a 10618 - Enchanted Diamond
-			quest::SummonItem(10618);
+			quest::summonitem(10618);
 			#:: Ding!
 			quest::ding();
 			#:: Spawn Kaesora >> enraged_spectral_librarian (88087) at the current location
