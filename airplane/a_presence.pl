d2ac6d67ed42c40e4fb026f16177bfbfde4386ff
Aleksander Spro <abspro@gmail.com>
2025-04-29 17:44:54 +0200

    Fix Gwan spawn id

--- airplane/a_presence.pl
+++ airplane/a_presence.pl
@@ -11,7 +11,7 @@ sub EVENT_ITEM {
 			# Monk Epic 1.0
 			quest::say("Hahaha! That dolt Eejag fell to the likes of you? Im not surprised. So, I guess this means you are here to challenge me. Normally, I wouldnt waste my time, but since you have defeated my younger brother, I suppose Im obligated.");
 			#:: Spawn one and only one The Plane of Sky >> Gwan (71069), with no grid or guildwar, at the current location
-			quest::spawn2(27119, 0, 0, $x, $y, -8, $h);
+			quest::spawn2(71069, 0, 0, $x, $y, -8, $h);
 			#:: Depop with spawn timer active
 			quest::depop_withtimer();
 		}
