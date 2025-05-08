48993edd2d5a4de115082c1a5005a1ba709fd64d
Aleksander Spro <abspro@gmail.com>
2025-05-08 18:01:55 +0200

    Fix missing shm epic quests

--- mistmoore/an_advisor.pl
+++ mistmoore/an_advisor.pl
@@ -1,5 +1,11 @@
 sub EVENT_SAY {
 	if($text=~/hail/i) {
+		quest::say("How dare you speak to me, you impudent sack of nauseating refuse! For this outrage of insubordination I call forth the vengeance of House Mistmoore! You now have little time left to live.  Enjoy it.");
 		quest::attack($name);
 	}
+}
+
+sub EVENT_DEATH_COMPLETE {
+	#:: Spawn one and only one The Castle of Mistmoore >> Black Dire (59129), without grid or guild war, at the given location
+	quest::unique_spawn(59129,0,0,-165,90,-221,56);
 }
